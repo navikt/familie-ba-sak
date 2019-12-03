8ed386d7bfe96e82fc862c199ce93bd68af1d4ba
Henning Solberg <henning.solberg@nav.no>
2019-12-03 20:43:02 +0100

    Lagt inn e2e profil.
    Kjører integrasjonstester på gha

--- src/test/kotlin/no/nav/familie/ba/sak/BehandlingIntegrationTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/BehandlingIntegrationTest.kt
@@ -3,7 +3,9 @@ package no.nav.familie.ba.sak
 import no.nav.familie.ba.sak.behandling.BehandlingslagerService
 import no.nav.familie.ba.sak.util.DbContainerInitializer
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -17,12 +19,14 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 @ExtendWith(SpringExtension::class)
 @ContextConfiguration(initializers = [DbContainerInitializer::class])
 @ActiveProfiles("postgres")
+@Tag("integration")
 class BehandlingIntegrationTest  {
 
     @Autowired
     private lateinit var behandlingslagerService: BehandlingslagerService
 
     @Test
+    @Tag("integration")
     fun `Kjør flyway migreringer og sjekk at behandlingslagerservice klarer å lese å skrive til postgresql`() {
         behandlingslagerService.nyBehandling("0","123456789010", "sdf")
         Assertions.assertEquals(1, behandlingslagerService.hentAlleBehandlinger().size)
