41a6453ad793a7af52e9df1df305eb85586a5f38
Henning Håkonsen <henninghaakonsen@me.com>
2019-12-16 21:49:37 +0100

    Fikser feil i flyway script for barn på vedtak.

--- src/main/resources/db/migration/V6__vedtak_barn.sql
+++ src/main/resources/db/migration/V6__vedtak_barn.sql
@@ -1,12 +1,13 @@
 create table BEHANDLING_VEDTAK_BARN
 (
     id                      bigint                                                  primary key,
-    fk_behandling_vedtak_id bigint       references BEHANDLING_VEDTAK_BARN (id)     not null,
+    fk_behandling_vedtak_id bigint       references BEHANDLING_VEDTAK (id)          not null,
     fk_person_id            bigint       references po_person (id)                  not null,
     versjon                 bigint       default 0                                  not null,
     opprettet_av            VARCHAR(20)  default 'VL'                               not null,
     opprettet_tid           TIMESTAMP(3) default localtimestamp                     not null,
     stonad_fom              TIMESTAMP(3)                                            not null,
+    belop                   numeric,
     endret_av               VARCHAR(20),
     endret_tid              TIMESTAMP(3)
 );
--- src/main/resources/db/migration/V7__vedtak_barn_belop.sql
+++
@@ -1,1 +0,0 @@
-alter table BEHANDLING_VEDTAK_BARN add column belop numeric;
