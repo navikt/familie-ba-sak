7e3a1a2abdd2c7669fbd42324e457a80717f9a39
Meng Zhu <mengzhu331@outlook.com>
2019-12-17 14:04:21 +0100

    fix PR issues

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/DokGenKlient.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/DokGenKlient.kt
@@ -19,9 +19,6 @@ class DokGenKlient(
         val url = URI.create(dokgenServiceUri + "/template/markdown/to-html")
         val response = utførRequest(POST, TEXT_MARKDOWN, url, markdown)
 
-        if (!response.statusCode.is2xxSuccessful) {
-            throw RuntimeException(response.toString()) //TODO feilhåndtering
-        }
         return response.body.orEmpty()
     }
 
@@ -29,15 +26,13 @@ class DokGenKlient(
         val url = URI.create(dokgenServiceUri + "/template/" + malNavn + "/create-markdown")
         val response = utførRequest(POST, APPLICATION_JSON, url, fletteFelter)
 
-        if (!response.statusCode.is2xxSuccessful) {
-            throw RuntimeException(response.toString()) //TODO feilhåndtering
-        }
         return response.body.orEmpty()
     }
 
     fun utførRequest(httpMethod: HttpMethod, mediaType: MediaType, requestUrl: URI, requestBody: Any? = null): ResponseEntity<String> {
         val headers = HttpHeaders()
         headers.contentType = mediaType
+        headers.acceptCharset.add(Charsets.UTF_8)
         headers.add(NavHttpHeaders.NAV_CALL_ID.asString(), MDC.get(MDCConstants.MDC_CALL_ID))
 
         return restTemplate.exchange(requestUrl, httpMethod, HttpEntity(requestBody, headers), String::class.java)
