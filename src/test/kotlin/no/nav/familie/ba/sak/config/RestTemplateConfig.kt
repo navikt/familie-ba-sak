f9a6dd31cf8fa508cf37435707a63ed2ab9a7838
Meng Zhu <mengzhu331@outlook.com>
2019-12-17 19:45:38 +0100

    recover RestTemplateConfig

--- src/test/kotlin/no/nav/familie/ba/sak/config/RestTemplateConfig.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/config/RestTemplateConfig.kt
@@ -1,18 +1,36 @@
 package no.nav.familie.ba.sak.config
 
 import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
-import org.springframework.http.converter.StringHttpMessageConverter
 import org.springframework.web.client.RestTemplate
-import java.nio.charset.StandardCharsets
+import java.time.Duration
 
+
+@Configuration
 @TestConfiguration
 @Profile("dev")
 class RestTemplateConfig {
+    @Bean
+    fun restTemplateMedProxy(): RestTemplate {
+        return RestTemplateBuilder()
+                .build()
+    }
 
     @Bean
+    @Primary
     fun restTemplate(): RestTemplate {
-        return RestTemplate(listOf(StringHttpMessageConverter(StandardCharsets.UTF_8)))
+        return RestTemplate()
+    }
+
+    @Bean
+    @Primary
+    fun restTemplateBuilderMedProxy(): RestTemplateBuilder {
+        return RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(5))
     }
 }
