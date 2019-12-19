428552109894db31c340654c01e48bb840bcad58
Henning Håkonsen <henninghaakonsen@me.com>
2019-12-19 15:51:52 +0100

    Mindre fikser for flyt mellom steg 2 og 3 i manuell saksbehandling.

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/domene/vedtak/BehandlingVedtakRepository.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/domene/vedtak/BehandlingVedtakRepository.kt
@@ -7,9 +7,6 @@ interface BehandlingVedtakRepository : JpaRepository<BehandlingVedtak?, Long?> {
     @Query(value = "SELECT bv FROM BehandlingVedtak bv JOIN bv.behandling b WHERE b.id = :behandlingId")
     fun finnVedtakForBehandling(behandlingId: Long?): List<BehandlingVedtak?>
 
-    @Query(value = "SELECT bv FROM BehandlingVedtak bv JOIN bv.behandling b WHERE b.id = :behandlingId")
-    fun finnBehandlingVedtak(behandlingId: Long?): BehandlingVedtak?
-
     @Query("SELECT bv FROM BehandlingVedtak bv JOIN bv.behandling b WHERE b.id = :behandlingId AND bv.aktiv = true")
     fun findByBehandlingAndAktiv(behandlingId: Long?): BehandlingVedtak?
 }
