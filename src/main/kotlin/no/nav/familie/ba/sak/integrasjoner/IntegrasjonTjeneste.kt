b6493f792d6f72a535f01d1d3513670047679bf8
Meng Zhu <meng.zhu@nav.no>
2020-01-21 13:44:18 +0100

    use familie-integrasjoner to archive barnetrygd vedtak

--- src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/IntegrasjonTjeneste.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/IntegrasjonTjeneste.kt
@@ -1,17 +1,26 @@
 package no.nav.familie.ba.sak.integrasjoner
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import no.nav.familie.ba.sak.common.BaseService
 import no.nav.familie.ba.sak.integrasjoner.IntegrasjonException
 import no.nav.familie.ba.sak.integrasjoner.domene.Personinfo
 import no.nav.familie.ba.sak.personopplysninger.domene.AktørId
 import no.nav.familie.kontrakter.felles.Ressurs
+import no.nav.familie.kontrakter.felles.arkivering.ArkiverDokumentRequest
+import no.nav.familie.kontrakter.felles.arkivering.ArkiverDokumentResponse
+import no.nav.familie.kontrakter.felles.arkivering.Dokument
+import no.nav.familie.kontrakter.felles.arkivering.FilType
 import no.nav.familie.kontrakter.felles.objectMapper
+import no.nav.familie.log.NavHttpHeaders
+import no.nav.familie.log.mdc.MDCConstants
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import org.slf4j.LoggerFactory
+import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
-import org.springframework.http.ResponseEntity
+import org.springframework.http.*
 import org.springframework.retry.annotation.Backoff
 import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
@@ -58,6 +67,30 @@ class IntegrasjonTjeneste (
         } catch (e: RestClientException) {
             throw IntegrasjonException("Kall mot integrasjon feilet ved uthenting av personinfo", e, uri, personIdent)
         }
+    }
+
+    @Retryable(value = [IntegrasjonException::class], maxAttempts = 3, backoff = Backoff(delay = 5000))
+    fun arkiverVedtakPdf(fnr: String, pdfByteArray: ByteArray): String?{
+        val uri = URI.create("$integrasjonerServiceUri/arkiv/v1")
+        logger.info("Sender vedtak pdf til DokArkiv: ${uri}");
+
+        return runCatching<ArkiverDokumentResponse> {
+            val dokumenter= listOf(Dokument(pdfByteArray, FilType.PDFA, "ba_vb.pdf", null, "BARNETRYGD_VEDTAKSBREV"))
+            val arkiverDokumentRequest= ArkiverDokumentRequest(fnr, true, dokumenter)
+            val arkiverDokumentResponse= sendArkivRequest(uri, arkiverDokumentRequest)
+            arkiverDokumentResponse
+        }.onFailure {
+            logger.error("Archiving failed for FNR ${fnr}: ${it.message}")
+        }.getOrNull()?.journalpostId
+    }
+
+    private fun sendArkivRequest(integrasjonerDokArkivPath : URI, arkiverDokumentRequest: ArkiverDokumentRequest): ArkiverDokumentResponse {
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+        headers.add(NavHttpHeaders.NAV_CALL_ID.asString(), MDC.get(MDCConstants.MDC_CALL_ID))
+        val response= restTemplate.exchange(integrasjonerDokArkivPath, HttpMethod.POST, HttpEntity<Any>(arkiverDokumentRequest, headers), Ressurs::class.java)
+        val mapper= ObjectMapper().registerKotlinModule()
+        return mapper.convertValue(response.body!!.data, ArkiverDokumentResponse::class.java)
     }
 
     companion object {
