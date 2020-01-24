d563b4bc54ccb80d5941b3759391370fca034a9e
Martine Enger <marenger@student.matnat.uio.no>
2020-01-24 09:40:31 +0100

    Send 400 opp til frontend når man prøver å opprette dobbel behandling

--- src/main/kotlin/no/nav/familie/ba/sak/mottak/MottakController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/mottak/MottakController.kt
@@ -9,10 +9,13 @@ import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.sikkerhet.OIDCUtil
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.security.token.support.core.exceptions.JwtTokenValidatorException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.lang.IllegalStateException
 
 @RestController
 @RequestMapping("/api")
@@ -23,18 +26,32 @@ class MottakController(
         private val fagsakService: FagsakService
 ) {
     @PostMapping(path = ["/behandling/opprett"])
-    fun opprettBehandling(@RequestBody nyBehandling: NyBehandling): Ressurs<RestFagsak> {
+    fun opprettBehandling(@RequestBody nyBehandling: NyBehandling): ResponseEntity<Ressurs<RestFagsak>> {
         val saksbehandlerId = try {
             oidcUtil.getClaim("preferred_username") ?: "VL"
         } catch (e: JwtTokenValidatorException) {
             "VL"
         }
 
-        FagsakController.logger.info("{} oppretter ny behandling", saksbehandlerId)
+        return Result.runCatching {
 
-        val fagsak = behandlingService.opprettBehandling(nyBehandling)
+            FagsakController.logger.info("{} oppretter ny behandling", saksbehandlerId)
 
-        return fagsakService.hentRestFagsak(fagsakId = fagsak.id)
+            val fagsak = behandlingService.opprettBehandling(nyBehandling)
+
+            fagsakService.hentRestFagsak(fagsakId = fagsak.id)
+        }.fold(
+                onSuccess = {
+                    ResponseEntity.ok(it)
+                },
+                onFailure = {
+                    if (it is IllegalStateException)
+                        ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                                Ressurs.failure("Kan ikke opprette ny behandling på fagsak med eksisterende aktiv behandling")
+                        )
+                    else ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Ressurs.failure(it.message))
+                }
+        )
     }
 }
 
