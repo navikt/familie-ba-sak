c11c75e77826c7511e67d2030be484b007d3d847
Tor Nærland <tor.nerland@nav.no>
2020-01-29 13:45:39 +0100

    Legger til MessageConverter ByteArray i RestTemplateConfig

--- src/test/kotlin/no/nav/familie/ba/sak/config/RestTemplateConfig.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/config/RestTemplateConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.http.converter.ByteArrayHttpMessageConverter
 import org.springframework.http.converter.StringHttpMessageConverter
 import org.springframework.web.client.RestTemplate
 import java.nio.charset.StandardCharsets
@@ -14,7 +15,7 @@ import java.time.Duration
 class RestTemplateConfig {
     @Bean
     fun restTemplate(): RestTemplate {
-        return RestTemplate(listOf(StringHttpMessageConverter(StandardCharsets.UTF_8)))
+        return RestTemplate(listOf(StringHttpMessageConverter(StandardCharsets.UTF_8), ByteArrayHttpMessageConverter()))
     }
 
     @Bean
