525d3b022168ec59fb904d78106b1a8a81ef1de0
Henning Håkonsen <henninghaakonsen@me.com>
2020-02-06 12:00:14 +0100

    Lagt til status og type fra behandling på restdomene klassene til frontend.

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/restDomene/RestBehandling.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/restDomene/RestBehandling.kt
@@ -1,6 +1,11 @@
 package no.nav.familie.ba.sak.behandling.restDomene
 
+import no.nav.familie.ba.sak.behandling.domene.BehandlingStatus
+import no.nav.familie.ba.sak.behandling.domene.BehandlingType
+
 data class RestBehandling(val aktiv: Boolean,
                           val behandlingId: Long?,
+                          val type: BehandlingType,
+                          val status: BehandlingStatus,
                           val barnasFødselsnummer: List<String?>?,
                           val vedtakForBehandling: List<RestVedtak?>)
