d7515aeedb36e22929df71b047aadc50f44b0e5a
Meng Zhu <meng.zhu@nav.no>
2020-02-06 23:09:23 +0100

    disable tests that need dokgen

--- src/test/kotlin/no/nav/familie/ba/sak/behandling/DokGenServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/behandling/DokGenServiceTest.kt
@@ -6,6 +6,7 @@ import no.nav.familie.ba.sak.behandling.domene.Fagsak
 import no.nav.familie.ba.sak.behandling.domene.vedtak.Vedtak
 import no.nav.familie.ba.sak.behandling.domene.vedtak.VedtakResultat
 import no.nav.familie.ba.sak.personopplysninger.domene.PersonIdent
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,6 +19,7 @@ import java.time.LocalDate
 @SpringBootTest
 @ExtendWith(SpringExtension::class)
 @ActiveProfiles("dev")
+@Disabled("DokGen must be available")
 class DokGenServiceTest(@Autowired
                         private val dokGenService: DokGenService) {
 
