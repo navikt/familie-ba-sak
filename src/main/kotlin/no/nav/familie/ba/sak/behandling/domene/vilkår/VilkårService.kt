744d68eb0bf471cecb8b76ca758bf241f02c931e
Henning Håkonsen <henninghaakonsen@me.com>
2020-02-12 09:14:03 +0100

    Legger inn infomelding om endret status på behandling.

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/domene/vilkår/VilkårService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/domene/vilkår/VilkårService.kt
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service
 
 @Service
 class VilkårService {
+
     fun vurderVilkår(personopplysningGrunnlag: PersonopplysningGrunnlag,
                      restSamletVilkårResultat: List<RestVilkårResultat>): SamletVilkårResultat {
         val samletVilkårResultat = SamletVilkårResultat()
