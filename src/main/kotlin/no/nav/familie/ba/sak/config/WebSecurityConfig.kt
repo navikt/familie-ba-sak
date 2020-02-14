6f3708b86e0cf04d4d49e83cec080821b77e7bb9
Tor Nærland <tor.nerland@nav.no>
2020-02-14 19:23:11 +0100

    legger til WebSecurityConfig

---
+++ src/main/kotlin/no/nav/familie/ba/sak/config/WebSecurityConfig.kt
@@ -0,0 +1,12 @@
+package no.nav.familie.ba.sak.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+class WebSecurityConfig : WebSecurityConfigurerAdapter()
