0012c0e54c2bf1beb47c2a5dadb0464942ad1d81
Geirmund Simonsen <geirmund.simonsen@nav.no>
2020-02-18 19:02:23 +0100

    Logikk for å sjekke diskresjonskode flyttet til egen fil, refaktorert til å ta imot en liste av Personinfo

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/ArbeidsfordelingService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/ArbeidsfordelingService.kt
@@ -1,12 +1,10 @@
 package no.nav.familie.ba.sak.behandling
 
-import no.nav.familie.ba.sak.behandling.domene.Behandling
 import no.nav.familie.ba.sak.behandling.domene.BehandlingRepository
 import no.nav.familie.ba.sak.behandling.domene.Fagsak
 import no.nav.familie.ba.sak.behandling.domene.personopplysninger.PersonopplysningGrunnlagRepository
 import no.nav.familie.ba.sak.integrasjoner.IntegrasjonTjeneste
 import no.nav.familie.ba.sak.integrasjoner.domene.Arbeidsfordelingsenhet
-import no.nav.familie.ba.sak.integrasjoner.domene.Personinfo
 
 import java.lang.RuntimeException
 
@@ -15,30 +13,19 @@ class ArbeidsfordelingService(val behandlingRepository: BehandlingRepository,
                               val integrasjonTjeneste: IntegrasjonTjeneste) {
     fun hentBehandlendeEnhet(fagsak: Fagsak): List<Arbeidsfordelingsenhet> {
         val søker = integrasjonTjeneste.hentPersoninfoFor(fagsak.personIdent.ident)
-        val aktivBehandling = behandlingRepository.findByFagsakAndAktiv(fagsak.id) ?: throw RuntimeException()
-        val strengesteDiskresjonskode = finnStrengesteDiskresjonskode(søker, aktivBehandling)
+
+        val aktivBehandling = behandlingRepository.findByFagsakAndAktiv(fagsak.id)
+                ?: throw RuntimeException("Kunne ikke finne en aktiv behandling på fagsak med ID: ${fagsak.id}")
+
+        val personopplysningGrunnlag = personopplysningGrunnlagRepository.findByBehandlingAndAktiv(aktivBehandling.id)
+                ?: throw RuntimeException("Kunne ikke finne et aktivt personopplysningsgrunnlag på behandling med ID: ${aktivBehandling.id}")
+
+        val personinfoliste = personopplysningGrunnlag.barna.map {
+            barn -> integrasjonTjeneste.hentPersoninfoFor(barn.personIdent.ident)
+        }.plus(søker)
+
+        val strengesteDiskresjonskode = SikkerhetService.finnStrengesteDiskresjonskode(personinfoliste)
+
         return integrasjonTjeneste.hentBehandlendeEnhet(søker.geografiskTilknytning, strengesteDiskresjonskode)
     }
-
-    private fun finnStrengesteDiskresjonskode(søker: Personinfo, aktivBehandling: Behandling): String? {
-        val personopplysningGrunnlag = personopplysningGrunnlagRepository.findByBehandlingAndAktiv(aktivBehandling.id) ?: throw RuntimeException()
-
-        return personopplysningGrunnlag.barna.fold(søker.diskresjonskode, { diskresjonskode, barn ->
-            if (diskresjonskode != Diskresjonskode.KODE6.kode) {
-                integrasjonTjeneste.hentPersoninfoFor(barn.personIdent.ident).diskresjonskode.run {
-                    when (this) {
-                        Diskresjonskode.KODE6.kode, Diskresjonskode.KODE7.kode -> this
-                        else -> diskresjonskode
-                    }
-                }
-            } else {
-                diskresjonskode
-            }
-        })
-    }
-}
-
-enum class Diskresjonskode(val kode: String) {
-    KODE6("SPSF"),
-    KODE7("SPFO")
 }
