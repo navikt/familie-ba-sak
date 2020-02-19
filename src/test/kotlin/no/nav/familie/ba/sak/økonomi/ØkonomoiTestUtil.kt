23bf485a54c6cf2201953cb0a7fd8d7e05dd554b
Jørn Ola Birkeland <jorn.ola.birkeland@nav.no>
2020-02-19 11:59:18 +0100

    Legger til test for å ha sikkerhetsnett for periodisering av utbetalinger

---
+++ src/test/kotlin/no/nav/familie/ba/sak/økonomi/ØkonomoiTestUtil.kt
@@ -0,0 +1,88 @@
+package no.nav.familie.ba.sak.økonomi
+
+import no.nav.familie.ba.sak.behandling.domene.*
+import no.nav.familie.ba.sak.behandling.domene.personopplysninger.Person
+import no.nav.familie.ba.sak.behandling.domene.personopplysninger.PersonType
+import no.nav.familie.ba.sak.behandling.domene.personopplysninger.PersonopplysningGrunnlag
+import no.nav.familie.ba.sak.behandling.domene.vedtak.Vedtak
+import no.nav.familie.ba.sak.behandling.domene.vedtak.VedtakPerson
+import no.nav.familie.ba.sak.behandling.domene.vedtak.VedtakResultat
+import no.nav.familie.ba.sak.behandling.domene.vedtak.Ytelsetype
+import no.nav.familie.ba.sak.personopplysninger.domene.PersonIdent
+import no.nav.fpsak.tidsserie.LocalDateSegment
+import java.time.LocalDate
+import java.util.*
+
+var nesteVedtakId: Long = 1
+    get() {
+        val v = field
+        field += 50
+        return v
+    }
+
+var nesteBehandlingId: Long = 1
+    get() {
+        val v = field
+        field += 50
+        return v
+    }
+
+
+val defaultFagsak = Fagsak(1, null, PersonIdent("12345"), FagsakStatus.OPPRETTET)
+fun lagBehandling() = Behandling(id = nesteBehandlingId,
+                                 fagsak = defaultFagsak,
+                                 type = BehandlingType.FØRSTEGANGSBEHANDLING,
+                                 kategori = BehandlingKategori.NASJONAL,
+                                 underkategori = BehandlingUnderkategori.ORDINÆR)
+
+fun tilfeldigPerson(fødselsdato: LocalDate = LocalDate.now(), personType: PersonType = PersonType.BARN) = Person(
+        personIdent = PersonIdent(UUID.randomUUID().toString().substring(0, 18)),
+        fødselsdato = fødselsdato,
+        type = personType,
+        personopplysningGrunnlag = PersonopplysningGrunnlag(null)
+)
+
+fun lagVedtak(behandling: Behandling = lagBehandling(),
+              resultat: VedtakResultat = VedtakResultat.INNVILGET,
+              forrigeVedtak: Vedtak? = null,
+              opphørsdato: LocalDate? = null) =
+        Vedtak(id = nesteVedtakId,
+               behandling = behandling,
+               ansvarligSaksbehandler = "ansvarligSaksbehandler",
+               vedtaksdato = LocalDate.now(),
+               stønadBrevMarkdown = "",
+               resultat = resultat,
+               begrunnelse = "",
+               forrigeVedtakId = forrigeVedtak?.id,
+               opphørsdato = opphørsdato
+        )
+
+fun lagPersonVedtak(fom: String,
+                    tom: String,
+                    ytelsetype: Ytelsetype = Ytelsetype.ORDINÆR_BARNETRYGD,
+                    beløp: Int = sats(ytelsetype),
+                    vedtak: Vedtak = lagVedtak()): VedtakPerson {
+    return VedtakPerson(
+            person = tilfeldigPerson(),
+            stønadFom = dato(fom),
+            stønadTom = dato(tom),
+            beløp = beløp,
+            vedtak = vedtak,
+            type = ytelsetype
+    )
+}
+
+fun sats(ytelsetype: Ytelsetype) =
+        when (ytelsetype) {
+            Ytelsetype.ORDINÆR_BARNETRYGD -> 1054
+            Ytelsetype.UTVIDET_BARNETRYGD -> 1054
+            Ytelsetype.SMÅBARNSTILLEGG -> 660
+        }
+
+
+fun lagSegmentBeløp(fom: String, tom: String, beløp: Int): LocalDateSegment<Int> =
+        LocalDateSegment(dato(fom), dato(tom), beløp)
+
+fun dato(s: String) = LocalDate.parse(s)
+
+
