2e485942e3c5769cf7c95fd6f3fc15234b2c42b6
Geirmund Simonsen <geirmund.simonsen@nav.no>
2020-02-19 11:53:25 +0000

    Kall arbeidsfordeling via familie-integrasjoner (#89)
    - Setter opp integrasjon mot Arbeidsfordeling via familie-integrasjoner
    - Lager utilityfunksjon som finner strengeste diskresjonskode

---
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/ArbeidsfordelingService.kt
@@ -0,0 +1,31 @@
+package no.nav.familie.ba.sak.behandling
+
+import no.nav.familie.ba.sak.behandling.domene.BehandlingRepository
+import no.nav.familie.ba.sak.behandling.domene.Fagsak
+import no.nav.familie.ba.sak.behandling.domene.personopplysninger.PersonopplysningGrunnlagRepository
+import no.nav.familie.ba.sak.integrasjoner.IntegrasjonTjeneste
+import no.nav.familie.ba.sak.integrasjoner.domene.Arbeidsfordelingsenhet
+
+import java.lang.RuntimeException
+
+class ArbeidsfordelingService(val behandlingRepository: BehandlingRepository,
+                              val personopplysningGrunnlagRepository: PersonopplysningGrunnlagRepository,
+                              val integrasjonTjeneste: IntegrasjonTjeneste) {
+    fun hentBehandlendeEnhet(fagsak: Fagsak): List<Arbeidsfordelingsenhet> {
+        val søker = integrasjonTjeneste.hentPersoninfoFor(fagsak.personIdent.ident)
+
+        val aktivBehandling = behandlingRepository.findByFagsakAndAktiv(fagsak.id)
+                ?: throw RuntimeException("Kunne ikke finne en aktiv behandling på fagsak med ID: ${fagsak.id}")
+
+        val personopplysningGrunnlag = personopplysningGrunnlagRepository.findByBehandlingAndAktiv(aktivBehandling.id)
+                ?: throw RuntimeException("Kunne ikke finne et aktivt personopplysningsgrunnlag på behandling med ID: ${aktivBehandling.id}")
+
+        val personinfoliste = personopplysningGrunnlag.barna.map {
+            barn -> integrasjonTjeneste.hentPersoninfoFor(barn.personIdent.ident)
+        }.plus(søker)
+
+        val strengesteDiskresjonskode = finnStrengesteDiskresjonskode(personinfoliste)
+
+        return integrasjonTjeneste.hentBehandlendeEnhet(søker.geografiskTilknytning, strengesteDiskresjonskode)
+    }
+}
