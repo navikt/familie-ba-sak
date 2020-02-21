69b9984436eacf507eea50e8bb7a39016435ff3f
Henning Håkonsen <henninghaakonsen@me.com>
2020-02-21 13:13:19 +0100

    Øker endret av feltet på alle tabeller som implementerer BaseEntitet.

--- src/main/kotlin/no/nav/familie/ba/sak/config/WebConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/WebConfig.kt
@@ -2,14 +2,12 @@ package no.nav.familie.ba.sak.config
 
 import no.nav.familie.http.interceptor.InternLoggerInterceptor
 import no.nav.familie.sikkerhet.OIDCUtil
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig(
-        @Autowired
         private val oidcUtil: OIDCUtil
 ): WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
