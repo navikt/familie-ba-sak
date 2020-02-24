90f210f790ac353f259081dd4227a34f04c78d2f
Henning Håkonsen <henninghaakonsen@me.com>
2020-02-24 09:10:53 +0100

    Retter feil i flyway-script.

--- src/main/resources/db/migration/V24__base_entitet_oke_felter.sql
+++ src/main/resources/db/migration/V24__base_entitet_oke_felter.sql
@@ -14,4 +14,4 @@ alter table gr_personopplysninger alter column opprettet_av set data type varcha
 alter table vedtak alter column opprettet_av set data type varchar(512);
 alter table vedtak_person alter column opprettet_av set data type varchar(512);
 alter table samlet_vilkar_resultat alter column opprettet_av set data type varchar(512);
-alter table vilkar_resultat alter column endret_av set data type varchar(512);
+alter table vilkar_resultat alter column opprettet_av set data type varchar(512);
