edead157320654785abe67c78710756e99f8f5b3
Nina Kjekstad <ninakjekstad@Ninas-MacBook-Pro.local>
2020-02-28 10:18:59 +0100

    Mock totrinnskontroll

--- src/test/kotlin/DevLauncher.kt
+++ src/test/kotlin/DevLauncher.kt
@@ -5,7 +5,7 @@ object DevLauncher {
     @JvmStatic
     fun main(args: Array<String>) {
         val app = SpringApplicationBuilder(ApplicationConfig::class.java)
-                .profiles("dev", "mock-dokgen-java")
+                .profiles("dev", "mock-dokgen-java", "mock-totrinnkontroll")
         app.run(*args)
     }
 }
---
+++ src/test/kotlin/no/nav/familie/ba/sak/config/ØkonomiTestConfig.kt
@@ -0,0 +1,41 @@
+package no.nav.familie.ba.sak.config
+
+import no.nav.familie.ba.sak.behandling.DokGenService
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Profile
+/*
+@TestConfiguration
+class ØkonomiTestConfig {
+
+    @Bean
+    @Profile("mock-dokgen")
+    @Primary
+    fun mockDokGenService(): DokGenService {
+        //eliminate complain from Mockito of null parameter
+        fun <T> any(): T = Mockito.any<T>()
+
+        val dokgenService = mock(DokGenService::class.java)
+        `when`(dokgenService.lagHtmlFraMarkdown("Innvilget", "TEST_MARKDOWN_MOCKUP")).thenReturn("<HTML>HTML_MOCKUP</HTML>")
+        `when`(dokgenService.lagHtmlFraMarkdown("Avslag", "TEST_MARKDOWN_MOCKUP")).thenReturn("<HTML>HTML_MOCKUP</HTML>")
+        `when`(dokgenService.hentStønadBrevMarkdown(any())).thenReturn("TEST_MARKDOWN_MOCKUP")
+        return dokgenService
+    }
+
+    @Bean
+    @Profile("mock-dokgen-negative")
+    @Primary
+    fun mockDokGenNegativeService(): DokGenService {
+        //eliminate complain from Mockito of null parameter
+        fun <T> any(): T = Mockito.any<T>()
+
+        val dokgenService = mock(DokGenService::class.java)
+        `when`(dokgenService.lagHtmlFraMarkdown("Innvilget", "TEST_MARKDOWN_MOCKUP")).thenThrow(RuntimeException())
+        `when`(dokgenService.hentStønadBrevMarkdown(any())).thenReturn("TEST_MARKDOWN_MOCKUP")
+        return dokgenService
+    }
+}*/
