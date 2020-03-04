791edeabdab2c15cc784c1c1c07b998278560de6
Nina Kjekstad <ninakjek@users.noreply.github.com>
2020-03-04 10:33:08 +0100

    Legg til opphørsdato på vedtak  (#127)

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/vedtak/VedtakService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/vedtak/VedtakService.kt
@@ -11,6 +11,7 @@ import no.nav.familie.ba.sak.behandling.domene.personopplysninger.Personopplysni
 import no.nav.familie.ba.sak.behandling.fagsak.FagsakService
 import no.nav.familie.ba.sak.behandling.restDomene.RestFagsak
 import no.nav.familie.ba.sak.behandling.restDomene.RestVilkårResultat
+import no.nav.familie.ba.sak.common.førsteDagINesteMåned
 import no.nav.familie.ba.sak.common.sisteDagIForrigeMåned
 import no.nav.familie.ba.sak.dokument.DokGenKlient
 import no.nav.familie.ba.sak.personopplysninger.domene.PersonIdent
@@ -90,7 +91,7 @@ class VedtakService(private val behandlingService: BehandlingService,
                 ansvarligSaksbehandler = ansvarligSaksbehandler,
                 vedtaksdato = LocalDate.now(),
                 forrigeVedtakId = forrigeVedtak?.id,
-                opphørsdato = if (behandling.resultat == BehandlingResultat.OPPHØRT) LocalDate.now() else null,
+                opphørsdato = if (behandling.resultat == BehandlingResultat.OPPHØRT) LocalDate.now().førsteDagINesteMåned() else null,
                 stønadBrevMarkdown = if (behandling.resultat != BehandlingResultat.INNVILGET) Result.runCatching {
                     dokGenKlient.hentStønadBrevMarkdown(behandling,
                                                         ansvarligSaksbehandler)
