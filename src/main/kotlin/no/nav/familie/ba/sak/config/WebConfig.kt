ed4259fc7011fd93ae6322049a7f474d9a794f1b
Bent J. Lorentzen <bent.lorentzen@nav.no>
2020-03-06 16:34:23 +0100

    Oppgradert felles.

--- src/main/kotlin/no/nav/familie/ba/sak/config/WebConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/WebConfig.kt
@@ -3,10 +3,12 @@ package no.nav.familie.ba.sak.config
 import no.nav.familie.http.interceptor.InternLoggerInterceptor
 import no.nav.familie.sikkerhet.OIDCUtil
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
+@Import(OIDCUtil::class)
 class WebConfig(
         private val oidcUtil: OIDCUtil
 ): WebMvcConfigurer {
