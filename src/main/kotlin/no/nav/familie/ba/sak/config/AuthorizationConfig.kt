2394e3f38d1f9e7b3094adaa1511afb1cbf01559
Jørn Ola Birkeland <jorn.ola.birkeland@nav.no>
2020-03-10 11:50:46 +0100

    Sørger for at autorisering ikke feilaktig er skrudd av

--- src/main/kotlin/no/nav/familie/ba/sak/config/AuthorizationConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/AuthorizationConfig.kt
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-//@Configuration
+@Configuration
 class AuthorizationConfig(
         private val oidcUtil: OIDCUtil,
         @Value("\${ACCEPTED_CLIENTS}")
