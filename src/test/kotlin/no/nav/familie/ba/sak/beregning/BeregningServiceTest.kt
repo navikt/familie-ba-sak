9ee7442219b558fd9ff99f31df1b0ea5f3903835
Martine Enger <marenger@student.matnat.uio.no>
2020-03-18 16:17:43 +0100

    Fullført beregningsresultat-test

--- src/test/kotlin/no/nav/familie/ba/sak/beregning/BeregningServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/beregning/BeregningServiceTest.kt
@@ -5,7 +5,6 @@ import no.nav.familie.ba.sak.behandling.fagsak.FagsakService
 import no.nav.familie.ba.sak.common.DbContainerInitializer
 import no.nav.familie.ba.sak.common.lagBehandling
 import no.nav.familie.ba.sak.common.randomFnr
-import no.nav.familie.ba.sak.økonomi.lagUtbetalingsoppdrag
 import no.nav.familie.kontrakter.felles.oppdrag.Opphør
 import no.nav.familie.kontrakter.felles.oppdrag.Utbetalingsoppdrag
 import no.nav.familie.kontrakter.felles.oppdrag.Utbetalingsperiode
@@ -81,10 +80,93 @@ class BeregningServiceTest {
     @Test
     fun skalLagreRiktigBeregningsresultatForOpphoer() {
 
+        val fnr = randomFnr()
+        val dagensDato = LocalDate.now()
+        val opphørsDato = dagensDato.plusMonths(1).withDayOfMonth(1)
+
+        val fagsak = fagsakService.hentEllerOpprettFagsakForPersonIdent(fnr)
+        val behandling = behandlingService.lagreNyOgDeaktiverGammelBehandling(lagBehandling(fagsak))
+        val utbetalingsoppdrag = Utbetalingsoppdrag(
+                Utbetalingsoppdrag.KodeEndring.NY,
+                "BA",
+                fagsak.id.toString(),
+                UUID.randomUUID().toString(),
+                "SAKSBEHANDLERID",
+                LocalDateTime.now(),
+                listOf(Utbetalingsperiode(true,
+                        Opphør(opphørsDato),
+                        1,
+                        null,
+                        dagensDato,
+                        "BATR",
+                        dagensDato.withDayOfMonth(1),
+                        dagensDato.plusMonths(10),
+                        BigDecimal.ONE,
+                        Utbetalingsperiode.SatsType.MND,
+                        fnr,
+                        behandling.id
+                ))
+        )
+
+        beregningService.lagreBeregningsresultat(behandling, utbetalingsoppdrag)
+        val beregningResultat = beregningService.hentBeregningsresultatForBehandling(behandling.id)
+
+        Assertions.assertNotNull(beregningResultat)
+        Assertions.assertEquals(opphørsDato, beregningResultat.stønadFom)
+        Assertions.assertEquals(dagensDato.plusMonths(10), beregningResultat.stønadTom)
+        Assertions.assertTrue(beregningResultat.erOpphør)
     }
 
     @Test
     fun skalLagreRiktigBeregningsresultatForRevurdering() {
 
+        val fnr = randomFnr()
+        val dagensDato = LocalDate.now()
+        val revurderingFra = dagensDato.plusMonths(3).withDayOfMonth(1)
+        val tomDato = dagensDato.plusMonths(10)
+
+        val fagsak = fagsakService.hentEllerOpprettFagsakForPersonIdent(fnr)
+        val behandling = behandlingService.lagreNyOgDeaktiverGammelBehandling(lagBehandling(fagsak))
+        val utbetalingsoppdrag = Utbetalingsoppdrag(
+                Utbetalingsoppdrag.KodeEndring.NY,
+                "BA",
+                fagsak.id.toString(),
+                UUID.randomUUID().toString(),
+                "SAKSBEHANDLERID",
+                LocalDateTime.now(),
+                listOf(Utbetalingsperiode(true,
+                        Opphør(dagensDato.withDayOfMonth(1)),
+                        1,
+                        null,
+                        dagensDato,
+                        "BATR",
+                        dagensDato.withDayOfMonth(1),
+                        tomDato,
+                        BigDecimal.ONE,
+                        Utbetalingsperiode.SatsType.MND,
+                        fnr,
+                        behandling.id
+                ), Utbetalingsperiode(false,
+                        null,
+                        2,
+                        1,
+                        dagensDato,
+                        "BATR",
+                        revurderingFra,
+                        tomDato,
+                        BigDecimal.ONE,
+                        Utbetalingsperiode.SatsType.MND,
+                        fnr,
+                        behandling.id
+                ))
+        )
+
+        beregningService.lagreBeregningsresultat(behandling, utbetalingsoppdrag)
+        val beregningResultat = beregningService.hentBeregningsresultatForBehandling(behandling.id)
+
+        Assertions.assertNotNull(beregningResultat)
+        Assertions.assertEquals(revurderingFra, beregningResultat.stønadFom)
+        Assertions.assertEquals(tomDato, beregningResultat.stønadTom)
+        Assertions.assertFalse(beregningResultat.erOpphør)
     }
 }
