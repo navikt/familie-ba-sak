85a176fba687cbed973938bdd6d87f8912a9d520
Nina Kjekstad <nina.kjekstad@nav.no>
2020-03-18 16:57:57 +0100

    Legg ved flywayscript for testing lokalt

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/domene/BehandlingResultat.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/domene/BehandlingResultat.kt
@@ -30,4 +30,11 @@ data class BehandlingResultat(
     }
 }
 
-
+//TODO: Legg ved rett før merge
+/*
+alter table behandling rename column resultat to brev;
+alter table vilkar_resultat rename constraint vilkar_resultat_samlet_vilkar_resultat_id_fkey to vilkar_resultat_periode_resultat_id_fkey;
+alter table samlet_vilkar_resultat rename to periode_resultat;
+alter table periode_resultat rename constraint samlet_vilkar_resultat_fk_behandling_id_fkey to periode_resultat_fk_behandling_id_fkey;
+alter index samlet_vilkar_resultat_pkey rename to periode_resultat_pkey;
+ */
