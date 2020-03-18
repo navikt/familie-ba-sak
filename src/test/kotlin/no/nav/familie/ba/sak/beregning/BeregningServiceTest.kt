702a571a443b468bd7e99e9b58f08058248c9291
Martine Enger <marenger@student.matnat.uio.no>
2020-03-18 15:25:57 +0100

    [WIP] Starten på test for lagring av beregningsresultat

---
+++ src/test/kotlin/no/nav/familie/ba/sak/beregning/BeregningServiceTest.kt
@@ -0,0 +1,90 @@
+package no.nav.familie.ba.sak.beregning
+
+import no.nav.familie.ba.sak.behandling.BehandlingService
+import no.nav.familie.ba.sak.behandling.fagsak.FagsakService
+import no.nav.familie.ba.sak.common.DbContainerInitializer
+import no.nav.familie.ba.sak.common.lagBehandling
+import no.nav.familie.ba.sak.common.randomFnr
+import no.nav.familie.ba.sak.økonomi.lagUtbetalingsoppdrag
+import no.nav.familie.kontrakter.felles.oppdrag.Opphør
+import no.nav.familie.kontrakter.felles.oppdrag.Utbetalingsoppdrag
+import no.nav.familie.kontrakter.felles.oppdrag.Utbetalingsperiode
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
+
+@SpringBootTest
+@ExtendWith(SpringExtension::class)
+@ContextConfiguration(initializers = [DbContainerInitializer::class])
+@ActiveProfiles("postgres")
+@Tag("integration")
+class BeregningServiceTest {
+
+    @Autowired
+    private lateinit var beregningService: BeregningService
+
+    @Autowired
+    private lateinit var fagsakService: FagsakService
+
+    @Autowired
+    private lateinit var behandlingService: BehandlingService
+
+    @Test
+    fun skalLagreRiktigBeregningsresultatForFGB() {
+        val fnr = randomFnr()
+        val dagensDato = LocalDate.now()
+
+        val fagsak = fagsakService.hentEllerOpprettFagsakForPersonIdent(fnr)
+        val behandling = behandlingService.lagreNyOgDeaktiverGammelBehandling(lagBehandling(fagsak))
+        val utbetalingsoppdrag = Utbetalingsoppdrag(
+                Utbetalingsoppdrag.KodeEndring.NY,
+                "BA",
+                fagsak.id.toString(),
+                UUID.randomUUID().toString(),
+                "SAKSBEHANDLERID",
+                LocalDateTime.now(),
+                listOf(Utbetalingsperiode(false,
+                        null,
+                        1,
+                        null,
+                        dagensDato,
+                        "BATR",
+                        dagensDato.withDayOfMonth(1),
+                        dagensDato.plusMonths(10),
+                        BigDecimal.ONE,
+                        Utbetalingsperiode.SatsType.MND,
+                        fnr,
+                        behandling.id
+                ))
+        )
+
+        beregningService.lagreBeregningsresultat(behandling, utbetalingsoppdrag)
+        val beregningResultat = beregningService.hentBeregningsresultatForBehandling(behandling.id)
+
+        Assertions.assertNotNull(beregningResultat)
+        Assertions.assertEquals(dagensDato.withDayOfMonth(1), beregningResultat.stønadFom)
+        Assertions.assertEquals(dagensDato.plusMonths(10), beregningResultat.stønadTom)
+        Assertions.assertFalse(beregningResultat.erOpphør)
+
+    }
+
+    @Test
+    fun skalLagreRiktigBeregningsresultatForOpphoer() {
+
+    }
+
+    @Test
+    fun skalLagreRiktigBeregningsresultatForRevurdering() {
+
+    }
+}
