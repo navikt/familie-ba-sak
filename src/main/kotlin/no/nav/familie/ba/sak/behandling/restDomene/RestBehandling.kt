2d5775d4a5287c29620cf08a90dc6f2d8d9c3e76
Nina Kjekstad <nina.kjekstad@nav.no>
2020-03-19 14:48:38 +0100

    Oppdatert restbehandling

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/restDomene/RestBehandling.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/restDomene/RestBehandling.kt
@@ -1,10 +1,10 @@
 package no.nav.familie.ba.sak.behandling.restDomene
 
 import no.nav.familie.ba.sak.behandling.domene.*
-import no.nav.familie.ba.sak.behandling.vilkår.PeriodeResultat
 import no.nav.familie.ba.sak.behandling.vilkår.Vilkår
 import no.nav.familie.ba.sak.behandling.steg.StegType
 import no.nav.nare.core.evaluations.Resultat
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 data class RestBehandling(val aktiv: Boolean,
@@ -16,17 +16,33 @@ data class RestBehandling(val aktiv: Boolean,
                           val personer: List<RestPerson>,
                           val opprettetTidspunkt: LocalDateTime,
                           val underkategori: BehandlingUnderkategori,
-                          val periodeResultat: List<RestVilkårResultat>,
+                          val behandlingResultat: List<RestPersonVilkårResultat>, // TODO: Endret format og navn
                           val vedtakForBehandling: List<RestVedtak?>,
-                          val resultat: BrevType,
+                          val brevType: BrevType, //TODO: Endret format og navn
                           val begrunnelse: String)
 
-fun PeriodeResultat.toRestPeriodeResultat() = this.periodeResultat.map {
-    RestVilkårResultat(vilkårType = it.vilkårType, resultat = it.resultat, personIdent = it.person.personIdent.ident)
+fun BehandlingResultat.toRestBehandlingResultat() = this.behandlingResultat.map {
+    RestPersonVilkårResultat(personIdent = "12345678910",
+                             vurderteVilkår = listOf(
+                                     RestVilkårResultat(vilkårType = Vilkår.BOSATT_I_RIKET,
+                                                        resultat = Resultat.JA,
+                                                        fom = LocalDate.now(),
+                                                        tom = LocalDate.now(),
+                                                        begrunnelse = "OK")))
+    //TODO: Map behandlingsresultatet delt i perioder til behandlingsresultat delt i personer m/tilhørende vilkår+perioder
+    // Gjøre dette i en egen periodeservice?
+    //RestVilkårResultat(vilkårType = it.vilkårType, resultat = it.resultat, personIdent = it.person.personIdent.ident)
 }
 
-data class RestVilkårResultat(
+data class RestPersonVilkårResultat(
         val personIdent: String,
+        val vurderteVilkår: List<RestVilkårResultat>
+)
+
+data class RestVilkårResultat(
         val vilkårType: Vilkår,
-        val resultat: Resultat
+        val resultat: Resultat,
+        val fom: LocalDate,
+        val tom: LocalDate,
+        val begrunnelse: String
 )
