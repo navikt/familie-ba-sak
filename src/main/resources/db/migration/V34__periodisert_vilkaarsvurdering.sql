c61ab4751b76fa1f620ade2aa8d342be9f0a555c
Nina Kjekstad <nina.kjekstad@nav.no>
2020-03-26 11:53:00 +0100

    Migrering av eksisterende saker i db

--- src/main/resources/db/migration/V34__periodisert_vilkaarsvurdering.sql
+++ src/main/resources/db/migration/V34__periodisert_vilkaarsvurdering.sql
@@ -9,23 +9,37 @@ CREATE TABLE BEHANDLING_RESULTAT
     ENDRET_AV        VARCHAR,
     ENDRET_TID       TIMESTAMP(3)
 );
+alter table BEHANDLING_RESULTAT add column SAM_VIL_RES_ID BIGINT;
+
+CREATE SEQUENCE BEHANDLING_RESULTAT_SEQ INCREMENT BY 50 START WITH 1000000 NO CYCLE;
+insert into BEHANDLING_RESULTAT (id, SAM_VIL_RES_ID, fk_behandling_id, aktiv, versjon, opprettet_av, opprettet_tid, endret_av, endret_tid)
+select nextval('BEHANDLING_RESULTAT_SEQ'), id, fk_behandling_id, aktiv, versjon, opprettet_av, opprettet_tid, endret_av, endret_tid from samlet_vilkar_resultat;
+
+alter table samlet_vilkar_resultat add column person_ident varchar;
+update samlet_vilkar_resultat svr set person_ident=(select distinct p.person_ident from po_person p, vilkar_resultat vr where p.id = vr.fk_person_id and vr.samlet_vilkar_resultat_id=svr.id limit 1);
+
+alter table samlet_vilkar_resultat add column behandling_resultat_id references BEHANDLING_RESULTAT (id) default null;
+update samlet_vilkar_resultat svr set behandling_resultat_id=(select distinct br.id from behandling_resultat br, samlet_vilkar_resultat svr where svr.fk_behandling_id = br.fk_behandling_id);
 
 alter table behandling
     rename column resultat to brev_type;
 
-alter table vilkar_resultat
-    rename constraint vilkar_resultat_samlet_vilkar_resultat_id_fkey to vilkar_resultat_periode_resultat_id_fkey;
-
 alter table samlet_vilkar_resultat
-    drop column fk_behandling_id, drop column aktiv;
+    drop column fk_behandling_id,
+    drop column aktiv;
+
 alter table samlet_vilkar_resultat
     rename to periode_resultat;
 alter table periode_resultat
-    add column behandling_resultat_id bigint references BEHANDLING_RESULTAT (id) default null;
-alter table periode_resultat
-    add column person_ident varchar default '' not null;
-alter table periode_resultat
-    add column periode_fom timestamp(3);
-alter table periode_resultat
+    add column behandling_resultat_id bigint references BEHANDLING_RESULTAT (id) default null,
+    add column person_ident varchar default '' not null,
+    add column periode_fom timestamp(3),
     add column periode_tom timestamp(3);
 alter index samlet_vilkar_resultat_pkey rename to periode_resultat_pkey;
+
+alter table vilkar_resultat
+    rename constraint vilkar_resultat_samlet_vilkar_resultat_id_fkey to vilkar_resultat_periode_resultat_id_fkey;
+alter table vilkar_resultat
+    rename column samlet_vilkar_resultat_id to periode_resultat_id;
+alter table vilkar_resultat
+    drop column fk_person_id;
