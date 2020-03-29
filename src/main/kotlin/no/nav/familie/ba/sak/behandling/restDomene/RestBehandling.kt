d91878a3db92e7d6d69b23840473bfa89224d7dd
Nina Kjekstad <nina.kjekstad@nav.no>
2020-03-29 21:06:15 +0200

    Gjør fom-dato nullable, da det enda ikke blir brukt

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/restDomene/RestBehandling.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/restDomene/RestBehandling.kt
@@ -24,7 +24,7 @@ data class RestBehandling(val aktiv: Boolean,
 
 data class RestPeriodeResultat(
         val personIdent: String,
-        val periodeFom: LocalDate,
+        val periodeFom: LocalDate?,
         val periodeTom: LocalDate?,
         val vilkårResultater: List<RestVilkårResultat>?
 )
