3eb05a53e1e99d07a1c74abc0fd6e3c0399a8c1d
Meng Zhu <meng.zhu@nav.no>
2020-04-02 01:25:58 +0200

    disable test-søk

--- src/test/kotlin/DevLauncher.kt
+++ src/test/kotlin/DevLauncher.kt
@@ -5,7 +5,7 @@ object DevLauncher {
     @JvmStatic
     fun main(args: Array<String>) {
         val app = SpringApplicationBuilder(ApplicationConfig::class.java)
-                .profiles("dev", "mock-totrinnkontroll", "mock-dokgen-java", "mock-iverksett", "test-søk")
+                .profiles("dev", "mock-totrinnkontroll", "mock-dokgen-java", "mock-iverksett")
         app.run(*args)
     }
 }
