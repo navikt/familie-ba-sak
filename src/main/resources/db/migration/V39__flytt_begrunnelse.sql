62c93a2a6b4b51b764a288eb1a17634cfca1c021
Nina Kjekstad <nina.kjekstad@nav.no>
2020-04-02 18:06:03 +0200

    Begrunnelse på alle perioderesultater

--- src/main/resources/db/migration/V39__flytt_begrunnelse.sql
+++ src/main/resources/db/migration/V39__flytt_begrunnelse.sql
@@ -10,8 +10,7 @@ FROM (WITH behandling_resultat_begrunnelse AS (
     WHERE br.FK_BEHANDLING_ID = b.id)
                  SELECT brb.begrunnelse, pr.id as periode_resultat_id
                  from behandling_resultat_begrunnelse brb
-                          inner join periode_resultat pr on pr.fk_behandling_resultat_id = brb.ID
-                 limit 1) as periode_resultat_begrunnelse
+                          inner join periode_resultat pr on pr.fk_behandling_resultat_id = brb.ID) as periode_resultat_begrunnelse
 
 where periode_resultat_begrunnelse.periode_resultat_id = vr.fk_periode_resultat_id;
 
