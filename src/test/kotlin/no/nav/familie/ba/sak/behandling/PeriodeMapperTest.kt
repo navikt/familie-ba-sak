242ea868a5252dc59ff6db49914f1845a5de06c2
Nina Kjekstad <nina.kjekstad@nav.no>
2020-04-15 15:26:05 +0200

    Kontroller periodedatoer også

--- src/test/kotlin/no/nav/familie/ba/sak/behandling/PeriodeMapperTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/behandling/PeriodeMapperTest.kt
@@ -47,13 +47,13 @@ class PeriodeMapperTest {
         val personResultat = PersonResultat(behandlingResultat = behandlingResultat, personIdent = "")
 
         val tidslinje1 = LocalDateTimeline(listOf(LocalDateSegment(datoer[0],
-                                                                   datoer[2],
+                                                                   datoer[2].minusDays(1),
                                                                    VilkårResultat(personResultat = personResultat,
                                                                                   vilkårType = Vilkår.UNDER_18_ÅR,
                                                                                   resultat = Resultat.JA,
                                                                                   begrunnelse = ""))))
         val tidslinje2 = LocalDateTimeline(listOf(LocalDateSegment(datoer[1],
-                                                                   datoer[3],
+                                                                   datoer[3].minusDays(1),
                                                                    VilkårResultat(personResultat = personResultat,
                                                                                   vilkårType = Vilkår.BOSATT_I_RIKET,
                                                                                   resultat = Resultat.JA,
@@ -61,9 +61,9 @@ class PeriodeMapperTest {
         val kombinertTidslinje = foldTidslinjer(listOf(tidslinje1, tidslinje2))
 
         assert(kombinertTidslinje.toSegments().size == 3)
-        val segment1 = kombinertTidslinje.getSegment(LocalDateInterval(datoer[0], datoer[1]))
-        val segment2 = kombinertTidslinje.getSegment(LocalDateInterval(datoer[1].plusDays(1), datoer[2]))
-        val segment3 = kombinertTidslinje.getSegment(LocalDateInterval(datoer[2].plusDays(1), datoer[3]))
+        val segment1 = kombinertTidslinje.getSegment(LocalDateInterval(datoer[0], datoer[1].minusDays(1)))
+        val segment2 = kombinertTidslinje.getSegment(LocalDateInterval(datoer[1], datoer[2].minusDays(1)))
+        val segment3 = kombinertTidslinje.getSegment(LocalDateInterval(datoer[2], datoer[3].minusDays(1)))
 
         assert(segment1.value.size == 1)
         assert(segment2.value.size == 2)
@@ -76,7 +76,7 @@ class PeriodeMapperTest {
     }
 
     @Test
-    fun `Mapper tre PersonResultater til fire PeriodeResultater med rette vilkår for to personer`() {
+    fun `Mapper tre PersonResultater til fire PeriodeResultater med rette vilkår og datoer for to personer`() {
         val fnr1 = randomFnr()
         val fnr2 = randomFnr()
         val personResultat1 = PersonResultat(behandlingResultat = behandlingResultat, personIdent = fnr1)
@@ -85,41 +85,58 @@ class PeriodeMapperTest {
                                                                 vilkårType = Vilkår.UNDER_18_ÅR,
                                                                 resultat = Resultat.JA,
                                                                 periodeFom = datoer[0],
-                                                                periodeTom = datoer[2],
+                                                                periodeTom = datoer[2].minusDays(1),
                                                                 begrunnelse = ""),
                                                  VilkårResultat(personResultat = personResultat1,
                                                                 vilkårType = Vilkår.BOSATT_I_RIKET,
                                                                 resultat = Resultat.JA,
                                                                 periodeFom = datoer[1],
-                                                                periodeTom = datoer[3],
+                                                                periodeTom = datoer[3].minusDays(1),
                                                                 begrunnelse = ""))
         personResultat2.vilkårResultater = setOf(VilkårResultat(personResultat = personResultat1,
                                                                 vilkårType = Vilkår.LOVLIG_OPPHOLD,
                                                                 resultat = Resultat.JA,
                                                                 periodeFom = datoer[1],
-                                                                periodeTom = datoer[4],
+                                                                periodeTom = datoer[4].minusDays(1),
                                                                 begrunnelse = ""))
         behandlingResultat.personResultater = setOf(personResultat1, personResultat2)
         val periodeResultater = behandlingResultat.periodeResultater.toList()
 
+        /*
+        Person 1 med to overlappende perioder som skal splittes til tre
+        Person 2 med én periode som skal bevares som én
+         */
         assert(periodeResultater.size == 4)
 
+        // Person 1 første periode
         assert(periodeResultater[0].vilkårResultater.size == 1)
         assert(periodeResultater[0].personIdent == fnr1)
         assert(periodeResultater[0].vilkårResultater.any { it.vilkårType == Vilkår.UNDER_18_ÅR })
+        assert(periodeResultater[0].periodeFom!! == datoer[0])
+        assert(periodeResultater[0].periodeTom!! == datoer[1].minusDays(1))
 
+        // Person 1 andre periode (overlappende)
         assert(periodeResultater[1].vilkårResultater.size == 2)
         assert(periodeResultater[1].personIdent == fnr1)
         assert(periodeResultater[1].vilkårResultater.any { it.vilkårType == Vilkår.UNDER_18_ÅR })
         assert(periodeResultater[1].vilkårResultater.any { it.vilkårType == Vilkår.BOSATT_I_RIKET })
+        assert(periodeResultater[1].periodeFom!! == datoer[1])
+        assert(periodeResultater[1].periodeTom!! == datoer[2].minusDays(1))
 
+        // Person 1 tredje periode
         assert(periodeResultater[2].vilkårResultater.size == 1)
         assert(periodeResultater[2].personIdent == fnr1)
         assert(periodeResultater[2].vilkårResultater.any { it.vilkårType == Vilkår.BOSATT_I_RIKET })
+        assert(periodeResultater[2].vilkårResultater.any { it.vilkårType == Vilkår.BOSATT_I_RIKET })
+        assert(periodeResultater[2].periodeFom!! == datoer[2])
+        assert(periodeResultater[2].periodeTom!! == datoer[3].minusDays(1))
 
+        // Person 2
         assert(periodeResultater[3].vilkårResultater.size == 1)
         assert(periodeResultater[3].personIdent == fnr2)
         assert(periodeResultater[3].vilkårResultater.any { it.vilkårType == Vilkår.LOVLIG_OPPHOLD })
+        assert(periodeResultater[3].vilkårResultater.any { it.vilkårType == Vilkår.LOVLIG_OPPHOLD })
+        assert(periodeResultater[3].periodeFom!! == datoer[1])
+        assert(periodeResultater[3].periodeTom!! == datoer[4].minusDays(1))
     }
-
 }
