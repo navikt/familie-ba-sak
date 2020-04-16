7de869df30e420b2170b341a52be0689a3917b38
Nina Kjekstad <nina.kjekstad@nav.no>
2020-04-16 11:36:57 +0200

    Legg til periode i periodemapping

--- src/test/kotlin/no/nav/familie/ba/sak/behandling/PeriodeMapperTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/behandling/PeriodeMapperTest.kt
@@ -47,23 +47,24 @@ class PeriodeMapperTest {
         val personResultat = PersonResultat(behandlingResultat = behandlingResultat, personIdent = "")
 
         val tidslinje1 = LocalDateTimeline(listOf(LocalDateSegment(datoer[0],
-                                                                   datoer[2].minusDays(1),
+                                                                   datoer[2],
                                                                    VilkårResultat(personResultat = personResultat,
                                                                                   vilkårType = Vilkår.UNDER_18_ÅR,
                                                                                   resultat = Resultat.JA,
                                                                                   begrunnelse = ""))))
         val tidslinje2 = LocalDateTimeline(listOf(LocalDateSegment(datoer[1],
-                                                                   datoer[3].minusDays(1),
+                                                                   datoer[3],
                                                                    VilkårResultat(personResultat = personResultat,
                                                                                   vilkårType = Vilkår.BOSATT_I_RIKET,
                                                                                   resultat = Resultat.JA,
                                                                                   begrunnelse = ""))))
+
         val kombinertTidslinje = lagTidslinjeMedOverlappendePerioder(listOf(tidslinje1, tidslinje2))
 
         assert(kombinertTidslinje.toSegments().size == 3)
         val segment1 = kombinertTidslinje.getSegment(LocalDateInterval(datoer[0], datoer[1].minusDays(1)))
-        val segment2 = kombinertTidslinje.getSegment(LocalDateInterval(datoer[1], datoer[2].minusDays(1)))
-        val segment3 = kombinertTidslinje.getSegment(LocalDateInterval(datoer[2], datoer[3].minusDays(1)))
+        val segment2 = kombinertTidslinje.getSegment(LocalDateInterval(datoer[1], datoer[2]))
+        val segment3 = kombinertTidslinje.getSegment(LocalDateInterval(datoer[2].plusDays(1), datoer[3]))
 
         assert(segment1.value.size == 1)
         assert(segment2.value.size == 2)
@@ -92,8 +93,14 @@ class PeriodeMapperTest {
                                                                 resultat = Resultat.JA,
                                                                 periodeFom = datoer[1],
                                                                 periodeTom = datoer[3],
+                                                                begrunnelse = ""),
+                                                 VilkårResultat(personResultat = personResultat1,
+                                                                vilkårType = Vilkår.LOVLIG_OPPHOLD,
+                                                                resultat = Resultat.JA,
+                                                                periodeFom = datoer[1],
+                                                                periodeTom = datoer[4],
                                                                 begrunnelse = ""))
-        personResultat2.vilkårResultater = setOf(VilkårResultat(personResultat = personResultat1,
+        personResultat2.vilkårResultater = setOf(VilkårResultat(personResultat = personResultat2,
                                                                 vilkårType = Vilkår.LOVLIG_OPPHOLD,
                                                                 resultat = Resultat.JA,
                                                                 periodeFom = datoer[1],
@@ -103,10 +110,18 @@ class PeriodeMapperTest {
         val periodeResultater = behandlingResultat.periodeResultater.toList()
 
         /*
-        Person 1 med to overlappende perioder som skal splittes til tre
+        Person 1 med tre overlappende perioder som skal splittes til fire
         Person 2 med én periode som skal bevares som én
+
+        Siste periode vil alltid få
          */
-        assert(periodeResultater.size == 4)
+        assert(periodeResultater.size == 5)
+
+        println(periodeResultater[0].toString())
+        println(periodeResultater[1].toString())
+        println(periodeResultater[2].toString())
+        println(periodeResultater[3].toString())
+        println(periodeResultater[4].toString())
 
         // Person 1 første periode
         assert(periodeResultater[0].vilkårResultater.size == 1)
@@ -116,26 +131,34 @@ class PeriodeMapperTest {
         assert(periodeResultater[0].periodeTom!! == datoer[1].minusDays(1))
 
         // Person 1 andre periode (overlappende)
-        assert(periodeResultater[1].vilkårResultater.size == 2)
+        assert(periodeResultater[1].vilkårResultater.size == 3)
         assert(periodeResultater[1].personIdent == fnr1)
         assert(periodeResultater[1].vilkårResultater.any { it.vilkårType == Vilkår.UNDER_18_ÅR })
         assert(periodeResultater[1].vilkårResultater.any { it.vilkårType == Vilkår.BOSATT_I_RIKET })
+        assert(periodeResultater[1].vilkårResultater.any { it.vilkårType == Vilkår.LOVLIG_OPPHOLD })
         assert(periodeResultater[1].periodeFom!! == datoer[1])
         assert(periodeResultater[1].periodeTom!! == datoer[2])
 
         // Person 1 tredje periode
-        assert(periodeResultater[2].vilkårResultater.size == 1)
+        assert(periodeResultater[2].vilkårResultater.size == 2)
         assert(periodeResultater[2].personIdent == fnr1)
         assert(periodeResultater[2].vilkårResultater.any { it.vilkårType == Vilkår.BOSATT_I_RIKET })
+        assert(periodeResultater[2].vilkårResultater.any { it.vilkårType == Vilkår.LOVLIG_OPPHOLD })
         assert(periodeResultater[2].periodeFom!! == datoer[2].plusDays(1))
         assert(periodeResultater[2].periodeTom!! == datoer[3])
 
-        // Person 2
+        // Person 1 fjerde periode
         assert(periodeResultater[3].vilkårResultater.size == 1)
-        assert(periodeResultater[3].personIdent == fnr2)
+        assert(periodeResultater[3].personIdent == fnr1)
         assert(periodeResultater[3].vilkårResultater.any { it.vilkårType == Vilkår.LOVLIG_OPPHOLD })
-        assert(periodeResultater[3].vilkårResultater.any { it.vilkårType == Vilkår.LOVLIG_OPPHOLD })
-        assert(periodeResultater[3].periodeFom!! == datoer[1])
+        assert(periodeResultater[3].periodeFom!! == datoer[3].plusDays(1))
         assert(periodeResultater[3].periodeTom!! == datoer[4])
+
+        // Person 2
+        assert(periodeResultater[4].vilkårResultater.size == 1)
+        assert(periodeResultater[4].personIdent == fnr2)
+        assert(periodeResultater[4].vilkårResultater.any { it.vilkårType == Vilkår.LOVLIG_OPPHOLD })
+        assert(periodeResultater[4].periodeFom!! == datoer[1])
+        assert(periodeResultater[4].periodeTom!! == datoer[4])
     }
 }
