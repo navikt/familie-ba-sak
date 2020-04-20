918236b1f058c464ebfda8149dde759c8cedef13
nav-jorn-ola-birkeland <47781778+nav-jorn-ola-birkeland@users.noreply.github.com>
2020-04-20 07:51:19 +0200

    Csv tester beregning (#204)
    * Legger til CSV-tester og refaktorer litt
    
    * Fjerner feilaktig endring i test-datagenerator
    
    * Legger til test for søker med to barn
    
    * Fjerner en unødvendig endring

--- src/main/kotlin/no/nav/familie/ba/sak/beregning/BeregningService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/beregning/BeregningService.kt
@@ -1,29 +1,20 @@
 package no.nav.familie.ba.sak.beregning
 
 import no.nav.familie.ba.sak.behandling.domene.Behandling
-import no.nav.familie.ba.sak.behandling.domene.BehandlingResultat
 import no.nav.familie.ba.sak.behandling.domene.BehandlingResultatRepository
-import no.nav.familie.ba.sak.behandling.domene.BehandlingResultatType
 import no.nav.familie.ba.sak.behandling.fagsak.FagsakService
 import no.nav.familie.ba.sak.behandling.grunnlag.personopplysninger.PersonopplysningGrunnlag
 import no.nav.familie.ba.sak.behandling.restDomene.RestFagsak
 import no.nav.familie.ba.sak.behandling.vedtak.AndelTilkjentYtelse
 import no.nav.familie.ba.sak.behandling.vedtak.AndelTilkjentYtelseRepository
-import no.nav.familie.ba.sak.behandling.vedtak.Ytelsetype
-import no.nav.familie.ba.sak.beregning.domene.PeriodeResultat
-import no.nav.familie.ba.sak.beregning.domene.SatsType
 import no.nav.familie.ba.sak.beregning.domene.TilkjentYtelse
 import no.nav.familie.ba.sak.beregning.domene.TilkjentYtelseRepository
-import no.nav.familie.ba.sak.common.førsteDagINesteMåned
-import no.nav.familie.ba.sak.common.sisteDagIForrigeMåned
-import no.nav.familie.ba.sak.common.sisteDagIMåned
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.objectMapper
 import no.nav.familie.kontrakter.felles.oppdrag.Utbetalingsoppdrag
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
-import java.time.YearMonth
 
 @Service
 class BeregningService(
@@ -31,7 +22,7 @@ class BeregningService(
         private val fagsakService: FagsakService,
         private val tilkjentYtelseRepository: TilkjentYtelseRepository,
         private val behandlingResultatRepository: BehandlingResultatRepository,
-        private val satsService: SatsService
+        private val tilkjentYtelseService: TilkjentYtelseService
 ) {
     fun hentAndelerTilkjentYtelseForBehandling(behandlingId: Long): List<AndelTilkjentYtelse> {
         return andelTilkjentYtelseRepository.finnAndelerTilkjentYtelseForBehandling(behandlingId)
@@ -50,19 +41,9 @@ class BeregningService(
         val behandlingResultat = behandlingResultatRepository.findByBehandlingAndAktiv(behandling.id)
                 ?: throw IllegalStateException("Kunne ikke hente behandlingsresultat for behandling med id ${behandling.id}")
 
-        val tilkjentYtelse = TilkjentYtelse(
-                behandling = behandling,
-                opprettetDato = LocalDate.now(),
-                endretDato = LocalDate.now()
-        )
+        val tilkjentYtelse = tilkjentYtelseService
+                .beregnTilkjentYtelse(behandlingResultat, personopplysningGrunnlag)
 
-        val andelerTilkjentYtelse = mapBehandlingResultatTilAndelerTilkjentYtelse(
-                behandling.id,
-                behandlingResultat,
-                tilkjentYtelse,
-                personopplysningGrunnlag
-        )
-        tilkjentYtelse.andelerTilkjentYtelse.addAll(andelerTilkjentYtelse)
         tilkjentYtelseRepository.save(tilkjentYtelse)
 
         return fagsakService.hentRestFagsak(behandling.fagsak.id)
@@ -95,84 +76,5 @@ class BeregningService(
             this.opphørFom = opphørsdato
         }
     }
-
-    private fun mapBehandlingResultatTilAndelerTilkjentYtelse(behandlingId: Long,
-                                                              behandlingResultat: BehandlingResultat,
-                                                              tilkjentYtelse: TilkjentYtelse,
-                                                              personopplysningGrunnlag: PersonopplysningGrunnlag)
-            : List<AndelTilkjentYtelse> {
-
-        val identBarnMap = personopplysningGrunnlag.barna
-                .associateBy { it.personIdent.ident }
-
-        val søkerMap = personopplysningGrunnlag.søker
-                .associateBy { it.personIdent.ident }
-
-        val innvilgetPeriodeResultatSøker = behandlingResultat.periodeResultater.filter {
-            søkerMap.containsKey(it.personIdent) && it.hentSamletResultat() == BehandlingResultatType.INNVILGET
-        }
-        val innvilgedePeriodeResultatBarna = behandlingResultat.periodeResultater.filter {
-            identBarnMap.containsKey(it.personIdent) && it.hentSamletResultat() == BehandlingResultatType.INNVILGET
-        }
-
-        return innvilgedePeriodeResultatBarna.flatMap { periodeResultatBarn ->
-            innvilgetPeriodeResultatSøker.filter { it.overlapper(periodeResultatBarn) }.flatMap { overlappendePerioderesultatSøker ->
-                val person = identBarnMap[periodeResultatBarn.personIdent]!!
-                val stønadFom = maks(overlappendePerioderesultatSøker.periodeFom, periodeResultatBarn.periodeFom)
-                val stønadTom = minimum(overlappendePerioderesultatSøker.periodeTom, periodeResultatBarn.periodeTom)
-
-                val beløpsperioder = satsService.hentGyldigSatsFor(
-                        satstype = SatsType.ORBA,
-                        stønadFraOgMed = settRiktigStønadFom(stønadFom),
-                        stønadTilOgMed = settRiktigStønadTom(periodeResultatBarn.periodeTomKommerFra18ÅrsVilkår, stønadTom)
-                )
-
-                beløpsperioder.map { beløpsperiode ->
-                    AndelTilkjentYtelse(
-                            behandlingId = behandlingId,
-                            tilkjentYtelse = tilkjentYtelse,
-                            personId = person.id,
-                            stønadFom = beløpsperiode.fraOgMed.atDay(1),
-                            stønadTom = beløpsperiode.tilOgMed.atEndOfMonth(),
-                            beløp = beløpsperiode.beløp,
-                            type = Ytelsetype.ORDINÆR_BARNETRYGD
-                    )
-                }
-            }
-        }
-    }
-
-    private fun settRiktigStønadFom(fraOgMed: LocalDate): YearMonth =
-            YearMonth.from(fraOgMed.førsteDagINesteMåned())
-
-    private fun settRiktigStønadTom(erBarnetrygdTil18ÅrsDag: Boolean, tilOgMed: LocalDate): YearMonth {
-        return if (erBarnetrygdTil18ÅrsDag)
-            YearMonth.from(tilOgMed.sisteDagIForrigeMåned())
-        else
-            YearMonth.from(tilOgMed.plusMonths(1).sisteDagIMåned())
-    }
 }
 
-private fun maks(periodeFomSoker: LocalDate?, periodeFomBarn: LocalDate?): LocalDate {
-    if (periodeFomSoker == null && periodeFomBarn == null) {
-        throw IllegalStateException("Både søker og barn kan ikke ha null i periodeFom dato")
-    }
-    return when {
-        periodeFomSoker == null -> periodeFomBarn!!
-        periodeFomBarn == null -> periodeFomSoker
-        periodeFomBarn > periodeFomSoker -> periodeFomBarn
-        else -> periodeFomSoker
-    }
-}
-
-private fun minimum(periodeTomSoker: LocalDate?, periodeTomBarn: LocalDate?): LocalDate {
-    if (periodeTomSoker == null && periodeTomBarn == null) {
-        throw IllegalStateException("Både søker og barn kan ikke ha null i periodeTom dato")
-    }
-    return when {
-        periodeTomSoker == null -> periodeTomBarn!!
-        periodeTomBarn == null -> periodeTomSoker
-        periodeTomBarn > periodeTomSoker -> periodeTomSoker
-        else -> periodeTomBarn
-    }
-}
