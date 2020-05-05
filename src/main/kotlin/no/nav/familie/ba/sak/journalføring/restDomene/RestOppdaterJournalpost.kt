a18f9516d9eff2d84d52bfd00073d869b7093f75
tnarland <47184872+tnarland@users.noreply.github.com>
2020-05-05 09:54:42 +0200

    Manuell journalføring backend

---
+++ src/main/kotlin/no/nav/familie/ba/sak/journalføring/restDomene/RestOppdaterJournalpost.kt
@@ -0,0 +1,20 @@
+package no.nav.familie.ba.sak.journalføring.restDomene
+
+import no.nav.familie.kontrakter.felles.journalpost.LogiskVedlegg
+import java.time.LocalDateTime
+
+
+data class RestOppdaterJournalpost(val avsender: INavnOgIdent,
+                                   val bruker: INavnOgIdent,
+                                   val datoMottatt: LocalDateTime?,
+                                   val dokumentTittel: String,
+                                   val dokumentInfoId: String,
+                                   val knyttTilFagsak: Boolean,
+                                   val eksisterendeLogiskeVedlegg: List<LogiskVedlegg>,
+                                   val logiskeVedlegg: List<LogiskVedlegg>,
+                                   val navIdent: String
+)
+
+data class INavnOgIdent (val navn: String,
+                         val id: String
+)
