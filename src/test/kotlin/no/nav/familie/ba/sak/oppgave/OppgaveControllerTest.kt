3e7ae3de0786deab351623ce4f0d47a214c89162
Geirmund Simonsen <geirmund.simonsen@nav.no>
2020-05-06 10:27:39 +0200

    endring av assert-rammeverk

--- src/test/kotlin/no/nav/familie/ba/sak/oppgave/OppgaveControllerTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/oppgave/OppgaveControllerTest.kt
@@ -87,14 +87,14 @@ class OppgaveControllerTest {
         } returns listOf(Oppgave(tema = Tema.BAR))
         val response = oppgaveController.hentOppgaver(FinnOppgaveRequest())
         val oppgaver = response.body?.data as List<Oppgave>
-        Assertions.assertThat(oppgaver).hasSize(1)
-        Assertions.assertThat(oppgaver.first().tema).isEqualTo(Tema.BAR)
+        Assertions.assertEquals(1, oppgaver.size)
+        Assertions.assertEquals(Tema.BAR, oppgaver.first().tema)
     }
 
     @Test
     fun `hentOppgaver skal feile ved ukjent behandlingstema`() {
         val oppgaver = oppgaveController.hentOppgaver(FinnOppgaveRequest(behandlingstema = "ab1000"))
-        Assertions.assertThat(oppgaver.body?.status).isEqualTo(Ressurs.Status.FEILET)
-        Assertions.assertThat(oppgaver.body?.melding).isEqualTo("Ugyldig behandlingstema")
+        Assertions.assertEquals(Ressurs.Status.FEILET, oppgaver.body?.status)
+        Assertions.assertEquals("Ugyldig behandlingstema", oppgaver.body?.melding)
     }
 }
