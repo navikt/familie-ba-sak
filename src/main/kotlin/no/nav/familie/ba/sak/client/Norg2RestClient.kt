d9b9560c6354db0d220a65f09e5332d79df213b9
Henning Håkonsen <henninghaakonsen@me.com>
2020-06-02 14:34:50 +0200

    Debug logging av URI mot norg2.

--- src/main/kotlin/no/nav/familie/ba/sak/client/Norg2RestClient.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/client/Norg2RestClient.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ba.sak.client
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import no.nav.familie.http.client.AbstractRestClient
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestOperations
@@ -18,6 +19,8 @@ class Norg2RestClient(@Value("\${NORG2_BASE_URL}") private val norg2BaseUrl: URI
 
     fun hentEnhet(enhet: String?): Enhet {
         try {
+            LoggerFactory.getLogger(this::class.java)
+                    .info("Henter enhet fra ${kontaktinformasjonUri.pathSegment("$enhet").build().toUri()}")
             return getForEntity(kontaktinformasjonUri.pathSegment("$enhet").build().toUri())
         } catch (e: Exception) {
             throw RuntimeException("Feil ved henting av enhet fra NORG2", e)
