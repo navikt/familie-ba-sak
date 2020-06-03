b16648879916b30d435a99c6ea32c1eaa10b8545
Nina Kjekstad <nina.kjekstad@nav.no>
2020-06-03 10:55:21 +0200

    Må sette constraint not null etter verdier er lagt til

--- src/main/resources/db/migration/V52__personident_andel.sql
+++ src/main/resources/db/migration/V52__personident_andel.sql
@@ -1,7 +1,11 @@
 ALTER TABLE ANDEL_TILKJENT_YTELSE
-    ADD COLUMN person_ident varchar not null;
+    ADD COLUMN person_ident varchar;
 
 UPDATE ANDEL_TILKJENT_YTELSE
 SET PERSON_IDENT = (
-    SELECT P.person_ident FROM po_person P
-                                   JOIN andel_tilkjent_ytelse aty ON p.id = aty.fk_person_id)
+    SELECT P.person_ident
+    FROM po_person P
+             JOIN andel_tilkjent_ytelse aty ON p.id = aty.fk_person_id);
+
+ALTER TABLE ANDEL_TILKJENT_YTELSE
+    ALTER COLUMN person_ident SET NOT NULL;
