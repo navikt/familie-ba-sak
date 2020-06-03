b8c7b16ed6090cdc693fc99726b2a428e0404bbe
Nina Kjekstad <nina.kjekstad@nav.no>
2020-06-03 14:14:27 +0200

    Slet med migrering når flere matcher. Skriv om script litt

--- src/main/resources/db/migration/V52__personident_andel.sql
+++ src/main/resources/db/migration/V52__personident_andel.sql
@@ -1,11 +1,11 @@
 ALTER TABLE ANDEL_TILKJENT_YTELSE
     ADD COLUMN person_ident varchar;
 
-UPDATE ANDEL_TILKJENT_YTELSE
+UPDATE ANDEL_TILKJENT_YTELSE aty
 SET PERSON_IDENT = (
     SELECT P.person_ident
     FROM po_person P
-             JOIN andel_tilkjent_ytelse aty ON p.id = aty.fk_person_id);
+             WHERE aty.fk_person_id = p.id limit 1);
 
 ALTER TABLE ANDEL_TILKJENT_YTELSE
     ALTER COLUMN person_ident SET NOT NULL;
