b7181f05dd6b1bc213aab76597be3f32e724fc31
Stig Helge Strøm <stig.helge.strom@nav.no>
2020-06-04 22:22:48 +0200

    Lagt til integrasjontest for infotrygd feed klient, flere properties på metadata

--- src/main/kotlin/no/nav/familie/ba/sak/task/SendFeedTilInfotrygdTask.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/task/SendFeedTilInfotrygdTask.kt
@@ -4,14 +4,18 @@ import no.nav.familie.ba.sak.infotrygd.InfotrygdFeedClient
 import no.nav.familie.ba.sak.infotrygd.InfotrygdFeedDto
 import no.nav.familie.ba.sak.infotrygd.InfotrygdFeedService
 import no.nav.familie.kontrakter.felles.objectMapper
+import no.nav.familie.log.IdUtils
+import no.nav.familie.log.mdc.MDCConstants
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
+import org.slf4j.MDC
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 @TaskStepBeskrivelse(taskStepType = SendFeedTilInfotrygdTask.TASK_STEP_TYPE,
-        beskrivelse = "Send fødselshendelse til Infotrygd feed.")
+                     beskrivelse = "Send fødselshendelse til Infotrygd feed.")
 class SendFeedTilInfotrygdTask(
         private val infotrygdFeedClient: InfotrygdFeedClient) : AsyncTaskStep {
 
@@ -26,8 +30,16 @@ class SendFeedTilInfotrygdTask(
         fun opprettTask(fnrBarn: String): Task {
             InfotrygdFeedService.secureLogger.info("Send fødselsmelding for $fnrBarn til Infotrygd.")
 
+            val metadata = Properties().apply {
+                this["personIdentBarn"] = fnrBarn
+                if (!MDC.get(MDCConstants.MDC_CALL_ID).isNullOrEmpty()) {
+                    this["callId"] = MDC.get(MDCConstants.MDC_CALL_ID) ?: IdUtils.generateId()
+                }
+            }
+
             return Task.nyTask(type = TASK_STEP_TYPE,
-                    payload = objectMapper.writeValueAsString(InfotrygdFeedDto(fnrBarn = fnrBarn))
+                               payload = objectMapper.writeValueAsString(InfotrygdFeedDto(fnrBarn = fnrBarn)),
+                               properties = metadata
             )
         }
     }
