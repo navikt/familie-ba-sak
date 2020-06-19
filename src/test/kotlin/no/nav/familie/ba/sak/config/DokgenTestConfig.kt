d39e521b4368cbf051867ce49ad4c59183cb4af5
Tor Nærland <tor.nerland@nav.no>
2020-06-19 15:15:17 +0200

    fikser kompileringsfeil DokgenTestConfig etter opprydning i DokgenKlient

--- src/test/kotlin/no/nav/familie/ba/sak/config/DokgenTestConfig.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/config/DokgenTestConfig.kt
@@ -2,11 +2,8 @@ package no.nav.familie.ba.sak.config
 
 import io.mockk.every
 import io.mockk.mockk
-import no.nav.familie.ba.sak.common.lagBehandling
-import no.nav.familie.ba.sak.common.lagVedtak
 import no.nav.familie.ba.sak.dokument.DokGenKlient
 import no.nav.familie.ba.sak.dokument.DokumentService
-import no.nav.familie.ba.sak.dokument.testDokumentHeaderFelter
 import no.nav.familie.kontrakter.felles.Ressurs.Companion.success
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
@@ -35,12 +32,4 @@ class DokgenTestConfig {
         return dokGenKlient
     }
 
-    @Bean
-    @Profile("mock-dokgen-negative")
-    @Primary
-    fun mockDokGenNegativeService(): DokGenKlient {
-        val dokgenService: DokGenKlient = mockk()
-        every { dokgenService.lagHtmlFraMarkdown("Innvilget", "TEST_MARKDOWN_MOCKUP", testDokumentHeaderFelter) } throws IllegalStateException()
-        return dokgenService
-    }
 }
