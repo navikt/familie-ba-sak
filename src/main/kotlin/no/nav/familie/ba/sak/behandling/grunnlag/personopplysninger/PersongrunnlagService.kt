5b13f659a34881673085febebe154c86a57449a3
Meng Zhu <meng.zhu@nav.no>
2020-06-24 13:02:24 +0200

    Refactor to kotlin style

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/grunnlag/personopplysninger/PersongrunnlagService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/grunnlag/personopplysninger/PersongrunnlagService.kt
@@ -3,22 +3,17 @@ package no.nav.familie.ba.sak.behandling.grunnlag.personopplysninger
 import no.nav.familie.ba.sak.behandling.domene.Behandling
 import no.nav.familie.ba.sak.integrasjoner.IntegrasjonClient
 import no.nav.familie.ba.sak.integrasjoner.domene.Ident
-import no.nav.familie.ba.sak.personopplysninger.domene.AktørId
 import no.nav.familie.ba.sak.personopplysninger.domene.PersonIdent
 import no.nav.familie.ba.sak.sikkerhet.SikkerhetContext
 import no.nav.familie.kontrakter.felles.personinfo.Bostedsadresse
-import no.nav.familie.kontrakter.felles.personinfo.Vegadresse
 import org.slf4j.LoggerFactory
-import org.springframework.core.env.Environment
 import org.springframework.stereotype.Service
-import java.time.LocalDate
 
 @Service
 class PersongrunnlagService(
         private val personRepository: PersonRepository,
         private val personopplysningGrunnlagRepository: PersonopplysningGrunnlagRepository,
-        private val integrasjonClient: IntegrasjonClient,
-        private val environment: Environment
+        private val integrasjonClient: IntegrasjonClient
 ) {
 
     fun lagreOgDeaktiverGammel(personopplysningGrunnlag: PersonopplysningGrunnlag): PersonopplysningGrunnlag {
@@ -51,63 +46,23 @@ class PersongrunnlagService(
                                                      behandling: Behandling) {
         val personopplysningGrunnlag = lagreOgDeaktiverGammel(PersonopplysningGrunnlag(behandlingId = behandling.id))
 
-        val (søker, barn) =
-        if(environment.activeProfiles.contains("e2e")){
-            val søker= e2eMockSøker(fødselsnummer, personopplysningGrunnlag)
-            val barn= e2eMockBarn(barnasFødselsnummer, personopplysningGrunnlag)
-            Pair(søker, barn)
-        }else{
-            val personinfo = integrasjonClient.hentPersoninfoFor(fødselsnummer)
-            val aktørId = integrasjonClient.hentAktivAktørId(Ident(fødselsnummer))
-            val søker = Person(personIdent = behandling.fagsak.hentAktivIdent(),
-                               type = PersonType.SØKER,
-                               personopplysningGrunnlag = personopplysningGrunnlag,
-                               fødselsdato = personinfo.fødselsdato,
-                               aktørId = aktørId,
-                               navn = personinfo.navn ?: "",
-                               bostedsadresse = GrBostedsadresse.fraBostedsadresse(personinfo.bostedsadresse),
-                               kjønn = personinfo.kjønn ?: Kjønn.UKJENT
-            )
-            val barn = hentBarn(barnasFødselsnummer, personopplysningGrunnlag)
-            Pair(søker, barn)
-        }
+        val personinfo = integrasjonClient.hentPersoninfoFor(fødselsnummer)
+        val aktørId = integrasjonClient.hentAktivAktørId(Ident(fødselsnummer))
+        val søker = Person(personIdent = behandling.fagsak.hentAktivIdent(),
+                           type = PersonType.SØKER,
+                           personopplysningGrunnlag = personopplysningGrunnlag,
+                           fødselsdato = personinfo.fødselsdato,
+                           aktørId = aktørId,
+                           navn = personinfo.navn ?: "",
+                           bostedsadresse = GrBostedsadresse.fraBostedsadresse(personinfo.bostedsadresse),
+                           kjønn = personinfo.kjønn ?: Kjønn.UKJENT
+        )
         personopplysningGrunnlag.personer.add(søker)
-        personopplysningGrunnlag.personer.addAll(barn)
+        personopplysningGrunnlag.personer.addAll(hentBarn(barnasFødselsnummer, personopplysningGrunnlag))
 
         secureLogger.info("Setter persongrunnlag med søker: ${fødselsnummer} og barn: ${barnasFødselsnummer}")
         secureLogger.info("Barna på persongrunnlaget som lagres: ${personopplysningGrunnlag.barna.map { it.personIdent.ident }}")
         personopplysningGrunnlagRepository.save(personopplysningGrunnlag)
-    }
-
-    private fun e2eMockSøker(fødselsnummer: String, personopplysningGrunnlag: PersonopplysningGrunnlag): Person {
-        return Person(personIdent = PersonIdent(fødselsnummer),
-                      type = PersonType.SØKER,
-                      personopplysningGrunnlag = personopplysningGrunnlag,
-                      fødselsdato = LocalDate.of(1990, 1, 1),
-                      aktørId = AktørId("e2eaktoerid"),
-                      navn = "Etoe Tester",
-                      bostedsadresse = GrBostedsadresse.fraBostedsadresse(Bostedsadresse(vegadresse = Vegadresse(
-                              1, "1", "B", "H101",
-                              "Testerveg", "0101", "whatever", "1011"
-                      ))),
-                      kjønn = Kjønn.KVINNE
-        )
-    }
-
-    private fun e2eMockBarn(barnasFødselsnummer: List<String>, personopplysningGrunnlag: PersonopplysningGrunnlag): List<Person> {
-        return listOf(Person(personIdent = if (!barnasFødselsnummer.isEmpty()) PersonIdent(barnasFødselsnummer.first()) else PersonIdent(
-                "12345678910"),
-                             type = PersonType.BARN,
-                             personopplysningGrunnlag = personopplysningGrunnlag,
-                             fødselsdato = LocalDate.of(2009, 1, 1),
-                             aktørId = AktørId("e2ebarnaktoerid"),
-                             navn = "Etoe Barnet",
-                             bostedsadresse = GrBostedsadresse.fraBostedsadresse(Bostedsadresse(vegadresse = Vegadresse(
-                                     1, "1", "B", "H101",
-                                     "Testerveg", "0101", "whatever", "1011"
-                             ))),
-                             kjønn = Kjønn.KVINNE
-        ))
     }
 
     private fun tilGrBostedsadresse(bostedsadresse: Bostedsadresse?): Set<GrBostedsadresse> {
