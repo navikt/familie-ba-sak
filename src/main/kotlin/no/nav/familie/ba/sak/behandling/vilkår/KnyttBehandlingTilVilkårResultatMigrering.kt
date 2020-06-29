91cfc9d0a771ffd4dca484d8dfaee8ecc6419ded
Henning Håkonsen <henninghaakonsen@me.com>
2020-06-29 11:48:43 +0200

    Henter alle behandlingsresultater og fyller ut behandling id dersom det ikke finnes.

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/vilkår/KnyttBehandlingTilVilkårResultatMigrering.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/vilkår/KnyttBehandlingTilVilkårResultatMigrering.kt
@@ -1,29 +1,22 @@
 package no.nav.familie.ba.sak.behandling.vilkår
 
-import no.nav.familie.ba.sak.behandling.domene.BehandlingRepository
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
 class KnyttBehandlingTilVilkårResultatMigrering(
-        private val behandlingRepository: BehandlingRepository,
         private val behandlingResultatRepository: BehandlingResultatRepository
 ) {
 
     @Scheduled(initialDelay = 1000, fixedDelay = Long.MAX_VALUE)
     fun migrerVilkårResultat() {
         LOG.info("Migrerer over behandling id til vilkår resultat: start")
-        val behandlinger = behandlingRepository.findAll()
 
-        behandlinger.forEach { behandling ->
-            val behandlingResultat = behandlingResultatRepository.findByBehandlingAndAktiv(behandlingId = behandling.id)
-
-            if (behandlingResultat != null) {
-                behandlingResultat.personResultater.forEach { personResultat ->
-                    personResultat.vilkårResultater.forEach {
-                        it.behandlingId = behandling.id
-                    }
+        behandlingResultatRepository.findAll().forEach { behandlingResultat ->
+            behandlingResultat.personResultater.forEach { personResultat ->
+                personResultat.vilkårResultater.forEach {
+                    it.behandlingId = it.behandlingId ?: behandlingResultat.behandling.id
                 }
 
                 behandlingResultatRepository.save(behandlingResultat)
