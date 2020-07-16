9e93e9273141673ccd4badc3459645d3e6ed1d9d
Henning Håkonsen <henninghaakonsen@me.com>
2020-07-16 10:39:57 +0200

    Dummy commit for å trigge deploy.

--- src/main/kotlin/no/nav/familie/ba/sak/oppgave/OppgaveService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/oppgave/OppgaveService.kt
@@ -103,6 +103,6 @@ class OppgaveService(private val integrasjonClient: IntegrasjonClient,
     }
 
     companion object {
-        val LOG = LoggerFactory.getLogger(this::class.java)
+        private val LOG = LoggerFactory.getLogger(this::class.java)
     }
 }
