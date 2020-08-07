75d8cb3ec80c680134ec68de6fce8869e86c9ba5
Geirmund Simonsen <geirmund.simonsen@nav.no>
2020-08-07 13:18:34 +0200

    fjern ubrukt kode

--- src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/domene/Arbeidsforhold.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/domene/Arbeidsforhold.kt
@@ -6,21 +6,17 @@ class Arbeidsforhold(val navArbeidsforholdId: Long? = null,
                      val arbeidsforholdId: String? = null,
                      val arbeidstaker: Arbeidstaker? = null,
                      val arbeidsgiver: Arbeidsgiver? = null,
-                     val opplysningspliktig: Opplysningspliktig? = null,
                      val type: String? = null,
                      val ansettelsesperiode: Ansettelsesperiode? = null,
-                     val arbeidsavtaler: List<Arbeidsavtaler>? = null,
-                     val varsler: List<Varsler>? = null)
+                     val arbeidsavtaler: List<Arbeidsavtaler>? = null)
 
 class Arbeidstaker(val type: String? = null,
                    val offentligIdent: String? = null,
                    val aktoerId: String? = null)
 
-class Arbeidsgiver(val type: String? = null,
-                   val organisasjonsnummer: String? = null)
-
-class Opplysningspliktig(val type: String? = null,
-                         val organisasjonsnummer: String? = null)
+class Arbeidsgiver(val type: ArbeidsgiverType? = null,
+                   val organisasjonsnummer: String? = null,
+                   val offentligIdent: String? = null)
 
 class Ansettelsesperiode(val periode: Periode? = null,
                          val bruksperiode: Periode? = null)
@@ -33,11 +29,13 @@ class Arbeidsavtaler(val arbeidstidsordning: String? = null,
                      val bruksperiode: Periode? = null,
                      val gyldighetsperiode: Periode? = null)
 
-class Varsler(val entitet: String? = null,
-              val varslingskode: String? = null)
-
 class Periode(val fom: LocalDate? = null,
               val tom: LocalDate? = null)
+
+enum class ArbeidsgiverType {
+    Organisasjon,
+    Person
+}
 
 class ArbeidsforholdRequest(val personIdent: String,
                             val ansettelsesperiodeFom: LocalDate)
