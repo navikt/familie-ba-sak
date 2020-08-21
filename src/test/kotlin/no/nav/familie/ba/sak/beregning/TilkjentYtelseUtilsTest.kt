a31a8b2270de7c30af9bfeac5d26971bad73ab31
Martine Enger <marenger@student.matnat.uio.no>
2020-08-21 13:11:03 +0200

    Tester på å hente riktig nåværende beløp

--- src/test/kotlin/no/nav/familie/ba/sak/beregning/TilkjentYtelseUtilsTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/beregning/TilkjentYtelseUtilsTest.kt
@@ -57,4 +57,51 @@ class TilkjentYtelseUtilsTest {
 
         assertEquals(18972, etterbetalingsbeløp)
     }
+
+    @Test
+    fun `Skal hente riktig nåværende beløp for to barn og etterbetaling`() {
+        val behandling = lagBehandling()
+        val baseDato = LocalDate.now()
+        val personopplysningGrunnlag = lagTestPersonopplysningGrunnlag(behandling.id, ClientMocks.søkerFnr[0], ClientMocks.barnFnr.toList())
+        val vedtak = lagVedtak(behandling)
+        val barn1 = personopplysningGrunnlag.barna.first()
+        val barn2 = personopplysningGrunnlag.barna.last()
+
+        val andelTilkjentYtelseBarn1 = lagAndelTilkjentYtelse(baseDato.minusYears(1).withDayOfMonth(1).toString(),
+                                                              baseDato.plusYears(1).sisteDagIMåned().toString(),
+                                                              behandling = behandling,
+                                                              person = barn1)
+        val andelTilkjentYtelseBarn2 = lagAndelTilkjentYtelse(baseDato.minusMonths(1).withDayOfMonth(1).toString(),
+                                                              baseDato.plusYears(1).toString(),
+                                                              behandling = behandling,
+                                                              person = barn2)
+        val tilkjentYtelse = lagInitiellTilkjentYtelse(behandling).copy(andelerTilkjentYtelse = mutableSetOf(andelTilkjentYtelseBarn1, andelTilkjentYtelseBarn2))
+        val beregningsoversikt = TilkjentYtelseUtils.hentBeregningOversikt(tilkjentYtelse, personopplysningGrunnlag)
+
+        val nåværendeBeløp = TilkjentYtelseUtils.beregnNåværendeBeløp(beregningsoversikt, vedtak)
+
+        assertEquals(2108, nåværendeBeløp)
+    }
+
+    @Test
+    fun `Skal hente riktig nåværende beløp for ett barn uten etterbetaling`() {
+        val behandling = lagBehandling()
+        val baseDato = LocalDate.now()
+        val personopplysningGrunnlag = lagTestPersonopplysningGrunnlag(behandling.id,
+                                                                       ClientMocks.søkerFnr[0],
+                                                                       ClientMocks.barnFnr.toList().subList(0, 1))
+        val vedtak = lagVedtak(behandling)
+        val barn1 = personopplysningGrunnlag.barna.first()
+
+        val andelTilkjentYtelseBarn1 = lagAndelTilkjentYtelse(baseDato.plusMonths(1).withDayOfMonth(1).toString(),
+                                                              baseDato.plusYears(5).sisteDagIMåned().toString(),
+                                                              behandling = behandling,
+                                                              person = barn1)
+        val tilkjentYtelse = lagInitiellTilkjentYtelse(behandling).copy(andelerTilkjentYtelse = mutableSetOf(andelTilkjentYtelseBarn1))
+        val beregningsoversikt = TilkjentYtelseUtils.hentBeregningOversikt(tilkjentYtelse, personopplysningGrunnlag)
+
+        val nåværendeBeløp = TilkjentYtelseUtils.beregnNåværendeBeløp(beregningsoversikt, vedtak)
+
+        assertEquals(1054, nåværendeBeløp)
+    }
 }
