d3177f393bc41db040aa44623f8460256784858b
Nina Kjekstad <nina.kjekstad@nav.no>
2020-08-21 11:40:43 +0200

    Fiks test

--- src/test/kotlin/no/nav/familie/ba/sak/dokument/DokumentControllerTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/dokument/DokumentControllerTest.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ba.sak.dokument
 
 import io.mockk.every
 import io.mockk.mockk
+import no.nav.familie.ba.sak.behandling.BehandlingService
 import no.nav.familie.ba.sak.behandling.vedtak.VedtakService
 import no.nav.familie.ba.sak.common.lagVedtak
 import no.nav.familie.kontrakter.felles.Ressurs
@@ -20,7 +21,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 @Tag("integration")
 class DokumentControllerTest(
         @Autowired
-        private val dokumentService: DokumentService
+        private val dokumentService: DokumentService,
+        @Autowired
+        private val behandlingService: BehandlingService
+
 ) {
 
     @Test
@@ -28,7 +32,7 @@ class DokumentControllerTest(
     fun `Test generer vedtaksbrev`() {
         val mockDokumentService: DokumentService = mockk()
         val vedtakService: VedtakService = mockk(relaxed = true)
-        val mockDokumentController = DokumentController(mockDokumentService, vedtakService)
+        val mockDokumentController = DokumentController(mockDokumentService, vedtakService, behandlingService)
         every { vedtakService.hent(any()) } returns lagVedtak()
         every { mockDokumentService.genererBrevForVedtak(any()) } returns "pdf".toByteArray()
 
@@ -41,7 +45,7 @@ class DokumentControllerTest(
     fun `Test hent pdf vedtak`() {
         val mockDokumentService: DokumentService = mockk()
         val vedtakService: VedtakService = mockk()
-        val mockDokumentController = DokumentController(mockDokumentService, vedtakService)
+        val mockDokumentController = DokumentController(mockDokumentService, vedtakService, behandlingService)
         every { vedtakService.hent(any()) } returns lagVedtak()
         every { mockDokumentService.hentBrevForVedtak(any()) } returns Ressurs.success("pdf".toByteArray())
 
