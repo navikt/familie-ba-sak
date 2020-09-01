1e099e5a17c7f054b0b412e933d00e31eb3ec8c6
Nina Kjekstad <nina.kjekstad@nav.no>
2020-09-01 15:31:11 +0200

    Legg til metrikk for oppgavetyper

--- src/main/kotlin/no/nav/familie/ba/sak/task/OpprettOppgaveTask.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/task/OpprettOppgaveTask.kt
@@ -1,5 +1,8 @@
 package no.nav.familie.ba.sak.task
 
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.Metrics
+import no.nav.familie.ba.sak.behandling.vilkår.BehandlingResultatType
 import no.nav.familie.ba.sak.oppgave.OppgaveService
 import no.nav.familie.ba.sak.task.dto.OpprettOppgaveDTO
 import no.nav.familie.kontrakter.felles.objectMapper
@@ -19,6 +22,11 @@ class OpprettOppgaveTask(
         private val oppgaveService: OppgaveService,
         private val taskRepository: TaskRepository) : AsyncTaskStep {
 
+
+    private val antallOppgaveTyper: Map<Oppgavetype, Counter> = Oppgavetype.values().map {
+        it to Metrics.counter("oppgave.opprettet", "type", it.name)
+    }.toMap()
+
     override fun doTask(task: Task) {
         val opprettOppgaveDTO = objectMapper.readValue(task.payload, OpprettOppgaveDTO::class.java)
         task.metadata["oppgaveId"] = oppgaveService.opprettOppgave(
@@ -27,6 +35,7 @@ class OpprettOppgaveTask(
                 opprettOppgaveDTO.fristForFerdigstillelse
         )
         taskRepository.saveAndFlush(task)
+        antallOppgaveTyper[opprettOppgaveDTO.oppgavetype]?.increment()
     }
 
     companion object {
@@ -35,7 +44,9 @@ class OpprettOppgaveTask(
         fun opprettTask(behandlingId: Long, oppgavetype: Oppgavetype, fristForFerdigstillelse: LocalDate): Task {
             return Task.nyTask(
                     type = TASK_STEP_TYPE,
-                    payload = objectMapper.writeValueAsString(OpprettOppgaveDTO(behandlingId, oppgavetype, fristForFerdigstillelse))
+                    payload = objectMapper.writeValueAsString(OpprettOppgaveDTO(behandlingId,
+                                                                                oppgavetype,
+                                                                                fristForFerdigstillelse))
             )
         }
     }
