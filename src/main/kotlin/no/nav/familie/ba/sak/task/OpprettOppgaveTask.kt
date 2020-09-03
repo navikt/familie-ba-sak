7e4894527aaf004ed9a62879e4e93650873d7625
Geirmund Simonsen <geirmund.simonsen@nav.no>
2020-09-03 10:22:08 +0200

    variabelnavn skal være likt klassenavn

--- src/main/kotlin/no/nav/familie/ba/sak/task/OpprettOppgaveTask.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/task/OpprettOppgaveTask.kt
@@ -20,12 +20,12 @@ class OpprettOppgaveTask(
         private val taskRepository: TaskRepository) : AsyncTaskStep {
 
     override fun doTask(task: Task) {
-        val opprettOppgaveDTO = objectMapper.readValue(task.payload, OpprettOppgaveTaskDTO::class.java)
+        val opprettOppgaveTaskDTO = objectMapper.readValue(task.payload, OpprettOppgaveTaskDTO::class.java)
         task.metadata["oppgaveId"] = oppgaveService.opprettOppgave(
-                opprettOppgaveDTO.behandlingId,
-                opprettOppgaveDTO.oppgavetype,
-                opprettOppgaveDTO.fristForFerdigstillelse,
-                beskrivelse = opprettOppgaveDTO.beskrivelse
+                opprettOppgaveTaskDTO.behandlingId,
+                opprettOppgaveTaskDTO.oppgavetype,
+                opprettOppgaveTaskDTO.fristForFerdigstillelse,
+                beskrivelse = opprettOppgaveTaskDTO.beskrivelse
         )
         taskRepository.saveAndFlush(task)
     }
