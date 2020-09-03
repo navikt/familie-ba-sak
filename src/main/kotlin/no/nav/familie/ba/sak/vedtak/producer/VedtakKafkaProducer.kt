88a30cae76366e5760e3ac48626291c9646c4511
Stig Helge Strøm <stig.helge.strom@nav.no>
2020-09-03 14:58:32 +0200

    Task for publiser vedtak

--- src/main/kotlin/no/nav/familie/ba/sak/vedtak/producer/VedtakKafkaProducer.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/vedtak/producer/VedtakKafkaProducer.kt
@@ -3,25 +3,48 @@ package no.nav.familie.ba.sak.vedtak.producer
 import no.nav.familie.eksterne.kontrakter.VedtakDVH
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 import java.util.UUID.randomUUID
 
+interface VedtakProducer {
+    fun sendMessage(vedtak: VedtakDVH): Long
+}
+
 @Service
-@Profile("kafka-lokal", "preprod", "prod")
-class VedtakKafkaProducer {
+@ConditionalOnProperty(
+        value=["funksjonsbrytere.vedtak.producer.enabled"],
+        havingValue = "true",
+        matchIfMissing = false)
+@Primary
+class VedtakKafkaProducer : VedtakProducer {
 
     @Autowired
     lateinit var kafkaTemplate: KafkaTemplate<String, VedtakDVH>
 
-    fun sendMessage(vedtak: VedtakDVH) {
-        val response = kafkaTemplate.send(TOPIC, randomUUID().toString(), vedtak).get()
+    override fun sendMessage(vedtak: VedtakDVH): Long {
+        val response = kafkaTemplate.send(TOPIC, vedtak.behandlingsId, vedtak).get()
         logger.info("$TOPIC -> message sent -> ${response.recordMetadata.offset()}")
+        return response.recordMetadata.offset()
     }
 
     companion object {
         private val logger = LoggerFactory.getLogger(VedtakKafkaProducer::class.java)
         private const val TOPIC = "aapen-barnetrygd-vedtak-v1"
+    }
+}
+
+@Service
+class VedtakMockProducer : VedtakProducer {
+
+
+    val logger = LoggerFactory.getLogger(this::class.java)
+
+    override fun sendMessage(vedtak: VedtakDVH): Long {
+        logger.info("Skipper sending av vedtak for ${vedtak.behandlingsId} fordi kafka ikke er enablet")
+        return 0
     }
 }
--- src/test/kotlin/DevLauncherPostgres.kt
+++ src/test/kotlin/DevLauncherPostgres.kt
@@ -10,7 +10,6 @@ fun main(args: Array<String>) {
                                                                      "mock-infotrygd-feed",
                                                                      "mock-infotrygd-barnetrygd",
                                                                      "mock-sts",
-    //                                                                 "kafka-lokal",
                                                                      "mock-pdl"
     ).run(*args)
 }
