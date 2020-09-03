19bfb5f9d731321b81403344bf50cf0f80a2b8eb
Geirmund Simonsen <geirmund.simonsen@nav.no>
2020-09-03 15:03:31 +0200

    Feature/legge til beskrivelse til oppgave (#415)
    * Utvid opprettOppgave med beskrivelse som parameter.
    
    * undergoing work
    
    * beskrivelse -> begrunnelse
    Videre uttrekk av begrunnelse fra regler i korrekt rekkefølge.
    Endret lovlig opphold (EØS) til å gi begrunnelser for alle tilfeller.
    
    * fiks tester
    
    * Riktig rekkefølge på filtreringsregler, omskrivning slik at man ikke er avhengig av implementasjonsdetaljer i NARE.
    
    * add mocks to make new code work with tests
    
    * test code for beskrivelse av filtreringsregler
    
    * test code for beskrivelse av vilkårsvurdering
    
    * formuleringsendringer
    
    * Legge til test for filtreringsregler rekkefølge
    
    * Forbedret navngivning.
    
    * la til fødselsdato til oppgavesbeskrivelse
    
    * lag function for sammenligner adresser
    
    * la til mer space
    
    * variabelnavn skal være likt klassenavn
    
    * Fix error with tests after adding birthday to cause of oppgaver
    
    * fix merge issue
    
    Co-authored-by: Meng Zhu <meng.zhu@nav.no>
    Co-authored-by: Meng Zhu <mengzhu331@outlook.com>

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/fødselshendelse/filtreringsregler/Filtreringsregler.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/fødselshendelse/filtreringsregler/Filtreringsregler.kt
@@ -2,7 +2,8 @@ package no.nav.familie.ba.sak.behandling.fødselshendelse.filtreringsregler
 
 import no.nav.nare.core.specifications.Spesifikasjon
 
-
+// Rekkefølgen på reglene er vesentlig, og brukes når man finner riktig begrunnelse
+// i forbindelse med opprettelse av oppgave.
 enum class Filtreringsregler(val spesifikasjon: Spesifikasjon<Fakta>) {
 
     MOR_HAR_GYLDIG_FOEDSELSNUMMER(Spesifikasjon(
@@ -20,30 +21,30 @@ enum class Filtreringsregler(val spesifikasjon: Spesifikasjon<Fakta>) {
             "BARNET_ER_UNDER_6_MND",
             implementasjon = { barnetErUnder6mnd(this) })
     ),
-    MOR_ER_OVER_18_AAR(Spesifikasjon(
-            "Mor er over 18 år",
-            "MOR_ER_OVER_18_AAR",
-            implementasjon = { morErOver18år(this) })
-    ),
-    MER_ENN_5_MND_SIDEN_FORRIGE_BARN(Spesifikasjon(
-            "Det har gått mer enn 5 måneder siden forrige barn ble født",
-            "MER_ENN_5_MND_SIDEN_FORRIGE_BARN",
-            implementasjon = { merEnn5mndSidenForrigeBarn(this) })
+    BARNET_LEVER(Spesifikasjon(
+            "Barnet lever",
+            "BARNET_LEVER",
+            implementasjon = { barnetLever(this) })
     ),
     MOR_LEVER(Spesifikasjon(
             "Mor lever",
             "MOR_LEVER",
             implementasjon = { morLever(this) })
     ),
-    BARNET_LEVER(Spesifikasjon(
-            "Barnet lever",
-            "BARNET_LEVER",
-            implementasjon = { barnetLever(this) })
+    MOR_ER_OVER_18_AAR(Spesifikasjon(
+            "Mor er over 18 år",
+            "MOR_ER_OVER_18_AAR",
+            implementasjon = { morErOver18år(this) })
     ),
     MOR_HAR_IKKE_VERGE(Spesifikasjon(
             "Mor har ikke verge",
             "MOR_HAR_IKKE_VERGE",
             implementasjon = { morHarIkkeVerge(this) })
+    ),
+    MER_ENN_5_MND_SIDEN_FORRIGE_BARN(Spesifikasjon(
+            "Det har gått mer enn 5 måneder siden forrige barn ble født",
+            "MER_ENN_5_MND_SIDEN_FORRIGE_BARN",
+            implementasjon = { merEnn5mndSidenForrigeBarn(this) })
     );
 
     override fun toString(): String {
