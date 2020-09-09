208f679f3b280b2dea7d211f4be97a6d647d6929
MarcusGustafson74 <marcus.gustafson@nav.no>
2020-09-09 08:35:51 +0200

    Update src/main/kotlin/no/nav/familie/ba/sak/behandling/fødselshendelse/filtreringsregler/Fakta.kt
    Co-authored-by: Henning Håkonsen <henning.hakonsen@nav.no>

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/fødselshendelse/filtreringsregler/Fakta.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/fødselshendelse/filtreringsregler/Fakta.kt
@@ -4,9 +4,8 @@ import no.nav.familie.ba.sak.behandling.grunnlag.personopplysninger.Person
 import no.nav.familie.ba.sak.pdl.internal.PersonInfo
 
 data class Fakta(val mor: Person,
-                 val barn: List<Person>,
+                 val barnaFraHendelse: List<Person>,
                  val restenAvBarna: List<PersonInfo>,
                  val morLever: Boolean,
                  val barnetLever: Boolean,
                  val morHarVerge: Boolean)
-
