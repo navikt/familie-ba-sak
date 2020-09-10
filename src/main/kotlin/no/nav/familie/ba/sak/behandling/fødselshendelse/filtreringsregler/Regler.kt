26ee08e16992bcb7538f5b6edc8f5ddd07a5ecad
Øyvind Hagen <oyvind.hagen@nav.no>
2020-09-10 16:18:00 +0200

    Fjernet filtreringsregler (d-nummer og under 6 år) som er dekket av ba-mottak.

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/fødselshendelse/filtreringsregler/Regler.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/fødselshendelse/filtreringsregler/Regler.kt
@@ -5,27 +5,6 @@ import no.nav.familie.ba.sak.common.erFraInneværendeMåned
 import no.nav.nare.core.evaluations.Evaluering
 import java.time.LocalDate
 
-internal fun morHarGyldigFødselsnummer(fakta: Fakta): Evaluering {
-    return when (!erDnummer(fakta.mor.personIdent.ident)) {
-        true -> Evaluering.ja("Mor har gyldig fødselsnummer.")
-        false -> Evaluering.nei("Mor har ikke gyldig fødselsnummer.")
-    }
-}
-
-internal fun barnetHarGyldigFødselsnummer(fakta: Fakta): Evaluering {
-    return when (!erDnummer(fakta.barn.personIdent.ident)) {
-        true -> Evaluering.ja("Barnet har gyldig fødselsnummer.")
-        false -> Evaluering.nei("Barnet har ikke gyldig fødselsnummer.")
-    }
-}
-
-internal fun barnetErUnder6mnd(fakta: Fakta): Evaluering {
-    return when (LocalDate.now().minusMonths(6).isBefore(fakta.barn.fødselsdato)) {
-        true -> Evaluering.ja("Barnet er under 6 måneder.")
-        false -> Evaluering.nei("Barnet er over 6 måneder.")
-    }
-}
-
 internal fun morErOver18år(fakta: Fakta): Evaluering {
     return when (LocalDate.now().isAfter(fakta.mor.fødselsdato.plusYears(18))) {
         true -> Evaluering.ja("Mor er over 18 år.")
@@ -74,8 +53,4 @@ internal fun barnetsFødselsdatoInnebærerIkkeEtterbetaling(fakta: Fakta): Evalu
             Evaluering.nei("Saken medfører etterbetaling.")
         else -> Evaluering.ja("Saken medfører ikke etterbetaling.")
     }
-}
-
-private fun erDnummer(personIdent: String): Boolean {
-    return personIdent.substring(0, 1).toInt() > 3
 }
