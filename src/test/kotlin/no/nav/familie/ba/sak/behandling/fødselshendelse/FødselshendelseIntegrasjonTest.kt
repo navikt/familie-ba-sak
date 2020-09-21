71a03cf6a6cd1c5454412a01622a5b4abc28364c
Geirmund Simonsen <geirmund.simonsen@nav.no>
2020-09-21 16:07:25 +0200

    Saksbehandlertoken på auth header mot pdl (#458)
    Tar i bruk interceptor (BearerTokenWithSTSFallbackClientInterceptor) som støtter videreføring av saksbehandlertoken dersom saksbehandler er i kontekst, og som ellers defaulter til STS.
    
    Dette gjelder PdlRestClient.

--- src/test/kotlin/no/nav/familie/ba/sak/behandling/fødselshendelse/FødselshendelseIntegrasjonTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/behandling/fødselshendelse/FødselshendelseIntegrasjonTest.kt
@@ -19,6 +19,7 @@ import no.nav.familie.ba.sak.beregning.SatsService
 import no.nav.familie.ba.sak.beregning.domene.AndelTilkjentYtelseRepository
 import no.nav.familie.ba.sak.beregning.domene.SatsType
 import no.nav.familie.ba.sak.common.DbContainerInitializer
+import no.nav.familie.ba.sak.common.førsteDagIInneværendeMåned
 import no.nav.familie.ba.sak.config.FeatureToggleService
 import no.nav.familie.ba.sak.infotrygd.InfotrygdBarnetrygdClient
 import no.nav.familie.ba.sak.infotrygd.InfotrygdFeedService
@@ -141,8 +142,8 @@ class FødselshendelseIntegrasjonTest(
         Assert.assertEquals(2, andelTilkjentYtelser.filter { it.beløp == satsTillegg.beløp }.size)
 
         val reffom = now
-        val reftom = now.plusYears(18).minusMonths(2)
-        val fom = of(reffom.year, reffom.month, 1)
+        val reftom = now.plusYears(18).minusMonths(1)
+        val fom = of(reffom.year, reffom.month, 1).plusMonths(1)
         val tom = of(reftom.year, reftom.month, reftom.lengthOfMonth())
 
         val (barn1, barn2) = andelTilkjentYtelser.partition { it.personIdent == barnefnr[0] }
@@ -192,8 +193,8 @@ class FødselshendelseIntegrasjonTest(
         Assert.assertEquals(1, andelTilkjentYtelser.filter { it.beløp == satsTillegg.beløp }.size)
 
         val reffom = now
-        val reftom = now.plusYears(18).minusMonths(2)
-        val fom = of(reffom.year, reffom.month, 1)
+        val reftom = now.plusYears(18).minusMonths(1)
+        val fom = of(reffom.year, reffom.month, 1).plusMonths(1)
         val tom = of(reftom.year, reftom.month, reftom.lengthOfMonth())
 
         Assert.assertEquals(fom, andelTilkjentYtelser.minByOrNull { it.stønadFom }!!.stønadFom)
@@ -252,7 +253,8 @@ class MockConfiguration {
         every {
             personopplysningerServiceMock.hentPersoninfoMedRelasjoner(barnefnr[0])
         } returns PersonInfo(
-                fødselsdato = now.minusMonths(1),
+                //fødselsdato = now.minusMonths(1),
+                fødselsdato = now.førsteDagIInneværendeMåned(),
                 navn = "Gutt Barn",
                 kjønn = Kjønn.MANN,
                 sivilstand = SIVILSTAND.UGIFT,
@@ -263,7 +265,8 @@ class MockConfiguration {
         every {
             personopplysningerServiceMock.hentPersoninfoMedRelasjoner(barnefnr[1])
         } returns PersonInfo(
-                fødselsdato = now.minusMonths(1),
+                //fødselsdato = now.minusMonths(1),
+                fødselsdato = now.førsteDagIInneværendeMåned(),
                 navn = "Jente Barn",
                 kjønn = Kjønn.KVINNE,
                 sivilstand = SIVILSTAND.UGIFT,
@@ -274,7 +277,8 @@ class MockConfiguration {
         every {
             personopplysningerServiceMock.hentPersoninfoMedRelasjoner(barnefnr[2])
         } returns PersonInfo(
-                fødselsdato = now.minusMonths(1),
+                //fødselsdato = now.minusMonths(1),
+                fødselsdato = now.førsteDagIInneværendeMåned(),
                 navn = "Gutt Barn To",
                 kjønn = Kjønn.MANN,
                 sivilstand = SIVILSTAND.UGIFT,
