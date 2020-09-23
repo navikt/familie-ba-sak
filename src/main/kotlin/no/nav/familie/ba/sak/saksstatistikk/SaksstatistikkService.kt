dcb1cc3453060556ea22ad2daf692f5732cda6f9
Tor Nærland <tor.nerland@nav.no>
2020-09-23 09:43:35 +0200

    Mappet flesteparten av "required"-feltene, som ikke krever avklaring.
    Endret til modulbasert versjonering av eksterne kontrakter i pom.xml,
    dvs. eget versjonnummer for hhv. stønadsstatistikk og saksstatistikk.

--- src/main/kotlin/no/nav/familie/ba/sak/saksstatistikk/SaksstatistikkService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/saksstatistikk/SaksstatistikkService.kt
@@ -1,51 +1,65 @@
 package no.nav.familie.ba.sak.saksstatistikk
 
+import no.nav.familie.ba.sak.arbeidsfordeling.ArbeidsfordelingService
 import no.nav.familie.ba.sak.behandling.BehandlingService
 import no.nav.familie.ba.sak.behandling.domene.BehandlingOpprinnelse
+import no.nav.familie.ba.sak.journalføring.JournalføringService
 import no.nav.familie.ba.sak.journalføring.domene.JournalføringRepository
 import no.nav.familie.eksterne.kontrakter.saksstatistikk.BehandlingDVH
+import no.nav.familie.kontrakter.felles.journalpost.Journalposttype
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.ZonedDateTime
 
 class SaksstatistikkService(private val behandlingService: BehandlingService,
-                            private val journalføringRepository: JournalføringRepository) {
+                            private val journalføringRepository: JournalføringRepository,
+                            private val journalføringService: JournalføringService,
+                            private val arbeidsfordelingService: ArbeidsfordelingService) {
 
     fun loggBehandlingStatus(behandlingId: Long): BehandlingDVH {
         val behandling = behandlingService.hent(behandlingId)
         val journalpost = journalføringRepository.findByBehandlingId(behandlingId)
 
-        var datoMottatt: LocalDate
+        var datoMottatt: LocalDateTime // TODO: Endre til LocalDateTime i kontrakten
         when (behandling.opprinnelse) {
             BehandlingOpprinnelse.MANUELL -> {
-                datoMottatt = journalpost.
+                datoMottatt = journalpost.mapNotNull { journalføringService.hentJournalpost(it.journalpostId).data }
+                                      .filter { it.journalposttype == Journalposttype.I }
+                                      .filter { it.tittel != null && it.tittel!!.contains("søknad", ignoreCase = true) }
+                                      .mapNotNull { it.datoMottatt }
+                                      .minOrNull() ?: throw error("")
             }
-
-
+            BehandlingOpprinnelse.AUTOMATISK_VED_FØDSELSHENDELSE -> {
+                datoMottatt = behandling.opprettetTidspunkt  // TODO: trenger avklaring
+            }
         }
+
+        val behandlendeEnhetsKode = arbeidsfordelingService.bestemBehandlendeEnhet(behandling)
+        val ansvarligEnhetKode = arbeidsfordelingService.hentBehandlendeEnhet(behandling.fagsak).firstOrNull()?.enhetId
 
         val behandlingDVH = BehandlingDVH(funksjonellTid = ZonedDateTime.now(),
                                           tekniskTid = ZonedDateTime.now(),
                                           mottattDato = LocalDate.now(),
                                           registrertDato = LocalDate.now(), //motta
-                                          behandlingId = "behandlingId",
-                                          sakId = "sakId",
-                                          behandlingType = "behandlingType",
-                                          behandlingStatus = "behandlingStatus",
-                                          utenlandstilsnitt = "utenlandstilsnitt",
-                                          ansvarligEnhetKode = "kode",   //der hvor person hører hjemme
-                                          behandlendeEnhetKode = "kode", //der hvor saksbehandler hører hjemme
-                                          ansvarligEnhetType = "ansvarligEnhetType",
+                                          behandlingId = behandling.id.toString(),
+                                          sakId = behandling.fagsak.id.toString(),
+                                          behandlingType = behandling.type.name,
+                                          behandlingStatus = behandling.status.name,
+                                          utenlandstilsnitt = "NASJONAL", // TODO
+                                          ansvarligEnhetKode = ansvarligEnhetKode!!,
+                                          behandlendeEnhetKode = behandlendeEnhetsKode, //TODO Hva gjør vi hvis behandlendeEnhetsKode ikke finnes? //der hvor saksbehandler hører hjemme
+                                          ansvarligEnhetType = "NORG",
                                           behandlendeEnhetType = "NORG",
-                                          totrinnsbehandling = true,
-                                          avsender = "ba-sak",
-                                          versjon = 2,
+                                          totrinnsbehandling = behandling.opprinnelse == BehandlingOpprinnelse.MANUELL,
+                                          avsender = "familie-ba-sak",
+                                          versjon = 2, // TODO kan vurdere å bumpe den manuelt sammen med en test som feiler hvis vi glemmer det. Kan også undersøke om det går an å plukke ut noe fra manifestet
                                           vedtaksDato = LocalDate.now(),
                                           relatertBehandlingId = null,
                                           vedtakId = "vedtakId",
                                           resultat = "resultat",
                                           resultatBegrunnelse = "resultatBegrunnelse",
-                                          behandlingTypeBeskrivelse = "behandlingTypeBeskrivelse",
-                                          behandlingStatusBeskrivelse = "behandlingStatusBeskrivelse",
+                                          behandlingTypeBeskrivelse = behandling.type.visningsnavn,
+                                          behandlingStatusBeskrivelse = null,
                                           resultatBegrunnelseBeskrivelse = "resultatBegrunnelseBeskrivelse",
                                           utenlandstilsnittBeskrivelse = "relatertButenlandstilsnittBeskrivelseehandlingId",
                                           beslutter = "beslutter",
