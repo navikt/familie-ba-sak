ed2778c744d25175171863c3106fae24add14301
Nina Kjekstad <nina.kjekstad@nav.no>
2020-10-05 16:26:15 +0200

    Trekk ut metrikker i egen komponent

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/steg/FerdigstillBehandling.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/steg/FerdigstillBehandling.kt
@@ -1,56 +1,29 @@
 package no.nav.familie.ba.sak.behandling.steg
 
-import io.micrometer.core.instrument.Counter
-import io.micrometer.core.instrument.DistributionSummary
-import io.micrometer.core.instrument.Metrics
+import no.nav.familie.ba.sak.behandling.BehandlingMetrikker
 import no.nav.familie.ba.sak.behandling.BehandlingService
 import no.nav.familie.ba.sak.behandling.domene.Behandling
 import no.nav.familie.ba.sak.behandling.domene.BehandlingStatus
 import no.nav.familie.ba.sak.behandling.fagsak.FagsakService
 import no.nav.familie.ba.sak.behandling.fagsak.FagsakStatus
-import no.nav.familie.ba.sak.behandling.vedtak.VedtakService
-import no.nav.familie.ba.sak.behandling.vilkår.BehandlingResultatService
-import no.nav.familie.ba.sak.behandling.vilkår.BehandlingResultatType
-import no.nav.familie.ba.sak.behandling.vilkår.BehandlingresultatOgVilkårBegrunnelse
 import no.nav.familie.ba.sak.beregning.BeregningService
 import no.nav.familie.ba.sak.logg.LoggService
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.time.LocalDate.now
-import java.time.LocalDateTime
-import java.time.temporal.ChronoUnit
 
 @Service
 class FerdigstillBehandling(
         private val fagsakService: FagsakService,
         private val beregningService: BeregningService,
         private val behandlingService: BehandlingService,
-        private val behandlingResultatService: BehandlingResultatService,
-        private val vedtakService: VedtakService,
+        private val behandlingMetrikker: BehandlingMetrikker,
         private val loggService: LoggService
 ) : BehandlingSteg<String> {
-
-    private val antallBehandlingResultatTyper: Map<BehandlingResultatType, Counter> =
-            BehandlingResultatType.values().map {
-                it to Metrics.counter("behandling.resultat",
-                                      "type", it.name,
-                                      "beskrivelse", it.displayName)
-            }.toMap()
-
-    private val antallBrevBegrunnelser: Map<BehandlingresultatOgVilkårBegrunnelse, Counter> =
-            BehandlingresultatOgVilkårBegrunnelse.values().map {
-                it to Metrics.counter("brevbegrunnelse",
-                                      "type", it.name,
-                                      "beskrivelse", it.tittel)
-            }.toMap()
-
-    private val behandlingstid: DistributionSummary = Metrics.summary("behandling.tid")
 
     override fun utførStegOgAngiNeste(behandling: Behandling,
                                       data: String): StegType {
         LOG.info("Forsøker å ferdigstille behandling ${behandling.id}")
-
-        val behandlingResultatType = behandlingResultatService.hentBehandlingResultatTypeFraBehandling(behandling)
 
         if (behandling.status !== BehandlingStatus.IVERKSETTER_VEDTAK) {
             error("Prøver å ferdigstille behandling ${behandling.id}, men status er ${behandling.status}")
@@ -61,13 +34,7 @@ class FerdigstillBehandling(
 
         oppdaterFagsakStatus(behandling = behandling)
 
-        val dagerSidenOpprettet = ChronoUnit.DAYS.between(behandling.opprettetTidspunkt, LocalDateTime.now())
-        behandlingstid.record(dagerSidenOpprettet.toDouble())
-        antallBehandlingResultatTyper[behandlingResultatType]?.increment()
-
-        val vedtak = vedtakService.hentAktivForBehandling(behandlingId = behandling.id) ?: error("Finner ikke aktivt vedtak ved ferdigstilling av behandling ${behandling.id}")
-        vedtak.utbetalingBegrunnelser.mapNotNull { it.behandlingresultatOgVilkårBegrunnelse }
-                .forEach { brevbegrunelse: BehandlingresultatOgVilkårBegrunnelse -> antallBrevBegrunnelser[brevbegrunelse]?.increment() }
+        behandlingMetrikker.oppdaterBehandlingMetrikker(behandling)
 
         return hentNesteStegForNormalFlyt(behandling)
     }
