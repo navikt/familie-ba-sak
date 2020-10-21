94cc1474f908733e7f366478a1d8e18fdd2e0e39
Nina Kjekstad <nina.kjekstad@nav.no>
2020-10-21 19:34:47 +0200

    Legg til manglende felter for BaseEntitet

--- src/main/resources/db/migration/V95__legg_til_opplysningsplikt.sql
+++ src/main/resources/db/migration/V95__legg_til_opplysningsplikt.sql
@@ -4,8 +4,11 @@ CREATE TABLE OPPLYSNINGSPLIKT
     FK_BEHANDLING_ID        BIGINT REFERENCES behandling (id)   NOT NULL,
     OPPLYSNINGSPLIKT_STATUS TEXT,
     BEGRUNNELSE             TEXT,
-    OPPRETTET_AV            VARCHAR      DEFAULT 'VL'           NOT NULL,
-    OPPRETTET_TID           TIMESTAMP(3) DEFAULT localtimestamp NOT NULL
+    VERSJON                       BIGINT       DEFAULT 0              NOT NULL,
+    OPPRETTET_AV                  VARCHAR      DEFAULT 'VL'           NOT NULL,
+    OPPRETTET_TID                 TIMESTAMP(3) DEFAULT localtimestamp NOT NULL,
+    ENDRET_AV                     VARCHAR,
+    ENDRET_TID                    TIMESTAMP(3)
 );
 
 CREATE SEQUENCE OPPLYSNINGSPLIKT_SEQ INCREMENT BY 50 START WITH 1000000 NO CYCLE;
