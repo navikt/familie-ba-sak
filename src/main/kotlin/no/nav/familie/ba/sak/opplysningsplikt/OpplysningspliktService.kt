0f0cc3586281d07e0e903d7571a12490d493a7ce
Daphne Leebeek <daphne.leebeek@nav.no>
2020-10-23 14:45:41 +0200

    Legg til endepunkt for oppdatering av opplysningsplikt

--- src/main/kotlin/no/nav/familie/ba/sak/opplysningsplikt/OpplysningspliktService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/opplysningsplikt/OpplysningspliktService.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.ba.sak.opplysningsplikt
 
+import no.nav.familie.ba.sak.behandling.restDomene.RestOpplysningsplikt
 import org.springframework.stereotype.Service
 
 @Service
@@ -7,7 +8,6 @@ class OpplysningspliktService(
         private val opplysningspliktRepository: OpplysningspliktRepository
 ) {
 
-    fun hentOpplysningsplikt(behandlingId: Long): Opplysningsplikt? = opplysningspliktRepository.findByBehandlingId(behandlingId)
 
     fun lagreBlankOpplysningsplikt(behandlingId: Long) {
         val lagretOpplysningsplikt = opplysningspliktRepository.findByBehandlingId(behandlingId)
@@ -19,5 +19,13 @@ class OpplysningspliktService(
                 it.begrunnelse = null
             })
         }
+    }
+
+    fun oppdaterOpplysningsplikt(behandlingId: Long, restOpplysningsplikt: RestOpplysningsplikt) {
+        val lagretOpplysningsplikt = opplysningspliktRepository.findByBehandlingId(behandlingId) ?: error("Kunne ikke oppdatere opplysningsplikt fordi opplysningsplikt mangler på behandling")
+        opplysningspliktRepository.saveAndFlush(lagretOpplysningsplikt.also {
+            it.status = restOpplysningsplikt.status
+            it.begrunnelse = restOpplysningsplikt.begrunnelse
+        })
     }
 }
