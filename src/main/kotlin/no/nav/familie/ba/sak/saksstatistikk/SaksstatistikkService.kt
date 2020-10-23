7317fff6cd20232d7dbab3d7ebd5aab9b086be2b
tnarland <47184872+tnarland@users.noreply.github.com>
2020-10-23 10:43:12 +0200

    Fiks: Sende riktig resultatstruktur til datavarehus
    * Mapper resultat og resultatBegrunnelser fra behandlingResultat
    * Lar være å sende saksstatistikk for autovedtak med tilbakerulling

--- src/main/kotlin/no/nav/familie/ba/sak/saksstatistikk/SaksstatistikkService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/saksstatistikk/SaksstatistikkService.kt
@@ -4,10 +4,19 @@ import no.nav.familie.ba.sak.arbeidsfordeling.ArbeidsfordelingService
 import no.nav.familie.ba.sak.behandling.BehandlingService
 import no.nav.familie.ba.sak.behandling.domene.BehandlingÅrsak
 import no.nav.familie.ba.sak.behandling.fagsak.FagsakService
+import no.nav.familie.ba.sak.behandling.grunnlag.personopplysninger.PersonType
+import no.nav.familie.ba.sak.behandling.grunnlag.personopplysninger.PersongrunnlagService
 import no.nav.familie.ba.sak.behandling.vedtak.VedtakService
+import no.nav.familie.ba.sak.behandling.vilkår.BehandlingResultat
+import no.nav.familie.ba.sak.behandling.vilkår.BehandlingResultatService
+import no.nav.familie.ba.sak.behandling.vilkår.BehandlingResultatType.*
+import no.nav.familie.ba.sak.behandling.vilkår.Vilkår
+import no.nav.familie.ba.sak.common.Utils
 import no.nav.familie.ba.sak.common.Utils.hentPropertyFraMaven
+import no.nav.familie.ba.sak.config.FeatureToggleService
 import no.nav.familie.ba.sak.journalføring.JournalføringService
 import no.nav.familie.ba.sak.journalføring.domene.JournalføringRepository
+import no.nav.familie.ba.sak.nare.Resultat.*
 import no.nav.familie.ba.sak.pdl.PersonopplysningerService
 import no.nav.familie.ba.sak.sikkerhet.SikkerhetContext.SYSTEM_NAVN
 import no.nav.familie.ba.sak.totrinnskontroll.TotrinnskontrollService
@@ -23,18 +32,26 @@ import java.time.LocalDate
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
+
 @Service
 class SaksstatistikkService(private val behandlingService: BehandlingService,
+                            private val behandlingResultatService: BehandlingResultatService,
                             private val journalføringRepository: JournalføringRepository,
                             private val journalføringService: JournalføringService,
                             private val arbeidsfordelingService: ArbeidsfordelingService,
                             private val totrinnskontrollService: TotrinnskontrollService,
                             private val vedtakService: VedtakService,
                             private val fagsakService: FagsakService,
-                            private val personopplysningerService: PersonopplysningerService) {
+                            private val personopplysningerService: PersonopplysningerService,
+                            private val persongrunnlagService: PersongrunnlagService,
+                            private val featureToggleService: FeatureToggleService) {
 
-    fun mapTilBehandlingDVH(behandlingId: Long, forrigeBehandlingId: Long? = null): BehandlingDVH {
+    fun mapTilBehandlingDVH(behandlingId: Long, forrigeBehandlingId: Long? = null): BehandlingDVH? {
         val behandling = behandlingService.hent(behandlingId)
+
+        if (behandling.skalBehandlesAutomatisk && fødselshendelseSkalRullesTilbake()) return null
+
+        val behandlingResultat =  behandlingResultatService.hentAktivForBehandling(behandlingId)
 
         val datoMottatt = when (behandling.opprettetÅrsak) {
             BehandlingÅrsak.SØKNAD -> {
@@ -83,11 +100,9 @@ class SaksstatistikkService(private val behandlingService: BehandlingService,
                                           vedtaksDato = aktivtVedtak?.vedtaksdato,
                                           relatertBehandlingId = forrigeBehandlingId?.toString(),
                                           vedtakId = aktivtVedtak?.id?.toString(),
-                                          resultat = aktivtVedtak?.hentUtbetalingBegrunnelse(behandlingId)?.begrunnelseType?.name,
+                                          resultat = behandlingResultat?.samletResultat?.name,
                                           behandlingTypeBeskrivelse = behandling.type.visningsnavn,
-                                          resultatBegrunnelser = aktivtVedtak?.utbetalingBegrunnelser?.mapNotNull { it.vedtakBegrunnelse }
-                                                  ?.map { ResultatBegrunnelseDVH(it.name, it.tittel) }
-                                                  .orEmpty(),
+                                          resultatBegrunnelser = behandlingResultat?.samletResultatBegrunnelser() ?: emptyList(),
                                           behandlingOpprettetAv = behandling.opprettetAv,
                                           behandlingOpprettetType = "saksbehandlerId",
                                           behandlingOpprettetTypeBeskrivelse = "saksbehandlerId. VL ved automatisk behandling"
@@ -100,8 +115,12 @@ class SaksstatistikkService(private val behandlingService: BehandlingService,
         return behandlingDVH
     }
 
-    fun mapTilSakDvh(sakId: Long): SakDVH {
+    fun mapTilSakDvh(sakId: Long): SakDVH? {
         val fagsak = fagsakService.hentRestFagsak(sakId).getDataOrThrow()
+
+        //Skipper saker som har automatisk behandling
+        val skalBehandleAutomatisk = behandlingService.hentAktivForFagsak(fagsakId = fagsak.id)?.skalBehandlesAutomatisk ?: false
+        if (skalBehandleAutomatisk && fødselshendelseSkalRullesTilbake()) return null
 
         val søkersAktørId = personopplysningerService.hentAktivAktørId(Ident(fagsak.søkerFødselsnummer))
 
@@ -120,6 +139,53 @@ class SaksstatistikkService(private val behandlingService: BehandlingService,
                 versjon = hentPropertyFraMaven("familie.kontrakter.saksstatistikk") ?: "2",
         )
     }
+
+    private fun BehandlingResultat.samletResultatBegrunnelser(): List<ResultatBegrunnelseDVH> {
+        return when (samletResultat) {
+            IKKE_VURDERT -> emptyList()
+            AVSLÅTT -> finnÅrsakerTilAvslag()
+            DELVIS_INNVILGET -> TODO()
+            HENLAGT -> TODO()
+            OPPHØRT -> TODO()
+            INNVILGET -> listOf(ResultatBegrunnelseDVH("Alle vilkår er oppfylt",
+                                                       "Vilkår vurdert for søker: ${Vilkår.hentVilkårFor(PersonType.SØKER)}\n" +
+                                                       "Vilkår vurdert for barn: ${
+                                                           Vilkår.hentVilkårFor(PersonType.BARN).toMutableList().apply {
+                                                               if (behandling.skalBehandlesAutomatisk) this.remove(Vilkår.LOVLIG_OPPHOLD)
+                                                           }
+                                                       }"))
+        }
+    }
+
+    private fun BehandlingResultat.finnÅrsakerTilAvslag(): List<ResultatBegrunnelseDVH> {
+        val søker = persongrunnlagService.hentSøker(behandling)?.personIdent?.ident
+        val barna = persongrunnlagService.hentBarna(behandling).map { it.personIdent.ident }
+
+        val søkerResultatNei = personResultater.find { it.personIdent == søker }
+                ?.vilkårResultater?.filter { it.resultat == NEI }
+
+        if (!søkerResultatNei.isNullOrEmpty()) {
+            return søkerResultatNei.map { ResultatBegrunnelseDVH("${it.vilkårType.name} ikke oppfylt for søker") }
+        } else {
+
+            val negativeVilkårResultater = Vilkår.values().map { it to mutableListOf<String>() }.toMap()
+
+            personResultater.filter { barna.contains(it.personIdent) }
+                    .forEach { personResultat ->
+                        personResultat.vilkårResultater.filter { it.resultat == NEI }
+                                .forEach { vilkårResultatNei ->
+                                    negativeVilkårResultater[vilkårResultatNei.vilkårType]!!.add(personResultat.personIdent)
+                                }
+                    }
+
+            return negativeVilkårResultater.filterValues { it.isNotEmpty() }.map { (negativtVilkår, personer) ->
+                ResultatBegrunnelseDVH("${negativtVilkår.name} ikke oppfylt for barn ${Utils.slåSammen(personer)}")
+            }
+        }
+    }
+
+    private fun fødselshendelseSkalRullesTilbake() : Boolean =
+            featureToggleService.isEnabled("familie-ba-sak.rollback-automatisk-regelkjoring")
 
     companion object {
 
