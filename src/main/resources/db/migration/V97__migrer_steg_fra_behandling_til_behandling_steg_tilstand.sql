8e662dcee38e397622aee7887950e0904db5bd02
Marcus Gustafson <marcus.gustafson@computas.com>
2020-10-29 14:45:17 +0100

    Satt tilbake SQL-skript

--- src/main/resources/db/migration/V97__migrer_steg_fra_behandling_til_behandling_steg_tilstand.sql
+++ src/main/resources/db/migration/V97__migrer_steg_fra_behandling_til_behandling_steg_tilstand.sql
@@ -1,1 +1,12 @@
--- Tom med hensikt
+-- Det ser ut som at deploy til dev henger når man prøver å
+-- migrere data. Derfor kommenterer jeg ut denne delen.
+/*INSERT INTO
+    BEHANDLING_STEG_TILSTAND(fk_behandling_id, behandling_steg)
+    (
+        SELECT
+            id, steg
+        FROM
+            BEHANDLING
+        WHERE
+            steg is not null
+     );*/
