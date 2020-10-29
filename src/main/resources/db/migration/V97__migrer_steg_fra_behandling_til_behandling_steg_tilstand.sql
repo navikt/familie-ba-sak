6364d31c4c984abf74c6e5e21786e3b058d39517
Marcus Gustafson <marcus.gustafson@computas.com>
2020-10-29 12:36:47 +0100

    Kommenteret ut migreringskript etter som deploy henger

--- src/main/resources/db/migration/V97__migrer_steg_fra_behandling_til_behandling_steg_tilstand.sql
+++ src/main/resources/db/migration/V97__migrer_steg_fra_behandling_til_behandling_steg_tilstand.sql
@@ -1,4 +1,6 @@
-INSERT INTO
+-- Det ser ut som at deploy til dev henger når man prøver å
+-- migrere data. Derfor kommenterer jeg ut denne delen.
+/*INSERT INTO
     BEHANDLING_STEG_TILSTAND(fk_behandling_id, behandling_steg)
     (
         SELECT
@@ -7,4 +9,4 @@ INSERT INTO
             BEHANDLING
         WHERE
             steg is not null
-     );
+     );*/
