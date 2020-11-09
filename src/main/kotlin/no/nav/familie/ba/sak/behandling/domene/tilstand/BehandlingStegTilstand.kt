ba9da2ead6f733c7a3e59899b15c033997c90e94
Marcus Gustafson <marcus.gustafson@computas.com>
2020-11-09 10:25:29 +0100

    Endret steg realsjon på behandling til set fra list

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/domene/tilstand/BehandlingStegTilstand.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/domene/tilstand/BehandlingStegTilstand.kt
@@ -29,6 +29,20 @@ data class BehandlingStegTilstand(
         var behandlingStegStatus: BehandlingStegStatus = BehandlingStegStatus.IKKE_UTFØRT
 ) : BaseEntitet() {
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BehandlingStegTilstand
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
     override fun toString(): String {
         return "BehandlingStegTilstand(id=$id, behandling=${behandling.id}, behandlingSteg=$behandlingSteg, behandlingStegStatus=$behandlingStegStatus)"
     }
