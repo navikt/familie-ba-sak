a57ef25625c9a115aa7c372cf4fc87d8502060d2
Nina Kjekstad <nina.kjekstad@nav.no>
2020-11-10 14:40:41 +0100

    Legg til enhetstest

---
+++ src/test/kotlin/no/nav/familie/ba/sak/behandling/BehandlingTest.kt
@@ -0,0 +1,35 @@
+package no.nav.familie.ba.sak.behandling
+
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertTrue
+import no.nav.familie.ba.sak.behandling.domene.*
+import no.nav.familie.ba.sak.common.lagBehandling
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class BehandlingTest {
+
+    @Test
+    fun `erRentTekniskOpphør kastet feil hvis behandlingResultat og behandlingÅrsak ikke samsvarer ved teknisk opphør`() {
+        val behandling = lagBehandling(
+                behandlingType = BehandlingType.TEKNISK_OPPHØR,
+                årsak = BehandlingÅrsak.SØKNAD)
+        assertThrows<RuntimeException> { behandling.erTekniskOpphør() }
+    }
+
+    @Test
+    fun `erRentTekniskOpphør gir true når teknisk opphør`() {
+        val behandling = lagBehandling(
+                behandlingType = BehandlingType.TEKNISK_OPPHØR,
+                årsak = BehandlingÅrsak.TEKNISK_OPPHØR)
+        assertTrue(behandling.erTekniskOpphør())
+    }
+
+    @Test
+    fun `erRentTekniskOpphør gir false når ikke teknisk opphør`() {
+        val behandling = lagBehandling(
+                behandlingType = BehandlingType.REVURDERING,
+                årsak = BehandlingÅrsak.SØKNAD)
+        assertFalse(behandling.erTekniskOpphør())
+    }
+}
