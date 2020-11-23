c74f34e206d22e5e1c038a75b9ecf9907c6abf5d
Marcus Gustafson <marcus.gustafson@computas.com>
2020-11-23 10:00:05 +0100

    Endret navn på trukket søknad til soknad

--- src/main/kotlin/no/nav/familie/ba/sak/dokument/domene/MalMedData.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/dokument/domene/MalMedData.kt
@@ -9,7 +9,7 @@ enum class BrevType(val malId: String, val arkivType: String, val visningsTekst:
     INNHENTE_OPPLYSNINGER("innhente-opplysninger", "BARNETRYGD_INNHENTE_OPPLYSNINGER", "innhenting av opplysninger"),
     VARSEL_OM_REVURDERING("varsel-om-revurdering", "BARNETRYGD_VARSEL_OM_REVURDERING", "varsel om revurdering"),
     VEDTAK("vedtak", "BARNETRYGD_VEDTAK", "vedtak"),
-    HENLEGGE_TRUKKET_SØKNAD("henlegge-trukket-søknad", "BARNETRYGD_HENLEGGE_TRUKKET_SØKNAD", "henlegg trukket søknad");
+    HENLEGGE_TRUKKET_SØKNAD("henlegge-trukket-soknad", "BARNETRYGD_HENLEGGE_TRUKKET_SØKNAD", "henlegg trukket søknad");
 
     override fun toString(): String {
         return visningsTekst
