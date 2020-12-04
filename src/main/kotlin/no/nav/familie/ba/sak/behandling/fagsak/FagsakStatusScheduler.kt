2c26d72dc6c94bf1d1809d029a3009fd4b701a8b
Nina Kjekstad <nina.kjekstad@nav.no>
2020-12-04 13:15:12 +0100

    Fiks leaderelection ved oppdatering av fagsakstatuser den 1. i måneden

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/fagsak/FagsakStatusScheduler.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/fagsak/FagsakStatusScheduler.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.ba.sak.behandling.fagsak
 
+import no.nav.familie.leader.LeaderClient
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -14,11 +15,19 @@ class FagsakStatusScheduler(val fagsakService: FagsakService) {
 
     @Scheduled(cron = "0 0 7 1 * *")
     fun oppdaterFagsakStatuser() {
-        val antallOppdaterte = fagsakService.oppdaterLøpendeStatusPåFagsaker()
-        LOG.info("Oppdatert status på $antallOppdaterte fagsaker til ${FagsakStatus.AVSLUTTET.name}")
+        when (LeaderClient.isLeader()) {
+            true -> {
+                val antallOppdaterte = fagsakService.oppdaterLøpendeStatusPåFagsaker()
+                LOG.info("Oppdatert status på $antallOppdaterte fagsaker til ${FagsakStatus.AVSLUTTET.name}")
+            }
+            false -> {
+                LOG.info("Oppdatering av fagsakstatuser ikke kjørt på denne poden.")
+            }
+        }
     }
 
     companion object {
+
         val LOG = LoggerFactory.getLogger(FagsakStatusScheduler::class.java)
     }
 }
