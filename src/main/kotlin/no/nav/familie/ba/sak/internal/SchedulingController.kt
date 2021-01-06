a14b9586cb34e08740b31302beedc8617bad08c2
Øyvind Hagen <oyvind.hagen@nav.no>
2021-01-06 09:33:29 +0100

    Legger ti sjekk om appen kjører i prod for enabling av endepunkt.

--- src/main/kotlin/no/nav/familie/ba/sak/internal/SchedulingController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/internal/SchedulingController.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.ba.sak.internal
 
 import no.nav.familie.ba.sak.behandling.autobrev.Autobrev6og18ÅrScheduler
+import no.nav.familie.ba.sak.common.EnvService
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.context.annotation.Profile
@@ -12,12 +13,18 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/internal")
 @Profile(value = ["dev"])
-class SchedulingController(private val scheduler: Autobrev6og18ÅrScheduler) {
+class SchedulingController(
+        private val scheduler: Autobrev6og18ÅrScheduler,
+private val envService: EnvService) {
 
     @GetMapping(path = ["/autobrev"])
     @Unprotected
     fun kjørSchedulerForAutobrev(): ResponseEntity<Ressurs<String>> {
-        scheduler.opprettTask()
-        return ResponseEntity.ok(Ressurs.success("Laget task."))
+        return if (!envService.erProd()) {
+            scheduler.opprettTask()
+            ResponseEntity.ok(Ressurs.success("Laget task."))
+        } else {
+            ResponseEntity.ok(Ressurs.success("Endepunktet gjør ingenting i prod."))
+        }
     }
 }
