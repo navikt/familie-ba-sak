ca497ab62e8704d7dc2c832257db71d42cac4c19
Marcus Gustafson <marcus.gustafson@computas.com>
2021-01-08 08:51:36 +0100

    Utvidet test for Autobrev6og18ÅrService

--- src/test/kotlin/no/nav/familie/ba/sak/behandling/autobrev/Autobrev6og18ÅrServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/behandling/autobrev/Autobrev6og18ÅrServiceTest.kt
@@ -4,15 +4,18 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import no.nav.familie.ba.sak.behandling.BehandlingService
+import no.nav.familie.ba.sak.behandling.domene.Behandling
 import no.nav.familie.ba.sak.behandling.domene.BehandlingStatus
 import no.nav.familie.ba.sak.behandling.fagsak.FagsakStatus
 import no.nav.familie.ba.sak.behandling.grunnlag.personopplysninger.PersongrunnlagService
+import no.nav.familie.ba.sak.behandling.grunnlag.personopplysninger.PersonopplysningGrunnlag
 import no.nav.familie.ba.sak.behandling.grunnlag.personopplysninger.PersonopplysningGrunnlagRepository
 import no.nav.familie.ba.sak.behandling.steg.StegService
 import no.nav.familie.ba.sak.behandling.vedtak.VedtakService
 import no.nav.familie.ba.sak.common.*
 import no.nav.familie.ba.sak.task.dto.Autobrev6og18ÅrDTO
 import no.nav.familie.prosessering.domene.TaskRepository
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
@@ -33,20 +36,13 @@ internal class Autobrev6og18ÅrServiceTest {
                                                         persongrunnlagService = persongrunnlagService)
 
     @Test
-    fun `Verifiser at løpende fagsak med avsluttede behandlinger og barn på 18 oppretter en behandling for omregning`() {
+    fun `Verifiser at løpende fagsak med avsluttede behandlinger og barn på 18 ikke oppretter en behandling for omregning`() {
         val behandling = lagBehandling().also {
             it.fagsak.status = FagsakStatus.LØPENDE
             it.status = BehandlingStatus.AVSLUTTET
         }
 
-        val barn = tilfeldigPerson(fødselsdato = LocalDate.now().minusYears(Alder.atten.år.toLong()))
-        val søker = tilfeldigSøker()
-
-        val personopplysningGrunnlag = lagTestPersonopplysningGrunnlag(behandlingId = behandling.id,
-                                                                       søkerPersonIdent = søker.personIdent.ident,
-                                                                       barnasIdenter = listOf(barn.personIdent.ident),
-                                                                       barnFødselsdato = LocalDate.now()
-                                                                               .minusYears(Alder.atten.år.toLong()))
+        val personopplysningGrunnlag = lagTestPersonopplysningGrunnlag(alder = 18, behandling)
 
         every { behandlingService.hentAktivForFagsak(behandling.fagsak.id) } returns behandling
         every { behandlingService.opprettBehandling(any()) } returns behandling
@@ -59,8 +55,85 @@ internal class Autobrev6og18ÅrServiceTest {
 
         autobrev6og18ÅrService.opprettOmregningsoppgaveForBarnIBrytingsAlder(autobrev6og18ÅrDTO)
 
-        //verify(exactly = 1) { behandlingService.opprettBehandling(any()) }
+        verify(exactly = 0) { stegService.håndterVilkårsvurdering(any()) }
     }
 
-    //TODO: Opprett test for hver kode branche.
+    @Test
+    fun `Verifiser at behandling for omregning ikke opprettes om barn med angitt ålder ikke finnes`() {
+        val behandling = lagBehandling().also {
+            it.fagsak.status = FagsakStatus.LØPENDE
+            it.status = BehandlingStatus.AVSLUTTET
+        }
+
+        val personopplysningGrunnlag = lagTestPersonopplysningGrunnlag(alder = 7, behandling)
+
+        every { behandlingService.hentAktivForFagsak(behandling.fagsak.id) } returns behandling
+        every { behandlingService.opprettBehandling(any()) } returns behandling
+        every { personopplysningGrunnlagRepository.findByBehandlingAndAktiv(behandlingId = behandling.id) } returns personopplysningGrunnlag
+
+        val autobrev6og18ÅrDTO = Autobrev6og18ÅrDTO(fagsakId = behandling.fagsak.id,
+                                                    alder = Alder.seks.år,
+                                                    årMåned = inneværendeMåned())
+
+        autobrev6og18ÅrService.opprettOmregningsoppgaveForBarnIBrytingsAlder(autobrev6og18ÅrDTO)
+
+        verify(exactly = 0) { stegService.håndterVilkårsvurdering(any()) }
+    }
+
+    @Test
+    fun `Verifiser at behandling for omregning ikke opprettes om fagsak ikke er løpende`() {
+        val behandling = lagBehandling().also {
+            it.fagsak.status = FagsakStatus.OPPRETTET
+            it.status = BehandlingStatus.AVSLUTTET
+        }
+
+        val personopplysningGrunnlag = lagTestPersonopplysningGrunnlag(alder = 6, behandling)
+
+        every { behandlingService.hentAktivForFagsak(behandling.fagsak.id) } returns behandling
+        every { behandlingService.opprettBehandling(any()) } returns behandling
+        every { personopplysningGrunnlagRepository.findByBehandlingAndAktiv(behandlingId = behandling.id) } returns personopplysningGrunnlag
+
+        val autobrev6og18ÅrDTO = Autobrev6og18ÅrDTO(fagsakId = behandling.fagsak.id,
+                                                    alder = Alder.seks.år,
+                                                    årMåned = inneværendeMåned())
+
+        autobrev6og18ÅrService.opprettOmregningsoppgaveForBarnIBrytingsAlder(autobrev6og18ÅrDTO)
+
+        verify(exactly = 0) { stegService.håndterVilkårsvurdering(any()) }
+    }
+
+    @Test
+    fun `Verifiser at behandling for omregning kaster fei om behandling ikke er avsluttet`() {
+        val behandling = lagBehandling().also {
+            it.fagsak.status = FagsakStatus.LØPENDE
+            it.status = BehandlingStatus.OPPRETTET
+        }
+
+        val personopplysningGrunnlag = lagTestPersonopplysningGrunnlag(alder = 6, behandling)
+
+        every { behandlingService.hentAktivForFagsak(behandling.fagsak.id) } returns behandling
+        every { behandlingService.opprettBehandling(any()) } returns behandling
+        every { personopplysningGrunnlagRepository.findByBehandlingAndAktiv(behandlingId = behandling.id) } returns personopplysningGrunnlag
+
+        val autobrev6og18ÅrDTO = Autobrev6og18ÅrDTO(fagsakId = behandling.fagsak.id,
+                                                    alder = Alder.seks.år,
+                                                    årMåned = inneværendeMåned())
+
+        Assertions.assertThrows(IllegalStateException::class.java) {
+            autobrev6og18ÅrService.opprettOmregningsoppgaveForBarnIBrytingsAlder(autobrev6og18ÅrDTO)
+        }
+
+        verify(exactly = 0) { stegService.håndterVilkårsvurdering(any()) }
+    }
+
+    private fun lagTestPersonopplysningGrunnlag(alder: Long, behandling: Behandling): PersonopplysningGrunnlag {
+        val barn = tilfeldigPerson(fødselsdato = LocalDate.now().minusYears(alder))
+        val søker = tilfeldigSøker()
+
+        return lagTestPersonopplysningGrunnlag(behandlingId = behandling.id,
+                                                                       søkerPersonIdent = søker.personIdent.ident,
+                                                                       barnasIdenter = listOf(barn.personIdent.ident),
+                                                                       barnFødselsdato = LocalDate.now()
+                                                                               .minusYears(alder))
+    }
 }
