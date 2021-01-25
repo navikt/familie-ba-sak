8282bb548814854f1010114f9cc9a8514b8a8cef
Nina Kjekstad <nina.kjekstad@nav.no>
2021-01-25 13:54:59 +0100

    Håndter mottak av ressurs fra familie-brev!

--- src/main/kotlin/no/nav/familie/ba/sak/brev/BrevKlient.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/brev/BrevKlient.kt
@@ -1,6 +1,8 @@
 package no.nav.familie.ba.sak.brev
 
 import no.nav.familie.ba.sak.brev.domene.maler.Brev
+import no.nav.familie.ba.sak.common.assertGenerelleSuksessKriterier
+import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.log.NavHttpHeaders
 import no.nav.familie.log.mdc.MDCConstants
 import org.slf4j.LoggerFactory
@@ -10,6 +12,7 @@ import org.springframework.http.MediaType
 import org.springframework.http.RequestEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.exchange
 import java.net.URI
 
 @Component
@@ -17,20 +20,23 @@ class BrevKlient(
         @Value("\${FAMILIE_BREV_API_URL}") private val familieBrevUri: String,
         private val restTemplate: RestTemplate
 ) {
+
     fun genererBrev(målform: String, malnavn: String, body: Brev): ByteArray {
-        val url = URI.create("$familieBrevUri/api/ba-brev/dokument/${målform}/${malnavn}/pdf")
-        val request = RequestEntity.post(url)
+        val uri = URI.create("$familieBrevUri/api/ba-brev/dokument/${målform}/${malnavn}/pdf")
+        val request = RequestEntity.post(uri)
                 .contentType(MediaType.APPLICATION_JSON)
                 .acceptCharset(Charsets.UTF_8)
                 .header(NavHttpHeaders.NAV_CALL_ID.asString(), MDC.get(MDCConstants.MDC_CALL_ID))
                 .body(body)
 
-        secureLogger.info("Kaller familie brev($url) med data ${body.toBrevString()}")
-        val response = restTemplate.exchange(request, ByteArray::class.java)
-        return response.body ?: error("Klarte ikke generere brev med familie-brev")
+        secureLogger.info("Kaller familie brev($uri) med data ${body.toBrevString()}")
+        val response = restTemplate.exchange<Ressurs<ByteArray>>(request)
+        assertGenerelleSuksessKriterier(response.body)
+        return response.body?.data ?: error("Klarte ikke generere brev med familie-brev")
     }
 
     companion object {
+
         val secureLogger = LoggerFactory.getLogger("secureLogger")
     }
 }
