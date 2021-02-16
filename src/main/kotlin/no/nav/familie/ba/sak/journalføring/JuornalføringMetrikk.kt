cac421ef455b3ca0a78c646fa122636f99c9826b
Meng Zhu <meng.zhu@nav.no>
2021-02-16 12:35:56 +0100

    logs

--- src/main/kotlin/no/nav/familie/ba/sak/journalføring/JuornalføringMetrikk.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/journalføring/JuornalføringMetrikk.kt
@@ -6,6 +6,7 @@ import no.nav.familie.ba.sak.behandling.domene.Behandling
 import no.nav.familie.ba.sak.behandling.domene.BehandlingType
 import no.nav.familie.ba.sak.behandling.restDomene.RestJournalføring
 import no.nav.familie.kontrakter.felles.journalpost.Journalpost
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
@@ -40,17 +41,25 @@ class JuornalføringMetrikk {
     fun oppdaterJournalføringMetrikk(journalpost: Journalpost?, updatert: RestJournalføring, behandlinger: List<Behandling>) {
         if (updatert.knyttTilFagsak) {
             behandlinger.forEach {
+                LOG.info("Increase counter ${it.type} ${antallTilBehandling[it.type]}")
                 antallTilBehandling[it.type]?.increment()
             }
         } else {
+            LOG.info("Increase counter ${antallGenerellSak}")
             antallGenerellSak.increment()
         }
 
         val journalpostTittelLower = journalpost?.tittel?.toLowerCase()
         if (journalpostTittel.contains(journalpostTittelLower)) {
+            LOG.info("Increase counter ${journalpostTittelLower} ${antallJournalpostTittel[journalpostTittelLower]}")
             antallJournalpostTittel[journalpostTittelLower]?.increment()
         } else {
+            LOG.info("Increase counter ${antallJournalpostTittelFritekst}")
             antallJournalpostTittelFritekst.increment()
         }
+    }
+
+    companion object{
+        val LOG = LoggerFactory.getLogger(this::class.java)
     }
 }
