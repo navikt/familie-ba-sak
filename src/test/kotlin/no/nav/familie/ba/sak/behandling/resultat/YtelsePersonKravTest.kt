a5de97f01cb8388bbd4bfc7433c4edd21437244b
Nina Kjekstad <nina.kjekstad@nav.no>
2021-03-04 15:42:17 +0100

    Separer tester

---
+++ src/test/kotlin/no/nav/familie/ba/sak/behandling/resultat/YtelsePersonKravTest.kt
@@ -0,0 +1,131 @@
+package no.nav.familie.ba.sak.behandling.resultat
+
+import no.nav.familie.ba.sak.beregning.domene.YtelseType
+import no.nav.familie.ba.sak.common.inneværendeMåned
+import no.nav.familie.ba.sak.common.lagAndelTilkjentYtelse
+import no.nav.familie.ba.sak.common.lagSøknadDTO
+import no.nav.familie.ba.sak.common.tilfeldigPerson
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class YtelsePersonKravTest {
+
+    val søker = tilfeldigPerson()
+    val barn1 = tilfeldigPerson()
+    val barn2 = tilfeldigPerson()
+
+    @Test
+    fun `Skal kun finne søknadsytelsePersoner`() {
+        val søknadDTO = lagSøknadDTO(
+                søkerIdent = søker.personIdent.ident,
+                barnasIdenter = listOf(barn1.personIdent.ident)
+        )
+
+        val ytelsePersoner = YtelsePersonUtils.utledKrav(
+                søknadDTO = søknadDTO,
+                forrigeAndelerTilkjentYtelse = emptyList()
+        )
+
+        assertEquals(1, ytelsePersoner.size)
+        assertEquals(barn1.personIdent.ident, ytelsePersoner.first().personIdent)
+        assertEquals(YtelseType.ORDINÆR_BARNETRYGD, ytelsePersoner.first().ytelseType)
+        assertTrue(ytelsePersoner.first().erFramstiltKravForINåværendeBehandling())
+    }
+
+    @Test
+    fun `Skal kun finne endringsytelsePersoner`() {
+        val forrigeAndelBarn1 = lagAndelTilkjentYtelse(inneværendeMåned().minusYears(3).toString(),
+                                                       "2020-01",
+                                                       YtelseType.ORDINÆR_BARNETRYGD,
+                                                       1054,
+                                                       person = barn1)
+
+        val ytelsePersoner = YtelsePersonUtils.utledKrav(
+                søknadDTO = null,
+                forrigeAndelerTilkjentYtelse = listOf(forrigeAndelBarn1)
+        )
+
+        assertEquals(1, ytelsePersoner.size)
+        assertEquals(barn1.personIdent.ident, ytelsePersoner.first().personIdent)
+        assertEquals(YtelseType.ORDINÆR_BARNETRYGD, ytelsePersoner.first().ytelseType)
+        assertFalse(ytelsePersoner.first().erFramstiltKravForINåværendeBehandling())
+    }
+
+    @Test
+    fun `Skal finne 2 endringsytelsePersoner på samme barn`() {
+        val forrigeAndelBarn1Ordinær = lagAndelTilkjentYtelse(inneværendeMåned().minusYears(3).toString(),
+                                                              "2020-01",
+                                                              YtelseType.ORDINÆR_BARNETRYGD,
+                                                              1054,
+                                                              person = barn1)
+
+        val forrigeAndelBarn1Utvidet = lagAndelTilkjentYtelse(inneværendeMåned().minusYears(3).toString(),
+                                                              "2020-01",
+                                                              YtelseType.UTVIDET_BARNETRYGD,
+                                                              1054,
+                                                              person = barn1)
+
+        val ytelsePersoner = YtelsePersonUtils.utledKrav(
+                søknadDTO = null,
+                forrigeAndelerTilkjentYtelse = listOf(forrigeAndelBarn1Ordinær, forrigeAndelBarn1Utvidet)
+        )
+
+        assertEquals(2, ytelsePersoner.size)
+        assertTrue(ytelsePersoner.any { it.personIdent == barn1.personIdent.ident && it.ytelseType == YtelseType.ORDINÆR_BARNETRYGD && !it.erFramstiltKravForINåværendeBehandling() })
+        assertTrue(ytelsePersoner.any { it.personIdent == barn1.personIdent.ident && it.ytelseType == YtelseType.UTVIDET_BARNETRYGD && !it.erFramstiltKravForINåværendeBehandling() })
+    }
+
+    @Test
+    fun `Skal finne 1 av 2 endringsytelsePersoner og 1 søknadsytelsePersoner`() {
+        val søknadDTO = lagSøknadDTO(
+                søkerIdent = søker.personIdent.ident,
+                barnasIdenter = listOf(barn1.personIdent.ident)
+        )
+
+        val forrigeAndelBarn1Ordinær = lagAndelTilkjentYtelse(inneværendeMåned().minusYears(3).toString(),
+                                                              "2020-01",
+                                                              YtelseType.ORDINÆR_BARNETRYGD,
+                                                              1054,
+                                                              person = barn1)
+
+        val forrigeAndelBarn1Utvidet = lagAndelTilkjentYtelse(inneværendeMåned().minusYears(3).toString(),
+                                                              "2020-01",
+                                                              YtelseType.UTVIDET_BARNETRYGD,
+                                                              1054,
+                                                              person = barn1)
+
+        val ytelsePersoner = YtelsePersonUtils.utledKrav(
+                søknadDTO = søknadDTO,
+                forrigeAndelerTilkjentYtelse = listOf(forrigeAndelBarn1Ordinær, forrigeAndelBarn1Utvidet)
+        )
+
+        assertEquals(2, ytelsePersoner.size)
+        assertTrue(ytelsePersoner.any { it.personIdent == barn1.personIdent.ident && it.ytelseType == YtelseType.ORDINÆR_BARNETRYGD && it.erFramstiltKravForINåværendeBehandling() })
+        assertTrue(ytelsePersoner.any { it.personIdent == barn1.personIdent.ident && it.ytelseType == YtelseType.UTVIDET_BARNETRYGD && !it.erFramstiltKravForINåværendeBehandling() })
+    }
+
+    @Test
+    fun `Skal finne tidligere ytelseperson`() {
+        val søknadDTO = lagSøknadDTO(
+                søkerIdent = søker.personIdent.ident,
+                barnasIdenter = listOf(barn2.personIdent.ident)
+        )
+
+        val forrigeAndelBarn1= lagAndelTilkjentYtelse(inneværendeMåned().minusYears(3).toString(),
+                                                              "2020-01",
+                                                              YtelseType.ORDINÆR_BARNETRYGD,
+                                                              1054,
+                                                              person = barn1)
+
+        val ytelsePersoner = YtelsePersonUtils.utledKrav(
+                søknadDTO = søknadDTO,
+                forrigeAndelerTilkjentYtelse = listOf(forrigeAndelBarn1)
+        )
+
+        assertEquals(2, ytelsePersoner.size)
+        assertTrue(ytelsePersoner.any { it.personIdent == barn1.personIdent.ident && it.ytelseType == YtelseType.ORDINÆR_BARNETRYGD && it.kravOpprinnelse == KravOpprinnelse.TIDLIGERE })
+        assertTrue(ytelsePersoner.any { it.personIdent == barn2.personIdent.ident && it.ytelseType == YtelseType.ORDINÆR_BARNETRYGD && it.kravOpprinnelse == KravOpprinnelse.SØKNAD })
+    }
+}
--- src/test/kotlin/no/nav/familie/ba/sak/behandling/resultat/YtelsePersonResultatTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/behandling/resultat/YtelsePersonResultatTest.kt
@@ -11,104 +11,12 @@ import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
-class YtelsePersonUtilsTest {
+class YtelsePersonResultatTest {
 
     val søker = tilfeldigPerson()
     val barn1 = tilfeldigPerson()
     val barn2 = tilfeldigPerson()
 
-    @Test
-    fun `Skal kun finne søknadsytelsePersoner`() {
-        val søknadDTO = lagSøknadDTO(
-                søkerIdent = søker.personIdent.ident,
-                barnasIdenter = listOf(barn1.personIdent.ident)
-        )
-
-        val ytelsePersoner = YtelsePersonUtils.utledKrav(
-                søknadDTO = søknadDTO,
-                forrigeAndelerTilkjentYtelse = emptyList()
-        )
-
-        assertEquals(1, ytelsePersoner.size)
-        assertEquals(barn1.personIdent.ident, ytelsePersoner.first().personIdent)
-        assertEquals(YtelseType.ORDINÆR_BARNETRYGD, ytelsePersoner.first().ytelseType)
-        assertTrue(ytelsePersoner.first().erFramstiltKravForINåværendeBehandling())
-    }
-
-    @Test
-    fun `Skal kun finne endringsytelsePersoner`() {
-        val forrigeAndelBarn1 = lagAndelTilkjentYtelse(inneværendeMåned().minusYears(3).toString(),
-                                                       "2020-01",
-                                                       YtelseType.ORDINÆR_BARNETRYGD,
-                                                       1054,
-                                                       person = barn1)
-
-        val ytelsePersoner = YtelsePersonUtils.utledKrav(
-                søknadDTO = null,
-                forrigeAndelerTilkjentYtelse = listOf(forrigeAndelBarn1)
-        )
-
-        assertEquals(1, ytelsePersoner.size)
-        assertEquals(barn1.personIdent.ident, ytelsePersoner.first().personIdent)
-        assertEquals(YtelseType.ORDINÆR_BARNETRYGD, ytelsePersoner.first().ytelseType)
-        assertFalse(ytelsePersoner.first().erFramstiltKravForINåværendeBehandling())
-    }
-
-    @Test
-    fun `Skal finne 2 endringsytelsePersoner på samme barn`() {
-        val forrigeAndelBarn1Ordinær = lagAndelTilkjentYtelse(inneværendeMåned().minusYears(3).toString(),
-                                                              "2020-01",
-                                                              YtelseType.ORDINÆR_BARNETRYGD,
-                                                              1054,
-                                                              person = barn1)
-
-        val forrigeAndelBarn1Utvidet = lagAndelTilkjentYtelse(inneværendeMåned().minusYears(3).toString(),
-                                                              "2020-01",
-                                                              YtelseType.UTVIDET_BARNETRYGD,
-                                                              1054,
-                                                              person = barn1)
-
-        val ytelsePersoner = YtelsePersonUtils.utledKrav(
-                søknadDTO = null,
-                forrigeAndelerTilkjentYtelse = listOf(forrigeAndelBarn1Ordinær, forrigeAndelBarn1Utvidet)
-        )
-
-        assertEquals(2, ytelsePersoner.size)
-        assertTrue(ytelsePersoner.any { it.personIdent == barn1.personIdent.ident && it.ytelseType == YtelseType.ORDINÆR_BARNETRYGD && !it.erFramstiltKravForINåværendeBehandling() })
-        assertTrue(ytelsePersoner.any { it.personIdent == barn1.personIdent.ident && it.ytelseType == YtelseType.UTVIDET_BARNETRYGD && !it.erFramstiltKravForINåværendeBehandling() })
-    }
-
-    @Test
-    fun `Skal finne 1 av 2 endringsytelsePersoner og 1 søknadsytelsePersoner`() {
-        val søknadDTO = lagSøknadDTO(
-                søkerIdent = søker.personIdent.ident,
-                barnasIdenter = listOf(barn1.personIdent.ident)
-        )
-
-        val forrigeAndelBarn1Ordinær = lagAndelTilkjentYtelse(inneværendeMåned().minusYears(3).toString(),
-                                                              "2020-01",
-                                                              YtelseType.ORDINÆR_BARNETRYGD,
-                                                              1054,
-                                                              person = barn1)
-
-        val forrigeAndelBarn1Utvidet = lagAndelTilkjentYtelse(inneværendeMåned().minusYears(3).toString(),
-                                                              "2020-01",
-                                                              YtelseType.UTVIDET_BARNETRYGD,
-                                                              1054,
-                                                              person = barn1)
-
-        val ytelsePersoner = YtelsePersonUtils.utledKrav(
-                søknadDTO = søknadDTO,
-                forrigeAndelerTilkjentYtelse = listOf(forrigeAndelBarn1Ordinær, forrigeAndelBarn1Utvidet)
-        )
-
-        assertEquals(2, ytelsePersoner.size)
-        assertTrue(ytelsePersoner.any { it.personIdent == barn1.personIdent.ident && it.ytelseType == YtelseType.ORDINÆR_BARNETRYGD && it.erFramstiltKravForINåværendeBehandling() })
-        assertTrue(ytelsePersoner.any { it.personIdent == barn1.personIdent.ident && it.ytelseType == YtelseType.UTVIDET_BARNETRYGD && !it.erFramstiltKravForINåværendeBehandling() })
-    }
-
-
-    // Tester for ytelse person resultater
     @Test
     fun `Skal utelede innvilget første gang barnet blir vurdert`() {
         val andelBarn1 = lagAndelTilkjentYtelse(inneværendeMåned().minusYears(3).toString(),
@@ -724,5 +632,47 @@ class YtelsePersonUtilsTest {
 
         assertEquals(setOf(YtelsePersonResultat.OPPHØRT, YtelsePersonResultat.ENDRET),
                      ytelsePersonerMedResultat.find { it.personIdent == barn1.personIdent.ident }?.resultater)
+    }
+
+    @Test
+    fun `Skal sette opphørt på alle personer ved tidligere opphør`() {
+        val forrigeAndelBarn1 = lagAndelTilkjentYtelse(inneværendeMåned().minusYears(4).toString(),
+                                                       inneværendeMåned().minusYears(2).toString(),
+                                                       YtelseType.ORDINÆR_BARNETRYGD,
+                                                       1054,
+                                                       person = barn1)
+
+
+        val andelBarn2 = lagAndelTilkjentYtelse(inneværendeMåned().minusYears(3).toString(),
+                                                inneværendeMåned().minusYears(2).toString(),
+                                                YtelseType.ORDINÆR_BARNETRYGD,
+                                                1054,
+                                                person = barn2)
+
+        val ytelsePersoner = listOf(
+                YtelsePerson(
+                        personIdent = barn1.personIdent.ident,
+                        ytelseType = YtelseType.ORDINÆR_BARNETRYGD,
+                        kravOpprinnelse = KravOpprinnelse.TIDLIGERE,
+                ),
+
+                YtelsePerson(
+                        personIdent = barn1.personIdent.ident,
+                        ytelseType = YtelseType.ORDINÆR_BARNETRYGD,
+                        kravOpprinnelse = KravOpprinnelse.SØKNAD,
+                ),
+        )
+
+        val ytelsePersonerMedResultat = YtelsePersonUtils.populerYtelsePersonerMedResultat(ytelsePersoner = ytelsePersoner,
+                                                                                           forrigeAndelerTilkjentYtelse = listOf(
+                                                                                                   forrigeAndelBarn1),
+                                                                                           andelerTilkjentYtelse = listOf(
+                                                                                                   forrigeAndelBarn1, andelBarn2)
+        )
+
+        assertEquals(setOf(YtelsePersonResultat.OPPHØRT),
+                     ytelsePersonerMedResultat.find { it.personIdent == barn1.personIdent.ident }?.resultater)
+        assertEquals(setOf(YtelsePersonResultat.OPPHØRT, YtelsePersonResultat.INNVILGET),
+                     ytelsePersonerMedResultat.find { it.personIdent == barn2.personIdent.ident }?.resultater)
     }
 }
