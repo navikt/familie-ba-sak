c6f27993639d6e28fef29f529bdd874614267576
Nina Kjekstad <nina.kjekstad@nav.no>
2021-03-10 13:40:15 +0100

    Legg til begrunnelser for avslag

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/vilkår/VedtakBegrunnelseUtils.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/vilkår/VedtakBegrunnelseUtils.kt
@@ -5,7 +5,10 @@ import no.nav.familie.ba.sak.behandling.vilkår.Vilkår.*
 object VedtakBegrunnelseUtils {
 
     val vilkårMedVedtakBegrunnelser = mapOf<Vilkår, List<VedtakBegrunnelseSpesifikasjon>>(
-            UNDER_18_ÅR to listOf(VedtakBegrunnelseSpesifikasjon.REDUKSJON_UNDER_18_ÅR),
+            UNDER_18_ÅR to listOf(
+                    VedtakBegrunnelseSpesifikasjon.REDUKSJON_UNDER_18_ÅR,
+                    VedtakBegrunnelseSpesifikasjon.AVSLAG_UNDER_18_ÅR,
+            ),
             BOR_MED_SØKER to listOf(
                     VedtakBegrunnelseSpesifikasjon.INNVILGET_OMSORG_FOR_BARN,
                     VedtakBegrunnelseSpesifikasjon.INNVILGET_BOR_HOS_SØKER,
@@ -21,14 +24,18 @@ object VedtakBegrunnelseUtils {
                     VedtakBegrunnelseSpesifikasjon.OPPHØR_SØKER_FLYTTET_FRA_BARN,
                     VedtakBegrunnelseSpesifikasjon.OPPHØR_SØKER_HAR_IKKE_FAST_OMSORG,
                     VedtakBegrunnelseSpesifikasjon.OPPHØR_DELT_BOSTED_OPPHØRT_ENIGHET,
-                    VedtakBegrunnelseSpesifikasjon.OPPHØR_DELT_BOSTED_OPPHØRT_UENIGHET
+                    VedtakBegrunnelseSpesifikasjon.OPPHØR_DELT_BOSTED_OPPHØRT_UENIGHET,
+                    VedtakBegrunnelseSpesifikasjon.AVSLAG_BOR_HOS_SØKER,
+                    VedtakBegrunnelseSpesifikasjon.AVSLAG_OMSORG_FOR_BARN,
+                    VedtakBegrunnelseSpesifikasjon.AVSLAG_MEDLEM_I_FOLKETRYGDEN,
             ),
             BOSATT_I_RIKET to listOf(
                     VedtakBegrunnelseSpesifikasjon.INNVILGET_BOSATT_I_RIKTET,
                     VedtakBegrunnelseSpesifikasjon.INNVILGET_MEDLEM_I_FOLKETRYGDEN,
                     VedtakBegrunnelseSpesifikasjon.REDUKSJON_BOSATT_I_RIKTET,
                     VedtakBegrunnelseSpesifikasjon.OPPHØR_BARN_UTVANDRET,
-                    VedtakBegrunnelseSpesifikasjon.OPPHØR_SØKER_UTVANDRET
+                    VedtakBegrunnelseSpesifikasjon.OPPHØR_SØKER_UTVANDRET,
+                    VedtakBegrunnelseSpesifikasjon.AVSLAG_BOSATT_I_RIKET,
             ),
             LOVLIG_OPPHOLD to listOf(
                     VedtakBegrunnelseSpesifikasjon.INNVILGET_LOVLIG_OPPHOLD_OPPHOLDSTILLATELSE,
@@ -37,7 +44,11 @@ object VedtakBegrunnelseUtils {
                     VedtakBegrunnelseSpesifikasjon.INNVILGET_LOVLIG_OPPHOLD_SKJØNNSMESSIG_VURDERING_TREDJELANDSBORGER,
                     VedtakBegrunnelseSpesifikasjon.REDUKSJON_LOVLIG_OPPHOLD_OPPHOLDSTILLATELSE_BARN,
                     VedtakBegrunnelseSpesifikasjon.OPPHØR_BARN_HAR_IKKE_OPPHOLDSTILLATELSE,
-                    VedtakBegrunnelseSpesifikasjon.OPPHØR_SØKER_HAR_IKKE_OPPHOLDSTILLATELSE)
+                    VedtakBegrunnelseSpesifikasjon.OPPHØR_SØKER_HAR_IKKE_OPPHOLDSTILLATELSE,
+                    VedtakBegrunnelseSpesifikasjon.AVSLAG_LOVLIG_OPPHOLD_TREDJELANDSBORGER,
+                    VedtakBegrunnelseSpesifikasjon.AVSLAG_LOVLIG_OPPHOLD_EØS_BORGER,
+                    VedtakBegrunnelseSpesifikasjon.AVSLAG_LOVLIG_OPPHOLD_SKJØNNSMESSIG_VURDERING_TREDJELANDSBORGER,
+            )
     )
 
     val vedtakBegrunnelserIkkeTilknyttetVilkår = listOf(VedtakBegrunnelseSpesifikasjon.REDUKSJON_UNDER_6_ÅR,
