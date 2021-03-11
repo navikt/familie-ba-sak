97d73c2d628c4ea6042b0904f5769a4be672e913
mundal <halvorbmundal@gmail.com>
2021-03-11 15:02:17 +0100

    Opprett repositories for simuleringsobjektene

--- src/main/kotlin/no/nav/familie/ba/sak/simulering/domene/VedtakSimuleringPosteringRepository.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/simulering/domene/VedtakSimuleringPosteringRepository.kt
@@ -1,6 +1,15 @@
 package no.nav.familie.ba.sak.simulering.domene
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.transaction.annotation.Transactional
 
 interface VedtakSimuleringPosteringRepository : JpaRepository<VedtakSimuleringPostering, Long> {
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM VedtakSimuleringPostering vsp where vsp.vedtakSimuleringMottaker.id = :vedtakSimuleringMottakerId")
+    fun deleteByVedtakSimuleringMottakerId(vedtakSimuleringMottakerId: Long)
+
 }
