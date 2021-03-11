6a8ec070694d0ae1bc5683058becf4ebff9a60f9
mundal <halvorbmundal@gmail.com>
2021-03-11 15:22:36 +0100

    Fiks stack overflow når objektet oversettes til JSON

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/domene/tilstand/BehandlingStegTilstand.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/domene/tilstand/BehandlingStegTilstand.kt
@@ -1,12 +1,26 @@
 package no.nav.familie.ba.sak.behandling.domene.tilstand
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo
+import com.fasterxml.jackson.annotation.JsonIdentityReference
+import com.fasterxml.jackson.annotation.ObjectIdGenerators
 import no.nav.familie.ba.sak.behandling.domene.Behandling
 import no.nav.familie.ba.sak.behandling.steg.BehandlingStegStatus
 import no.nav.familie.ba.sak.behandling.steg.StegType
 import no.nav.familie.ba.sak.common.BaseEntitet
 import no.nav.familie.ba.sak.sikkerhet.RollestyringMotDatabase
 import java.util.*
-import javax.persistence.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.EntityListeners
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.SequenceGenerator
+import javax.persistence.Table
 
 @EntityListeners(RollestyringMotDatabase::class)
 @Entity(name = "BehandlingStegTilstand")
@@ -21,6 +35,8 @@ data class BehandlingStegTilstand(
 
         @ManyToOne(optional = false)
         @JoinColumn(name = "fk_behandling_id", nullable = false, updatable = false)
+        @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator::class, property = "id")
+        @JsonIdentityReference(alwaysAsId = true)
         val behandling: Behandling,
 
         @Enumerated(EnumType.STRING)
