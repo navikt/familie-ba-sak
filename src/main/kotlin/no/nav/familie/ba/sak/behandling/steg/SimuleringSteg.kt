fe815dceaabe99d61d6de68c97f5898042531d53
mundal <halvorbmundal@gmail.com>
2021-03-15 09:44:54 +0100

    Fjern ubrukte ressurser

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/steg/SimuleringSteg.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/steg/SimuleringSteg.kt
@@ -1,15 +1,11 @@
 package no.nav.familie.ba.sak.behandling.steg
 
 import no.nav.familie.ba.sak.behandling.domene.Behandling
-import no.nav.familie.ba.sak.behandling.vedtak.VedtakService
-import no.nav.familie.ba.sak.simulering.SimuleringService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class SimuleringSteg(
-        private val simuleringService: SimuleringService,
-        private val vedtakService: VedtakService
 ) : BehandlingSteg<String> {
 
     @Transactional
