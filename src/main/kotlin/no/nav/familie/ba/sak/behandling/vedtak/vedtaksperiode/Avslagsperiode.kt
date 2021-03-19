b233cf46c2db323ea9ead55d33119d918f838f74
Nina Kjekstad <nina.kjekstad@nav.no>
2021-03-19 09:23:05 +0100

    Filtrer avslagsperioder ved mapping

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/vedtak/vedtaksperiode/Avslagsperiode.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/vedtak/vedtaksperiode/Avslagsperiode.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ba.sak.behandling.vedtak.vedtaksperiode
 
 import no.nav.familie.ba.sak.behandling.vedtak.VedtakBegrunnelse
 import no.nav.familie.ba.sak.behandling.vedtak.grupperPåPeriode
+import no.nav.familie.ba.sak.behandling.vilkår.VedtakBegrunnelseType
 import java.time.LocalDate
 
 data class Avslagsperiode(
@@ -12,6 +13,7 @@ data class Avslagsperiode(
 
 fun mapTilAvslagsperioder(vedtakBegrunnelser: List<VedtakBegrunnelse>): List<Avslagsperiode> =
         vedtakBegrunnelser
+                .filter { it.begrunnelse.vedtakBegrunnelseType == VedtakBegrunnelseType.AVSLAG }
                 .grupperPåPeriode()
                 .map {
                     Avslagsperiode(periodeFom = it.key.fom,
