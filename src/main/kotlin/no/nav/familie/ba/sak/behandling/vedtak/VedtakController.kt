591e7ecb2470bbd76d1656554453d0959fe1d22a
Nina Kjekstad <ninakjek@users.noreply.github.com>
2021-03-22 12:09:25 +0100

    Legg til vedtaksperioder av typen avslag og tilby sammenslåtte avslagsbegrunnelser (#889)
    * Legg til mapping av vedtaksperioder
    * Slår sammen avslagbegrunnelsetekster som har samme fom, tom og begrunnelsetekst. F.eks: Dersom saksbehandler har avslått "bosatt i riket" på forelder og barn for samme periode og valgt samme begrunnelse på de, skal dette bli én begrunnelse "Du og barn født 01.01.99 ikke er bosatt i Norge fra januar 2000 til januar 2010.". Se tester.

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/vedtak/VedtakController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/vedtak/VedtakController.kt
@@ -2,11 +2,13 @@ package no.nav.familie.ba.sak.behandling.vedtak
 
 import no.nav.familie.ba.sak.behandling.BehandlingService
 import no.nav.familie.ba.sak.behandling.fagsak.FagsakService
+import no.nav.familie.ba.sak.behandling.restDomene.RestAvslagBegrunnelser
 import no.nav.familie.ba.sak.behandling.restDomene.RestDeleteVedtakBegrunnelser
 import no.nav.familie.ba.sak.behandling.restDomene.RestFagsak
 import no.nav.familie.ba.sak.behandling.restDomene.RestPostVedtakBegrunnelse
 import no.nav.familie.ba.sak.behandling.steg.BehandlerRolle
 import no.nav.familie.ba.sak.behandling.steg.StegService
+import no.nav.familie.ba.sak.behandling.vedtak.vedtaksperiode.Avslagsperiode
 import no.nav.familie.ba.sak.common.Periode
 import no.nav.familie.ba.sak.common.RessursUtils.notFound
 import no.nav.familie.ba.sak.sikkerhet.TilgangService
@@ -16,13 +18,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/fagsaker")
@@ -89,7 +85,6 @@ class VedtakController(
         return ResponseEntity.ok(fagsakService.hentRestFagsak(fagsakId))
     }
 
-
     @PostMapping(path = ["/{fagsakId}/send-til-beslutter"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun sendBehandlingTilBeslutter(@PathVariable fagsakId: Long,
                                    @RequestParam behandlendeEnhet: String): ResponseEntity<Ressurs<RestFagsak>> {
@@ -109,7 +104,6 @@ class VedtakController(
         stegService.håndterBeslutningForVedtak(behandling, restBeslutningPåVedtak)
         return ResponseEntity.ok(fagsakService.hentRestFagsak(fagsakId))
     }
-
 
     companion object {
 
