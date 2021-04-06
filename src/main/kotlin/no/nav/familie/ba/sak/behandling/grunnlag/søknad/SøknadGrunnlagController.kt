67cdd4ca50a5cd558d7816df85410c39cae36926
Henning Håkonsen <henning.hakonsen@nav.no>
2021-04-06 08:44:43 +0200

    Legger tilbake endepunkt for at gammel frontend fortsatt skal fungere. Deprecater endepunkt.

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/grunnlag/søknad/SøknadGrunnlagController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/grunnlag/søknad/SøknadGrunnlagController.kt
@@ -4,7 +4,10 @@ import no.nav.familie.ba.sak.behandling.BehandlingService
 import no.nav.familie.ba.sak.behandling.fagsak.FagsakService
 import no.nav.familie.ba.sak.behandling.restDomene.RestFagsak
 import no.nav.familie.ba.sak.behandling.restDomene.RestRegistrerSøknad
+import no.nav.familie.ba.sak.behandling.restDomene.SøknadDTO
 import no.nav.familie.ba.sak.behandling.steg.StegService
+import no.nav.familie.ba.sak.common.Feil
+import no.nav.familie.ba.sak.common.RessursUtils.illegalState
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.MediaType
@@ -17,7 +20,8 @@ import org.springframework.web.bind.annotation.*
 class SøknadGrunnlagController(
         private val fagsakService: FagsakService,
         private val behandlingService: BehandlingService,
-        private val stegService: StegService
+        private val stegService: StegService,
+        private val søknadGrunnlagService: SøknadGrunnlagService
 ) {
 
     @PostMapping(path = ["/{behandlingId}/registrere-søknad-og-hent-persongrunnlag"],
@@ -34,6 +38,24 @@ class SøknadGrunnlagController(
                         onSuccess = { ResponseEntity.ok(fagsakService.hentRestFagsak(behandling.fagsak.id)) },
                         onFailure = {
                             throw it
+                        }
+                )
+    }
+
+    @GetMapping(path = ["/{behandlingId}/søknad"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @Deprecated("Søknaden ligger nå på behandlingen og dette endepunktet skal ikke brukes")
+    fun hentSøknadV3(@PathVariable behandlingId: Long): ResponseEntity<Ressurs<SøknadDTO>> {
+        return Result.runCatching { søknadGrunnlagService.hentAktiv(behandlingId) }
+                .fold(
+                        onSuccess = {
+                            when (it) {
+                                null -> throw Feil(message = "Fant ikke søknadsgrunnlag på behandling",
+                                                   frontendFeilmelding = "Klarte ikke å hente søknadsgrunnlag på behandling")
+                                else -> ResponseEntity.ok(Ressurs.success(it.hentSøknadDto()))
+                            }
+                        },
+                        onFailure = {
+                            return illegalState((it.cause?.message ?: it.message).toString(), it)
                         }
                 )
     }
