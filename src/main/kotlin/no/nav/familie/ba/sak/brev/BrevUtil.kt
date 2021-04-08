5d6443b4ebcc1e9864b65cc802ce3c0d477b1be8
mundal <halvorbmundal@gmail.com>
2021-04-08 09:20:46 +0200

    Legg til automatisk vedtak 6 og 18 år

--- src/main/kotlin/no/nav/familie/ba/sak/brev/BrevUtil.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/brev/BrevUtil.kt
@@ -4,6 +4,7 @@ import no.nav.familie.ba.sak.behandling.domene.Behandling
 import no.nav.familie.ba.sak.behandling.domene.BehandlingResultat
 import no.nav.familie.ba.sak.behandling.domene.BehandlingResultat.*
 import no.nav.familie.ba.sak.behandling.domene.BehandlingType
+import no.nav.familie.ba.sak.behandling.domene.BehandlingÅrsak
 import no.nav.familie.ba.sak.behandling.steg.StegType
 import no.nav.familie.ba.sak.brev.domene.maler.Vedtaksbrevtype
 import no.nav.familie.ba.sak.common.Feil
@@ -17,11 +18,19 @@ fun hentVedtaksbrevtype(behandling: Behandling): Vedtaksbrevtype {
     }
 
     return if (behandling.skalBehandlesAutomatisk)
-        throw Feil("Det er ikke laget funksjonalitet for automatisk behandling med ny brevløsning.")
+        hentAutomatiskVedtaksbrevtype(behandling.opprettetÅrsak)
+
     else {
         hentManuellVedtaksbrevtype(behandling.type, behandling.resultat)
     }
 }
+
+private fun hentAutomatiskVedtaksbrevtype(behandlingÅrsak: BehandlingÅrsak) =
+        when (behandlingÅrsak) {
+            BehandlingÅrsak.OMREGNING_6ÅR -> Vedtaksbrevtype.AUTOVEDTAK_BARN6_ÅR
+            BehandlingÅrsak.OMREGNING_18ÅR -> Vedtaksbrevtype.AUTOVEDTAK_BARN18_ÅR
+            else -> throw Feil("Det er ikke laget funksjonalitet for automatisk behandling for ${behandlingÅrsak}")
+        }
 
 fun hentManuellVedtaksbrevtype(behandlingType: BehandlingType,
                                behandlingResultat: BehandlingResultat): Vedtaksbrevtype {
