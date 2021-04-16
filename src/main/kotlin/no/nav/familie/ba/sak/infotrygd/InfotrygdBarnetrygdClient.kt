33a39e11f3639ec0398c0f45c87242d39141aa16
Geirmund Simonsen <geirmund.simonsen@nav.no>
2021-04-16 11:11:39 +0200

    Legg til endepunkter i infotrygdklienten: - for åpen sak - for løpende barnetrygd (erstatter tidligere endepunkt)
    Eksponer løpende barnetrygd i ba-saks api for bruk i frontend.

--- src/main/kotlin/no/nav/familie/ba/sak/infotrygd/InfotrygdBarnetrygdClient.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/infotrygd/InfotrygdBarnetrygdClient.kt
@@ -28,12 +28,29 @@ class InfotrygdBarnetrygdClient(@Value("\${FAMILIE_BA_INFOTRYGD_BARNETRYGD_API_U
             return false
         }
 
-        val uri = URI.create("$clientUri/infotrygd/barnetrygd/lopendeSak")
+        val uri = URI.create("$clientUri/infotrygd/barnetrygd/lopende-barnetrygd")
 
         val request = InfotrygdSøkRequest(søkersIdenter, barnasIdenter)
 
         return try {
-            postForEntity<InfotrygdTreffResponse>(uri, request).ingenTreff.not()
+            postForEntity<InfotrygdLøpendeBarnetrygdResponse>(uri, request).harLøpendeBarnetrygd
+        } catch (ex: Exception) {
+            loggFeil(ex, uri)
+            throw ex
+        }
+    }
+
+    fun harÅpenSakIInfotrygd(søkersIdenter: List<String>, barnasIdenter: List<String> = emptyList()): Boolean {
+        if (environment.activeProfiles.contains("e2e")) {
+            return false
+        }
+
+        val uri = URI.create("$clientUri/infotrygd/barnetrygd/aapen-sak")
+
+        val request = InfotrygdSøkRequest(søkersIdenter, barnasIdenter)
+
+        return try {
+            postForEntity<InfotrygdÅpenSakResponse>(uri, request).harÅpenSak
         } catch (ex: Exception) {
             loggFeil(ex, uri)
             throw ex
--- src/main/kotlin/no/nav/familie/ba/sak/infotrygd/InfotrygdController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/infotrygd/InfotrygdController.kt
@@ -16,7 +16,8 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/infotrygd")
 @ProtectedWithClaims(issuer = "azuread")
 @Validated
-class InfotrygdController(private val infotrygdService: InfotrygdService) {
+class InfotrygdController(private val infotrygdBarnetrygdClient: InfotrygdBarnetrygdClient,
+                          private val infotrygdService: InfotrygdService) {
 
     @PostMapping(path = ["/hent-infotrygdsaker-for-soker"])
     fun hentInfotrygdsakerForSøker(@RequestBody personIdent: Personident): ResponseEntity<Ressurs<RestInfotrygdsaker>> {
@@ -33,6 +34,12 @@ class InfotrygdController(private val infotrygdService: InfotrygdService) {
 
         return ResponseEntity.ok(Ressurs.success(infotrygdstønader))
     }
+
+    @PostMapping(path = ["/har-lopende-sak"])
+    fun harLøpendeSak(@RequestBody personIdent: Personident): ResponseEntity<Ressurs<RestLøpendeSak>> {
+        val harLøpendeSak = infotrygdBarnetrygdClient.harLøpendeSakIInfotrygd(listOf(personIdent.ident))
+        return ResponseEntity.ok(Ressurs.success(RestLøpendeSak(harLøpendeSak)))
+    }
 }
 
 class Personident(val ident: String)
@@ -44,3 +51,5 @@ class RestInfotrygdsaker(val saker: List<Sak> = emptyList(),
 class RestInfotrygdstønader(val stønader: List<Stønad> = emptyList(),
                             val adressebeskyttelsegradering: ADRESSEBESKYTTELSEGRADERING? = null,
                             val harTilgang: Boolean = true)
+
+class RestLøpendeSak(val harLøpendeSak: Boolean)
