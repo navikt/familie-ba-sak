9b79b36734665265480d30215cbf60dfbed0d004
mundal <halvorbmundal@gmail.com>
2021-04-21 13:57:08 +0200

    Legg til findByVedtakId

--- src/main/kotlin/no/nav/familie/ba/sak/simulering/tilbakekreving/TilbakekrevingRepository.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/simulering/tilbakekreving/TilbakekrevingRepository.kt
@@ -1,5 +1,10 @@
 package no.nav.familie.ba.sak.simulering.tilbakekreving
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
-interface TilbakekrevingRepository : JpaRepository<Tilbakekreving, Long>
+interface TilbakekrevingRepository : JpaRepository<Tilbakekreving, Long> {
+
+    @Query(value = "SELECT t FROM Tilbakekreving t JOIN t.vedtak v WHERE v.id = :vedtakId")
+    fun findByVedtakId(vedtakId: Long): Tilbakekreving?
+}
