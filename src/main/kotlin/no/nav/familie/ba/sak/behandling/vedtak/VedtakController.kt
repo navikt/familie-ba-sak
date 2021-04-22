312e8f6d2a008f2202c047a18748e87c377fce4e
mundal <halvorbmundal@gmail.com>
2021-04-22 09:00:22 +0200

    Gjør lagreTilbakekrevingOgGåVidereTilNesteSteg transactional

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/vedtak/VedtakController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/vedtak/VedtakController.kt
@@ -1,15 +1,8 @@
 package no.nav.familie.ba.sak.behandling.vedtak
 
-import no.nav.familie.ba.sak.behandling.BehandlingService
 import no.nav.familie.ba.sak.behandling.fagsak.FagsakService
-import no.nav.familie.ba.sak.behandling.restDomene.RestDeleteVedtakBegrunnelser
 import no.nav.familie.ba.sak.behandling.restDomene.RestFagsak
-import no.nav.familie.ba.sak.behandling.restDomene.RestPostFritekstVedtakBegrunnelser
-import no.nav.familie.ba.sak.behandling.restDomene.RestPostVedtakBegrunnelse
-import no.nav.familie.ba.sak.behandling.steg.BehandlerRolle
 import no.nav.familie.ba.sak.behandling.steg.StegService
-import no.nav.familie.ba.sak.common.RessursUtils.notFound
-import no.nav.familie.ba.sak.sikkerhet.TilgangService
 import no.nav.familie.ba.sak.simulering.SimuleringService
 import no.nav.familie.ba.sak.simulering.domene.RestVedtakSimulering
 import no.nav.familie.ba.sak.simulering.tilbakekreving.RestTilbakekreving
@@ -18,17 +11,15 @@ import no.nav.familie.ba.sak.simulering.vedtakSimuleringMottakereTilRestSimuleri
 import no.nav.familie.ba.sak.validering.VedtaktilgangConstraint
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.transaction.Transactional
 
 @RestController
 @RequestMapping("/api/vedtak")
@@ -50,6 +41,7 @@ class VedtakController(
         return ResponseEntity.ok(Ressurs.success(restSimulering))
     }
 
+    @Transactional
     @PostMapping(path = ["/{vedtakId}/tilbakekreving"])
     fun lagreTilbakekrevingOgGåVidereTilNesteSteg(
             @PathVariable vedtakId: Long,
