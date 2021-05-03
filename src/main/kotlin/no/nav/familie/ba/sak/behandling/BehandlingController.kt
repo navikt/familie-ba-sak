ae3ab5cd67afabec4ee91129470457c10fffe3dc
mundal <halvorbmundal@gmail.com>
2021-05-03 16:21:18 +0200

    Legg til @Transactional

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/BehandlingController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/BehandlingController.kt
@@ -117,6 +117,7 @@ class BehandlingController(
         return ResponseEntity.ok(Ressurs.success(restSimulering))
     }
 
+    @Transactional
     @PostMapping(path = ["/{behandlingId}/tilbakekreving"])
     fun lagreTilbakekrevingOgGåVidereTilNesteSteg(
             @PathVariable behandlingId: Long,
