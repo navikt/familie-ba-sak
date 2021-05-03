1e67e5d910cb3579a9062f4149a4a5dc4c071531
Henning Håkonsen <henning.hakonsen@nav.no>
2021-05-03 08:51:56 +0200

    Fjerner debug-logging.

--- src/main/kotlin/no/nav/familie/ba/sak/pdl/PdlRestClient.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/pdl/PdlRestClient.kt
@@ -95,13 +95,12 @@ class PdlRestClient(@Value("\${PDL_URL}") pdlBaseUrl: URI,
         } catch (e: Exception) {
             val mostSpecificThrowable = NestedExceptionUtils.getMostSpecificCause(e)
 
-            secureLogger.info("Most specific feil: ${mostSpecificThrowable.message}", mostSpecificThrowable)
             when (e) {
                 is Feil -> throw e
-                else -> throw Feil(message = "Feil ved oppslag på person. Gav feil: ${e.message}.",
+                else -> throw Feil(message = "Feil ved oppslag på person. Gav feil: ${mostSpecificThrowable.message}.",
                                    frontendFeilmelding = "Feil oppsto ved oppslag på person $personIdent",
                                    httpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
-                                   throwable = e)
+                                   throwable = mostSpecificThrowable)
             }
         }
     }
