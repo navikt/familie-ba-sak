8fde70fe6ca37febc4ef87b824836dfd7dae918e
Geirmund Simonsen <geirmund.simonsen@nav.no>
2021-05-19 15:18:54 +0200

    !e2e som profil.

--- src/main/kotlin/no/nav/familie/ba/sak/config/BisysConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/BisysConfig.kt
@@ -4,33 +4,29 @@ import no.nav.familie.sikkerhet.OIDCUtil
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.env.Environment
+import org.springframework.context.annotation.Profile
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+@Profile("!e2e")
 @Configuration
 class BisysConfig(private val oidcUtil: OIDCUtil,
                   @Value("\${BISYS_CLIENT_ID:dummy}")
-                  private val bisysClientId: String,
-                  private val environment: Environment
+                  private val bisysClientId: String
 ) {
     @Bean
-    fun bisysFilter(): OncePerRequestFilter {
-        val enabled = !environment.activeProfiles.any { it == "e2e" }
-
-        return object : OncePerRequestFilter() {
-            override fun doFilterInternal(
-                request: HttpServletRequest,
-                response: HttpServletResponse,
-                filterChain: FilterChain
-            ) {
-                if (enabled && bisysClientId == oidcUtil.getClaim("azp") && !request.requestURI.startsWith("/api/bisys")) {
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Kun autorisert for kall mot /api/bisys*")
-                } else {
-                    filterChain.doFilter(request, response)
-                }
+    fun bisysFilter() = object : OncePerRequestFilter() {
+        override fun doFilterInternal(
+            request: HttpServletRequest,
+            response: HttpServletResponse,
+            filterChain: FilterChain
+        ) {
+            if (bisysClientId == oidcUtil.getClaim("azp") && !request.requestURI.startsWith("/api/bisys")) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Kun autorisert for kall mot /api/bisys*")
+            } else {
+                filterChain.doFilter(request, response)
             }
         }
     }
