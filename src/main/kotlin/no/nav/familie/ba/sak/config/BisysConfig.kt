9865b22c1624069f94a52eacbce416fee8858932
Geirmund Simonsen <geirmund.simonsen@nav.no>
2021-05-19 10:47:52 +0200

    Path authorization for Bisys (andre apper slipper igjennom)

---
+++ src/main/kotlin/no/nav/familie/ba/sak/config/BisysConfig.kt
@@ -0,0 +1,26 @@
+package no.nav.familie.ba.sak.config
+
+import no.nav.familie.sikkerhet.OIDCUtil
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Configuration
+class BisysConfig(private val oidcUtil: OIDCUtil,
+                  @Value("\${BISYS_CLIENT_ID}")
+                  private val bisysClientId: String) {
+    @Bean
+    fun bisysFilter() = object: OncePerRequestFilter() {
+        override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+            if (bisysClientId == oidcUtil.getClaim("azp") && !request.requestURI.startsWith("/api/bisys")) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Kun autorisert for kall mot /api/bisys*")
+            } else {
+                filterChain.doFilter(request, response)
+            }
+        }
+    }
+}
