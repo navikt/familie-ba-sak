2b5ec4f10b87445092201d15da96baeba1f8c4e8
Marcus Gustafson <marcus.gustafson@computas.com>
2021-05-26 17:00:33 +0200

    Fjerner et duplisert skript

--- src/main/resources/db/migration/V132__periode_paa_bostedsadresser_og_flere_bostedsadresser.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE po_bostedsadresse
-    ADD COLUMN fom DATE;
-ALTER TABLE po_bostedsadresse
-    ADD COLUMN tom DATE;
-
-ALTER TABLE po_bostedsadresse
-    ADD COLUMN fk_po_person_id BIGINT REFERENCES po_person (id);
