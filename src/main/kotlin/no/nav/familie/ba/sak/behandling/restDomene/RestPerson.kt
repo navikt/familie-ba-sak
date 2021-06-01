27ffa57701317912cb533382122d9e77448db28b
Nina Kjekstad <nina.kjekstad@nav.no>
2021-06-01 15:56:16 +0200

    Justering i visningsstring

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/restDomene/RestPerson.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/restDomene/RestPerson.kt
@@ -4,7 +4,6 @@ import no.nav.familie.ba.sak.behandling.grunnlag.personopplysninger.Kjønn
 import no.nav.familie.ba.sak.behandling.grunnlag.personopplysninger.Målform
 import no.nav.familie.ba.sak.behandling.grunnlag.personopplysninger.Person
 import no.nav.familie.ba.sak.behandling.grunnlag.personopplysninger.PersonType
-import no.nav.familie.ba.sak.common.Utils.storForbokstav
 import java.time.LocalDate
 import java.time.LocalDateTime
 
