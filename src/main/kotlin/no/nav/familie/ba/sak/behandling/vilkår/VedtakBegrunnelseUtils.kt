f5c3f1193f2cdba572e7c978102c68da42617dd1
Marcus Gustafson <marcus.gustafson@computas.com>
2021-06-02 11:01:09 +0200

    Tilgjenliggjøe frontend barn død begrunnelsestekst

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/vilkår/VedtakBegrunnelseUtils.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/vilkår/VedtakBegrunnelseUtils.kt
@@ -31,7 +31,9 @@ object VedtakBegrunnelseUtils {
                     VedtakBegrunnelseSpesifikasjon.AVSLAG_BOR_HOS_SØKER,
                     VedtakBegrunnelseSpesifikasjon.AVSLAG_OMSORG_FOR_BARN,
                     VedtakBegrunnelseSpesifikasjon.FORTSATT_INNVILGET_BOR_MED_SØKER,
-                    VedtakBegrunnelseSpesifikasjon.FORTSATT_INNVILGET_FAST_OMSORG
+                    VedtakBegrunnelseSpesifikasjon.FORTSATT_INNVILGET_FAST_OMSORG,
+                    VedtakBegrunnelseSpesifikasjon.OPPHØR_BARN_DØD,
+                    VedtakBegrunnelseSpesifikasjon.REDUKSJON_BARN_DØD,
             ),
             BOSATT_I_RIKET to listOf(
                     VedtakBegrunnelseSpesifikasjon.INNVILGET_BOSATT_I_RIKTET,
@@ -57,7 +59,7 @@ object VedtakBegrunnelseUtils {
                     VedtakBegrunnelseSpesifikasjon.AVSLAG_LOVLIG_OPPHOLD_SKJØNNSMESSIG_VURDERING_TREDJELANDSBORGER,
                     VedtakBegrunnelseSpesifikasjon.FORTSATT_INNVILGET_LOVLIG_OPPHOLD_TREDJELANDSBORGER,
                     VedtakBegrunnelseSpesifikasjon.FORTSATT_INNVILGET_LOVLIG_OPPHOLD_EØS,
-                    VedtakBegrunnelseSpesifikasjon.FORTSATT_INNVILGET_LOVLIG_OPPHOLD_OPPHOLDSTILLATELSE
+                    VedtakBegrunnelseSpesifikasjon.FORTSATT_INNVILGET_LOVLIG_OPPHOLD_OPPHOLDSTILLATELSE,
             )
     )
 
