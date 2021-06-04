8d5e3d0523a039eadbb4fe5569faf365af5151dd
Nina Kjekstad <nina.kjekstad@nav.no>
2021-06-04 09:55:08 +0200

    Skriv om controllerretur pga springstøtte for apiExceptionHandler

--- src/main/kotlin/no/nav/familie/ba/sak/behandling/grunnlag/personopplysninger/PersonController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/behandling/grunnlag/personopplysninger/PersonController.kt
@@ -3,9 +3,7 @@ package no.nav.familie.ba.sak.behandling.grunnlag.personopplysninger
 import no.nav.familie.ba.sak.behandling.fagsak.FagsakService
 import no.nav.familie.ba.sak.behandling.restDomene.RestFagsak
 import no.nav.familie.ba.sak.behandling.restDomene.RestPersonInfo
-import no.nav.familie.ba.sak.behandling.restDomene.tilRestFagsak
 import no.nav.familie.ba.sak.behandling.restDomene.tilRestPersonInfo
-import no.nav.familie.ba.sak.common.Feil
 import no.nav.familie.ba.sak.pdl.PersonopplysningerService
 import no.nav.familie.ba.sak.validering.PersontilgangConstraint
 import no.nav.familie.kontrakter.felles.Ressurs
@@ -28,61 +26,21 @@ class PersonController(private val personopplysningerService: Personopplysninger
 
     @GetMapping
     fun hentPerson(@RequestHeader personIdent: String): ResponseEntity<Ressurs<RestPersonInfo>> {
-        return Result.runCatching {
-            personopplysningerService.hentMaskertPersonInfoVedManglendeTilgang(personIdent)
-            ?: personopplysningerService.hentPersoninfoMedRelasjoner(personIdent).tilRestPersonInfo(personIdent)
-        }
-                .fold(
-                        onFailure = {
-                            throw Feil(message = "Hent person feilet: ${it.message}",
-                                       frontendFeilmelding = "Henting av person med ident '$personIdent' feilet.",
-                                       throwable = it)
-                        },
-                        onSuccess = {
-                            ResponseEntity.ok(Ressurs.success(it))
-                        }
-                )
+        val personinfo = personopplysningerService.hentMaskertPersonInfoVedManglendeTilgang(personIdent)
+                         ?: personopplysningerService.hentPersoninfoMedRelasjoner(personIdent).tilRestPersonInfo(personIdent)
+        return ResponseEntity.ok(Ressurs.success(personinfo))
     }
 
     @GetMapping(path = ["/enkel"])
     @PersontilgangConstraint
     fun hentPersonEnkel(@RequestHeader personIdent: String): ResponseEntity<Ressurs<RestPersonInfo>> {
-        return Result.runCatching {
-            personopplysningerService.hentPersoninfo(personIdent)
-        }
-                .fold(
-                        onFailure = {
-                            when (it) {
-                                is Feil -> throw it
-                                else -> throw Feil(message = "Hent person feilet: ${it.message}",
-                                                   frontendFeilmelding = "Henting av person med ident '$personIdent' feilet.",
-                                                   throwable = it)
-                            }
-                        },
-                        onSuccess = {
-                            ResponseEntity.ok(Ressurs.success(it.tilRestPersonInfo(personIdent)))
-                        }
-                )
+        val personinfo = personopplysningerService.hentPersoninfo(personIdent)
+        return ResponseEntity.ok(Ressurs.success(personinfo.tilRestPersonInfo(personIdent)))
     }
 
     @GetMapping(path = ["/oppdater-registeropplysninger/{behandlingId}"])
     fun hentOgOppdaterRegisteropplysninger(@PathVariable behandlingId: Long): ResponseEntity<Ressurs<RestFagsak>> {
-        return Result.runCatching {
-            val personopplysningGrunnlag = persongrunnlagService.oppdaterRegisteropplysninger(behandlingId)
-            fagsakService.hentRestFagsakForPerson(personopplysningGrunnlag.søker.personIdent)
-        }
-                .fold(
-                        onFailure = {
-                            when (it) {
-                                is Feil -> throw it
-                                else -> throw Feil(message = "Feil ved henting og oppdatering av personopplysningsgrunnlag: ${it.message}",
-                                                   frontendFeilmelding = "Feilet ved oppdatering av registeropplysninger på behandling '$behandlingId'.",
-                                                   throwable = it)
-                            }
-                        },
-                        onSuccess = {
-                            ResponseEntity.ok(it)
-                        }
-                )
+        val personopplysningGrunnlag = persongrunnlagService.oppdaterRegisteropplysninger(behandlingId)
+        return ResponseEntity.ok(fagsakService.hentRestFagsakForPerson(personopplysningGrunnlag.søker.personIdent))
     }
 }
