ac3462a097b1f031a47b2f4f900154f292288102
Nina Kjekstad <ninakjek@users.noreply.github.com>
2021-06-15 14:24:19 +0200

    Bugfix/cascade delete (#1144)
    * Oppdater fk for vedtaksbegrunnelse
    
    * Oppdater fk for andel tilkjent ytelse og fjern unødvendige funksjoner
    
    * Fjern unødvendig annotering
    
    * Oppdater fk for økonomisimuleringpostering og fjern unødvendige funksjoner

--- src/main/kotlin/no/nav/familie/ba/sak/beregning/domene/AndelTilkjentYtelseRepository.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/beregning/domene/AndelTilkjentYtelseRepository.kt
@@ -1,7 +1,6 @@
 package no.nav.familie.ba.sak.beregning.domene
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 
 interface AndelTilkjentYtelseRepository : JpaRepository<AndelTilkjentYtelse, Long> {
@@ -15,7 +14,4 @@ interface AndelTilkjentYtelseRepository : JpaRepository<AndelTilkjentYtelse, Lon
     @Query(value = "SELECT aty FROM AndelTilkjentYtelse aty WHERE aty.behandlingId IN :behandlingIder AND aty.stønadTom >= CURRENT_TIMESTAMP")
     fun finnLøpendeAndelerTilkjentYtelseForBehandlinger(behandlingIder: List<Long>): List<AndelTilkjentYtelse>
 
-    @Modifying
-    @Query(value = "DELETE FROM AndelTilkjentYtelse aty WHERE aty.behandlingId = :behandlingId")
-    fun slettAlleAndelerTilkjentYtelseForBehandling(behandlingId: Long)
 }
