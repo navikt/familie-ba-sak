207dad2d1a7231ecae31ad3cc9ec0b0eab03a6b4
Stig Helge Strøm <stig.helge.strom@nav.no>
2021-06-15 15:15:10 +0200

    Swagger for BISYS-tjensten, bedre validering og dokumentasjon

---
+++ src/main/kotlin/no/nav/familie/ba/sak/config/SwaggerConfig.kt
@@ -0,0 +1,61 @@
+package no.nav.familie.ba.sak.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiKey
+import springfox.documentation.service.AuthorizationScope
+import springfox.documentation.service.SecurityReference
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
+import springfox.documentation.spring.web.plugins.Docket
+import java.time.LocalDateTime
+import java.time.YearMonth
+
+@Configuration
+class SwaggerConfig {
+
+    private val bearer = "Bearer"
+
+    @Bean
+    fun bisysApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .directModelSubstitute(YearMonth::class.java, String::class.java)
+            .directModelSubstitute(LocalDateTime::class.java, String::class.java)
+            .groupName("BISYS")
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("no.nav.familie.ba.sak.bisys"))
+            .paths(PathSelectors.any())
+            .build()
+            .securitySchemes(securitySchemes())
+            .securityContexts(securityContext())
+            .apiInfo(
+                ApiInfoBuilder().description("Tjenester for BISYS. For å ta i bruk tjenesten så trenger man å få lagt inn client-id inn i liste som godkjente klienter. Ta kontakt med barnetrygdteamet i slackkanalen #team-familie")
+                    .build()
+            )
+    }
+
+    private fun securitySchemes(): List<ApiKey> {
+        return listOf(ApiKey(bearer, "Authorization", "header"))
+    }
+
+    private fun securityContext(): List<SecurityContext> {
+        return listOf(
+            SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/api.*"))
+                .build()
+        )
+    }
+
+    private fun defaultAuth(): List<SecurityReference> {
+        val authorizationScope = AuthorizationScope("global", "accessEverything")
+        val authorizationScopes = arrayOfNulls<AuthorizationScope>(1)
+        authorizationScopes[0] = authorizationScope
+        return listOf(SecurityReference(bearer, authorizationScopes))
+    }
+
+
+}
--- src/main/kotlin/no/nav/familie/ba/sak/ekstern/bisys/BisysService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/ekstern/bisys/BisysService.kt
@@ -1,12 +1,13 @@
 package no.nav.familie.ba.sak.ekstern.bisys
 
+
 import no.nav.familie.ba.sak.integrasjoner.infotrygd.InfotrygdBarnetrygdClient
+import no.nav.familie.ba.sak.common.toYearMonth
 import org.springframework.stereotype.Service
 import java.time.LocalDate
-import java.time.YearMonth
 
 @Service
 class BisysService(private val infotrygdBarnetrygdClient: InfotrygdBarnetrygdClient) {
     fun hentUtvidetBarnetrygd(personIdent: String, fraDato: LocalDate) =
-        infotrygdBarnetrygdClient.hentUtvidetBarnetrygd(personIdent, YearMonth.of(fraDato.year, fraDato.month))
+        infotrygdBarnetrygdClient.hentUtvidetBarnetrygd(personIdent, fraDato.toYearMonth())
 }
