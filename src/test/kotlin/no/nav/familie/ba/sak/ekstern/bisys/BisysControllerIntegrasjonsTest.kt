19b3fc24c3bcff98d4e201486e5a321cbdbf96e6
Stig Helge Strøm <stig.helge.strom@nav.no>
2021-06-21 12:49:59 +0200

    Fikset bisys-tjenesten og lagt til tester

---
+++ src/test/kotlin/no/nav/familie/ba/sak/ekstern/bisys/BisysControllerIntegrasjonsTest.kt
@@ -0,0 +1,238 @@
+package no.nav.familie.ba.sak.ekstern.bisys
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.equalToJson
+import com.github.tomakehurst.wiremock.client.WireMock.post
+import com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor
+import com.github.tomakehurst.wiremock.client.WireMock.stubFor
+import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
+import com.github.tomakehurst.wiremock.client.WireMock.verify
+import no.nav.familie.ba.sak.WebSpringAuthTestRunner
+import no.nav.familie.ba.sak.common.EksternTjenesteFeil
+import no.nav.familie.ba.sak.common.randomFnr
+import no.nav.familie.kontrakter.felles.objectMapper
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.within
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
+import org.springframework.core.io.ClassPathResource
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.HttpServerErrorException
+import org.springframework.web.client.postForEntity
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.YearMonth
+import java.time.temporal.ChronoUnit
+
+
+@ActiveProfiles("postgres", "mock-pdl", "mock-oauth")
+@AutoConfigureWireMock(port = 28085)
+@TestPropertySource(properties = ["FAMILIE_BA_INFOTRYGD_BARNETRYGD_API_URL=http://localhost:28085"])
+@Tag("integration")
+class BisysControllerIntegrasjonsTest : WebSpringAuthTestRunner() {
+
+
+    @Test
+    fun `Skal kaste feil når fraDato er mer enn 5 år tilbake i tid`() {
+        val fnr = randomFnr()
+
+        val requestEntity = byggRequestEntity(
+            BisysUtvidetBarnetrygdRequest(
+                fnr, LocalDate.now().minusYears(5).minusDays(1)
+            )
+        )
+
+        val error = assertThrows<HttpClientErrorException> {
+            restTemplate.postForEntity<Any>(
+                hentUrl("/api/bisys/hent-utvidet-barnetrygd"),
+                requestEntity
+            )
+        }
+
+        assertThat(error.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+        val errorObject = objectMapper.readValue<EksternTjenesteFeil>(error.responseBodyAsByteArray)
+        assertThat(errorObject.melding).isEqualTo("fraDato kan ikke være lenger enn 5 år tilbake i tid")
+        assertThat(errorObject.path).isEqualTo("/api/bisys/hent-utvidet-barnetrygd")
+        assertThat(errorObject.timestamp).isCloseTo(LocalDateTime.now(), within(10, ChronoUnit.SECONDS))
+    }
+
+    @Test
+    fun `Skal kaste gode feilmeldinger ved feil mot infotrygd-barnetrygd`() {
+        val fnr = randomFnr()
+
+        stubFor(
+            post(urlEqualTo("/infotrygd/barnetrygd/utvidet"))
+                .willReturn(
+                    aResponse()
+                        .withStatus(500)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("foobar")
+                )
+        )
+
+        val requestEntity = byggRequestEntity(
+            BisysUtvidetBarnetrygdRequest(
+                fnr, LocalDate.now()
+            )
+        )
+
+        val error = assertThrows<HttpServerErrorException> {
+            restTemplate.postForEntity<Any>(
+                hentUrl("/api/bisys/hent-utvidet-barnetrygd"),
+                requestEntity
+            )
+        }
+
+        assertThat(error.statusCode).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR)
+        val errorObject = objectMapper.readValue<EksternTjenesteFeil>(error.responseBodyAsByteArray)
+        assertThat(errorObject.melding).isEqualTo("Henting av utvidet barnetrygd feilet. Gav feil: 500 Server Error: [foobar]")
+        assertThat(errorObject.path).isEqualTo("/api/bisys/hent-utvidet-barnetrygd")
+        assertThat(errorObject.timestamp).isCloseTo(LocalDateTime.now(), within(10, ChronoUnit.SECONDS))
+        assertThat(errorObject.exception).contains("HttpServerErrorException")
+        assertThat(errorObject.stackTrace).isNotEmpty
+    }
+
+    @Test
+    fun `Skal returnere tom periode hvis det ikke er noen utbetalinger i infotrygd-barnetrygd`() {
+        val fnr = randomFnr()
+
+        stubFor(
+            post(urlEqualTo("/infotrygd/barnetrygd/utvidet"))
+                .willReturn(
+                    aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(gyldigOppgaveResponse("tom"))
+                )
+        )
+
+        val requestEntity = byggRequestEntity(
+            BisysUtvidetBarnetrygdRequest(
+                fnr, LocalDate.now().minusYears(4)
+            )
+        )
+
+        val responseEntity = restTemplate.postForEntity<BisysUtvidetBarnetrygdResponse>(
+            hentUrl("/api/bisys/hent-utvidet-barnetrygd"),
+            requestEntity
+        )
+        verify(
+            postRequestedFor(urlEqualTo("/infotrygd/barnetrygd/utvidet"))
+                .withRequestBody(equalToJson("""{"personIdent":"$fnr", "fraDato":"${YearMonth.now().minusYears(4)}" }"""))
+
+        )
+
+        assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(responseEntity.body).isNotNull
+        assertThat(responseEntity.body!!.perioder).isEmpty()
+    }
+
+
+    @Test
+    fun `Skal returnere perioder hvis det er noen utbetalinger i infotrygd-barnetrygd`() {
+        val fnr = randomFnr()
+
+        stubFor(
+            post(urlEqualTo("/infotrygd/barnetrygd/utvidet"))
+                .willReturn(
+                    aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(gyldigOppgaveResponse("med-perioder"))
+                )
+        )
+
+        val requestEntity = byggRequestEntity(
+            BisysUtvidetBarnetrygdRequest(
+                fnr, LocalDate.now().minusYears(4)
+            )
+        )
+
+        val responseEntity = restTemplate.postForEntity<BisysUtvidetBarnetrygdResponse>(
+            hentUrl("/api/bisys/hent-utvidet-barnetrygd"),
+            requestEntity
+        )
+        verify(
+            postRequestedFor(urlEqualTo("/infotrygd/barnetrygd/utvidet"))
+                .withRequestBody(equalToJson("""{"personIdent":"$fnr", "fraDato":"${YearMonth.now().minusYears(4)}" }"""))
+
+        )
+
+        assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(responseEntity.body).isNotNull
+        assertThat(responseEntity.body!!.perioder)
+            .hasSize(2)
+        assertThat(responseEntity.body!!.perioder)
+            .contains(UtvidetBarnetrygdPeriode(BisysStønadstype.SMÅBARNSTILLEGG, YearMonth.of(2019, 12), null, 660.0))
+        assertThat(responseEntity.body!!.perioder)
+            .contains(UtvidetBarnetrygdPeriode(BisysStønadstype.UTVIDET, YearMonth.of(2019, 12), null, 1054.0))
+
+    }
+
+
+    @Test
+    fun `Skal kaste feil tilgang når bisys kaller tjenste som ikke er bisys-relatert`() {
+        val fnr = randomFnr()
+
+        val header = HttpHeaders()
+        header.contentType = MediaType.APPLICATION_JSON
+        header.setBearerAuth(
+            hentTokenForBisys()
+        )
+        val ikkeBisysTjeneste = HttpEntity<String>(
+            "tullball", header
+        )
+
+        val error = assertThrows<HttpClientErrorException> {
+            restTemplate.postForEntity<Any>(
+                hentUrl("/api/tullballtjeneste"),
+                ikkeBisysTjeneste
+            )
+        }
+
+        assertThat(error.statusCode).isEqualTo(HttpStatus.UNAUTHORIZED)
+    }
+
+
+    private fun byggRequestEntity(request: BisysUtvidetBarnetrygdRequest): HttpEntity<String> {
+        val header = HttpHeaders()
+        header.contentType = MediaType.APPLICATION_JSON
+        header.setBearerAuth(
+            hentTokenForBisys()
+        )
+        val requestEntity = HttpEntity<String>(
+            objectMapper.writeValueAsString(
+                request
+            ), header
+        )
+        return requestEntity
+    }
+
+    private fun hentTokenForBisys() = token(
+        mapOf(
+            "groups" to listOf("SAKSBEHANDLER"),
+            "name" to "Mock McMockface",
+            "preferred_username" to "mock.mcmockface@nav.no"
+        ),
+        clientId = "dummy"
+    ).toString()
+
+    private fun gyldigOppgaveResponse(filnavn: String): String {
+        return Files.readString(
+            ClassPathResource("ekstern/bisys-$filnavn.json").file.toPath(),
+            StandardCharsets.UTF_8
+        )
+    }
+}
