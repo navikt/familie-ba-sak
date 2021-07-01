1dc2efd6848806ae5c6a3834b82d8fa7c762c038
Nina Kjekstad <nina.kjekstad@nav.no>
2021-07-01 14:36:41 +0200

    Utplukking av saker med satsendring

--- src/main/kotlin/no/nav/familie/ba/sak/internal/SchedulingController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/internal/SchedulingController.kt
@@ -3,7 +3,6 @@ package no.nav.familie.ba.sak.internal
 import no.nav.familie.ba.sak.kjerne.autobrev.Autobrev6og18ÅrScheduler
 import no.nav.familie.ba.sak.common.EnvService
 import no.nav.familie.ba.sak.kjerne.autorevurdering.SatsendringService
-import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingRepository
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.ResponseEntity
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController
 class SchedulingController(
         private val scheduler: Autobrev6og18ÅrScheduler,
         private val satsendringService: SatsendringService,
-        private val behandlingRepository: BehandlingRepository,
         private val envService: EnvService) {
 
     @GetMapping(path = ["/autobrev"])
@@ -31,13 +29,11 @@ class SchedulingController(
         }
     }
 
-    // TODO: Erstattes av schedulertrigging når automatisk utplukking er implementert
     @GetMapping(path = ["/test-satsendring/{behandlingId}"])
     @Unprotected
     fun utførSatsendringPåBehandling(@PathVariable behandlingId: Long): ResponseEntity<Ressurs<String>> {
         return if (envService.erPreprod() || envService.erDev() || envService.erE2E()) {
-            val behandling = behandlingRepository.finnBehandling(behandlingId = behandlingId)
-            satsendringService.utførSatsendring(behandling)
+            satsendringService.utførSatsendring(behandlingId)
             ResponseEntity.ok(Ressurs.success("Trigget satsendring"))
         } else {
             ResponseEntity.ok(Ressurs.success("Endepunktet gjør ingenting i prod."))
