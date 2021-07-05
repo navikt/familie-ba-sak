87b45b5b1ba24290370101c8bfc35d6ed32bac3c
Marcus Gustafson <marcus.gustafson@computas.com>
2021-07-05 16:44:25 +0200

    Fikset noen feil i migreringsskript

--- src/main/resources/db/migration/V145__migrer_vadtaksbegrunnelse_til_veddtaksperiode_og_begrunnelse.sql
+++ src/main/resources/db/migration/V145__migrer_vadtaksbegrunnelse_til_veddtaksperiode_og_begrunnelse.sql
@@ -5,7 +5,7 @@ INSERT INTO vedtaksperiode (id, fk_vedtak_id, fom, tom, type)
         WHEN 'REDUKSJON' THEN 'UTBETALING'
         WHEN 'AVSLAG' THEN 'AVSLAG'
         WHEN 'OPPHØR' THEN 'OPPHØR'
-        WHEN 'FORTSATT_INNVILGET' THEN 'FORTSATT_INNVILGET'
+        WHEN 'FORTSATT' THEN 'FORTSATT_INNVILGET'
         END AS type
     FROM vedtak_begrunnelse vb
     INNER JOIN vedtak v
@@ -13,32 +13,44 @@ INSERT INTO vedtaksperiode (id, fk_vedtak_id, fom, tom, type)
     INNER JOIN behandling b
     ON v.fk_behandling_id = b.id
     WHERE NOT EXISTS(
-        SELECT id
-        FROM vedtaksperiode v
-        WHERE v.fk_vedtak_id = vb.fk_vedtak_id AND
-            v.fom = vb.fom AND
-            v.tom = vb.tom AND
-            v.type = v.type)
+            SELECT id
+            FROM vedtaksperiode v
+            WHERE v.fk_vedtak_id = vb.fk_vedtak_id AND
+                (v.fom IS NULL OR v.fom = vb.fom) AND
+                (v.tom IS NULL OR v.tom = vb.tom) AND
+                v.type = v.type)
         AND b.status != 'FATTER_VEDTAK'
         AND b.resultat != 'FORTSATT_INNVILGET'
     GROUP BY fk_vedtak_id, fom, tom, type)
 
-INSERT INTO vedtaksbegrunnelse(id, fk_vedtaksperiode_id, fom, tom, vedtak_begrunnelse_spesifikasjon, oppprette_av, opprettet_tid, endret_av, endret_tid)
-    SELECT nextval('vedtaksbegrunnelse_seq'), vp.id, fom, tom, begrunnelse, oppprette_av, opprettet_tid, endret_av, endret_tid
+INSERT INTO vedtaksbegrunnelse(id, fk_vedtaksperiode_id, vedtak_begrunnelse_spesifikasjon)
+    SELECT nextval('vedtaksbegrunnelse_seq'), vp.id, begrunnelse
     FROM vedtak_begrunnelse vb
     INNER JOIN vedtaksperiode vp
-        ON vb.fk_vedtak_id = vp.fk_vedtak_id AND
-           vb.fom = vp.fom AND
-           vb.tom = vp.tom AND
-           split_part(vb.begrunnelse,'_', 1)  = vp.type
+        ON vp.fk_vedtak_id = vb.fk_vedtak_id AND
+           (vp.fom IS NULL OR vp.fom = vb.fom) AND
+           (vp.tom IS NULL OR vp.tom = vb.tom) AND
+           vp.type = CASE split_part(vb.begrunnelse,'_', 1)
+                        WHEN 'INNVILGET' THEN 'UTBETALING'
+                        WHEN 'REDUKSJON' THEN 'UTBETALING'
+                        WHEN 'AVSLAG' THEN 'AVSLAG'
+                        WHEN 'OPPHØR' THEN 'OPPHØR'
+                        WHEN 'FORTSATT' THEN 'FORTSATT_INNVILGET'
+                     END
     WHERE vb.begrunnelse NOT LIKE '%FRITEKST%'
 
-INSERT INTO vedtaksbegrunnelse_fritekst(id, fk_vedtaksperiode_id, fritekst, oppprette_av, opprettet_tid, endret_av, endret_tid)
-    SELECT nextval('vedtaksbegrunnelse_fritekst_seq'), vp.id, brev_begrunnelse, oppprette_av, opprettet_tid, endret_av, endret_tid
+INSERT INTO vedtaksbegrunnelse_fritekst(id, fk_vedtaksperiode_id, fritekst)
+    SELECT nextval('vedtaksbegrunnelse_fritekst_seq'), vp.id, brev_begrunnelse
     FROM vedtak_begrunnelse vb
     INNER JOIN vedtaksperiode vp
-        ON vb.fk_vedtak_id = vp.fk_vedtak_id AND
-           vb.fom = vp.fom AND
-           vb.tom = vp.tom AND
-           split_part(vb.begrunnelse,'_', 1)  = vp.type
+        ON vp.fk_vedtak_id = vb.fk_vedtak_id AND
+           (vp.fom IS NULL OR vp.fom = vb.fom) AND
+           (vp.tom IS NULL OR vp.tom = vb.tom) AND
+           vp.type = CASE split_part(vb.begrunnelse,'_', 1)
+                        WHEN 'INNVILGET' THEN 'UTBETALING'
+                        WHEN 'REDUKSJON' THEN 'UTBETALING'
+                        WHEN 'AVSLAG' THEN 'AVSLAG'
+                        WHEN 'OPPHØR' THEN 'OPPHØR'
+                        WHEN 'FORTSATT' THEN 'FORTSATT_INNVILGET'
+                     END
     WHERE vb.begrunnelse LIKE '%FRITEKST%'
