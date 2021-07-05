39ee2bedeea25d60e81466f65cf06187ef6f8a33
Marcus Gustafson <marcus.gustafson@computas.com>
2021-07-05 18:33:00 +0200

    Fikset feil i migreringsskript

--- src/main/resources/db/migration/V145__migrer_vadtaksbegrunnelse_til_veddtaksperiode_og_begrunnelse.sql
+++ src/main/resources/db/migration/V145__migrer_vadtaksbegrunnelse_til_veddtaksperiode_og_begrunnelse.sql
@@ -16,8 +16,8 @@ INSERT INTO vedtaksperiode (id, fk_vedtak_id, fom, tom, type)
             SELECT id
             FROM vedtaksperiode v
             WHERE v.fk_vedtak_id = vb.fk_vedtak_id AND
-                (v.fom IS NULL OR v.fom = vb.fom) AND
-                (v.tom IS NULL OR v.tom = vb.tom) AND
+                (v.fom IS NULL AND vb.fom IS NULL) OR v.fom = vb.fom) AND
+                (v.tom IS NULL AND vb.tom IS NULL) OR v.tom = vb.tom) AND
                 v.type = v.type)
         AND b.status != 'FATTER_VEDTAK'
         AND b.resultat != 'FORTSATT_INNVILGET'
@@ -28,8 +28,8 @@ INSERT INTO vedtaksbegrunnelse(id, fk_vedtaksperiode_id, vedtak_begrunnelse_spes
     FROM vedtak_begrunnelse vb
     INNER JOIN vedtaksperiode vp
         ON vp.fk_vedtak_id = vb.fk_vedtak_id AND
-           (vp.fom IS NULL OR vp.fom = vb.fom) AND
-           (vp.tom IS NULL OR vp.tom = vb.tom) AND
+           (vp.fom IS NULL AND vb.fom IS NULL) OR vp.fom = vb.fom) AND
+           (vp.tom IS NULL AND vb.tom IS NULL) OR vp.tom = vb.tom) AND
            vp.type = CASE split_part(vb.begrunnelse,'_', 1)
                         WHEN 'INNVILGET' THEN 'UTBETALING'
                         WHEN 'REDUKSJON' THEN 'UTBETALING'
@@ -44,8 +44,8 @@ INSERT INTO vedtaksbegrunnelse_fritekst(id, fk_vedtaksperiode_id, fritekst)
     FROM vedtak_begrunnelse vb
     INNER JOIN vedtaksperiode vp
         ON vp.fk_vedtak_id = vb.fk_vedtak_id AND
-           (vp.fom IS NULL OR vp.fom = vb.fom) AND
-           (vp.tom IS NULL OR vp.tom = vb.tom) AND
+           (vp.fom IS NULL AND vb.fom IS NULL) OR vp.fom = vb.fom) AND
+           (vp.tom IS NULL AND vb.tom IS NULL) OR vp.tom = vb.tom) AND
            vp.type = CASE split_part(vb.begrunnelse,'_', 1)
                         WHEN 'INNVILGET' THEN 'UTBETALING'
                         WHEN 'REDUKSJON' THEN 'UTBETALING'
