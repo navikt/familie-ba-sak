e493818806e14b4024e07dc03b7a4b3819765aeb
Mathias Fossum <mathias.s.fossum@gmail.com>
2021-07-09 13:34:55 +0200

    Verdikjedetest for valg av fagsystem

--- src/test/kotlin/no/nav/familie/ba/sak/kjerne/automatiskVurdering/VerdikjedeTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/kjerne/automatiskVurdering/VerdikjedeTest.kt
@@ -4,12 +4,21 @@ import io.mockk.every
 import no.nav.familie.ba.sak.common.DbContainerInitializer
 import no.nav.familie.ba.sak.integrasjoner.pdl.PdlRestClient
 import no.nav.familie.ba.sak.integrasjoner.pdl.PersonopplysningerService
+import no.nav.familie.ba.sak.integrasjoner.pdl.internal.ForelderBarnRelasjon
+import no.nav.familie.ba.sak.integrasjoner.pdl.internal.Personident
+import no.nav.familie.ba.sak.kjerne.automatiskvurdering.FagsystemRegelVurdering
+import no.nav.familie.ba.sak.kjerne.automatiskvurdering.VelgFagSystemService
 import no.nav.familie.ba.sak.kjerne.behandling.NyBehandlingHendelse
 import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingResultat
+import no.nav.familie.ba.sak.kjerne.fagsak.FagsakService
+import no.nav.familie.ba.sak.kjerne.fagsak.FagsakStatus
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersongrunnlagService
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersonopplysningGrunnlagRepository
+import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.domene.PersonIdent
 import no.nav.familie.ba.sak.kjerne.steg.StegService
+import no.nav.familie.kontrakter.felles.personopplysning.FORELDERBARNRELASJONROLLE
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -38,6 +47,8 @@ class Integrasjonstest(
         @Autowired val persongrunnlagService: PersongrunnlagService,
         @Autowired val personopplysningGrunnlagRepository: PersonopplysningGrunnlagRepository,
         @Autowired val pdlRestClient: PdlRestClient,
+        @Autowired val velgFagSystemService: VelgFagSystemService,
+        @Autowired val fagSakService: FagsakService
 ) {
 
     @Test
@@ -60,7 +71,10 @@ class Integrasjonstest(
     @Test
     fun `Skal ikke passere vilkårsvurdering dersom barn er over 18`() {
         val barn = genererAutomatiskTestperson(LocalDate.parse("1999-10-10"), emptySet(), emptyList())
-        val søker = genererAutomatiskTestperson(LocalDate.parse("1998-10-10"), setOf("12345678910"), emptyList())
+        val søker = genererAutomatiskTestperson(LocalDate.parse("1998-10-10"),
+                                                setOf(ForelderBarnRelasjon(Personident("11211211211"),
+                                                                           FORELDERBARNRELASJONROLLE.BARN)),
+                                                emptyList())
         every { personopplysningerService.hentPersoninfoMedRelasjoner("11211211211") } returns barn
         every { personopplysningerService.hentPersoninfoMedRelasjoner("10987654321") } returns søker
 
@@ -74,7 +88,10 @@ class Integrasjonstest(
     @Test
     fun `Skal ikke passere vilkårsvurdering dersom barn ikke bor med mor`() {
         val barn = genererAutomatiskTestperson(LocalDate.now(), emptySet(), emptyList())
-        val søker = genererAutomatiskTestperson(LocalDate.parse("1998-10-10"), setOf("12345678910"), emptyList())
+        val søker = genererAutomatiskTestperson(LocalDate.parse("1998-10-10"),
+                                                setOf(ForelderBarnRelasjon(Personident("11211211211"),
+                                                                           FORELDERBARNRELASJONROLLE.BARN)),
+                                                emptyList())
         every { personopplysningerService.hentPersoninfoMedRelasjoner("11211211211") } returns barn
         every { personopplysningerService.hentPersoninfoMedRelasjoner("10987654321") } returns søker
 
@@ -83,5 +100,33 @@ class Integrasjonstest(
         val behandlingEtterVilkår = stegService.håndterVilkårsvurdering(behandlingFørVilkår)
         val personopplysningsgrunnlag = personopplysningGrunnlagRepository.findByBehandlingAndAktiv(behandlingEtterVilkår.id)
         Assertions.assertEquals(BehandlingResultat.AVSLÅTT, behandlingEtterVilkår.resultat)
+    }
+
+    @Test
+    fun `skal velge BAsak når mor har løpende sak i BA sak`() {
+
+        val fagsak = fagSakService.hentEllerOpprettFagsak(PersonIdent("10987654321"), true)
+        fagSakService.oppdaterStatus(fagsak, FagsakStatus.LØPENDE)
+
+        val nyBehandling = NyBehandlingHendelse("10987654321", listOf("11211211211"))
+
+        /*stegService.håndterVilkårsvurdering(eksisterendeBehandling)
+        stegService.håndterJournalførVedtaksbrev(eksisterendeBehandling)
+        stegService.håndterFerdigstillBehandling(eksisterendeBehandling)*/
+
+        assertEquals(velgFagSystemService.velgFagsystem(nyBehandling), FagsystemRegelVurdering.SEND_TIL_BA)
+    }
+
+    @Test
+    fun `skal velge Infotrygd når mor ikke har løpende sak i BA sak`() {
+
+        fagSakService.hentEllerOpprettFagsak(PersonIdent("10987654321"), true)
+        val nyBehandling = NyBehandlingHendelse("10987654321", listOf("11211211211"))
+
+        /*stegService.håndterVilkårsvurdering(eksisterendeBehandling)
+        stegService.håndterJournalførVedtaksbrev(eksisterendeBehandling)
+        stegService.håndterFerdigstillBehandling(eksisterendeBehandling)*/
+
+        assertEquals(velgFagSystemService.velgFagsystem(nyBehandling), FagsystemRegelVurdering.SEND_TIL_INFOTRYGD)
     }
 }
