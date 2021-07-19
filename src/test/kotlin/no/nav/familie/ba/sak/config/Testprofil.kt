2f2ba360f005b60cf672464a86d38ed191df5441
mundal <halvorbmundal@gmail.com>
2021-07-19 15:20:37 +0200

    Legg til "mock-brev-klient",               "mock-infotrygd-feed",               og "mock-arbeidsfordeling" i AbstractSpringIntegrationTest

--- src/test/kotlin/no/nav/familie/ba/sak/config/Testprofil.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/config/Testprofil.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.ba.sak.config
 
 import no.nav.familie.ba.sak.common.DbContainerInitializer
+import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.TestInstance
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
@@ -15,8 +16,12 @@ import org.springframework.test.context.ContextConfiguration
     "mock-task-repository",
     "mock-infotrygd-barnetrygd",
     "mock-tilbakekreving-klient",
+    "mock-brev-klient",
+    "mock-infotrygd-feed",
+    "mock-arbeidsfordeling",
 )
 @ContextConfiguration(initializers = [DbContainerInitializer::class])
 @AutoConfigureWireMock(port = 28085)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Tag("integration")
 abstract class AbstractSpringIntegrationTest
