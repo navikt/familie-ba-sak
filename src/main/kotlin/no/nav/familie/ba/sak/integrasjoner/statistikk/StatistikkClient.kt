4b48ca514e173a3fb1b960126ee7d5faa19f7ca4
Stig Helge Strøm <stig.helge.strom@nav.no>
2021-08-05 13:27:25 +0200

    Tester for klient

--- src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/statistikk/StatistikkClient.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/statistikk/StatistikkClient.kt
@@ -1,9 +1,14 @@
 package no.nav.familie.ba.sak.integrasjoner.statistikk
 
 import no.nav.familie.http.client.AbstractRestClient
+import no.nav.familie.log.NavHttpHeaders
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Service
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestOperations
 import java.net.URI
 
@@ -16,13 +21,30 @@ class StatistikkClient(@Value("\${FAMILIE_STATISTIKK_URL}") val baseUri: URI,
     fun hentSakStatistikk(offset: Long): String {
         val uri = URI.create("$baseUri/statistikk/sak/$offset")
 
-        return getForEntity(uri)
+        return try {
+            getForEntity(uri, httpHeaders())
+        } catch (e: Exception) {
+            if (e is HttpClientErrorException) {
+                logger.error("Kall mot statistikk feilet: httpkode: ${e.statusCode}, body ${e.responseBodyAsString} ", e)
+            }
+            throw e
+        }
     }
 
     fun hentBehandlingStatistikk(offset: Long): String {
         val uri = URI.create("$baseUri/statistikk/behandling/$offset")
 
-        return getForEntity(uri)
+        return getForEntity(uri, httpHeaders())
+    }
+
+    private fun httpHeaders(): HttpHeaders {
+        return HttpHeaders().apply {
+            add(HttpHeaders.CONTENT_TYPE, "application/json")
+        }
+    }
+
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(StatistikkClient::class.java)
     }
 
 }
