071a8ba7edc967e8f06d11c75a2396254b2da8a6
Meng Zhu <meng.zhu@nav.no>
2021-09-01 14:38:32 +0200

    use RelationalMappingContext to find jdbc tables

--- src/main/kotlin/no/nav/familie/ba/sak/config/e2e/DatabaseCleanupService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/e2e/DatabaseCleanupService.kt
@@ -1,10 +1,10 @@
 package no.nav.familie.ba.sak.config.e2e
 
-import org.reflections.Reflections
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.InitializingBean
 import org.springframework.context.annotation.Profile
 import org.springframework.core.env.Environment
+import org.springframework.data.relational.core.mapping.RelationalMappingContext
+import org.springframework.data.relational.core.sql.IdentifierProcessing
 import org.springframework.stereotype.Service
 import javax.persistence.EntityManager
 import javax.persistence.Table
@@ -22,36 +22,39 @@ import org.springframework.data.relational.core.mapping.Table as JdbcTable
 @Service
 @Profile("dev", "e2e", "postgres")
 class DatabaseCleanupService(
-        private val entityManager: EntityManager,
-        private val environment: Environment
-) : InitializingBean {
+    private val entityManager: EntityManager,
+    private val environment: Environment,
+    private val relationalMappingContext: RelationalMappingContext
+){
 
     private val logger = LoggerFactory.getLogger(DatabaseCleanupService::class.java)
-    private lateinit var tableNames: List<String>
 
-    /**
-     * Uses the JPA metamodel to find all managed types then try to get the [Table] annotation's from each (if present) to discover the table name.
-     * If the [Table] annotation is not defined then we skip that entity (oops :p)
-     */
-    override fun afterPropertiesSet() {
-        val metaModel: Metamodel = entityManager.metamodel
-        tableNames = metaModel.managedTypes
-                             .filter {
-                                 it.javaType.kotlin.findAnnotation<Table>() != null || it.javaType.kotlin.findAnnotation<JdbcTable>() != null
-                             }
-                             .map {
-                                 val tableAnnotation: Table? = it.javaType.kotlin.findAnnotation()
-                                 val jdbcTableAnnotation: JdbcTable? = it.javaType.kotlin.findAnnotation()
-                                 tableAnnotation?.name ?: jdbcTableAnnotation?.value
-                                 ?: throw IllegalStateException("should never get here")
-                             } + getJdbcTableNames()
-
-    }
+    private var tableNames: List<String>? = null
+        /**
+         * Uses the JPA metamodel to find all managed types then try to get the [Table] annotation's from each (if present) to discover the table name.
+         * If the [Table] annotation is not defined then we skip that entity (oops :p)
+         * JDBC tables must be found out in another way
+         */
+        get() {
+            if (field == null) {
+                val metaModel: Metamodel = entityManager.metamodel
+                field = metaModel.managedTypes
+                    .filter {
+                        it.javaType.kotlin.findAnnotation<Table>() != null || it.javaType.kotlin.findAnnotation<JdbcTable>() != null
+                    }
+                    .map {
+                        val tableAnnotation: Table? = it.javaType.kotlin.findAnnotation()
+                        val jdbcTableAnnotation: JdbcTable? = it.javaType.kotlin.findAnnotation()
+                        tableAnnotation?.name ?: jdbcTableAnnotation?.value
+                        ?: throw IllegalStateException("should never get here")
+                    } + getJdbcTableNames()
+            }
+            return field
+        }
 
     private fun getJdbcTableNames(): List<String> {
-        val ref = Reflections("no.nav.familie")
-        return ref.getTypesAnnotatedWith(JdbcTable::class.java).map {
-            it.getAnnotation(JdbcTable::class.java).value
+        return relationalMappingContext.persistentEntities.map { entity ->
+            entity.tableName.toSql(IdentifierProcessing.NONE)
         }
     }
 
@@ -63,12 +66,12 @@ class DatabaseCleanupService(
         logger.info("Truncating tables: $tableNames")
         entityManager.flush()
         if (environment.activeProfiles.contains("e2e") || environment.activeProfiles.contains("postgres")) {
-            tableNames.forEach { tableName ->
+            tableNames?.forEach { tableName ->
                 entityManager.createNativeQuery("TRUNCATE TABLE $tableName CASCADE").executeUpdate()
             }
         } else {
             entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TO FALSE").executeUpdate()
-            tableNames.forEach { tableName ->
+            tableNames?.forEach { tableName ->
                 entityManager.createNativeQuery("TRUNCATE TABLE $tableName").executeUpdate()
             }
             entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TO TRUE").executeUpdate()
