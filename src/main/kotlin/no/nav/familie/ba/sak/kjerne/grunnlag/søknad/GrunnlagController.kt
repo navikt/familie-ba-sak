af225f454201f2b5eb2e2ee6d5b469fde959f23a
Nina Kjekstad <ninakjek@users.noreply.github.com>
2021-09-06 14:51:54 +0200

    Feature/legg til barn (#1385)
    * Oppdater toggles
    
    * Legg til funksjonalitet for å legge til barn
    
    * Oppdater logg
    
    * Oppdater request
    
    * Oppdater test for oppdatering av søknadsgrunnalg

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/grunnlag/søknad/GrunnlagController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/grunnlag/søknad/GrunnlagController.kt
@@ -4,7 +4,9 @@ import no.nav.familie.ba.sak.kjerne.behandling.BehandlingService
 import no.nav.familie.ba.sak.kjerne.fagsak.FagsakService
 import no.nav.familie.ba.sak.ekstern.restDomene.RestFagsak
 import no.nav.familie.ba.sak.ekstern.restDomene.RestRegistrerSøknad
+import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersongrunnlagService
 import no.nav.familie.ba.sak.kjerne.steg.StegService
+import no.nav.familie.ba.sak.kjerne.steg.TilbakestillBehandlingService
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.MediaType
@@ -14,9 +16,11 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/api/behandlinger")
 @ProtectedWithClaims(issuer = "azuread")
-class SøknadGrunnlagController(
+class GrunnlagController(
         private val fagsakService: FagsakService,
         private val behandlingService: BehandlingService,
+        private val persongrunnlagService: PersongrunnlagService,
+        private val tilbakestillService: TilbakestillBehandlingService,
         private val stegService: StegService
 ) {
 
@@ -27,14 +31,21 @@ class SøknadGrunnlagController(
                                                restRegistrerSøknad: RestRegistrerSøknad): ResponseEntity<Ressurs<RestFagsak>> {
         val behandling = behandlingService.hent(behandlingId = behandlingId)
 
-        return Result.runCatching {
-            stegService.håndterSøknad(behandling = behandling, restRegistrerSøknad = restRegistrerSøknad)
-        }
-                .fold(
-                        onSuccess = { ResponseEntity.ok(fagsakService.hentRestFagsak(behandling.fagsak.id)) },
-                        onFailure = {
-                            throw it
-                        }
-                )
+        stegService.håndterSøknad(behandling = behandling, restRegistrerSøknad = restRegistrerSøknad)
+        return ResponseEntity.ok(fagsakService.hentRestFagsak(behandling.fagsak.id))
     }
+
+    @PostMapping(path = ["/{behandlingId}/legg-til-barn"])
+    fun leggTilBarnIPersonopplysningsgrunnlag(@PathVariable behandlingId: Long,
+                                              @RequestBody
+                                              leggTilBarnDto: LeggTilBarnDto): ResponseEntity<Ressurs<RestFagsak>> {
+        val behandling = behandlingService.hent(behandlingId = behandlingId)
+        persongrunnlagService.leggTilBarnIPersonopplysningsgrunnlag(behandling = behandling,
+                                                                    nyttBarnIdent = leggTilBarnDto.barnIdent)
+        tilbakestillService.initierOgSettBehandlingTilVilårsvurdering(behandling)
+        return ResponseEntity.ok(fagsakService.hentRestFagsak(behandling.fagsak.id))
+    }
+
+    class LeggTilBarnDto(val barnIdent: String)
+
 }
