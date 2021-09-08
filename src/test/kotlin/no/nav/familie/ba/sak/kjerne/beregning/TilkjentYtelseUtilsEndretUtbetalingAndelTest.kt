4d84a17514ebed0a9ac7594491c45e22f3a250e7
Marcus Gustafson <marcus.gustafson@computas.com>
2021-09-08 11:29:11 +0200

    WIP

--- src/test/kotlin/no/nav/familie/ba/sak/kjerne/beregning/TilkjentYtelseUtilsEndretUtbetalingAndelTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/kjerne/beregning/TilkjentYtelseUtilsEndretUtbetalingAndelTest.kt
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.math.BigDecimal
+import java.math.BigInteger
 import java.time.LocalDate
 import java.time.YearMonth
 
@@ -21,8 +22,8 @@ internal class TilkjentYtelseUtilsEndretUtbetalingAndelTest {
 
     val behandling = lagBehandling()
     val tilkjentYtelse =
-            TilkjentYtelse(behandling = behandling, endretDato = LocalDate.now(), opprettetDato = LocalDate.now())
-    val beløp = 100
+        TilkjentYtelse(behandling = behandling, endretDato = LocalDate.now(), opprettetDato = LocalDate.now())
+    val beløp = BigDecimal(100)
 
     val barn1 = tilfeldigPerson(personType = PersonType.BARN)
     val barn2 = tilfeldigPerson(personType = PersonType.BARN)
@@ -36,118 +37,190 @@ internal class TilkjentYtelseUtilsEndretUtbetalingAndelTest {
     fun `teste nye andeler tilkjent ytelse for to barn med endrete utbetalingsandeler`() {
 
         val andelTilkjentytelseForBarn1 = listOf(
-                MånedPeriode(YearMonth.of(2018, 1), YearMonth.of(2018, 4)),
-                MånedPeriode(YearMonth.of(2018, 10), YearMonth.of(2021, 8)))
-                .map {
-                    lagAndelTilkjentYtelse(barn1, it.fom, it.tom)
-                }
+            MånedPeriode(YearMonth.of(2018, 1), YearMonth.of(2018, 4)),
+            MånedPeriode(YearMonth.of(2018, 10), YearMonth.of(2021, 8))
+        )
+            .map {
+                lagAndelTilkjentYtelse(barn1, it.fom, it.tom)
+            }
 
         val andelTilkjentytelseForBarn2 = listOf(
-                MånedPeriode(YearMonth.of(2018, 2), YearMonth.of(2018, 8)),
-                MånedPeriode(YearMonth.of(2018, 11), YearMonth.of(2021, 9)))
-                .map {
-                    lagAndelTilkjentYtelse(barn2, it.fom, it.tom)
-                }
+            MånedPeriode(YearMonth.of(2018, 2), YearMonth.of(2018, 8)),
+            MånedPeriode(YearMonth.of(2018, 11), YearMonth.of(2021, 9))
+        )
+            .map {
+                lagAndelTilkjentYtelse(barn2, it.fom, it.tom)
+            }
 
         val endretUtbetalingerForBarn1 = listOf(
-                MånedPeriode(YearMonth.of(2015, 1), YearMonth.of(2018, 3)))
-                .map {
-                    lagEndretUtbetalingAndel(barn1, it.fom, it.tom, 50)
-                }
+            MånedPeriode(YearMonth.of(2015, 1), YearMonth.of(2018, 3))
+        )
+            .map {
+                lagEndretUtbetalingAndel(barn1, it.fom, it.tom, 50)
+            }
 
         val endretUtbetalingerForBarn2 = listOf(
-                MånedPeriode(YearMonth.of(2018, 4), YearMonth.of(2018, 5)),
-                MånedPeriode(YearMonth.of(2018, 7), YearMonth.of(2021, 9)))
-                .map {
-                    lagEndretUtbetalingAndel(barn2, it.fom, it.tom, 50)
-                }
+            MånedPeriode(YearMonth.of(2018, 4), YearMonth.of(2018, 5)),
+            MånedPeriode(YearMonth.of(2018, 7), YearMonth.of(2021, 9))
+        )
+            .map {
+                lagEndretUtbetalingAndel(barn2, it.fom, it.tom, 50)
+            }
 
         val andelerTilkjentYtelserEtterEUA =
-                TilkjentYtelseUtils.oppdaterTilkjentYtelseMedEndretUtbetalingAndeler(
-                        andelTilkjentytelseForBarn1 + andelTilkjentytelseForBarn2,
-                        endretUtbetalingerForBarn1 + endretUtbetalingerForBarn2)
+            TilkjentYtelseUtils.oppdaterTilkjentYtelseMedEndretUtbetalingAndeler(
+                andelTilkjentytelseForBarn1 + andelTilkjentytelseForBarn2,
+                endretUtbetalingerForBarn1 + endretUtbetalingerForBarn2
+            )
 
-        val andelerTilkjentYtelserEtterEUA2 =
-                TilkjentYtelseUtils.oppdaterTilkjentYtelseMedEndretUtbetalingAndeler2(
-                        andelTilkjentytelseForBarn1 + andelTilkjentytelseForBarn2,
-                        endretUtbetalingerForBarn1 + endretUtbetalingerForBarn2)
-
-        assertEquals(andelerTilkjentYtelserEtterEUA, andelerTilkjentYtelserEtterEUA2)
         assertEquals(8, andelerTilkjentYtelserEtterEUA.size)
+
+        verifiserAndelTilkjentYtelse(
+            andelerTilkjentYtelserEtterEUA[0],
+            barn1.personIdent.ident,
+            beløp/ BigDecimal(2),
+            YearMonth.of(2018, 1),
+            YearMonth.of(2018, 3)
+        )
+
+        verifiserAndelTilkjentYtelse(
+            andelerTilkjentYtelserEtterEUA[1],
+            barn1.personIdent.ident,
+            beløp,
+            YearMonth.of(2018, 4),
+            YearMonth.of(2018, 4)
+        )
+
+        verifiserAndelTilkjentYtelse(
+            andelerTilkjentYtelserEtterEUA[2],
+            barn1.personIdent.ident,
+            beløp,
+            YearMonth.of(2018, 10),
+            YearMonth.of(2021, 8)
+        )
+
+        verifiserAndelTilkjentYtelse(
+            andelerTilkjentYtelserEtterEUA[3],
+            barn2.personIdent.ident,
+            beløp,
+            YearMonth.of(2018, 2),
+            YearMonth.of(2018, 3)
+        )
+
+        verifiserAndelTilkjentYtelse(
+            andelerTilkjentYtelserEtterEUA[4],
+            barn2.personIdent.ident,
+            beløp/ BigDecimal(2),
+            YearMonth.of(2018, 4),
+            YearMonth.of(2018, 5)
+        )
+
+        verifiserAndelTilkjentYtelse(
+            andelerTilkjentYtelserEtterEUA[5],
+            barn2.personIdent.ident,
+            beløp,
+            YearMonth.of(2018, 6),
+            YearMonth.of(2018, 6)
+        )
+
+        verifiserAndelTilkjentYtelse(
+            andelerTilkjentYtelserEtterEUA[6],
+            barn2.personIdent.ident,
+            beløp/BigDecimal(2),
+            YearMonth.of(2018, 7),
+            YearMonth.of(2018, 8)
+        )
+
+        verifiserAndelTilkjentYtelse(
+            andelerTilkjentYtelserEtterEUA[7],
+            barn2.personIdent.ident,
+            beløp/BigDecimal(2),
+            YearMonth.of(2018, 11),
+            YearMonth.of(2021, 9)
+        )
     }
 
     @Test
     fun `Ba`() {
 
         val andelerTilkjentYtelser = listOf(
-                AndelTilkjentYtelse(
-                        behandlingId = behandling.id,
-                        tilkjentYtelse = tilkjentYtelse,
-                        personIdent = barn1.personIdent.ident,
-                        beløp = beløp,
-                        stønadFom = YearMonth.of(2021, 6),
-                        stønadTom = YearMonth.of(2021, 8),
-                        type = YtelseType.ORDINÆR_BARNETRYGD
-                ),
-                AndelTilkjentYtelse(
-                        behandlingId = behandling.id,
-                        tilkjentYtelse = tilkjentYtelse,
-                        personIdent = barn1.personIdent.ident,
-                        beløp = beløp,
-                        stønadFom = YearMonth.of(2021, 9),
-                        stønadTom = YearMonth.of(2021, 11),
-                        type = YtelseType.ORDINÆR_BARNETRYGD
-                )
+            AndelTilkjentYtelse(
+                behandlingId = behandling.id,
+                tilkjentYtelse = tilkjentYtelse,
+                personIdent = barn1.personIdent.ident,
+                beløp = beløp.toInt(),
+                stønadFom = YearMonth.of(2021, 6),
+                stønadTom = YearMonth.of(2021, 8),
+                type = YtelseType.ORDINÆR_BARNETRYGD
+            ),
+            AndelTilkjentYtelse(
+                behandlingId = behandling.id,
+                tilkjentYtelse = tilkjentYtelse,
+                personIdent = barn1.personIdent.ident,
+                beløp = beløp.toInt(),
+                stønadFom = YearMonth.of(2021, 9),
+                stønadTom = YearMonth.of(2021, 11),
+                type = YtelseType.ORDINÆR_BARNETRYGD
+            )
         )
 
         val endretUtbetalinger = listOf(
-                EndretUtbetalingAndel(
-                        behandlingId = behandling.id,
-                        person = barn1,
-                        prosent = BigDecimal(50),
-                        fom = YearMonth.of(2021, 6),
-                        tom = YearMonth.of(2021, 7),
-                        årsak = Årsak.DELT_BOSTED,
-                        begrunnelse = "Halv utbetaling"
-                ),
-                EndretUtbetalingAndel(
-                        behandlingId = behandling.id,
-                        person = barn1,
-                        prosent = BigDecimal(0),
-                        fom = YearMonth.of(2021, 9),
-                        tom = YearMonth.of(2021, 10),
-                        årsak = Årsak.DELT_BOSTED,
-                        begrunnelse = "Halv utbetaling"
-                ),
+            EndretUtbetalingAndel(
+                behandlingId = behandling.id,
+                person = barn1,
+                prosent = BigDecimal(2),
+                fom = YearMonth.of(2021, 6),
+                tom = YearMonth.of(2021, 7),
+                årsak = Årsak.DELT_BOSTED,
+                begrunnelse = "Halv utbetaling"
+            ),
+            EndretUtbetalingAndel(
+                behandlingId = behandling.id,
+                person = barn1,
+                prosent = BigDecimal(0),
+                fom = YearMonth.of(2021, 9),
+                tom = YearMonth.of(2021, 10),
+                årsak = Årsak.DELT_BOSTED,
+                begrunnelse = "Halv utbetaling"
+            ),
         )
 
         val andelerTilkjentYtelserEtterEUA =
-                TilkjentYtelseUtils.oppdaterTilkjentYtelseMedEndretUtbetalingAndeler(andelerTilkjentYtelser, endretUtbetalinger)
+            TilkjentYtelseUtils.oppdaterTilkjentYtelseMedEndretUtbetalingAndeler(andelerTilkjentYtelser, endretUtbetalinger)
 
-        val andelerTilkjentYtelserEtterEUA2 =
-                TilkjentYtelseUtils.oppdaterTilkjentYtelseMedEndretUtbetalingAndeler2(andelerTilkjentYtelser, endretUtbetalinger)
-
-        assertEquals(andelerTilkjentYtelserEtterEUA, andelerTilkjentYtelserEtterEUA2)
         assertEquals(4, andelerTilkjentYtelserEtterEUA.size)
     }
 
+    private fun verifiserAndelTilkjentYtelse(
+        andelTilkjentYtelse: AndelTilkjentYtelse,
+        forventetBarnIdent: String,
+        forventetBeløp: BigDecimal,
+        forventetStønadFom: YearMonth,
+        forventetStønadTom: YearMonth
+    ) {
+        assertEquals(forventetBarnIdent, andelTilkjentYtelse.personIdent)
+        assertEquals(forventetBeløp, BigDecimal(andelTilkjentYtelse.beløp))
+        assertEquals(forventetStønadFom, andelTilkjentYtelse.stønadFom)
+        assertEquals(forventetStønadTom, andelTilkjentYtelse.stønadTom)
+    }
+
     private fun lagAndelTilkjentYtelse(barn: Person, fom: YearMonth, tom: YearMonth) = AndelTilkjentYtelse(
-            behandlingId = behandling.id,
-            tilkjentYtelse = tilkjentYtelse,
-            personIdent = barn.personIdent.ident,
-            beløp = beløp,
-            stønadFom = fom,
-            stønadTom = tom,
-            type = YtelseType.ORDINÆR_BARNETRYGD
+        behandlingId = behandling.id,
+        tilkjentYtelse = tilkjentYtelse,
+        personIdent = barn.personIdent.ident,
+        beløp = beløp.toInt(),
+        stønadFom = fom,
+        stønadTom = tom,
+        type = YtelseType.ORDINÆR_BARNETRYGD
     )
 
     private fun lagEndretUtbetalingAndel(barn: Person, fom: YearMonth, tom: YearMonth, prosent: Int) = EndretUtbetalingAndel(
-            behandlingId = behandling.id,
-            person = barn,
-            prosent = BigDecimal(prosent),
-            fom = fom,
-            tom = tom,
-            årsak = Årsak.DELT_BOSTED,
-            begrunnelse = "Begrunnelse for endring"
+        behandlingId = behandling.id,
+        person = barn,
+        prosent = BigDecimal(prosent),
+        fom = fom,
+        tom = tom,
+        årsak = Årsak.DELT_BOSTED,
+        begrunnelse = "Begrunnelse for endring"
     )
 }
