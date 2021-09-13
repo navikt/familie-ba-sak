cddafedbdf79e34dddcea699a40500b178a4580b
Nina Kjekstad <nina.kjekstad@nav.no>
2021-09-13 18:17:01 +0200

    Lagre data underveis for debugging

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vilkårsvurdering/domene/Vilkårsvurdering.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vilkårsvurdering/domene/Vilkårsvurdering.kt
@@ -46,6 +46,10 @@ data class Vilkårsvurdering(
         )
         var personResultater: Set<PersonResultat> = setOf(),
 
+        @Column(name = "ytelsePersoner", columnDefinition = "text")
+        var ytelsePersoner: String? = null,
+
+
         ) : BaseEntitet() {
 
     override fun toString(): String {
