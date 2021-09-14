92655ca1138da8790b21e1134a267278bdcdb001
Meng Zhu <meng.zhu@nav.no>
2021-09-15 00:07:34 +0200

    revert debug

--- src/main/kotlin/no/nav/familie/ba/sak/Application.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/Application.kt
@@ -2,20 +2,12 @@ package no.nav.familie.ba.sak
 
 import no.nav.familie.ba.sak.config.ApplicationConfig
 import no.nav.familie.ba.sak.config.DelayedShutdownHook
-import no.nav.familie.prosessering.internal.TaskScheduler
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.ConfigurableApplicationContext
-import org.springframework.scheduling.annotation.Scheduled
 
 @SpringBootApplication
-class FamilieBaSakApplication(val taskScheduler: TaskScheduler){
-
-    @Scheduled(fixedDelayString = "3000000")
-    fun testTaskMaintenance(){
-        taskScheduler.settPermanentPlukketTilKlarTilPlukk()
-    }
-}
+class FamilieBaSakApplication
 
 fun main(args: Array<String>) {
     val app = SpringApplication(ApplicationConfig::class.java)
