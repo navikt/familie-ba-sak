f72b147c464aa148d67c292c14373dd954eb7928
Meng Zhu <meng.zhu@nav.no>
2021-09-23 15:48:09 +0200

    Fix warning from spring jdbc (#1431)
    * only scan prosessering for jdbc
    
    * do not scan jdbc repository for jpa
    
    Co-authored-by: Henning Håkonsen <henning.hakonsen@nav.no>

--- src/main/kotlin/no/nav/familie/ba/sak/config/DatabaseConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/DatabaseConfig.kt
@@ -2,16 +2,23 @@ package no.nav.familie.ba.sak.config
 
 import no.nav.familie.prosessering.PropertiesWrapperTilStringConverter
 import no.nav.familie.prosessering.StringTilPropertiesWrapperConverter
+import no.nav.familie.prosessering.domene.TaskRepository
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.FilterType
 import org.springframework.data.jdbc.core.convert.JdbcCustomConversions
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @Configuration
 @EnableJdbcAuditing
-@EnableJdbcRepositories("no.nav.familie")
+@EnableJdbcRepositories("no.nav.familie.prosessering")
+@EnableJpaRepositories("no.nav.familie", excludeFilters = arrayOf(ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = arrayOf(TaskRepository::class))))
+@EnableJpaAuditing
 class DatabaseConfig : AbstractJdbcConfiguration() {
     @Bean
     override fun jdbcCustomConversions(): JdbcCustomConversions {
