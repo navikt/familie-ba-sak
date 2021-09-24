68794a472e814f7d1660510d6a54e08178ac5211
Marcus Gustafson <marcus.gustafson@computas.com>
2021-09-24 10:41:24 +0200

    Oppdaterte skript 157

--- src/main/resources/db/migration/V157__fagsak_legg_til_unique_index_på_arkivert.sql
+++ src/main/resources/db/migration/V157__fagsak_legg_til_unique_index_på_arkivert.sql
@@ -1,7 +1,2 @@
-UPDATE fagsak_person
-SET arkivert = TRUE
-WHERE fk_fagsak_id = 1078652;
-
-UPDATE fagsak
-SET arkivert = TRUE
-WHERE id = 1078652;
+CREATE UNIQUE INDEX uidx_fagsak_person_ident_ikke_arkivert ON fagsak_person(ident)
+    WHERE arkivert = false;
