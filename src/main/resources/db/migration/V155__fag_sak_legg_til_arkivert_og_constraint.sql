e8d325c1adbacaf5ff036c37e2c75704285f088f
Marcus Gustafson <marcus.gustafson@computas.com>
2021-09-24 09:55:32 +0200

    Flytte index creating til skript 156

--- src/main/resources/db/migration/V155__fag_sak_legg_til_arkivert_og_constraint.sql
+++ src/main/resources/db/migration/V155__fag_sak_legg_til_arkivert_og_constraint.sql
@@ -3,6 +3,3 @@ ALTER TABLE fagsak_person
 
 ALTER TABLE fagsak
     ADD COLUMN arkivert     BOOLEAN      DEFAULT FALSE       NOT NULL;
-
-CREATE UNIQUE INDEX uidx_fagsak_person_ident_ikke_arkivert ON fagsak_person(ident)
-    WHERE arkivert = false;
--- src/main/resources/db/migration/V156__arkiver_feilende_fag_sak_i_prod.sql
+++ src/main/resources/db/migration/V156__arkiver_feilende_fag_sak_i_prod.sql
@@ -1,3 +1,6 @@
+CREATE UNIQUE INDEX uidx_fagsak_person_ident_ikke_arkivert ON fagsak_person(ident)
+    WHERE arkivert = false;
+
 UPDATE fagsak_person
 SET arkivert = TRUE
 WHERE fk_fagsak_id = 1078652;
