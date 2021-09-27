7b8f08cee53d0214ac3bbb452ed1d0dca8949e14
mundal <halvorbmundal@gmail.com>
2021-09-27 12:22:48 +0200

    opprett fraRestEndretUtbetalingAndel

--- src/main/kotlin/no/nav/familie/ba/sak/ekstern/restDomene/RestEndretUtbetalingAndel.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/ekstern/restDomene/RestEndretUtbetalingAndel.kt
@@ -1,6 +1,5 @@
 package no.nav.familie.ba.sak.ekstern.restDomene
 
-import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.EndretUtbetalingAndel
 import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.Årsak
 import java.math.BigDecimal
 import java.time.YearMonth
@@ -13,14 +12,4 @@ class RestEndretUtbetalingAndel(
     val tom: YearMonth?,
     val årsak: Årsak?,
     val begrunnelse: String?,
-)
-
-fun EndretUtbetalingAndel.tilRestEndretUtbetalingAndel() = RestEndretUtbetalingAndel(
-        id = this.id,
-        personIdent = this.person?.personIdent?.ident,
-        prosent = this.prosent,
-        fom = this.fom,
-        tom = this.tom,
-        årsak = this.årsak,
-        begrunnelse = this.begrunnelse
 )
--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/endretutbetaling/EndretUtbetalingAndelService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/endretutbetaling/EndretUtbetalingAndelService.kt
@@ -6,6 +6,8 @@ import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandling
 import no.nav.familie.ba.sak.kjerne.beregning.BeregningService
 import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.EndretUtbetalingAndel
 import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.EndretUtbetalingAndelRepository
+import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.fraRestEndretUtbetalingAndel
+import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersongrunnlagService
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersonopplysningGrunnlagRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -15,22 +17,20 @@ class EndretUtbetalingAndelService(
     private val endretUtbetalingAndelRepository: EndretUtbetalingAndelRepository,
     private val personopplysningGrunnlagRepository: PersonopplysningGrunnlagRepository,
     private val beregningService: BeregningService,
+    private val persongrunnlagService: PersongrunnlagService,
 ) {
 
     @Transactional
     fun oppdaterEndretUtbetalingAndelOgOppdaterTilkjentYtelse(
-            behandling: Behandling,
-            endretUtbetalingAndelId: Long,
-            restEndretUtbetalingAndel: RestEndretUtbetalingAndel
+        behandling: Behandling,
+        endretUtbetalingAndelId: Long,
+        restEndretUtbetalingAndel: RestEndretUtbetalingAndel
     ) {
-       val endretUtbetalingAndel = endretUtbetalingAndelRepository.getById(endretUtbetalingAndelId)
+        val endretUtbetalingAndel = endretUtbetalingAndelRepository.getById(endretUtbetalingAndelId)
+        val person =
+            persongrunnlagService.hentPersonerPåBehandling(listOf(restEndretUtbetalingAndel.personIdent!!), behandling).first()
 
-        endretUtbetalingAndel.fom = restEndretUtbetalingAndel.fom
-        endretUtbetalingAndel.tom = restEndretUtbetalingAndel.tom
-        endretUtbetalingAndel.prosent = restEndretUtbetalingAndel.prosent
-        endretUtbetalingAndel.årsak = restEndretUtbetalingAndel.årsak
-
-        endretUtbetalingAndelRepository.save(endretUtbetalingAndel)
+        endretUtbetalingAndel.fraRestEndretUtbetalingAndel(restEndretUtbetalingAndel, person)
 
         val personopplysningGrunnlag = personopplysningGrunnlagRepository.findByBehandlingAndAktiv(behandlingId = behandling.id)
             ?: throw Feil("Fant ikke personopplysninggrunnlag på behandling ${behandling.id}")
@@ -40,10 +40,10 @@ class EndretUtbetalingAndelService(
 
     @Transactional
     fun fjernEndretUtbetalingAndelOgOppdaterTilkjentYtelse(
-            behandling: Behandling,
-            endretUtbetalingAndelId: Long,
+        behandling: Behandling,
+        endretUtbetalingAndelId: Long,
     ) {
-       endretUtbetalingAndelRepository.deleteById(endretUtbetalingAndelId)
+        endretUtbetalingAndelRepository.deleteById(endretUtbetalingAndelId)
 
         val personopplysningGrunnlag = personopplysningGrunnlagRepository.findByBehandlingAndAktiv(behandlingId = behandling.id)
             ?: throw Feil("Fant ikke personopplysninggrunnlag på behandling ${behandling.id}")
