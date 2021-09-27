0a372a9dceffb52f94b8e667dae50371b45aed00
Nina Kjekstad <ninakjek@users.noreply.github.com>
2021-09-27 15:25:29 +0200

    Feature/endringsperioder begrunnelser (#1461)
    * Formatering
    
    * Legg til begrunnelser på endring

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/endretutbetaling/domene/EndretUtbetalingAndel.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/endretutbetaling/domene/EndretUtbetalingAndel.kt
@@ -6,6 +6,8 @@ import no.nav.familie.ba.sak.common.YearMonthConverter
 import no.nav.familie.ba.sak.common.overlapperHeltEllerDelvisMed
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelse
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.Person
+import no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser.VedtakBegrunnelseSpesifikasjon
+import no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser.VedtakBegrunnelseSpesifikasjonListConverter
 import no.nav.familie.ba.sak.sikkerhet.RollestyringMotDatabase
 import java.math.BigDecimal
 import java.time.YearMonth
@@ -29,48 +31,53 @@ import javax.persistence.Table
 @Entity(name = "EndretUtbetalingAndel")
 @Table(name = "ENDRET_UTBETALING_ANDEL")
 data class EndretUtbetalingAndel(
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "endret_utbetaling_andel_seq_generator")
-    @SequenceGenerator(
-        name = "endret_utbetaling_andel_seq_generator",
-        sequenceName = "endret_utbetaling_andel_seq",
-        allocationSize = 50
-    )
-    val id: Long = 0,
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "endret_utbetaling_andel_seq_generator")
+        @SequenceGenerator(
+                name = "endret_utbetaling_andel_seq_generator",
+                sequenceName = "endret_utbetaling_andel_seq",
+                allocationSize = 50
+        )
+        val id: Long = 0,
 
-    @Column(name = "fk_behandling_id", nullable = false, updatable = false)
-    val behandlingId: Long,
+        @Column(name = "fk_behandling_id", nullable = false, updatable = false)
+        val behandlingId: Long,
 
-    @ManyToOne @JoinColumn(name = "fk_po_person_id", nullable = false)
-    val person: Person,
+        @ManyToOne @JoinColumn(name = "fk_po_person_id", nullable = false)
+        val person: Person,
 
-    @Column(name = "prosent", nullable = false)
-    val prosent: BigDecimal,
+        @Column(name = "prosent", nullable = false)
+        val prosent: BigDecimal,
 
-    @Column(name = "fom", nullable = false, columnDefinition = "DATE")
-    @Convert(converter = YearMonthConverter::class)
-    val fom: YearMonth,
+        @Column(name = "fom", nullable = false, columnDefinition = "DATE")
+        @Convert(converter = YearMonthConverter::class)
+        val fom: YearMonth,
 
-    @Column(name = "tom", nullable = false, columnDefinition = "DATE")
-    @Convert(converter = YearMonthConverter::class)
-    val tom: YearMonth,
+        @Column(name = "tom", nullable = false, columnDefinition = "DATE")
+        @Convert(converter = YearMonthConverter::class)
+        val tom: YearMonth,
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "aarsak", nullable = false)
-    val årsak: Årsak,
+        @Enumerated(EnumType.STRING)
+        @Column(name = "aarsak", nullable = false)
+        val årsak: Årsak,
 
-    @Column(name = "begrunnelse", nullable = false)
-    var begrunnelse: String,
+        @Column(name = "begrunnelse", nullable = false)
+        var begrunnelse: String,
 
-    @ManyToMany
-    @JoinTable(
-            name = "ANDEL_TIL_ENDRET_ANDEL",
-            joinColumns = [JoinColumn(name = "fk_endret_utbetaling_andel_id")] ,
-            inverseJoinColumns = [JoinColumn(name = "fk_andel_tilkjent_ytelse_id")]
-    )
-    val andelTilkjentYtelser: List<AndelTilkjentYtelse> = emptyList(),
+        @ManyToMany
+        @JoinTable(
+                name = "ANDEL_TIL_ENDRET_ANDEL",
+                joinColumns = [JoinColumn(name = "fk_endret_utbetaling_andel_id")],
+                inverseJoinColumns = [JoinColumn(name = "fk_andel_tilkjent_ytelse_id")]
+        )
+        val andelTilkjentYtelser: List<AndelTilkjentYtelse> = emptyList(),
 
-) : BaseEntitet() {
+        @Column(name = "vedtak_begrunnelse_spesifikasjoner")
+        @Convert(converter = VedtakBegrunnelseSpesifikasjonListConverter::class)
+        var vedtakBegrunnelseSpesifikasjoner: List<VedtakBegrunnelseSpesifikasjon> = emptyList(),
+
+        ) : BaseEntitet() {
+
     fun overlapperMed(periode: MånedPeriode) = periode.overlapperHeltEllerDelvisMed(this.periode())
 
     fun periode() = MånedPeriode(this.fom, this.tom)
