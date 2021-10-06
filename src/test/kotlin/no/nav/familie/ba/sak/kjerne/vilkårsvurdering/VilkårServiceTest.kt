a3effe6a3c037b6898f8497668c63ff045d2b699
Henning Håkonsen <henning.hakonsen@nav.no>
2021-10-06 10:27:43 +0200

    Prøver å fjerne auto saving og heller bruke IntelliJ format on save funksjon. (#1503)

--- src/test/kotlin/no/nav/familie/ba/sak/kjerne/vilkårsvurdering/VilkårServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/kjerne/vilkårsvurdering/VilkårServiceTest.kt
@@ -36,11 +36,11 @@ import javax.sql.DataSource
 
 @SpringBootTest
 @ActiveProfiles(
-        "dev",
-        "mock-pdl",
-        "mock-økonomi",
-        "mock-infotrygd-barnetrygd",
-        "mock-brev-klient",
+    "dev",
+    "mock-pdl",
+    "mock-økonomi",
+    "mock-infotrygd-barnetrygd",
+    "mock-brev-klient",
 )
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class VilkårServiceTest(
