929f914fb70cf788a20cc184d765a0a08da6f06a
Øyvind Hagen <oyvind.hagen@gmail.com>
2021-10-11 14:49:02 +0200

    Lagt til riktig håndtering av satsendring slik at automatisk behandli… (#1526)
    * Lagt til riktig håndtering av satsendring slik at automatisk behandling går igjennom.
    
    * Setter tidspunkt for scheduler.

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandling/domene/Behandling.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandling/domene/Behandling.kt
@@ -130,6 +130,7 @@ data class Behandling(
         return when {
             skalBehandlesAutomatisk && erOmregning() && resultat == BehandlingResultat.FORTSATT_INNVILGET -> true
             skalBehandlesAutomatisk && resultat == BehandlingResultat.INNVILGET -> true
+            skalBehandlesAutomatisk && erSatsendring() && resultat == BehandlingResultat.ENDRET -> true
             else -> false
         }
     }
@@ -171,13 +172,15 @@ data class Behandling(
         return this
     }
 
-    fun erKlage(): Boolean = this.opprettetÅrsak == BehandlingÅrsak.KLAGE
+    fun erKlage() = this.opprettetÅrsak == BehandlingÅrsak.KLAGE
 
     fun erMigrering() =
         type == BehandlingType.MIGRERING_FRA_INFOTRYGD || type == BehandlingType.MIGRERING_FRA_INFOTRYGD_OPPHØRT
 
-    fun erOmregning(): Boolean =
+    fun erOmregning() =
         this.opprettetÅrsak == BehandlingÅrsak.OMREGNING_6ÅR || this.opprettetÅrsak == BehandlingÅrsak.OMREGNING_18ÅR
+
+    fun erSatsendring() = this.opprettetÅrsak == BehandlingÅrsak.SATSENDRING
 
     fun hentYtelseTypeTilVilkår(): YtelseType = when (underkategori) {
         BehandlingUnderkategori.UTVIDET -> YtelseType.UTVIDET_BARNETRYGD
