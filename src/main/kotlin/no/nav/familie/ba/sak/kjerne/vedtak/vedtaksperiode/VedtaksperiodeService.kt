eee40cb2c55f9675fd3bccf3180b41b11209a884
Ida Merete Enholm <ida.merete.enholm@nav.no>
2021-10-18 10:13:19 +0200

    ktlint

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/VedtaksperiodeService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/VedtaksperiodeService.kt
@@ -299,8 +299,8 @@ class VedtaksperiodeService(
                 utbetalingsperioderUtenEndringer +
                     hentOpphørsperioder(vedtak.behandling)
                 ).map {
-                    it.tilVedtaksperiodeMedBegrunnelse(vedtak)
-                }
+                it.tilVedtaksperiodeMedBegrunnelse(vedtak)
+            }
         val avslagsperioder = hentAvslagsperioderMedBegrunnelser(vedtak)
 
         val endretUtbetalingsperioder = hentEndredeUtbetalingsperioderMedBegrunnelser(
@@ -392,9 +392,9 @@ class VedtaksperiodeService(
                             val vedtakBegrunnelseType = it.vedtakBegrunnelseType
 
                             if (triggesAv.vilkår.contains(Vilkår.UTVIDET_BARNETRYGD) && (
-                                    utbetalingsperiode?.ytelseTyper
-                                        ?: emptyList()
-                                    ).contains(YtelseType.UTVIDET_BARNETRYGD) &&
+                                utbetalingsperiode?.ytelseTyper
+                                    ?: emptyList()
+                                ).contains(YtelseType.UTVIDET_BARNETRYGD) &&
                                 vedtakBegrunnelseType == VedtakBegrunnelseType.INNVILGELSE
                             ) {
                                 gyldigeBegrunnelser.add(it)
@@ -406,8 +406,8 @@ class VedtaksperiodeService(
                                     triggesAv = triggesAv,
                                     vedtakBegrunnelseType = vedtakBegrunnelseType,
                                     endretUtbetalingAndeler = endretUtbetalingAndelRepository.findByBehandlingId(
-                                        behandling.id
-                                    )
+                                            behandling.id
+                                        )
                                 )
                             ) {
                                 gyldigeBegrunnelser.add(it)
