66a363ee247121bc3441d163cb56d8b976944dcf
Henning Håkonsen <henning.hakonsen@nav.no>
2021-10-27 13:17:32 +0200

    ktlint

--- src/test/kotlin/no/nav/familie/ba/sak/kjerne/verdikjedetester/BehandlingSatsendringTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/kjerne/verdikjedetester/BehandlingSatsendringTest.kt
@@ -4,8 +4,6 @@ import io.mockk.every
 import io.mockk.mockkObject
 import io.mockk.unmockkObject
 import no.nav.familie.ba.sak.common.LocalDateService
-import no.nav.familie.ba.sak.kjerne.autobrev.Autobrev6og18ÅrService
-import no.nav.familie.ba.sak.kjerne.autobrev.FinnAlleBarn6og18ÅrTask
 import no.nav.familie.ba.sak.kjerne.autorevurdering.SatsendringService
 import no.nav.familie.ba.sak.kjerne.behandling.BehandlingService
 import no.nav.familie.ba.sak.kjerne.behandling.NyBehandlingHendelse
@@ -18,7 +16,6 @@ import no.nav.familie.ba.sak.kjerne.vedtak.VedtakService
 import no.nav.familie.ba.sak.kjerne.verdikjedetester.mockserver.domene.RestScenario
 import no.nav.familie.ba.sak.kjerne.verdikjedetester.mockserver.domene.RestScenarioPerson
 import no.nav.familie.ba.sak.task.BehandleFødselshendelseTask
-import no.nav.familie.ba.sak.task.OpprettTaskService
 import no.nav.familie.kontrakter.felles.personopplysning.Bostedsadresse
 import no.nav.familie.kontrakter.felles.personopplysning.Matrikkeladresse
 import org.junit.jupiter.api.Assertions.assertEquals
