4f1e1ebfbb2884da1a1f7a7811c3735514984ecc
Meng Zhu <meng.zhu@nav.no>
2021-10-28 15:58:02 +0200

    feature toggle

--- src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/infotrygd/InfotrygdFeedService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/infotrygd/InfotrygdFeedService.kt
@@ -1,12 +1,17 @@
 package no.nav.familie.ba.sak.integrasjoner.infotrygd
 
+import no.nav.familie.ba.sak.config.FeatureToggleConfig.Companion.SEND_START_BEHANDLING_TIL_INFOTRYGD
+import no.nav.familie.ba.sak.config.FeatureToggleService
 import no.nav.familie.ba.sak.task.OpprettTaskService
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import javax.transaction.Transactional
 
 @Service
-class InfotrygdFeedService(val opprettTaskService: OpprettTaskService) {
+class InfotrygdFeedService(
+    val opprettTaskService: OpprettTaskService,
+    val featureToggleService: FeatureToggleService
+) {
 
     @Transactional
     fun sendTilInfotrygdFeed(barnsIdenter: List<String>) {
@@ -16,6 +21,10 @@ class InfotrygdFeedService(val opprettTaskService: OpprettTaskService) {
 
     @Transactional
     fun sendStartBehandlingTilInfotrygdFeed(fnrStoenadsmottaker: String) {
+        if (!featureToggleService.isEnabled(SEND_START_BEHANDLING_TIL_INFOTRYGD)) {
+            logger.info("send ikke startBehandling til infotrygd fordi feature er togglet av")
+            return
+        }
         logger.info("Send startBehandling til Infotrygd.")
         opprettTaskService.opprettSendStartBehandlingTilInfotrygdTask(fnrStoenadsmottaker)
     }
