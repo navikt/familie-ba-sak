a5b41dc98becc02e53dc437148044c43e8d30c3a
Tor Nærland <tor.nerland@nav.no>
2021-11-09 16:30:50 +0100

    Utvidet VelgFagsystemIntegrasjonTest

--- src/test/kotlin/no/nav/familie/ba/sak/kjerne/fødselshendelse/VelgFagsystemIntegrasjonTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/kjerne/fødselshendelse/VelgFagsystemIntegrasjonTest.kt
@@ -4,6 +4,7 @@ import io.mockk.every
 import no.nav.familie.ba.sak.common.randomFnr
 import no.nav.familie.ba.sak.config.AbstractSpringIntegrationTest
 import no.nav.familie.ba.sak.config.e2e.DatabaseCleanupService
+import no.nav.familie.ba.sak.integrasjoner.infotrygd.InfotrygdBarnetrygdClient
 import no.nav.familie.ba.sak.integrasjoner.infotrygd.InfotrygdService
 import no.nav.familie.ba.sak.integrasjoner.pdl.PersonopplysningerService
 import no.nav.familie.ba.sak.kjerne.behandling.NyBehandlingHendelse
@@ -11,6 +12,8 @@ import no.nav.familie.ba.sak.kjerne.fagsak.FagsakService
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersongrunnlagService
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersonopplysningGrunnlagRepository
 import no.nav.familie.ba.sak.kjerne.steg.StegService
+import no.nav.familie.kontrakter.ba.infotrygd.InfotrygdSøkResponse
+import no.nav.familie.kontrakter.ba.infotrygd.Stønad
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -30,6 +33,7 @@ class VelgFagsystemIntegrasjonTest(
     @Autowired val velgFagSystemService: VelgFagSystemService,
     @Autowired val fagSakService: FagsakService,
     @Autowired val infotrygdService: InfotrygdService,
+    @Autowired val infotrygdBarnetrygdClient: InfotrygdBarnetrygdClient,
     @Autowired val databaseCleanupService: DatabaseCleanupService
 ) : AbstractSpringIntegrationTest() {
 
@@ -52,5 +56,17 @@ class VelgFagsystemIntegrasjonTest(
         val nyBehandling = NyBehandlingHendelse(søkerFnr, listOf(søkerFnr))
 
         assertEquals(FagsystemRegelVurdering.SEND_TIL_INFOTRYGD, velgFagSystemService.velgFagsystem(nyBehandling))
+    }
+
+    @Test
+    fun `skal IKKE velge ba-sak når mor har stønadhistorikk i Infotrygd`() {
+        val nyBehandling = NyBehandlingHendelse(søkerFnr, listOf(søkerFnr))
+        val fagsystemUtfallInfotrygd = FagsystemUtfall.SAKER_I_INFOTRYGD_MEN_IKKE_LØPENDE_UTBETALINGER
+
+        every { infotrygdBarnetrygdClient.hentStønader(any(), any(), historikk = true) } returns InfotrygdSøkResponse (
+                listOf(Stønad(opphørtFom = "012020")), emptyList()
+        )
+        assertEquals(FagsystemRegelVurdering.SEND_TIL_INFOTRYGD, velgFagSystemService.velgFagsystem(nyBehandling))
+        assertEquals(1.0, velgFagSystemService.foreslåttUtfallForValgAvFagsystem[fagsystemUtfallInfotrygd]!!.count())
     }
 }
