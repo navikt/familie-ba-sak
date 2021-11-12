67cca815cff9dfe2ae9f267b1e4d394b24f2fae2
Henning Håkonsen <henning.hakonsen@nav.no>
2021-11-12 15:16:52 +0100

    Laget generell feilmelding og lagt til plasser det passer.

--- src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/økonomi/UtbetalingsoppdragUtils.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/økonomi/UtbetalingsoppdragUtils.kt
@@ -1,17 +1,17 @@
 package no.nav.familie.ba.sak.integrasjoner.økonomi
 
 import no.nav.familie.ba.sak.common.FunksjonellFeil
+import no.nav.familie.ba.sak.common.KONTAKT_TEAMET_SUFFIX
 import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingResultat
 import no.nav.familie.kontrakter.felles.oppdrag.Utbetalingsoppdrag
 
 fun Utbetalingsoppdrag.valider(behandlingsresultat: BehandlingResultat) {
     if (this.utbetalingsperiode.isNotEmpty() && behandlingsresultat == BehandlingResultat.FORTSATT_INNVILGET) {
-        throw FunksjonellFeil("Behandling har resultat fortsatt innvilget, men det finnes utbetalingsperioder som ifølge systemet skal endres.")
+        throw FunksjonellFeil("Behandling har resultat fortsatt innvilget, men det finnes utbetalingsperioder som ifølge systemet skal endres. $KONTAKT_TEAMET_SUFFIX")
     } else if (this.utbetalingsperiode.isEmpty()) {
         throw FunksjonellFeil(
             "Utbetalingsoppdraget inneholder ingen utbetalingsperioder " +
-                "og det er grunn til å tro at denne ikke bør simuleres eller iverksettes. " +
-                "Undersøk om dette er riktig."
+                "og det er grunn til å tro at denne ikke bør simuleres eller iverksettes. $KONTAKT_TEAMET_SUFFIX"
         )
     }
 }
