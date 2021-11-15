527bf82f2bb15674bf7844cde4d11ec4d65f1a74
André Wolden <wolden.andre@gmail.com>
2021-11-15 08:47:11 +0100

    fix migration

--- src/main/resources/db/migration/V168__utdypende_vilkarsvurderinger.sql
+++ src/main/resources/db/migration/V168__utdypende_vilkarsvurderinger.sql
@@ -1,28 +1,13 @@
 ALTER TABLE vilkar_resultat
     ADD COLUMN utdypende_vilkarsvurderinger VARCHAR;
 
+-- er_skjonnsmessig_vurdert VURDERING_ANNET_GRUNNLAG
+-- er_medlemskap_vurdert VURDERT_MEDLEMSKAP
+-- er_delt_bosted DELT_BOSTED
 UPDATE vilkar_resultat
-SET utdypende_vilkarsvurderinger = 'VURDERING_ANNET_GRUNNLAG'
-WHERE vilkar_resultat.er_skjonnsmessig_vurdert = true
-  AND vilkar_resultat.utdypende_vilkarsvurderinger IS NULL;
-
-
-UPDATE vilkar_resultat
-SET utdypende_vilkarsvurderinger =
-        CASE
-            WHEN octet_length(vilkar_resultat.utdypende_vilkarsvurderinger) > 0
-                THEN concat(utdypende_vilkarsvurderinger, ';', 'VURDERT_MEDLEMSKAP')
-            ELSE 'VURDERT_MEDLEMSKAP'
-            END
-WHERE vilkar_resultat.er_medlemskap_vurdert = true
-  AND vilkar_resultat.utdypende_vilkarsvurderinger IS NULL;
-
-UPDATE vilkar_resultat
-SET utdypende_vilkarsvurderinger =
-        CASE
-            WHEN octet_length(vilkar_resultat.utdypende_vilkarsvurderinger) > 0
-                THEN concat(utdypende_vilkarsvurderinger, ';', 'DELT_BOSTED')
-            ELSE 'DELT_BOSTED'
-            END
-WHERE vilkar_resultat.er_delt_bosted = true
-  AND vilkar_resultat.utdypende_vilkarsvurderinger IS NULL;
+SET utdypende_vilkarsvurderinger
+        = concat_ws(';'
+        , CASE WHEN er_skjonnsmessig_vurdert = TRUE THEN 'VURDERING_ANNET_GRUNNLAG' END
+        , CASE WHEN er_medlemskap_vurdert = TRUE THEN 'VURDERT_MEDLEMSKAP' END
+        , CASE WHEN er_delt_bosted = TRUE THEN 'DELT_BOSTED' END)
+WHERE vilkar_resultat.utdypende_vilkarsvurderinger IS NULL
