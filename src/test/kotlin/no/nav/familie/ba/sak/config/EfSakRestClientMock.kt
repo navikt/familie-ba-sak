083c8dcf3afe144db87488d3f004b75189a32347
Henning Håkonsen <henning.hakonsen@nav.no>
2021-11-23 09:57:56 +0100

    Retter bug hvor småbarnstilleggandeler ble fjernet når man la inn endret andeler. Mindre forbedringer på flyt.

--- src/test/kotlin/no/nav/familie/ba/sak/config/EfSakRestClientMock.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/config/EfSakRestClientMock.kt
@@ -35,9 +35,9 @@ class EfSakRestClientMock {
                     perioder = listOf(
                         PeriodeOvergangsstønad(
                             personIdent = hentPerioderMedFullOvergangsstønadSlot.captured,
-                            fomDato = LocalDate.now().minusYears(2),
+                            fomDato = LocalDate.now(),
                             datakilde = PeriodeOvergangsstønad.Datakilde.EF,
-                            tomDato = LocalDate.now().minusMonths(3),
+                            tomDato = LocalDate.now().plusMonths(3),
                         )
                     )
                 )
