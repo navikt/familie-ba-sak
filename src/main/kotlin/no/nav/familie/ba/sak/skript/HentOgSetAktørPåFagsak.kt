27d6476381d44c6cb97eab4e08deecae3d8473d3
Marcus Gustafson <marcus.gustafson@computas.com>
2021-11-25 18:49:21 +0100

    begrens til leader

--- src/main/kotlin/no/nav/familie/ba/sak/skript/HentOgSetAktørPåFagsak.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/skript/HentOgSetAktørPåFagsak.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ba.sak.skript
 
 import no.nav.familie.ba.sak.kjerne.fagsak.FagsakRepository
 import no.nav.familie.ba.sak.kjerne.personident.PersonidentService
+import no.nav.familie.leader.LeaderClient
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -15,15 +16,18 @@ class HentOgSetAktørPåFagsak(val fagsakRepository: FagsakRepository, val perso
     fun hentOgSetAktørPåFagsak() {
         val fagsaker = fagsakRepository.finnAlleFagsakerUtenAktør()
 
-        logger.info("Sett aktørid på fagsak ${fagsaker.size}")
-        // Prod inneholder 33 fagsaker uten aktør id, mens preprod inneholder 240 
-        if (fagsaker.size > 250) throw error("Skal maks sette aktør id på 250 fagsaker fant ${fagsaker.size}.")
+        if (LeaderClient.isLeader() == true) {
+            logger.info("Sett aktørid på fagsak ${fagsaker.size}")
+            // Prod inneholder 33 fagsaker uten aktør id, mens preprod inneholder 240 
+            if (fagsaker.size > 250) throw error("Skal maks sette aktør id på 250 fagsaker fant ${fagsaker.size}.")
 
-        fagsaker.forEach {
-            logger.info("Sett aktørid på fagsak ${it.id}")
-            val aktør = personidentService.hentOgLagreAktørId(it.hentAktivIdent().ident)
-            it.aktør = aktør
-            fagsakRepository.save(it)
+            fagsaker.forEach {
+                logger.info("Sett aktørid på fagsak ${it.id}")
+                val aktør = personidentService.hentOgLagreAktørId(it.hentAktivIdent().ident)
+                it.aktør = aktør
+                fagsakRepository.save(it)
+            }
+            logger.info("Ferdig satt aktørid på fagsak")
         }
     }
 
