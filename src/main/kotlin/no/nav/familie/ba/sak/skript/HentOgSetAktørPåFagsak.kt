6125e3943960f9a3d3fe71ac1437510c7cce397a
Marcus Gustafson <marcus.gustafson@computas.com>
2021-11-26 08:21:22 +0100

    Splitte i en transaktion per opdatering og la inn try/catch

--- src/main/kotlin/no/nav/familie/ba/sak/skript/HentOgSetAktørPåFagsak.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/skript/HentOgSetAktørPåFagsak.kt
@@ -1,8 +1,8 @@
 package no.nav.familie.ba.sak.skript
 
+import no.nav.familie.ba.sak.kjerne.fagsak.Fagsak
 import no.nav.familie.ba.sak.kjerne.fagsak.FagsakRepository
 import no.nav.familie.ba.sak.kjerne.personident.PersonidentService
-import no.nav.familie.leader.LeaderClient
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -11,25 +11,31 @@ import org.springframework.transaction.annotation.Transactional
 @Component
 class HentOgSetAktørPåFagsak(val fagsakRepository: FagsakRepository, val personidentService: PersonidentService) {
 
-    @Transactional
     @Scheduled(initialDelay = 1000 * 30, fixedDelay = Long.MAX_VALUE)
     fun hentOgSetAktørPåFagsak() {
         val fagsaker = fagsakRepository.finnAlleFagsakerUtenAktør()
 
-        if (LeaderClient.isLeader() == true) {
-            logger.info("Sett aktørid på fagsak ${fagsaker.size}")
-            // Prod inneholder 33 fagsaker uten aktør id, mens preprod inneholder 240 
-            if (fagsaker.size > 250) throw error("Skal maks sette aktør id på 250 fagsaker fant ${fagsaker.size}.")
+        logger.info("Sett aktørid på fagsak ${fagsaker.size}")
+        // Prod inneholder 33 fagsaker uten aktør id, mens preprod inneholder 240 
+        if (fagsaker.size > 250) throw error("Skal maks sette aktør id på 250 fagsaker fant ${fagsaker.size}.")
 
-            fagsaker.forEach {
-                logger.info("Sett aktørid på fagsak ${it.id}")
-                val aktør = personidentService.hentOgLagreAktørId(it.hentAktivIdent().ident)
-                it.aktør = aktør
-                fagsakRepository.save(it)
-            }
-            logger.info("Ferdig satt aktørid på fagsak")
-        } else {
-            logger.info("Poden er ikke satt som leader")
+        var teller = 0
+        fagsaker.forEach { teller += hentOgSetAktørPåFagsak(it) }
+        logger.info("Ferdig satt aktørid på fagsak: $teller")
+    }
+
+    @Transactional
+    fun hentOgSetAktørPåFagsak(fagsak: Fagsak): Int {
+
+        try {
+            logger.info("Sett aktørid på fagsak ${fagsak.id}")
+            val aktør = personidentService.hentOgLagreAktørId(fagsak.hentAktivIdent().ident)
+            fagsak.aktør = aktør
+            fagsakRepository.save(fagsak)
+            return 1
+        } catch (e: Exception) {
+            logger.warn("Feilet å sette aktør på fagsak: ${fagsak.aktør}")
+            return 0
         }
     }
 
