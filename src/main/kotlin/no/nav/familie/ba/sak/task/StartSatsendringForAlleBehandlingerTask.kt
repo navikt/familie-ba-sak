594840a92277d0520cdb08553701d54b9cf22fea
Henning Solberg <henning.solberg@nav.no>
2021-11-27 19:37:49 +0100

    Lagt inn støtte for å kjøre satsendringer async

---
+++ src/main/kotlin/no/nav/familie/ba/sak/task/StartSatsendringForAlleBehandlingerTask.kt
@@ -0,0 +1,46 @@
+package no.nav.familie.ba.sak.task
+
+import no.nav.familie.ba.sak.config.TaskRepositoryWrapper
+import no.nav.familie.ba.sak.kjerne.autovedtak.satsendring.SatsendringService
+import no.nav.familie.prosessering.AsyncTaskStep
+import no.nav.familie.prosessering.TaskStepBeskrivelse
+import no.nav.familie.prosessering.domene.Task
+import org.springframework.stereotype.Service
+import java.time.YearMonth
+import java.util.Properties
+
+@Service
+@TaskStepBeskrivelse(
+    taskStepType = StartSatsendringForAlleBehandlingerTask.TASK_STEP_TYPE,
+    beskrivelse = "Utfør satsendring for alle behandlinger",
+    maxAntallFeil = 1
+)
+class StartSatsendringForAlleBehandlingerTask(
+    val satsendringService: SatsendringService,
+    val taskRepository: TaskRepositoryWrapper
+) : AsyncTaskStep {
+
+    override fun doTask(task: Task) {
+        val gammelSats = task.payload.toLong()
+        satsendringService.finnBehandlingerForSatsendring(gammelSats, YearMonth.now()).forEach {
+            taskRepository.save(SatsendringTask.opprettTask(it))
+        }
+
+    }
+
+    companion object {
+
+        const val TASK_STEP_TYPE = "startsatsendringforallebehandlinger"
+
+        fun opprettTask(gammelSats: Long): Task {
+            return Task(
+                type = TASK_STEP_TYPE,
+                payload = gammelSats.toString(),
+                properties = Properties().apply {
+                    this["måned"] = YearMonth.now()
+                    this["gammelSats"] = gammelSats
+                }
+            )
+        }
+    }
+}
