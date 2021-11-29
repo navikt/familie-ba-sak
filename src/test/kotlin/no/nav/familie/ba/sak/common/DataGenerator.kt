75edc65a1d2544a6bde55d6b944604e0787c4ac0
André Wolden <wolden.andre@gmail.com>
2021-11-29 15:12:27 +0100

    utdypende vilkårsvurdering opprydding (#1682)
    * fjerner deprecatede felter på VilkårResultat og bruken av de
    
    * fjerner deprecatede felter på VilkårResultat og bruken av de
    
    * bruker utdypendeVilkårsvurderinger på PeriodeVilkår
    
    * fiks migration name etter master merge

--- src/test/kotlin/no/nav/familie/ba/sak/common/DataGenerator.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/common/DataGenerator.kt
@@ -473,7 +473,9 @@ fun lagPersonResultat(
                     resultat = resultat,
                     begrunnelse = "",
                     behandlingId = vilkårsvurdering.behandling.id,
-                    erDeltBosted = erDeltBosted && it == Vilkår.BOR_MED_SØKER
+                    utdypendeVilkårsvurderinger = listOfNotNull(
+                        if (erDeltBosted && it == Vilkår.BOR_MED_SØKER) UtdypendeVilkårsvurdering.DELT_BOSTED else null
+                    )
                 )
             }.toSet()
         )
@@ -926,7 +928,6 @@ fun lagVilkårResultat(
     periodeTom: LocalDate? = LocalDate.of(2010, 1, 31),
     begrunnelse: String = "",
     behandlingId: Long = lagBehandling().id,
-    erMedlemskapVurdert: Boolean = false,
     utdypendeVilkårsvurderinger: List<UtdypendeVilkårsvurdering> = emptyList()
 ) = VilkårResultat(
     personResultat = personResultat,
@@ -936,10 +937,7 @@ fun lagVilkårResultat(
     periodeTom = periodeTom,
     begrunnelse = begrunnelse,
     behandlingId = behandlingId,
-    erMedlemskapVurdert = erMedlemskapVurdert,
-    utdypendeVilkårsvurderinger = (
-        utdypendeVilkårsvurderinger + listOfNotNull(if (erMedlemskapVurdert) UtdypendeVilkårsvurdering.VURDERT_MEDLEMSKAP else null)
-        ).distinct()
+    utdypendeVilkårsvurderinger = utdypendeVilkårsvurderinger
 )
 
 val guttenBarnesenFødselsdato = LocalDate.now().withDayOfMonth(10).minusYears(6)
