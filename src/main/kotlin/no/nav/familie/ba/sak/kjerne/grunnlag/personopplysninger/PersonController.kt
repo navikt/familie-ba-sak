abcaff25042be3df8281547035b522e9e270cf62
Henning Håkonsen <henning.hakonsen@nav.no>
2021-11-29 18:38:18 +0100

    Flyttet ut endepunkt til egen controller.

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/grunnlag/personopplysninger/PersonController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/grunnlag/personopplysninger/PersonController.kt
@@ -6,17 +6,13 @@ import no.nav.familie.ba.sak.ekstern.restDomene.tilRestPersonInfo
 import no.nav.familie.ba.sak.integrasjoner.familieintegrasjoner.IntegrasjonClient
 import no.nav.familie.ba.sak.integrasjoner.pdl.PersonopplysningerService
 import no.nav.familie.ba.sak.kjerne.behandling.UtvidetBehandlingService
-import no.nav.familie.ba.sak.kjerne.personident.PersonidentService
 import no.nav.familie.ba.sak.sikkerhet.validering.PersontilgangConstraint
-import no.nav.familie.kontrakter.felles.PersonIdent
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -29,15 +25,8 @@ class PersonController(
     private val personopplysningerService: PersonopplysningerService,
     private val persongrunnlagService: PersongrunnlagService,
     private val integrasjonClient: IntegrasjonClient,
-    private val utvidetBehandlingService: UtvidetBehandlingService,
-    private val personidentService: PersonidentService
+    private val utvidetBehandlingService: UtvidetBehandlingService
 ) {
-
-    @PostMapping(path = ["/ny-ident"])
-    fun håndterNyIdent(@RequestBody nyIdent: PersonIdent): ResponseEntity<Ressurs<String>> {
-        personidentService.håndterNyIdent(nyIdent)
-        return ResponseEntity.ok(Ressurs.success("Håndtert ny ident"))
-    }
 
     @GetMapping
     fun hentPerson(@RequestHeader personIdent: String): ResponseEntity<Ressurs<RestPersonInfo>> {
