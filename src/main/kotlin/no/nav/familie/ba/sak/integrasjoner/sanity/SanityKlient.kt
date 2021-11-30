5e86351e4783d4925ec8c9156da51d36a7484c8d
halvormundal <halvorbmundal@gmail.com>
2021-11-30 13:52:31 +0100

    Bruk get for entity

--- src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/sanity/SanityKlient.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/sanity/SanityKlient.kt
@@ -2,11 +2,10 @@ package no.nav.familie.ba.sak.kjerne.dokument
 
 import no.nav.familie.ba.sak.kjerne.dokument.domene.RestSanityBegrunnelse
 import no.nav.familie.ba.sak.kjerne.dokument.domene.SanityBegrunnelse
+import no.nav.familie.http.client.AbstractRestClient
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.core.ParameterizedTypeReference
-import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestOperations
 import java.net.URI
@@ -20,21 +19,16 @@ data class SanityRespons<T>(
 @Component
 class SanityKlient(
     @Value("\${SANITY_FAMILIE_API_URL}") private val sanityFamilieApi: String,
-    @Qualifier("medProxy") val restOperations: RestOperations,
-) {
+    @Qualifier("medProxy") restOperations: RestOperations,
+) : AbstractRestClient(restOperations, "sanity") {
 
     fun hentSanityBegrunnelser(): List<SanityBegrunnelse> {
         val parameters = java.net.URLEncoder.encode(hentDokumenter, "utf-8")
         val url = URI.create("$sanityFamilieApi?query=$parameters")
         logger.info("Henter begrunnelser fra sanity")
 
-        val response = restOperations.exchange(
-            url,
-            HttpMethod.GET,
-            null,
-            object : ParameterizedTypeReference<SanityRespons<RestSanityBegrunnelse>>() {},
-        )
-        val restSanityBegrunnelser = response.body?.result ?: error("Klarte ikke hente begrunnelsene fra familie-brev.")
+        val response = getForEntity<SanityRespons<RestSanityBegrunnelse>>(url)
+        val restSanityBegrunnelser = response.result
         return restSanityBegrunnelser.map { it.tilSanityBegrunnelse() }
     }
 
