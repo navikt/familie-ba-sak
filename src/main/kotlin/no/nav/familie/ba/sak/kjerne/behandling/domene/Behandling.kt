9767dfdec16131c12d19636330286bf414e902f6
Henning Solberg <henning.solberg@nav.no>
2021-12-01 21:07:23 +0100

    ktlint

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandling/domene/Behandling.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandling/domene/Behandling.kt
@@ -217,11 +217,9 @@ data class Behandling(
     fun erMigrering() =
         type == BehandlingType.MIGRERING_FRA_INFOTRYGD || type == BehandlingType.MIGRERING_FRA_INFOTRYGD_OPPHØRT
 
-
     fun erSatsendring() = this.opprettetÅrsak == BehandlingÅrsak.SATSENDRING
-  
-    fun erManuellMigrering() = erMigrering() && opprettetÅrsak == BehandlingÅrsak.ENDRE_MIGRERINGSDATO
 
+    fun erManuellMigrering() = erMigrering() && opprettetÅrsak == BehandlingÅrsak.ENDRE_MIGRERINGSDATO
 
     private fun erOmregning() =
         this.opprettetÅrsak == BehandlingÅrsak.OMREGNING_6ÅR || this.opprettetÅrsak == BehandlingÅrsak.OMREGNING_18ÅR
