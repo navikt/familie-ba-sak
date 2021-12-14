7762cdeb2b30ae5947df0acc5a6794bfb3523ac8
Henning Håkonsen <henning.hakonsen@nav.no>
2021-12-14 16:43:10 +0100

    Legger til test for fortsatt innvilget periode.

--- src/test/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/UtvidetVedtaksperiodeMedBegrunnelserTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/UtvidetVedtaksperiodeMedBegrunnelserTest.kt
@@ -23,6 +23,70 @@ class UtvidetVedtaksperiodeMedBegrunnelserTest {
     val søker = tilfeldigSøker()
 
     @Test
+    fun `Skal kun legge på utbetalingsdetaljer som gjelder riktig andeler tilkjent ytelse for fortsatt innvilget`() {
+        val behandling = lagBehandling()
+
+        val personopplysningGrunnlag = lagTestPersonopplysningGrunnlag(
+            behandlingId = behandling.id,
+            barnasIdenter = listOf(barn1.aktør.aktivFødselsnummer(), barn2.aktør.aktivFødselsnummer()),
+            søkerPersonIdent = søker.aktør.aktivFødselsnummer()
+        )
+
+        val fom = YearMonth.of(2018, 6)
+        val tom = YearMonth.of(2018, 8)
+
+        val endretUtbetalingAndel = lagEndretUtbetalingAndel(
+            behandlingId = behandling.id,
+            fom = fom,
+            tom = tom,
+            person = barn2
+        )
+
+        val andelerTilkjentYtelse = listOf(
+            lagAndelTilkjentYtelse(
+                behandling = behandling,
+                endretUtbetalingAndeler = emptyList(),
+                fom = fom.minusMonths(2),
+                tom = tom,
+                person = barn1
+            ),
+            lagAndelTilkjentYtelse(
+                behandling = behandling,
+                endretUtbetalingAndeler = listOf(endretUtbetalingAndel),
+                fom = fom,
+                tom = tom,
+                person = barn2
+            ),
+            lagAndelTilkjentYtelse(
+                behandling = behandling,
+                endretUtbetalingAndeler = emptyList(),
+                fom = tom.plusMonths(1),
+                tom = tom.plusMonths(3),
+                person = barn1
+            )
+        )
+
+        val vedtaksperiodeMedBegrunnelser = lagVedtaksperiodeMedBegrunnelser(
+            fom = fom.førsteDagIInneværendeMåned(),
+            tom = tom.sisteDagIInneværendeMåned(),
+            type = Vedtaksperiodetype.FORTSATT_INNVILGET
+        )
+
+        val utvidetVedtaksperiodeMedBegrunnelser =
+            vedtaksperiodeMedBegrunnelser.tilUtvidetVedtaksperiodeMedBegrunnelser(
+                personopplysningGrunnlag,
+                andelerTilkjentYtelse
+            )
+
+        Assertions.assertEquals(1, utvidetVedtaksperiodeMedBegrunnelser.utbetalingsperiodeDetaljer.size)
+        Assertions.assertEquals(
+            barn1.tilRestPerson().personIdent,
+            utvidetVedtaksperiodeMedBegrunnelser.utbetalingsperiodeDetaljer.single().person.personIdent
+        )
+        Assertions.assertFalse(utvidetVedtaksperiodeMedBegrunnelser.utbetalingsperiodeDetaljer.single().erPåvirketAvEndring)
+    }
+
+    @Test
     fun `Skal kun legge på utbetalingsdetaljer som gjelder riktig andeler tilkjent ytelse for utbetaling`() {
         val behandling = lagBehandling()
 
