f87fd2001019291237c3377526318580acb60baa
André Wolden <wolden.andre@gmail.com>
2021-12-15 21:16:28 +0100

    spørring for alle behandlinger med opphør småbarnstillegg for måned

--- src/test/kotlin/no/nav/familie/ba/sak/kjerne/verdikjedetester/RennameMeTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/kjerne/verdikjedetester/RennameMeTest.kt
@@ -3,6 +3,7 @@ package no.nav.familie.ba.sak.kjerne.verdikjedetester
 import io.mockk.every
 import no.nav.familie.ba.sak.common.førsteDagIInneværendeMåned
 import no.nav.familie.ba.sak.common.lagSøknadDTO
+import no.nav.familie.ba.sak.common.toYearMonth
 import no.nav.familie.ba.sak.config.FeatureToggleService
 import no.nav.familie.ba.sak.ekstern.restDomene.RestMinimalFagsak
 import no.nav.familie.ba.sak.ekstern.restDomene.RestPersonResultat
@@ -16,8 +17,10 @@ import no.nav.familie.ba.sak.kjerne.autovedtak.fødselshendelse.Resultat
 import no.nav.familie.ba.sak.kjerne.autovedtak.småbarnstillegg.VedtakOmOvergangsstønadService
 import no.nav.familie.ba.sak.kjerne.behandling.BehandlingService
 import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandling
+import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingRepository
 import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingType
 import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingUnderkategori
+import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingÅrsak
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelseRepository
 import no.nav.familie.ba.sak.kjerne.fagsak.Beslutning
 import no.nav.familie.ba.sak.kjerne.fagsak.FagsakService
@@ -34,6 +37,7 @@ import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.kontrakter.felles.ef.PeriodeOvergangsstønad
 import no.nav.familie.kontrakter.felles.ef.PerioderOvergangsstønadResponse
 import no.nav.familie.kontrakter.felles.tilbakekreving.Tilbakekrevingsvalg
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Order
@@ -49,6 +53,7 @@ import java.time.LocalDate
 class RennameMeTest(
     @Autowired private val fagsakService: FagsakService,
     @Autowired private val behandlingService: BehandlingService,
+    @Autowired private val behandlingRepository: BehandlingRepository,
     @Autowired private val vedtakService: VedtakService,
     @Autowired private val stegService: StegService,
     @Autowired private val featureToggleService: FeatureToggleService,
@@ -67,7 +72,7 @@ class RennameMeTest(
 
         // Opprett innslag av fagsaker og behandlinger i databasen
 
-        val barnFødselsdato1: LocalDate = LocalDate.now().minusYears(16)
+        val barnFødselsdato1: LocalDate = LocalDate.now().minusYears(2)
         val personScenario1 = lagScenario(barnFødselsdato1)
         val fagsak1: RestMinimalFagsak = lagFagsak(personScenario = personScenario1)
         val beh1 = lagFullførtBehandling(
@@ -77,15 +82,16 @@ class RennameMeTest(
             barnFødselsdato = barnFødselsdato1,
             behandlingType = BehandlingType.FØRSTEGANGSBEHANDLING
         )
-        val beh2 = lagFullførtBehandling(
+        val beh2 = lagRevurderingBehandling(
             fagsak = fagsak1,
             opphørAvSmåbarnstilleggNå = true,
             personScenario = personScenario1,
             barnFødselsdato = barnFødselsdato1,
-            behandlingType = BehandlingType.REVURDERING
+            behandlingType = BehandlingType.REVURDERING,
+            behandlingÅrsak = BehandlingÅrsak.SMÅBARNSTILLEGG
         )
 
-        val barnFødselsdato2: LocalDate = LocalDate.now().minusYears(15)
+        val barnFødselsdato2: LocalDate = LocalDate.now().minusYears(2)
         val personScenario2 = lagScenario(barnFødselsdato2)
         val fagsak2: RestMinimalFagsak = lagFagsak(personScenario = personScenario2)
         val beh3 = lagFullførtBehandling(
@@ -95,37 +101,24 @@ class RennameMeTest(
             barnFødselsdato = barnFødselsdato2,
             behandlingType = BehandlingType.FØRSTEGANGSBEHANDLING
         )
-        val beh4 = lagFullførtBehandling(
+        val beh4 = lagRevurderingBehandling(
             fagsak = fagsak2,
             opphørAvSmåbarnstilleggNå = true,
             personScenario = personScenario2,
             barnFødselsdato = barnFødselsdato2,
-            behandlingType = BehandlingType.REVURDERING
-        )
-
-        val barnFødselsdato3: LocalDate = LocalDate.now().minusYears(14)
-        val personScenario3 = lagScenario(barnFødselsdato3)
-        val fagsak3: RestMinimalFagsak = lagFagsak(personScenario = personScenario3)
-        val beh5 = lagFullførtBehandling(
-            fagsak = fagsak3,
-            opphørAvSmåbarnstilleggNå = false,
-            personScenario = personScenario3,
-            barnFødselsdato = barnFødselsdato3,
-            behandlingType = BehandlingType.FØRSTEGANGSBEHANDLING
-        )
-        val beh6 = lagFullførtBehandling(
-            fagsak = fagsak3,
-            opphørAvSmåbarnstilleggNå = false,
-            personScenario = personScenario3,
-            barnFødselsdato = barnFødselsdato3,
-            behandlingType = BehandlingType.REVURDERING
+            behandlingType = BehandlingType.REVURDERING,
+            behandlingÅrsak = BehandlingÅrsak.SMÅBARNSTILLEGG
         )
 
         // kjør spørring og verifiser riktig output / innslag i resultat
-        val behandlinger: List<Behandling> =
-            behandlingService.hentAlleBehandlingerMedOpphørSmåbarnstilleggInneværendeMåned()
+        val behandlinger: List<Long> =
+            behandlingService.hentAlleBehandlingsIderMedOpphørSmåbarnstilleggIMåned(
+                måned = LocalDate.now().toYearMonth().minusMonths(1)
+            )
 
-        assertTrue(behandlinger.size == 2)
+        assertTrue(behandlinger.containsAll(listOf(beh2.id, beh4.id)))
+        assertFalse(behandlinger.contains(beh1.id))
+        assertFalse(behandlinger.contains(beh3.id))
     }
 
     fun lagScenario(barnFødselsdato: LocalDate): RestScenario = mockServerKlient().lagScenario(
@@ -212,6 +205,108 @@ class RennameMeTest(
         val restUtvidetBehandlingEtterBehandlingsResultat =
             familieBaSakKlient().behandlingsresultatStegOgGåVidereTilNesteSteg(
                 behandlingId = restUtvidetBehandling.data!!.behandlingId
+            )
+
+        val restUtvidetBehandlingEtterVurderTilbakekreving =
+            familieBaSakKlient().lagreTilbakekrevingOgGåVidereTilNesteSteg(
+                restUtvidetBehandlingEtterBehandlingsResultat.data!!.behandlingId,
+                RestTilbakekreving(Tilbakekrevingsvalg.IGNORER_TILBAKEKREVING, begrunnelse = "begrunnelse")
+            )
+
+        val vedtaksperiodeId =
+            restUtvidetBehandlingEtterVurderTilbakekreving.data!!.vedtak!!.vedtaksperioderMedBegrunnelser.first()
+        familieBaSakKlient().oppdaterVedtaksperiodeMedStandardbegrunnelser(
+            vedtaksperiodeId = vedtaksperiodeId.id,
+            restPutVedtaksperiodeMedStandardbegrunnelser = RestPutVedtaksperiodeMedStandardbegrunnelser(
+                standardbegrunnelser = listOf(
+                    VedtakBegrunnelseSpesifikasjon.INNVILGET_BOR_HOS_SØKER
+                )
+            )
+        )
+        val restUtvidetBehandlingEtterSendTilBeslutter =
+            familieBaSakKlient().sendTilBeslutter(behandlingId = restUtvidetBehandlingEtterVurderTilbakekreving.data!!.behandlingId)
+
+        familieBaSakKlient().iverksettVedtak(
+            behandlingId = restUtvidetBehandlingEtterSendTilBeslutter.data!!.behandlingId,
+            restBeslutningPåVedtak = RestBeslutningPåVedtak(
+                Beslutning.GODKJENT
+            ),
+            beslutterHeaders = HttpHeaders().apply {
+                setBearerAuth(
+                    token(
+                        mapOf(
+                            "groups" to listOf("SAKSBEHANDLER", "BESLUTTER"),
+                            "azp" to "azp-test",
+                            "name" to "Mock McMockface Beslutter",
+                            "preferred_username" to "mock.mcmockface.beslutter@nav.no"
+                        )
+                    )
+                )
+            }
+        )
+        return håndterIverksettingAvBehandling(
+            behandlingEtterVurdering = behandlingService.hentAktivForFagsak(fagsakId = fagsak.id)!!,
+            søkerFnr = personScenario.søker.ident!!,
+            fagsakService = fagsakService,
+            vedtakService = vedtakService,
+            stegService = stegService
+        )
+    }
+
+    fun lagRevurderingBehandling(
+        fagsak: RestMinimalFagsak,
+        opphørAvSmåbarnstilleggNå: Boolean,
+        personScenario: RestScenario,
+        barnFødselsdato: LocalDate,
+        behandlingType: BehandlingType,
+        behandlingÅrsak: BehandlingÅrsak
+    ): Behandling {
+
+        every { efSakRestClient.hentPerioderMedFullOvergangsstønad(any()) } returns PerioderOvergangsstønadResponse(
+            perioder = if (opphørAvSmåbarnstilleggNå) listOf(
+                PeriodeOvergangsstønad(
+                    personIdent = personScenario.søker.ident!!,
+                    fomDato = barnFødselsdato.plusYears(1),
+                    tomDato = LocalDate.now().minusMonths(1).førsteDagIInneværendeMåned(),
+                    datakilde = PeriodeOvergangsstønad.Datakilde.EF
+                )
+            ) else emptyList()
+        )
+
+        val restBehandling: Ressurs<RestUtvidetBehandling> = familieBaSakKlient().opprettBehandling(
+            søkersIdent = fagsak.søkerFødselsnummer,
+            behandlingUnderkategori = BehandlingUnderkategori.UTVIDET,
+            behandlingType = behandlingType,
+            behandlingÅrsak = behandlingÅrsak
+        )
+        val behandling = behandlingService.hent(restBehandling.data!!.behandlingId)
+
+        restBehandling.data!!.personResultater.forEach { restPersonResultat ->
+            restPersonResultat.vilkårResultater.filter { it.resultat == Resultat.IKKE_VURDERT }.forEach {
+                familieBaSakKlient().putVilkår(
+                    behandlingId = restBehandling.data!!.behandlingId,
+                    vilkårId = it.id,
+                    restPersonResultat =
+                    RestPersonResultat(
+                        personIdent = restPersonResultat.personIdent,
+                        vilkårResultater = listOf(
+                            it.copy(
+                                resultat = Resultat.OPPFYLT,
+                                periodeFom = barnFødselsdato
+                            )
+                        )
+                    )
+                )
+            }
+        }
+
+        familieBaSakKlient().validerVilkårsvurdering(
+            behandlingId = restBehandling.data!!.behandlingId
+        )
+
+        val restUtvidetBehandlingEtterBehandlingsResultat =
+            familieBaSakKlient().behandlingsresultatStegOgGåVidereTilNesteSteg(
+                behandlingId = restBehandling.data!!.behandlingId
             )
 
         val restUtvidetBehandlingEtterVurderTilbakekreving =
