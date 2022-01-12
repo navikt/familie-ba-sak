9fa591a596acf2ae484d1664f5ae7105d3a205c2
haldersaikat <saikat.halder@nav.no>
2022-01-12 14:23:41 +0100

    oppdaterte skripten

--- src/main/resources/db/migration/V177__gcp_database_tilgang.sql
+++ src/main/resources/db/migration/V177__gcp_database_tilgang.sql
@@ -1,33 +1,33 @@
 alter
-default privileges in schema public grant all on tables to 'saikat.halder@nav.no';
+default privileges in schema public grant all on tables to "henning.solberg@nav.no";
 alter
-default privileges in schema public grant all on tables to 'henning.solberg@nav.no';
+default privileges in schema public grant all on tables to "henning.hakonsen@nav.no";
 alter
-default privileges in schema public grant all on tables to 'henning.hakonsen@nav.no';
+default privileges in schema public grant all on tables to "andre.wolden@nav.no";
 alter
-default privileges in schema public grant all on tables to 'andre.wolden@nav.no';
+default privileges in schema public grant all on tables to "geir.roger.moen@nav.no";
 alter
-default privileges in schema public grant all on tables to 'geir.roger.moen@nav.no';
+default privileges in schema public grant all on tables to "halvor.mundal@nav.no";
 alter
-default privileges in schema public grant all on tables to 'halvor.mundal@nav.no';
+default privileges in schema public grant all on tables to "marcus.gustafson@nav.no";
 alter
-default privileges in schema public grant all on tables to 'marcus.gustafson@nav.no';
+default privileges in schema public grant all on tables to "ida.merete.enholm@nav.no";
 alter
-default privileges in schema public grant all on tables to 'ida.merete.enholm@nav.no';
+default privileges in schema public grant all on tables to "saikat.halder@nav.no";
 
 grant all
-on all tables in schema public to 'saikat.halder@nav.no';
+on all tables in schema public to "henning.solberg@nav.no";
 grant all
-on all tables in schema public to 'henning.solberg@nav.no';
+on all tables in schema public to "henning.hakonsen@nav.no";
 grant all
-on all tables in schema public to 'henning.hakonsen@nav.no';
+on all tables in schema public to "andre.wolden@nav.no";
 grant all
-on all tables in schema public to 'andre.wolden@nav.no';
+on all tables in schema public to "geir.roger.moen@nav.no";
 grant all
-on all tables in schema public to 'geir.roger.moen@nav.no';
+on all tables in schema public to "halvor.mundal@nav.no";
 grant all
-on all tables in schema public to 'halvor.mundal@nav.no';
+on all tables in schema public to "marcus.gustafson@nav.no";
 grant all
-on all tables in schema public to 'marcus.gustafson@nav.no';
+on all tables in schema public to "ida.merete.enholm@nav.no";
 grant all
-on all tables in schema public to 'ida.merete.enholm@nav.no';
+on all tables in schema public to "saikat.halder@nav.no";
