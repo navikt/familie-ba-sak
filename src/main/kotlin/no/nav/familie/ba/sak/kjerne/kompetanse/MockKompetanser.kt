e0a1cf9509e264a21188042fa0d2453f4b309686
Jørn Ola Birkeland <jorn.ola.birkeland@nav.no>
2022-01-26 12:54:41 +0100

    Legger til minimal funksjonalitet bak endepunktene

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/kompetanse/MockKompetanser.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/kompetanse/MockKompetanser.kt
@@ -9,18 +9,28 @@ object MockKompetanser {
     val barn2 = 222222L
     val barn3 = 333333L
 
-    private val kompetanser = mutableListOf(
-        Kompetanse(
-            id = 1,
-            fom = LocalDate.of(2021, 2, 1),
-            tom = LocalDate.of(2021, 11, 30),
-            barn = listOf(barn1, barn2, barn3)
+    private val malKompetanse = Kompetanse(
+        id = 1L,
+        fom = LocalDate.of(2021, 2, 1),
+        tom = LocalDate.of(2021, 11, 30),
+        barn = listOf(barn1, barn2, barn3)
+    )
+
+    private val kompetanser = mutableMapOf(
+        Pair(
+            1L,
+            malKompetanse.copy(id = 1)
         )
     )
 
-    fun hentKompetanser(behandlingId: Long): List<Kompetanse> = kompetanser
+    fun hentKompetanser(behandlingId: Long): Collection<Kompetanse> = kompetanser.values
+        .map { it.copy(behandlingId = behandlingId) }
 
-    fun oppdaterKompetanse(kompetanse: Kompetanse): List<Kompetanse> {
-        return kompetanser
+    // TODO Bør gjøre noe annet enn å legge til en tom kompetanse ...
+    fun oppdaterKompetanse(kompetanse: Kompetanse): Collection<Kompetanse> {
+        val nyId = kompetanser.keys.maxOf { it } + 1
+        kompetanser[kompetanse.id] = kompetanse
+        kompetanser[nyId] = malKompetanse.copy(id = nyId)
+        return kompetanser.values
     }
 }
