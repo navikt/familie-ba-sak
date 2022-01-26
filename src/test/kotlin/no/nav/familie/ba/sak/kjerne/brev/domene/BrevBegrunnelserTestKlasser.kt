4e823f032588e2eb0d05cc9dc5b31c4f2114b1e2
halvorbmundal <halvorbmundal@users.noreply.github.com>
2022-01-26 09:49:39 +0100

    Feature/endrede utbetalinger vedtaksside (#1940)
    * Endre slik at begrunnelser hentes fra vedtakssiden
    
    * Sett på release toggel for ny funksjonallitet
    
    * MinimerData
    
    * Toggle lagring av endringsbegrunnelser på behandlingsresultatsteget
    
    * Endre Tester
    
    * Legg til tester på begrunnelser
    
    * Endre navn BrevGrunnlag -> BehandlingsgrunnlagForBrev
    
    * Bruk MinimertEndretAndel i TriggesAv.erTriggereOppfyltForEndretUtbetaling
    
    * Bruk MinimertEndretAndel i TriggesAv.erTriggereOppfyltForEndretUtbetaling
    
    * Lag datagenerator for MinimertEndretUtbetalingAndel
    
    * Bruk periodeErOppyltForYtelseTypeGammel i gammel trigges av funskjon
    
    * Fix feilende test
    
    * Fix feilende test
    
    * Fiks nullpointer
    
    * Fiks nullpointer
    
    * ktlint
    
    * Hent gyldig begrunnelse direkte fra endret utbetaling
    
    * Hent begrunnelser kun fra aktuelle personer
    
    * Valider endret utbetaling lagt til er mulig å legge til
    
    * Ikke legg til begrunnelse for endringsperiode til vedtaksperiode med begrunnelse når den opprettes
    
    * Fjern komma
    
    * Legg til tester
    
    * Legg bak toggle
    
    * Bruk any framfor map+contains
    
    * Legg på begrunnelse på endret utbetaling

---
+++ src/test/kotlin/no/nav/familie/ba/sak/kjerne/brev/domene/BrevBegrunnelserTestKlasser.kt
@@ -0,0 +1,106 @@
+package no.nav.familie.ba.sak.kjerne.brev.domene
+
+import no.nav.familie.ba.sak.common.randomAktørId
+import no.nav.familie.ba.sak.common.randomFnr
+import no.nav.familie.ba.sak.kjerne.autovedtak.fødselshendelse.Resultat
+import no.nav.familie.ba.sak.kjerne.beregning.domene.YtelseType
+import no.nav.familie.ba.sak.kjerne.brev.UtvidetScenarioForEndringsperiode
+import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.Årsak
+import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersonType
+import no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser.VedtakBegrunnelseSpesifikasjon
+import no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser.domene.MinimertPerson
+import no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser.domene.MinimertVedtaksperiode
+import no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.Vedtaksperiodetype
+import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.AnnenVurdering
+import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.Vilkår
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.YearMonth
+
+data class BrevBegrunnelserTestConfig(
+    val beskrivelse: String,
+
+    val fom: LocalDate?,
+    val tom: LocalDate?,
+    val vedtaksperiodetype: Vedtaksperiodetype,
+
+    // Brukes for å se om det er en reduksjon i småbarnstillegg eller utvidet
+    val ytelserForSøkerForrigeMåned: List<YtelseType>,
+
+    val utvidetScenarioForEndringsperiode: UtvidetScenarioForEndringsperiode,
+
+    val ytelserForBarnOgSøkerIPeriode: List<YtelseType>,
+    val erFørsteVedtaksperiodePåFagsak: Boolean,
+    val personerPåBehandling: List<BrevbegrunnelserTestPerson>,
+
+    val forventetOutput: List<VedtakBegrunnelseSpesifikasjon>
+) {
+    fun hentMinimertVedtaksperiode() = MinimertVedtaksperiode(
+        fom = this.fom,
+        tom = this.tom,
+        type = this.vedtaksperiodetype,
+        ytelseTyperForPeriode = ytelserForBarnOgSøkerIPeriode.toSet()
+    )
+
+    fun hentMinimertePersonResultater() = this.personerPåBehandling.map { it.tilMinimertePersonResultater() }
+    fun hentMinimertePersoner() = this.personerPåBehandling.map { it.tilMinimertPerson() }
+    fun hentAktørIderMedUtbetaling() = this.personerPåBehandling.map { it.aktørId }
+    fun hentEndretUtbetalingAndeler() = this.personerPåBehandling.flatMap { it.tilMinimerteEndredeUtbetalingAndeler() }
+}
+
+data class BrevbegrunnelserTestPerson(
+    val personIdent: String = randomFnr(),
+    val aktørId: String = randomAktørId(personIdent).aktørId,
+    val type: PersonType,
+    val fødselsdato: LocalDate,
+    val overstyrteVilkårresultater: List<MinimertVilkårResultat>,
+    val andreVurderinger: List<AnnenVurdering>,
+    val endredeUtbetalinger: List<EndretUtbetalingAndelPåPerson>,
+) {
+    fun tilMinimertPerson() = MinimertPerson(
+        aktivPersonIdent = this.personIdent,
+        aktørId = this.aktørId,
+        type = this.type,
+        fødselsdato = this.fødselsdato,
+    )
+
+    fun tilMinimerteEndredeUtbetalingAndeler() =
+        endredeUtbetalinger.map { it.tilMinimertEndretUtbetalingAndel(aktørId = this.aktørId) }
+
+    fun tilMinimertePersonResultater() = MinimertRestPersonResultat(
+        personIdent = this.personIdent,
+        minimerteVilkårResultater = hentVilkårForPerson(),
+        andreVurderinger = this.andreVurderinger,
+    )
+
+    private fun hentVilkårForPerson() =
+        this.overstyrteVilkårresultater +
+            Vilkår.hentVilkårFor(this.type)
+                .filter { vilkår -> !this.overstyrteVilkårresultater.any { it.vilkårType == vilkår } }
+                .map { vilkår ->
+                    MinimertVilkårResultat(
+                        vilkårType = vilkår,
+                        periodeFom = this.fødselsdato,
+                        periodeTom = null,
+                        resultat = Resultat.OPPFYLT,
+                        utdypendeVilkårsvurderinger = emptyList(),
+                        erEksplisittAvslagPåSøknad = false,
+                    )
+                }
+}
+
+data class EndretUtbetalingAndelPåPerson(
+    val fom: YearMonth,
+    val tom: YearMonth,
+    val årsak: Årsak,
+    val prosent: BigDecimal,
+) {
+    fun tilMinimertEndretUtbetalingAndel(aktørId: String) =
+        MinimertEndretAndel(
+            aktørId = aktørId,
+            fom = fom,
+            tom = tom,
+            årsak = årsak,
+            prosent = prosent
+        )
+}
