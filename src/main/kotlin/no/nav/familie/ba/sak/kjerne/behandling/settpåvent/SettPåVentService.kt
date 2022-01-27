71f68b3ce206bf04681d901500fcf99372a774b5
halvormundal <halvorbmundal@gmail.com>
2022-01-27 15:49:27 +0100

    Legg til mer validering på sett på vent

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandling/settpåvent/SettPåVentService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandling/settpåvent/SettPåVentService.kt
@@ -1,7 +1,6 @@
 package no.nav.familie.ba.sak.kjerne.behandling.settpåvent
 
 import no.nav.familie.ba.sak.common.Feil
-import no.nav.familie.ba.sak.common.FunksjonellFeil
 import no.nav.familie.ba.sak.kjerne.behandling.BehandlingService
 import org.springframework.stereotype.Service
 import java.time.LocalDate
@@ -21,21 +20,9 @@ class SettPåVentService(
     }
 
     fun settBehandlingPåVent(behandlingId: Long, frist: LocalDate, årsak: SettPåVentÅrsak): SettPåVent {
-        if (finnAktivSettPåVentPåBehandling(behandlingId) != null) {
-            throw Feil(
-                "Behandling $behandlingId er allerede satt på vent."
-
-            )
-        }
-
-        if (frist.isBefore(LocalDate.now())) {
-            throw FunksjonellFeil(
-                melding = "Frist for å vente på behandling $behandlingId er satt før dagens dato.",
-                frontendFeilmelding = "Fristen er satt før dagens dato.",
-            )
-        }
-
         val behandling = behandlingService.hent(behandlingId)
+        val settPåVent: SettPåVent? = finnAktivSettPåVentPåBehandling(behandlingId)
+        validerBehandlingKanSettesPåVent(settPåVent, frist, behandling)
 
         return settPåVentRepository.save(SettPåVent(behandling = behandling, frist = frist, årsak = årsak))
     }
