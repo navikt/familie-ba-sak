5027651972203e1e3413347f22174ae95aa4a952
halvormundal <halvorbmundal@gmail.com>
2022-01-28 15:53:45 +0100

    Gjør funsjon public

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/VedtaksperiodeService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/VedtaksperiodeService.kt
@@ -374,7 +374,7 @@ class VedtaksperiodeService(
     }
 
     @Deprecated("Skal ikke brukes lenger. Bruk VedtaksperiodeUtil.hentGyldigeBegrunnelserForVedtaksperiode")
-    private fun hentGyldigeBegrunnelserForVedtaksperiodeGammel(
+    fun hentGyldigeBegrunnelserForVedtaksperiodeGammel(
         utvidetVedtaksperiodeMedBegrunnelser: UtvidetVedtaksperiodeMedBegrunnelser,
         behandling: Behandling,
         sanityBegrunnelser: List<SanityBegrunnelse>,
