6f3f26f46ee38a66de50c217ea91a58bb55695c7
Ida Merete Enholm <ida.merete.enholm@nav.no>
2022-01-31 15:18:38 +0100

    Lag dataklasse for Dødsfall

---
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/grunnlag/personopplysninger/Dødsfall.kt
@@ -0,0 +1,51 @@
+package no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger
+
+import no.nav.familie.ba.sak.common.BaseEntitet
+import no.nav.familie.ba.sak.integrasjoner.pdl.internal.PdlKontaktinformasjonForDødsbo
+import java.time.LocalDate
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.OneToOne
+import javax.persistence.SequenceGenerator
+import javax.persistence.Table
+
+@Entity(name = "Dødsfall")
+@Table(name = "po_doedsfall")
+data class Dødsfall(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "po_doedsfall_seq_generator")
+    @SequenceGenerator(name = "po_doedsfall_seq_generator", sequenceName = "po_doedsfall_seq", allocationSize = 50)
+    val id: Long = 0,
+
+    @OneToOne
+    @JoinColumn(name = "fk_po_person_id", referencedColumnName = "id", nullable = false)
+    val person: Person,
+
+    @Column(name = "doedsfall_dato", nullable = false)
+    val dødsfallDato: LocalDate,
+
+    @Column(name = "doedsfall_adresse", nullable = true)
+    val dødsfallAdresse: String?,
+
+    @Column(name = "doedsfall_postnummer", nullable = true)
+    val dødsfallPostnummer: String?,
+
+    @Column(name = "doedsfall_poststed", nullable = true)
+    val dødsfallPoststed: String?,
+) : BaseEntitet() {
+    fun hentAdresseToString(): String { return """$dødsfallAdresse, $dødsfallPostnummer $dødsfallPoststed""" }
+}
+
+fun lagDødsfall(person: Person, dødsfallDatoFraPdl: LocalDate, dødsfallAdresseFraPdl: PdlKontaktinformasjonForDødsbo?): Dødsfall {
+    return Dødsfall(
+        person = person,
+        dødsfallDato = dødsfallDatoFraPdl,
+        dødsfallAdresse = dødsfallAdresseFraPdl?.adresselinje1,
+        dødsfallPostnummer = dødsfallAdresseFraPdl?.postnummer,
+        dødsfallPoststed = dødsfallAdresseFraPdl?.poststedsnavn
+    )
+}
