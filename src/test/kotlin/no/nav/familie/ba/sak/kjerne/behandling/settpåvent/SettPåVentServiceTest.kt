ac5b53d565b3cb042f70c85e170999fc79565dd2
halvorbmundal <halvorbmundal@users.noreply.github.com>
2022-01-31 11:37:26 +0100

    Feature/sett på vent (#1964)
    * Wip sett på vent
    
    * WIP
    
    * WIP
    
    * Legg til kolonner tid_tatt_av_vent og tid_satt_paa_vent på sett_paa_vent-tabell
    
    * Forenkle funskjoner
    
    * Legg til validering på at behandlingen ikke er på vent i stegene
    
    * Legg til tester for sett på vent service
    
    * Bruk rest sett på vent på RestUtvidetBehandling
    
    * Legg til mer validering på sett på vent
    
    * Disable test
    
    Co-authored-by: Marcus Gustafson <marcus.gustafson@computas.com>

---
+++ src/test/kotlin/no/nav/familie/ba/sak/kjerne/behandling/settpåvent/SettPåVentServiceTest.kt
@@ -0,0 +1,166 @@
+package no.nav.familie.ba.sak.kjerne.behandling
+
+import no.nav.familie.ba.sak.common.Feil
+import no.nav.familie.ba.sak.common.FunksjonellFeil
+import no.nav.familie.ba.sak.common.kjørStegprosessForFGB
+import no.nav.familie.ba.sak.common.randomFnr
+import no.nav.familie.ba.sak.config.AbstractSpringIntegrationTest
+import no.nav.familie.ba.sak.config.DatabaseCleanupService
+import no.nav.familie.ba.sak.kjerne.behandling.settpåvent.SettPåVentRepository
+import no.nav.familie.ba.sak.kjerne.behandling.settpåvent.SettPåVentService
+import no.nav.familie.ba.sak.kjerne.behandling.settpåvent.SettPåVentÅrsak
+import no.nav.familie.ba.sak.kjerne.fagsak.FagsakService
+import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersongrunnlagService
+import no.nav.familie.ba.sak.kjerne.steg.StegService
+import no.nav.familie.ba.sak.kjerne.steg.StegType
+import no.nav.familie.ba.sak.kjerne.vedtak.VedtakService
+import no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.VedtaksperiodeService
+import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.VilkårsvurderingService
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
+import java.time.LocalDate
+
+@Tag("integration")
+class SettPåVentServiceTest(
+    @Autowired private val fagsakService: FagsakService,
+    @Autowired private val databaseCleanupService: DatabaseCleanupService,
+    @Autowired private val stegService: StegService,
+    @Autowired private val settPåVentService: SettPåVentService,
+    @Autowired private val vedtakService: VedtakService,
+    @Autowired private val persongrunnlagService: PersongrunnlagService,
+    @Autowired private val vilkårsvurderingService: VilkårsvurderingService,
+    @Autowired private val vedtaksperiodeService: VedtaksperiodeService,
+    @Autowired private val settPåVentRepository: SettPåVentRepository,
+) : AbstractSpringIntegrationTest() {
+
+    @BeforeAll
+    fun init() {
+        databaseCleanupService.truncate()
+    }
+
+    @Test
+    fun `Kan ikke endre på behandling etter at den er satt på vent`() {
+        val behandlingEtterVilkårsvurderingSteg = kjørStegprosessForFGB(
+            tilSteg = StegType.VILKÅRSVURDERING,
+            søkerFnr = randomFnr(),
+            barnasIdenter = listOf(randomFnr()),
+            fagsakService = fagsakService,
+            vedtakService = vedtakService,
+            persongrunnlagService = persongrunnlagService,
+            vilkårsvurderingService = vilkårsvurderingService,
+            stegService = stegService,
+            vedtaksperiodeService = vedtaksperiodeService,
+        )
+
+        settPåVentService.settBehandlingPåVent(
+            behandlingId = behandlingEtterVilkårsvurderingSteg.id,
+            frist = LocalDate.now().plusDays(21),
+            årsak = SettPåVentÅrsak.AVVENTER_DOKUMENTASJON
+        )
+
+        assertThrows<Feil> {
+            stegService.håndterBehandlingsresultat(behandlingEtterVilkårsvurderingSteg)
+        }
+    }
+
+    @Test
+    fun `Kan endre på behandling etter venting er deaktivert`() {
+        val behandlingEtterVilkårsvurderingSteg = kjørStegprosessForFGB(
+            tilSteg = StegType.VILKÅRSVURDERING,
+            søkerFnr = randomFnr(),
+            barnasIdenter = listOf(randomFnr()),
+            fagsakService = fagsakService,
+            vedtakService = vedtakService,
+            persongrunnlagService = persongrunnlagService,
+            vilkårsvurderingService = vilkårsvurderingService,
+            stegService = stegService,
+            vedtaksperiodeService = vedtaksperiodeService,
+        )
+
+        settPåVentService.settBehandlingPåVent(
+            behandlingId = behandlingEtterVilkårsvurderingSteg.id,
+            frist = LocalDate.now().plusDays(21),
+            årsak = SettPåVentÅrsak.AVVENTER_DOKUMENTASJON
+        )
+
+        val nå = LocalDate.now()
+
+        val settPåVent = settPåVentService.deaktiverSettBehandlingPåVent(
+            behandlingId = behandlingEtterVilkårsvurderingSteg.id,
+            nå = nå
+        )
+
+        Assertions.assertEquals(
+            nå,
+            settPåVentRepository.findByIdOrNull(settPåVent.id)!!.tidTattAvVent
+        )
+
+        assertDoesNotThrow {
+            stegService.håndterBehandlingsresultat(behandlingEtterVilkårsvurderingSteg)
+        }
+    }
+
+    @Test
+    fun `Kan ikke sette ventefrist til før dagens dato`() {
+        val behandlingEtterVilkårsvurderingSteg = kjørStegprosessForFGB(
+            tilSteg = StegType.VILKÅRSVURDERING,
+            søkerFnr = randomFnr(),
+            barnasIdenter = listOf(randomFnr()),
+            fagsakService = fagsakService,
+            vedtakService = vedtakService,
+            persongrunnlagService = persongrunnlagService,
+            vilkårsvurderingService = vilkårsvurderingService,
+            stegService = stegService,
+            vedtaksperiodeService = vedtaksperiodeService,
+        )
+
+        assertThrows<FunksjonellFeil> {
+            settPåVentService.settBehandlingPåVent(
+                behandlingId = behandlingEtterVilkårsvurderingSteg.id,
+                frist = LocalDate.now().minusDays(1),
+                årsak = SettPåVentÅrsak.AVVENTER_DOKUMENTASJON
+            )
+        }
+    }
+
+    @Test
+    fun `Kan oppdatare set på vent på behandling`() {
+        val behandlingEtterVilkårsvurderingSteg = kjørStegprosessForFGB(
+            tilSteg = StegType.VILKÅRSVURDERING,
+            søkerFnr = randomFnr(),
+            barnasIdenter = listOf(randomFnr()),
+            fagsakService = fagsakService,
+            vedtakService = vedtakService,
+            persongrunnlagService = persongrunnlagService,
+            vilkårsvurderingService = vilkårsvurderingService,
+            stegService = stegService,
+            vedtaksperiodeService = vedtaksperiodeService,
+        )
+
+        val behandlingId = behandlingEtterVilkårsvurderingSteg.id
+
+        val frist1 = LocalDate.now().plusDays(21)
+        settPåVentService.settBehandlingPåVent(
+            behandlingId = behandlingId,
+            frist = frist1,
+            årsak = SettPåVentÅrsak.AVVENTER_DOKUMENTASJON
+        )
+
+        Assertions.assertEquals(frist1, settPåVentService.finnAktivSettPåVentPåBehandlingThrows(behandlingId).frist)
+
+        val frist2 = LocalDate.now().plusDays(9)
+        settPåVentService.oppdaterSettBehandlingPåVent(
+            behandlingId = behandlingId,
+            frist = frist2,
+            årsak = SettPåVentÅrsak.AVVENTER_DOKUMENTASJON
+        )
+
+        Assertions.assertEquals(frist2, settPåVentService.finnAktivSettPåVentPåBehandlingThrows(behandlingId).frist)
+    }
+}
