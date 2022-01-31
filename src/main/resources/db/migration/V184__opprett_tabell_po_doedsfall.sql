e0cc5dea2d17bbb70ded04fe322acaf1ffca381c
Ida Merete Enholm <ida.merete.enholm@nav.no>
2022-01-31 08:51:43 +0100

    Legg på prefixet doedsfall_ på alle kolonnene

--- src/main/resources/db/migration/V184__opprett_tabell_po_doedsfall.sql
+++ src/main/resources/db/migration/V184__opprett_tabell_po_doedsfall.sql
@@ -4,9 +4,9 @@ create TABLE po_doedsfall
     fk_po_person_id         bigint       REFERENCES PO_PERSON (id)      NOT NULL,
     versjon                 bigint       DEFAULT 0                      NOT NULL,
     doedsfall_dato          TIMESTAMP(3)                                NOT NULL,
-    adresse                 VARCHAR      DEFAULT null,
-    postnummer              VARCHAR      DEFAULT null,
-    poststed                VARCHAR      DEFAULT null
+    doedsfall_adresse       VARCHAR      DEFAULT null,
+    doedsfall_postnummer    VARCHAR      DEFAULT null,
+    doedsfall_poststed      VARCHAR      DEFAULT null
 );
 
 create sequence po_doedsfall_seq increment by 50 start with 1000000 NO CYCLE;
