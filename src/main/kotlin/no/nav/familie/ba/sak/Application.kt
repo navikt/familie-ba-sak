fef9d667dae519880bd5e8dddc73f2767bd2f459
Henning Solberg <henning.solberg@nav.no>
2022-01-31 14:01:59 +0100

    fjernet kode som ikke var nødvendig etter spring boot 2.3

--- src/main/kotlin/no/nav/familie/ba/sak/Application.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/Application.kt
@@ -1,17 +1,12 @@
 package no.nav.familie.ba.sak
 
 import no.nav.familie.ba.sak.config.ApplicationConfig
-import no.nav.familie.ba.sak.config.DelayedShutdownHook
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.ConfigurableApplicationContext
 
 @SpringBootApplication
 class FamilieBaSakApplication
 
 fun main(args: Array<String>) {
-    val app = SpringApplication(ApplicationConfig::class.java)
-    app.setRegisterShutdownHook(false)
-    val applicationContext: ConfigurableApplicationContext = app.run(*args)
-    Runtime.getRuntime().addShutdownHook(DelayedShutdownHook(applicationContext))
+    SpringApplication(ApplicationConfig::class.java)
 }
