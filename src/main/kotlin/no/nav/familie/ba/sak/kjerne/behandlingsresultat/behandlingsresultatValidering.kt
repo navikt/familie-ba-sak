e24384b23f66e0f5902619185e5331bc55359ad5
halvormundal <halvorbmundal@gmail.com>
2022-02-02 13:43:03 +0100

    Valider på alle vilkårene

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/behandlingsresultatValidering.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/behandlingsresultatValidering.kt
@@ -1,36 +1,37 @@
 package no.nav.familie.ba.sak.kjerne.behandlingsresultat
 
 import no.nav.familie.ba.sak.common.FunksjonellFeil
-import no.nav.familie.ba.sak.common.TIDENES_ENDE
-import no.nav.familie.ba.sak.common.tilKortString
+import no.nav.familie.ba.sak.common.Utils
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersonopplysningGrunnlag
-import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.Vilkår
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.Vilkårsvurdering
 import java.time.LocalDate
 
-fun validerSøkerBosattIRiketTomdato(
+fun validerIngenVilkårSattEtterSøkersDød(
     personopplysningGrunnlag: PersonopplysningGrunnlag,
     vilkårsvurdering: Vilkårsvurdering,
 
 ) {
     val vilkårResultaterSøker =
         vilkårsvurdering.hentPersonResultaterTil(personopplysningGrunnlag.søker.aktør.aktørId)
-    val søkerBorIRiketTom =
+    val vilkårSomEnderEtterSøkersDød =
         vilkårResultaterSøker
-            .filter { it.vilkårType == Vilkår.BOSATT_I_RIKET }
-            .maxByOrNull { it.periodeTom ?: TIDENES_ENDE }?.periodeTom
+            .groupBy { it.vilkårType }
+            .mapNotNull { (vilkårType, vilkårResultater) ->
+                vilkårType.takeIf {
+                    vilkårResultater.any {
+                        it.periodeTom?.isAfter(
+                            LocalDate.now()
+                        ) ?: true
+                    }
+                }
+            }
 
-    if (søkerBorIRiketTom == null) {
+    if (vilkårSomEnderEtterSøkersDød.isNotEmpty()) {
         throw FunksjonellFeil(
-            "Ved behandlingsårsak \"Dødsfall Bruker\" må bosatt i riket vilkåret avsluttes " +
-                "dagen søker døde, men bosatt i riket vilkåret har ingen til og med dato"
-        )
-    }
-
-    if (søkerBorIRiketTom.isAfter(LocalDate.now())) {
-        throw FunksjonellFeil(
-            "Ved behandlingsårsak \"Dødsfall Bruker\" må bosatt i riket vilkåret avsluttes " +
-                "dagen søker døde, men til og med datoen er satt til ${søkerBorIRiketTom.tilKortString()} "
+            "Ved behandlingsårsak \"Dødsfall Bruker\" må vilkårene på søker avsluttes " +
+                "senest dagen søker døde, men " +
+                Utils.slåSammen(vilkårSomEnderEtterSøkersDød.map { "\"" + it.beskrivelse + "\"" }) +
+                " vilkåret til søker slutter etter søkers død."
         )
     }
 }
