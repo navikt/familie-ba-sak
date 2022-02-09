69dd8ed5f3105f4c81b0ce9564079b5c487af7ca
tnarland <47184872+tnarland@users.noreply.github.com>
2022-02-09 15:49:27 +0100

    Skyggesak scheduler (#2022)
    * Laget scheduling for skyggesak
    * Lagrer sak til skyggesakRepository istedenfor å sende direkte

---
+++ src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/skyggesak/SkyggesakScheduler.kt
@@ -0,0 +1,54 @@
+package no.nav.familie.ba.sak.integrasjoner.skyggesak
+
+import no.nav.familie.ba.sak.integrasjoner.familieintegrasjoner.IntegrasjonClient
+import no.nav.familie.leader.LeaderClient
+import no.nav.familie.log.mdc.MDCConstants
+import org.slf4j.LoggerFactory
+import org.slf4j.MDC
+import org.springframework.data.domain.Pageable
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
+import java.util.UUID
+
+@Component
+class SkyggesakScheduler(
+    val skyggesakRepository: SkyggesakRepository,
+    val integrasjonClient: IntegrasjonClient
+) {
+
+    private val logger = LoggerFactory.getLogger(SkyggesakScheduler::class.java)
+
+    @Scheduled(fixedDelay = 60000)
+    fun opprettSkyggesaker() {
+        if (LeaderClient.isLeader() == true) {
+            sendSkyggesaker()
+        }
+    }
+
+    @Transactional
+    fun sendSkyggesaker() {
+        val skyggesaker = skyggesakRepository.finnSkyggesakerKlareForSending(Pageable.ofSize(400))
+
+        for (skyggesak in skyggesaker) {
+            try {
+                MDC.put(MDCConstants.MDC_CALL_ID, UUID.randomUUID().toString())
+                logger.info("Opprettet skyggesak for fagsak ${skyggesak.fagsak.id}")
+                integrasjonClient.opprettSkyggesak(skyggesak.fagsak.aktør, skyggesak.fagsak.id)
+                skyggesakRepository.save(skyggesak.copy(sendtTidspunkt = LocalDateTime.now()))
+            } catch (e: Exception) {
+                logger.warn("Kunne ikke opprette skyggesak for fagsak ${skyggesak.fagsak.id}")
+                secureLogger.warn("Kunne ikke opprette skyggesak for fagsak ${skyggesak.fagsak.id}", e)
+            } finally {
+                MDC.clear()
+            }
+        }
+    }
+
+    companion object {
+
+        private val logger = LoggerFactory.getLogger(SkyggesakScheduler::class.java)
+        private val secureLogger = LoggerFactory.getLogger("secureLogger")
+    }
+}
