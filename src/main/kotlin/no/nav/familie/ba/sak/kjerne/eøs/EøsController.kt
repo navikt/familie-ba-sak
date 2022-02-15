a43303c0e560c5e665a1484c1fa1edd66d6f82fb
Jørn Ola Birkeland <jorn.ola.birkeland@nav.no>
2022-02-15 13:10:41 +0100

    Legger til funksjonbryter på EøsController

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/eøs/EøsController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/eøs/EøsController.kt
@@ -1,9 +1,12 @@
 package no.nav.familie.ba.sak.kjerne.eøs
 
 import no.nav.familie.ba.sak.common.NullableMånedPeriode
+import no.nav.familie.ba.sak.config.FeatureToggleConfig
+import no.nav.familie.ba.sak.config.FeatureToggleService
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.Person
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
@@ -16,11 +19,17 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/eøs")
 @ProtectedWithClaims(issuer = "azuread")
 @Validated
-class EøsController(val eøsService: EøsService) {
+class EøsController(
+    private val featureToggleService: FeatureToggleService,
+    val eøsService: EøsService
+) {
     @GetMapping(path = ["{behandlingId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun utledEøsPerioder(
         @PathVariable behandlingId: Long
     ): ResponseEntity<Ressurs<Map<Person, List<NullableMånedPeriode>>>> {
-        return ResponseEntity.ok(Ressurs.success(eøsService.utledEøsPerioder(behandlingId)))
+        if (featureToggleService.isEnabled(FeatureToggleConfig.KAN_BEHANDLE_EØS))
+            return ResponseEntity.ok(Ressurs.success(eøsService.utledEøsPerioder(behandlingId)))
+        else
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build()
     }
 }
