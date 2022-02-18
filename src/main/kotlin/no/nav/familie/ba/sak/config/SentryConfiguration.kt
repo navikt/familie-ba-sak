652ff21f69547969d2735f89de5d20658f077bba
halvormundal <halvorbmundal@gmail.com>
2022-02-18 15:26:14 +0100

    Lag kibanalenke i tag til sentry ved feil

--- src/main/kotlin/no/nav/familie/ba/sak/config/SentryConfiguration.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/SentryConfiguration.kt
@@ -5,6 +5,7 @@ import io.sentry.SentryOptions
 import io.sentry.protocol.User
 import no.nav.familie.ba.sak.sikkerhet.SikkerhetContext
 import org.slf4j.LoggerFactory
+import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.NestedExceptionUtils
@@ -32,6 +33,8 @@ class SentryConfiguration(
 
                 event.setTag("metodeSomFeier", metodeSomFeiler)
                 event.setTag("bruker", SikkerhetContext.hentSaksbehandlerEpost())
+                event.setTag("kibanalenke", hentKibanalenke(environment, MDC.get("callId")))
+
                 event.fingerprints = listOf(
                     "{{ default }}",
                     event.transaction,
@@ -42,6 +45,17 @@ class SentryConfiguration(
             }
         }
     }
+
+    private fun hentKibanalenke(miljø: String, callId: String) =
+        "https://logs.adeo.no/app/discover#/view/?_g=(refreshInterval:(pause:!t, value :0), " +
+            "time:(from:now-12h,to:now))&_a=(columns:!(message,envclass,environment,level,application,host)," +
+            "filters:!(('\$state':(store:appState),meta:(alias:!n,disabled:!f," +
+            "index:'96e648c0-980a-11e9-830a-e17bbd64b4db',key:envclass,negate:!f,params:(query:p)," +
+            "type:phrase),query:(match_phrase:(envclass:$miljø))),('\$state':(store:appState)," +
+            "meta:(alias:!n,disabled:!f,index:'96e648c0-980a-11e9-830a-e17bbd64b4db'," +
+            "key:x_callId,negate:!f,params:(query:'65c60464-b295-4421-ba74-18075f0d4e25'),type:phrase)," +
+            "query:(match_phrase:(x_callId:'$callId'))))," +
+            "index:'logstash-*',interval:auto,query:(language:lucene,query:''),sort:!(!('@timestamp',desc)))"
 
     fun finnMetodeSomFeiler(e: Throwable?): String {
         val firstElement = e?.stackTrace?.firstOrNull {
