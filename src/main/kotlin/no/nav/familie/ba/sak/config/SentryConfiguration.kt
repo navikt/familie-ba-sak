7de55e1ef62d933d0ee934cc941bc63b0486e2d1
halvorbmundal <halvorbmundal@users.noreply.github.com>
2022-02-21 16:31:41 +0100

    Bruk lenke som er kortere enn 200 tegn i sentry tag (#2078)

--- src/main/kotlin/no/nav/familie/ba/sak/config/SentryConfiguration.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/SentryConfiguration.kt
@@ -33,7 +33,7 @@ class SentryConfiguration(
 
                 event.setTag("metodeSomFeier", metodeSomFeiler)
                 event.setTag("bruker", SikkerhetContext.hentSaksbehandlerEpost())
-                event.setTag("kibanalenke", hentKibanalenke(environment, MDC.get("callId")))
+                event.setTag("kibanalenke", hentKibanalenke(MDC.get("callId")))
 
                 event.fingerprints = listOf(
                     "{{ default }}",
@@ -46,16 +46,8 @@ class SentryConfiguration(
         }
     }
 
-    private fun hentKibanalenke(miljø: String, callId: String) =
-        "https://logs.adeo.no/app/discover#/view/?_g=(refreshInterval:(pause:!t, value :0), " +
-            "time:(from:now-12h,to:now))&_a=(columns:!(message,envclass,environment,level,application,host)," +
-            "filters:!(('\$state':(store:appState),meta:(alias:!n,disabled:!f," +
-            "index:'96e648c0-980a-11e9-830a-e17bbd64b4db',key:envclass,negate:!f,params:(query:p)," +
-            "type:phrase),query:(match_phrase:(envclass:$miljø))),('\$state':(store:appState)," +
-            "meta:(alias:!n,disabled:!f,index:'96e648c0-980a-11e9-830a-e17bbd64b4db'," +
-            "key:x_callId,negate:!f,params:(query:'65c60464-b295-4421-ba74-18075f0d4e25'),type:phrase)," +
-            "query:(match_phrase:(x_callId:'$callId'))))," +
-            "index:'logstash-*',interval:auto,query:(language:lucene,query:''),sort:!(!('@timestamp',desc)))"
+    private fun hentKibanalenke(callId: String) =
+        "https://logs.adeo.no/app/discover#/?_g=(time:(from:now-1M,to:now))&_a=(filters:!((query:(match_phrase:(x_callId:'$callId')))))"
 
     fun finnMetodeSomFeiler(e: Throwable?): String {
         val firstElement = e?.stackTrace?.firstOrNull {
