6058eb0c79fea1900b8f076654c6b8329e056b1e
halvormundal <halvorbmundal@gmail.com>
2022-03-11 12:34:16 +0100

    Fjern logging som allerede blir gjort et annet sted

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/brev/domene/BrevperiodeData.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/brev/domene/BrevperiodeData.kt
@@ -1,12 +1,9 @@
 package no.nav.familie.ba.sak.kjerne.brev.domene
 
-import no.nav.familie.ba.sak.common.Feil
-import no.nav.familie.ba.sak.common.convertDataClassToJson
 import no.nav.familie.ba.sak.kjerne.behandlingsresultat.MinimertUregistrertBarn
 import no.nav.familie.ba.sak.kjerne.brev.UtvidetScenarioForEndringsperiode
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.Målform
 import no.nav.familie.ba.sak.kjerne.vedtak.domene.Begrunnelse
-import org.slf4j.LoggerFactory
 
 data class BrevperiodeData(
     val restBehandlingsgrunnlagForBrev: RestBehandlingsgrunnlagForBrev,
@@ -16,31 +13,18 @@ data class BrevperiodeData(
     val minimertVedtaksperiode: MinimertVedtaksperiode,
     val utvidetScenarioForEndringsperiode: UtvidetScenarioForEndringsperiode = UtvidetScenarioForEndringsperiode.IKKE_UTVIDET_YTELSE,
 ) {
-    fun hentBegrunnelserOgFritekster(): List<Begrunnelse> {
-        return try {
-            minimertVedtaksperiode
-                .tilBrevPeriodeGrunnlagMedPersoner(
-                    restBehandlingsgrunnlagForBrev = this.restBehandlingsgrunnlagForBrev,
-                    erFørsteVedtaksperiodePåFagsak = this.erFørsteVedtaksperiodePåFagsak,
-                    erUregistrerteBarnPåbehandling = this.uregistrerteBarn.isNotEmpty(),
-                )
-                .byggBegrunnelserOgFritekster(
-                    restBehandlingsgrunnlagForBrev = this.restBehandlingsgrunnlagForBrev,
-                    uregistrerteBarn = this.uregistrerteBarn,
-                    brevMålform = this.brevMålform
-                )
-        } catch (exception: Exception) {
-            val brevPeriodeForLogging = this.tilBrevperiodeForLogging()
-
-            secureLogger.error(
-                "Feil ved generering av brevbegrunnelse. Data som ble sendt inn var: ${
-                brevPeriodeForLogging.convertDataClassToJson()
-                }",
-                exception
+    fun hentBegrunnelserOgFritekster(): List<Begrunnelse> =
+        minimertVedtaksperiode
+            .tilBrevPeriodeGrunnlagMedPersoner(
+                restBehandlingsgrunnlagForBrev = this.restBehandlingsgrunnlagForBrev,
+                erFørsteVedtaksperiodePåFagsak = this.erFørsteVedtaksperiodePåFagsak,
+                erUregistrerteBarnPåbehandling = this.uregistrerteBarn.isNotEmpty(),
             )
-            throw Feil(message = "Feil ved generering av brevbegrunnelse: ", throwable = exception)
-        }
-    }
+            .byggBegrunnelserOgFritekster(
+                restBehandlingsgrunnlagForBrev = this.restBehandlingsgrunnlagForBrev,
+                uregistrerteBarn = this.uregistrerteBarn,
+                brevMålform = this.brevMålform
+            )
 
     fun tilBrevperiodeForLogging() =
         minimertVedtaksperiode.tilBrevPeriodeForLogging(
@@ -48,9 +32,4 @@ data class BrevperiodeData(
             uregistrerteBarn = this.uregistrerteBarn,
             brevMålform = this.brevMålform,
         )
-
-    companion object {
-
-        private val secureLogger = LoggerFactory.getLogger("secureLogger")
-    }
 }
