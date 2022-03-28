073fa418f2cb0768b53adb6c44aceb93a53d9e1f
nav-jorn-ola-birkeland <47781778+nav-jorn-ola-birkeland@users.noreply.github.com>
2022-03-28 12:59:50 +0200

    Feature/kompetanse vs eøs (#2263)
    Legge til funksjonalitet for å matche kompetanser med regelverksperioder. Samtidig ble det en del flytting av kode og tester, og litt generelle utvidelser av tidslinje-rammeverket.

---
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/eøs/kompetanse/AktørKompetanseTidslinje.kt
@@ -0,0 +1,34 @@
+package no.nav.familie.ba.sak.kjerne.eøs.kompetanse
+
+import no.nav.familie.ba.sak.kjerne.beregning.AktørId
+import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.Kompetanse
+import no.nav.familie.ba.sak.kjerne.tidslinje.Periode
+import no.nav.familie.ba.sak.kjerne.tidslinje.Tidslinje
+import no.nav.familie.ba.sak.kjerne.tidslinje.tid.Måned
+import no.nav.familie.ba.sak.kjerne.tidslinje.tid.MånedTidspunkt.Companion.tilTidspunktEllerUendeligLengeSiden
+import no.nav.familie.ba.sak.kjerne.tidslinje.tid.MånedTidspunkt.Companion.tilTidspunktEllerUendeligLengeTil
+import java.time.YearMonth
+
+class AktørKompetanseTidslinje(
+    private val aktørId: AktørId,
+    private val kompetanser: List<Kompetanse>,
+) : Tidslinje<Kompetanse, Måned>() {
+
+    override fun fraOgMed() = kompetanser
+        .map { it.fom.tilTidspunktEllerUendeligLengeSiden { it.tom ?: YearMonth.now() } }
+        .minOrNull() ?: throw IllegalStateException("Listen av kompetanser er tom")
+
+    override fun tilOgMed() = kompetanser
+        .map { it.tom.tilTidspunktEllerUendeligLengeTil { it.fom ?: YearMonth.now() } }
+        .maxOrNull() ?: throw IllegalStateException("Listen av kompetanser er tom")
+
+    override fun lagPerioder(): Collection<Periode<Kompetanse, Måned>> {
+        return kompetanser.map { it.tilPeriode() }
+    }
+
+    private fun Kompetanse.tilPeriode() = Periode(
+        fraOgMed = this.fom.tilTidspunktEllerUendeligLengeSiden { tom ?: YearMonth.now() },
+        tilOgMed = this.tom.tilTidspunktEllerUendeligLengeTil { fom ?: YearMonth.now() },
+        innhold = this.copy(fom = null, tom = null, barnAktørIder = setOf(aktørId)).also { id = this.id }
+    )
+}
