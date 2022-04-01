76b50cad2562924bc072cd46abb07b4f49e7b139
Jørn Ola Birkeland <jorn.ola.birkeland@nav.no>
2022-04-01 15:21:44 +0200

    Fjerner Toveis- og ListeKombinator, til fordel for rendyrkning av funksjonell tilnærming

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/tidslinje/komposisjon/TidslinjeKombinator.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/tidslinje/komposisjon/TidslinjeKombinator.kt
@@ -1,9 +1,67 @@
 package no.nav.familie.ba.sak.kjerne.tidslinje.komposisjon
 
-interface ToveisKombinator<V, H, R> {
-    fun kombiner(venstre: V?, høyre: H?): R?
+import no.nav.familie.ba.sak.kjerne.tidslinje.Tidslinje
+import no.nav.familie.ba.sak.kjerne.tidslinje.tid.Tidsenhet
+import no.nav.familie.ba.sak.kjerne.tidslinje.tid.Tidspunkt
+import no.nav.familie.ba.sak.kjerne.tidslinje.tidslinjer.TomTidslinje
+
+fun <K, V, H, R, T : Tidsenhet> Map<K, Tidslinje<V, T>>.kombinerInnvendigMed(
+    tidslinjeMap: Map<K, Tidslinje<H, T>>,
+    mapKombinator: (K) -> (V?, H?) -> R?
+): Map<K, Tidslinje<R, T>> {
+    return this
+        .filterKeys { tidslinjeMap.containsKey(it) }
+        .mapValues { (key, venstre) ->
+            val høyre: Tidslinje<H, T> = tidslinjeMap.getValue(key)
+            val kombinator: (V?, H?) -> R? = mapKombinator(key)
+            val resultat: Tidslinje<R, T> = venstre.kombinerMed(høyre, kombinator)
+            resultat
+        }
 }
 
-interface ListeKombinator<T, R> {
-    fun kombiner(liste: Iterable<T>): R?
+fun <K, V, H, R, T : Tidsenhet> Map<K, Tidslinje<V, T>>.kombinerUtvendigMed(
+    tidslinjeMap: Map<K, Tidslinje<H, T>>,
+    mapKombinator: (K) -> (V?, H?) -> R?
+): Map<K, Tidslinje<R, T>> {
+
+    val alleKeys = keys + tidslinjeMap.keys
+
+    return alleKeys
+        .map {
+            val høyre: Tidslinje<H, T> = tidslinjeMap[it] ?: TomTidslinje()
+            val venstre: Tidslinje<V, T> = this[it] ?: TomTidslinje()
+            val kombinator: (V?, H?) -> R? = mapKombinator(it)
+            val resultat: Tidslinje<R, T> = venstre.kombinerMed(høyre, kombinator)
+            it to resultat
+        }.toMap()
+}
+
+fun <V, H, R, T : Tidsenhet> Tidslinje<V, T>.snittKombinerMed(
+    høyreTidslinje: Tidslinje<H, T>,
+    kombinator: (V?, H?) -> R?
+): Tidslinje<R, T> {
+    val venstreTidslinje = this
+    return object : TidslinjeSomStykkerOppTiden<R, T>(venstreTidslinje, høyreTidslinje) {
+        override fun finnInnholdForTidspunkt(tidspunkt: Tidspunkt<T>): R? =
+            kombinator(
+                venstreTidslinje.innholdForTidspunkt(tidspunkt),
+                høyreTidslinje.innholdForTidspunkt(tidspunkt)
+            )
+    }
+}
+
+fun <A, B, C, R, T : Tidsenhet> Tidslinje<A, T>.snittKombinerMed(
+    tidslinjeB: Tidslinje<B, T>,
+    tidslinjeC: Tidslinje<C, T>,
+    kombinator: (A?, B?, C?) -> R?
+): Tidslinje<R, T> {
+    val tidslinjeA = this
+    return object : TidslinjeSomStykkerOppTiden<R, T>(tidslinjeA, tidslinjeB, tidslinjeC) {
+        override fun finnInnholdForTidspunkt(tidspunkt: Tidspunkt<T>): R? =
+            kombinator(
+                tidslinjeA.innholdForTidspunkt(tidspunkt),
+                tidslinjeB.innholdForTidspunkt(tidspunkt),
+                tidslinjeC.innholdForTidspunkt(tidspunkt)
+            )
+    }
 }
