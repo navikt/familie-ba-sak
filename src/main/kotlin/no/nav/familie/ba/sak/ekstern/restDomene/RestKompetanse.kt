bf728a46d352f18a7c6ee0c5e48c5d00fcce7b65
Henning Håkonsen <henning.hakonsen@nav.no>
2022-04-01 09:09:28 +0200

    Endrer til å bruke rest-kompetanse på inn/ut trafikk. Svarer med utvidet-behandling på endringer av kompetanse.

--- src/main/kotlin/no/nav/familie/ba/sak/ekstern/restDomene/RestKompetanse.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/ekstern/restDomene/RestKompetanse.kt
@@ -3,6 +3,7 @@ package no.nav.familie.ba.sak.ekstern.restDomene
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.Kompetanse
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.KompetanseStatus
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.util.vurderStatus
+import no.nav.familie.ba.sak.kjerne.personident.Aktør
 import java.time.YearMonth
 
 data class RestKompetanse(
@@ -27,4 +28,10 @@ fun Kompetanse.tilRestKompetanse() = RestKompetanse(
     primærland = this.primærland,
     sekundærland = this.sekundærland,
     status = this.vurderStatus()
+)
+
+fun RestKompetanse.tilKompetanse(barnAktører: List<Aktør>) = Kompetanse(
+    fom = this.fom,
+    tom = this.tom,
+    barnAktører = barnAktører.toSet(),
 )
--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/eøs/kompetanse/KompetanseController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/eøs/kompetanse/KompetanseController.kt
@@ -1,10 +1,14 @@
 package no.nav.familie.ba.sak.kjerne.eøs.kompetanse
 
-import no.nav.familie.ba.sak.common.Feil
+import no.nav.familie.ba.sak.common.FunksjonellFeil
 import no.nav.familie.ba.sak.config.FeatureToggleConfig
 import no.nav.familie.ba.sak.config.FeatureToggleService
+import no.nav.familie.ba.sak.ekstern.restDomene.RestKompetanse
+import no.nav.familie.ba.sak.ekstern.restDomene.RestUtvidetBehandling
+import no.nav.familie.ba.sak.ekstern.restDomene.tilKompetanse
+import no.nav.familie.ba.sak.kjerne.behandling.UtvidetBehandlingService
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.Kompetanse
-import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.util.vurderStatus
+import no.nav.familie.ba.sak.kjerne.personident.PersonidentService
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.HttpStatus
@@ -12,7 +16,6 @@ import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -25,61 +28,57 @@ import org.springframework.web.bind.annotation.RestController
 @Validated
 class KompetanseController(
     private val featureToggleService: FeatureToggleService,
-    private val kompetanseService: KompetanseService
+    private val kompetanseService: KompetanseService,
+    private val personidentService: PersonidentService,
+    private val utvidetBehandlingService: UtvidetBehandlingService
 ) {
-    @GetMapping(path = ["{behandlingId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun hentKompetanser(
-        @PathVariable behandlingId: Long
-    ): ResponseEntity<Ressurs<Collection<Kompetanse>>> {
-        if (!featureToggleService.isEnabled(FeatureToggleConfig.KAN_BEHANDLE_EØS))
-            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build()
-
-        val kompetanser = kompetanseService.hentKompetanser(behandlingId).vurderStatus()
-        return ResponseEntity.ok(Ressurs.success(kompetanser))
-    }
 
     @PutMapping(path = ["{behandlingId}/{kompetanseId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun oppdaterKompetanse(
         @PathVariable behandlingId: Long,
         @PathVariable kompetanseId: Long,
-        @RequestBody kompetanse: Kompetanse
-    ): ResponseEntity<Ressurs<Collection<Kompetanse>>> {
+        @RequestBody restKompetanse: RestKompetanse
+    ): ResponseEntity<Ressurs<RestUtvidetBehandling>> {
         if (!featureToggleService.isEnabled(FeatureToggleConfig.KAN_BEHANDLE_EØS))
             return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build()
+
+        val barnAktører = restKompetanse.barnIdenter.map { personidentService.hentAktør(it) }
+        val kompetanse = restKompetanse.tilKompetanse(barnAktører = barnAktører)
 
         val gjeldendeKompetanse = kompetanseService.hentKompetanse(kompetanseId)
         validerOppdatering(gjeldendeKompetanse, kompetanse)
 
-        val oppdaterteKompetanser = kompetanseService.oppdaterKompetanse(kompetanseId, kompetanse)
-            .vurderStatus()
+        kompetanseService.oppdaterKompetanse(kompetanseId, kompetanse)
 
-        return ResponseEntity.ok(Ressurs.success(oppdaterteKompetanser))
+        return ResponseEntity.ok(Ressurs.success(utvidetBehandlingService.lagRestUtvidetBehandling(behandlingId = behandlingId)))
     }
 
     @DeleteMapping(path = ["{behandlingId}/{kompetanseId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun slettKompetanse(
         @PathVariable behandlingId: Long,
         @PathVariable kompetanseId: Long
-    ): ResponseEntity<Ressurs<Collection<Kompetanse>>> {
+    ): ResponseEntity<Ressurs<RestUtvidetBehandling>> {
         if (!featureToggleService.isEnabled(FeatureToggleConfig.KAN_BEHANDLE_EØS))
             return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build()
 
-        val oppdaterteKompetanser = kompetanseService.slettKompetanse(kompetanseId)
-            .vurderStatus()
+        kompetanseService.slettKompetanse(kompetanseId)
 
-        return ResponseEntity.ok(Ressurs.success(oppdaterteKompetanser))
+        return ResponseEntity.ok(Ressurs.success(utvidetBehandlingService.lagRestUtvidetBehandling(behandlingId = behandlingId)))
     }
 
     private fun validerOppdatering(gjeldendeKompetanse: Kompetanse, oppdatertKompetanse: Kompetanse) {
         if (oppdatertKompetanse.fom == null)
-            throw Feil("Manglende fra-og-med", httpStatus = HttpStatus.BAD_REQUEST)
+            throw FunksjonellFeil("Manglende fra-og-med", httpStatus = HttpStatus.BAD_REQUEST)
         if (oppdatertKompetanse.fom > oppdatertKompetanse.tom)
-            throw Feil("Fra-og-med er etter til-og-med", httpStatus = HttpStatus.BAD_REQUEST)
+            throw FunksjonellFeil("Fra-og-med er etter til-og-med", httpStatus = HttpStatus.BAD_REQUEST)
         if (oppdatertKompetanse.barnAktører.isEmpty())
-            throw Feil("Mangler barn", httpStatus = HttpStatus.BAD_REQUEST)
+            throw FunksjonellFeil("Mangler barn", httpStatus = HttpStatus.BAD_REQUEST)
         if (oppdatertKompetanse.fom < gjeldendeKompetanse.fom)
-            throw Feil("Setter fra-og-med tidligere", httpStatus = HttpStatus.BAD_REQUEST)
+            throw FunksjonellFeil("Setter fra-og-med tidligere", httpStatus = HttpStatus.BAD_REQUEST)
         if (!gjeldendeKompetanse.barnAktører.containsAll(oppdatertKompetanse.barnAktører))
-            throw Feil("Oppdaterer barn som ikke er knyttet til kompetansen", httpStatus = HttpStatus.BAD_REQUEST)
+            throw FunksjonellFeil(
+                "Oppdaterer barn som ikke er knyttet til kompetansen",
+                httpStatus = HttpStatus.BAD_REQUEST
+            )
     }
 }
