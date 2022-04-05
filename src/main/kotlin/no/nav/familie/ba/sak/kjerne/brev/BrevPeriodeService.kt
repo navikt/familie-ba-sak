f2802b2566d6655013557eaacebe6494d94e25c3
Ida Merete Enholm <ida.merete.enholm@nav.no>
2022-04-05 09:10:17 +0200

    Sende med alle endringsperioder

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/brev/BrevPeriodeService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/brev/BrevPeriodeService.kt
@@ -1,9 +1,6 @@
 package no.nav.familie.ba.sak.kjerne.brev
 
 import no.nav.familie.ba.sak.common.convertDataClassToJson
-import no.nav.familie.ba.sak.common.erDagenFør
-import no.nav.familie.ba.sak.common.førsteDagIInneværendeMåned
-import no.nav.familie.ba.sak.common.sisteDagIInneværendeMåned
 import no.nav.familie.ba.sak.config.FeatureToggleConfig
 import no.nav.familie.ba.sak.config.FeatureToggleService
 import no.nav.familie.ba.sak.integrasjoner.sanity.SanityService
@@ -91,7 +88,7 @@ class BrevPeriodeService(
                 vedtaksperiodeMedBegrunnelser,
                 andelerTilkjentYtelse
             ),
-            endredeAndeler = endredeUtbetalingAndeler.filter { it.tom?.sisteDagIInneværendeMåned()?.erDagenFør(minimertVedtaksperiode.fom?.førsteDagIInneværendeMåned()) == true }
+            endredeAndeler = endredeUtbetalingAndeler
         )
 
         if (skalLogge) {
