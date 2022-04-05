8b9d9a7d78577d63552e271f791fe26f79bd7f23
Saikat Halder <45660476+haldersaikat@users.noreply.github.com>
2022-04-05 15:07:27 +0200

    lagre ned overstyrt endringstidspunkt og generer vedtaksperiode basert på datoen (#2304)
    * generer vedtaksperioder for manuelt overstyring av endringstidspunkt
    
    * oppdatert ClientMocks.kt slik at testene kan kjøres ordentlig
    
    * fikset logikken basert på review kommentar
    
    * lagre ned overstyrt endringstidspunkt og generer vedtaksperiode basert på datoen
    
    * fikset ObjectOptimisticLockingFailure
    
    * flyttet utledEndringstidpunkt til en egen funskjon
    
    * fikset review kommentar
    
    * refaktorte metode og legge til enhetstester

--- src/main/kotlin/no/nav/familie/ba/sak/ekstern/restDomene/RestVedtaksperiodeMedBegrunnelser.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/ekstern/restDomene/RestVedtaksperiodeMedBegrunnelser.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.ba.sak.ekstern.restDomene
 
 import no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser.Standardbegrunnelse
+import java.time.LocalDate
 
 data class RestPutVedtaksperiodeMedFritekster(
     val fritekster: List<String> = emptyList(),
@@ -8,6 +9,11 @@ data class RestPutVedtaksperiodeMedFritekster(
 
 data class RestPutVedtaksperiodeMedStandardbegrunnelser(
     val standardbegrunnelser: List<Standardbegrunnelse>,
+)
+
+data class RestGenererVedtaksperioderForOverstyrtEndringstidspunkt(
+    val behandlingId: Long,
+    val overstyrtEndringstidspunkt: LocalDate
 )
 
 data class RestPutGenererFortsattInnvilgetVedtaksperioder(
