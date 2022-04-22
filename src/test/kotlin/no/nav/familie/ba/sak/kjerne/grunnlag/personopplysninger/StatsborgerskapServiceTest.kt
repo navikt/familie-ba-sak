88995a52063d2ae20b81e2abefca69316d85abf8
Kristine Steine <kristine.steine@bekk.no>
2022-04-22 12:04:19 +0200

    Omskriving av statsborgerskapservice (#2361)
    * Omskriving: bruk intervaller heller enn datosplitt for medlemskapsvurdering (WIP)
    
    * Omskriving: bruk dagen etter medlemskapsperiode som neste endringsdato
    
    * Omskriving: filtrer vekk datoer som er null eller utenfor statsborgerskapsperiode før kombinering av resultatliste
    
    * Omskriving: fjern metoder og tester som kun ble brukt for debugging under omskriving
    
    * Legg til tester for hentSterkesteMedlemskap
    
    * Håndter ukjent statsborgerskapsperiode ved vurdering av sterkeste medlemskap
    
    * Omskriving: bruk intervaller heller enn datosplitt i hentSterkesteMedlemskap
    
    * Omskriving: trekk ut filtrering til navngitt funksjon
    
    * Omskriving: fjern gammel funksjon som har blitt erstattet
    
    * Omskriving: sikre at et intervall slutter dagen før neste starter
    
    * Omskriving: utdyp variabelnavn og funksjonsnavn
    
    * Omskriving: endre parameternavn for utilfunksjoner
    
    * Omskriving: ta med navngitte parametere i funksjonskall
    
    * Omskriving: endre og forklar nullsjekk

--- src/test/kotlin/no/nav/familie/ba/sak/kjerne/grunnlag/personopplysninger/StatsborgerskapServiceTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/kjerne/grunnlag/personopplysninger/StatsborgerskapServiceTest.kt
@@ -47,11 +47,15 @@ internal class StatsborgerskapServiceTest {
 
         assertEquals(2, grStatsborgerskap.size)
         assertEquals(FOM_1990, grStatsborgerskap.sortedBy { it.gyldigPeriode?.fom }.first().gyldigPeriode?.fom)
+        val dagenFørPolenBleMedlemAvEØS = FOM_2004.minusDays(1)
+        assertEquals(
+            dagenFørPolenBleMedlemAvEØS,
+            grStatsborgerskap.sortedBy { it.gyldigPeriode?.fom }.first().gyldigPeriode?.tom
+        )
         assertEquals(
             Medlemskap.TREDJELANDSBORGER,
             grStatsborgerskap.sortedBy { it.gyldigPeriode?.fom }.first().medlemskap
         )
-
         assertEquals(FOM_2004, grStatsborgerskap.sortedBy { it.gyldigPeriode?.fom }.last().gyldigPeriode?.fom)
         assertEquals(Medlemskap.EØS, grStatsborgerskap.sortedBy { it.gyldigPeriode?.fom }.last().medlemskap)
     }
@@ -162,10 +166,13 @@ internal class StatsborgerskapServiceTest {
 
     @Test
     fun `Skal evaluere britiske statsborgere under Brexit som først EØS, nå tredjelandsborgere`() {
+        val datoFørBrexit = LocalDate.of(1989, 3, 1)
+        val datoEtterBrexit = LocalDate.of(2020, 5, 1)
+
         val statsborgerStorbritanniaMedPeriodeUnderBrexit = Statsborgerskap(
             "GBR",
-            gyldigFraOgMed = LocalDate.of(1989, 3, 1),
-            gyldigTilOgMed = LocalDate.of(2020, 5, 1),
+            gyldigFraOgMed = datoFørBrexit,
+            gyldigTilOgMed = datoEtterBrexit,
             bekreftelsesdato = null
         )
         val grStatsborgerskapUnderBrexit = statsborgerskapService.hentStatsborgerskapMedMedlemskap(
@@ -173,10 +180,66 @@ internal class StatsborgerskapServiceTest {
             person = lagPerson()
         )
         assertEquals(2, grStatsborgerskapUnderBrexit.size)
+        assertEquals(datoFørBrexit, grStatsborgerskapUnderBrexit.first().gyldigPeriode?.fom)
+        assertEquals(TOM_2010, grStatsborgerskapUnderBrexit.first().gyldigPeriode?.tom)
         assertEquals(Medlemskap.EØS, grStatsborgerskapUnderBrexit.sortedBy { it.gyldigPeriode?.fom }.first().medlemskap)
         assertEquals(
             Medlemskap.TREDJELANDSBORGER,
             grStatsborgerskapUnderBrexit.sortedBy { it.gyldigPeriode?.fom }.last().medlemskap
         )
+    }
+
+    @Test
+    fun `hentSterkesteMedlemskap - skal finne sterkeste medlemskap i statsborgerperioden`() {
+        val statsborgerStorbritannia = Statsborgerskap(
+            "GBR",
+            gyldigFraOgMed = LocalDate.of(1990, 4, 1),
+            gyldigTilOgMed = null,
+            bekreftelsesdato = null
+        )
+        val statsborgerPolen = Statsborgerskap(
+            "POL",
+            gyldigFraOgMed = LocalDate.of(1990, 4, 1),
+            gyldigTilOgMed = null,
+            bekreftelsesdato = null
+        )
+        val statsborgerSerbia = Statsborgerskap(
+            "SRB",
+            gyldigFraOgMed = LocalDate.of(1990, 4, 1),
+            gyldigTilOgMed = null,
+            bekreftelsesdato = null
+        )
+        val statsborgerNorge = Statsborgerskap(
+            "NOR",
+            gyldigFraOgMed = LocalDate.of(1990, 4, 1),
+            gyldigTilOgMed = null,
+            bekreftelsesdato = null
+        )
+
+        assertEquals(Medlemskap.EØS, statsborgerskapService.hentSterkesteMedlemskap(statsborgerStorbritannia))
+        assertEquals(Medlemskap.EØS, statsborgerskapService.hentSterkesteMedlemskap(statsborgerPolen))
+        assertEquals(Medlemskap.TREDJELANDSBORGER, statsborgerskapService.hentSterkesteMedlemskap(statsborgerSerbia))
+        assertEquals(Medlemskap.NORDEN, statsborgerskapService.hentSterkesteMedlemskap(statsborgerNorge))
+    }
+
+    @Test
+    fun `hentSterkesteMedlemskap - om statsborgerperiode er ukjent vurderer vi basert på dagens medlemskap`() {
+        val statsborgerStorbritanniaMedNullDatoer = Statsborgerskap(
+            "GBR",
+            gyldigFraOgMed = null,
+            gyldigTilOgMed = null,
+            bekreftelsesdato = null
+        )
+        val statsborgerPolenMedNullDatoer = Statsborgerskap(
+            "POL",
+            gyldigFraOgMed = null,
+            gyldigTilOgMed = null,
+            bekreftelsesdato = null
+        )
+        assertEquals(
+            Medlemskap.TREDJELANDSBORGER,
+            statsborgerskapService.hentSterkesteMedlemskap(statsborgerStorbritanniaMedNullDatoer)
+        )
+        assertEquals(Medlemskap.EØS, statsborgerskapService.hentSterkesteMedlemskap(statsborgerPolenMedNullDatoer))
     }
 }
