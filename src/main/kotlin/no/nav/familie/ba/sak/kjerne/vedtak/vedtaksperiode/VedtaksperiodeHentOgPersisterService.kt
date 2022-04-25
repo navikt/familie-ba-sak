7840912444c6f460bcc704df8c8a92ef5f0f0e22
halvormundal <halvorbmundal@gmail.com>
2022-04-25 11:48:28 +0200

    Legg repository-funksjoner bak servicelogikk

---
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/VedtaksperiodeHentOgPersisterService.kt
@@ -0,0 +1,35 @@
+package no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode
+
+import no.nav.familie.ba.sak.common.Feil
+import no.nav.familie.ba.sak.kjerne.vedtak.Vedtak
+import no.nav.familie.ba.sak.kjerne.vedtak.domene.VedtaksperiodeMedBegrunnelser
+import no.nav.familie.ba.sak.kjerne.vedtak.domene.VedtaksperiodeRepository
+import org.springframework.stereotype.Service
+
+@Service
+class VedtaksperiodeHentOgPersisterService(
+
+    private val vedtaksperiodeRepository: VedtaksperiodeRepository,
+) {
+
+    fun hentVedtaksperiodeThrows(vedtaksperiodeId: Long): VedtaksperiodeMedBegrunnelser =
+        vedtaksperiodeRepository.hentVedtaksperiode(vedtaksperiodeId)
+            ?: throw Feil("Fant ikke vedtaksperiode med id $vedtaksperiodeId")
+
+    fun lagre(vedtaksperiodeMedBegrunnelser: VedtaksperiodeMedBegrunnelser): VedtaksperiodeMedBegrunnelser {
+        validerVedtaksperiodeMedBegrunnelser(vedtaksperiodeMedBegrunnelser)
+        return vedtaksperiodeRepository.save(vedtaksperiodeMedBegrunnelser)
+    }
+
+    fun lagre(vedtaksperiodeMedBegrunnelser: List<VedtaksperiodeMedBegrunnelser>): List<VedtaksperiodeMedBegrunnelser> {
+        vedtaksperiodeMedBegrunnelser.forEach { validerVedtaksperiodeMedBegrunnelser(it) }
+        return vedtaksperiodeRepository.saveAll(vedtaksperiodeMedBegrunnelser)
+    }
+
+    fun slettVedtaksperioderFor(vedtak: Vedtak) {
+        vedtaksperiodeRepository.slettVedtaksperioderFor(vedtak)
+    }
+
+    fun finnVedtaksperioderFor(vedtakId: Long): List<VedtaksperiodeMedBegrunnelser> =
+        vedtaksperiodeRepository.finnVedtaksperioderFor(vedtakId)
+}
