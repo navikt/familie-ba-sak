8001d4686cdfb7d60428a03bfaae05697a5149bf
Ida Merete Enholm <ida.merete.enholm@nav.no>
2022-04-28 12:49:24 +0200

    Skriv om testene til å dekke caset som ikke funket

--- src/test/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/VedtaksperiodeUtilTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/VedtaksperiodeUtilTest.kt
@@ -88,9 +88,16 @@ class VedtaksperiodeUtilTest {
     fun `Skal kun lage en vedtaksperiode med type UTBETALING_MED_REDUKSJON_FRA_SIST_IVERKSATTE_BEHANDLING hvis reduksjonsperiodene er sammenhengende`() {
         val vedtak = lagVedtak()
 
-        val utbetalingsperiode = VedtaksperiodeMedBegrunnelser(
+        val b2bFomUtbetalingsperiode = LocalDate.now().minusYears(2).førsteDagIInneværendeMåned()
+        val utbetalingsperiode1 = VedtaksperiodeMedBegrunnelser(
             vedtak = vedtak,
-            fom = LocalDate.now().minusYears(1).førsteDagIInneværendeMåned(),
+            fom = LocalDate.now().minusYears(3).førsteDagIInneværendeMåned(),
+            tom = b2bFomUtbetalingsperiode.minusMonths(1).sisteDagIMåned(),
+            type = Vedtaksperiodetype.UTBETALING
+        )
+        val utbetalingsperiode2 = VedtaksperiodeMedBegrunnelser(
+            vedtak = vedtak,
+            fom = b2bFomUtbetalingsperiode,
             tom = LocalDate.now().plusYears(1).sisteDagIMåned(),
             type = Vedtaksperiodetype.UTBETALING
         )
@@ -118,26 +125,31 @@ class VedtaksperiodeUtilTest {
         )
 
         val oppdaterteUtbetalingsperioder = oppdaterUtbetalingsperioderMedReduksjonFraForrigeBehandling(
-            utbetalingsperioder = listOf(utbetalingsperiode),
+            utbetalingsperioder = listOf(utbetalingsperiode1, utbetalingsperiode2),
             reduksjonsperioder = listOf(reduksjonsperiode1, reduksjonsperiode2, reduksjonsperiode3)
         )
 
-        Assertions.assertEquals(3, oppdaterteUtbetalingsperioder.size)
+        Assertions.assertEquals(4, oppdaterteUtbetalingsperioder.size)
 
         val periode1 = oppdaterteUtbetalingsperioder[0]
         val periode2 = oppdaterteUtbetalingsperioder[1]
         val periode3 = oppdaterteUtbetalingsperioder[2]
+        val periode4 = oppdaterteUtbetalingsperioder[3]
 
         Assertions.assertEquals(Vedtaksperiodetype.UTBETALING, periode1.type)
-        Assertions.assertEquals(utbetalingsperiode.fom, periode1.fom)
-        Assertions.assertEquals(reduksjonsperiode1.fom?.minusMonths(1)?.sisteDagIMåned(), periode1.tom)
+        Assertions.assertEquals(utbetalingsperiode1.fom, periode1.fom)
+        Assertions.assertEquals(b2bFomUtbetalingsperiode.minusMonths(1)?.sisteDagIMåned(), periode1.tom)
 
-        Assertions.assertEquals(Vedtaksperiodetype.UTBETALING_MED_REDUKSJON_FRA_SIST_IVERKSATTE_BEHANDLING, periode2.type)
-        Assertions.assertEquals(reduksjonsperiode1.fom, periode2.fom)
-        Assertions.assertEquals(reduksjonsperiode3.tom, periode2.tom)
+        Assertions.assertEquals(Vedtaksperiodetype.UTBETALING, periode2.type)
+        Assertions.assertEquals(b2bFomUtbetalingsperiode, periode2.fom)
+        Assertions.assertEquals(reduksjonsperiode1.fom?.minusMonths(1)?.sisteDagIMåned(), periode2.tom)
 
-        Assertions.assertEquals(Vedtaksperiodetype.UTBETALING, periode3.type)
-        Assertions.assertEquals(reduksjonsperiode3.tom?.plusMonths(1)?.førsteDagIInneværendeMåned(), periode3.fom)
-        Assertions.assertEquals(utbetalingsperiode.tom, periode3.tom)
+        Assertions.assertEquals(Vedtaksperiodetype.UTBETALING_MED_REDUKSJON_FRA_SIST_IVERKSATTE_BEHANDLING, periode3.type)
+        Assertions.assertEquals(reduksjonsperiode1.fom, periode3.fom)
+        Assertions.assertEquals(reduksjonsperiode3.tom, periode3.tom)
+
+        Assertions.assertEquals(Vedtaksperiodetype.UTBETALING, periode4.type)
+        Assertions.assertEquals(reduksjonsperiode3.tom?.plusMonths(1)?.førsteDagIInneværendeMåned(), periode4.fom)
+        Assertions.assertEquals(utbetalingsperiode2.tom, periode4.tom)
     }
 }
