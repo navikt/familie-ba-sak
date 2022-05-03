b43d9e4fa625535738b97f824853abb34adbc876
halvormundal <halvorbmundal@gmail.com>
2022-05-03 09:16:51 +0200

    Fiks MDC for tester

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/config/AbstractSpringIntegrationTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/config/AbstractSpringIntegrationTest.kt
@@ -6,6 +6,7 @@ import no.nav.familie.ba.sak.common.DbContainerInitializer
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.TestInstance
+import org.slf4j.MDC
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
@@ -32,6 +33,7 @@ abstract class AbstractSpringIntegrationTest : AbstractMockkSpringRunner() {
 
     init {
         wireMockServer.start()
+        MDC.put("callId", "callId")
     }
 
     @AfterAll
