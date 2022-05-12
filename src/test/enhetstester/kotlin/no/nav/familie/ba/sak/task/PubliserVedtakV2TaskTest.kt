398fcfd6abde4a5effe778f6bf61a9829875b3e0
Meng Zhu <meng.zhu@nav.no>
2022-05-12 20:38:59 +0200

    add mocks

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/task/PubliserVedtakV2TaskTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/task/PubliserVedtakV2TaskTest.kt
@@ -6,11 +6,14 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.slot
 import io.mockk.verify
+import no.nav.familie.ba.sak.common.EnvService
 import no.nav.familie.ba.sak.config.TaskRepositoryWrapper
 import no.nav.familie.ba.sak.statistikk.producer.KafkaProducer
+import no.nav.familie.ba.sak.statistikk.stønadsstatistikk.StønadsstatistikkService
 import no.nav.familie.eksterne.kontrakter.VedtakDVHV2
 import no.nav.familie.prosessering.domene.Task
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
@@ -23,8 +26,19 @@ class PubliserVedtakV2TaskTest {
     @MockK(relaxed = true)
     private lateinit var kafkaProducerMock: KafkaProducer
 
+    @MockK(relaxed = true)
+    private lateinit var stønadsstatistikkService: StønadsstatistikkService
+
+    @MockK
+    private lateinit var env: EnvService
+
     @InjectMockKs
     private lateinit var publiserVedtakV2Task: PubliserVedtakV2Task
+
+    @BeforeEach
+    fun initMocks() {
+        every { env.erProd() } returns false
+    }
 
     @Test
     fun skalOppretteTask() {
