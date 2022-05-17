9efdf29e7b6e7feae4be6096a3d6cca91cd66037
Jørn Ola Birkeland <jorn.ola.birkeland@nav.no>
2022-05-17 19:04:57 +0200

    Legger til funksjon på ValutakursService for å tilpasse valutakurs til utenlandsk periodebeløp

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/eøs/valutakurs/ValutakursService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/eøs/valutakurs/ValutakursService.kt
@@ -1,6 +1,10 @@
 package no.nav.familie.ba.sak.kjerne.eøs.valutakurs
 
 import no.nav.familie.ba.sak.kjerne.eøs.felles.PeriodeOgBarnSkjemaService
+import no.nav.familie.ba.sak.kjerne.eøs.felles.beregning.tilTidslinjerForBarna
+import no.nav.familie.ba.sak.kjerne.eøs.felles.medBehandlingId
+import no.nav.familie.ba.sak.kjerne.eøs.utenlandskperiodebeløp.UtenlandskPeriodebeløpService
+import no.nav.familie.ba.sak.kjerne.eøs.valutakurs.beregning.tilpassValutakurserTilUtenlandskePeriodebeløp
 import no.nav.familie.ba.sak.kjerne.steg.TilbakestillBehandlingService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -9,11 +13,15 @@ import org.springframework.transaction.annotation.Transactional
 class ValutakursService(
     repository: ValutakursRepository,
     tilbakestillBehandlingService: TilbakestillBehandlingService,
+    private val utenlandskPeriodebeløpService: UtenlandskPeriodebeløpService
 ) {
     val serviceDelegate = PeriodeOgBarnSkjemaService(
         repository,
         tilbakestillBehandlingService
     )
+
+    fun hentValutakurser(behandlingId: Long) =
+        serviceDelegate.hentMedBehandlingId(behandlingId)
 
     fun oppdaterValutakurs(behandlingId: Long, valutakurs: Valutakurs) =
         serviceDelegate.endreSkjemaer(behandlingId, valutakurs)
@@ -24,4 +32,19 @@ class ValutakursService(
     @Transactional
     fun kopierOgErstattValutakurser(fraBehandlingId: Long, tilBehandlingId: Long) =
         serviceDelegate.kopierOgErstattSkjemaer(fraBehandlingId, tilBehandlingId)
+
+    @Transactional
+    fun tilpassUtenlandskPeriodebeløpTilKompetanser(behandlingId: Long) {
+        val gjeldendeValutakurser = hentValutakurser(behandlingId)
+        val barnasUtenlandskePeriodebeløpTidslinjer = utenlandskPeriodebeløpService
+            .hentUtenlandskePeriodebeløp(behandlingId)
+            .tilTidslinjerForBarna()
+
+        val oppdaterteValutakurser = tilpassValutakurserTilUtenlandskePeriodebeløp(
+            gjeldendeValutakurser,
+            barnasUtenlandskePeriodebeløpTidslinjer
+        ).medBehandlingId(behandlingId)
+
+        serviceDelegate.lagreSkjemaDifferanse(gjeldendeValutakurser, oppdaterteValutakurser)
+    }
 }
