e21a121bf9ff6a2875280c4ee24e28c00614bc58
halvormundal <halvorbmundal@gmail.com>
2022-05-19 17:12:02 +0200

    legg til test på beregnDifferanseOrdinær

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/eøs/differanseberegning/DifferanseberegningsUtilsTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/eøs/differanseberegning/DifferanseberegningsUtilsTest.kt
@@ -4,85 +4,71 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class DifferanseberegningsUtilsTest {
+    val utbetalingsbeløpNorge = 2000
 
     @Test
-    fun `Skal gi riktig utenlandsk beløp for årlig utbetaling`() {
+    fun `Skal gi riktig differanse for årlig utbetaling ved ordinær sekundærlandssak`() {
 
         val utenlandskSatsÅrlig = 1200
 
-        Assertions.assertEquals(
-            1000,
-            beregnUtbetalingsbeløpUtlandINok(
-                satsUtland = utenlandskSatsÅrlig,
-                kurs = 10.0,
-                intervall = Intervall.ÅRLIG,
-                erSkuddår = false
-            )
+        val utbetalingsbeløpUtlandINok = beregnUtbetalingsbeløpUtlandINok(
+            satsUtland = utenlandskSatsÅrlig, kurs = 10.0, intervall = Intervall.ÅRLIG, erSkuddår = false
         )
+
+        Assertions.assertEquals(1000, utbetalingsbeløpUtlandINok)
+        Assertions.assertEquals(1000, beregnDifferanseOrdinær(utbetalingsbeløpNorge, utbetalingsbeløpUtlandINok))
     }
 
     @Test
-    fun `Skal gi riktig utenlandsk beløp for kvartalvis utbetaling`() {
+    fun `Skal gi riktig differanse for kvartalvis utbetaling ved ordinær sekundærlandssak`() {
 
         val utenlandskSatsKvartalsvis = 400
 
-        Assertions.assertEquals(
-            1000,
-            beregnUtbetalingsbeløpUtlandINok(
-                satsUtland = utenlandskSatsKvartalsvis,
-                kurs = 10.0,
-                intervall = Intervall.KVARTALSVIS,
-                erSkuddår = false
-            )
+        val utbetalingsbeløpUtlandINok = beregnUtbetalingsbeløpUtlandINok(
+            satsUtland = utenlandskSatsKvartalsvis, kurs = 10.0, intervall = Intervall.KVARTALSVIS, erSkuddår = false
         )
+
+        Assertions.assertEquals(1000, utbetalingsbeløpUtlandINok)
+        Assertions.assertEquals(1000, beregnDifferanseOrdinær(utbetalingsbeløpNorge, utbetalingsbeløpUtlandINok))
     }
 
     @Test
-    fun `Skal gi riktig utenlandsk beløp for månedlig utbetaling`() {
+    fun `Skal gi riktig differanse for månedlig utbetaling ved ordinær sekundærlandssak`() {
 
         val utenlandskSatsMåendlig = 100
 
-        Assertions.assertEquals(
-            1000,
-            beregnUtbetalingsbeløpUtlandINok(
-                satsUtland = utenlandskSatsMåendlig,
-                kurs = 10.0,
-                intervall = Intervall.MÅNEDLIG,
-                erSkuddår = false
-            )
+        val utbetalingsbeløpUtlandINok = beregnUtbetalingsbeløpUtlandINok(
+            satsUtland = utenlandskSatsMåendlig, kurs = 10.0, intervall = Intervall.MÅNEDLIG, erSkuddår = false
         )
+
+        Assertions.assertEquals(1000, utbetalingsbeløpUtlandINok)
+        Assertions.assertEquals(1000, beregnDifferanseOrdinær(utbetalingsbeløpNorge, utbetalingsbeløpUtlandINok))
     }
 
     @Test
-    fun `Skal gi riktig utenlandsk beløp for ukentlig utbetaling`() {
+    fun `Skal gi riktig differanse for ukentlig utbetaling ved ordinær sekundærlandssak`() {
 
         val utenlandskSatsUkentlig = 25
 
-        Assertions.assertEquals(
-            1086,
-            beregnUtbetalingsbeløpUtlandINok(
-                satsUtland = utenlandskSatsUkentlig,
-                kurs = 10.0,
-                intervall = Intervall.UKENTLIG,
-                erSkuddår = false
-            )
+        val utbetalingsbeløpUtlandINok = beregnUtbetalingsbeløpUtlandINok(
+            satsUtland = utenlandskSatsUkentlig, kurs = 10.0, intervall = Intervall.UKENTLIG, erSkuddår = false
         )
+
+        Assertions.assertEquals(1086, utbetalingsbeløpUtlandINok)
+        Assertions.assertEquals(914, beregnDifferanseOrdinær(utbetalingsbeløpNorge, utbetalingsbeløpUtlandINok))
     }
 
     @Test
-    fun `Skal gi riktig utenlandsk beløp for ukentlig utbetaling ved skuddår`() {
+    fun `Skal gi riktig differanse for ukentlig utbetaling ved skuddår ved ordinær sekundærlandssak`() {
 
         val utenlandskSatsUkentlig = 25
 
-        Assertions.assertEquals(
-            1089,
-            beregnUtbetalingsbeløpUtlandINok(
-                satsUtland = utenlandskSatsUkentlig,
-                kurs = 10.0,
-                intervall = Intervall.UKENTLIG,
-                erSkuddår = true
-            )
+        val utbetalingsbeløpUtlandINok = beregnUtbetalingsbeløpUtlandINok(
+            satsUtland = utenlandskSatsUkentlig, kurs = 10.0, intervall = Intervall.UKENTLIG, erSkuddår = true
         )
+
+        Assertions.assertEquals(1089, utbetalingsbeløpUtlandINok)
+        Assertions.assertEquals(911, beregnDifferanseOrdinær(utbetalingsbeløpNorge, utbetalingsbeløpUtlandINok))
     }
 
     @Test
