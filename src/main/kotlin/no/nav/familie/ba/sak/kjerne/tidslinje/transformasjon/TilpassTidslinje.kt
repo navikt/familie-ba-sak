ffabe692f76af3bcf2b9d645c7913a4dabb55f3a
Jørn Ola Birkeland <jorn.ola.birkeland@nav.no>
2022-05-21 15:17:55 +0200

    Endrer beskrivelse av Tidslinje.tilpassTil, som har endret seg

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/tidslinje/transformasjon/TilpassTidslinje.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/tidslinje/transformasjon/TilpassTidslinje.kt
@@ -8,16 +8,15 @@ import no.nav.familie.ba.sak.kjerne.tidslinje.tid.Tidsenhet
  * Extension-metode for å tilpasse en tidslinje til en [mønsterTidslinje]
  * Returnerer en tidlinje av samme type som [this] med samme fordeling av innhold som [mønsterTidslinje]
  * Hvis [mønsterTidslinje] mangler verdi (null) i en periode, så vil resultet også mangle verdi i perioden
- * Hivs [mønsterTidslinje] har innhold som mangler i [this], vil [nyttInnhold] kalles for å generere innholdet
- * Hvis begge har innhold, vil innholdet fra [this] bli brukt
+ * Hivs [mønsterTidslinje] har innhold, vil [tilpassInnhold] kalles for eventuelt å generere innholdet
  * Resulterende tidslinje vil strekke seg fra tidligste fra-og-med til seneste til-og-med fra begge tidslinjer
  */
 fun <I, M, T : Tidsenhet> Tidslinje<I, T>.tilpassTil(
     mønsterTidslinje: Tidslinje<M, T>,
-    nyttInnhold: (I?, M) -> I
+    tilpassInnhold: (I?, M) -> I
 ) = this.snittKombinerMed(mønsterTidslinje) { thisInnhold: I?, mønsterInnhold: M? ->
     when {
         mønsterInnhold == null -> null
-        else -> thisInnhold ?: nyttInnhold(thisInnhold, mønsterInnhold)
+        else -> tilpassInnhold(thisInnhold, mønsterInnhold)
     }
 }
