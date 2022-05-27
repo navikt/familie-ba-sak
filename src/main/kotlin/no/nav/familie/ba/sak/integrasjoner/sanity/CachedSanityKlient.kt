a5069d831fc4e97421e85960edf18bbb916e23bd
halvormundal <halvorbmundal@gmail.com>
2022-05-27 17:23:34 +0200

    bruk hentEØSBegrunnelser i hentEØSBegrunnelserCached

--- src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/sanity/CachedSanityKlient.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/sanity/CachedSanityKlient.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.ba.sak.integrasjoner.sanity
 
 import no.nav.familie.ba.sak.kjerne.brev.domene.SanityBegrunnelse
+import no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser.SanityEØSBegrunnelse
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Component
@@ -16,7 +17,7 @@ class CachedSanityKlient(
     }
 
     @Cacheable("sanityEØSBegrunnelser", cacheManager = "shortCache")
-    fun hentEØSBegrunnelserCached(): List<SanityBegrunnelse> {
-        return hentNasjonaleBegrunnelser(datasett = sanityDatasett)
+    fun hentEØSBegrunnelserCached(): List<SanityEØSBegrunnelse> {
+        return hentEØSBegrunnelser(datasett = sanityDatasett)
     }
 }
