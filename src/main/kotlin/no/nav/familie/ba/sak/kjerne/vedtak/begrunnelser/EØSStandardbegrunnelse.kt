314ef2e385193719d23620ab4f45952965f6b2c2
halvormundal <halvorbmundal@gmail.com>
2022-05-27 16:46:20 +0200

    Legg til enumverdier i EØSStandardbegrunnelse

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/begrunnelser/EØSStandardbegrunnelse.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/begrunnelser/EØSStandardbegrunnelse.kt
@@ -1,3 +1,86 @@
 package no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser
 
-enum class EØSStandardbegrunnelse
+enum class EØSStandardbegrunnelse : IVedtakBegrunnelse {
+    INNVILGET_PRIMÆRLAND_UK_STANDARD {
+        override val sanityApiNavn = "innvilgetPrimarlandUKStandard"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_BARNET_BOR_I_NORGE {
+        override val sanityApiNavn = "innvilgetPrimarlandBarnetBorINorge"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_TILLEGGSBEGRUNNELSE_REFUSJON {
+        override val sanityApiNavn = "innvilgetTilleggsbegrunnelseRefusjon"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_BARNETRYGD_ALLEREDE_UTBETALT {
+        override val sanityApiNavn = "innvilgetPrimarlandBarnetrygdAlleredeUtbetalt"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_UK_BARNETRYGD_ALLEREDEUTBETALT {
+        override val sanityApiNavn = "innvilgetPrimarlandUkBarnetrygdAlleredeUtbetalt"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_TILLEGGSBEGRUNNELSE_REFUSJON_UAVKLART {
+        override val sanityApiNavn = "innvilgetTilleggsbegrunnelseRefusjonUavklart"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_BEGGE_FORELDRE_BOSATT_I_NORGE {
+        override val sanityApiNavn = "innvilgetPrimarlandBeggeForeldreBosattINorge"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_UK_OG_UTLAND_STANDARD {
+        override val sanityApiNavn = "innvilgetPrimarlandUKOgUtlandStandard"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_SÆRKULLSBARN_ANDRE_BARN_OVERTATT_ANSVAR {
+        override val sanityApiNavn = "innvilgetPrimarlandSaerkullsbarnAndreBarnOvertattAnsvar"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_UK_TO_ARBEIDSLAND_NORGE_UTBETALER {
+        override val sanityApiNavn = "innvilgetPrimarlandUkToArbeidslandNorgeUtbetaler"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_TO_ARBEIDSLAND_NORGE_UTBETALER {
+        override val sanityApiNavn = "innvilgetPrimarlandToArbeidslandNorgeUtbetaler"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_STANDARD {
+        override val sanityApiNavn = "innvilgetPrimarlandStandard"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_UK_ALENEANSVAR {
+        override val sanityApiNavn = "innvilgetPrimarlandUKAleneansvar"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_TILLEGGSBEGRUNNELSE_UTBETALING_TIL_ANNEN_FORELDER {
+        override val sanityApiNavn = "innvilgetTilleggsbegrunnelseUtbetalingTilAnnenForelder"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_BARNET_FLYTTET_TIL_NORGE {
+        override val sanityApiNavn = "innvilgetPrimarlandBarnetFlyttetTilNorge"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_BEGGE_FORELDRE_JOBBER_I_NORGE {
+        override val sanityApiNavn = "innvilgetPrimarlandBeggeForeldreJobberINorge"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_TO_ARBEIDSLAND_ANNET_LAND_UTBETALER {
+        override val sanityApiNavn = "innvilgetPrimarlandToArbeidslandAnnetLandUtbetaler"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_SÆRKULLSBARN_ANDRE_BARN {
+        override val sanityApiNavn = "innvilgetPrimarlandSarkullsbarnAndreBarn"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_UK_TO_ARBEIDSLAND_ANNET_LAND_UTBETALER {
+        override val sanityApiNavn = "innvilgetPrimarlandUkToArbeidslandAnnetLandUtbetaler"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    },
+    INNVILGET_PRIMÆRLAND_ALENEANSVAR {
+        override val sanityApiNavn = "innvilgetPrimarlandAleneansvar"
+        override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
+    };
+
+    override val kanDelesOpp: Boolean = false
+}
