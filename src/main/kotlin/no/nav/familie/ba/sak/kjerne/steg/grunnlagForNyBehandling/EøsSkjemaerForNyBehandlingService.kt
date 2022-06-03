2721200dfea099bed671bfdf457c73c814a7c928
Ida Merete Enholm <ida.merete.enholm@nav.no>
2022-06-03 10:17:57 +0200

    Bruk BehandlingId i stedet for Long

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/steg/grunnlagForNyBehandling/EøsSkjemaerForNyBehandlingService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/steg/grunnlagForNyBehandling/EøsSkjemaerForNyBehandlingService.kt
@@ -3,6 +3,7 @@ package no.nav.familie.ba.sak.kjerne.steg.grunnlagForNyBehandling
 import no.nav.familie.ba.sak.config.FeatureToggleConfig
 import no.nav.familie.ba.sak.config.FeatureToggleService
 import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandling
+import no.nav.familie.ba.sak.kjerne.eøs.felles.BehandlingId
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.KompetanseService
 import no.nav.familie.ba.sak.kjerne.eøs.utenlandskperiodebeløp.UtenlandskPeriodebeløpService
 import no.nav.familie.ba.sak.kjerne.eøs.valutakurs.ValutakursService
@@ -19,16 +20,16 @@ class EøsSkjemaerForNyBehandlingService(
     fun kopierEøsSkjemaer(behandling: Behandling, forrigeBehandlingSomErVedtatt: Behandling?) {
         if (featureToggleService.isEnabled(FeatureToggleConfig.KAN_BEHANDLE_EØS) && forrigeBehandlingSomErVedtatt != null) {
             kompetanseService.kopierOgErstattKompetanser(
-                fraBehandlingId = forrigeBehandlingSomErVedtatt.id,
-                tilBehandlingId = behandling.id
+                fraBehandlingId = BehandlingId(forrigeBehandlingSomErVedtatt.id),
+                tilBehandlingId = BehandlingId(behandling.id)
             )
             utenlandskPeriodebeløpService.kopierOgErstattUtenlandskPeriodebeløp(
-                fraBehandlingId = forrigeBehandlingSomErVedtatt.id,
-                tilBehandlingId = behandling.id
+                fraBehandlingId = BehandlingId(forrigeBehandlingSomErVedtatt.id),
+                tilBehandlingId = BehandlingId(behandling.id)
             )
             valutakursService.kopierOgErstattValutakurser(
-                fraBehandlingId = forrigeBehandlingSomErVedtatt.id,
-                tilBehandlingId = behandling.id
+                fraBehandlingId = BehandlingId(forrigeBehandlingSomErVedtatt.id),
+                tilBehandlingId = BehandlingId(behandling.id)
             )
         }
     }
