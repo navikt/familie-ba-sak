1a253bad0832e3afe4f758e287b6e2d9355c38b7
halvorbmundal <halvorbmundal@users.noreply.github.com>
2022-06-07 16:32:42 +0200

    Feature/eøs hjemler og robustgjøring (#2550)
    * Legg til eøs-hjemler i objektet fra Sanity. Robustgjør kallet mot sanity
    
    Signed-off-by: halvormundal <halvorbmundal@gmail.com>
    
    * Bruk ÆØÅ når vi konverterer fra RestSanityEØSBegrunnelse til SanityEØSBegrunnelse
    
    Signed-off-by: halvormundal <halvorbmundal@gmail.com>
    
    Co-authored-by: Ida Merete Enholm <ida.merete.enholm@nav.no>

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/brev/domene/eøs/EØSBegrunnelseUtil.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/brev/domene/eøs/EØSBegrunnelseUtil.kt
@@ -3,6 +3,7 @@ package no.nav.familie.ba.sak.kjerne.brev.domene.eøs
 import no.nav.familie.ba.sak.kjerne.brev.domene.MinimertKompetanse
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.AnnenForeldersAktivitet
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.KompetanseResultat
+import no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser.hentBarnetsBostedslandFraLandkode
 
 fun hentKompetanserForEØSBegrunnelse(
     eøsBegrunnelseMedTriggere: EØSBegrunnelseMedTriggere,
@@ -23,8 +24,7 @@ fun EØSBegrunnelseMedTriggere.erGyldigForKompetanseMedData(
 ) =
     sanityEØSBegrunnelse.annenForeldersAktivitet
         .contains(annenForeldersAktivitetFraKompetanse) &&
-        sanityEØSBegrunnelse.barnetsBostedsland.map { it.tilLandkode() }
-            .contains(barnetsBostedslandFraKompetanse) &&
+        sanityEØSBegrunnelse.barnetsBostedsland.contains(hentBarnetsBostedslandFraLandkode(barnetsBostedslandFraKompetanse)) &&
         sanityEØSBegrunnelse.kompetanseResultat.contains(
             resultatFraKompetanse
         )
