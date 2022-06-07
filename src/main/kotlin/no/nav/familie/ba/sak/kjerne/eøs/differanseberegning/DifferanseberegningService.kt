214b91faaaabea323ab436b853ee9102fd6d8d57
Jørn Ola Birkeland <jorn.ola.birkeland@nav.no>
2022-06-07 18:18:59 +0200

    Fjerner spring-sykler som krasjer oppstart og herder diff-beregningstjenestene

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/eøs/differanseberegning/DifferanseberegningService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/eøs/differanseberegning/DifferanseberegningService.kt
@@ -3,13 +3,9 @@ package no.nav.familie.ba.sak.kjerne.eøs.differanseberegning
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelse
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelseDomene
 import no.nav.familie.ba.sak.kjerne.beregning.domene.TilkjentYtelse
-import no.nav.familie.ba.sak.kjerne.beregning.domene.TilkjentYtelseRepository
-import no.nav.familie.ba.sak.kjerne.eøs.felles.BehandlingId
 import no.nav.familie.ba.sak.kjerne.eøs.felles.beregning.tilSeparateTidslinjerForBarna
 import no.nav.familie.ba.sak.kjerne.eøs.utenlandskperiodebeløp.UtenlandskPeriodebeløp
-import no.nav.familie.ba.sak.kjerne.eøs.utenlandskperiodebeløp.UtenlandskPeriodebeløpService
 import no.nav.familie.ba.sak.kjerne.eøs.valutakurs.Valutakurs
-import no.nav.familie.ba.sak.kjerne.eøs.valutakurs.ValutakursService
 import no.nav.familie.ba.sak.kjerne.personident.Aktør
 import no.nav.familie.ba.sak.kjerne.tidslinje.Periode
 import no.nav.familie.ba.sak.kjerne.tidslinje.Tidslinje
@@ -21,85 +17,49 @@ import no.nav.familie.ba.sak.kjerne.tidslinje.tid.Måned
 import no.nav.familie.ba.sak.kjerne.tidslinje.tid.MånedTidspunkt.Companion.tilTidspunkt
 import no.nav.familie.ba.sak.kjerne.tidslinje.tidslinje
 import no.nav.familie.ba.sak.kjerne.tidslinje.transformasjon.beskjærEtter
-import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
 
-@Service
-class DifferanseberegningService(
-    private val valutakursService: ValutakursService,
-    private val utenlandskPeriodebeløpService: UtenlandskPeriodebeløpService,
-    private val tilkjentYtelseRepository: TilkjentYtelseRepository,
-) {
-    @Transactional
-    fun beregnDifferanseFraTilkjentYtelse(behandlingId: BehandlingId, tilkjentYtelse: TilkjentYtelse) {
-        val valutakurser = valutakursService.hentValutakurser(behandlingId)
-        val utenlandskePeriodebeløp = utenlandskPeriodebeløpService.hentUtenlandskePeriodebeløp(behandlingId)
+fun beregnDifferanse(
+    tilkjentYtelse: TilkjentYtelse,
+    utenlandskePeriodebeløp: Collection<UtenlandskPeriodebeløp>,
+    valutakurser: Collection<Valutakurs>
+): TilkjentYtelse {
 
-        val nyTilkjentYtelse = beregnDifferanse(tilkjentYtelse, utenlandskePeriodebeløp, valutakurser)
-        tilkjentYtelseRepository.save(nyTilkjentYtelse)
+    if (utenlandskePeriodebeløp.isEmpty() && valutakurser.isEmpty())
+        return tilkjentYtelse
+
+    val utenlandskePeriodebeløpTidslinjer = utenlandskePeriodebeløp.tilSeparateTidslinjerForBarna()
+    val valutakursTidslinjer = valutakurser.tilSeparateTidslinjerForBarna()
+    val andelTilkjentYtelseTidslinjer = tilkjentYtelse.tilSeparateTidslinjerForBarna()
+
+    val barnMedAlleTidslinjer: Set<Aktør> =
+        utenlandskePeriodebeløpTidslinjer.keys
+            .intersect(valutakursTidslinjer.keys)
+            .intersect(andelTilkjentYtelseTidslinjer.keys)
+
+    val barnasAndelerTilkjentYtelse = barnMedAlleTidslinjer.flatMap { aktør ->
+        val utenlandskePeriodebeløpTidslinje = utenlandskePeriodebeløpTidslinjer.getValue(aktør)
+        val valutakursTidslinje = valutakursTidslinjer.getValue(aktør)
+        val andelTilkjentYtelseTidslinje = andelTilkjentYtelseTidslinjer.getValue(aktør)
+
+        val utenlandskePeriodebeløpINorskeKroner =
+            utenlandskePeriodebeløpTidslinje.tidspunktKombinerMed(valutakursTidslinje) { tidspunkt, upb, vk ->
+                upb.multipliserMed(vk, tidspunkt)
+            }
+
+        val differanseberegnetAndelTilkjentYtelseTidslinjer =
+            andelTilkjentYtelseTidslinje.kombinerUtenNullMed(utenlandskePeriodebeløpINorskeKroner) { aty, beløp ->
+                aty.medUtenlandskPeriodebeløp(beløp)
+            }
+
+        differanseberegnetAndelTilkjentYtelseTidslinjer
+            .filtrerIkkeNull()
+            .beskjærEtter(andelTilkjentYtelseTidslinje) // Skal ikke være lenger enn denne
+            .filtrer { it!!.kalkulertUtbetalingsbeløp > 0 } // Vi sender bare positive beløp til oppdragssystemet
+            .tilAndelTilkjentYtelse()
     }
 
-    @Transactional
-    fun beregnDifferanseFraUtenlandskePeridebeløp(
-        behandlingId: BehandlingId,
-        utenlandskePeriodebeløp: Collection<UtenlandskPeriodebeløp>
-    ) {
-        val tilkjentYtelse = tilkjentYtelseRepository.findByBehandling(behandlingId.id)
-        val valutakurser = valutakursService.hentValutakurser(behandlingId)
-
-        val nyTilkjentYtelse = beregnDifferanse(tilkjentYtelse, utenlandskePeriodebeløp, valutakurser)
-        tilkjentYtelseRepository.save(nyTilkjentYtelse)
-    }
-
-    @Transactional
-    fun beregnDifferanseFraValutakurser(behandlingId: BehandlingId, valutakurser: Collection<Valutakurs>) {
-        val tilkjentYtelse = tilkjentYtelseRepository.findByBehandling(behandlingId.id)
-        val utenlandskePeriodebeløp = utenlandskPeriodebeløpService.hentUtenlandskePeriodebeløp(behandlingId)
-
-        val nyTilkjentYtelse = beregnDifferanse(tilkjentYtelse, utenlandskePeriodebeløp, valutakurser)
-        tilkjentYtelseRepository.save(nyTilkjentYtelse)
-    }
-
-    private fun beregnDifferanse(
-        tilkjentYtelse: TilkjentYtelse,
-        utenlandskePeriodebeløp: Collection<UtenlandskPeriodebeløp>,
-        valutakurser: Collection<Valutakurs>
-    ): TilkjentYtelse {
-
-        val utenlandskePeriodebeløpTidslinjer = utenlandskePeriodebeløp.tilSeparateTidslinjerForBarna()
-        val valutakursTidslinjer = valutakurser.tilSeparateTidslinjerForBarna()
-        val andelTilkjentYtelseTidslinjer = tilkjentYtelse.tilSeparateTidslinjerForBarna()
-
-        val barnMedAlleTidslinjer: Set<Aktør> =
-            utenlandskePeriodebeløpTidslinjer.keys
-                .intersect(valutakursTidslinjer.keys)
-                .intersect(andelTilkjentYtelseTidslinjer.keys)
-
-        val barnasAndelerTilkjentYtelse = barnMedAlleTidslinjer.flatMap { aktør ->
-            val utenlandskePeriodebeløpTidslinje = utenlandskePeriodebeløpTidslinjer.getValue(aktør)
-            val valutakursTidslinje = valutakursTidslinjer.getValue(aktør)
-            val andelTilkjentYtelseTidslinje = andelTilkjentYtelseTidslinjer.getValue(aktør)
-
-            val utenlandskePeriodebeløpINorskeKroner =
-                utenlandskePeriodebeløpTidslinje.tidspunktKombinerMed(valutakursTidslinje) { tidspunkt, upb, vk ->
-                    upb.multipliserMed(vk, tidspunkt)
-                }
-
-            val differanseberegnetAndelTilkjentYtelseTidslinjer =
-                andelTilkjentYtelseTidslinje.kombinerUtenNullMed(utenlandskePeriodebeløpINorskeKroner) { aty, beløp ->
-                    aty.medUtenlandskPeriodebeløp(beløp)
-                }
-
-            differanseberegnetAndelTilkjentYtelseTidslinjer
-                .filtrerIkkeNull()
-                .beskjærEtter(andelTilkjentYtelseTidslinje) // Skal ikke være lenger enn denne
-                .filtrer { it!!.kalkulertUtbetalingsbeløp > 0 } // Vi sender bare positive beløp til oppdragssystemet
-                .tilAndelTilkjentYtelse()
-        }
-
-        return tilkjentYtelse.kopierOgErstattBarnasAndeler(barnasAndelerTilkjentYtelse)
-    }
+    return tilkjentYtelse.kopierOgErstattBarnasAndeler(barnasAndelerTilkjentYtelse)
 }
 
 fun TilkjentYtelse.tilSeparateTidslinjerForBarna(): Map<Aktør, Tidslinje<AndelTilkjentYtelse, Måned>> {
--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/eøs/differanseberegning/TilpassDifferanseberegning.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/eøs/differanseberegning/TilpassDifferanseberegning.kt
@@ -2,38 +2,64 @@ package no.nav.familie.ba.sak.kjerne.eøs.differanseberegning
 
 import no.nav.familie.ba.sak.kjerne.beregning.TilkjentYtelseEndretAbonnent
 import no.nav.familie.ba.sak.kjerne.beregning.domene.TilkjentYtelse
+import no.nav.familie.ba.sak.kjerne.beregning.domene.TilkjentYtelseRepository
 import no.nav.familie.ba.sak.kjerne.eøs.felles.BehandlingId
 import no.nav.familie.ba.sak.kjerne.eøs.felles.PeriodeOgBarnSkjemaEndringAbonnent
+import no.nav.familie.ba.sak.kjerne.eøs.felles.PeriodeOgBarnSkjemaRepository
 import no.nav.familie.ba.sak.kjerne.eøs.utenlandskperiodebeløp.UtenlandskPeriodebeløp
 import no.nav.familie.ba.sak.kjerne.eøs.valutakurs.Valutakurs
 import org.springframework.stereotype.Service
 
 @Service
 class TilpassDifferanseberegningEtterTilkjentYtelseService(
-    private val differanseberegningService: DifferanseberegningService
+    private val valutakursRepository: PeriodeOgBarnSkjemaRepository<Valutakurs>,
+    private val utenlandskPeriodebeløpRepository: PeriodeOgBarnSkjemaRepository<UtenlandskPeriodebeløp>,
+    private val tilkjentYtelseRepository: TilkjentYtelseRepository
 ) : TilkjentYtelseEndretAbonnent {
     override fun endretTilkjentYtelse(tilkjentYtelse: TilkjentYtelse) {
-        differanseberegningService.beregnDifferanseFraTilkjentYtelse(
-            BehandlingId(tilkjentYtelse.behandling.id),
-            tilkjentYtelse
-        )
+        val behandlingId = BehandlingId(tilkjentYtelse.behandling.id)
+        val valutakurser = valutakursRepository.finnFraBehandlingId(behandlingId.id)
+        val utenlandskePeriodebeløp = utenlandskPeriodebeløpRepository.finnFraBehandlingId(behandlingId.id)
+
+        val nyTilkjentYtelse = beregnDifferanse(tilkjentYtelse, utenlandskePeriodebeløp, valutakurser)
+        tilkjentYtelseRepository.lagreVedEndring(tilkjentYtelse, nyTilkjentYtelse)
     }
 }
 
 @Service
 class TilpassDifferanseberegningEtterUtenlandskPeriodebeløpService(
-    private val differanseberegningService: DifferanseberegningService
+    private val valutakursRepository: PeriodeOgBarnSkjemaRepository<Valutakurs>,
+    private val tilkjentYtelseRepository: TilkjentYtelseRepository
 ) : PeriodeOgBarnSkjemaEndringAbonnent<UtenlandskPeriodebeløp> {
-    override fun skjemaerEndret(behandlingId: BehandlingId, endretTil: Collection<UtenlandskPeriodebeløp>) {
-        differanseberegningService.beregnDifferanseFraUtenlandskePeridebeløp(behandlingId, endretTil)
+    override fun skjemaerEndret(
+        behandlingId: BehandlingId,
+        utenlandskePeriodebeløp: Collection<UtenlandskPeriodebeløp>
+    ) {
+        val tilkjentYtelse = tilkjentYtelseRepository.findByBehandlingOptional(behandlingId.id) ?: return
+        val valutakurser = valutakursRepository.finnFraBehandlingId(behandlingId.id)
+
+        val nyTilkjentYtelse = beregnDifferanse(tilkjentYtelse, utenlandskePeriodebeløp, valutakurser)
+        tilkjentYtelseRepository.lagreVedEndring(tilkjentYtelse, nyTilkjentYtelse)
     }
 }
 
 @Service
 class TilpassDifferanseberegningEtterValutakursService(
-    private val differanseberegningService: DifferanseberegningService
+    private val utenlandskPeriodebeløpRepository: PeriodeOgBarnSkjemaRepository<UtenlandskPeriodebeløp>,
+    private val tilkjentYtelseRepository: TilkjentYtelseRepository
 ) : PeriodeOgBarnSkjemaEndringAbonnent<Valutakurs> {
-    override fun skjemaerEndret(behandlingId: BehandlingId, endretTil: Collection<Valutakurs>) {
-        differanseberegningService.beregnDifferanseFraValutakurser(behandlingId, endretTil)
+    override fun skjemaerEndret(behandlingId: BehandlingId, valutakurser: Collection<Valutakurs>) {
+        val tilkjentYtelse = tilkjentYtelseRepository.findByBehandlingOptional(behandlingId.id) ?: return
+        val utenlandskePeriodebeløp = utenlandskPeriodebeløpRepository.finnFraBehandlingId(behandlingId.id)
+
+        val nyTilkjentYtelse = beregnDifferanse(tilkjentYtelse, utenlandskePeriodebeløp, valutakurser)
+        tilkjentYtelseRepository.lagreVedEndring(tilkjentYtelse, nyTilkjentYtelse)
+    }
+}
+
+fun TilkjentYtelseRepository.lagreVedEndring(forrigeTilkjentYtelse: TilkjentYtelse, nyTilkjentYtelse: TilkjentYtelse) {
+    if (forrigeTilkjentYtelse != nyTilkjentYtelse) {
+        this.delete(forrigeTilkjentYtelse)
+        this.save(nyTilkjentYtelse)
     }
 }
