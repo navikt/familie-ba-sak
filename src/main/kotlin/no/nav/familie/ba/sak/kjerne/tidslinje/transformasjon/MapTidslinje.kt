da6c219e2bd756e92665c9877a3f2493e76e10f0
Jørn Ola Birkeland <jorn.ola.birkeland@nav.no>
2022-06-12 16:16:32 +0200

    Utvider og endrer litt på tidslinje-rammeverket. Ny kombinator, som tar inn tid, samt endrer 'map' til å komprimere

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/tidslinje/transformasjon/MapTidslinje.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/tidslinje/transformasjon/MapTidslinje.kt
@@ -1,18 +1,19 @@
 package no.nav.familie.ba.sak.kjerne.tidslinje.transformasjon
 
-import no.nav.familie.ba.sak.kjerne.tidslinje.Periode
 import no.nav.familie.ba.sak.kjerne.tidslinje.Tidslinje
-import no.nav.familie.ba.sak.kjerne.tidslinje.komposisjon.TidslinjeMedAvhengigheter
+import no.nav.familie.ba.sak.kjerne.tidslinje.komposisjon.TidslinjeSomStykkerOppTiden
+import no.nav.familie.ba.sak.kjerne.tidslinje.komposisjon.innholdForTidspunkt
 import no.nav.familie.ba.sak.kjerne.tidslinje.tid.Tidsenhet
+import no.nav.familie.ba.sak.kjerne.tidslinje.tid.Tidspunkt
 
 /**
  * Extension-metode for å map'e innhold fra en type og verdi til en annen
+ * Hvis det nå oppstår tilgrensende perioder med samme innhold, slås de sammen
  */
 fun <I, T : Tidsenhet, R> Tidslinje<I, T>.map(mapper: (I?) -> R?): Tidslinje<R, T> {
     val tidslinje = this
-    return object : TidslinjeMedAvhengigheter<R, T>(listOf(tidslinje)) {
-        override fun lagPerioder() = tidslinje.perioder().map {
-            Periode(it.fraOgMed, it.tilOgMed, mapper(it.innhold))
-        }
+    return object : TidslinjeSomStykkerOppTiden<R, T>(listOf(tidslinje)) {
+        override fun finnInnholdForTidspunkt(tidspunkt: Tidspunkt<T>) =
+            mapper(tidslinje.innholdForTidspunkt(tidspunkt))
     }
 }
