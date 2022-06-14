bb7a2ecf6dfc4ab6e12b69a57b33068171e1ee63
halvormundal <halvorbmundal@gmail.com>
2022-06-14 16:26:44 +0200

    Ikke bruk tilTidslinje i UtbetalingsperiodeMedBegrunnelseUtil

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/UtbetalingsperiodeMedBegrunnelser/UtbetalingsperiodeMedBegrunnelseUtil.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/UtbetalingsperiodeMedBegrunnelser/UtbetalingsperiodeMedBegrunnelseUtil.kt
@@ -2,11 +2,12 @@ package no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.UtbetalingsperiodeMed
 
 import no.nav.familie.ba.sak.common.førsteDagIInneværendeMåned
 import no.nav.familie.ba.sak.common.sisteDagIMåned
-import no.nav.familie.ba.sak.kjerne.eøs.felles.beregning.tilTidslinje
+import no.nav.familie.ba.sak.kjerne.eøs.felles.beregning.tilSeparateTidslinjerForBarna
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.Kompetanse
 import no.nav.familie.ba.sak.kjerne.tidslinje.Tidslinje
 import no.nav.familie.ba.sak.kjerne.tidslinje.komposisjon.kombinerMed
 import no.nav.familie.ba.sak.kjerne.tidslinje.tid.Måned
+import no.nav.familie.ba.sak.kjerne.tidslinje.transformasjon.map
 import no.nav.familie.ba.sak.kjerne.vedtak.domene.VedtaksperiodeMedBegrunnelser
 import no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.VedtaksperiodeMedBegrunnelserTidslinjeMåned
 
@@ -16,25 +17,41 @@ fun slåSammenUtbetalingsperioderMedKompetanse(
 ): List<VedtaksperiodeMedBegrunnelser> {
     if (kompetanser.isEmpty()) return utbetalingsperioder
 
-    val kompetanseTidslinje = kompetanser.tilTidslinje()
+    val kompetanseTidslinjer = kompetanser.tilSeparateTidslinjerForBarna()
     val utbetalingsTidslinje = VedtaksperiodeMedBegrunnelserTidslinjeMåned(utbetalingsperioder)
 
-    val kombinertTidslinje =
-        utbetalingsTidslinje.kombinerMed(kompetanseTidslinje) { vedtaksperiodeMedBegrunnelser, kompetanse ->
-            vedtaksperiodeMedBegrunnelser?.let {
-                UtbetalingsperiodeMedOverlappendeKompetanse(
-                    it,
-                    kompetanse
-                )
+    val initiellKombinertTidslinje = utbetalingsTidslinje.map {
+        it?.let {
+            UtbetalingsperiodeMedOverlappendeKompetanse(
+                it,
+                emptyList()
+            )
+        }
+    }
+
+    val kombinertTidslinje = kompetanseTidslinjer.values.fold(
+        initiellKombinertTidslinje
+    ) { acc, kompetanseTidslinje ->
+        acc.kombinerMed(kompetanseTidslinje) { utbetalingsperiodeMedOverlappendeKompetanse, kompetanse ->
+            utbetalingsperiodeMedOverlappendeKompetanse?.let {
+                if (kompetanse == null) {
+                    utbetalingsperiodeMedOverlappendeKompetanse
+                } else {
+                    UtbetalingsperiodeMedOverlappendeKompetanse(
+                        utbetalingsperiodeMedOverlappendeKompetanse.vedtaksperiodeMedBegrunnelser,
+                        utbetalingsperiodeMedOverlappendeKompetanse.kompetanser + kompetanse
+                    )
+                }
             }
         }
+    }
 
     return kombinertTidslinje.lagVedtaksperioderMedBegrunnelser()
 }
 
 data class UtbetalingsperiodeMedOverlappendeKompetanse(
     val vedtaksperiodeMedBegrunnelser: VedtaksperiodeMedBegrunnelser,
-    val kompetanse: Kompetanse?,
+    val kompetanser: List<Kompetanse>,
 )
 
 fun Tidslinje<UtbetalingsperiodeMedOverlappendeKompetanse, Måned>.lagVedtaksperioderMedBegrunnelser(): List<VedtaksperiodeMedBegrunnelser> =
--- src/test/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/UtbetalingsperiodeMedBegrunnelser/UtbetalingsperiodeMedBegrunnelserUtilsTest.kt
+++ src/test/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/UtbetalingsperiodeMedBegrunnelser/UtbetalingsperiodeMedBegrunnelserUtilsTest.kt
@@ -5,12 +5,16 @@ import no.nav.familie.ba.sak.common.førsteDagIInneværendeMåned
 import no.nav.familie.ba.sak.common.lagVedtaksperiodeMedBegrunnelser
 import no.nav.familie.ba.sak.common.sisteDagIInneværendeMåned
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.lagKompetanse
+import no.nav.familie.ba.sak.kjerne.personident.Aktør
 import no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.Vedtaksperiodetype
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.time.YearMonth
 
 class UtbetalingsperiodeMedBegrunnelserUtilsTest {
+
+    val barnAktør1 = Aktør(aktørId = "1111111111111")
+    val barnAktør2 = Aktør(aktørId = "2222222222222")
 
     @Test
     fun `skal splitte opp utbetalingsperioder når det er overlappende kompetanse`() {
@@ -27,7 +31,7 @@ class UtbetalingsperiodeMedBegrunnelserUtilsTest {
             tom = periode2.tom.sisteDagIInneværendeMåned(),
         )
 
-        val kompetanse1 = lagKompetanse(fom = periode2.fom, tom = periode2.tom)
+        val kompetanse1 = lagKompetanse(fom = periode2.fom, tom = periode2.tom, barnAktører = setOf(barnAktør1))
 
         val utbetalingsperiode2 = lagVedtaksperiodeMedBegrunnelser(
             type = Vedtaksperiodetype.UTBETALING,
@@ -35,7 +39,7 @@ class UtbetalingsperiodeMedBegrunnelserUtilsTest {
             tom = periode4.tom.sisteDagIInneværendeMåned()
         )
 
-        val kompetanse2 = lagKompetanse(fom = periode4.fom, tom = periode5.tom)
+        val kompetanse2 = lagKompetanse(fom = periode4.fom, tom = periode5.tom, barnAktører = setOf(barnAktør1))
 
         val utbetalingsperiodeMedReduksjonFraSistIverksatteBehandling = lagVedtaksperiodeMedBegrunnelser(
             type = Vedtaksperiodetype.UTBETALING_MED_REDUKSJON_FRA_SIST_IVERKSATTE_BEHANDLING,
@@ -82,7 +86,8 @@ class UtbetalingsperiodeMedBegrunnelserUtilsTest {
             )
         }
 
-        val kompetanse = lagKompetanse(fom = YearMonth.of(2020, 5), tom = YearMonth.of(2021, 5))
+        val kompetanse =
+            lagKompetanse(fom = YearMonth.of(2020, 5), tom = YearMonth.of(2021, 5), barnAktører = setOf(barnAktør1))
 
         val utbetalingsperiodeMedKompetanseSplitter = slåSammenUtbetalingsperioderMedKompetanse(
             utbetalingsperioder = utbetalingsperioder,
@@ -90,5 +95,40 @@ class UtbetalingsperiodeMedBegrunnelserUtilsTest {
         )
 
         Assertions.assertEquals(7, utbetalingsperiodeMedKompetanseSplitter.size)
+    }
+
+    @Test
+    fun `Skal splitte opp utbetalingsperiodene riktig når det er kompetanse på forskjellige personer`() {
+
+        val periode1 = MånedPeriode(YearMonth.of(2021, 1), YearMonth.of(2021, 5))
+        val periode2 = MånedPeriode(YearMonth.of(2021, 5), YearMonth.of(2021, 7))
+        val periode3 = MånedPeriode(YearMonth.of(2021, 7), YearMonth.of(2022, 1))
+
+        val utbetalingsperiode1 = lagVedtaksperiodeMedBegrunnelser(
+            type = Vedtaksperiodetype.UTBETALING,
+            fom = periode1.fom.førsteDagIInneværendeMåned(),
+            tom = periode3.tom.sisteDagIInneværendeMåned(),
+        )
+
+        val kompetanse1 =
+            lagKompetanse(fom = periode1.fom, tom = periode2.tom, barnAktører = setOf(barnAktør1))
+        val kompetanse2 =
+            lagKompetanse(fom = periode2.fom, tom = periode3.tom, barnAktører = setOf(barnAktør2))
+
+        val forventedePerioder =
+            listOf(periode1, periode2, periode3)
+
+        val utbetalingsperiodeMedKompetanseSplitter = slåSammenUtbetalingsperioderMedKompetanse(
+            utbetalingsperioder = listOf(utbetalingsperiode1),
+            kompetanser = listOf(kompetanse1, kompetanse2)
+        )
+
+        Assertions.assertEquals(3, utbetalingsperiodeMedKompetanseSplitter.size)
+        utbetalingsperiodeMedKompetanseSplitter
+            .zip(forventedePerioder)
+            .forEach { (utbetalingsperiode, forventetPeriode) ->
+                Assertions.assertEquals(forventetPeriode.fom.førsteDagIInneværendeMåned(), utbetalingsperiode.fom)
+                Assertions.assertEquals(forventetPeriode.tom.sisteDagIInneværendeMåned(), utbetalingsperiode.tom)
+            }
     }
 }
