c83f575f2217bc39c5930ca74bc2f7314abecef8
halvormundal <halvorbmundal@gmail.com>
2022-07-14 16:25:26 +0200

    Send med hvilke behandlingsresultater vi fikk ved ugyldig behandlingsresultat

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatUtils.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatUtils.kt
@@ -23,10 +23,10 @@ import no.nav.fpsak.tidsserie.StandardCombinators
 
 object BehandlingsresultatUtils {
 
-    private val ikkeStøttetFeil =
+    private fun ikkeStøttetFeil(behandlingsresultater: MutableSet<YtelsePersonResultat>) =
         Feil(
             frontendFeilmelding = "Behandlingsresultatet du har fått på behandlingen er ikke støttet i løsningen enda. Ta kontakt med Team familie om du er uenig i resultatet.",
-            message = "Behandlingsresultatet er ikke støttet i løsningen, se securelogger for resultatene som ble utledet."
+            message = "Behandlingsresultatet $behandlingsresultater er ikke støttet i løsningen, se securelogger for resultatene som ble utledet."
         )
 
     fun utledBehandlingsresultatDataForPerson(
@@ -154,7 +154,7 @@ object BehandlingsresultatUtils {
             samledeResultater.matcherAltOgHarBådeEndretOgOpphørtResultat(
                 setOf(YtelsePersonResultat.AVSLÅTT)
             ) -> Behandlingsresultat.AVSLÅTT_ENDRET_OG_OPPHØRT
-            else -> throw ikkeStøttetFeil
+            else -> throw ikkeStøttetFeil(samledeResultater)
         }
     }
 
