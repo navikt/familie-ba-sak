fdb0f61780bbb6bbc4f4d82ec0c0cb8cf605af25
halvorbmundal <halvorbmundal@users.noreply.github.com>
2022-07-21 13:30:56 +0200

    Håndter forlenget overgangsstønadsperiode (#2686)
    * Lag test som feiler når en overgangsstønadsperiode blir forlenget
    
    * Splitt opp overgangsstønadsperioder som er utvidet fra forrige behandling
    
    * søkersIdent2 -> søkersIdent.
    søkersAktør2 -> søkersAktør.
    inline andelerTilkjentYtelse.
    
    * Fjern ubrukte parametere
    
    * Flyt sjekk forom overgangsstønaden gjelder for én person til splitFramtidigePerioderFraForrigeBehandling()
    
    * Bytt navn på test

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/beregning/domene/InternPeriodeOvergangsstønad.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/beregning/domene/InternPeriodeOvergangsstønad.kt
@@ -1,8 +1,10 @@
 package no.nav.familie.ba.sak.kjerne.beregning.domene
 
+import no.nav.familie.ba.sak.common.Feil
 import no.nav.familie.ba.sak.common.forrigeMåned
 import no.nav.familie.ba.sak.common.isSameOrBefore
 import no.nav.familie.ba.sak.common.toYearMonth
+import no.nav.familie.ba.sak.kjerne.tidslinje.komposisjon.kombinerMed
 import no.nav.familie.kontrakter.felles.ef.PeriodeOvergangsstønad
 import java.time.LocalDate
 
@@ -32,4 +34,41 @@ fun List<InternPeriodeOvergangsstønad>.slåSammenSammenhengendePerioder(): List
                 sammenslåttePerioder.apply { add(removeLast().copy(tomDato = nestePeriode.tomDato)) }
             } else sammenslåttePerioder.apply { add(nestePeriode) }
         }
+}
+
+/***
+ * Dersom vi i en behandling har overgangsstønad i tre måneder:
+ * |OOO-----|
+ * som fører til småbarnstillegg.
+ * Og så utvides overgangsstønadsperioden til fem måneder:
+ * |OOOOO---|
+ * som fører til småbarnstillegg i alle månedene.
+ * Ønsker vi å kunne begrunne de to siste månedene med småbarnstillegg.
+ * Splitter derfor opp overgangsstønads-perioden slik at vi kan begrunne endringen for søker i riktig periode.
+ * |OOO-----|
+ * |---OO---|
+ *
+ ***/
+fun List<InternPeriodeOvergangsstønad>.splitFramtidigePerioderFraForrigeBehandling(
+    gamleOvergangsstønadPerioder: List<InternPeriodeOvergangsstønad>
+): List<InternPeriodeOvergangsstønad> {
+    val erOvergangsstønadForMerEnnEnPerson =
+        (this + gamleOvergangsstønadPerioder).map { it.personIdent }.toSet().size > 1
+    if (erOvergangsstønadForMerEnnEnPerson)
+        throw Feil("Antar overgangsstønad for kun søker, men fant overgangsstønad for mer enn en person.")
+
+    val tidligerePerioder = this.filter { it.tomDato.isSameOrBefore(LocalDate.now()) }
+    val framtidigePerioder = this.minus(tidligerePerioder)
+    val nyeOvergangsstønadTidslinje = InternPeriodeOvergangsstønadTidslinje(framtidigePerioder)
+
+    val gammelOvergangsstønadTidslinje = InternPeriodeOvergangsstønadTidslinje(gamleOvergangsstønadPerioder)
+
+    val oppsplittedeFramtigigePerioder = gammelOvergangsstønadTidslinje
+        .kombinerMed(nyeOvergangsstønadTidslinje) { gammelOvergangsstønadPeriode, nyOvergangsstønadPeriode ->
+            if (nyOvergangsstønadPeriode == null) null
+            else gammelOvergangsstønadPeriode ?: nyOvergangsstønadPeriode
+        }
+        .lagInternePerioderOvergangsstønad()
+
+    return tidligerePerioder + oppsplittedeFramtigigePerioder
 }
