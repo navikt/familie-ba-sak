02f84595d7e8a3949a3b6b1ec1f315246735ff3b
Henning Solberg <henning.solberg@nav.no>
2022-08-02 12:48:31 +0200

    la til flyway konfig kode

--- src/main/kotlin/no/nav/familie/ba/sak/config/FlywayConfiguration.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/FlywayConfiguration.kt
@@ -1,11 +1,12 @@
 package no.nav.familie.ba.sak.config
 
-import org.flywaydb.core.api.configuration.FluentConfiguration
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Profile
+import org.springframework.core.env.Environment
 
 @Profile("!dev")
 @ConstructorBinding
@@ -13,9 +14,20 @@ import org.springframework.context.annotation.Profile
 data class FlywayConfiguration(private val role: String) {
 
     @Bean
-    fun flywayConfig(): FlywayConfigurationCustomizer {
-        return FlywayConfigurationCustomizer { c: FluentConfiguration ->
-            c.initSql("SET ROLE \"$role\"")
+    fun flywayConfig(
+        @Value("\${spring.flyway.placeholders.ignoreIfProd}") ignoreIfProd: String,
+        environment: Environment
+    ): FlywayConfigurationCustomizer {
+        val isProd = environment.activeProfiles.contains("prod")
+        val ignore = ignoreIfProd == "--"
+        return FlywayConfigurationCustomizer {
+            it.initSql(String.format("SET ROLE \"%s\"", role))
+            if (isProd && !ignore) {
+                throw RuntimeException("Prod profile-en har ikke riktig verdi for placeholder ignoreIfProd=$ignoreIfProd")
+            }
+            if (!isProd && ignore) {
+                throw RuntimeException("Profile=${environment.activeProfiles} har ignoreIfProd=false")
+            }
         }
     }
 }
