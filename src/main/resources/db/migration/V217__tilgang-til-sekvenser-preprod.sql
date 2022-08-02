55f52c697812c119a758b4f38f2fa5a38fd1ed35
Henning Solberg <henning.solberg@nav.no>
2022-08-02 12:18:17 +0200

    formatering

--- src/main/resources/db/migration/V217__tilgang-til-sekvenser-preprod.sql
+++ src/main/resources/db/migration/V217__tilgang-til-sekvenser-preprod.sql
@@ -1,9 +1,9 @@
 DO $$
-BEGIN
+    BEGIN
         IF EXISTS
             ( SELECT 1 from pg_roles where rolname='cloudsqliamuser')
         THEN
-${ignoreIfProd} GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO cloudsqliamuser;
-END IF ;
-END
+            ${ignoreIfProd} GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO cloudsqliamuser;
+        END IF ;
+    END
 $$ ;
