cfd345507cdd308862b6891308f9031741dc0830
Henning Solberg <henning.solberg@nav.no>
2022-08-02 12:16:49 +0200

    gir tilgang til å oppdatere sekvenser i preprod

---
+++ src/main/resources/db/migration/V217__tilgang-til-sekvenser-preprod.sql
@@ -0,0 +1,9 @@
+DO $$
+BEGIN
+        IF EXISTS
+            ( SELECT 1 from pg_roles where rolname='cloudsqliamuser')
+        THEN
+${ignoreIfProd} GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO cloudsqliamuser;
+END IF ;
+END
+$$ ;
