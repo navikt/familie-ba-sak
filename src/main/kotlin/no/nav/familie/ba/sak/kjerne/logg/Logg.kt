527f8779d94c33d521ab4f0766ac56111b82168e
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2022-08-04 08:50:22 +0200

    Tea-7453:  Tar med fødselsnummer for barna i tekst-feltet for historikken, for å synleggjera for saksbehandlar kven det var som ikkje vart vurdert (#2673)
    * Tea-7453: Tar med fødselsnummer for barna i tekst-feltet for historikken, for å synleggjera for saksbehandlar kven det var som ikkje vart vurdert
    
    Signed-off-by: Mads Opheim <mads.opheim@computas.com>
    
    * Tea-7453: Tittel er for mange av loggtypane konstant, så da synes eg det er betre å flytte det inn i enumen og samle informasjonen der. Avgrensar det i denne runden til å gå på kun dei som er hardkoda
    
    Signed-off-by: Mads Opheim <mads.opheim@computas.com>
    
    * Tea-7453: Standardverdi blank tekst for tekst. Mange stadar hardkodar vi uansett det når vi sender inn, så da kan vi like godt ha det som standard og overstyre der vi treng det.
    
    Inliner også eit par titlar
    
    Signed-off-by: Mads Opheim <mads.opheim@computas.com>
    
    * Tea-7453: Viss to logginnslag blir oppretta tilstrekkjeleg samtidig, som f.eks. skjer iblant ved innlesing av fødselshending og oppretting av førstegangsbehandling, var rekkjefølgja vi viste dei fram for brukaren ikkje-deterministisk. Men vi veit jo kva vi lagra først, så bruker id-kolonna, som er strengt aukande, til sortering heller enn tidspunkt
    
    Signed-off-by: Mads Opheim <mads.opheim@computas.com>
    
    * Tea-7453: Todo på det gjenståande
    
    Signed-off-by: Mads Opheim <mads.opheim@computas.com>
    
    * Tea-7453: Loggar fødselsdato heller enn full identifikator
    
    Signed-off-by: Mads Opheim <mads.opheim@computas.com>
    
    * Tea-7453: Etter kodegjennomgang, sorterer først logginnslag på tidspunkt, og dernest, viss to innslag kom samtidig, på id
    
    Signed-off-by: Mads Opheim <mads.opheim@computas.com>
    
    * Tea-7453: Trekkjer ut konvertering av fnr ut frå Utils til ei eiga klasse. Utils-klassa her har mange nok ansvarsområde allereie
    
    Signed-off-by: Mads Opheim <mads.opheim@computas.com>
    
    * Tea-7453: Bruker fødselsnummer-objekt frå vår eiga modellklasse som vi sjølv har kontroll over
    
    * Tea-7453: Ktlint

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/logg/Logg.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/logg/Logg.kt
@@ -49,28 +49,39 @@ data class Logg(
      */
     @Column(name = "tekst")
     val tekst: String
-)
+) {
+    constructor(behandlingId: Long, type: LoggType, rolle: BehandlerRolle, tekst: String = "") : this(
+        behandlingId = behandlingId,
+        type = type,
+        tittel = type.tittel,
+        rolle = rolle,
+        tekst = tekst
+    )
+}
 
-enum class LoggType(val visningsnavn: String) {
-    AUTOVEDTAK_TIL_MANUELL_BEHANDLING("Autovedtak til manuell behandling"),
+enum class LoggType(val visningsnavn: String, val tittel: String = visningsnavn) {
+    AUTOVEDTAK_TIL_MANUELL_BEHANDLING("Autovedtak til manuell behandling", "Automatisk behandling stoppet"),
     FØDSELSHENDELSE("Fødselshendelse"), // Deprecated, bruk livshendelse
     LIVSHENDELSE("Livshendelse"),
-    BEHANDLENDE_ENHET_ENDRET("Behandlende enhet endret"),
+    BEHANDLENDE_ENHET_ENDRET("Behandlende enhet endret", "Endret enhet på behandling"),
     BEHANDLING_OPPRETTET("Behandling opprettet"),
-    BEHANDLINGSTYPE_ENDRET("Endret behandlingstype"),
+    BEHANDLINGSTYPE_ENDRET("Endret behandlingstype", "Endret behandlingstema"),
     BARN_LAGT_TIL("Barn lagt til på behandling"),
     DOKUMENT_MOTTATT("Dokument ble mottatt"),
     SØKNAD_REGISTRERT("Søknaden ble registrert"),
     VILKÅRSVURDERING("Vilkårsvurdering"),
-    SEND_TIL_BESLUTTER("Send til beslutter"),
-    SEND_TIL_SYSTEM("Send til system"),
-    GODKJENNE_VEDTAK("Godkjenne vedtak"),
-    MIGRERING_BEKREFTET("Migrering bekreftet"),
-    DISTRIBUERE_BREV("Distribuere brev"),
-    BREV_IKKE_DISTRIBUERT("Brev ikke distribuert"),
-    BREV_IKKE_DISTRIBUERT_UKJENT_DØDSBO("Brev ikke distribuert. Ukjent dødsbo"),
-    FERDIGSTILLE_BEHANDLING("Ferdigstille behandling"),
-    HENLEGG_BEHANDLING("Henlegg behandling"),
+    SEND_TIL_BESLUTTER("Send til beslutter", "Sendt til beslutter"),
+    SEND_TIL_SYSTEM("Send til system", "Sendt til system"),
+    GODKJENNE_VEDTAK("Godkjenne vedtak", "Vedtak godkjent"),
+    MIGRERING_BEKREFTET("Migrering bekreftet", "Migrering bekreftet"),
+    DISTRIBUERE_BREV("Distribuere brev", "Brev sendt"),
+    BREV_IKKE_DISTRIBUERT("Brev ikke distribuert", "Brevet ble ikke distribuert fordi mottaker har ukjent adresse"),
+    BREV_IKKE_DISTRIBUERT_UKJENT_DØDSBO(
+        "Brev ikke distribuert. Ukjent dødsbo",
+        "Mottaker har ukjent dødsboadresse, og brevet blir ikke sendt før adressen er satt"
+    ),
+    FERDIGSTILLE_BEHANDLING("Ferdigstille behandling", "Ferdigstilt behandling"),
+    HENLEGG_BEHANDLING("Henlegg behandling", "Behandlingen er henlagt"),
     BEHANDLIG_SATT_PÅ_VENT("Behandlingen er satt på vent"),
     BEHANDLIG_GJENOPPTATT("Behandling gjenopptatt"),
     VENTENDE_BEHANDLING_ENDRET("Behandlingen er oppdatert"),
