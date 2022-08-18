fbaa70d4fb5ab58cc4df6eb93cf9634e976b3566
Ida Merete Enholm <ida.merete.enholm@nav.no>
2022-08-18 12:59:39 +0200

    Ny måte å generere småbarnstillegg (#2737)
    * Rename gammel funksjon
    
    * Lag ny metode for å generere småbarnstillegg som baserer seg på jobi-tidslinjer
    
    * Legg ny funksjonalitet bak feature toggle
    
    * Endre logikk for å kombinere tidslinjer
    
    * Send med featuretoggleservice som parameter
    
    * Trekk funksjonalitet ut i egen funksjon
    
    * Trekk funksjonalitet ut i egen funksjon
    
    * Endre fra validering til filtrering
    
    * Legg på mer sikkerhet rundt hvem sin andel vi snakker om
    
    * Send med bolsk verdi for toggle i stedet for service
    
    * Flytt funksjoner til utils-fil
    
    * Skriv tester for funksjonen som genererer tidslinje hvor det finnes barn under 3 år som gir rett til småbarnstillegg
    
    * Flytt enum ut av generator-objekt
    
    * Filtrer bort null-verdier
    
    * Oppdater til rett enum
    
    * Kombiner overgangsstønad tidslinje til månedtidslinje med rett kombinator
    
    * Gjør om til månedtidslinje for OS i funksjon heller enn utenfor
    
    * Flytt enums til utils fil
    
    * Filtrer bort null-verdier i kombinasjonstidslinje
    
    * Skriv tester for kombinasjonsfunksjon
    
    * Bruk heller jobi-funksjon som kan kombinere tre ulike tidslinjer heller enn 2x kombinerMed
    
    * Forenkle kombinator-funksjon for dag til måned OS
    
    * Endre navn på tester
    
    * Formattering
    
    * Skrive tester
    
    * Skrive om tidslinje-logikk til å bevare splitter i overgangsstønad-perioder
    
    * Rett opp tester som feiler
    
    * Fjern "open" class fra AndelTilkjentYtelseTidslinje
    
    * Gjør filtrering om til validering
    
    * Trekk andel tilkjent ytelse fom og tom ut i egen variabel
    
    * Fjern tidslinje fra variabelnavn i kombinerMed
    
    * Endre navn på variabel og oppdatere forklaring etter endring av logikk i kombinator-funksjon
    
    * Trekk felles variabler ut av hver enkelt test
    
    * Kaste feil ved scenarier som ikke skal skje
    
    * Endre navn på under3ÅrTidslinje til erTilOgMed3ÅrTidslinje

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/tidslinje/eksperimentelt/TidslinjeSnittKombinasjon.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/tidslinje/eksperimentelt/TidslinjeSnittKombinasjon.kt
@@ -7,31 +7,6 @@ import no.nav.familie.ba.sak.kjerne.tidslinje.tid.Tidsenhet
 import no.nav.familie.ba.sak.kjerne.tidslinje.tid.Tidspunkt
 
 /**
- * Extension-metode for å kombinere tre tidslinjer
- * Kombinasjonen baserer seg på TidslinjeSomStykkerOppTiden, som itererer gjennom alle tidspunktene
- * fra minste fraOgMed til største fraOgMed() fra alle tidslinjene
- * Tidsenhet (T) må være av samme type
- * Hver av tidslinjene kan ha ulik innholdstype, hhv A, B og C
- * Kombintor-funksjonen tar inn (nullable) av A, B og C og returner (nullable) R
- * Resultatet er en tidslinje med tidsenhet T og innhold R
- */
-fun <A, B, C, R, T : Tidsenhet> Tidslinje<A, T>.kombinerMed(
-    tidslinjeB: Tidslinje<B, T>,
-    tidslinjeC: Tidslinje<C, T>,
-    kombinator: (A?, B?, C?) -> R?
-): Tidslinje<R, T> {
-    val tidslinjeA = this
-    return object : TidslinjeSomStykkerOppTiden<R, T>(tidslinjeA, tidslinjeB, tidslinjeC) {
-        override fun finnInnholdForTidspunkt(tidspunkt: Tidspunkt<T>): R? =
-            kombinator(
-                tidslinjeA.innholdForTidspunkt(tidspunkt),
-                tidslinjeB.innholdForTidspunkt(tidspunkt),
-                tidslinjeC.innholdForTidspunkt(tidspunkt)
-            )
-    }
-}
-
-/**
  * Extension-metode for å kombinere en Collection av tidslinjer
  * Kombinasjonen baserer seg på TidslinjeSomStykkerOppTiden, som itererer gjennom alle tidspunktene
  * fra minste fraOgMed til største fraOgMed() fra alle tidslinjene
