924d1a50ebc1fbdc6d10186e59a0d9b1223d37cf
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2022-08-22 12:43:55 +0200

    Tea-9097: Legg til mulighet for å legge og fjerne småbarnstillegg gitt en behandling.

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/logg/LoggService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/logg/LoggService.kt
@@ -411,6 +411,24 @@ class LoggService(
         )
     }
 
+    fun opprettSmåbarnstilleggLogg(
+        behandling: Behandling,
+        tittel: String
+    ) {
+        lagre(
+            Logg(
+                behandlingId = behandling.id,
+                type = LoggType.MANUELT_SMÅBARNSTILLEGG_JUSTERING,
+                rolle = SikkerhetContext.hentRolletilgangFraSikkerhetscontext(
+                    rolleConfig,
+                    BehandlerRolle.SAKSBEHANDLER
+                ),
+                tittel = tittel,
+                tekst = ""
+            )
+        )
+    }
+
     fun gjenopptaBehandlingLogg(behandling: Behandling) {
         lagre(
             Logg(
