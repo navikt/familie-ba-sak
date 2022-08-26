17732519e8f4fb47bbcc2198747724b861c7860f
Mads Opheim <mads.opheim@computas.com>
2022-08-26 09:17:48 +0200

    Tea-8868: Testar på at sjekken framleis er der for andre vilkårstypar

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/eøs/vilkårsvurdering/VilkårsresultatDagTidslinjeKtTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/eøs/vilkårsvurdering/VilkårsresultatDagTidslinjeKtTest.kt
@@ -4,10 +4,13 @@ import no.nav.familie.ba.sak.common.lagBehandling
 import no.nav.familie.ba.sak.common.lagVilkårsvurdering
 import no.nav.familie.ba.sak.common.randomAktør
 import no.nav.familie.ba.sak.kjerne.autovedtak.fødselshendelse.Resultat
+import no.nav.familie.ba.sak.kjerne.tidslinje.Tidslinje.Companion.TidslinjeFeilException
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.PersonResultat
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.Vilkår
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.VilkårResultat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 import java.time.LocalDate
 
 internal class VilkårsresultatDagTidslinjeKtTest {
@@ -21,19 +24,36 @@ internal class VilkårsresultatDagTidslinjeKtTest {
         )
 
         val vilkårResultat = setOf(
-            lagVilkårResultat(personResultat, Resultat.OPPFYLT, fom = LocalDate.of(2020, 3, 31)),
-            lagVilkårResultat(personResultat, Resultat.IKKE_OPPFYLT, fom = null)
+            lagVilkårResultat(personResultat, Resultat.OPPFYLT, fom = LocalDate.of(2020, 3, 31), Vilkår.BOR_MED_SØKER),
+            lagVilkårResultat(personResultat, Resultat.IKKE_OPPFYLT, fom = null, Vilkår.BOR_MED_SØKER)
         )
 
         val tidslinje = vilkårResultat.tilVilkårRegelverkResultatTidslinje()
-        tidslinje.perioder()
+        assertDoesNotThrow { tidslinje.perioder() }
     }
 
-    private fun lagVilkårResultat(personResultat: PersonResultat, resultat: Resultat, fom: LocalDate?) =
+    @Test
+    fun `kan ikke ha to overlappende perioder hvis det er bosatt i riket-vilkåret`() {
+        val personAktørId = randomAktør()
+        val personResultat = PersonResultat(
+            vilkårsvurdering = lagVilkårsvurdering(personAktørId, lagBehandling(), Resultat.OPPFYLT),
+            aktør = personAktørId
+        )
+
+        val vilkårResultat = setOf(
+            lagVilkårResultat(personResultat, Resultat.OPPFYLT, fom = LocalDate.of(2020, 3, 31), Vilkår.BOSATT_I_RIKET),
+            lagVilkårResultat(personResultat, Resultat.IKKE_OPPFYLT, fom = null, Vilkår.BOSATT_I_RIKET)
+        )
+
+        val tidslinje = vilkårResultat.tilVilkårRegelverkResultatTidslinje()
+        assertThrows<TidslinjeFeilException> { tidslinje.perioder() }
+    }
+
+    private fun lagVilkårResultat(personResultat: PersonResultat, resultat: Resultat, fom: LocalDate?, vilkår: Vilkår) =
         VilkårResultat(
             personResultat = personResultat,
             resultat = resultat,
-            vilkårType = Vilkår.BOR_MED_SØKER,
+            vilkårType = vilkår,
             periodeFom = fom,
             periodeTom = null,
             begrunnelse = "",
