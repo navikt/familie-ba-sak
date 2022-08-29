a5f607b804620b1c02ef81547529cf935a7badba
bragejahren <brage.ekroll.jahren@nav.no>
2022-08-29 14:11:31 +0200

    Erstattet clearAllMocks() med clearMocks(...spesifikkeMocks) for å fikse feilende tester

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/integrasjoner/ecb/ECBServiceTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/integrasjoner/ecb/ECBServiceTest.kt
@@ -1,8 +1,10 @@
 package no.nav.familie.ba.sak.integrasjoner.ecb
+
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.unmockkAll
 import no.nav.familie.http.ecb.ECBRestClient
 import no.nav.familie.http.ecb.Frequency
 import no.nav.familie.http.ecb.domene.ECBExchangeRate
@@ -13,14 +15,17 @@ import no.nav.familie.http.ecb.domene.ECBExchangeRatesData
 import no.nav.familie.http.ecb.domene.ECBExchangeRatesDataSet
 import no.nav.familie.http.ecb.domene.ECBExchangeRatesForCurrency
 import no.nav.familie.http.ecb.domene.toExchangeRates
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import java.math.BigDecimal
 import java.time.LocalDate
 
 @ExtendWith(MockKExtension::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ECBServiceTest {
 
     @MockK
@@ -29,11 +34,26 @@ class ECBServiceTest {
     @InjectMockKs
     private lateinit var ecbService: ECBService
 
+    @AfterAll
+    fun tearDown() {
+        unmockkAll()
+    }
+
     @Test
     fun `Hent valutakurs for utenlandsk valuta til NOK og sjekk at beregning av kurs er riktig`() {
         val valutakursDato = LocalDate.of(2022, 6, 28)
-        val ecbExchangeRatesData = createECBResponse(Frequency.Daily, listOf(Pair("NOK", BigDecimal.valueOf(10.337)), Pair("SEK", BigDecimal.valueOf(10.6543))), valutakursDato.toString())
-        every { ecbClient.getExchangeRates(Frequency.Daily, listOf("NOK", "SEK"), valutakursDato) } returns ecbExchangeRatesData.toExchangeRates()
+        val ecbExchangeRatesData = createECBResponse(
+            Frequency.Daily,
+            listOf(Pair("NOK", BigDecimal.valueOf(10.337)), Pair("SEK", BigDecimal.valueOf(10.6543))),
+            valutakursDato.toString()
+        )
+        every {
+            ecbClient.getExchangeRates(
+                Frequency.Daily,
+                listOf("NOK", "SEK"),
+                valutakursDato
+            )
+        } returns ecbExchangeRatesData.toExchangeRates()
         val SEKtilNOKValutakurs = ecbService.hentValutakurs("SEK", valutakursDato)
         assertEquals(BigDecimal.valueOf(0.9702185972), SEKtilNOKValutakurs)
     }
@@ -41,16 +61,36 @@ class ECBServiceTest {
     @Test
     fun `Test at ECBService kaster ESBServiceException dersom de returnerte kursene ikke inneholder kurs for forespurt valuta`() {
         val valutakursDato = LocalDate.of(2022, 7, 22)
-        val ecbExchangeRatesData = createECBResponse(Frequency.Daily, listOf(Pair("NOK", BigDecimal.valueOf(10.337))), valutakursDato.toString())
-        every { ecbClient.getExchangeRates(Frequency.Daily, listOf("NOK", "SEK"), valutakursDato) } returns ecbExchangeRatesData.toExchangeRates()
+        val ecbExchangeRatesData = createECBResponse(
+            Frequency.Daily,
+            listOf(Pair("NOK", BigDecimal.valueOf(10.337))),
+            valutakursDato.toString()
+        )
+        every {
+            ecbClient.getExchangeRates(
+                Frequency.Daily,
+                listOf("NOK", "SEK"),
+                valutakursDato
+            )
+        } returns ecbExchangeRatesData.toExchangeRates()
         assertThrows<ECBServiceException> { ecbService.hentValutakurs("SEK", valutakursDato) }
     }
 
     @Test
     fun `Test at ECBService kaster ESBServiceException dersom de returnerte kursene ikke inneholder kurser med forespurt dato`() {
         val valutakursDato = LocalDate.of(2022, 7, 20)
-        val ecbExchangeRatesData = createECBResponse(Frequency.Daily, listOf(Pair("NOK", BigDecimal.valueOf(10.337)), Pair("SEK", BigDecimal.valueOf(10.6543))), valutakursDato.minusDays(1).toString())
-        every { ecbClient.getExchangeRates(Frequency.Daily, listOf("NOK", "SEK"), valutakursDato) } returns ecbExchangeRatesData.toExchangeRates()
+        val ecbExchangeRatesData = createECBResponse(
+            Frequency.Daily,
+            listOf(Pair("NOK", BigDecimal.valueOf(10.337)), Pair("SEK", BigDecimal.valueOf(10.6543))),
+            valutakursDato.minusDays(1).toString()
+        )
+        every {
+            ecbClient.getExchangeRates(
+                Frequency.Daily,
+                listOf("NOK", "SEK"),
+                valutakursDato
+            )
+        } returns ecbExchangeRatesData.toExchangeRates()
         assertThrows<ECBServiceException> { ecbService.hentValutakurs("SEK", valutakursDato) }
     }
 
@@ -58,18 +98,40 @@ class ECBServiceTest {
     fun `Test at ECBService returnerer NOK til EUR dersom den forespurte valutaen er EUR`() {
         val nokTilEur = BigDecimal.valueOf(9.4567)
         val valutakursDato = LocalDate.of(2022, 7, 20)
-        val ecbExchangeRatesData = createECBResponse(Frequency.Daily, listOf(Pair("NOK", BigDecimal.valueOf(9.4567))), valutakursDato.toString())
-        every { ecbClient.getExchangeRates(Frequency.Daily, listOf("NOK", "EUR"), valutakursDato) } returns ecbExchangeRatesData.toExchangeRates()
+        val ecbExchangeRatesData = createECBResponse(
+            Frequency.Daily,
+            listOf(Pair("NOK", BigDecimal.valueOf(9.4567))),
+            valutakursDato.toString()
+        )
+        every {
+            ecbClient.getExchangeRates(
+                Frequency.Daily,
+                listOf("NOK", "EUR"),
+                valutakursDato
+            )
+        } returns ecbExchangeRatesData.toExchangeRates()
         assertEquals(nokTilEur, ecbService.hentValutakurs("EUR", valutakursDato))
     }
 
-    private fun createECBResponse(frequency: Frequency, exchangeRates: List<Pair<String, BigDecimal>>, exchangeRateDate: String): ECBExchangeRatesData {
+    private fun createECBResponse(
+        frequency: Frequency,
+        exchangeRates: List<Pair<String, BigDecimal>>,
+        exchangeRateDate: String
+    ): ECBExchangeRatesData {
         return ECBExchangeRatesData(
             ECBExchangeRatesDataSet(
                 exchangeRates.map {
                     ECBExchangeRatesForCurrency(
-                        listOf(ECBExchangeRateKey("CURRENCY", it.first), ECBExchangeRateKey("FREQ", frequency.toFrequencyParam())),
-                        listOf(ECBExchangeRate(ECBExchangeRateDate(exchangeRateDate), ECBExchangeRateValue((it.second))))
+                        listOf(
+                            ECBExchangeRateKey("CURRENCY", it.first),
+                            ECBExchangeRateKey("FREQ", frequency.toFrequencyParam())
+                        ),
+                        listOf(
+                            ECBExchangeRate(
+                                ECBExchangeRateDate(exchangeRateDate),
+                                ECBExchangeRateValue((it.second))
+                            )
+                        )
                     )
                 }
             )
--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/eøs/valutakurs/ValutakursControllerTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/eøs/valutakurs/ValutakursControllerTest.kt
@@ -1,10 +1,11 @@
 package no.nav.familie.ba.sak.kjerne.eøs.valutakurs
 
-import io.mockk.clearAllMocks
+import io.mockk.clearMocks
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.unmockkAll
 import io.mockk.verify
 import no.nav.familie.ba.sak.config.FeatureToggleService
 import no.nav.familie.ba.sak.config.tilAktør
@@ -14,6 +15,7 @@ import no.nav.familie.ba.sak.ekstern.restDomene.tilValutakurs
 import no.nav.familie.ba.sak.integrasjoner.ecb.ECBService
 import no.nav.familie.ba.sak.kjerne.behandling.UtvidetBehandlingService
 import no.nav.familie.ba.sak.kjerne.personident.PersonidentService
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -47,11 +49,12 @@ class ValutakursControllerTest {
 
     private val barnId = "12345678910"
 
-    private val restValutakurs: RestValutakurs = RestValutakurs(1, YearMonth.of(2020, 1), null, listOf(barnId), null, null, null, UtfyltStatus.OK)
+    private val restValutakurs: RestValutakurs =
+        RestValutakurs(1, YearMonth.of(2020, 1), null, listOf(barnId), null, null, null, UtfyltStatus.OK)
 
     @BeforeEach
     fun setup() {
-        clearAllMocks()
+        clearMocks(featureToggleService, personidentService, valutakursService, ecbService, utvidetBehandlingService)
         every { featureToggleService.isEnabled(any()) } returns true
         every { personidentService.hentAktør(any()) } returns tilAktør(barnId)
         every { valutakursService.hentValutakurs(any()) } returns restValutakurs.tilValutakurs(listOf(tilAktør(barnId)))
@@ -59,11 +62,21 @@ class ValutakursControllerTest {
         every { utvidetBehandlingService.lagRestUtvidetBehandling(any()) } throws Exception("Stopp prosessering")
     }
 
+    @AfterAll
+    fun tearDown() {
+        unmockkAll()
+    }
+
     @Test
     fun `Test at valutakurs hentes fra ECB dersom dato og valuta er satt`() {
         val valutakursDato = LocalDate.of(2022, 1, 1)
         val valuta = "SEK"
-        assertThrows<Exception> { valutakursController.oppdaterValutakurs(1, restValutakurs.copy(valutakursdato = valutakursDato, valutakode = valuta)) }
+        assertThrows<Exception> {
+            valutakursController.oppdaterValutakurs(
+                1,
+                restValutakurs.copy(valutakursdato = valutakursDato, valutakode = valuta)
+            )
+        }
         verify(exactly = 1) { ecbService.hentValutakurs("SEK", valutakursDato) }
         verify(exactly = 1) { valutakursService.oppdaterValutakurs(any(), any()) }
     }
@@ -79,7 +92,12 @@ class ValutakursControllerTest {
     @Test
     fun `Test at valutakurs ikke hentes fra ECB dersom valuta ikke er satt`() {
         val valutakursDato = LocalDate.of(2022, 1, 1)
-        assertThrows<Exception> { valutakursController.oppdaterValutakurs(1, restValutakurs.copy(valutakursdato = valutakursDato)) }
+        assertThrows<Exception> {
+            valutakursController.oppdaterValutakurs(
+                1,
+                restValutakurs.copy(valutakursdato = valutakursDato)
+            )
+        }
         verify(exactly = 0) { ecbService.hentValutakurs("SEK", valutakursDato) }
         verify(exactly = 1) { valutakursService.oppdaterValutakurs(any(), any()) }
     }
@@ -87,7 +105,12 @@ class ValutakursControllerTest {
     @Test
     fun `Test at valutakurs ikke hentes fra ECB dersom ISK og før 1 feb 2018`() {
         val valutakursDato = LocalDate.of(2018, 1, 31)
-        assertThrows<Exception> { valutakursController.oppdaterValutakurs(1, restValutakurs.copy(valutakursdato = valutakursDato, valutakode = "ISK")) }
+        assertThrows<Exception> {
+            valutakursController.oppdaterValutakurs(
+                1,
+                restValutakurs.copy(valutakursdato = valutakursDato, valutakode = "ISK")
+            )
+        }
         verify(exactly = 0) { ecbService.hentValutakurs("ISK", valutakursDato) }
         verify(exactly = 1) { valutakursService.oppdaterValutakurs(any(), any()) }
     }
@@ -95,7 +118,12 @@ class ValutakursControllerTest {
     @Test
     fun `Test at valutakurs hentes fra ECB dersom ISK og etter 1 feb 2018`() {
         val valutakursDato = LocalDate.of(2018, 2, 1)
-        assertThrows<Exception> { valutakursController.oppdaterValutakurs(1, restValutakurs.copy(valutakursdato = valutakursDato, valutakode = "ISK")) }
+        assertThrows<Exception> {
+            valutakursController.oppdaterValutakurs(
+                1,
+                restValutakurs.copy(valutakursdato = valutakursDato, valutakode = "ISK")
+            )
+        }
         verify(exactly = 1) { ecbService.hentValutakurs("ISK", valutakursDato) }
         verify(exactly = 1) { valutakursService.oppdaterValutakurs(any(), any()) }
     }
