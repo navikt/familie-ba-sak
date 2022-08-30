8ef0978d2a4a781112211780ab1e57ebfa151d2d
Ida Merete Enholm <ida.merete.enholm@nav.no>
2022-08-30 11:41:25 +0200

    Generer utvidet og småbarnstillegg basert på andeler + endringsperioder (#2728)
    * Skrive tester
    
    * Lag ny funksjon bak feature toggle og deprecate gammel funksjon
    
    * Oppdater test med feature toggle
    
    * Slå sammen utvidet-andeler som er sammenhengende ved generering av småbarnstillegg
    
    * Skriv ny logikk for å generere tilkjent ytelse andeler
    
    * Trekk generering av andeler for barna ut i egen funksjon
    
    * Fjern muterbarhet i funksjonen som oppdaterer andeler med endringsperioder
    
    * Trekk generering av utvidet-andeler ut i egen funksjon + bruk utvidet m/endringsperioder for å generere småbarnstillegg
    
    * Oppdater test til å støtte ny småbarnstillegg logikk
    
    * Ta hensyn til utvidet-prosent når man genererer småbarnstillegg
    
    * Rett opp navn på test
    
    * Sett småbarnstillegg-beløp på riktig måte (tillater nå 0kr-andeler)
    
    * Se på barna sine andeler etter at etterbetaling 3 år endringer er lagt til når man skal generere småbarnstillegg
    
    * Skriv ny test + tillatt flere i barn i hver test
    
    * Legg til ny måte å beregne perioder for barn til småbarnstillegg
    
    * Legg til ny måte å beregne perioder for barn til småbarnstillegg
    
    * Legg til ny måte å beregne perioder for barn til småbarnstillegg
    
    * Reverte småbarnstillegg-endringer
    
    * Reverte småbarnstillegg-endringer
    
    * Slett tidslinje som ikke skal brukes
    
    * Oppdater toggle-navn etter merge
    
    * Bruk oppdatert funksjon for å generere småbarnstillegg
    
    * Ktlint
    
    * Rett opp feil i test
    
    * Legg på en sjekk om småbarnstillegg i det hele tatt er mulig før man kaller på funksjonen som lager småbarnstillegg-andeler
    
    * Flytte litt rundt på koden
    
    * Kun sende med utvidet vilkår og ikke hele vilkårsvurderingen
    
    * Lage grunnmur for tester
    
    * Skriv flere tester for delt bosted
    
    * Forenkle tester
    
    * ktlint
    
    * Tester for etterbetaling 3 år
    
    * Tester for småbarnstillegg
    
    * Starte på forenkling av tester
    
    * ktlint
    
    * Justere litt på struktur, linjeskift for bedre lesbarhet etc.
    
    * Justeringer etter qa 1
    
    * Oppdater testnavn
    
    * Oppdater tester slik at de gjenspeiler skissene. Legg til skissene til kodebase.
    
    * Oppdater tester slik at de gjenspeiler skissene. Legg til skissene til kodebase.
    
    * Refaktorerte tester.
    
    * Justeringer etter QA
    
    Co-authored-by: Uy Quang Nguyen <uy.quang.nguyen@nav.no>
    Co-authored-by: Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/beregning/VilkårTilTilkjentYtelseTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/beregning/VilkårTilTilkjentYtelseTest.kt
@@ -71,7 +71,7 @@ class VilkårTilTilkjentYtelseTest {
 
         val personopplysningGrunnlag = lagTestPersonopplysningGrunnlag(vilkårsvurdering.behandling.id, søker, barn1)
 
-        val faktiskTilkjentYtelse = TilkjentYtelseUtils.beregnTilkjentYtelse(
+        val faktiskTilkjentYtelse = TilkjentYtelseUtils.beregnTilkjentYtelseGammel(
             vilkårsvurdering = vilkårsvurdering,
             personopplysningGrunnlag = personopplysningGrunnlag,
             behandling = lagBehandling()
@@ -134,7 +134,7 @@ class VilkårTilTilkjentYtelseTest {
 
         val personopplysningGrunnlag = lagTestPersonopplysningGrunnlag(vilkårsvurdering.behandling.id, søker, barn1)
 
-        val faktiskTilkjentYtelse = TilkjentYtelseUtils.beregnTilkjentYtelse(
+        val faktiskTilkjentYtelse = TilkjentYtelseUtils.beregnTilkjentYtelseGammel(
             vilkårsvurdering = vilkårsvurdering,
             personopplysningGrunnlag = personopplysningGrunnlag,
             behandling = lagBehandling()
@@ -211,7 +211,7 @@ class VilkårTilTilkjentYtelseTest {
         val personopplysningGrunnlag =
             lagTestPersonopplysningGrunnlag(vilkårsvurdering.behandling.id, søker, barn1, barn2)
 
-        val faktiskTilkjentYtelse = TilkjentYtelseUtils.beregnTilkjentYtelse(
+        val faktiskTilkjentYtelse = TilkjentYtelseUtils.beregnTilkjentYtelseGammel(
             vilkårsvurdering = vilkårsvurdering,
             personopplysningGrunnlag = personopplysningGrunnlag,
             behandling = lagBehandling()
