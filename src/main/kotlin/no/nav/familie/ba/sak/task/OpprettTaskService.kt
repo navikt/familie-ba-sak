346aa70164d52c0ee8e0553bc3b66e0c71138a57
Stig Strøm <stig.helge.strom@nav.no>
2022-09-06 08:11:30 +0200

    Setter egen callId for hver autobrev task (#2807)

--- src/main/kotlin/no/nav/familie/ba/sak/task/OpprettTaskService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/task/OpprettTaskService.kt
@@ -8,7 +8,9 @@ import no.nav.familie.ba.sak.task.dto.AutobrevOpphørSmåbarnstilleggDTO
 import no.nav.familie.kontrakter.felles.objectMapper
 import no.nav.familie.kontrakter.felles.oppgave.Oppgavetype
 import no.nav.familie.log.IdUtils
+import no.nav.familie.log.mdc.MDCConstants
 import no.nav.familie.prosessering.domene.Task
+import org.slf4j.MDC
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 import java.util.Properties
@@ -43,39 +45,56 @@ class OpprettTaskService(
     }
 
     fun opprettAutovedtakFor6Og18ÅrBarn(fagsakId: Long, alder: Int) {
-        taskRepository.save(
-            Task(
-                type = SendAutobrev6og18ÅrTask.TASK_STEP_TYPE,
-                payload = objectMapper.writeValueAsString(
-                    Autobrev6og18ÅrDTO(
-                        fagsakId = fagsakId,
-                        alder = alder,
-                        årMåned = inneværendeMåned()
-                    )
-                ),
-                properties = Properties().apply {
-                    this["fagsak"] = fagsakId.toString()
-                    this["callId"] = IdUtils.generateId()
-                }
+        overstyrTaskMedNyCallId(IdUtils.generateId()) {
+            taskRepository.save(
+                Task(
+                    type = SendAutobrev6og18ÅrTask.TASK_STEP_TYPE,
+                    payload = objectMapper.writeValueAsString(
+                        Autobrev6og18ÅrDTO(
+                            fagsakId = fagsakId,
+                            alder = alder,
+                            årMåned = inneværendeMåned()
+                        )
+                    ),
+                    properties = Properties().apply {
+                        this["fagsak"] = fagsakId.toString()
+                    }
+                )
             )
-        )
+        }
     }
 
     fun opprettAutovedtakForOpphørSmåbarnstilleggTask(fagsakId: Long) {
-        taskRepository.save(
-            Task(
-                type = SendAutobrevOpphørSmåbarnstilleggTask.TASK_STEP_TYPE,
-                payload = objectMapper.writeValueAsString(
-                    AutobrevOpphørSmåbarnstilleggDTO(
-                        fagsakId = fagsakId
-                    )
-                ),
-                properties = Properties().apply {
-                    this["fagsakId"] = fagsakId.toString()
-                    this["callId"] = IdUtils.generateId()
-                }
+        overstyrTaskMedNyCallId(IdUtils.generateId()) {
+            taskRepository.save(
+                Task(
+                    type = SendAutobrevOpphørSmåbarnstilleggTask.TASK_STEP_TYPE,
+                    payload = objectMapper.writeValueAsString(
+                        AutobrevOpphørSmåbarnstilleggDTO(
+                            fagsakId = fagsakId
+                        )
+                    ),
+                    properties = Properties().apply {
+                        this["fagsakId"] = fagsakId.toString()
+                    }
+                )
             )
-        )
+        }
+    }
+
+    private inline fun <T> overstyrTaskMedNyCallId(callId: String, body: () -> T): T {
+        val originalCallId = MDC.get(MDCConstants.MDC_CALL_ID) ?: null
+
+        return try {
+            MDC.put(MDCConstants.MDC_CALL_ID, callId)
+            body()
+        } finally {
+            if (originalCallId == null) {
+                MDC.remove(MDCConstants.MDC_CALL_ID)
+            } else {
+                MDC.put(MDCConstants.MDC_CALL_ID, originalCallId)
+            }
+        }
     }
 
     companion object {
