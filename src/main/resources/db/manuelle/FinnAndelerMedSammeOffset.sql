7a818847fb97e6e4f7d15983fa235e22993e4533
Mads Opheim <mads.opheim@computas.com>
2022-09-15 09:58:19 +0200

    Tea-9533: Tar også med når behandlinga vart oppretta

--- src/main/resources/db/manuelle/FinnAndelerMedSammeOffset.sql
+++ src/main/resources/db/manuelle/FinnAndelerMedSammeOffset.sql
@@ -1,4 +1,4 @@
-select aty.periode_offset, b.id, count(1) from andel_tilkjent_ytelse aty
+select aty.periode_offset, b.id as "Behandlingsid", b.opprettet_tid as "Behandling opprettet", count(1) from andel_tilkjent_ytelse aty
                                                    inner join behandling b on b.id = aty.fk_behandling_id
-group by (b.id, aty.periode_offset)
+group by (b.id, aty.periode_offset, b.opprettet_tid)
 having count(aty.periode_offset) > 1;
