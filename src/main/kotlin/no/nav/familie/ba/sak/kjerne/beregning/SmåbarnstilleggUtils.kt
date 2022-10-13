0a07877553fc12a2d06af2dbfecf59fc0340993b
Ida Merete Enholm <ida.merete.enholm@nav.no>
2022-10-13 15:31:56 +0200

    Ta i bruk ny metode å generere småbarnstillegg på i funksjonen som sjekker om vedtak påvirker fagsak

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/beregning/SmåbarnstilleggUtils.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/beregning/SmåbarnstilleggUtils.kt
@@ -51,15 +51,18 @@ fun vedtakOmOvergangsstønadPåvirkerFagsak(
     forrigeAndelerTilkjentYtelse: List<AndelTilkjentYtelseMedEndreteUtbetalinger>,
     barnasAktørerOgFødselsdatoer: List<Pair<Aktør, LocalDate>>
 ): Boolean {
-    val (forrigeSøkersSmåbarnstilleggAndeler, forrigeSøkersAndreAndeler) = forrigeAndelerTilkjentYtelse.partition { it.erSmåbarnstillegg() }
+    val (forrigeSmåbarnstilleggAndeler, forrigeAndelerIkkeSmåbarnstillegg) = forrigeAndelerTilkjentYtelse.partition { it.erSmåbarnstillegg() }
 
-    val nyeSmåbarnstilleggAndeler = småbarnstilleggBarnetrygdGenerator.lagSmåbarnstilleggAndelerGammel(
+    val (utvidetAndeler, barnasAndeler) = forrigeAndelerIkkeSmåbarnstillegg.partition { it.erUtvidet() }
+
+    val nyeSmåbarnstilleggAndeler = småbarnstilleggBarnetrygdGenerator.lagSmåbarnstilleggAndeler(
         perioderMedFullOvergangsstønad = nyePerioderMedFullOvergangsstønad,
-        andelerTilkjentYtelse = forrigeSøkersAndreAndeler,
+        barnasAndeler = barnasAndeler,
+        utvidetAndeler = utvidetAndeler,
         barnasAktørerOgFødselsdatoer = barnasAktørerOgFødselsdatoer
     )
 
-    return forrigeSøkersSmåbarnstilleggAndeler.erUlike(nyeSmåbarnstilleggAndeler)
+    return forrigeSmåbarnstilleggAndeler.erUlike(nyeSmåbarnstilleggAndeler)
 }
 
 fun hentInnvilgedeOgReduserteAndelerSmåbarnstillegg(
