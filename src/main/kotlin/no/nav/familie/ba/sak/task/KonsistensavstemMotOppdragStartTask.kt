c3a496467c8dfdb117cb261d80ba5a7132e83d9d
Stig Helge Strøm <stig.helge.strom@nav.no>
2022-10-24 13:42:14 +0200

    perf: begrense antall kall mot database ved konistensavstemming og mulighet til å trigge for test i dryRun

--- src/main/kotlin/no/nav/familie/ba/sak/task/KonsistensavstemMotOppdragStartTask.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/task/KonsistensavstemMotOppdragStartTask.kt
@@ -6,7 +6,10 @@ import no.nav.familie.kontrakter.felles.objectMapper
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
+import org.slf4j.LoggerFactory
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 import java.util.UUID
 
 @Service
@@ -47,7 +50,40 @@ class KonsistensavstemMotOppdragStartTask(val avstemmingService: AvstemmingServi
         )
     }
 
+    fun dryRunKonsistensavstemming() {
+        logger.info("Start: hent behandlinger klar for konsistensavstemming ${LocalDateTime.now()}")
+        var relevanteBehandlinger = avstemmingService.hentSisteIverksatteBehandlingerFraLøpendeFagsaker()
+
+        for (chunkNr in 1..100) {
+            logger.info("Chunk $chunkNr: hent behandlinger klar for konsistensavstemming ${LocalDateTime.now()}")
+            relevanteBehandlinger =
+                avstemmingService.hentSisteIverksatteBehandlingerFraLøpendeFagsaker(relevanteBehandlinger.nextPageable())
+        }
+        logger.info("Slutt: hent behandlinger klar for konsistensavstemming ${LocalDateTime.now()}")
+    }
+
+    fun dryRunKonsistensavstemmingOmskriving(size: Int) {
+        logger.info("Start: hent behandlinger klar for konsistensavstemming omskriving ${LocalDateTime.now()}")
+        val sideantall = Pageable.ofSize(size)
+        var relevanteBehandlinger = avstemmingService.hentSisteIverksatteBehandlingerFraLøpendeFagsaker(sideantall)
+        logger.info("Antall sider: ${relevanteBehandlinger.totalPages} og antallBehandlinger ${relevanteBehandlinger.size}")
+        var chunkNr = 0
+
+        for (pageNumber in 1..10) {
+            relevanteBehandlinger.chunked(500).forEach { behandlinger ->
+                logger.info("Chunk $chunkNr: hent behandlinger klar for konsistensavstemming omskriving ${LocalDateTime.now()}")
+                chunkNr = chunkNr.inc()
+            }
+            relevanteBehandlinger =
+                avstemmingService.hentSisteIverksatteBehandlingerFraLøpendeFagsaker(relevanteBehandlinger.nextPageable())
+            logger.info("Antall sider: $relevanteBehandlinger og antallBehandlinger ${relevanteBehandlinger.size}")
+        }
+
+        logger.info("Slutt: hent behandlinger klar for konsistensavstemming omskriving${LocalDateTime.now()}")
+    }
+
     companion object {
         const val TASK_STEP_TYPE = "konsistensavstemMotOppdragStart"
+        private val logger = LoggerFactory.getLogger(KonsistensavstemMotOppdragStartTask::class.java)
     }
 }
