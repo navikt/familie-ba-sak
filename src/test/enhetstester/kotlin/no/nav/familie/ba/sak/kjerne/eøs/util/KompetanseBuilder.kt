90c08b2b1035e90f91c32eedab57d3741dc702bd
Jørn Ola Birkeland <jorn.ola.birkeland@nav.no>
2022-10-26 15:18:46 +0200

    Utvider testcontrollere for å kunne injisere vilkårsvurdering og kompetanse med postman

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/eøs/util/KompetanseBuilder.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/eøs/util/KompetanseBuilder.kt
@@ -1,8 +1,10 @@
 package no.nav.familie.ba.sak.kjerne.tidslinje.util
 
 import no.nav.familie.ba.sak.kjerne.eøs.felles.BehandlingId
+import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.AnnenForeldersAktivitet
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.Kompetanse
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.KompetanseResultat
+import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.SøkersAktivitet
 import no.nav.familie.ba.sak.kjerne.eøs.util.SkjemaBuilder
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.Person
 import no.nav.familie.ba.sak.kjerne.tidslinje.tid.Måned
@@ -13,14 +15,30 @@ class KompetanseBuilder(
     behandlingId: BehandlingId = BehandlingId(1)
 ) : SkjemaBuilder<Kompetanse, KompetanseBuilder>(startMåned, behandlingId) {
 
-    fun medKompetanse(k: String, vararg barn: Person, annenForeldersAktivitetsland: String? = null) = medSkjema(k, barn.toList()) {
-        when (it) {
-            '-' -> Kompetanse.NULL.copy(annenForeldersAktivitetsland = annenForeldersAktivitetsland)
-            'S' -> Kompetanse.NULL.copy(resultat = KompetanseResultat.NORGE_ER_SEKUNDÆRLAND, annenForeldersAktivitetsland = annenForeldersAktivitetsland)
-            'P' -> Kompetanse.NULL.copy(resultat = KompetanseResultat.NORGE_ER_PRIMÆRLAND, annenForeldersAktivitetsland = annenForeldersAktivitetsland)
-            else -> null
+    fun medKompetanse(k: String, vararg barn: Person, annenForeldersAktivitetsland: String? = null) =
+        medSkjema(k, barn.toList()) {
+            when (it) {
+                '-' -> Kompetanse.NULL.copy(annenForeldersAktivitetsland = annenForeldersAktivitetsland)
+                'S' -> Kompetanse.NULL.copy(
+                    resultat = KompetanseResultat.NORGE_ER_SEKUNDÆRLAND,
+                    annenForeldersAktivitetsland = annenForeldersAktivitetsland
+                ).fyllUt()
+                'P' -> Kompetanse.NULL.copy(
+                    resultat = KompetanseResultat.NORGE_ER_PRIMÆRLAND,
+                    annenForeldersAktivitetsland = annenForeldersAktivitetsland
+                ).fyllUt()
+                else -> null
+            }
         }
-    }
 
     fun byggKompetanser(): Collection<Kompetanse> = bygg()
 }
+
+fun Kompetanse.fyllUt() = this.copy(
+    resultat = resultat ?: KompetanseResultat.NORGE_ER_SEKUNDÆRLAND,
+    annenForeldersAktivitetsland = annenForeldersAktivitetsland ?: "DK",
+    barnetsBostedsland = barnetsBostedsland ?: "NO",
+    søkersAktivitet = søkersAktivitet ?: SøkersAktivitet.ARBEIDER,
+    annenForeldersAktivitet = annenForeldersAktivitet ?: AnnenForeldersAktivitet.I_ARBEID,
+    søkersAktivitetsland = søkersAktivitetsland ?: "SE"
+)
