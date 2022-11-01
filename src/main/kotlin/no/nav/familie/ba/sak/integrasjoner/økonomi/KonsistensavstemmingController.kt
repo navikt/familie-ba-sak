61e004740aa904342d925cefeb6de7dfddda4177
Stig Helge Strøm <stig.helge.strom@nav.no>
2022-11-01 08:58:52 +0100

    Controllermetode for å trigge konsistensavstemming manuelt ved behov

--- src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/økonomi/KonsistensavstemmingController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/økonomi/KonsistensavstemmingController.kt
@@ -7,11 +7,11 @@ import no.nav.familie.kontrakter.felles.objectMapper
 import no.nav.familie.prosessering.domene.Task
 import no.nav.familie.prosessering.domene.TaskRepository
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
@@ -30,8 +30,26 @@ class KonsistensavstemmingController(
 
     @PostMapping(path = ["/dryrun"], produces = [MediaType.APPLICATION_JSON_VALUE])
     @Transactional
-    @Unprotected
     fun kjørKonsistensavstemmingUtenSendingTilØkonomi(): ResponseEntity<Ressurs<String>> {
+        val (transaksjonsId, task) = opprettKonsistensavstemMotOppdragStartTask(false, LocalDateTime.now())
+
+        return ResponseEntity.ok(Ressurs.success("Testkjører konsistensavstemming uten å sende til økonomi. transaksjonsId=$transaksjonsId callId=${task.callId}"))
+    }
+
+    @PostMapping(path = ["/run"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @Transactional
+    fun kjørKonsistensavstemming(@RequestBody request: StartKonsistensavstemming): ResponseEntity<Ressurs<String>> {
+        val (transaksjonsId, task) = opprettKonsistensavstemMotOppdragStartTask(true, request.triggerTid)
+
+        return ResponseEntity.ok(Ressurs.success("Kjører konsistensavstemming. transaksjonsId=$transaksjonsId callId=${task.callId}"))
+    }
+
+    data class StartKonsistensavstemming(val triggerTid: LocalDateTime)
+
+    private fun opprettKonsistensavstemMotOppdragStartTask(
+        sendTilØkonomi: Boolean,
+        triggerTid: LocalDateTime
+    ): Pair<UUID, Task> {
         val transaksjonsId = UUID.randomUUID()
         val batch = batchRepository.saveAndFlush(Batch(kjøreDato = LocalDate.now(), status = KjøreStatus.MANUELL))
         val task = taskRepository.save(
@@ -40,14 +58,14 @@ class KonsistensavstemmingController(
                 payload = objectMapper.writeValueAsString(
                     KonsistensavstemmingStartTaskDTO(
                         batchId = batch.id,
-                        avstemmingdato = LocalDateTime.now(),
+                        avstemmingdato = triggerTid,
                         transaksjonsId = transaksjonsId,
-                        sendTilØkonomi = false
+                        sendTilØkonomi = sendTilØkonomi
                     )
-                )
+                ),
+                triggerTid = triggerTid
             )
         )
-
-        return ResponseEntity.ok(Ressurs.success("Testkjører konsistensavstemming uten å sende til økonomi. transaksjonsId=$transaksjonsId callId=${task.callId}"))
+        return Pair(transaksjonsId, task)
     }
 }
