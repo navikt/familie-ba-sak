4c98c0632e78e8a5aa3d36db96119012345afa3e
Mads Opheim <mads.opheim@nav.no>
2022-11-11 08:43:43 +0100

    Tar bort unødvendig ekskludering, dette skjer av seg sjølv no når repository-klassa ikkje lenger er synleg

--- src/main/kotlin/no/nav/familie/ba/sak/config/DatabaseConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/DatabaseConfig.kt
@@ -3,9 +3,7 @@ package no.nav.familie.ba.sak.config
 import no.nav.familie.prosessering.PropertiesWrapperTilStringConverter
 import no.nav.familie.prosessering.StringTilPropertiesWrapperConverter
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.FilterType
 import org.springframework.data.jdbc.core.convert.JdbcCustomConversions
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing
@@ -17,8 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 @EnableJdbcAuditing
 @EnableJdbcRepositories("no.nav.familie.prosessering")
 @EnableJpaRepositories(
-    "no.nav.familie",
-    excludeFilters = [ComponentScan.Filter(type = FilterType.REGEX, pattern = [".*TaskRepository", ".*TaskLoggRepository"])]
+    "no.nav.familie"
 )
 @EnableJpaAuditing
 class DatabaseConfig : AbstractJdbcConfiguration() {
