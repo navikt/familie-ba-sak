41d86b9221ff49fb3c60224148b2fe1c7351bcd0
Mads Opheim <mads.opheim@nav.no>
2022-11-23 13:20:37 +0100

    Historikkinnslag, pluss endepunkt for å fjerne trekk i løpende utbetaling

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/trekkILøpendeUtbetaling/TrekkILøpendeUtbetalingController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/trekkILøpendeUtbetaling/TrekkILøpendeUtbetalingController.kt
@@ -4,6 +4,7 @@ import no.nav.familie.ba.sak.kjerne.steg.BehandlerRolle
 import no.nav.familie.ba.sak.sikkerhet.TilgangService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -28,6 +29,17 @@ class TrekkILøpendeUtbetalingController(
             handling = "legg til trekk i løpende utbetaling"
         )
         service.leggTilTrekkILøpendeUtbetaling(trekkILøpendeUtbetaling)
+    }
+
+    @DeleteMapping
+    fun fjernTrekkILøpendeUtbetaling(
+        @RequestBody trekkILøpendeUtbetaling: RestTrekkILøpendeUtbetaling
+    ) {
+        tilgangService.verifiserHarTilgangTilHandling(
+            minimumBehandlerRolle = BehandlerRolle.VEILEDER,
+            handling = "Fjerner trekk i løpende utbetaling"
+        )
+        service.fjernTrekkILøpendeUtbetaling(trekkILøpendeUtbetaling)
     }
 
     @GetMapping
--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/trekkILøpendeUtbetaling/TrekkILøpendeUtbetalingService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/trekkILøpendeUtbetaling/TrekkILøpendeUtbetalingService.kt
@@ -1,12 +1,16 @@
 package no.nav.familie.ba.sak.kjerne.vedtak.trekkILøpendeUtbetaling
 
+import no.nav.familie.ba.sak.kjerne.logg.LoggService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class TrekkILøpendeUtbetalingService(
     @Autowired
-    private val repository: TrekkILøpendeUtbetalingRepository
+    private val repository: TrekkILøpendeUtbetalingRepository,
+
+    @Autowired
+    private val loggService: LoggService
 ) {
     fun leggTilTrekkILøpendeUtbetaling(trekkILøpendeUtbetaling: RestTrekkILøpendeUtbetaling) {
         repository.save(
@@ -17,6 +21,19 @@ class TrekkILøpendeUtbetalingService(
                 feilutbetaltBeløp = trekkILøpendeUtbetaling.feilutbetaltBeløp
             )
         )
+        loggService.loggTrekkILøpendeUtbetalingLagtTil(behandlingId = trekkILøpendeUtbetaling.behandlingId)
+    }
+
+    fun fjernTrekkILøpendeUtbetaling(trekkILøpendeUtbetaling: RestTrekkILøpendeUtbetaling) {
+        repository.delete(
+            TrekkILøpendeUtbetaling(
+                behandlingId = trekkILøpendeUtbetaling.behandlingId,
+                fom = trekkILøpendeUtbetaling.fom,
+                tom = trekkILøpendeUtbetaling.tom,
+                feilutbetaltBeløp = trekkILøpendeUtbetaling.feilutbetaltBeløp
+            )
+        )
+        loggService.loggTrekkILøpendeUtbetalingFjernet(behandlingId = trekkILøpendeUtbetaling.behandlingId)
     }
 
     fun hentTrekkILøpendeUtbetalinger() = repository.findAll().map {
