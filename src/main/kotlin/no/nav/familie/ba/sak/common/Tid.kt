9c6c74d58883e9842d687db6e0c11863cdf84d17
Geir Roger Moen <58559732+grmoen-nav@users.noreply.github.com>
2022-11-28 13:48:45 +0100

    Riktig datoformat på vedtaksdato i logginnslag for korrigert vedtak (#3102)

--- src/main/kotlin/no/nav/familie/ba/sak/common/Tid.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/common/Tid.kt
@@ -15,6 +15,7 @@ val TIDENES_ENDE = LocalDate.MAX
 fun LocalDate.tilddMMyy() = this.format(DateTimeFormatter.ofPattern("ddMMyy", nbLocale))
 fun LocalDate.tilyyyyMMdd() = this.format(DateTimeFormatter.ofPattern("yyyy-MM-dd", nbLocale))
 fun LocalDate.tilKortString() = this.format(DateTimeFormatter.ofPattern("dd.MM.yy", nbLocale))
+fun LocalDate.tilddMMyyyy() = this.format(DateTimeFormatter.ofPattern("dd.MM.yyyy", nbLocale))
 fun YearMonth.tilKortString() = this.format(DateTimeFormatter.ofPattern("MM.yy", nbLocale))
 fun LocalDate.tilDagMånedÅr() = this.format(DateTimeFormatter.ofPattern("d. MMMM yyyy", nbLocale))
 fun LocalDate.tilMånedÅr() = this.format(DateTimeFormatter.ofPattern("MMMM yyyy", nbLocale))
