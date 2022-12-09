8c99f6bbb7151c6b58b12b456ecbda314d95ca91
sarahjelle <sara.hjelle@bekk.no>
2022-12-09 09:59:57 +0100

    Lag egen metode for å hente basert på behandlingId

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/trekkILøpendeUtbetaling/TrekkILøpendeUtbetalingRepository.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/trekkILøpendeUtbetaling/TrekkILøpendeUtbetalingRepository.kt
@@ -1,5 +1,10 @@
 package no.nav.familie.ba.sak.kjerne.vedtak.trekkILøpendeUtbetaling
 
+import no.nav.familie.ba.sak.kjerne.vedtak.Vedtak
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
-interface TrekkILøpendeUtbetalingRepository : JpaRepository<TrekkILøpendeUtbetaling, Long>
+interface TrekkILøpendeUtbetalingRepository : JpaRepository<TrekkILøpendeUtbetaling, Long>{
+    @Query(value = "SELECT t FROM TrekkILøpendeUtbetaling t WHERE t.behandlingId = :behandlingId ORDER BY t.opprettetTidspunkt ASC")
+    fun finnTrekkILøpendeUtbetalingForBehandling(behandlingId: Long): List<TrekkILøpendeUtbetaling>
+}
