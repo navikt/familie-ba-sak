4c1f651aec200c01bc5bfe6023ea92cb1e462e84
sarahjelle <sara.hjelle@bekk.no>
2022-12-09 09:58:00 +0100

    Legg til TrekkILøpendeUtbetaling i behandling

--- src/main/kotlin/no/nav/familie/ba/sak/ekstern/restDomene/RestUtvidetBehandling.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/ekstern/restDomene/RestUtvidetBehandling.kt
@@ -6,6 +6,7 @@ import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingType
 import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandlingsresultat
 import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingÅrsak
 import no.nav.familie.ba.sak.kjerne.steg.StegType
+import no.nav.familie.ba.sak.kjerne.vedtak.trekkILøpendeUtbetaling.RestTrekkILøpendeUtbetaling
 import no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.Utbetalingsperiode
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -42,5 +43,6 @@ data class RestUtvidetBehandling(
     val utenlandskePeriodebeløp: List<RestUtenlandskPeriodebeløp>,
     val verge: VergeInfo?,
     val korrigertEtterbetaling: RestKorrigertEtterbetaling?,
-    val korrigertVedtak: RestKorrigertVedtak?
+    val korrigertVedtak: RestKorrigertVedtak?,
+    val trekkILøpendeUtbetaling: List<RestTrekkILøpendeUtbetaling>?
 )
