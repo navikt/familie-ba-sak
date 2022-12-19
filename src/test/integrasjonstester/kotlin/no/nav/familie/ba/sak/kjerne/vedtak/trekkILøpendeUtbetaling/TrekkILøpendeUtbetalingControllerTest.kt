4680e09198deb19be0c57968cd50ba4d2fcafc80
sarahjelle <sara.hjelle@bekk.no>
2022-12-19 13:25:39 +0100

    Oppdatere variabel og funksjonsnavn i controller

--- src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/trekkILøpendeUtbetaling/TrekkILøpendeUtbetalingControllerTest.kt
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/trekkILøpendeUtbetaling/TrekkILøpendeUtbetalingControllerTest.kt
@@ -25,22 +25,22 @@ class TrekkILøpendeUtbetalingControllerTest(
         val fagsak =
             defaultFagsak(aktør = randomAktør().also { aktørIdRepository.save(it) }).let { fagsakRepository.save(it) }
         val behandling = lagBehandling(fagsak = fagsak).let { behandlingRepository.save(it) }
-        val trekk = RestTrekkILøpendeUtbetaling(
+        val trekk = RestFeilutbetaltValuta(
             id = 0,
             fom = LocalDate.of(2020, Month.JANUARY, 1),
             tom = LocalDate.of(2021, Month.MAY, 31),
             feilutbetaltBeløp = 1234
         )
 
-        val id = service.leggTilTrekkILøpendeUtbetaling(trekkILøpendeUtbetaling = trekk, behandlingId = behandling.id)
+        val id = service.leggTilFeilutbetaltValutaPeriode(feilutbetaltValuta = trekk, behandlingId = behandling.id)
 
-        service.hentTrekkILøpendeUtbetalinger(behandlingId = behandling.id)
+        service.hentFeilutbetaltValutaPerioder(behandlingId = behandling.id)
             .also { Assertions.assertThat(it[0].id).isEqualTo(id) }
             .also { Assertions.assertThat(it[0].fom).isNotNull() }
             .also { Assertions.assertThat(it[0].tom).isNotNull() }
 
-        service.oppdaterTrekkILøpendeUtbetaling(
-            trekkILøpendeUtbetaling = RestTrekkILøpendeUtbetaling(
+        service.oppdaterFeilutbetaltValutaPeriode(
+            feilutbetaltValuta = RestFeilutbetaltValuta(
                 id = id,
                 fom = LocalDate.of(2020, Month.JANUARY, 1),
                 tom = LocalDate.of(2020, Month.MAY, 31),
@@ -49,26 +49,26 @@ class TrekkILøpendeUtbetalingControllerTest(
             id = id
         )
 
-        service.hentTrekkILøpendeUtbetalinger(behandlingId = behandling.id)
+        service.hentFeilutbetaltValutaPerioder(behandlingId = behandling.id)
             .also { Assertions.assertThat(it.get(0).id).isEqualTo(id) }
             .also { Assertions.assertThat(it.get(0).tom).isEqualTo("2020-05-31") }
 
-        val trekk2 = RestTrekkILøpendeUtbetaling(
+        val trekk2 = RestFeilutbetaltValuta(
             id = 0,
             fom = LocalDate.of(2019, Month.DECEMBER, 1),
             tom = LocalDate.of(2019, Month.DECEMBER, 31),
             feilutbetaltBeløp = 100
         )
 
-        val id2 = service.leggTilTrekkILøpendeUtbetaling(trekkILøpendeUtbetaling = trekk2, behandlingId = behandling.id)
+        val id2 = service.leggTilFeilutbetaltValutaPeriode(feilutbetaltValuta = trekk2, behandlingId = behandling.id)
 
-        service.hentTrekkILøpendeUtbetalinger(behandlingId = behandling.id)
+        service.hentFeilutbetaltValutaPerioder(behandlingId = behandling.id)
             .also { Assertions.assertThat(it.size).isEqualTo(2) }
             .also { Assertions.assertThat(it.get(0).id).isEqualTo(id2) }
 
-        service.fjernTrekkILøpendeUtbetaling(id = id, behandlingId = behandling.id)
+        service.fjernFeilutbetaltValutaPeriode(id = id, behandlingId = behandling.id)
 
-        service.hentTrekkILøpendeUtbetalinger(behandlingId = behandling.id)
+        service.hentFeilutbetaltValutaPerioder(behandlingId = behandling.id)
             .also { Assertions.assertThat(it.size).isEqualTo(1) }
             .also { Assertions.assertThat(it[0].id).isEqualTo(id2) }
     }
