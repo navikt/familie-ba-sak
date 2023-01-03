aced3bc5b48514acc6a07682cf7f255a6bbbe829
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-01-03 08:56:50 +0100

    Fjern toggle som ikke blir brukt noen steder

--- src/main/kotlin/no/nav/familie/ba/sak/config/featureToggle/FeatureToggleConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/featureToggle/FeatureToggleConfig.kt
@@ -10,7 +10,6 @@ class FeatureToggleConfig {
         const val KAN_MANUELT_MIGRERE_ANNET_ENN_DELT_BOSTED = "familie-ba-sak.manuell-migrering-ikke-delt-bosted"
         const val ENDRINGER_I_VALIDERING_FOR_MIGRERINGSBEHANDLING =
             "familie-ba-sak.endringer.validering.migeringsbehandling"
-        const val NY_MÅTE_Å_GENERERE_ANDELER_TILKJENT_YTELSE = "familie-ba-sak.behandling.generer-andeler-med-ny-metode"
         const val SJEKK_OM_UTVIDET_ER_ENDRET_BEHANDLINGSRESULTAT =
             "familie-ba-sak.behandling.behandlingsresultat-utvidet-endret"
         const val NY_MÅTE_Å_GENERERE_ATY_BARNA = "familie-ba-sak.behandling.ny-metode-generer-aty-barna"
