d3580fe21a10ebd5c276b4da60d92d6276578966
halvormundal <halvorbmundal@gmail.com>
2023-01-05 16:26:54 +0100

    ktlint

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/klage/KlageService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/klage/KlageService.kt
@@ -180,5 +180,5 @@ private enum class Årsak(
 ) {
 
     ÅPEN_BEHANDLING(IkkeOpprettetÅrsak.ÅPEN_BEHANDLING, KanIkkeOppretteRevurderingÅrsak.ÅPEN_BEHANDLING),
-    INGEN_BEHANDLING(IkkeOpprettetÅrsak.INGEN_BEHANDLING, KanIkkeOppretteRevurderingÅrsak.INGEN_BEHANDLING),
+    INGEN_BEHANDLING(IkkeOpprettetÅrsak.INGEN_BEHANDLING, KanIkkeOppretteRevurderingÅrsak.INGEN_BEHANDLING)
 }
