d93e2c3450c100177461d448f7ce24600766a89a
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-01-25 15:33:51 +0100

    Kopierer over utgangspunktet vi hadde fra gammel PR

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatService.kt
@@ -9,6 +9,7 @@ import no.nav.familie.ba.sak.ekstern.restDomene.SøknadDTO
 import no.nav.familie.ba.sak.kjerne.behandling.BehandlingHentOgPersisterService
 import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandling
 import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandlingsresultat
+import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingÅrsak
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelseMedEndreteUtbetalinger
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelerTilkjentYtelseOgEndreteUtbetalingerService
 import no.nav.familie.ba.sak.kjerne.beregning.domene.YtelseType
@@ -35,6 +36,33 @@ class BehandlingsresultatService(
     private val andelerTilkjentYtelseOgEndreteUtbetalingerService: AndelerTilkjentYtelseOgEndreteUtbetalingerService,
     private val featureToggleService: FeatureToggleService
 ) {
+
+    internal fun finnPersonerFremstiltKravFor(behandling: Behandling, søknadDTO: SøknadDTO?, forrigeBehandling: Behandling?): List<Aktør> {
+        val personerFremstiltKravFor = if (behandling.opprettetÅrsak == BehandlingÅrsak.SØKNAD || behandling.opprettetÅrsak == BehandlingÅrsak.FØDSELSHENDELSE) {
+            // alle barna som er krysset av på søknad
+            val barnFraSøknad = søknadDTO?.barnaMedOpplysninger
+                ?.filter { it.erFolkeregistrert && it.inkludertISøknaden }
+                ?.map { personidentService.hentAktør(it.ident) }
+                ?: emptyList()
+
+            // hvis det søkes om utvidet skal søker med
+            val utvidetBarnetrygdSøker = if (søknadDTO?.underkategori == BehandlingUnderkategoriDTO.UTVIDET) listOf(behandling.fagsak.aktør) else emptyList()
+
+            // alle nye barn
+            val nyeBarn = persongrunnlagService.finnNyeBarn(forrigeBehandling = forrigeBehandling, behandling = behandling)
+                .map { it.aktør }
+
+            // Hva gjør vi med barn som har fått satt eksplisitt avslag, men det er ikke søkt for personen?
+
+            barnFraSøknad + nyeBarn + utvidetBarnetrygdSøker
+        } else if (behandling.erManuellMigrering()) {
+            val nåværendePersonopplysningsgrunnlag = persongrunnlagService.hentAktivThrows(behandlingId = behandling.id)
+
+            nåværendePersonopplysningsgrunnlag.personer.map { it.aktør }
+        } else emptyList()
+
+        return personerFremstiltKravFor.distinct()
+    }
 
     internal fun utledBehandlingsresultat(behandlingId: Long): Behandlingsresultat {
         val behandling = behandlingHentOgPersisterService.hent(behandlingId = behandlingId)
