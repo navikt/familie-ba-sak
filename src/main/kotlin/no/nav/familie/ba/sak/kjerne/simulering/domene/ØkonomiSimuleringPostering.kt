f0ea4b4ffadf0e85ad6c2551c854eff9eae53508
halvorbmundal <halvorbmundal@users.noreply.github.com>
2023-01-27 08:19:47 +0100

    Ikke baser resultat på om feilutbetaling er på feilkonto: Legg til korrigertResultat (#3237)

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/simulering/domene/ØkonomiSimuleringPostering.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/simulering/domene/ØkonomiSimuleringPostering.kt
@@ -68,10 +68,7 @@ data class ØkonomiSimuleringPostering(
     val forfallsdato: LocalDate,
 
     @Column(name = "uten_inntrekk", nullable = false)
-    val utenInntrekk: Boolean,
-
-    @Column(name = "er_feilkonto", nullable = true)
-    val erFeilkonto: Boolean?
+    val utenInntrekk: Boolean
 ) : BaseEntitet() {
 
     override fun hashCode() = id.hashCode()
