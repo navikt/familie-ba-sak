7852aca0f8114db751b1079ea6a76143ef9a0117
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-01-30 16:31:25 +0100

    Lag egen tidslinje for AndelTilkjentYtelse

---
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/beregning/AndelTilkjentYtelseTidslinje.kt
@@ -0,0 +1,22 @@
+package no.nav.familie.ba.sak.kjerne.beregning
+
+import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelse
+import no.nav.familie.ba.sak.kjerne.tidslinje.Periode
+import no.nav.familie.ba.sak.kjerne.tidslinje.Tidslinje
+import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.Måned
+import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.MånedTidspunkt.Companion.tilTidspunkt
+
+class AndelTilkjentYtelseTidslinje(
+    private val andelerTilkjentYtelse: List<AndelTilkjentYtelse>
+) : Tidslinje<AndelTilkjentYtelse, Måned>() {
+
+    override fun lagPerioder(): List<Periode<AndelTilkjentYtelse, Måned>> {
+        return andelerTilkjentYtelse.map {
+            Periode(
+                fraOgMed = it.stønadFom.tilTidspunkt(),
+                tilOgMed = it.stønadTom.tilTidspunkt(),
+                innhold = it
+            )
+        }
+    }
+}
