c36b91fa9bf0565a10b0709665b40744e359b269
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-02-20 13:55:46 +0100

    Send inn personresultater i stedet for vilkårsvurdering

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatValideringUtils.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatValideringUtils.kt
@@ -2,15 +2,15 @@ package no.nav.familie.ba.sak.kjerne.behandlingsresultat
 
 import no.nav.familie.ba.sak.common.Feil
 import no.nav.familie.ba.sak.kjerne.personident.Aktør
-import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.Vilkårsvurdering
+import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.PersonResultat
 
 object BehandlingsresultatValideringUtils {
 
     internal fun validerAtBarePersonerFremstiltKravForEllerSøkerHarFåttEksplisittAvslag(
         personerFremstiltKravFor: List<Aktør>,
-        vilkårsvurdering: Vilkårsvurdering
+        personResultater: Set<PersonResultat>
     ) {
-        val personerSomHarEksplisittAvslag = vilkårsvurdering.personResultater.filter { it.harEksplisittAvslag() }
+        val personerSomHarEksplisittAvslag = personResultater.filter { it.harEksplisittAvslag() }
 
         if (personerSomHarEksplisittAvslag.any { !personerFremstiltKravFor.contains(it.aktør) && !it.erSøkersResultater() }) {
             throw Feil("Det eksisterer personer som har fått eksplisitt avslag, men som det ikke har blitt fremstilt krav for.")
