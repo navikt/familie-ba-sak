ae26dc91ed83660879be43ed448735e300fa91ff
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-02-20 13:15:35 +0100

    Snu på validering som var feil, kun kaste feil hvis eksplisitt avslag er satt på person som ikke er fremstilt krav for og ikke er søker

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatValideringUtils.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatValideringUtils.kt
@@ -12,7 +12,7 @@ object BehandlingsresultatValideringUtils {
     ) {
         val personerSomHarEksplisittAvslag = vilkårsvurdering.personResultater.filter { it.harEksplisittAvslag() }
 
-        if (personerSomHarEksplisittAvslag.all { personerFremstiltKravFor.contains(it.aktør) || it.erSøkersResultater() }) {
+        if (personerSomHarEksplisittAvslag.any { !personerFremstiltKravFor.contains(it.aktør) && !it.erSøkersResultater() }) {
             throw Feil("Det eksisterer personer som har fått eksplisitt avslag, men som det ikke har blitt fremstilt krav for.")
         }
     }
