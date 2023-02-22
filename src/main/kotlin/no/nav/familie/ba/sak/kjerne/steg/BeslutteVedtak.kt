c76a8665fac913a6774e8f5b7ad52d7afd9546eb
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2023-02-22 09:29:56 +0100

    fjerner BRUK_ANDELER_FOR_IVERKSETTELSE_SJEKK toggle og bruker NY_MÅTE_Å_BEREGNE_BEHANDLINGSRESULTAT istedenfor da disse skal ut i prod samtidig

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/steg/BeslutteVedtak.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/steg/BeslutteVedtak.kt
@@ -3,7 +3,6 @@ package no.nav.familie.ba.sak.kjerne.steg
 import no.nav.familie.ba.sak.common.Feil
 import no.nav.familie.ba.sak.common.FunksjonellFeil
 import no.nav.familie.ba.sak.config.FeatureToggleConfig
-import no.nav.familie.ba.sak.config.FeatureToggleConfig.Companion.BRUK_ANDELER_FOR_IVERKSETTELSE_SJEKK
 import no.nav.familie.ba.sak.config.FeatureToggleService
 import no.nav.familie.ba.sak.config.TaskRepositoryWrapper
 import no.nav.familie.ba.sak.kjerne.behandling.BehandlingService
@@ -151,7 +150,7 @@ class BeslutteVedtak(
     }
 
     private fun sjekkOmBehandlingSkalIverksettesOgHentNesteSteg(behandling: Behandling): StegType {
-        val nesteSteg = if (featureToggleService.isEnabled(BRUK_ANDELER_FOR_IVERKSETTELSE_SJEKK)) {
+        val nesteSteg = if (featureToggleService.isEnabled(FeatureToggleConfig.NY_MÅTE_Å_BEREGNE_BEHANDLINGSRESULTAT)) {
             val endringerIUtbetaling =
                 beregningService.erEndringerIUtbetalingMellomNåværendeOgForrigeBehandling(behandling)
 
