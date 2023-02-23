fff39a75500f5a6d4bfda5b5f46ab872b0919c97
halvormundal <halvorbmundal@gmail.com>
2023-02-23 13:48:36 +0100

    Ktlint

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/autovedtak/satsendring/SatsendringService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/autovedtak/satsendring/SatsendringService.kt
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service
 @Service
 class SatsendringService(
     private val behandlingHentOgPersisterService: BehandlingHentOgPersisterService,
-    private val andelerTilkjentYtelseOgEndreteUtbetalingerService: AndelerTilkjentYtelseOgEndreteUtbetalingerService,
+    private val andelerTilkjentYtelseOgEndreteUtbetalingerService: AndelerTilkjentYtelseOgEndreteUtbetalingerService
 ) {
     fun erFagsakOppdatertMedSisteSats(fagsakId: Long): Boolean {
         // Må se på siste iverksatte og ikke siste vedtatte siden vi ønsker å se på
