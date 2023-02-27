54c715c37a44bdcb7b17d23635c2e56667644edf
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-02-27 16:08:48 +0100

    Endring i vilkårsvurdering: kun sammenlign utdypende vilkårsvurdering hvis vilkår hvor det er obligatorisk også var satt i forrige behandling

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/forrigebehandling/EndringIVilkårsvurderingUtil.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/forrigebehandling/EndringIVilkårsvurderingUtil.kt
@@ -1,6 +1,5 @@
 package no.nav.familie.ba.sak.kjerne.forrigebehandling
 
-import no.nav.familie.ba.sak.common.førsteDagIInneværendeMåned
 import no.nav.familie.ba.sak.kjerne.autovedtak.fødselshendelse.Resultat
 import no.nav.familie.ba.sak.kjerne.forrigebehandling.EndringUtil.tilFørsteEndringstidspunktForDagtidslinje
 import no.nav.familie.ba.sak.kjerne.tidslinje.Tidslinje
@@ -8,10 +7,10 @@ import no.nav.familie.ba.sak.kjerne.tidslinje.komposisjon.kombiner
 import no.nav.familie.ba.sak.kjerne.tidslinje.komposisjon.kombinerUtenNullMed
 import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.Dag
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.PersonResultat
+import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.Regelverk
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.Vilkår
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.VilkårResultat
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.tilTidslinje
-import java.time.LocalDate
 import java.time.YearMonth
 
 object EndringIVilkårsvurderingUtil {
@@ -71,7 +70,7 @@ object EndringIVilkårsvurderingUtil {
         val endringIVilkårResultat =
             nåværendeVilkårResultatTidslinje.kombinerUtenNullMed(tidligereVilkårResultatTidslinje) { nåværende, forrige ->
 
-                nåværende.utdypendeVilkårsvurderinger.toSet() != forrige.utdypendeVilkårsvurderinger.toSet() ||
+                (forrige.erObligatoriskeFelterSatt() && nåværende.utdypendeVilkårsvurderinger.toSet() != forrige.utdypendeVilkårsvurderinger.toSet()) ||
                     nåværende.vurderesEtter != forrige.vurderesEtter ||
                     nåværende.periodeFom != forrige.periodeFom
             }
@@ -79,5 +78,19 @@ object EndringIVilkårsvurderingUtil {
         return endringIVilkårResultat
     }
 
-    private fun LocalDate?.førerIkkeTilOpphør(opphørstidspunkt: YearMonth): Boolean = this?.isBefore(opphørstidspunkt.minusMonths(1).førsteDagIInneværendeMåned()) == true
+    private fun VilkårResultat.erObligatoriskeFelterSatt(): Boolean {
+        return (this.utdypendeVilkårsvurderingErObligatorisk() && this.utdypendeVilkårsvurderinger.isNotEmpty()) || !this.utdypendeVilkårsvurderingErObligatorisk()
+    }
+
+    private fun VilkårResultat.utdypendeVilkårsvurderingErObligatorisk(): Boolean {
+        return if (this.vurderesEtter == Regelverk.NASJONALE_REGLER) false
+        else when (this.vilkårType) {
+            Vilkår.BOSATT_I_RIKET,
+            Vilkår.BOR_MED_SØKER -> true
+            Vilkår.UNDER_18_ÅR,
+            Vilkår.LOVLIG_OPPHOLD,
+            Vilkår.GIFT_PARTNERSKAP,
+            Vilkår.UTVIDET_BARNETRYGD -> false
+        }
+    }
 }
