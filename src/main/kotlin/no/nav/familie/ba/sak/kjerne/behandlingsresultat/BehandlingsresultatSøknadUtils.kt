fe3ae3e7a51ff2d21dc77e7da41f28b7458200a5
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-02-27 11:44:06 +0100

    Fra "på" til "for" i feilmelding

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatSøknadUtils.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatSøknadUtils.kt
@@ -129,7 +129,7 @@ object BehandlingsresultatSøknadUtils {
         val resultaterUtenIngenEndringer = this.filter { it != Søknadsresultat.INGEN_RELEVANTE_ENDRINGER }
 
         return when {
-            this.isEmpty() -> throw Feil(frontendFeilmelding = "Du har opprettet en behandling som følge av søknad, men har enten ikke krysset av på noen barn det er søkt for eller avslått/innvilget noen perioder.", message = "Klarer ikke utlede søknadsresultat. Finner ingen resultater.")
+            this.isEmpty() -> throw Feil(frontendFeilmelding = "Du har opprettet en behandling som følge av søknad, men har enten ikke krysset av for noen barn det er søkt for eller avslått/innvilget noen perioder.", message = "Klarer ikke utlede søknadsresultat. Finner ingen resultater.")
             this.size == 1 -> this.single()
             resultaterUtenIngenEndringer.size == 1 -> resultaterUtenIngenEndringer.single()
             resultaterUtenIngenEndringer.size == 2 && resultaterUtenIngenEndringer.containsAll(
