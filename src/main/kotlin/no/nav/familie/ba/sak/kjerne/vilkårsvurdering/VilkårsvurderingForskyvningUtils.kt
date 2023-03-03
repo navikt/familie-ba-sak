caf9544309fd2c782ccfb23013eeff481168b3db
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-03-03 10:03:23 +0100

    Legg på kommentar

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vilkårsvurdering/VilkårsvurderingForskyvningUtils.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vilkårsvurdering/VilkårsvurderingForskyvningUtils.kt
@@ -64,7 +64,7 @@ object VilkårsvurderingForskyvningUtils {
             }
 
         return if (vilkår == Vilkår.UNDER_18_ÅR) {
-            val minstePeriodeFom = this.minOf { it.periodeFom ?: throw Feil("Finner ikke fra og med dato på 'under 18 år'-vilkåret") }
+            val minstePeriodeFom = this.minOf { it.periodeFom ?: throw Feil("Finner ikke fra og med dato på 'under 18 år'-vilkåret") } // Fra og med dato skal være lik fødselsdato for under 18-vilkåret
             tidslinje.beskjærPå18År(fødselsdato = minstePeriodeFom)
         } else {
             tidslinje
