8eef548f1774e5565b60a18550fff67267f9573c
halvorbmundal <halvorbmundal@users.noreply.github.com>
2023-03-07 10:02:31 +0100

    Valider ingen endringer ved endre migreringsdato (#3410)
    * Split hentEndringerIUtbetalingMellomNåværendeOgForrigeBehandlingTidslinje ut i egen funskjon
    
    * Legg på validering at det ikke er noen endring i utbetalt beløp etter migreringsdatoen til forrige iverksatte behandling
    
    * Legg til test
    
    * Legg til logging ved feil
    
    * ktlint
    
    * fra og med -> til og med
    
    * Splitt funksjonell kode i util fil
    
    * behandling 1 -> første behandling

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatSteg.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatSteg.kt
@@ -7,6 +7,7 @@ import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandling
 import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingStatus
 import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingType
 import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandlingsresultat
+import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingÅrsak
 import no.nav.familie.ba.sak.kjerne.beregning.BeregningService
 import no.nav.familie.ba.sak.kjerne.beregning.TilkjentYtelseValidering.validerAtTilkjentYtelseHarFornuftigePerioderOgBeløp
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelerTilkjentYtelseOgEndreteUtbetalingerService
@@ -25,6 +26,7 @@ import no.nav.familie.ba.sak.kjerne.steg.StegType
 import no.nav.familie.ba.sak.kjerne.vedtak.VedtakService
 import no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.VedtaksperiodeService
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.VilkårService
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -78,6 +80,10 @@ class BehandlingsresultatSteg(
             endreteUtbetalingerMedAndeler.map { it.endretUtbetalingAndel },
             vilkårService.hentVilkårsvurdering(behandling.id)
         )
+
+        if (behandling.opprettetÅrsak == BehandlingÅrsak.ENDRE_MIGRERINGSDATO) {
+            validerIngenEndringIUtbetalingEtterMigreringsdatoenTilForrigeIverksatteBehandling(behandling)
+        }
     }
 
     @Transactional
@@ -149,5 +155,20 @@ class BehandlingsresultatSteg(
     private fun settBehandlingsresultat(behandling: Behandling, resultat: Behandlingsresultat): Behandling {
         behandling.resultat = resultat
         return behandlingHentOgPersisterService.lagreEllerOppdater(behandling)
+    }
+
+    fun validerIngenEndringIUtbetalingEtterMigreringsdatoenTilForrigeIverksatteBehandling(behandling: Behandling) {
+        val endringIUtbetalingTidslinje =
+            beregningService.hentEndringerIUtbetalingMellomNåværendeOgForrigeBehandlingTidslinje(behandling)
+
+        val migreringsdatoForrigeIverksatteBehandling = beregningService
+            .hentAndelerFraForrigeIverksattebehandling(behandling)
+            .minOf { it.stønadFom }
+
+        endringIUtbetalingTidslinje.kastFeilVedEndringEtter(migreringsdatoForrigeIverksatteBehandling, behandling)
+    }
+
+    companion object {
+        val logger = LoggerFactory.getLogger(this::class.java)!!
     }
 }
