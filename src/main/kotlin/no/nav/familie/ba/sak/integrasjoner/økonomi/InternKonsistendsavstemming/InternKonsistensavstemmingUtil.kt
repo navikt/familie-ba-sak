287f17ddf46e14c0b3efc78d9b6fcf9bc5e93388
halvormundal <halvorbmundal@gmail.com>
2023-03-09 10:37:14 +0100

    Endre logikken i erForskjellMellomAndelerOgOppdrag til å se på om det finnes en tilsvarende andel for oppdragsperiodene

--- src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/økonomi/InternKonsistendsavstemming/InternKonsistensavstemmingUtil.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/økonomi/InternKonsistendsavstemming/InternKonsistensavstemmingUtil.kt
@@ -1,11 +1,10 @@
 package no.nav.familie.ba.sak.integrasjoner.økonomi.InternKonsistendsavstemming
 
-import no.nav.familie.ba.sak.common.TIDENES_MORGEN
-import no.nav.familie.ba.sak.common.isSameOrAfter
 import no.nav.familie.ba.sak.common.secureLogger
 import no.nav.familie.ba.sak.common.toYearMonth
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelse
 import no.nav.familie.kontrakter.felles.oppdrag.Utbetalingsoppdrag
+import no.nav.familie.kontrakter.felles.oppdrag.Utbetalingsperiode
 
 fun erForskjellMellomAndelerOgOppdrag(
     andeler: List<AndelTilkjentYtelse>,
@@ -17,24 +16,66 @@ fun erForskjellMellomAndelerOgOppdrag(
             ?.filter { it.opphør == null }
             ?: emptyList()
 
-    val startFørsteOppdragsperiode = oppdragsperioder.minOfOrNull { it.vedtakdatoFom } ?: TIDENES_MORGEN
+    val forskjell: AndelOgOppdragForskjell = hentForskjellIAndelerOgUtbetalingsoppdrag(oppdragsperioder, andeler)
 
-    val sumUtbetalingFraAndeler = andeler
-        .filter { it.stønadFom.isSameOrAfter(startFørsteOppdragsperiode.toYearMonth()) }
-        .sumOf { it.kalkulertUtbetalingsbeløp }
-        .toBigDecimal()
-
-    val sumUtbetalingFraOppdrag = oppdragsperioder.sumOf { it.sats }
-
-    val erForskjellMellomAndelerOgOppdrag = sumUtbetalingFraOppdrag != sumUtbetalingFraAndeler
-
-    if (erForskjellMellomAndelerOgOppdrag) {
-        secureLogger.info(
-            "Fagsak $fagsakId har ulikt utbetalingsbeløp i andelene tilkjent ytelse og utbetalingsoppdraget." +
-                "\nSum utbetaling i utbetalingsperiodene=$sumUtbetalingFraOppdrag" +
-                "\nSum utbetaling i andelene=$sumUtbetalingFraAndeler"
+    when (forskjell) {
+        AndelOgOppdragForskjell.TOMT_OPPDRAG_OG_ANDELER_MED_UTBETALIGNG -> secureLogger.info(
+            "Fagsak $fagsakId har andeler med utbetaling, men har ikke sendt noe oppdragsperioder til økonomi"
         )
+
+        AndelOgOppdragForskjell.FORSKJELLIG_AVSLUTTNING_DATO -> secureLogger.info(
+            "Fagsak $fagsakId sine andeler har annen avsluttnignsdato enn oppdraget som er sendt til økonomi"
+        )
+
+        AndelOgOppdragForskjell.OPPDRAGSPERIODE_UTEN_TILSVARENDE_ANDEL -> secureLogger.info(
+            "Fagsak $fagsakId har sendt utbetalingsoppdrag til økonomi som ikke har tilsvarende andel tilkjent ytelse"
+        )
+        AndelOgOppdragForskjell.INGEN_FORSKJELL -> Unit
     }
 
-    return erForskjellMellomAndelerOgOppdrag
+    return forskjell != AndelOgOppdragForskjell.INGEN_FORSKJELL
+}
+
+private fun hentForskjellIAndelerOgUtbetalingsoppdrag(
+    oppdragsperioder: List<Utbetalingsperiode>,
+    andeler: List<AndelTilkjentYtelse>
+) = when {
+    erOppdragTomtOgAndelerMedUtbetaling(oppdragsperioder, andeler) ->
+        AndelOgOppdragForskjell.TOMT_OPPDRAG_OG_ANDELER_MED_UTBETALIGNG
+
+    erForskjelligTomDato(oppdragsperioder, andeler) ->
+        AndelOgOppdragForskjell.FORSKJELLIG_AVSLUTTNING_DATO
+
+    erOppdragsperiodeUtenTilsvarendeAndel(oppdragsperioder, andeler) ->
+        AndelOgOppdragForskjell.OPPDRAGSPERIODE_UTEN_TILSVARENDE_ANDEL
+
+    else -> AndelOgOppdragForskjell.INGEN_FORSKJELL
+}
+
+private fun erOppdragsperiodeUtenTilsvarendeAndel(
+    oppdragsperioder: List<Utbetalingsperiode>,
+    andeler: List<AndelTilkjentYtelse>
+) = !oppdragsperioder.all { oppdragsperiode ->
+    andeler.any {
+        it.kalkulertUtbetalingsbeløp.toBigDecimal() == oppdragsperiode.sats &&
+            it.stønadFom == oppdragsperiode.vedtakdatoFom.toYearMonth() &&
+            it.stønadTom == oppdragsperiode.vedtakdatoTom.toYearMonth()
+    }
+}
+
+private fun erForskjelligTomDato(
+    oppdragsperioder: List<Utbetalingsperiode>,
+    andeler: List<AndelTilkjentYtelse>
+) = oppdragsperioder.maxOf { it.vedtakdatoTom.toYearMonth() } != andeler.maxOf { it.stønadTom }
+
+private fun erOppdragTomtOgAndelerMedUtbetaling(
+    oppdragsperioder: List<Utbetalingsperiode>,
+    andeler: List<AndelTilkjentYtelse>
+) = oppdragsperioder.isEmpty() && andeler.sumOf { it.kalkulertUtbetalingsbeløp } != 0
+
+enum class AndelOgOppdragForskjell {
+    TOMT_OPPDRAG_OG_ANDELER_MED_UTBETALIGNG,
+    FORSKJELLIG_AVSLUTTNING_DATO,
+    OPPDRAGSPERIODE_UTEN_TILSVARENDE_ANDEL,
+    INGEN_FORSKJELL
 }
--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/integrasjoner/økonomi/InternKonsistendsavstemming/InternKonsistensavstemmingUtilTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/integrasjoner/økonomi/InternKonsistendsavstemming/InternKonsistensavstemmingUtilTest.kt
@@ -45,7 +45,7 @@ class InternKonsistensavstemmingUtilTest {
     }
 
     @Test
-    fun `skal registrere at det er forskjell det er diff etter første periode i utbetalingsoppdraget`() {
+    fun `skal at det er forskjellig avsluttningsdato mellom andelene og utbetalingsoppdraget`() {
         val andeler = listOf(
             lagAndelTilkjentYtelse(
                 fom = YearMonth.parse("2021-12"),
@@ -81,6 +81,34 @@ class InternKonsistensavstemmingUtilTest {
         val utbetalingsoppdrag = objectMapper.readValue<Utbetalingsoppdrag>(mockUtbetalingsoppdrag)
 
         Assertions.assertTrue(erForskjellMellomAndelerOgOppdrag(andeler, utbetalingsoppdrag, 0L))
+    }
+
+    @Test
+    fun `skal ikke si det er forskjell ved riktig utbetalingsoppdrag når det er flere kjeder`() {
+        val andelStringer = listOf(
+            "2021-05,2021-08,1354,ORDINÆR_BARNETRYGD",
+            "2021-09,2021-12,1654,ORDINÆR_BARNETRYGD",
+
+            "2022-01,2023-02,1676,ORDINÆR_BARNETRYGD",
+            "2023-03,2024-11,1723,ORDINÆR_BARNETRYGD",
+            "2024-12,2036-11,1083,ORDINÆR_BARNETRYGD",
+
+            "2021-05,2023-02,1054,UTVIDET_BARNETRYGD",
+            "2023-03,2036-11,2489,UTVIDET_BARNETRYGD"
+        )
+
+        val andeler = andelStringer.map { it.split(",") }.map {
+            lagAndelTilkjentYtelse(
+                fom = YearMonth.parse(it[0]),
+                tom = YearMonth.parse(it[1]),
+                beløp = it[2].toInt(),
+                ytelseType = YtelseType.valueOf(it[3])
+            )
+        }
+
+        val utbetalingsoppdrag = objectMapper.readValue<Utbetalingsoppdrag>(utbetalingsoppdragMockMedUtvidet)
+
+        Assertions.assertFalse(erForskjellMellomAndelerOgOppdrag(andeler, utbetalingsoppdrag, 0L))
     }
 }
 
@@ -153,6 +181,129 @@ private val mockUtbetalingsoppdrag = """
           "satsType": "MND",
           "utbetalesTil": "1",
           "behandlingId": 1,
+          "utbetalingsgrad": null
+        }
+      ],
+      "gOmregning": false
+    }
+""".trimIndent()
+
+private val utbetalingsoppdragMockMedUtvidet = """
+    {
+      "kodeEndring": "ENDR",
+      "fagSystem": "BA",
+      "saksnummer": "200028561",
+      "aktoer": "02416938515",
+      "saksbehandlerId": "VL",
+      "avstemmingTidspunkt": "2023-02-08T15:57:38.341011606",
+      "utbetalingsperiode": [
+        {
+          "erEndringPåEksisterendePeriode": true,
+          "opphør": {
+            "opphørDatoFom": "2022-01-01"
+          },
+          "periodeId": 3,
+          "forrigePeriodeId": 2,
+          "datoForVedtak": "2023-02-08",
+          "klassifisering": "BATR",
+          "vedtakdatoFom": "2024-12-01",
+          "vedtakdatoTom": "2036-11-30",
+          "sats": 1054,
+          "satsType": "MND",
+          "utbetalesTil": "02416938515",
+          "behandlingId": 100134370,
+          "utbetalingsgrad": null
+        },
+        {
+          "erEndringPåEksisterendePeriode": true,
+          "opphør": {
+            "opphørDatoFom": "2021-05-01"
+          },
+          "periodeId": 4,
+          "forrigePeriodeId": null,
+          "datoForVedtak": "2023-02-08",
+          "klassifisering": "BATR",
+          "vedtakdatoFom": "2021-05-01",
+          "vedtakdatoTom": "2036-11-30",
+          "sats": 1054,
+          "satsType": "MND",
+          "utbetalesTil": "02416938515",
+          "behandlingId": 100134370,
+          "utbetalingsgrad": null
+        },
+        {
+          "erEndringPåEksisterendePeriode": false,
+          "opphør": null,
+          "periodeId": 5,
+          "forrigePeriodeId": 3,
+          "datoForVedtak": "2023-02-08",
+          "klassifisering": "BATR",
+          "vedtakdatoFom": "2022-01-01",
+          "vedtakdatoTom": "2023-02-28",
+          "sats": 1676,
+          "satsType": "MND",
+          "utbetalesTil": "02416938515",
+          "behandlingId": 100134370,
+          "utbetalingsgrad": null
+        },
+        {
+          "erEndringPåEksisterendePeriode": false,
+          "opphør": null,
+          "periodeId": 6,
+          "forrigePeriodeId": 5,
+          "datoForVedtak": "2023-02-08",
+          "klassifisering": "BATR",
+          "vedtakdatoFom": "2023-03-01",
+          "vedtakdatoTom": "2024-11-30",
+          "sats": 1723,
+          "satsType": "MND",
+          "utbetalesTil": "02416938515",
+          "behandlingId": 100134370,
+          "utbetalingsgrad": null
+        },
+        {
+          "erEndringPåEksisterendePeriode": false,
+          "opphør": null,
+          "periodeId": 7,
+          "forrigePeriodeId": 6,
+          "datoForVedtak": "2023-02-08",
+          "klassifisering": "BATR",
+          "vedtakdatoFom": "2024-12-01",
+          "vedtakdatoTom": "2036-11-30",
+          "sats": 1083,
+          "satsType": "MND",
+          "utbetalesTil": "02416938515",
+          "behandlingId": 100134370,
+          "utbetalingsgrad": null
+        },
+        {
+          "erEndringPåEksisterendePeriode": false,
+          "opphør": null,
+          "periodeId": 8,
+          "forrigePeriodeId": 4,
+          "datoForVedtak": "2023-02-08",
+          "klassifisering": "BATR",
+          "vedtakdatoFom": "2021-05-01",
+          "vedtakdatoTom": "2023-02-28",
+          "sats": 1054,
+          "satsType": "MND",
+          "utbetalesTil": "02416938515",
+          "behandlingId": 100134370,
+          "utbetalingsgrad": null
+        },
+        {
+          "erEndringPåEksisterendePeriode": false,
+          "opphør": null,
+          "periodeId": 9,
+          "forrigePeriodeId": 8,
+          "datoForVedtak": "2023-02-08",
+          "klassifisering": "BATR",
+          "vedtakdatoFom": "2023-03-01",
+          "vedtakdatoTom": "2036-11-30",
+          "sats": 2489,
+          "satsType": "MND",
+          "utbetalesTil": "02416938515",
+          "behandlingId": 100134370,
           "utbetalingsgrad": null
         }
       ],
