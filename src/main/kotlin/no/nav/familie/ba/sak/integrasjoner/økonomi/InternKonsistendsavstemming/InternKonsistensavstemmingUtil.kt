023288234e1c36c542df37e37b6552c46e0fd46d
halvormundal <halvorbmundal@gmail.com>
2023-03-14 12:05:50 +0100

    Endre valideringen på forskjellen til mellom andelene og oppdrag til å se bort i fra fom og tom, så lenge det er samme beløp på samme dato

--- src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/økonomi/InternKonsistendsavstemming/InternKonsistensavstemmingUtil.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/økonomi/InternKonsistendsavstemming/InternKonsistensavstemmingUtil.kt
@@ -1,57 +1,90 @@
 package no.nav.familie.ba.sak.integrasjoner.økonomi.InternKonsistendsavstemming
 
+import no.nav.familie.ba.sak.common.Utils
 import no.nav.familie.ba.sak.common.secureLogger
-import no.nav.familie.ba.sak.common.toYearMonth
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelse
+import no.nav.familie.ba.sak.kjerne.tidslinje.Periode
+import no.nav.familie.ba.sak.kjerne.tidslinje.Tidslinje
+import no.nav.familie.ba.sak.kjerne.tidslinje.komposisjon.kombinerMed
+import no.nav.familie.ba.sak.kjerne.tidslinje.tidslinje
+import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.Måned
+import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.MånedTidspunkt.Companion.tilMånedTidspunkt
+import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.MånedTidspunkt.Companion.tilTidspunkt
+import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.tilYearMonth
 import no.nav.familie.kontrakter.felles.oppdrag.Utbetalingsoppdrag
 import no.nav.familie.kontrakter.felles.oppdrag.Utbetalingsperiode
+import java.math.BigDecimal
 
 fun erForskjellMellomAndelerOgOppdrag(
     andeler: List<AndelTilkjentYtelse>,
     utbetalingsoppdrag: Utbetalingsoppdrag?,
     fagsakId: Long
 ): Boolean {
-    val oppdragsperioder =
+    val utbetalingsperioder =
         utbetalingsoppdrag?.utbetalingsperiode
             ?.filter { it.opphør == null }
             ?: emptyList()
 
-    val forskjellMellomAndeleneOgUtbetalingsoppdraget: AndelOgOppdragForskjell = hentForskjellIAndelerOgUtbetalingsoppdrag(oppdragsperioder, andeler)
+    val forskjellMellomAndeleneOgUtbetalingsoppdraget =
+        hentForskjellIAndelerOgUtbetalingsoppdrag(utbetalingsperioder, andeler)
 
     when (forskjellMellomAndeleneOgUtbetalingsoppdraget) {
-        AndelOgOppdragForskjell.OPPDRAGSPERIODE_UTEN_TILSVARENDE_ANDEL -> secureLogger.info(
-            "Fagsak $fagsakId har sendt utbetalingsperiode(r) til økonomi som ikke har tilsvarende andel tilkjent ytelse" +
+        is OPPDRAGSPERIODER_UTEN_TILSVARENDE_ANDEL -> secureLogger.info(
+            "Fagsak $fagsakId har sendt utbetalingsperiode(r) til økonomi som ikke har tilsvarende andel tilkjent ytelse." +
+                "\nDet er differanse i periodene ${forskjellMellomAndeleneOgUtbetalingsoppdraget.perioderMedForskjell.tilTidStrenger()}." +
                 "\n\nSiste utbetalingsoppdrag som er sendt til familie-øknonomi på fagsaken er:" +
-                "\n $utbetalingsoppdrag"
+                "\n$utbetalingsoppdrag"
         )
-        AndelOgOppdragForskjell.INGEN_FORSKJELL -> Unit
+
+        is INGEN_FORSKJELL -> Unit
     }
 
-    return forskjellMellomAndeleneOgUtbetalingsoppdraget != AndelOgOppdragForskjell.INGEN_FORSKJELL
+    return forskjellMellomAndeleneOgUtbetalingsoppdraget is INGEN_FORSKJELL
 }
 
 private fun hentForskjellIAndelerOgUtbetalingsoppdrag(
-    oppdragsperioder: List<Utbetalingsperiode>,
+    utbetalingsperioder: List<Utbetalingsperiode>,
     andeler: List<AndelTilkjentYtelse>
-) = when {
-    erOppdragsperiodeUtenTilsvarendeAndel(oppdragsperioder, andeler) ->
-        AndelOgOppdragForskjell.OPPDRAGSPERIODE_UTEN_TILSVARENDE_ANDEL
+): AndelOgOppdragForskjell {
+    val erBeløpLiktTidslinje = utbetalingsperioder.tilBeløpstidslinje()
+        .kombinerMed(andeler.tilBeløpstidslinje()) { utbetaling, andel ->
+            utbetaling == andel
+        }
 
-    else -> AndelOgOppdragForskjell.INGEN_FORSKJELL
+    val perioderMedForskjell = erBeløpLiktTidslinje.perioder().filter { it.innhold == false }
+
+    return if (perioderMedForskjell.isEmpty()) {
+        INGEN_FORSKJELL
+    } else OPPDRAGSPERIODER_UTEN_TILSVARENDE_ANDEL(perioderMedForskjell)
 }
 
-private fun erOppdragsperiodeUtenTilsvarendeAndel(
-    oppdragsperioder: List<Utbetalingsperiode>,
-    andeler: List<AndelTilkjentYtelse>
-) = !oppdragsperioder.all { oppdragsperiode ->
-    andeler.any {
-        it.kalkulertUtbetalingsbeløp.toBigDecimal() == oppdragsperiode.sats &&
-            it.stønadFom == oppdragsperiode.vedtakdatoFom.toYearMonth() &&
-            it.stønadTom == oppdragsperiode.vedtakdatoTom.toYearMonth()
+private fun List<Utbetalingsperiode>.tilBeløpstidslinje(): Tidslinje<BigDecimal, Måned> = tidslinje {
+    this.map {
+        Periode(
+            fraOgMed = it.vedtakdatoFom.tilMånedTidspunkt(),
+            tilOgMed = it.vedtakdatoTom.tilMånedTidspunkt(),
+            innhold = it.sats
+        )
     }
 }
 
-enum class AndelOgOppdragForskjell {
-    OPPDRAGSPERIODE_UTEN_TILSVARENDE_ANDEL,
-    INGEN_FORSKJELL
+private fun List<AndelTilkjentYtelse>.tilBeløpstidslinje(): Tidslinje<BigDecimal, Måned> = tidslinje {
+    this.map {
+        Periode(
+            fraOgMed = it.stønadFom.tilTidspunkt(),
+            tilOgMed = it.stønadTom.tilTidspunkt(),
+            innhold = it.kalkulertUtbetalingsbeløp.toBigDecimal()
+        )
+    }
 }
+
+private fun List<Periode<*, Måned>>.tilTidStrenger() =
+    Utils.slåSammen(this.map { "${it.fraOgMed.tilYearMonth()} - ${it.tilOgMed.tilYearMonth()}" })
+
+private sealed interface AndelOgOppdragForskjell
+
+private data class OPPDRAGSPERIODER_UTEN_TILSVARENDE_ANDEL(val perioderMedForskjell: List<Periode<Boolean, Måned>>) :
+    AndelOgOppdragForskjell
+
+private object INGEN_FORSKJELL : AndelOgOppdragForskjell
+
