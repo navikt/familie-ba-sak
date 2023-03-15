b8cdc993832ebf506c187f6173aa2f94db3ea06a
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-03-15 14:43:39 +0100

    Skriv tester

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/vilkårsvurdering/VilkårTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/vilkårsvurdering/VilkårTest.kt
@@ -5,75 +5,142 @@ import no.nav.familie.ba.sak.kjerne.fagsak.FagsakType
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersonType
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.Vilkår
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class VilkårTest {
 
-    @Test
-    fun `Hent relevante vilkår for persontype SØKER`() {
-        val relevanteVilkår = Vilkår.hentVilkårFor(personType = PersonType.SØKER, fagsakType = FagsakType.NORMAL)
-        val vilkårForSøker = setOf(
-            Vilkår.BOSATT_I_RIKET,
-            Vilkår.LOVLIG_OPPHOLD
-        )
-        Assertions.assertEquals(vilkårForSøker, relevanteVilkår)
-    }
-
-    @Test
-    fun `Hent relevante vilkår for persontype BARN`() {
-        val relevanteVilkår = Vilkår.hentVilkårFor(personType = PersonType.BARN, fagsakType = FagsakType.NORMAL)
-        val vilkårForBarn = setOf(
-            Vilkår.UNDER_18_ÅR,
-            Vilkår.BOR_MED_SØKER,
-            Vilkår.GIFT_PARTNERSKAP,
-            Vilkår.BOSATT_I_RIKET,
-            Vilkår.LOVLIG_OPPHOLD
-        )
-        Assertions.assertEquals(vilkårForBarn, relevanteVilkår)
-    }
-
-    @Test
-    fun `Valider gyldige vilkårspermutasjoner for barn og søker`() {
-        Assertions.assertEquals(
-            setOf(
+    @Nested
+    inner class `Hent relevante vilkår for persontype BARN` {
+        @Test
+        fun `For ordinær nasjonal sak`() {
+            val relevanteVilkår = Vilkår.hentVilkårFor(personType = PersonType.BARN, ytelseType = YtelseType.ORDINÆR_BARNETRYGD, fagsakType = FagsakType.NORMAL)
+            val vilkårForBarn = setOf(
                 Vilkår.UNDER_18_ÅR,
                 Vilkår.BOR_MED_SØKER,
                 Vilkår.GIFT_PARTNERSKAP,
                 Vilkår.BOSATT_I_RIKET,
                 Vilkår.LOVLIG_OPPHOLD
-            ),
-            Vilkår.hentVilkårFor(personType = PersonType.BARN, fagsakType = FagsakType.NORMAL)
-        )
+            )
+            Assertions.assertEquals(vilkårForBarn, relevanteVilkår)
+        }
 
-        Assertions.assertEquals(
-            setOf(
-                Vilkår.BOSATT_I_RIKET,
-                Vilkår.LOVLIG_OPPHOLD
-            ),
-            Vilkår.hentVilkårFor(personType = PersonType.SØKER, fagsakType = FagsakType.NORMAL)
-        )
-    }
-
-    @Test
-    fun `Valider gyldige vilkårspermutasjoner for barn og søker ved utvidet barnetrygd`() {
-        Assertions.assertEquals(
-            setOf(
+        @Test
+        fun `For utvidet nasjonal sak`() {
+            val relevanteVilkår = Vilkår.hentVilkårFor(personType = PersonType.BARN, ytelseType = YtelseType.UTVIDET_BARNETRYGD, fagsakType = FagsakType.NORMAL)
+            val vilkårForBarn = setOf(
                 Vilkår.UNDER_18_ÅR,
                 Vilkår.BOR_MED_SØKER,
                 Vilkår.GIFT_PARTNERSKAP,
                 Vilkår.BOSATT_I_RIKET,
                 Vilkår.LOVLIG_OPPHOLD
-            ),
-            Vilkår.hentVilkårFor(personType = PersonType.BARN, ytelseType = YtelseType.UTVIDET_BARNETRYGD, fagsakType = FagsakType.NORMAL)
-        )
+            )
+            Assertions.assertEquals(vilkårForBarn, relevanteVilkår)
+        }
 
-        Assertions.assertEquals(
-            setOf(
+        @Test
+        fun `For ordinær institusjonssak`() {
+            val relevanteVilkår = Vilkår.hentVilkårFor(personType = PersonType.BARN, ytelseType = YtelseType.ORDINÆR_BARNETRYGD, fagsakType = FagsakType.INSTITUSJON)
+            val vilkårForBarn = setOf(
+                Vilkår.UNDER_18_ÅR,
+                Vilkår.BOR_MED_SØKER,
+                Vilkår.GIFT_PARTNERSKAP,
+                Vilkår.BOSATT_I_RIKET,
+                Vilkår.LOVLIG_OPPHOLD
+            )
+            Assertions.assertEquals(vilkårForBarn, relevanteVilkår)
+        }
+
+        @Test
+        fun `For utvidet institusjonssak`() {
+            val relevanteVilkår = Vilkår.hentVilkårFor(personType = PersonType.BARN, ytelseType = YtelseType.UTVIDET_BARNETRYGD, fagsakType = FagsakType.INSTITUSJON)
+            val vilkårForBarn = setOf(
+                Vilkår.UNDER_18_ÅR,
+                Vilkår.BOR_MED_SØKER,
+                Vilkår.GIFT_PARTNERSKAP,
+                Vilkår.BOSATT_I_RIKET,
+                Vilkår.LOVLIG_OPPHOLD
+            )
+            Assertions.assertEquals(vilkårForBarn, relevanteVilkår)
+        }
+
+        @Test
+        fun `For ordinær enslig mindreårig sak`() {
+            val relevanteVilkår = Vilkår.hentVilkårFor(personType = PersonType.BARN, ytelseType = YtelseType.ORDINÆR_BARNETRYGD, fagsakType = FagsakType.BARN_ENSLIG_MINDREÅRIG)
+            val vilkårForBarn = setOf(
+                Vilkår.UNDER_18_ÅR,
+                Vilkår.BOR_MED_SØKER,
+                Vilkår.GIFT_PARTNERSKAP,
+                Vilkår.BOSATT_I_RIKET,
+                Vilkår.LOVLIG_OPPHOLD
+            )
+            Assertions.assertEquals(vilkårForBarn, relevanteVilkår)
+        }
+
+        @Test
+        fun `For utvidet enslig mindreårig sak`() {
+            val relevanteVilkår = Vilkår.hentVilkårFor(personType = PersonType.BARN, ytelseType = YtelseType.UTVIDET_BARNETRYGD, fagsakType = FagsakType.BARN_ENSLIG_MINDREÅRIG)
+            val vilkårForBarn = setOf(
+                Vilkår.UNDER_18_ÅR,
+                Vilkår.BOR_MED_SØKER,
+                Vilkår.GIFT_PARTNERSKAP,
                 Vilkår.BOSATT_I_RIKET,
                 Vilkår.LOVLIG_OPPHOLD,
                 Vilkår.UTVIDET_BARNETRYGD
-            ),
-            Vilkår.hentVilkårFor(personType = PersonType.SØKER, ytelseType = YtelseType.UTVIDET_BARNETRYGD, fagsakType = FagsakType.NORMAL)
-        )
+            )
+            Assertions.assertEquals(vilkårForBarn, relevanteVilkår)
+        }
+    }
+
+    @Nested
+    inner class `Hent relevante vilkår for persontype SØKER` {
+        @Test
+        fun `For ordinær nasjonal sak`() {
+            val relevanteVilkår = Vilkår.hentVilkårFor(personType = PersonType.SØKER, ytelseType = YtelseType.ORDINÆR_BARNETRYGD, fagsakType = FagsakType.NORMAL)
+            val vilkårForBarn = setOf(
+                Vilkår.BOSATT_I_RIKET,
+                Vilkår.LOVLIG_OPPHOLD
+            )
+            Assertions.assertEquals(vilkårForBarn, relevanteVilkår)
+        }
+
+        @Test
+        fun `For utvidet nasjonal sak`() {
+            val relevanteVilkår = Vilkår.hentVilkårFor(personType = PersonType.SØKER, ytelseType = YtelseType.UTVIDET_BARNETRYGD, fagsakType = FagsakType.NORMAL)
+            val vilkårForBarn = setOf(
+                Vilkår.BOSATT_I_RIKET,
+                Vilkår.LOVLIG_OPPHOLD,
+                Vilkår.UTVIDET_BARNETRYGD
+            )
+            Assertions.assertEquals(vilkårForBarn, relevanteVilkår)
+        }
+
+        @Test
+        fun `For ordinær institusjonssak`() {
+            val relevanteVilkår = Vilkår.hentVilkårFor(personType = PersonType.SØKER, ytelseType = YtelseType.ORDINÆR_BARNETRYGD, fagsakType = FagsakType.INSTITUSJON)
+            val vilkårForBarn = emptySet<Vilkår>()
+            Assertions.assertEquals(vilkårForBarn, relevanteVilkår)
+        }
+
+        @Test
+        fun `For utvidet institusjonssak`() {
+            val relevanteVilkår = Vilkår.hentVilkårFor(personType = PersonType.SØKER, ytelseType = YtelseType.UTVIDET_BARNETRYGD, fagsakType = FagsakType.INSTITUSJON)
+            val vilkårForBarn = emptySet<Vilkår>()
+            Assertions.assertEquals(vilkårForBarn, relevanteVilkår)
+        }
+
+        @Test
+        fun `For ordinær enslig mindreårig sak`() {
+            val relevanteVilkår = Vilkår.hentVilkårFor(personType = PersonType.SØKER, ytelseType = YtelseType.ORDINÆR_BARNETRYGD, fagsakType = FagsakType.BARN_ENSLIG_MINDREÅRIG)
+            val vilkårForBarn = emptySet<Vilkår>()
+            Assertions.assertEquals(vilkårForBarn, relevanteVilkår)
+        }
+
+        @Test
+        fun `For utvidet enslig mindreårig sak`() {
+            val relevanteVilkår = Vilkår.hentVilkårFor(personType = PersonType.SØKER, ytelseType = YtelseType.UTVIDET_BARNETRYGD, fagsakType = FagsakType.BARN_ENSLIG_MINDREÅRIG)
+            val vilkårForBarn = emptySet<Vilkår>()
+            Assertions.assertEquals(vilkårForBarn, relevanteVilkår)
+        }
     }
 }
