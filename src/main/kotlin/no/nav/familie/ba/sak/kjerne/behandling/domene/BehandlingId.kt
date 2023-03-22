de90e7b2dec233976a5d985e11c92212e163e69f
Kristine Steine <kristine.steine@bekk.no>
2023-03-22 14:49:14 +0100

    Tilby konvertering mellom BehandlingId og databaseverdi

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandling/domene/BehandlingId.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandling/domene/BehandlingId.kt
@@ -1,5 +1,19 @@
 package no.nav.familie.ba.sak.kjerne.behandling.domene
 
+import javax.persistence.AttributeConverter
+import javax.persistence.Converter
+
 data class BehandlingId(
     val id: Long
 )
+
+@Converter
+class BehandlingIdConverter :
+    AttributeConverter<BehandlingId, Long> {
+
+    override fun convertToDatabaseColumn(behandlingId: BehandlingId) =
+        behandlingId.id
+
+    override fun convertToEntityAttribute(id: Long): BehandlingId =
+        BehandlingId(id)
+}
