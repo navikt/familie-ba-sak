c13d32e4e5fce4897ef15b76a16eda0537de612b
Kristine Steine <kristine.steine@bekk.no>
2023-03-29 21:01:31 +0200

    Bruk BehandlingId i VedtakPeriodeValidering.kt

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/VedtakPeriodeValidering.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/VedtakPeriodeValidering.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode
 
 import no.nav.familie.ba.sak.common.tilKortString
+import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingId
 import no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser.VedtakBegrunnelseType
 import no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.domene.UtvidetVedtaksperiodeMedBegrunnelser
 import org.slf4j.Logger
@@ -8,7 +9,7 @@ import org.slf4j.LoggerFactory
 
 // Håpet er at denne skal kaste feil på sikt, men enn så lenge blir det for strengt. Logger for å se behovet.
 fun List<UtvidetVedtaksperiodeMedBegrunnelser>.validerPerioderInneholderBegrunnelser(
-    behandlingId: Long,
+    behandlingId: BehandlingId,
     fagsakId: Long
 ) {
     this.forEach {
@@ -23,7 +24,7 @@ fun List<UtvidetVedtaksperiodeMedBegrunnelser>.validerPerioderInneholderBegrunne
 }
 
 private fun UtvidetVedtaksperiodeMedBegrunnelser.validerMinstEnEndretUtbetalingbegrunnelseVedEndretUtbetaling(
-    behandlingId: Long,
+    behandlingId: BehandlingId,
     fagsakId: Long
 ) {
     val erMuligÅVelgeEndretUtbetalingBegrunnelse =
@@ -32,12 +33,12 @@ private fun UtvidetVedtaksperiodeMedBegrunnelser.validerMinstEnEndretUtbetalingb
         this.begrunnelser.any { it.standardbegrunnelse.vedtakBegrunnelseType == VedtakBegrunnelseType.ENDRET_UTBETALING }
 
     if (erMuligÅVelgeEndretUtbetalingBegrunnelse && !erValgtEndretUtbetalingBegrunnelse) {
-        logger.warn("Vedtaksperioden ${this.fom?.tilKortString() ?: ""} - ${this.tom?.tilKortString() ?: ""} mangler endretubetalingsbegrunnelse. Fagsak: $fagsakId, behandling: $behandlingId")
+        logger.warn("Vedtaksperioden ${this.fom?.tilKortString() ?: ""} - ${this.tom?.tilKortString() ?: ""} mangler endretubetalingsbegrunnelse. Fagsak: $fagsakId, behandling: ${behandlingId.id}")
     }
 }
 
 private fun UtvidetVedtaksperiodeMedBegrunnelser.validerMinstEnInnvilgetbegrunnelseVedInnvilgelse(
-    behandlingId: Long,
+    behandlingId: BehandlingId,
     fagsakId: Long
 ) {
     val erMuligÅVelgeInnvilgetBegrunnelse =
@@ -46,12 +47,12 @@ private fun UtvidetVedtaksperiodeMedBegrunnelser.validerMinstEnInnvilgetbegrunne
         this.begrunnelser.any { it.standardbegrunnelse.vedtakBegrunnelseType.erInnvilget() }
 
     if (erMuligÅVelgeInnvilgetBegrunnelse && !erValgtInnvilgetBegrunnelse) {
-        logger.warn("Vedtaksperioden ${this.fom?.tilKortString() ?: ""} - ${this.tom?.tilKortString() ?: ""} mangler innvilgelsebegrunnelse. Fagsak: $fagsakId, behandling: $behandlingId")
+        logger.warn("Vedtaksperioden ${this.fom?.tilKortString() ?: ""} - ${this.tom?.tilKortString() ?: ""} mangler innvilgelsebegrunnelse. Fagsak: $fagsakId, behandling: ${behandlingId.id}")
     }
 }
 
 private fun UtvidetVedtaksperiodeMedBegrunnelser.validerMinstEnReduksjonsbegrunnelseVedReduksjon(
-    behandlingId: Long,
+    behandlingId: BehandlingId,
     fagsakId: Long
 ) {
     val erMuligÅVelgeReduksjonBegrunnelse =
@@ -60,16 +61,16 @@ private fun UtvidetVedtaksperiodeMedBegrunnelser.validerMinstEnReduksjonsbegrunn
         this.begrunnelser.any { it.standardbegrunnelse.vedtakBegrunnelseType.erReduksjon() }
 
     if (erMuligÅVelgeReduksjonBegrunnelse && !erValgtReduksjonBegrunnelse) {
-        logger.warn("Vedtaksperioden ${this.fom?.tilKortString() ?: ""} - ${this.tom?.tilKortString() ?: ""} mangler reduksjonsbegrunnelse. Fagsak: $fagsakId, behandling: $behandlingId")
+        logger.warn("Vedtaksperioden ${this.fom?.tilKortString() ?: ""} - ${this.tom?.tilKortString() ?: ""} mangler reduksjonsbegrunnelse. Fagsak: $fagsakId, behandling: ${behandlingId.id}")
     }
 }
 
 private fun UtvidetVedtaksperiodeMedBegrunnelser.validerMinstEnBegrunnelseValgt(
-    behandlingId: Long,
+    behandlingId: BehandlingId,
     fagsakId: Long
 ) {
     if (this.begrunnelser.isEmpty()) {
-        logger.warn("Vedtaksperioden ${this.fom?.tilKortString() ?: ""} - ${this.tom?.tilKortString() ?: ""} har ingen begrunnelser knyttet til seg. Fagsak: $fagsakId, behandling: $behandlingId")
+        logger.warn("Vedtaksperioden ${this.fom?.tilKortString() ?: ""} - ${this.tom?.tilKortString() ?: ""} har ingen begrunnelser knyttet til seg. Fagsak: $fagsakId, behandling: ${behandlingId.id}")
     }
 }
 
