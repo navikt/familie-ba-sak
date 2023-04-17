3a3f52b7661968a2daae0ff88eba35e9b770fe6d
charliemidtlyng <charlie.midtlyng@bekk.no>
2023-04-17 13:42:01 +0200

    Skal ikke feile dersom man ikke finner noen tester å kjøre. Den finner ingen tester når den prøver å kjøre verdikjedetestene - og det er jo meningen, men vi får ikke excludet cucumber-suiten i de tilfellene

--- src/test/integrasjonstester/kotlin/RunCucumberTest.kt
+++ src/test/integrasjonstester/kotlin/RunCucumberTest.kt
@@ -4,7 +4,7 @@ import org.junit.platform.suite.api.IncludeEngines
 import org.junit.platform.suite.api.SelectClasspathResource
 import org.junit.platform.suite.api.Suite
 
-@Suite
+@Suite(failIfNoTests = false)
 @IncludeEngines("cucumber")
 @SelectClasspathResource("no/nav/familie/ba/sak/cucumber")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
