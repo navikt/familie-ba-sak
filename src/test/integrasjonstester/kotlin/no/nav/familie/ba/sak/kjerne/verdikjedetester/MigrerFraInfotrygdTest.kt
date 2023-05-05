2ac3ea42a76b83f7c149254f80127520eda3fd6a
Stig Helge Strøm <stig.helge.strom@nav.no>
2023-05-05 16:02:04 +0200

    assert på fagsaktype for EM og Inst

--- src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/kjerne/verdikjedetester/MigrerFraInfotrygdTest.kt
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/kjerne/verdikjedetester/MigrerFraInfotrygdTest.kt
@@ -968,6 +968,7 @@ class MigrerFraInfotrygdTest(
                 assertThat(behandling.fagsak.institusjon).isNull()
             }
         }
+        assertThat(behandling.fagsak.type).isEqualTo(fagsakType)
 
         generellAssertFagsak(
             restFagsak = restFagsakEtterBehandlingAvsluttet,
