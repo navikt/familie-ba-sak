7bd38ee84a280454cec8d8828141adb74a85e5c2
halvormundal <halvorbmundal@gmail.com>
2023-05-09 10:18:54 +0200

    Fjern kode som ikke var i bruk

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/domene/UtvidetVedtaksperiodeMedBegrunnelser.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/domene/UtvidetVedtaksperiodeMedBegrunnelser.kt
@@ -1,9 +1,5 @@
 package no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.domene
 
-import no.nav.familie.ba.sak.common.MånedPeriode
-import no.nav.familie.ba.sak.common.TIDENES_ENDE
-import no.nav.familie.ba.sak.common.TIDENES_MORGEN
-import no.nav.familie.ba.sak.common.toYearMonth
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelseMedEndreteUtbetalinger
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersonopplysningGrunnlag
 import no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser.IVedtakBegrunnelse
@@ -25,12 +21,7 @@ data class UtvidetVedtaksperiodeMedBegrunnelser(
     val fritekster: List<String> = emptyList(),
     val gyldigeBegrunnelser: List<IVedtakBegrunnelse> = emptyList(),
     val utbetalingsperiodeDetaljer: List<UtbetalingsperiodeDetalj> = emptyList()
-) {
-    fun hentMånedPeriode() = MånedPeriode(
-        (this.fom ?: TIDENES_MORGEN).toYearMonth(),
-        (this.tom ?: TIDENES_ENDE).toYearMonth()
-    )
-}
+)
 
 fun List<UtvidetVedtaksperiodeMedBegrunnelser>.sorter(): List<UtvidetVedtaksperiodeMedBegrunnelser> {
     val (perioderMedFom, perioderUtenFom) = this.partition { it.fom != null }
