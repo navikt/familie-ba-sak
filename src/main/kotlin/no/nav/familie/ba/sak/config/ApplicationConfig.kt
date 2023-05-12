acce67d5bfe249a60b4679067c245dc7db396adc
Johan Blomgren <blommish@users.noreply.github.com>
2023-05-12 09:25:52 +0200

    Legger til accesslog då vi ikke får ut noe når service discovery blir… (#3603)

--- src/main/kotlin/no/nav/familie/ba/sak/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/ApplicationConfig.kt
@@ -3,6 +3,7 @@ package no.nav.familie.ba.sak.config
 import com.fasterxml.jackson.databind.ObjectMapper
 import no.nav.familie.http.client.RetryOAuth2HttpClient
 import no.nav.familie.log.filter.LogFilter
+import no.nav.familie.log.filter.RequestTimeFilter
 import no.nav.security.token.support.client.core.http.OAuth2HttpClient
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenResponse
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
@@ -37,6 +38,15 @@ class ApplicationConfig {
         val filterRegistration: FilterRegistrationBean<LogFilter> = FilterRegistrationBean()
         filterRegistration.filter = LogFilter()
         filterRegistration.order = 1
+        return filterRegistration
+    }
+
+    @Bean
+    fun requestTimeFilter(): FilterRegistrationBean<RequestTimeFilter> {
+        log.info("Registering RequestTimeFilter")
+        val filterRegistration = FilterRegistrationBean<RequestTimeFilter>()
+        filterRegistration.filter = RequestTimeFilter()
+        filterRegistration.order = 2
         return filterRegistration
     }
 
