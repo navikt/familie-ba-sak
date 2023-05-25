a6b5e283b869156abd9f31083beec8664af15198
Johan Blomgren <blommish@users.noreply.github.com>
2023-05-25 13:52:47 +0200

    Legger til kolonne i eget script

--- src/main/resources/db/migration/V243__flere_aktive_behandlinger.sql
+++ src/main/resources/db/migration/V243__flere_aktive_behandlinger.sql
@@ -1,10 +1,5 @@
-ALTER TABLE behandling
-    ADD COLUMN aktivert_tid TIMESTAMP(3);
-
-UPDATE behandling
-SET aktivert_tid = opprettet_tid;
-ALTER TABLE behandling
-    ALTER COLUMN aktivert_tid SET NOT NULL;
+UPDATE behandling SET aktivert_tid = opprettet_tid;
+ALTER TABLE behandling ALTER COLUMN aktivert_tid DROP DEFAULT;
 
 -- Kun en behandling kan ha status annet enn AVSLUTTET eller SATT_PÅ_MASKINELL_VENT
 CREATE UNIQUE INDEX UIDX_BEHANDLING_02 ON behandling (fk_fagsak_id) WHERE (status <> 'AVSLUTTET' AND status <> 'SATT_PÅ_MASKINELL_VENT');
