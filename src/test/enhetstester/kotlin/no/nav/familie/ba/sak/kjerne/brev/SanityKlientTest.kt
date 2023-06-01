96590d4cdc064ef1b8a38c7bea176cb4c1375aa9
Johan Blomgren <blommish@users.noreply.github.com>
2023-06-01 14:59:08 +0200

    Endrer SanityService til å returnere en map av begrunnelser på enums (#3662)
    * SanityService returnerer en map på enum og begrunnelse for å unngå 500k iterasjoner/oppslag av apiNavn
    
    * Flytter cached til service då den mapper om enums og verdier
    
    * Endrer triggesAv til å være en del av sanityBegrunnelse for å unngå initiering i loops på et fast objekt
    
    * Endrer lister som itereres ofte over til set

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/brev/SanityKlientTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/brev/SanityKlientTest.kt
@@ -1,18 +1,24 @@
 package no.nav.familie.ba.sak.kjerne.brev
 
-import no.nav.familie.ba.sak.config.TestSanityKlient
-import org.junit.jupiter.api.Assertions
+import no.nav.familie.ba.sak.config.testSanityKlient
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-class SanityKlientTest() {
+class SanityKlientTest {
 
     @Test
     fun `Skal teste at vi klarer å hente begrunnelser fra sanity-apiet`() {
-        Assertions.assertTrue(TestSanityKlient.hentBegrunnelser().isNotEmpty())
+        val hentBegrunnelser = testSanityKlient.hentBegrunnelser()
+        val begrunnelserPåApiNavn = hentBegrunnelser.associateBy { it.apiNavn }
+        assertThat(hentBegrunnelser).hasSize(begrunnelserPåApiNavn.size)
+        assertThat(hentBegrunnelser).isNotEmpty
     }
 
     @Test
     fun `Skal teste at vi klarer å hente eøs-begrunnelser fra sanity-apiet`() {
-        Assertions.assertTrue(TestSanityKlient.hentEØSBegrunnelser().isNotEmpty())
+        val hentEØSBegrunnelser = testSanityKlient.hentEØSBegrunnelser()
+        val begrunnelserPåApiNavn = hentEØSBegrunnelser.associateBy { it.apiNavn }
+        assertThat(hentEØSBegrunnelser).hasSize(begrunnelserPåApiNavn.size)
+        assertThat(hentEØSBegrunnelser).isNotEmpty
     }
 }
