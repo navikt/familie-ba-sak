5b748d8a8d980fb7081e827ef39ddd0b5f6a5b64
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2023-06-13 13:51:18 +0200

    legg til valgbarhet i RestSanityBegrunnelse (#3703)
    endre query til å returnere alle verdier og bruk valgbarhet for å styre
    fomTekst i autoBrev
    
    ### 💰 Hva skal gjøres, og hvorfor?
    
    https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-12362
    bruker feil tittel-tekst ved autobrev til tider. Ønsker å hele bruke
    valgbarhet-feltet i sanity enn å gå utifra at det er autobrec dersom vi
    har reduksjon_18_år eller reduksjon_6_år.
    
    ### 🔎️ Er det noe spesielt du ønsker tilbakemelding om?
    Blir det riktig å gå utifra at man kan bruke valgbarhet og kunne man
    bare fjernet `?: "Du får:"`
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [ ] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei
    
    ---------
    
    Signed-off-by: fredrikpf <fredrik.pfeil@gmail.com>

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/brev/domene/SanityBegrunnelse.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/brev/domene/SanityBegrunnelse.kt
@@ -37,6 +37,7 @@ data class SanityBegrunnelse(
     val endretUtbetalingsperiodeDeltBostedUtbetalingTrigger: EndretUtbetalingsperiodeDeltBostedTriggere? = null,
     val endretUtbetalingsperiodeTriggere: List<EndretUtbetalingsperiodeTrigger>? = null,
     val utvidetBarnetrygdTriggere: List<UtvidetBarnetrygdTrigger>? = null,
+    val valgbarhet: Valgbarhet? = null,
 ) : ISanityBegrunnelse {
 
     val triggesAv: TriggesAv by lazy { this.tilTriggesAv() }
@@ -58,6 +59,7 @@ data class RestSanityBegrunnelse(
     val endretUtbetalingsperiodeDeltBostedUtbetalingTrigger: String?,
     val endretUtbetalingsperiodeTriggere: List<String>? = emptyList(),
     val utvidetBarnetrygdTriggere: List<String>? = emptyList(),
+    val valgbarhet: String? = null,
 ) {
     fun tilSanityBegrunnelse(): SanityBegrunnelse? {
         if (apiNavn == null) return null
@@ -107,6 +109,7 @@ data class RestSanityBegrunnelse(
             utvidetBarnetrygdTriggere = utvidetBarnetrygdTriggere?.mapNotNull {
                 finnEnumverdi(it, UtvidetBarnetrygdTrigger.values(), apiNavn)
             },
+            valgbarhet = valgbarhet?.let { finnEnumverdi(valgbarhet, Valgbarhet.values(), apiNavn) },
         )
     }
 }
@@ -185,6 +188,13 @@ enum class UtvidetBarnetrygdTrigger {
     SMÅBARNSTILLEGG,
 }
 
+enum class Valgbarhet {
+    STANDARD,
+    AUTOMATISK,
+    TILLEGSTEKST,
+    SAKSPESIFIKK,
+}
+
 private fun SanityBegrunnelse.tilTriggesAv(): TriggesAv {
     return TriggesAv(
         vilkår = this.vilkaar?.map { it.tilVilkår() }?.toSet() ?: emptySet(),
@@ -213,6 +223,7 @@ private fun SanityBegrunnelse.tilTriggesAv(): TriggesAv {
         deltbosted = this.inneholderBorMedSøkerTrigger(VilkårTrigger.DELT_BOSTED),
         deltBostedSkalIkkeDeles = this.inneholderBorMedSøkerTrigger(VilkårTrigger.DELT_BOSTED_SKAL_IKKE_DELES),
         valgbar = !this.inneholderØvrigTrigger(ØvrigTrigger.ALLTID_AUTOMATISK),
+        valgbarhet = this.valgbarhet,
         etterEndretUtbetaling = this.endretUtbetalingsperiodeTriggere
             ?.contains(EndretUtbetalingsperiodeTrigger.ETTER_ENDRET_UTBETALINGSPERIODE) ?: false,
         endretUtbetalingSkalUtbetales = this.endretUtbetalingsperiodeDeltBostedUtbetalingTrigger
