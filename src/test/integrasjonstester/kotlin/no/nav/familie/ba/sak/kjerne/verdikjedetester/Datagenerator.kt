226686741ace417e0ee6c9bef7599596a570bd8e
Stig Strøm <stig.helge.strom@nav.no>
2023-06-19 08:18:36 +0200

    Sletter migreringservice, controller og tester (#3713)

--- src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/kjerne/verdikjedetester/Datagenerator.kt
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/kjerne/verdikjedetester/Datagenerator.kt
@@ -90,39 +90,6 @@ fun lagInfotrygdSak(beløp: Double, identBarn: List<String>, valg: String? = "OR
     )
 }
 
-fun lagInfotrygdSakMedSmåbarnstillegg(
-    beløp: Double,
-    identBarn: List<String>,
-): Sak {
-    return Sak(
-        stønad = Stønad(
-            barn = identBarn.map { Barn(it, barnetrygdTom = "000000") },
-            delytelse = listOf(
-                Delytelse(
-                    fom = LocalDate.now(),
-                    tom = null,
-                    beløp = beløp,
-                    typeDelytelse = "MS",
-                    typeUtbetaling = "M",
-                ),
-                Delytelse(
-                    fom = LocalDate.now(),
-                    tom = null,
-                    beløp = 660.0,
-                    typeDelytelse = "SM",
-                    typeUtbetaling = "M",
-                ),
-            ),
-            opphørsgrunn = "0",
-            antallBarn = identBarn.size,
-            virkningFom = "797790",
-        ),
-        status = "FB",
-        valg = "UT",
-        undervalg = "EF",
-    )
-}
-
 fun fullførBehandlingFraVilkårsvurderingAlleVilkårOppfylt(
     restUtvidetBehandling: RestUtvidetBehandling,
     personScenario: RestScenario,
