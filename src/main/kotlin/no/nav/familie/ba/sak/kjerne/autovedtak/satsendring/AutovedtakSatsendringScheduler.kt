182a4105530e09ee492364489421036b3af4a935
Stig Strøm <stig.helge.strom@nav.no>
2023-06-21 17:30:11 +0200

    Finjusterer antall satsendringer utenfor arbeidstid (#3743)

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/autovedtak/satsendring/AutovedtakSatsendringScheduler.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/autovedtak/satsendring/AutovedtakSatsendringScheduler.kt
@@ -11,24 +11,24 @@ class AutovedtakSatsendringScheduler(
 ) {
     @Scheduled(cron = CRON_HVERT_10_MIN_UKEDAG)
     fun triggSatsendringJuli2023() {
-        startSatsendring()
+        startSatsendring(1200)
     }
 
     @Scheduled(cron = CRON_HVERT_5_MIN_UKEDAG_UTENFOR_ARBEIDSTID)
     fun triggSatsendringJuli2023UtenforArbeidstid() {
-        startSatsendring()
+        startSatsendring(1000)
     }
 
-    @Scheduled(cron = CRON_HVERT_10_MIN_LØRDAG)
+    @Scheduled(cron = CRON_HVERT_5_MIN_LØRDAG)
     fun triggSatsendringJuli2023Lørdag() {
-        startSatsendring()
+        startSatsendring(1000)
     }
 
-    private fun startSatsendring() {
+    private fun startSatsendring(antallFagsaker: Int) {
         if (LeaderClient.isLeader() == true) {
             logger.info("Starter schedulert jobb for satsendring juli 2023")
             startSatsendring.startSatsendring(
-                antallFagsaker = 1200,
+                antallFagsaker = antallFagsaker,
             )
         }
     }
@@ -37,6 +37,6 @@ class AutovedtakSatsendringScheduler(
         val logger = LoggerFactory.getLogger(AutovedtakSatsendringScheduler::class.java)
         const val CRON_HVERT_10_MIN_UKEDAG = "0 */10 6-15 * * MON-FRI"
         const val CRON_HVERT_5_MIN_UKEDAG_UTENFOR_ARBEIDSTID = "0 */5 16-20 * * MON-FRI"
-        const val CRON_HVERT_10_MIN_LØRDAG = "0 */5 7-17 * * SAT"
+        const val CRON_HVERT_5_MIN_LØRDAG = "0 */5 7-17 * * SAT"
     }
 }
