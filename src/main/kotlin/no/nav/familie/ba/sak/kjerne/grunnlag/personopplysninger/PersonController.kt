82e85763ed32acd3d21c2bb2c3decc039570f452
marius-nav <Marius.Glad.Fredriksen@nav.no>
2023-06-23 11:43:32 +0200

    NAV-11431 - Fjernet personidenterMedStrengtFortroligGradering endepunkt (som ikke lenger et tiltenkt bruk fra front-end)

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/grunnlag/personopplysninger/PersonController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/grunnlag/personopplysninger/PersonController.kt
@@ -17,7 +17,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
@@ -46,15 +45,6 @@ class PersonController(
             ?: personopplysningerService.hentPersoninfoMedRelasjonerOgRegisterinformasjon(aktør)
                 .tilRestPersonInfo(personIdent)
         return ResponseEntity.ok(Ressurs.success(personinfo))
-    }
-
-    @PostMapping(path = ["/personidenterMedStrengtFortroligGradering"])
-    fun returnerPersonIdenterMedStrengtFortroligGradering(
-        @RequestBody personIdenter: List<String>,
-    ): ResponseEntity<Ressurs<List<String>>> {
-        val personIdenterMedStrengtFortroligGradering =
-            familieIntegrasjonerTilgangskontrollService.returnerPersonerMedAdressebeskyttelse(personIdenter)
-        return ResponseEntity.ok(Ressurs.success(personIdenterMedStrengtFortroligGradering))
     }
 
     @GetMapping(path = ["/enkel"])
