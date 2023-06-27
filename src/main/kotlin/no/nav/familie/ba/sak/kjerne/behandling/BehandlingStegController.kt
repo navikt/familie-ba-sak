27c3cf2e9de69f31cb5bb82fffff648bf54a26d5
marius-nav <Marius.Glad.Fredriksen@nav.no>
2023-06-27 14:26:30 +0200

    NAV-11431 Fjern utkommentert kode

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandling/BehandlingStegController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandling/BehandlingStegController.kt
@@ -201,20 +201,6 @@ class BehandlingStegController(
         }
     }
 
-    /* private fun validerAtBehandlingIkkeInneholderStrengtFortroligePersonerMedManuelleBrevmottakere(behandlingId: Long) {
-        val brevmottakere = brevmottakerService.hentBrevmottakere(behandlingId)
-        val personopplysningGrunnlag = persongrunnlagService.hentAktiv(behandlingId = behandlingId)
-        val personer = personopplysningGrunnlag?.søkerOgBarn
-        val personIdentList = personer?.map { it.aktør.aktivFødselsnummer() } ?: emptyList()
-        val strengtFortroligAdresseBeskyttelseIdentList = if (personIdentList.isNotEmpty()) { familieIntegrasjonerTilgangskontrollService.returnerPersonerMedAdressebeskyttelse(personIdentList) } else { emptyList() }
-        if (brevmottakere.isNotEmpty() && strengtFortroligAdresseBeskyttelseIdentList.isNotEmpty()) {
-            val kommaSeparertListeAvStrengtFortroligIdenter = strengtFortroligAdresseBeskyttelseIdentList.joinToString { it }
-            val melding = "Behandlingen (id: " + behandlingId + ") inneholder " + strengtFortroligAdresseBeskyttelseIdentList.size + " person(er) med strengt fortrolig adressebeskyttelse og kan ikke kombineres med manuelle brevmottakere (" + brevmottakere.size + " stk)."
-            val frontendFeilmelding = "Behandlingen inneholder personer med strengt fortrolig adressebeskyttelse (" + kommaSeparertListeAvStrengtFortroligIdenter + ") og kan ikke kombineres med manuelle brevmottakere."
-            throw FunksjonellFeil(melding, frontendFeilmelding)
-        }
-    } */
-
     @PostMapping(path = ["registrer-institusjon-og-verge"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun registerInstitusjonOgVerge(
         @PathVariable behandlingId: Long,
