aa467171962e5f2633bc0da1a990bfaba4a80595
marius-nav <Marius.Glad.Fredriksen@nav.no>
2023-06-28 07:28:45 +0200

    NAV-11431  exit-early i valideringfuksjonen

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandling/ValiderBrevmottakerService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandling/ValiderBrevmottakerService.kt
@@ -13,15 +13,18 @@ class ValiderBrevmottakerService(
     private val familieIntegrasjonerTilgangskontrollService: FamilieIntegrasjonerTilgangskontrollService,
 ) {
     fun validerAtBehandlingIkkeInneholderStrengtFortroligePersonerMedManuelleBrevmottakere(behandlingId: Long) {
-        val brevmottakere = brevmottakerService.hentBrevmottakere(behandlingId)
-        val personopplysningGrunnlag = persongrunnlagService.hentAktiv(behandlingId = behandlingId)
-        val personer = personopplysningGrunnlag?.søkerOgBarn
-        val personIdentList = personer?.map { it.aktør.aktivFødselsnummer() } ?: emptyList()
-        val strengtFortroligAdresseBeskyttelseIdentList = if (personIdentList.isNotEmpty()) { familieIntegrasjonerTilgangskontrollService.returnerPersonerMedAdressebeskyttelse(personIdentList) } else { emptyList() }
-        if (brevmottakere.isNotEmpty() && strengtFortroligAdresseBeskyttelseIdentList.isNotEmpty()) {
-            val kommaSeparertListeAvStrengtFortroligIdenter = strengtFortroligAdresseBeskyttelseIdentList.joinToString { it }
-            val melding = "Behandlingen (id: $behandlingId) inneholder ${strengtFortroligAdresseBeskyttelseIdentList.size} person(er) med strengt fortrolig adressebeskyttelse og kan ikke kombineres med manuelle brevmottakere (${brevmottakere.size} stk)."
-            val frontendFeilmelding = "Behandlingen inneholder personer med strengt fortrolig adressebeskyttelse ($kommaSeparertListeAvStrengtFortroligIdenter) og kan ikke kombineres med manuelle brevmottakere."
+        val brevmottakere = brevmottakerService.hentBrevmottakere(behandlingId).takeIf { it.isNotEmpty() } ?: return
+        val personopplysningGrunnlag = persongrunnlagService.hentAktivThrows(behandlingId = behandlingId)
+        val personIdentList = personopplysningGrunnlag.søkerOgBarn
+            .takeIf { it.isNotEmpty() }
+            ?.map { it.aktør.aktivFødselsnummer() }
+            ?: return
+        val strengtFortroligePersoner =
+            familieIntegrasjonerTilgangskontrollService.returnerPersonerMedAdressebeskyttelse(personIdentList)
+        if (strengtFortroligePersoner.isNotEmpty()) {
+            val melding = "Behandlingen (id: $behandlingId) inneholder ${strengtFortroligePersoner.size} person(er) med strengt fortrolig adressebeskyttelse og kan ikke kombineres med manuelle brevmottakere (${brevmottakere.size} stk)."
+            val frontendFeilmelding =
+                "Behandlingen inneholder personer med strengt fortrolig adressebeskyttelse og kan ikke kombineres med manuelle brevmottakere."
             throw FunksjonellFeil(melding, frontendFeilmelding)
         }
     }
