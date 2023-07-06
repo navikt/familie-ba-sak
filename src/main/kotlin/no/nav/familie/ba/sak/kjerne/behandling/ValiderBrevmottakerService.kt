87465219929ea656941483fed498e58630ab2e36
marius-nav <126786453+marius-nav@users.noreply.github.com>
2023-07-06 10:11:49 +0200

    Nav 11431 fortrolig barn uten manuelle brevmottakere (#3696)
    ### 💰 Hva skal gjøres, og hvorfor?
    _Skriv 1 eller 2 setninger om hvilken funksjonell endring som blir
    implementert. Ta med en **lenke til Favro** og
    skriv en kort beskrivelse av hvorfor endringen skal gjøres._
    
    
    https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-11431
    
    Nytt endepunkt for validering av fortrolig adressebeskyttelse
    (STRENGT_FORTROLIG / STRENGT_FORTROLIG_UTLAND)
    Mottar array av personidenter og returnerer de som PDL anser å ha
    fortrolig adressebeskyttelse)
    
    * Validering i backend for at behandlinger med personer med strengt
    fortrolig adressebeskyttelse ikke skal kunne ha manuelle brevmottakere i
    siste steg “send-til-beslutter”
    * Samme validering ved "Send brev" i DokumentController (ref Eivind)
    
    
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [X] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [x] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei
    
    ---------
    
    Co-authored-by: Tor Nærland <tor.nerland@nav.no>
    Co-authored-by: Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>
    Co-authored-by: tnarland <47184872+tnarland@users.noreply.github.com>

---
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandling/ValiderBrevmottakerService.kt
@@ -0,0 +1,31 @@
+package no.nav.familie.ba.sak.kjerne.behandling
+
+import no.nav.familie.ba.sak.common.FunksjonellFeil
+import no.nav.familie.ba.sak.integrasjoner.familieintegrasjoner.FamilieIntegrasjonerTilgangskontrollService
+import no.nav.familie.ba.sak.kjerne.brev.mottaker.BrevmottakerService
+import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersongrunnlagService
+import org.springframework.stereotype.Service
+
+@Service
+class ValiderBrevmottakerService(
+    private val brevmottakerService: BrevmottakerService,
+    private val persongrunnlagService: PersongrunnlagService,
+    private val familieIntegrasjonerTilgangskontrollService: FamilieIntegrasjonerTilgangskontrollService,
+) {
+    fun validerAtBehandlingIkkeInneholderStrengtFortroligePersonerMedManuelleBrevmottakere(behandlingId: Long) {
+        val brevmottakere = brevmottakerService.hentBrevmottakere(behandlingId).takeIf { it.isNotEmpty() } ?: return
+        val personopplysningGrunnlag = persongrunnlagService.hentAktivThrows(behandlingId = behandlingId)
+        val personIdenter = personopplysningGrunnlag.søkerOgBarn
+            .takeIf { it.isNotEmpty() }
+            ?.map { it.aktør.aktivFødselsnummer() }
+            ?: return
+        val strengtFortroligePersonIdenter =
+            familieIntegrasjonerTilgangskontrollService.hentIdenterMedStrengtFortroligAdressebeskyttelse(personIdenter)
+        if (strengtFortroligePersonIdenter.isNotEmpty()) {
+            val melding = "Behandlingen (id: $behandlingId) inneholder ${strengtFortroligePersonIdenter.size} person(er) med strengt fortrolig adressebeskyttelse og kan ikke kombineres med manuelle brevmottakere (${brevmottakere.size} stk)."
+            val frontendFeilmelding =
+                "Behandlingen inneholder personer med strengt fortrolig adressebeskyttelse og kan ikke kombineres med manuelle brevmottakere."
+            throw FunksjonellFeil(melding, frontendFeilmelding)
+        }
+    }
+}
---
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/behandling/ValiderBrevmottakerServiceTest.kt
@@ -0,0 +1,109 @@
+package no.nav.familie.ba.sak.kjerne.behandling
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import no.nav.familie.ba.sak.common.FunksjonellFeil
+import no.nav.familie.ba.sak.common.lagTestPersonopplysningGrunnlag
+import no.nav.familie.ba.sak.common.tilfeldigPerson
+import no.nav.familie.ba.sak.integrasjoner.familieintegrasjoner.FamilieIntegrasjonerTilgangskontrollService
+import no.nav.familie.ba.sak.kjerne.brev.mottaker.Brevmottaker
+import no.nav.familie.ba.sak.kjerne.brev.mottaker.BrevmottakerService
+import no.nav.familie.ba.sak.kjerne.brev.mottaker.MottakerType
+import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersonType
+import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersongrunnlagService
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+
+class ValiderBrevmottakerServiceTest {
+    private val brevmottakerService = mockk<BrevmottakerService>()
+    private val persongrunnlagService = mockk<PersongrunnlagService>()
+    private val familieIntegrasjonerTilgangskontrollService = mockk<FamilieIntegrasjonerTilgangskontrollService>()
+    val validerBrevmottakerService = ValiderBrevmottakerService(
+        brevmottakerService,
+        persongrunnlagService,
+        familieIntegrasjonerTilgangskontrollService,
+    )
+
+    private val behandlingId = 0L
+    val brevmottaker = Brevmottaker(
+        behandlingId = behandlingId,
+        type = MottakerType.DØDSBO,
+        navn = "Donald Duck",
+        adresselinje1 = "Andebyveien 1",
+        postnummer = "0000",
+        poststed = "OSLO",
+        landkode = "NO",
+    )
+    val søker = tilfeldigPerson(personType = PersonType.SØKER)
+
+    @Test
+    fun `Skal ikke kaste funksjonell feil når en behandling ikke inneholder noen manuelle brevmottakere`() {
+        every { brevmottakerService.hentBrevmottakere(behandlingId) } returns emptyList()
+
+        validerBrevmottakerService.validerAtBehandlingIkkeInneholderStrengtFortroligePersonerMedManuelleBrevmottakere(
+            behandlingId,
+        )
+
+        verify(exactly = 1) { brevmottakerService.hentBrevmottakere(behandlingId) }
+        verify(exactly = 0) { persongrunnlagService.hentAktivThrows(any()) }
+        verify(exactly = 0) {
+            familieIntegrasjonerTilgangskontrollService.hentIdenterMedStrengtFortroligAdressebeskyttelse(
+                any(),
+            )
+        }
+    }
+
+    @Test
+    fun `Skal kaste en FunksjonellFeil exception når en behandling inneholder minst en strengt fortrolig person og minst en manuell brevmottaker`() {
+        every { brevmottakerService.hentBrevmottakere(behandlingId) } returns listOf(brevmottaker)
+        every { persongrunnlagService.hentAktivThrows(behandlingId) } returns lagTestPersonopplysningGrunnlag(
+            behandlingId,
+            søker,
+        )
+        every { familieIntegrasjonerTilgangskontrollService.hentIdenterMedStrengtFortroligAdressebeskyttelse(any()) } returns listOf(
+            søker.aktør.aktivFødselsnummer(),
+        )
+
+        assertThatThrownBy {
+            validerBrevmottakerService.validerAtBehandlingIkkeInneholderStrengtFortroligePersonerMedManuelleBrevmottakere(
+                behandlingId,
+            )
+        }.isInstanceOf(FunksjonellFeil::class.java).hasMessageContaining("strengt fortrolig adressebeskyttelse og kan ikke kombineres med manuelle brevmottakere")
+    }
+
+    @Test
+    fun `Skal ikke kaste funksjonell feil når behandling ikke inneholder noen strengt fortrolige personer og inneholder minst en manuell brevmottaker`() {
+        every { brevmottakerService.hentBrevmottakere(behandlingId) } returns listOf(brevmottaker)
+        every { persongrunnlagService.hentAktivThrows(behandlingId) } returns lagTestPersonopplysningGrunnlag(
+            behandlingId,
+            søker,
+        )
+        every { familieIntegrasjonerTilgangskontrollService.hentIdenterMedStrengtFortroligAdressebeskyttelse(any()) } returns emptyList()
+
+        validerBrevmottakerService.validerAtBehandlingIkkeInneholderStrengtFortroligePersonerMedManuelleBrevmottakere(
+            behandlingId,
+        )
+        verify(exactly = 1) {
+            familieIntegrasjonerTilgangskontrollService.hentIdenterMedStrengtFortroligAdressebeskyttelse(
+                any(),
+            )
+        }
+    }
+
+    @Test
+    fun `Skal ikke kaste en exception når en behandling inneholder minst en strengt fortrolig person og ingen manuelle brevmottakere`() {
+        every { brevmottakerService.hentBrevmottakere(behandlingId) } returns emptyList()
+        every { persongrunnlagService.hentAktivThrows(behandlingId) } returns lagTestPersonopplysningGrunnlag(
+            behandlingId,
+            søker,
+        )
+        every { familieIntegrasjonerTilgangskontrollService.hentIdenterMedStrengtFortroligAdressebeskyttelse(any()) } returns listOf(
+            søker.aktør.aktivFødselsnummer(),
+        )
+
+        validerBrevmottakerService.validerAtBehandlingIkkeInneholderStrengtFortroligePersonerMedManuelleBrevmottakere(
+            behandlingId,
+        )
+    }
+}
