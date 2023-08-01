37bd4fe2dec13fe6772f05bab9ea5693822d06e5
halvorbmundal <halvorbmundal@users.noreply.github.com>
2023-08-01 13:44:22 +0200

    Lag avslagsperiode ved uregistrerte barn (#3791)
    ### 💰 Hva skal gjøres, og hvorfor?
    [Favro](https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-13886)
    
    Dersom vi har et uregistrert barn skal vi få en avslagsperiode uten fom-
    eller tomdato med begrunnelsen `AVSLAG_UREGISTRERT_BARN`
    
    Endrer så avslagsperioden kommer med sammen med begrunnelsen
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [x] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [x] Jeg har skrevet tester.

--- src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/cucumber/VedtaksperiodeUtil.kt
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/cucumber/VedtaksperiodeUtil.kt
@@ -24,6 +24,7 @@ import no.nav.familie.ba.sak.cucumber.domeneparser.parseValgfriDato
 import no.nav.familie.ba.sak.cucumber.domeneparser.parseValgfriEnum
 import no.nav.familie.ba.sak.cucumber.domeneparser.parseValgfriLong
 import no.nav.familie.ba.sak.cucumber.domeneparser.parseValgfriString
+import no.nav.familie.ba.sak.ekstern.restDomene.BarnMedOpplysninger
 import no.nav.familie.ba.sak.kjerne.autovedtak.fødselshendelse.Resultat
 import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandling
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelse
@@ -263,6 +264,7 @@ fun lagVedtaksPerioder(
     andelerTilkjentYtelse: Map<Long, List<AndelTilkjentYtelse>>,
     endringstidspunkt: Map<Long, LocalDate?>,
     overgangsstønad: Map<Long, List<InternPeriodeOvergangsstønad>?>,
+    uregistrerteBarn: List<BarnMedOpplysninger>,
 ): List<VedtaksperiodeMedBegrunnelser> {
     val vedtak = vedtaksListe.find { it.behandling.id == behandlingId && it.aktiv }
         ?: error("Finner ikke vedtak")
@@ -274,6 +276,7 @@ fun lagVedtaksPerioder(
         endredeUtbetalinger = endredeUtbetalinger[behandlingId] ?: emptyList(),
         andelerTilkjentYtelse = andelerTilkjentYtelse[behandlingId] ?: emptyList(),
         perioderOvergangsstønad = overgangsstønad[behandlingId] ?: emptyList(),
+        uregistrerteBarn = uregistrerteBarn,
     )
 
     val forrigeBehandlingId = behandlingTilForrigeBehandling[behandlingId]
@@ -289,6 +292,7 @@ fun lagVedtaksPerioder(
             endredeUtbetalinger = endredeUtbetalinger[forrigeBehandlingId] ?: emptyList(),
             andelerTilkjentYtelse = andelerTilkjentYtelse[forrigeBehandlingId] ?: emptyList(),
             perioderOvergangsstønad = overgangsstønad[behandlingId] ?: emptyList(),
+            uregistrerteBarn = emptyList(),
         )
     }
 
@@ -310,6 +314,7 @@ fun lagVedtaksPerioderMedUtledetEndringsTidspunkt(
     endredeUtbetalinger: Map<Long, List<EndretUtbetalingAndel>>,
     andelerTilkjentYtelse: Map<Long, List<AndelTilkjentYtelse>>,
     overgangsstønad: Map<Long, List<InternPeriodeOvergangsstønad>?>,
+    uregistrerteBarn: List<BarnMedOpplysninger>,
 ): List<VedtaksperiodeMedBegrunnelser> {
     val vedtak = vedtaksListe.find { it.behandling.id == behandlingId && it.aktiv }
         ?: error("Finner ikke vedtak")
@@ -321,6 +326,7 @@ fun lagVedtaksPerioderMedUtledetEndringsTidspunkt(
         endredeUtbetalinger = endredeUtbetalinger[behandlingId] ?: emptyList(),
         andelerTilkjentYtelse = andelerTilkjentYtelse[behandlingId] ?: emptyList(),
         perioderOvergangsstønad = overgangsstønad[behandlingId] ?: emptyList(),
+        uregistrerteBarn = uregistrerteBarn,
     )
 
     val forrigeBehandlingId = behandlingTilForrigeBehandling[behandlingId]
@@ -336,6 +342,7 @@ fun lagVedtaksPerioderMedUtledetEndringsTidspunkt(
             endredeUtbetalinger = endredeUtbetalinger[forrigeBehandlingId] ?: emptyList(),
             andelerTilkjentYtelse = andelerTilkjentYtelse[forrigeBehandlingId] ?: emptyList(),
             perioderOvergangsstønad = overgangsstønad[behandlingId] ?: emptyList(),
+            uregistrerteBarn = uregistrerteBarn,
         )
     }
 
