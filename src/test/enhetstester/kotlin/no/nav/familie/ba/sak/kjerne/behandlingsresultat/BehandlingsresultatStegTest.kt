1a1ad3043cb51c472577eaf23749d0e0bf54b261
bragejahren <70642183+bragejahren@users.noreply.github.com>
2023-08-01 14:55:41 +0200

    Utvidet testdekning for satsendring (#3747)
    Favro:
    [NAV-13498](https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-13498)
    
    ### 💰 Hva skal gjøres, og hvorfor?
    Under kjøring av satsendring fant vi ut at vi ikke hadde kopiert over
    endrede utbetalingsandeler fra forrige behandling. Dette førte til at
    satsendringen i noen tilfeller ga endringer i utbetaling ut over endring
    i sats. Vi fikk rettet feilen for den pågående satskjøringen og patchet
    fagsakene som ble påvirket av feilen.
    
    Validering av andelene for satsendring var lagt inn i koden, men ble
    aldri kjørt fordi koden kun ble trigget for manuelle behandlinger. Har
    nå enablet valideringskoden for satsendring og lagt til tester for
    kopiering av endrede-utbetalingsandeler.
    
    ### ✅ Checklist
    - [x] Jeg har skrevet tester.
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatStegTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatStegTest.kt
@@ -6,7 +6,11 @@ import io.mockk.mockk
 import io.mockk.runs
 import no.nav.familie.ba.sak.common.Feil
 import no.nav.familie.ba.sak.common.FunksjonellFeil
+import no.nav.familie.ba.sak.common.førsteDagIInneværendeMåned
+import no.nav.familie.ba.sak.common.lagAndelTilkjentYtelse
 import no.nav.familie.ba.sak.common.lagBehandling
+import no.nav.familie.ba.sak.common.lagInitiellTilkjentYtelse
+import no.nav.familie.ba.sak.common.lagPerson
 import no.nav.familie.ba.sak.common.lagVedtak
 import no.nav.familie.ba.sak.kjerne.behandling.BehandlingHentOgPersisterService
 import no.nav.familie.ba.sak.kjerne.behandling.BehandlingService
@@ -16,9 +20,15 @@ import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingType
 import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandlingsresultat
 import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingÅrsak
 import no.nav.familie.ba.sak.kjerne.beregning.BeregningService
+import no.nav.familie.ba.sak.kjerne.beregning.SatsService
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelseRepository
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelerTilkjentYtelseOgEndreteUtbetalingerService
+import no.nav.familie.ba.sak.kjerne.beregning.domene.SatsType
+import no.nav.familie.ba.sak.kjerne.beregning.domene.TilkjentYtelse
+import no.nav.familie.ba.sak.kjerne.forrigebehandling.EndringIUtbetalingUtil
+import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.Person
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersongrunnlagService
+import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersonopplysningGrunnlag
 import no.nav.familie.ba.sak.kjerne.simulering.SimuleringService
 import no.nav.familie.ba.sak.kjerne.steg.EndringerIUtbetalingForBehandlingSteg
 import no.nav.familie.ba.sak.kjerne.steg.StegType
@@ -30,11 +40,14 @@ import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.MånedTidspunkt.Companio
 import no.nav.familie.ba.sak.kjerne.vedtak.VedtakService
 import no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.VedtaksperiodeService
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.VilkårService
+import org.assertj.core.api.Assertions.assertThatCode
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import java.math.BigDecimal
 import java.time.YearMonth
 
 class BehandlingsresultatStegTest {
@@ -175,6 +188,225 @@ class BehandlingsresultatStegTest {
     }
 
     @Test
+    fun `preValiderSteg - skal validere andeler ved satsendring og ikke kaste feil når endringene i andeler kun er relatert til endring i sats`() {
+        val søker = lagPerson()
+        val barn = lagPerson()
+        val forrigeBehandling =
+            lagBehandling(behandlingType = BehandlingType.FØRSTEGANGSBEHANDLING, årsak = BehandlingÅrsak.SØKNAD)
+
+        val forrigeTilkjentYtelse = lagInitiellTilkjentYtelse(behandling = forrigeBehandling)
+        forrigeTilkjentYtelse.andelerTilkjentYtelse.addAll(
+            mutableSetOf(
+                lagAndelTilkjentYtelse(
+                    fom = YearMonth.of(2023, 1),
+                    tom = YearMonth.of(2023, 2),
+                    behandling = forrigeBehandling,
+                    tilkjentYtelse = forrigeTilkjentYtelse,
+                    beløp = SatsService.finnGjeldendeSatsForDato(
+                        SatsType.ORBA,
+                        YearMonth.of(2023, 1).førsteDagIInneværendeMåned(),
+                    ),
+                    aktør = barn.aktør,
+                ),
+                lagAndelTilkjentYtelse(
+                    fom = YearMonth.of(2023, 3),
+                    tom = YearMonth.of(2033, 1),
+                    behandling = forrigeBehandling,
+                    tilkjentYtelse = forrigeTilkjentYtelse,
+                    beløp = SatsService.finnGjeldendeSatsForDato(
+                        SatsType.ORBA,
+                        YearMonth.of(2023, 3).førsteDagIInneværendeMåned(),
+                    ),
+                    aktør = barn.aktør,
+                ),
+            ),
+        )
+
+        val behandling = lagBehandling(
+            fagsak = forrigeBehandling.fagsak,
+            behandlingType = BehandlingType.REVURDERING,
+            årsak = BehandlingÅrsak.SATSENDRING,
+        )
+        val tilkjentYtelse = lagInitiellTilkjentYtelse(behandling = behandling)
+        tilkjentYtelse.andelerTilkjentYtelse.addAll(
+            mutableSetOf(
+                lagAndelTilkjentYtelse(
+                    fom = YearMonth.of(2023, 1),
+                    tom = YearMonth.of(2023, 2),
+                    behandling = forrigeBehandling,
+                    tilkjentYtelse = forrigeTilkjentYtelse,
+                    beløp = SatsService.finnGjeldendeSatsForDato(
+                        SatsType.ORBA,
+                        YearMonth.of(2023, 1).førsteDagIInneværendeMåned(),
+                    ),
+                    aktør = barn.aktør,
+                ),
+                lagAndelTilkjentYtelse(
+                    fom = YearMonth.of(2023, 3),
+                    tom = YearMonth.of(2023, 6),
+                    behandling = forrigeBehandling,
+                    tilkjentYtelse = forrigeTilkjentYtelse,
+                    beløp = SatsService.finnGjeldendeSatsForDato(
+                        SatsType.ORBA,
+                        YearMonth.of(2023, 3).førsteDagIInneværendeMåned(),
+                    ),
+                    aktør = barn.aktør,
+                ),
+                lagAndelTilkjentYtelse(
+                    fom = YearMonth.of(2023, 7),
+                    tom = YearMonth.of(2033, 1),
+                    behandling = forrigeBehandling,
+                    tilkjentYtelse = forrigeTilkjentYtelse,
+                    beløp = SatsService.finnGjeldendeSatsForDato(
+                        SatsType.ORBA,
+                        YearMonth.of(2023, 7).førsteDagIInneværendeMåned(),
+                    ),
+                    aktør = barn.aktør,
+                ),
+            ),
+        )
+        lagMocksForPreValiderStegSatsendring(
+            behandling = behandling,
+            tilkjentYtelse = tilkjentYtelse,
+            forrigeBehandling = forrigeBehandling,
+            forrigeTilkjentYtelse = forrigeTilkjentYtelse,
+            søker = søker,
+            barn = listOf(barn),
+        )
+
+        assertThatCode { behandlingsresultatSteg.preValiderSteg(behandling) }.doesNotThrowAnyException()
+    }
+
+    @Test
+    fun `preValiderSteg - skal validere andeler ved satsendring og kaste feil når endringene i andeler er relatert til noe annet enn endring i sats`() {
+        val søker = lagPerson()
+        val barn = lagPerson()
+        val forrigeBehandling =
+            lagBehandling(behandlingType = BehandlingType.FØRSTEGANGSBEHANDLING, årsak = BehandlingÅrsak.SØKNAD)
+        val forrigeTilkjentYtelse = lagInitiellTilkjentYtelse(behandling = forrigeBehandling)
+        forrigeTilkjentYtelse.andelerTilkjentYtelse.addAll(
+            mutableSetOf(
+                lagAndelTilkjentYtelse(
+                    fom = YearMonth.of(2023, 1),
+                    tom = YearMonth.of(2023, 2),
+                    behandling = forrigeBehandling,
+                    tilkjentYtelse = forrigeTilkjentYtelse,
+                    beløp = SatsService.finnGjeldendeSatsForDato(
+                        SatsType.ORBA,
+                        YearMonth.of(2023, 1).førsteDagIInneværendeMåned(),
+                    ),
+                    aktør = barn.aktør,
+                    prosent = BigDecimal(50),
+                ),
+                lagAndelTilkjentYtelse(
+                    fom = YearMonth.of(2023, 3),
+                    tom = YearMonth.of(2033, 1),
+                    behandling = forrigeBehandling,
+                    tilkjentYtelse = forrigeTilkjentYtelse,
+                    beløp = SatsService.finnGjeldendeSatsForDato(
+                        SatsType.ORBA,
+                        YearMonth.of(2023, 3).førsteDagIInneværendeMåned(),
+                    ),
+                    aktør = barn.aktør,
+                    prosent = BigDecimal(50),
+                ),
+            ),
+        )
+
+        val behandling = lagBehandling(
+            fagsak = forrigeBehandling.fagsak,
+            behandlingType = BehandlingType.REVURDERING,
+            årsak = BehandlingÅrsak.SATSENDRING,
+        )
+        val tilkjentYtelse = lagInitiellTilkjentYtelse(behandling = behandling)
+        tilkjentYtelse.andelerTilkjentYtelse.addAll(
+            mutableSetOf(
+                lagAndelTilkjentYtelse(
+                    fom = YearMonth.of(2023, 1),
+                    tom = YearMonth.of(2023, 2),
+                    behandling = forrigeBehandling,
+                    tilkjentYtelse = forrigeTilkjentYtelse,
+                    beløp = SatsService.finnGjeldendeSatsForDato(
+                        SatsType.ORBA,
+                        YearMonth.of(2023, 1).førsteDagIInneværendeMåned(),
+                    ),
+                    aktør = barn.aktør,
+                    prosent = BigDecimal(50),
+                ),
+                lagAndelTilkjentYtelse(
+                    fom = YearMonth.of(2023, 3),
+                    tom = YearMonth.of(2023, 6),
+                    behandling = forrigeBehandling,
+                    tilkjentYtelse = forrigeTilkjentYtelse,
+                    beløp = SatsService.finnGjeldendeSatsForDato(
+                        SatsType.ORBA,
+                        YearMonth.of(2023, 3).førsteDagIInneværendeMåned(),
+                    ),
+                    aktør = barn.aktør,
+                    prosent = BigDecimal(100),
+                ),
+                lagAndelTilkjentYtelse(
+                    fom = YearMonth.of(2023, 7),
+                    tom = YearMonth.of(2033, 1),
+                    behandling = forrigeBehandling,
+                    tilkjentYtelse = forrigeTilkjentYtelse,
+                    beløp = SatsService.finnGjeldendeSatsForDato(
+                        SatsType.ORBA,
+                        YearMonth.of(2023, 7).førsteDagIInneværendeMåned(),
+                    ),
+                    aktør = barn.aktør,
+                    prosent = BigDecimal(50),
+                ),
+            ),
+        )
+        lagMocksForPreValiderStegSatsendring(
+            behandling = behandling,
+            tilkjentYtelse = tilkjentYtelse,
+            forrigeBehandling = forrigeBehandling,
+            forrigeTilkjentYtelse = forrigeTilkjentYtelse,
+            søker = søker,
+            barn = listOf(barn),
+        )
+
+        assertThatThrownBy { behandlingsresultatSteg.preValiderSteg(behandling) }.isInstanceOf(Feil::class.java)
+            .hasMessage("Satsendring kan ikke endre på prosenten til en andel")
+    }
+
+    private fun lagMocksForPreValiderStegSatsendring(
+        behandling: Behandling,
+        tilkjentYtelse: TilkjentYtelse,
+        forrigeBehandling: Behandling,
+        forrigeTilkjentYtelse: TilkjentYtelse,
+        søker: Person,
+        barn: List<Person>,
+    ) {
+        val personopplysningGrunnlag = mockk<PersonopplysningGrunnlag>()
+        every { vilkårService.hentVilkårsvurderingThrows(behandling.id) } returns mockk()
+        every { vilkårService.hentVilkårsvurdering(behandling.id) } returns mockk()
+        every { persongrunnlagService.hentBarna(any<Behandling>()) } returns emptyList()
+        every { beregningService.hentTilkjentYtelseForBehandling(behandling.id) } returns tilkjentYtelse
+
+        every { persongrunnlagService.hentAktivThrows(any()) } returns personopplysningGrunnlag
+        every { personopplysningGrunnlag.søker } returns søker
+        every { personopplysningGrunnlag.barna } returns barn
+        every { behandlingHentOgPersisterService.hentForrigeBehandlingSomErVedtatt(behandling) } returns forrigeBehandling
+        every { andelTilkjentYtelseRepository.finnAndelerTilkjentYtelseForBehandling(forrigeBehandling.id) } returns forrigeTilkjentYtelse.andelerTilkjentYtelse.toList()
+        every {
+            andelerTilkjentYtelseOgEndreteUtbetalingerService
+                .finnEndreteUtbetalingerMedAndelerTilkjentYtelse(behandling.id)
+        } returns emptyList()
+
+        every { beregningService.hentEndringerIUtbetalingFraForrigeBehandlingSendtTilØkonomiTidslinje(behandling) } returns EndringIUtbetalingUtil.lagEndringIUtbetalingTidslinje(
+            nåværendeAndeler = tilkjentYtelse.andelerTilkjentYtelse.toList(),
+            forrigeAndeler = forrigeTilkjentYtelse.andelerTilkjentYtelse.toList(),
+        )
+
+        every {
+            beregningService
+                .hentAndelerFraForrigeIverksattebehandling(behandling)
+        } returns emptyList()
+    }
+
     fun `skal gå rett fra behandlingsresultat til iverksetting for alle fødselshendelser`() {
         val fødselshendelseBehandling = behandling.copy(
             skalBehandlesAutomatisk = true,
