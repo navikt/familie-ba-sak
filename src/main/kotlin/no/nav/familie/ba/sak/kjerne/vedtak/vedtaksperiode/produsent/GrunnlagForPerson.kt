6f4f1b582699e7431c0dc00da6a1297eb0570016
halvorbmundal <halvorbmundal@users.noreply.github.com>
2023-08-08 09:04:34 +0200

    Lag ikke-utbetalings-perioder når det andelene er endret til 0% og det ikke er delt bosted (#3800)
    ### 💰 Hva skal gjøres, og hvorfor?
    
    [Favro](https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-13242)
    
    Hvorfor:
    Vi har enkelte scenarier som i dag blir rare. Dette gjelder f.eks saker
    der bruker kun oppfyller vilkårene tilbake i tid for en periode som var
    mer enn 3 år tilbake i tid. I dag får vi vedtaksbrevet "Du får
    barnetrygd", men ønsket er at vi her skal avslå ytelsen fordi de i
    realiteten ikke får barnetrygd.
    
    Hva: 
    Endrer så perioder med endret utbetaling 0% med alle årsaker utenom
    "Delt bosted", skal gi vedtakperiode av typen "Ingen utbetaling"
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [x] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/produsent/GrunnlagForPerson.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/produsent/GrunnlagForPerson.kt
@@ -44,7 +44,10 @@ data class GrunnlagForPersonInnvilget(
     val kompetanse: KompetanseForVedtaksperiode? = null,
     val endretUtbetalingAndel: EndretUtbetalingAndelForVedtaksperiode? = null,
     val overgangsstønad: OvergangsstønadForVedtaksperiode? = null,
-) : GrunnlagForPerson
+) : GrunnlagForPerson {
+    fun erEndretTilIngenUtbetalingIkkeDeltBosted() =
+        andeler.all { it.prosent == BigDecimal.ZERO } && endretUtbetalingAndel?.årsak != Årsak.DELT_BOSTED == true
+}
 
 data class GrunnlagForPersonIkkeInnvilget(
     override val person: Person,
@@ -100,10 +103,12 @@ data class EndretUtbetalingAndelForVedtaksperiode(
 data class AndelForVedtaksperiode(
     val kalkulertUtbetalingsbeløp: Int,
     val type: YtelseType,
+    val prosent: BigDecimal,
 ) {
     constructor(andelTilkjentYtelse: AndelTilkjentYtelse) : this(
         kalkulertUtbetalingsbeløp = andelTilkjentYtelse.kalkulertUtbetalingsbeløp,
         type = andelTilkjentYtelse.type,
+        prosent = andelTilkjentYtelse.prosent,
     )
 }
 
