9b10163804083a8d030877d1b7ebbdd3d6874d68
marius-nav <Marius.Glad.Fredriksen@nav.no>
2023-08-22 13:11:05 +0200

    NAV-14562 - endre personident til null (fra tom streng) for kafka start- og slutt-melding

--- src/main/kotlin/no/nav/familie/ba/sak/ekstern/pensjon/HentAlleIdenterTilPsysResponseDTO.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/ekstern/pensjon/HentAlleIdenterTilPsysResponseDTO.kt
@@ -1,7 +1,7 @@
 package no.nav.familie.ba.sak.ekstern.pensjon
 
 import java.util.UUID
-data class HentAlleIdenterTilPsysResponseDTO(val meldingstype: Meldingstype, val requestId: UUID, val personident: String)
+data class HentAlleIdenterTilPsysResponseDTO(val meldingstype: Meldingstype, val requestId: UUID, val personident: String?)
 
 enum class Meldingstype {
     START, DATA, SLUTT
