b0c2ab3009bb81c2260d6dda2f7d0f4bfef32eb8
marius-nav <Marius.Glad.Fredriksen@nav.no>
2023-09-01 13:23:35 +0200

    NAV-12150 Håndtere fødselshendelser hvor søker sine vilkår ikke er oppfylt (og dermed ingen andeler blir generert) - work in progress..

---
+++ src/main/kotlin/no/nav/familie/ba/sak/task/OpprettVurderKonsekvensForYtelseOppgave.kt
@@ -0,0 +1,53 @@
+package no.nav.familie.ba.sak.task
+
+import no.nav.familie.ba.sak.integrasjoner.oppgave.OppgaveService
+import no.nav.familie.ba.sak.task.dto.OpprettVurderKonsekvensForYtelseOppgaveTaskDTO
+import no.nav.familie.kontrakter.felles.objectMapper
+import no.nav.familie.kontrakter.felles.oppgave.Oppgavetype
+import no.nav.familie.prosessering.AsyncTaskStep
+import no.nav.familie.prosessering.TaskStepBeskrivelse
+import no.nav.familie.prosessering.domene.Task
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+
+@Service
+@TaskStepBeskrivelse(
+    taskStepType = OpprettVurderKonsekvensForYtelseOppgave.TASK_STEP_TYPE,
+    beskrivelse = "Opprett oppgave i GOSYS for fødselshendelse som ikke lar seg utføre automatisk",
+    maxAntallFeil = 3,
+)
+class OpprettVurderKonsekvensForYtelseOppgave(
+    private val oppgaveService: OppgaveService,
+) : AsyncTaskStep {
+
+    override fun doTask(task: Task) {
+        val opprettVurderKonsekvensForYtelseOppgaveTaskDTO = objectMapper.readValue(task.payload, OpprettVurderKonsekvensForYtelseOppgaveTaskDTO::class.java)
+        task.metadata["oppgaveId"] = oppgaveService.opprettOppgave(
+            oppgavetype = opprettVurderKonsekvensForYtelseOppgaveTaskDTO.oppgavetype,
+            fristForFerdigstillelse = opprettVurderKonsekvensForYtelseOppgaveTaskDTO.fristForFerdigstillelse,
+            beskrivelse = opprettVurderKonsekvensForYtelseOppgaveTaskDTO.beskrivelse,
+        )
+    }
+
+    companion object {
+
+        const val TASK_STEP_TYPE = "opprettVurderKonsekvensForYtelseOppgave"
+
+        fun opprettTask(
+            oppgavetype: Oppgavetype,
+            fristForFerdigstillelse: LocalDate,
+            beskrivelse: String? = null,
+        ): Task {
+            return Task(
+                type = TASK_STEP_TYPE,
+                payload = objectMapper.writeValueAsString(
+                    OpprettVurderKonsekvensForYtelseOppgaveTaskDTO(
+                        oppgavetype,
+                        fristForFerdigstillelse,
+                        beskrivelse,
+                    ),
+                ),
+            )
+        }
+    }
+}
