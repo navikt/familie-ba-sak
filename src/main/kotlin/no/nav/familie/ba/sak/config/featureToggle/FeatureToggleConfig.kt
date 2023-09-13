f65d7b1239a61c84f75cfd753817f27bb7d62622
tnarland <47184872+tnarland@users.noreply.github.com>
2023-09-13 09:25:12 +0200

    Fix: Kompetanse ble ikke korrekt når bruker gikk fra EØS til nasjonal på 2 av 3 barn. (#3905)
    https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-12319
    
    Det er forventet at kompetanse fortsatt må settes for gjenværende EØS-barn.
    Utsetter kombineringen av barns RegelverkResultat med søkers regelverkResultat. Oppretter instans av ny klasse som tar vare på resultatene til evaluering senere istedenfor. Får dermed filtrert etter regelverket spesifikt gjeldende barnet for regelverkresultat OPPFYLT_BLANDET_REGELVERK ved henting av "barnasEøsRegelverkTidslinjer"
    
    Legger den funksjonelle endringen bak en ny feature toggle i Unleash Next

--- src/main/kotlin/no/nav/familie/ba/sak/config/featureToggle/FeatureToggleConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/featureToggle/FeatureToggleConfig.kt
@@ -21,6 +21,9 @@ class FeatureToggleConfig {
 
         // Ny utbetalingsgenerator
         const val KONTROLLER_NY_UTBETALINGSGENERATOR = "familie.ba.sak.kontroller-ny-utbetalingsgenerator"
+
+        // Unleash Next toggles
+        const val ENDRET_EØS_REGELVERKFILTER_FOR_BARN = "familie-ba-sak.endret-eos-regelverkfilter-for-barn"
     }
 }
 
