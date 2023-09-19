805978aa48149cd26e2cff79cc4606595d055320
Stig Helge Strøm <stig.helge.strom@nav.no>
2023-09-19 14:41:45 +0200

    refaktorering

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/steg/FerdigstillBehandling.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/steg/FerdigstillBehandling.kt
@@ -48,6 +48,12 @@ class FerdigstillBehandling(
         behandlingMetrikker.oppdaterBehandlingMetrikker(behandling)
         if (behandling.status == BehandlingStatus.IVERKSETTER_VEDTAK && behandling.resultat != Behandlingsresultat.AVSLÅTT) {
             oppdaterFagsakStatus(behandling = behandling)
+
+            val tilkjentYtelse = tilkjentYtelseRepository.findByBehandling(behandlingId = behandling.id)
+            if (skalOppdatereStønadFomOgTomForIverksatteBehandlingerIkkeSendtTilOppdrag(tilkjentYtelse)) {
+                tilkjentYtelse.stønadTom = tilkjentYtelse.andelerTilkjentYtelse.maxOfOrNull { it.stønadTom }
+                tilkjentYtelse.stønadFom = tilkjentYtelse.andelerTilkjentYtelse.minOfOrNull { it.stønadFom }
+            }
         } else { // Dette betyr henleggelse.
             if (behandlingHentOgPersisterService.hentBehandlinger(behandling.fagsak.id).size == 1) {
                 fagsakService.oppdaterStatus(behandling.fagsak, FagsakStatus.AVSLUTTET)
@@ -62,18 +68,10 @@ class FerdigstillBehandling(
         behandlingService.oppdaterStatusPåBehandling(behandlingId = behandling.id, status = BehandlingStatus.AVSLUTTET)
         snikeIKøenService.reaktiverBehandlingPåMaskinellVent(behandlingSomFerdigstilles = behandling)
 
-        if (!behandling.erHenlagt()) {
-            val tilkjentYtelse = tilkjentYtelseRepository.findByBehandling(behandlingId = behandling.id)
-            if (skalOppdatereStønadFomOgTomForIkkeIverksatteBehandlinger(tilkjentYtelse)) {
-                tilkjentYtelse.stønadTom = tilkjentYtelse.andelerTilkjentYtelse.maxOfOrNull { it.stønadTom }
-                tilkjentYtelse.stønadFom = tilkjentYtelse.andelerTilkjentYtelse.minOfOrNull { it.stønadFom }
-            }
-        }
-
         return hentNesteStegForNormalFlyt(behandling)
     }
 
-    private fun skalOppdatereStønadFomOgTomForIkkeIverksatteBehandlinger(tilkjentYtelse: TilkjentYtelse) =
+    private fun skalOppdatereStønadFomOgTomForIverksatteBehandlingerIkkeSendtTilOppdrag(tilkjentYtelse: TilkjentYtelse) =
         tilkjentYtelse.stønadFom == null && tilkjentYtelse.stønadTom == null && tilkjentYtelse.utbetalingsoppdrag == null
 
     private fun oppdaterFagsakStatus(behandling: Behandling) {
