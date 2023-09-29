7e3acf54d1ac1899f476e14ae8c78c0861914236
Kristine Steine <kristine.steine@bekk.no>
2023-09-29 10:29:00 +0200

    Begrunn innvilgelse med utvidet selv om vilkåret har vært oppfylt i forrige periode

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/vedtakBegrunnelseProdusent/VilkårFilterUtil.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/vedtakBegrunnelseProdusent/VilkårFilterUtil.kt
@@ -5,6 +5,7 @@ import no.nav.familie.ba.sak.kjerne.brev.domene.UtvidetBarnetrygdTrigger
 import no.nav.familie.ba.sak.kjerne.brev.domene.VilkårTrigger
 import no.nav.familie.ba.sak.kjerne.brev.domene.tilUtdypendeVilkårsvurderinger
 import no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.vedtakBegrunnelseProdusent.IBegrunnelseGrunnlagForPeriode
+import no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.vedtaksperiodeProdusent.AndelForVedtaksperiode
 import no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.vedtaksperiodeProdusent.VilkårResultatForVedtaksperiode
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.UtdypendeVilkårsvurdering
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.Vilkår
@@ -77,8 +78,12 @@ private fun finnUtgjørendeVilkår(
     )
 
     return if (begrunnelseGrunnlag.dennePerioden.erOrdinæreVilkårInnvilget()) {
+        val utvidetTriggetAvInnvilgelse = hentUtvidetTriggetAvInnvilgelse(
+            andelerForrigePeriode = begrunnelseGrunnlag.forrigePeriode?.andeler,
+            oppfylteVilkårResultaterDennePerioden = oppfylteVilkårResultaterDennePerioden,
+        )
         when (sanityBegrunnelse.periodeResultat) {
-            SanityPeriodeResultat.INNVILGET_ELLER_ØKNING -> vilkårTjent + vilkårEndret
+            SanityPeriodeResultat.INNVILGET_ELLER_ØKNING -> vilkårTjent + vilkårEndret + utvidetTriggetAvInnvilgelse
             SanityPeriodeResultat.INGEN_ENDRING -> vilkårEndret
             SanityPeriodeResultat.IKKE_INNVILGET,
             SanityPeriodeResultat.REDUKSJON,
@@ -130,4 +135,14 @@ private fun hentVilkårResultaterTapt(
     val vilkårTapt = oppfyltForrigePeriode - oppfyltDennePerioden
 
     return oppfylteVilkårResultaterForrigePeriode.filter { it.vilkårType in vilkårTapt }
+}
+
+private fun hentUtvidetTriggetAvInnvilgelse(
+    andelerForrigePeriode: Iterable<AndelForVedtaksperiode>?,
+    oppfylteVilkårResultaterDennePerioden: List<VilkårResultatForVedtaksperiode>,
+): List<VilkårResultatForVedtaksperiode> {
+    val ingenAndelerForrigePeriode = andelerForrigePeriode == null || !andelerForrigePeriode.any()
+    val utvidetOppfyltDennePerioden =
+        oppfylteVilkårResultaterDennePerioden.filter { it.vilkårType == Vilkår.UTVIDET_BARNETRYGD }
+    return if (ingenAndelerForrigePeriode) utvidetOppfyltDennePerioden else emptyList()
 }
