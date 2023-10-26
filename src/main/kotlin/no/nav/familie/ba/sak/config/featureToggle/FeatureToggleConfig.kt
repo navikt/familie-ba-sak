6a7991d5e05f22480861e70987d89749bb56adcc
Stig Helge Strøm <stig.helge.strom@nav.no>
2023-10-26 09:41:13 +0200

    Flyttet kontroller-ny-utbetalingsgenerator til unleash-next

--- src/main/kotlin/no/nav/familie/ba/sak/config/featureToggle/FeatureToggleConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/featureToggle/FeatureToggleConfig.kt
@@ -15,17 +15,15 @@ class FeatureToggleConfig {
         const val EØS_PRAKSISENDRING_SEPTEMBER2023 =
             "familie-ba-sak.behandling.eos-annen-forelder-omfattet-av-norsk-lovgivning"
 
-        // Ny utbetalingsgenerator
-        const val KONTROLLER_NY_UTBETALINGSGENERATOR = "familie.ba.sak.kontroller-ny-utbetalingsgenerator"
 
         // Unleash Next toggles
         const val ENDRET_EØS_REGELVERKFILTER_FOR_BARN = "familie-ba-sak.endret-eos-regelverkfilter-for-barn"
         const val NY_GENERERING_AV_BREVOBJEKTER = "familie-ba-sak.ny-generering-av-brevobjekter"
-
         const val SATSENDRING_ENABLET: String = "familie-ba-sak.satsendring-enablet"
 
         // Ny utbetalingsgenerator
         const val BRUK_NY_UTBETALINGSGENERATOR = "familie.ba.sak.bruk-ny-utbetalingsgenerator"
+        const val KONTROLLER_NY_UTBETALINGSGENERATOR = "familie-ba-sak.kontroller-ny-utbetalingsgenerator"
     }
 }
 
