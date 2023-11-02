06d264953ca75d7a65847a2f67433dbfa7998451
halvormundal <halvorbmundal@gmail.com>
2023-11-02 11:03:10 +0100

    Flytt KAN_MANUELT_KORRIGERE_MED_VEDTAKSBREV over til ny unleash

--- src/main/kotlin/no/nav/familie/ba/sak/config/featureToggle/FeatureToggleConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/featureToggle/FeatureToggleConfig.kt
@@ -3,7 +3,6 @@ package no.nav.familie.ba.sak.config
 class FeatureToggleConfig {
     companion object {
         // Operasjonelle
-        const val KAN_MANUELT_KORRIGERE_MED_VEDTAKSBREV = "familie-ba-sak.behandling.korreksjon-vedtaksbrev"
         const val IKKE_STOPP_MIGRERINGSBEHANDLING = "familie-ba-sak.ikke.stopp.migeringsbehandling"
         const val TEKNISK_VEDLIKEHOLD_HENLEGGELSE = "familie-ba-sak.teknisk-vedlikehold-henleggelse.tilgangsstyring"
         const val TEKNISK_ENDRING = "familie-ba-sak.behandling.teknisk-endring"
@@ -17,6 +16,9 @@ class FeatureToggleConfig {
         // Ny utbetalingsgenerator
         const val BRUK_NY_UTBETALINGSGENERATOR = "familie.ba.sak.bruk-ny-utbetalingsgenerator"
         const val KONTROLLER_NY_UTBETALINGSGENERATOR = "familie-ba-sak.kontroller-ny-utbetalingsgenerator"
+
+        // Operasjonelle
+        const val KAN_MANUELT_KORRIGERE_MED_VEDTAKSBREV = "familie-ba-sak.behandling.korreksjon-vedtaksbrev"
     }
 }
 
