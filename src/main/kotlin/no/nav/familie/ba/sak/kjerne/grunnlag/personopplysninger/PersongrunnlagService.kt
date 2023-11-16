f6349ddd20712204c26600cc17e8bb1edfb633c1
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-11-16 10:24:36 +0100

    Bug/chore: kast funksjonell feil hvis man prøver å legge til søker som barn på behandlingen (#4191)
    ### 💰 Hva skal gjøres, og hvorfor?
    Favro:
    https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-17018
    
    Kaster funksjonell feil hvis saksbehandler prøver å legge til søker som
    barn på behandlingen. I dag får vi en del feil i sentry på dette, så det
    er fint å heller kaste funksjonell feil så det styrer unna varsel på
    sentry.
    Det er ingen tilfeller hvor saksbehandler skal manuelt legge til søker
    som barn på en behandling (funker uansett ikke i dag å gjøre det), og vi
    kan derfor trygt legge inn denne sjekken.
    
    ### 🔎️ Er det noe spesielt du ønsker tilbakemelding om?
    Nei
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [ ] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    _Jeg har ikke skrevet tester fordi:_
    Gir lite verdi
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/grunnlag/personopplysninger/PersongrunnlagService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/grunnlag/personopplysninger/PersongrunnlagService.kt
@@ -133,6 +133,10 @@ class PersongrunnlagService(
 
         val personopplysningGrunnlag = hentAktivThrows(behandlingId = behandling.id)
 
+        if (nyttbarnAktør == personopplysningGrunnlag.søker.aktør) {
+            throw FunksjonellFeil("Du kan ikke legge til søker som barn på behandlingen")
+        }
+
         val barnIGrunnlag = personopplysningGrunnlag.barna.map { it.aktør }
 
         if (barnIGrunnlag.contains(nyttbarnAktør)) {
