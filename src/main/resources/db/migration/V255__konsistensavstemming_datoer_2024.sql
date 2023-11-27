4c753a175fbb9b05e2ed2ec071f195ca2857f533
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2023-11-27 08:13:34 +0100

    Legg til dato for konsistensavstemming 2024
    rename

---
+++ src/main/resources/db/migration/V255__konsistensavstemming_datoer_2024.sql
@@ -0,0 +1,13 @@
+INSERT INTO batch (id, kjoredato)
+VALUES (nextval('batch_seq'), TO_TIMESTAMP('04-01-2024', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('30-01-2024', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('28-02-2024', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('29-03-2024', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('25-04-2024', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('24-05-2024', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('28-06-2024', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('30-07-2024', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('30-08-2024', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('27-09-2024', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('30-10-2024', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('25-11-2024', 'DD-MM-YYYY SS:MS'));
