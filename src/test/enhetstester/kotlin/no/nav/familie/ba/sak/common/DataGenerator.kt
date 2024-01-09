0cea0377ffb9ec2d52759fd374f3f893275961ea
Ida Merete Enholm <ida.merete.enholm@nav.no>
2024-01-09 17:52:44 +0100

    revert endringer som ikke trengs lenger

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/common/DataGenerator.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/common/DataGenerator.kt
@@ -1122,7 +1122,6 @@ fun lagVilkårResultat(
     utdypendeVilkårsvurderinger: List<UtdypendeVilkårsvurdering> = emptyList(),
     erEksplisittAvslagPåSøknad: Boolean = false,
     standardbegrunnelser: List<IVedtakBegrunnelse> = emptyList(),
-    vurderesEtter: Regelverk = Regelverk.NASJONALE_REGLER,
 ) = VilkårResultat(
     personResultat = personResultat,
     vilkårType = vilkårType,
@@ -1134,7 +1133,6 @@ fun lagVilkårResultat(
     utdypendeVilkårsvurderinger = utdypendeVilkårsvurderinger,
     erEksplisittAvslagPåSøknad = erEksplisittAvslagPåSøknad,
     standardbegrunnelser = standardbegrunnelser,
-    vurderesEtter = vurderesEtter,
 )
 
 val guttenBarnesenFødselsdato = LocalDate.now().withDayOfMonth(10).minusYears(6)
