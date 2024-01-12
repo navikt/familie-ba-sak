1fd60b252626fb9580f0bd9b7104fe9c2121c0e6
Henning Solberg <henning.solberg@nav.no>
2024-01-12 22:20:50 +0100

    ktlint

--- src/main/kotlin/no/nav/familie/ba/sak/internal/ForvalterService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/internal/ForvalterService.kt
@@ -40,7 +40,6 @@ import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
-import java.time.LocalDateTime
 import java.time.YearMonth
 
 @Service
