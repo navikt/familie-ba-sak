58843a44f897ea1d03eb9fffa3811bcb9cd936a2
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2024-01-15 16:17:56 +0100

    Revert "filtrer ut satsendringsfeil som feiler på vilkår og legg de i satskjø…" (#4302)
    Reverts navikt/familie-ba-sak#4301
    
    får UnexpectedRollbackException: Transaction silently rolled back
    because it has been marked as rollback-only

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatStegTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatStegTest.kt
@@ -6,7 +6,6 @@ import io.mockk.mockk
 import io.mockk.runs
 import no.nav.familie.ba.sak.common.Feil
 import no.nav.familie.ba.sak.common.FunksjonellFeil
-import no.nav.familie.ba.sak.common.SatsendringAndelFeil
 import no.nav.familie.ba.sak.common.førsteDagIInneværendeMåned
 import no.nav.familie.ba.sak.common.lagAndelTilkjentYtelse
 import no.nav.familie.ba.sak.common.lagBehandling
@@ -390,7 +389,7 @@ class BehandlingsresultatStegTest {
             barn = listOf(barn),
         )
 
-        assertThatThrownBy { behandlingsresultatSteg.preValiderSteg(behandling) }.isInstanceOf(SatsendringAndelFeil::class.java)
+        assertThatThrownBy { behandlingsresultatSteg.preValiderSteg(behandling) }.isInstanceOf(Feil::class.java)
             .hasMessage("Satsendring kan ikke endre på prosenten til en andel")
     }
 
