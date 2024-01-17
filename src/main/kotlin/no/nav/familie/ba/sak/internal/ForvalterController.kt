f43bfd81dc12484be6b660695f8a951d1fa960bc
halvorbmundal <halvorbmundal@users.noreply.github.com>
2024-01-17 16:33:00 +0100

    Legg til endepunkt for å rekjøre satsendringer i swagger (#4314)

--- src/main/kotlin/no/nav/familie/ba/sak/internal/ForvalterController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/internal/ForvalterController.kt
@@ -9,6 +9,7 @@ import no.nav.familie.ba.sak.integrasjoner.ecb.ECBService
 import no.nav.familie.ba.sak.integrasjoner.familieintegrasjoner.IntegrasjonClient
 import no.nav.familie.ba.sak.integrasjoner.oppgave.domene.OppgaveRepository
 import no.nav.familie.ba.sak.integrasjoner.økonomi.ØkonomiService
+import no.nav.familie.ba.sak.kjerne.autovedtak.satsendring.domene.SatskjøringRepository
 import no.nav.familie.ba.sak.kjerne.autovedtak.småbarnstillegg.RestartAvSmåbarnstilleggService
 import no.nav.familie.ba.sak.kjerne.steg.BehandlerRolle
 import no.nav.familie.ba.sak.sikkerhet.TilgangService
@@ -50,6 +51,7 @@ class ForvalterController(
     private val økonomiService: ØkonomiService,
     private val opprettTaskService: OpprettTaskService,
     private val taskService: TaskService,
+    private val satskjøringRepository: SatskjøringRepository,
 ) {
     private val logger: Logger = LoggerFactory.getLogger(ForvalterController::class.java)
 
@@ -249,6 +251,22 @@ class ForvalterController(
         val task = taskService.findById(taskId)
         if (task.type != GrensesnittavstemMotOppdrag.TASK_STEP_TYPE) error("sisteTaskId må være av typen ${GrensesnittavstemMotOppdrag.TASK_STEP_TYPE}")
         opprettTaskService.opprettGrensesnittavstemMotOppdragTask(GrensesnittavstemMotOppdrag.nesteAvstemmingDTO(task.triggerTid.toLocalDate()))
+        return ResponseEntity.ok("Ok")
+    }
+
+    @PostMapping("/satsendringer/{satstid}/feiltype/{feiltype}/rekjør")
+    @Operation(
+        summary = "Rekjør satsendringer med feiltype lik feiltypen som er sendt inn",
+        description =
+            "Dette endepunktet sletter alle rader fra Satskjøring der ferdigtid ikke er satt og med feiltypen som er sendt inn. " +
+                "Det gjør at satsendringen kjøres på nytt på fagsaken.",
+    )
+    fun rekjørSatsendringMedFeiltype(
+        @PathVariable satstid: YearMonth,
+        @PathVariable feiltype: String,
+    ): ResponseEntity<String> {
+        val satskjøringerSomSkalRekjøres = satskjøringRepository.finnPåFeilTypeOgFerdigTidIkkeNull(feiltype, satstid)
+        satskjøringRepository.deleteAll(satskjøringerSomSkalRekjøres)
         return ResponseEntity.ok("Ok")
     }
 }
