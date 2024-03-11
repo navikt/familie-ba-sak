085816b63637e21ad940744be0864f6aa91721aa
halvor <halvorbmundal@gmail.com>
2024-03-11 12:13:30 +0100

    Ktlint

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/eøs/valutakurs/ValutakursController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/eøs/valutakurs/ValutakursController.kt
@@ -15,7 +15,6 @@ import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
