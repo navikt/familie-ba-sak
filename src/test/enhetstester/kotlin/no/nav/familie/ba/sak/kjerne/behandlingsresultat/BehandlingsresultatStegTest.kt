fbfa1799054a727eeb42af0ee1d1716a46e14100
bragejahren <brage.ekroll.jahren@nav.no>
2024-03-13 13:22:55 +0100

    Legger til og justerer på tester

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatStegTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatStegTest.kt
@@ -14,7 +14,9 @@ import no.nav.familie.ba.sak.common.lagInitiellTilkjentYtelse
 import no.nav.familie.ba.sak.common.lagPerson
 import no.nav.familie.ba.sak.common.lagVedtak
 import no.nav.familie.ba.sak.common.lagVilkårsvurdering
+import no.nav.familie.ba.sak.common.randomAktør
 import no.nav.familie.ba.sak.common.tilPersonEnkel
+import no.nav.familie.ba.sak.common.toYearMonth
 import no.nav.familie.ba.sak.kjerne.autovedtak.fødselshendelse.Resultat
 import no.nav.familie.ba.sak.kjerne.behandling.BehandlingHentOgPersisterService
 import no.nav.familie.ba.sak.kjerne.behandling.BehandlingService
@@ -29,6 +31,7 @@ import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelseReposito
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelerTilkjentYtelseOgEndreteUtbetalingerService
 import no.nav.familie.ba.sak.kjerne.beregning.domene.SatsType
 import no.nav.familie.ba.sak.kjerne.beregning.domene.TilkjentYtelse
+import no.nav.familie.ba.sak.kjerne.eøs.differanseberegning.domene.Intervall
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.lagUtenlandskPeriodebeløp
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.lagValutakurs
 import no.nav.familie.ba.sak.kjerne.eøs.utenlandskperiodebeløp.UtenlandskPeriodebeløpRepository
@@ -57,6 +60,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import java.math.BigDecimal
+import java.time.LocalDate
 import java.time.YearMonth
 
 class BehandlingsresultatStegTest {
@@ -178,7 +182,7 @@ class BehandlingsresultatStegTest {
     }
 
     @Test
-    fun `skal kaste exception dersom det finnes utenlandsperiodebeløp eller valutakurser som ikke er fylt ut`() {
+    fun `skal kaste exception dersom det finnes utenlandsperiodebeløp som ikke er fylt ut`() {
         every { mockBehandlingsresultatService.utledBehandlingsresultat(any()) } returns Behandlingsresultat.FORTSATT_INNVILGET
 
         every {
@@ -196,6 +200,52 @@ class BehandlingsresultatStegTest {
             "Kan ikke fullføre behandlingsresultat-steg før utenlandsk periodebeløp og valutakurs er fylt ut for alle barn og perioder",
             exception.message,
         )
+    }
+
+    @Test
+    fun `skal kaste exception dersom det finnes valutakurser som ikke er fylt ut`() {
+        every { mockBehandlingsresultatService.utledBehandlingsresultat(any()) } returns Behandlingsresultat.FORTSATT_INNVILGET
+
+        every {
+            behandlingService.oppdaterBehandlingsresultat(
+                any(),
+                any(),
+            )
+        } returns behandling.copy(resultat = Behandlingsresultat.FORTSATT_INNVILGET)
+
+        every { utenlandskPeriodebeløpRepository.finnFraBehandlingId(any()) } returns listOf(lagUtenlandskPeriodebeløp(fom = LocalDate.now().toYearMonth(), beløp = BigDecimal.valueOf(100), intervall = Intervall.MÅNEDLIG, valutakode = "SEK", utbetalingsland = "S", barnAktører = setOf(randomAktør())))
+        every { valutakursRepository.finnFraBehandlingId(any()) } returns listOf(lagValutakurs())
+
+        val exception = assertThrows<FunksjonellFeil> { behandlingsresultatSteg.utførStegOgAngiNeste(behandling, "") }
+        assertEquals(
+            "Kan ikke fullføre behandlingsresultat-steg før utenlandsk periodebeløp og valutakurs er fylt ut for alle barn og perioder",
+            exception.message,
+        )
+    }
+
+    @Test
+    fun `skal ikke kaste exception dersom upb og valutakurser er utfylt`() {
+        every { mockBehandlingsresultatService.utledBehandlingsresultat(any()) } returns Behandlingsresultat.FORTSATT_INNVILGET
+
+        every {
+            behandlingService.oppdaterBehandlingsresultat(
+                any(),
+                any(),
+            )
+        } returns behandling.copy(resultat = Behandlingsresultat.FORTSATT_INNVILGET)
+
+        every { utenlandskPeriodebeløpRepository.finnFraBehandlingId(any()) } returns listOf(lagUtenlandskPeriodebeløp(fom = LocalDate.now().toYearMonth(), beløp = BigDecimal.valueOf(100), intervall = Intervall.MÅNEDLIG, valutakode = "SEK", utbetalingsland = "S", barnAktører = setOf(randomAktør())))
+        every { valutakursRepository.finnFraBehandlingId(any()) } returns listOf(lagValutakurs(fom = LocalDate.now().toYearMonth(), valutakode = "SEK", valutakursdato = LocalDate.now(), kurs = BigDecimal.valueOf(1.2), barnAktører = setOf(randomAktør())))
+
+        every { beregningService.hentEndringerIUtbetalingFraForrigeBehandlingSendtTilØkonomi(any()) } returns EndringerIUtbetalingForBehandlingSteg.ENDRING_I_UTBETALING
+
+        every { behandlingHentOgPersisterService.hentForrigeBehandlingSomErIverksatt(any()) } returns lagBehandling()
+
+        every { beregningService.kanAutomatiskIverksetteSmåbarnstilleggEndring(any(), any()) } returns true
+
+        every { behandlingService.oppdaterStatusPåBehandling(any(), any()) } returns lagBehandling()
+
+        assertDoesNotThrow { behandlingsresultatSteg.utførStegOgAngiNeste(behandling, "") }
     }
 
     @Test
