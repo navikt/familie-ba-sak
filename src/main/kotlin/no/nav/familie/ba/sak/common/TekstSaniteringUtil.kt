da4800894cdb34eff05ee22899aa641486e945c3
Stig Strøm <stig.helge.strom@nav.no>
2024-04-03 15:04:42 +0200

    Tillater kolon i ident, siden journalposter med EESSI kommer inn med kolon i ident (#4465)
    ### 💰 Hva skal gjøres, og hvorfor?
    Journalføring feiler nå ved mottak av EESSI dokumenter sendt inn av et
    firma. ID på avsendre vil være på et format som PL:PL21212120WR, som
    henviser til et utenlandsk orgnr. Myker opp valideringen til å også
    godta kolon
    
    ### 🔎️ Er det noe spesielt du ønsker tilbakemelding om?
    _Er det noe du er usikker på eller ønsker å diskutere? Beskriv det
    gjerne her eller kommenter koden det gjelder._
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [x] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [x] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    Testet også lokalt
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [ ] Nei

--- src/main/kotlin/no/nav/familie/ba/sak/common/TekstSaniteringUtil.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/common/TekstSaniteringUtil.kt
@@ -5,3 +5,5 @@ const val ALFANUMERISKE_TEGN = "a-zæøåA-ZÆØÅ0-9"
 fun String.saner(): String = Regex("[^$ALFANUMERISKE_TEGN]*").replace(this, "")
 
 fun String.erAlfanummerisk(): Boolean = Regex("[$ALFANUMERISKE_TEGN]*").matches(this)
+
+fun String.erAlfanummeriskPlussKolon(): Boolean = Regex("[$ALFANUMERISKE_TEGN:]*").matches(this)
