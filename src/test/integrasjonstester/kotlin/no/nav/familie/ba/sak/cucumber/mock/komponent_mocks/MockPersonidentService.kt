ea9b3df63a3f8ea5478b4a0eb2610cdf39993b82
halvor <halvorbmundal@gmail.com>
2024-04-03 13:38:33 +0200

    tyrkleif

--- src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/cucumber/mock/komponent_mocks/MockPersonidentService.kt
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/cucumber/mock/komponent_mocks/MockPersonidentService.kt
@@ -7,16 +7,16 @@ import no.nav.familie.ba.sak.kjerne.personident.PersonidentService
 
 fun mockPersonidentService(
     dataFraCucumber: BegrunnelseTeksterStepDefinition,
-    nyBehanldingId: Long,
+    nyBehandlingId: Long,
 ): PersonidentService {
     val personidentService = mockk<PersonidentService>()
     every { personidentService.hentOgLagreAktør(any(), any()) } answers {
         val personIdent = firstArg<String>()
-        dataFraCucumber.persongrunnlag[nyBehanldingId]!!.personer.single { it.aktør.aktivFødselsnummer() == personIdent }.aktør
+        dataFraCucumber.persongrunnlag[nyBehandlingId]!!.personer.single { it.aktør.aktivFødselsnummer() == personIdent }.aktør
     }
     every { personidentService.hentAktør(any()) } answers {
         val personIdent = firstArg<String>()
-        dataFraCucumber.persongrunnlag[nyBehanldingId]!!.personer.single { it.aktør.aktivFødselsnummer() == personIdent }.aktør
+        dataFraCucumber.persongrunnlag[nyBehandlingId]!!.personer.single { it.aktør.aktivFødselsnummer() == personIdent }.aktør
     }
     return personidentService
 }
