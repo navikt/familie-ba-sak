0dfd3adcd3a2004fffafa9dc0df31c61e33a244c
halvorbmundal <halvorbmundal@users.noreply.github.com>
2024-04-03 13:46:20 +0200

    Splitt opp cucumbermock (#4464)
    ### 💰 Hva skal gjøres, og hvorfor?
    I
    https://github.com/navikt/familie-ba-sak/pull/4433#discussion_r1549081288
    ble vi enige om å splitte opp CucumberMock-filen.
    
    Endrer så alle komponenter som blir mocket i cucumberMock flyttes ut til
    egne filer.

---
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/cucumber/mock/komponent_mocks/MockKompetanseRepository.kt
@@ -0,0 +1,29 @@
+﻿package no.nav.familie.ba.sak.cucumber.mock
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.familie.ba.sak.cucumber.BegrunnelseTeksterStepDefinition
+import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.KompetanseRepository
+import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.Kompetanse
+
+fun mockKompetanseRepository(dataFraCucumber: BegrunnelseTeksterStepDefinition): KompetanseRepository {
+    val kompetanseRepository = mockk<KompetanseRepository>()
+    every { kompetanseRepository.finnFraBehandlingId(any()) } answers {
+        val behandlingId = firstArg<Long>()
+        dataFraCucumber.kompetanser[behandlingId] ?: emptyList()
+    }
+    every { kompetanseRepository.deleteAll(any<Iterable<Kompetanse>>()) } answers {
+        val kompetanser = firstArg<Iterable<Kompetanse>>()
+        kompetanser.forEach {
+            dataFraCucumber.kompetanser[it.behandlingId] = dataFraCucumber.kompetanser[it.behandlingId]?.filter { kompetanse -> kompetanse != it } ?: emptyList()
+        }
+    }
+    every { kompetanseRepository.saveAll(any<Iterable<Kompetanse>>()) } answers {
+        val kompetanser = firstArg<Iterable<Kompetanse>>()
+        kompetanser.forEach {
+            dataFraCucumber.kompetanser[it.behandlingId] = (dataFraCucumber.kompetanser[it.behandlingId] ?: emptyList()) + it
+        }
+        kompetanser.toMutableList()
+    }
+    return kompetanseRepository
+}
---
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/cucumber/mock/komponent_mocks/MockSøknadGrunnlagService.kt
@@ -0,0 +1,21 @@
+﻿package no.nav.familie.ba.sak.cucumber.mock
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.familie.ba.sak.cucumber.BegrunnelseTeksterStepDefinition
+import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingÅrsak
+import no.nav.familie.ba.sak.kjerne.grunnlag.søknad.SøknadGrunnlagService
+
+fun mockSøknadGrunnlagService(dataFraCucumber: BegrunnelseTeksterStepDefinition): SøknadGrunnlagService {
+    val søknadGrunnlagService = mockk<SøknadGrunnlagService>()
+    every { søknadGrunnlagService.hentAktiv(any()) } answers {
+        val behandlingId = firstArg<Long>()
+        val behandling = dataFraCucumber.behandlinger[behandlingId]!!
+        if (behandling.opprettetÅrsak == BehandlingÅrsak.SØKNAD) {
+            error("Ikke implementert")
+        } else {
+            null
+        }
+    }
+    return søknadGrunnlagService
+}
