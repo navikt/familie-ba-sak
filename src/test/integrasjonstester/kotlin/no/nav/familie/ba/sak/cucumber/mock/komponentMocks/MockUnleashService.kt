e5914b483fb8dc1b4f02cf62e5739b21cf2be966
halvor <halvorbmundal@gmail.com>
2024-04-18 12:24:19 +0200

    Rename mappe komponent_mock til  koponentMock

---
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/cucumber/mock/komponentMocks/MockUnleashService.kt
@@ -0,0 +1,11 @@
+﻿package no.nav.familie.ba.sak.cucumber.mock.komponentMocks
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.familie.unleash.UnleashService
+
+fun mockUnleashService(): UnleashService {
+    val unleashService = mockk<UnleashService>()
+    every { unleashService.isEnabled(any()) } returns true
+    return unleashService
+}
