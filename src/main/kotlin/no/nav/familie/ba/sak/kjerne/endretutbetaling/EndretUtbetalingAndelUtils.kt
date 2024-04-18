14c45556e4cb976773ec998d7b567dc3c24ca0fa
Ida Merete Enholm <ida.merete.enholm@nav.no>
2024-04-18 12:56:31 +0200

    slett div kode som ikke brukes

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/endretutbetaling/EndretUtbetalingAndelUtils.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/endretutbetaling/EndretUtbetalingAndelUtils.kt
@@ -1,14 +1,8 @@
 package no.nav.familie.ba.sak.kjerne.endretutbetaling
 
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelse
-import no.nav.familie.ba.sak.kjerne.beregning.domene.YtelseType
 import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.EndretUtbetalingAndel
 import java.time.YearMonth
-
-fun erStartPåUtvidetSammeMåned(
-    andelTilkjentYtelser: List<AndelTilkjentYtelse>,
-    fom: YearMonth?,
-) = andelTilkjentYtelser.any { it.stønadFom == fom && it.type == YtelseType.UTVIDET_BARNETRYGD }
 
 fun beregnGyldigTomIFremtiden(
     andreEndredeAndelerPåBehandling: List<EndretUtbetalingAndel>,
