83ef62605afef5a194d94459e1090271ba22b8a1
halvor <halvorbmundal@gmail.com>
2024-04-22 10:00:52 +0200

    Fiks test

--- src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/cucumber/mock/komponentMocks/MockSimuleringService.kt
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/cucumber/mock/komponentMocks/MockSimuleringService.kt
@@ -7,5 +7,6 @@ import no.nav.familie.ba.sak.kjerne.simulering.SimuleringService
 fun mockSimuleringService(): SimuleringService {
     val simuleringService = mockk<SimuleringService>()
     every { simuleringService.oppdaterSimuleringPåBehandling(any()) } returns emptyList()
+    every { simuleringService.hentSimuleringPåBehandling(any()) } returns emptyList()
     return simuleringService
 }
