e22ad6926239d1d1f7559560602c157642525277
fredrikpf <fredrik.pfeil@gmail.com>
2024-04-24 15:38:43 +0200

    hent behandlinger som er avsluttet og aktive i stedet for for iverksatte

--- src/main/kotlin/no/nav/familie/ba/sak/task/OppdaterStønadTomPåTilkjentYtelseTask.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/task/OppdaterStønadTomPåTilkjentYtelseTask.kt
@@ -3,6 +3,7 @@ package no.nav.familie.ba.sak.task
 import no.nav.familie.ba.sak.common.Feil
 import no.nav.familie.ba.sak.integrasjoner.økonomi.utledStønadTom
 import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingRepository
+import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingStatus
 import no.nav.familie.ba.sak.kjerne.beregning.domene.TilkjentYtelseRepository
 import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.EndretUtbetalingAndelRepository
 import no.nav.familie.ba.sak.kjerne.fagsak.FagsakStatus
@@ -31,8 +32,9 @@ class OppdaterStønadTomPåTilkjentYtelseTask(
         logger.info("Oppdaterer stønadTom for TilkjentYtelse på fagsak $fagsakId")
 
         val behandling =
-            behandlingRepository.finnSisteIverksatteBehandling(fagsakId)
-                ?: throw Feil("Finner ingen behandling på fagsak $fagsakId")
+            behandlingRepository.finnBehandlinger(fagsakId)
+                .filter { it.status == BehandlingStatus.AVSLUTTET && it.aktiv }
+                .singleOrNull() ?: throw Feil("Finner ingen behandling på fagsak $fagsakId")
 
         if (behandling.fagsak.status != FagsakStatus.LØPENDE) {
             throw Feil("Prøvde å oppdatere stønadTom på en tilkjentYtelse på en fagsak som ikke er løpende. ")
