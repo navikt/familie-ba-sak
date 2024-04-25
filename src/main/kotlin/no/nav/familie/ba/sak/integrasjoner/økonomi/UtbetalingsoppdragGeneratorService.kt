3ee523e5c1a28750cffa918b2ae1eb0d761c96d3
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2024-04-25 16:02:46 +0200

    resterende behandlinger med feil tilkjent ytelse tom (#4517)
    ### 💰 Hva skal gjøres, og hvorfor?
    
    [Favro](https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-19991)
    
    Når vi finner behandlinger for å sjekke om stønadTom burde settes
    tidligere så vi tidligere på behandlinger som er iverksatt. Det blir
    feil når vi har fagsaker uten iverksatte behandlinger eller når
    behandlingen vi vil ente stønadTom fra ikke er iverksatt. Bytter derfor
    til å se etter siste behandling som er avsluttet i stedet.

--- src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/økonomi/UtbetalingsoppdragGeneratorService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/økonomi/UtbetalingsoppdragGeneratorService.kt
@@ -7,13 +7,14 @@ import no.nav.familie.ba.sak.kjerne.behandling.BehandlingHentOgPersisterService
 import no.nav.familie.ba.sak.kjerne.behandling.BehandlingService
 import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandling
 import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingType
+import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelse
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelseRepository
 import no.nav.familie.ba.sak.kjerne.beregning.domene.TilkjentYtelse
 import no.nav.familie.ba.sak.kjerne.beregning.domene.TilkjentYtelseRepository
 import no.nav.familie.ba.sak.kjerne.beregning.domene.tilAndelerTilkjentYtelseMedEndreteUtbetalinger
 import no.nav.familie.ba.sak.kjerne.endretutbetaling.EndretUtbetalingAndelHentOgPersisterService
 import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.EndretUtbetalingAndel
-import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.Årsak
+import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.førerTilOpphør
 import no.nav.familie.ba.sak.kjerne.fagsak.Fagsak
 import no.nav.familie.ba.sak.kjerne.vedtak.Vedtak
 import no.nav.familie.felles.utbetalingsgenerator.domain.AndelMedPeriodeIdLongId
@@ -22,7 +23,6 @@ import no.nav.familie.felles.utbetalingsgenerator.domain.IdentOgType
 import no.nav.familie.kontrakter.felles.objectMapper
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.YearMonth
 
@@ -142,23 +142,16 @@ private fun utledOpphør(
 }
 
 fun utledStønadTom(
-    tilkjentYtelse: TilkjentYtelse,
+    andelerTilkjentYtelse: Set<AndelTilkjentYtelse>,
     endretUtbetalingAndeler: List<EndretUtbetalingAndel>,
 ): YearMonth? {
-    val andelerMedEndringer = tilkjentYtelse.andelerTilkjentYtelse.tilAndelerTilkjentYtelseMedEndreteUtbetalinger(endretUtbetalingAndeler)
+    val andelerMedEndringer = andelerTilkjentYtelse.tilAndelerTilkjentYtelseMedEndreteUtbetalinger(endretUtbetalingAndeler)
 
     val andelerMedRelevantUtbetaling =
-        andelerMedEndringer.filterNot { it ->
-            val harEndringSomFørerTilOpphørVed0Prosent =
-                it.endreteUtbetalinger.any { endretUtbetaling ->
-                    endretUtbetaling.årsak in
-                        listOf(
-                            Årsak.ALLEREDE_UTBETALT,
-                            Årsak.ENDRE_MOTTAKER,
-                            Årsak.ETTERBETALING_3ÅR,
-                        )
-                }
-            harEndringSomFørerTilOpphørVed0Prosent && it.prosent == BigDecimal.ZERO
+        andelerMedEndringer.filterNot { andelTilkjentYtelseMedEndreteUtbetalinger ->
+            andelTilkjentYtelseMedEndreteUtbetalinger.endreteUtbetalinger.any { endretUtbetaling ->
+                endretUtbetaling.førerTilOpphør()
+            }
         }
 
     return andelerMedRelevantUtbetaling.maxOfOrNull { it.stønadTom }
@@ -172,7 +165,7 @@ fun oppdaterTilkjentYtelseMedUtbetalingsoppdrag(
     val opphør = utledOpphør(utbetalingsoppdrag, tilkjentYtelse.behandling)
 
     tilkjentYtelse.utbetalingsoppdrag = objectMapper.writeValueAsString(utbetalingsoppdrag)
-    tilkjentYtelse.stønadTom = utledStønadTom(tilkjentYtelse, endretUtbetalingAndeler)
+    tilkjentYtelse.stønadTom = utledStønadTom(tilkjentYtelse.andelerTilkjentYtelse, endretUtbetalingAndeler)
     tilkjentYtelse.stønadFom =
         if (opphør.erRentOpphør) null else tilkjentYtelse.andelerTilkjentYtelse.minOfOrNull { it.stønadFom }
     tilkjentYtelse.endretDato = LocalDate.now()
