afb598ae1fe2216bbc938d88a1f4b548370d6a69
Tor Nærland <47184872+tnarland@users.noreply.github.com>
2024-05-06 12:50:46 +0200

    Redusere testers avhengigheter til mockk-objekter kontrollert av ClientMocks (#4536)
    ### 💰 Hva skal gjøres, og hvorfor?
    Begynnelsen på noen av de tingene diskutert på gårsdagens hackathon ang.
    ClientMocks.
    
    Starter med å gjøre om to av de dynamiske mockk-konfigurasjonene
    (`UnleashService` og `PdlIdentRestClient`) som har bortkastet resetting
    av mock-verdier mellom hver test, til statiske test-implementasjoner
    
    ### 🔎️ Er det noe spesielt du ønsker tilbakemelding om?
    _Er det noe du er usikker på eller ønsker å diskutere? Beskriv det
    gjerne her eller kommenter koden det gjelder._
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [ ] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    _Jeg har ikke skrevet tester fordi:_
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [ ] Nei

---
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/config/MockPdlIdentRestClient.kt
@@ -0,0 +1,45 @@
+package no.nav.familie.ba.sak.config
+
+import no.nav.familie.ba.sak.common.randomFnr
+import no.nav.familie.ba.sak.integrasjoner.pdl.PdlIdentRestClient
+import no.nav.familie.ba.sak.integrasjoner.pdl.domene.IdentInformasjon
+import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestOperations
+import java.lang.Integer.min
+import java.net.URI
+
+@Service
+@Profile("mock-ident-client")
+@Primary
+class MockPdlIdentRestClient(
+    restOperations: RestOperations,
+) : PdlIdentRestClient(URI("dummy_uri"), restOperations) {
+    override fun hentIdenter(
+        personIdent: String,
+        historikk: Boolean,
+    ): List<IdentInformasjon> {
+        return when {
+            historikk ->
+                listOf(
+                    IdentInformasjon(personIdent, historisk = false, gruppe = "FOLKEREGISTERIDENT"),
+                    IdentInformasjon(randomFnr(), historisk = true, gruppe = "FOLKEREGISTERIDENT"),
+                )
+
+            else ->
+                listOf(
+                    IdentInformasjon(
+                        ident = personIdent.substring(0, min(11, personIdent.length)),
+                        historisk = false,
+                        gruppe = "FOLKEREGISTERIDENT",
+                    ),
+                    IdentInformasjon(
+                        ident = personIdent.substring(0, min(11, personIdent.length)) + "00",
+                        historisk = false,
+                        gruppe = "AKTORID",
+                    ),
+                )
+        }
+    }
+}
