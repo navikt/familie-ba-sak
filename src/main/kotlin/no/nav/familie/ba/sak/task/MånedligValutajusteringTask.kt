6f3644a659f6a5ca750e7beaa0c21360bb3cb4c7
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-06-05 14:56:51 +0200

    Gi egen callId til tasker som oppdaterer valutakurs for en fagsak

--- src/main/kotlin/no/nav/familie/ba/sak/task/MånedligValutajusteringTask.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/task/MånedligValutajusteringTask.kt
@@ -1,12 +1,15 @@
 package no.nav.familie.ba.sak.task
 
 import no.nav.familie.ba.sak.kjerne.autovedtak.månedligvalutajustering.AutovedtakMånedligValutajusteringService
+import no.nav.familie.ba.sak.task.OpprettTaskService.Companion.overstyrTaskMedNyCallId
 import no.nav.familie.kontrakter.felles.objectMapper
 import no.nav.familie.log.IdUtils
+import no.nav.familie.log.mdc.MDCConstants
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
 import org.slf4j.LoggerFactory
+import org.slf4j.MDC
 import org.springframework.stereotype.Service
 import java.time.YearMonth
 
@@ -45,15 +48,17 @@ class MånedligValutajusteringTask(
             fagsakId: Long,
             valutajusteringsMåned: YearMonth,
         ): Task =
-            Task(
-                type = TASK_STEP_TYPE,
-                payload = objectMapper.writeValueAsString(MånedligValutajusteringTaskDto(fagsakId = fagsakId, måned = valutajusteringsMåned)),
-                properties =
-                    mapOf(
-                        "fagsakId" to fagsakId.toString(),
-                        "måned" to valutajusteringsMåned.toString(),
-                        "callId" to IdUtils.generateId(),
-                    ).toProperties(),
-            )
+            overstyrTaskMedNyCallId(IdUtils.generateId()) {
+                Task(
+                    type = TASK_STEP_TYPE,
+                    payload = objectMapper.writeValueAsString(MånedligValutajusteringTaskDto(fagsakId = fagsakId, måned = valutajusteringsMåned)),
+                    properties =
+                        mapOf(
+                            "fagsakId" to fagsakId.toString(),
+                            "måned" to valutajusteringsMåned.toString(),
+                            "callId" to (MDC.get(MDCConstants.MDC_CALL_ID) ?: IdUtils.generateId()),
+                        ).toProperties(),
+                )
+            }
     }
 }
