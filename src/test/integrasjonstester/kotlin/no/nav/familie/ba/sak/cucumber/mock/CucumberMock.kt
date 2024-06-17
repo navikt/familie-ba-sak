5fa1fcdb8729757e8a0bdddb787bac14fec73913
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-06-17 13:32:15 +0200

    Oppdater mock

--- src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/cucumber/mock/CucumberMock.kt
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/cucumber/mock/CucumberMock.kt
@@ -452,6 +452,7 @@ class CucumberMock(
             valutakursRepository = valutakursRepository,
             andelTilkjentYtelseRepository = andelTilkjentYtelseRepository,
             simuleringService = simuleringService,
+            tilbakekrevingService = tilbakekrevingService,
         )
 
     val stegService =
