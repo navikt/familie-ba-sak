19b0be47f0b099da6955c4010c5c2ec2726d3be0
halvor <halvorbmundal@gmail.com>
2024-06-19 13:20:40 +0200

    Opprett eget objekt som inneholder endring i andel og flytt logging av endringer i egen funksjon

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatValideringUtils.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatValideringUtils.kt
@@ -8,9 +8,12 @@ import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingType
 import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandlingsresultat
 import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingÅrsak
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelse
-import no.nav.familie.ba.sak.kjerne.beregning.domene.tilTidslinjerPerAktørOgType
+import no.nav.familie.ba.sak.kjerne.beregning.domene.YtelseType
+import no.nav.familie.ba.sak.kjerne.beregning.domene.tilTidslinjerPerPersonOgType
 import no.nav.familie.ba.sak.kjerne.personident.Aktør
+import no.nav.familie.ba.sak.kjerne.tidslinje.Tidslinje
 import no.nav.familie.ba.sak.kjerne.tidslinje.komposisjon.outerJoin
+import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.Måned
 import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.MånedTidspunkt.Companion.tilTidspunkt
 import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.tilYearMonth
 import no.nav.familie.ba.sak.kjerne.tidslinje.transformasjon.beskjærTilOgMed
@@ -76,67 +79,58 @@ object BehandlingsresultatValideringUtils {
         nåMåned: YearMonth,
     ) {
         val forrigeMåned = nåMåned.minusMonths(1)
-        val andelerIFortidenTidslinje = andelerDenneBehandlingen.tilTidslinjerPerAktørOgType().beskjærTilOgMed(forrigeMåned.tilTidspunkt())
-        val andelerIFortidenForrigeBehanldingTidslinje = andelerForrigeBehandling.tilTidslinjerPerAktørOgType().beskjærTilOgMed(forrigeMåned.tilTidspunkt())
+        val andelerIFortidenTidslinje = andelerDenneBehandlingen.tilTidslinjerPerPersonOgType().beskjærTilOgMed(forrigeMåned.tilTidspunkt())
+        val andelerIFortidenForrigeBehanldingTidslinje = andelerForrigeBehandling.tilTidslinjerPerPersonOgType().beskjærTilOgMed(forrigeMåned.tilTidspunkt())
 
-        val erEndringTilbakeITidTidslinjer =
+        val endringerIAndelerTilbakeITidTidslinjer =
             andelerIFortidenTidslinje.outerJoin(andelerIFortidenForrigeBehanldingTidslinje) { nyAndel, gammelAndel ->
-                nyAndel?.kalkulertUtbetalingsbeløp != gammelAndel?.kalkulertUtbetalingsbeløp
-            }
-
-        erEndringTilbakeITidTidslinjer.forEach { _, erEndringTilbakeITidTidslinje ->
-            erEndringTilbakeITidTidslinje.perioder().forEach {
-                val erEndring = it.innhold == true
-                if (erEndring) {
-                    secureLogger.info(
-                        "Er endring i kalkulert utbetalt beløp i perioden ${it.fraOgMed.tilYearMonth()} til ${it.tilOgMed.tilYearMonth()}.\n" +
-                            "Andeler denne behandlingen: ${
-                                andelerDenneBehandlingen.sortedWith(
-                                    compareBy(
-                                        { it.aktør.aktørId },
-                                        { it.stønadFom },
-                                    ),
-                                )
-                            }\n" +
-                            "Andeler forrige behandling: ${
-                                andelerForrigeBehandling.sortedWith(
-                                    compareBy(
-                                        { it.aktør.aktørId },
-                                        { it.stønadFom },
-                                    ),
-                                )
-                            }",
-                    )
-                    throw Feil("Det er endringer i kalkulert utbetalt beløp som går tilbake i tid. Gjelder andelene fra ${it.fraOgMed.tilYearMonth()} til ${it.tilOgMed.tilYearMonth()}. Se secure log for mer detaljer.")
+                if (nyAndel?.kalkulertUtbetalingsbeløp != gammelAndel?.kalkulertUtbetalingsbeløp) {
+                    ErEndringIAndel(gammelAndel, nyAndel)
+                } else {
+                    IngenEndringIAndel
                 }
             }
-        }
+
+        endringerIAndelerTilbakeITidTidslinjer.kastFeilOgLoggVedEndringerIAndeler()
     }
 
     fun validerSatsErUendret(
         andelerDenneBehandlingen: List<AndelTilkjentYtelse>,
         andelerForrigeBehandling: List<AndelTilkjentYtelse>,
     ) {
-        val andelerDenneBehandlingTidslinje = andelerDenneBehandlingen.tilTidslinjerPerAktørOgType()
-        val andelerForrigeBehanldingTidslinje = andelerForrigeBehandling.tilTidslinjerPerAktørOgType()
+        val andelerDenneBehandlingTidslinje = andelerDenneBehandlingen.tilTidslinjerPerPersonOgType()
+        val andelerForrigeBehanldingTidslinje = andelerForrigeBehandling.tilTidslinjerPerPersonOgType()
 
-        val erEndringISatsTidslinjer =
+        val endringISatsTidslinjer =
             andelerDenneBehandlingTidslinje.outerJoin(andelerForrigeBehanldingTidslinje) { nyAndel, gammelAndel ->
-                nyAndel?.sats != gammelAndel?.sats
+                if (nyAndel?.sats != gammelAndel?.sats) {
+                    ErEndringIAndel(gammelAndel, nyAndel)
+                } else {
+                    IngenEndringIAndel
+                }
             }
 
-        erEndringISatsTidslinjer.forEach { _, erEndringISatsTidslinje ->
-            erEndringISatsTidslinje.perioder().forEach {
-                val erEndring = it.innhold == true
-                if (erEndring) {
+        endringISatsTidslinjer.kastFeilOgLoggVedEndringerIAndeler()
+    }
+
+    private fun Map<Pair<Aktør, YtelseType>, Tidslinje<EndringIAndel, Måned>>.kastFeilOgLoggVedEndringerIAndeler() {
+        this.forEach { _, endringIAndelTidslinje ->
+            endringIAndelTidslinje.perioder().forEach {
+                if (it.innhold is ErEndringIAndel) {
                     secureLogger.info(
-                        "Er endring i sats for andel i perioden ${it.fraOgMed.tilYearMonth()} til ${it.tilOgMed.tilYearMonth()}.\n" +
-                            "Andeler denne behandlingen: $andelerDenneBehandlingen\n" +
-                            "Andeler forrige behandling: $andelerForrigeBehandling",
+                        "Er endring i andel i perioden ${it.fraOgMed.tilYearMonth()} til ${it.tilOgMed.tilYearMonth()}.\n" +
+                            "Andel denne behandlingen: ${it.innhold.andelDenneBehandlingen}\n" +
+                            "Andel forrige behandling: ${it.innhold.andelForrigeBehandling}",
                     )
-                    throw Feil("Det er endringer i andelene relatert til sats som går tilbake i tid. Gjelder andelene fra ${it.fraOgMed.tilYearMonth()} til ${it.tilOgMed.tilYearMonth()}. Se secure log for mer detaljer.")
+                    throw Feil("Det er en uforventet endring i andel. Gjelder andel fra ${it.fraOgMed.tilYearMonth()} til ${it.tilOgMed.tilYearMonth()}. Se secure log for mer detaljer.")
                 }
             }
         }
     }
 }
+
+private sealed interface EndringIAndel
+
+private object IngenEndringIAndel : EndringIAndel
+
+private data class ErEndringIAndel(val andelForrigeBehandling: AndelTilkjentYtelse?, val andelDenneBehandlingen: AndelTilkjentYtelse?) : EndringIAndel
