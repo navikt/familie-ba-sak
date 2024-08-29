bc6bed11fd0d1592695759477f0369b28708d57c
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-08-29 09:21:45 +0200

    Test at riktig exception kastes ved forskjellige tilstander

---
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/autovedtak/månedligValutajustering/AutovedtakMånedligValutajusteringServiceTest.kt
@@ -0,0 +1,163 @@
+package no.nav.familie.ba.sak.kjerne.autovedtak.månedligValutajustering
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.familie.ba.sak.common.Feil
+import no.nav.familie.ba.sak.common.LocalDateProvider
+import no.nav.familie.ba.sak.common.defaultFagsak
+import no.nav.familie.ba.sak.common.lagBehandling
+import no.nav.familie.ba.sak.kjerne.autovedtak.månedligvalutajustering.AutovedtakMånedligValutajusteringService
+import no.nav.familie.ba.sak.kjerne.behandling.BehandlingHentOgPersisterService
+import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingStatus
+import no.nav.familie.ba.sak.kjerne.eøs.valutakurs.Valutakurs
+import no.nav.familie.ba.sak.kjerne.eøs.valutakurs.ValutakursService
+import no.nav.familie.ba.sak.kjerne.eøs.valutakurs.Vurderingsform
+import no.nav.familie.ba.sak.kjerne.fagsak.FagsakStatus
+import no.nav.familie.prosessering.error.RekjørSenereException
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.YearMonth
+import java.time.temporal.ChronoUnit
+
+class AutovedtakMånedligValutajusteringServiceTest {
+    val behandlingHentOgPersisterService = mockk<BehandlingHentOgPersisterService>()
+    val valutaKursService = mockk<ValutakursService>()
+    val localDateProvider = mockk<LocalDateProvider>()
+
+    val autovedtakMånedligValutajusteringService =
+        AutovedtakMånedligValutajusteringService(
+            behandlingHentOgPersisterService = behandlingHentOgPersisterService,
+            valutakursService = valutaKursService,
+            localDateProvider = localDateProvider,
+            autovedtakService = mockk(),
+            snikeIKøenService = mockk(),
+            behandlingService = mockk(),
+            simuleringService = mockk(),
+            taskRepository = mockk(),
+        )
+
+    @Test
+    fun `utførMånedligValutajustering kaster Feil hvis en annen enn nåværende måned blir sendt inn`() {
+        every { localDateProvider.now() } returns LocalDate.now()
+
+        assertThrows<Feil> {
+            autovedtakMånedligValutajusteringService.utførMånedligValutajustering(
+                fagsakId = 0,
+                måned = YearMonth.now().minusMonths(1),
+            )
+        }.run {
+            assertThat(message).isEqualTo("Prøver å utføre månedlig valutajustering for en annen måned enn nåværende måned.")
+        }
+    }
+
+    @Test
+    fun `utførMånedligValutajustering kaster IllegalStateException hvis fagsak ikke har en vedtatt behandling`() {
+        every { localDateProvider.now() } returns LocalDate.now()
+        every { behandlingHentOgPersisterService.hentSisteBehandlingSomErVedtatt(any()) } returns null
+
+        assertThrows<IllegalStateException> {
+            autovedtakMånedligValutajusteringService.utførMånedligValutajustering(
+                fagsakId = 0,
+                måned = YearMonth.now(),
+            )
+        }.run {
+            assertThat(message).isEqualTo("Fant ikke siste vedtatte behandling for 0")
+        }
+    }
+
+    @Test
+    fun `utførMånedligValutajustering kaster Feil hvis fagsakstatus ikke er løpende`() {
+        val fagsak = defaultFagsak()
+        val behandling = lagBehandling(fagsak = fagsak)
+
+        every { localDateProvider.now() } returns LocalDate.now()
+        every { behandlingHentOgPersisterService.hentSisteBehandlingSomErVedtatt(any()) } returns behandling
+        every { valutaKursService.hentValutakurser(any()) } returns
+            listOf(
+                Valutakurs(
+                    fom = YearMonth.now().minusMonths(1),
+                    tom = null,
+                    vurderingsform = Vurderingsform.AUTOMATISK,
+                ),
+            )
+
+        assertThrows<Feil> {
+            autovedtakMånedligValutajusteringService.utførMånedligValutajustering(
+                fagsakId = fagsak.id,
+                måned = YearMonth.now(),
+            )
+        }.run {
+            assertThat(message).isEqualTo("Forsøker å utføre satsendring på ikke løpende fagsak ${fagsak.id}")
+        }
+    }
+
+    @Test
+    fun `utførMånedligValutajustering kaster RekjørSenereException hvis åpen behandling har status FATTER_VEDTAK`() {
+        val fagsak = defaultFagsak().apply { status = FagsakStatus.LØPENDE }
+        val behandling = lagBehandling(fagsak = fagsak, status = BehandlingStatus.AVSLUTTET)
+        val åpenBehandling = lagBehandling(fagsak = fagsak, status = BehandlingStatus.FATTER_VEDTAK)
+        val klokkenSeksIMorgen = LocalDate.now().plusDays(1).atTime(6, 0)
+
+        every { localDateProvider.now() } returns LocalDate.now()
+        every { behandlingHentOgPersisterService.hentSisteBehandlingSomErVedtatt(any()) } returns behandling
+        every { behandlingHentOgPersisterService.finnAktivOgÅpenForFagsak(any()) } returns åpenBehandling
+        every { valutaKursService.hentValutakurser(any()) } returns
+            listOf(
+                Valutakurs(
+                    fom = YearMonth.now().minusMonths(1),
+                    tom = null,
+                    vurderingsform = Vurderingsform.AUTOMATISK,
+                ),
+            )
+
+        assertThrows<RekjørSenereException> {
+            autovedtakMånedligValutajusteringService.utførMånedligValutajustering(
+                fagsakId = fagsak.id,
+                måned = YearMonth.now(),
+            )
+        }.run {
+            assertThat(message).startsWith("Rekjører senere")
+            assertThat(årsak).startsWith("Åpen behandling har status FATTER_VEDTAK")
+            assertThat(triggerTid).isEqualTo(klokkenSeksIMorgen)
+        }
+    }
+
+    @ParameterizedTest
+    @EnumSource(BehandlingStatus::class, names = ["SATT_PÅ_MASKINELL_VENT", "IVERKSETTER_VEDTAK"])
+    fun `utførMånedligValutajustering kaster RekjørSenereException hvis åpen behandling har status SATT_PÅ_MASKINELL_VENT eller IVERKSETTER_VEDTAK`(
+        behandlingStatus: BehandlingStatus,
+    ) {
+        val fagsak = defaultFagsak().apply { status = FagsakStatus.LØPENDE }
+        val behandling = lagBehandling(fagsak = fagsak, status = BehandlingStatus.AVSLUTTET)
+        val åpenBehandling = lagBehandling(fagsak = fagsak, status = behandlingStatus)
+        val omEnTime = LocalDateTime.now().plusMinutes(119).truncatedTo(ChronoUnit.HOURS)
+
+        every { localDateProvider.now() } returns LocalDate.now()
+        every { behandlingHentOgPersisterService.hentSisteBehandlingSomErVedtatt(any()) } returns behandling
+        every { behandlingHentOgPersisterService.finnAktivOgÅpenForFagsak(any()) } returns åpenBehandling
+        every { valutaKursService.hentValutakurser(any()) } returns
+            listOf(
+                Valutakurs(
+                    fom = YearMonth.now().minusMonths(1),
+                    tom = null,
+                    vurderingsform = Vurderingsform.AUTOMATISK,
+                ),
+            )
+
+        assertThrows<RekjørSenereException> {
+            autovedtakMånedligValutajusteringService.utførMånedligValutajustering(
+                fagsakId = fagsak.id,
+                måned = YearMonth.now(),
+            )
+        }.run {
+            assertThat(message).startsWith("Rekjører senere")
+            assertThat(årsak).startsWith("Åpen behandling har status $behandlingStatus")
+            assertThat(triggerTid).isEqualTo(omEnTime)
+        }
+    }
+}
