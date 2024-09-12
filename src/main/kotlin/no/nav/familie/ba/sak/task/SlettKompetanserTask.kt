26a675a427268d9d6beff1b48c4138a7c51df59b
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2024-09-12 16:09:38 +0200

    slett alle PeriodeOgBarnSkjema og sett behandling tilbake til vilkårsvurderingssteget
    Signed-off-by: FredrikP <57641333+fredrikpf@users.noreply.github.com>

--- src/main/kotlin/no/nav/familie/ba/sak/task/SlettKompetanserTask.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/task/SlettKompetanserTask.kt
@@ -1,12 +1,10 @@
 package no.nav.familie.ba.sak.task
 
-import no.nav.familie.ba.sak.common.LocalDateProvider
-import no.nav.familie.ba.sak.common.toYearMonth
 import no.nav.familie.ba.sak.kjerne.behandling.BehandlingHentOgPersisterService
 import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingStatus
 import no.nav.familie.ba.sak.kjerne.eøs.felles.BehandlingId
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.KompetanseService
-import no.nav.familie.ba.sak.kjerne.steg.StegType
+import no.nav.familie.ba.sak.kjerne.steg.TilbakestillBehandlingService
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
@@ -22,31 +20,17 @@ import org.springframework.stereotype.Service
 class SlettKompetanserTask(
     private val kompetanseService: KompetanseService,
     private val behandlingHentOgPersisterService: BehandlingHentOgPersisterService,
-    private val localDateProvider: LocalDateProvider,
+    private val tilbakestillBehandlingService: TilbakestillBehandlingService,
 ) : AsyncTaskStep {
     override fun doTask(task: Task) {
         val behandlingId = task.payload.toLong()
 
         val behandling = behandlingHentOgPersisterService.hent(behandlingId)
-        if (!behandling.aktiv || behandling.status != BehandlingStatus.UTREDES || behandling.steg.rekkefølge > StegType.BEHANDLINGSRESULTAT.rekkefølge) {
-            error("Behandling $behandlingId er gått forbi behandlingsresultatsteget og bør settes tilbake til et tidligere steg før man sletter kompetanse.")
+        if (!behandling.aktiv || behandling.status != BehandlingStatus.UTREDES) {
+            error("Behandling $behandlingId er ikke i ferd med å utredes.")
         }
-        slettKompetanserForBehandlingRekursivt(BehandlingId(behandlingId))
-    }
-
-    // Rekursivt siden sletting av kompetanse kan propagere ut og skape feil hvis vi ikke henter inn alle kompetanser på nytt hver gang vi sletter
-    private fun slettKompetanserForBehandlingRekursivt(behandlingId: BehandlingId) {
-        val kompetanser = kompetanseService.hentKompetanser(behandlingId)
-
-        // autogeneres en tom kompetanse dersom alle er slettet
-        if (kompetanser.size == 1 &&
-            !kompetanser.first().erObligatoriskeFelterSatt() &&
-            kompetanser.first().fom!! < localDateProvider.now().toYearMonth()
-        ) {
-            return
-        }
-        kompetanseService.slettKompetanse(behandlingId, kompetanser.first().id)
-        slettKompetanserForBehandlingRekursivt(behandlingId)
+        kompetanseService.skjemaService.slettSkjemaer(BehandlingId(behandlingId))
+        tilbakestillBehandlingService.tilbakestillBehandlingTilVilkårsvurdering(behandling)
     }
 
     companion object {
