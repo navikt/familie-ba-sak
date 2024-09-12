46c25577fe0e0ded731171ea826e93bbb19c8400
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2024-09-12 16:18:34 +0200

    søk opp kompetanser i db på nytt mellom hver sletting så vi unngår å referere til kompetanser som allerede er slettet (#4769)
    ### 💰 Hva skal gjøres, og hvorfor?
    Når vi prøver å slette alle kompetanser får vi tidvis feil fordi vi
    referer til en kompetanse som allerede er slettet. Henter inn alle
    kompetansene på nytt mellom hver sletting for å unngå dette.
    
    ---------
    
    Signed-off-by: FredrikP <57641333+fredrikpf@users.noreply.github.com>

--- src/main/kotlin/no/nav/familie/ba/sak/task/SlettKompetanserTask.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/task/SlettKompetanserTask.kt
@@ -4,7 +4,7 @@ import no.nav.familie.ba.sak.kjerne.behandling.BehandlingHentOgPersisterService
 import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingStatus
 import no.nav.familie.ba.sak.kjerne.eøs.felles.BehandlingId
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.KompetanseService
-import no.nav.familie.ba.sak.kjerne.steg.StegType
+import no.nav.familie.ba.sak.kjerne.steg.TilbakestillBehandlingService
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
@@ -20,22 +20,17 @@ import org.springframework.stereotype.Service
 class SlettKompetanserTask(
     private val kompetanseService: KompetanseService,
     private val behandlingHentOgPersisterService: BehandlingHentOgPersisterService,
+    private val tilbakestillBehandlingService: TilbakestillBehandlingService,
 ) : AsyncTaskStep {
     override fun doTask(task: Task) {
         val behandlingId = task.payload.toLong()
 
         val behandling = behandlingHentOgPersisterService.hent(behandlingId)
-        if (!behandling.aktiv || behandling.status != BehandlingStatus.UTREDES || behandling.steg.rekkefølge > StegType.BEHANDLINGSRESULTAT.rekkefølge) {
-            error("Behandling $behandlingId er gått forbi behandlingsresultatsteget og bør settes tilbake til et tidligere steg før man sletter kompetanse.")
+        if (!behandling.aktiv || behandling.status != BehandlingStatus.UTREDES) {
+            error("Behandling $behandlingId er ikke i ferd med å utredes.")
         }
-        slettKompetanserForBehandling(BehandlingId(behandlingId))
-    }
-
-    private fun slettKompetanserForBehandling(behandlingId: BehandlingId) {
-        val kompetanser = kompetanseService.hentKompetanser(behandlingId)
-        kompetanser.forEach { kompetanse ->
-            kompetanseService.slettKompetanse(behandlingId, kompetanse.id)
-        }
+        kompetanseService.skjemaService.slettSkjemaer(BehandlingId(behandlingId))
+        tilbakestillBehandlingService.tilbakestillBehandlingTilVilkårsvurdering(behandling)
     }
 
     companion object {
