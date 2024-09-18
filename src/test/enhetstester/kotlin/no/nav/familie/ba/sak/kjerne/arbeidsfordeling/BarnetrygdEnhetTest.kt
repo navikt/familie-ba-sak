d5c4b4177a681a4eaf680d0b08682fd3a54e9d73
thoalm <thoalm@outlook.com>
2024-09-18 14:08:30 +0200

    NAV-22311: La til enhetstester for BarnetrygdEnhet

---
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/arbeidsfordeling/BarnetrygdEnhetTest.kt
@@ -0,0 +1,29 @@
+package no.nav.familie.ba.sak.kjerne.arbeidsfordeling
+
+import no.nav.familie.ba.sak.kjerne.arbeidsfordeling.BarnetrygdEnhet.Companion.erGyldigBehandlendeBarnetrygdEnhet
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class BarnetrygdEnhetTest {
+    @Nested
+    inner class ErGyldigBehandlendeBarnetrygdEnhetTest {
+        @Test
+        fun `skal returnere false hvis ugyldig enhetsnummer`() {
+            // Act
+            val erGyldig = erGyldigBehandlendeBarnetrygdEnhet("1")
+
+            // Assert
+            assertThat(erGyldig).isFalse()
+        }
+
+        @Test
+        fun `skal returnere true hvis gyldig enhetsnummer`() {
+            // Act
+            val erGyldig = erGyldigBehandlendeBarnetrygdEnhet(BarnetrygdEnhet.OSLO.enhetsnummer)
+
+            // Assert
+            assertThat(erGyldig).isTrue()
+        }
+    }
+}
