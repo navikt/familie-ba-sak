2f6c46d8cfd9c7b8849ef999484b4f5a839d43c3
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2024-11-06 15:15:05 +0100

    NAV-22995: Legger til og oppdatere eksistrende metoder for å lage testdata i forbindelse med endringer på utbetalingsoppdrag (#4873)
    ### 💰 Hva skal gjøres, og hvorfor?
    Favro:
    https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-22995
    
    Disse metodene skal brukes videre i en kommende PR:
    https://github.com/navikt/familie-ba-sak/pull/4852
    
    Metodene gjør det lettere å opprette testdata for tester. 
    
    Legger til disse metodene i en egen PR da den andre PRen er veldig stor.
    
    ### 🔎️ Er det noe spesielt du ønsker tilbakemelding om?
    Nei
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [ ] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    _Jeg har ikke skrevet tester fordi:_
    Ikke relevant.
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei

---
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/integrasjoner/økonomi/utbetalingsoppdrag/BeregnetUtbetalingsoppdragDataGenerator.kt
@@ -0,0 +1,65 @@
+package no.nav.familie.ba.sak.integrasjoner.økonomi.utbetalingsoppdrag
+
+import no.nav.familie.felles.utbetalingsgenerator.domain.AndelMedPeriodeIdLongId
+import no.nav.familie.felles.utbetalingsgenerator.domain.BeregnetUtbetalingsoppdragLongId
+import no.nav.familie.felles.utbetalingsgenerator.domain.Opphør
+import no.nav.familie.felles.utbetalingsgenerator.domain.Utbetalingsoppdrag
+import no.nav.familie.felles.utbetalingsgenerator.domain.Utbetalingsperiode
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.UUID
+
+fun lagBeregnetUtbetalingsoppdragLongId(
+    utbetalingsperioder: List<Utbetalingsperiode>,
+    andeler: List<AndelMedPeriodeIdLongId>,
+): BeregnetUtbetalingsoppdragLongId =
+    BeregnetUtbetalingsoppdragLongId(
+        Utbetalingsoppdrag(
+            kodeEndring = Utbetalingsoppdrag.KodeEndring.NY,
+            fagSystem = "BA",
+            saksnummer = "",
+            aktoer = UUID.randomUUID().toString(),
+            saksbehandlerId = "",
+            avstemmingTidspunkt = LocalDateTime.now(),
+            utbetalingsperiode = utbetalingsperioder,
+        ),
+        andeler = andeler,
+    )
+
+fun lagUtbetalingsperiode(
+    behandlingId: Long,
+    periodeId: Long,
+    forrigePeriodeId: Long?,
+    ytelseTypeBa: YtelsetypeBA,
+    fom: LocalDate = LocalDate.now(),
+    tom: LocalDate = LocalDate.now(),
+    opphør: Opphør? = null,
+): Utbetalingsperiode =
+    Utbetalingsperiode(
+        erEndringPåEksisterendePeriode = true,
+        opphør = opphør,
+        periodeId = periodeId,
+        forrigePeriodeId = forrigePeriodeId,
+        datoForVedtak = LocalDate.now(),
+        klassifisering = ytelseTypeBa.klassifisering,
+        vedtakdatoFom = fom,
+        vedtakdatoTom = tom,
+        sats = BigDecimal.valueOf(1054),
+        satsType = Utbetalingsperiode.SatsType.MND,
+        utbetalesTil = "",
+        behandlingId = behandlingId,
+    )
+
+fun lagAndelMedPeriodeIdLong(
+    id: Long,
+    periodeId: Long,
+    forrigePeriodeId: Long?,
+    kildeBehandlingId: Long,
+): AndelMedPeriodeIdLongId =
+    AndelMedPeriodeIdLongId(
+        id = id,
+        periodeId = periodeId,
+        forrigePeriodeId = forrigePeriodeId,
+        kildeBehandlingId = kildeBehandlingId,
+    )
