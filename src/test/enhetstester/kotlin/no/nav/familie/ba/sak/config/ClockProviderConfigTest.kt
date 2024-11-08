dbaa857e31fd662eeb062e9ee0aae7aee1959a26
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2024-11-08 11:59:02 +0100

    NAV-22995: Endrer fra Clock til ClockProvider for å sikre at sommertid/vintertid fungerer korrekt (#4876)
    Favro:
    [NAV-22995](https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-22995)
    
    ### 💰 Hva skal gjøres, og hvorfor?
    ClockProvider oppretter en ny instanse av Clock hver gang det kalles.
    Dette sikrer at sommertid/vintertid vil fungere.
    
    ### 🔎️ Er det noe spesielt du ønsker tilbakemelding om?
    Nei
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [x] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/config/ClockProviderConfigTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/config/ClockProviderConfigTest.kt
@@ -4,14 +4,14 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.Clock
 
-class ClockConfigTest {
+class ClockProviderConfigTest {
     @Test
-    fun `skal returnere system default zone clock`() {
+    fun `skal returnere system default zone clock fra clock provider`() {
         // Arrange
-        val clockConfig = ClockConfig()
+        val clockProvider = ClockProviderConfig().clockProvider()
 
         // Act
-        val clock = clockConfig.clock()
+        val clock = clockProvider.get()
 
         // Assert
         assertThat(clock).isEqualTo(Clock.systemDefaultZone())
