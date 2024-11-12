5edf822f6cc526599719c7a4c5106680f0f896fa
Hanne Olsen <hanne.olsen@nav.no>
2024-11-12 09:56:16 +0100

    Legger adressevalidering som del av ManuellBrevMottaker

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/brev/mottaker/BrevmottakerValidering.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/brev/mottaker/BrevmottakerValidering.kt
@@ -3,23 +3,8 @@ package no.nav.familie.ba.sak.kjerne.brev.mottaker
 import no.nav.familie.ba.sak.kjerne.brev.domene.ManuellBrevmottaker
 
 object BrevmottakerValidering {
-    fun erBrevmottakerGyldig(brevmottaker: ManuellBrevmottaker): Boolean {
-        if (brevmottaker.landkode == "NO") {
-            return brevmottaker.navn.isNotEmpty() &&
-                brevmottaker.adresselinje1.isNotEmpty() &&
-                brevmottaker.postnummer.isNotEmpty() &&
-                brevmottaker.poststed.isNotEmpty()
-        } else {
-            // Utenlandske manuelle brevmottakere skal ha postnummer og poststed satt i adresselinjene
-            return brevmottaker.navn.isNotEmpty() &&
-                brevmottaker.adresselinje1.isNotEmpty() &&
-                brevmottaker.postnummer.isEmpty() &&
-                brevmottaker.poststed.isEmpty()
-        }
-    }
-
     fun erBrevmottakereGyldige(brevmottakere: List<ManuellBrevmottaker>): Boolean =
         brevmottakere.all {
-            erBrevmottakerGyldig(it)
+            it.harGyldigAdresse()
         }
 }
