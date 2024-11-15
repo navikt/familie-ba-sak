c8f311a2bae4985c1ce62d9251a45708462d1e43
bragejahren <70642183+bragejahren@users.noreply.github.com>
2024-11-15 14:23:50 +0100

    Fjerner feature toggle: `OPPRETT_SAK_PÅ_RIKTIG_ENHET_OG_SAKSBEHANDLER` (#4896)
    ### 💰 Hva skal gjøres, og hvorfor?
    Fjerner feature toggelen da den har vært på i prod lenge.
    
    ### ✅ Checklist
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer.
    - [ ] Jeg har skrevet tester.
    
    Jeg har ikke skrevet tester fordi: Ingen endringer ut over fjerning av
    toggle. Eksisterende tester hvor toggle var på kjører fortsatt "OK".
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei

--- src/main/kotlin/no/nav/familie/ba/sak/config/featureToggle/FeatureToggleConfig.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/featureToggle/FeatureToggleConfig.kt
@@ -13,9 +13,6 @@ class FeatureToggleConfig {
         // NAV-21071 lagt bak toggle og kan evt fjernes på sikt hvis man ikke har trengt å skru den på igjen
         const val SKAL_OPPRETTE_FREMLEGGSOPPGAVE_EØS_MEDLEM = "familie-ba-sak.skalOpprettFremleggsoppgaveDersomEOSMedlem"
 
-        // NAV-22311
-        const val OPPRETT_SAK_PÅ_RIKTIG_ENHET_OG_SAKSBEHANDLER = "familie-ba-ks-sak.opprett-sak-paa-riktig-enhet-og-saksbehandler"
-
         // NAV-22995
         const val SKAL_BRUKE_NY_KLASSEKODE_FOR_UTVIDET_BARNETRYGD = "familie-ba-sak.skal-bruke-ny-klassekode-for-utvidet-barnetrygd"
 
