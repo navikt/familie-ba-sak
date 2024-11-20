4ec05744c538ad485a6e01e6c0fbdff3f0a26604
Uy Nguyen <uy.quang.nguyen@nav.no>
2024-11-20 09:56:21 +0100

    Oppretter endepunkt for oppslag av jp og fagsakid basert på jp

---
+++ src/main/kotlin/no/nav/familie/ba/sak/task/LogFagsakIdForJournalpostTask.kt
@@ -0,0 +1,43 @@
+package no.nav.familie.ba.sak.task
+
+import no.nav.familie.ba.sak.common.convertDataClassToJson
+import no.nav.familie.ba.sak.integrasjoner.familieintegrasjoner.IntegrasjonClient
+import no.nav.familie.prosessering.AsyncTaskStep
+import no.nav.familie.prosessering.TaskStepBeskrivelse
+import no.nav.familie.prosessering.domene.Task
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+@TaskStepBeskrivelse(
+    taskStepType = FerdigstillBehandlingTask.TASK_STEP_TYPE,
+    beskrivelse = "Logger fagsak id for journalpost i secure logs",
+    maxAntallFeil = 1,
+)
+class LogFagsakIdForJournalpostTask(
+    val integrasjonClient: IntegrasjonClient,
+) : AsyncTaskStep {
+    override fun doTask(task: Task) {
+        val secureLogger = LoggerFactory.getLogger("secureLogger")
+
+        val journalpostId = task.payload
+
+        secureLogger.info("Henter journalpost m/ id $journalpostId for oppslag av fagsak id")
+
+        val journalpost = integrasjonClient.hentJournalpost(journalpostId)
+
+        journalpost.sak?.let { secureLogger.info(it.convertDataClassToJson()) }
+    }
+
+    companion object {
+        const val TASK_STEP_TYPE = "logFagsakIdForJournalpost"
+
+        fun opprettTask(
+            journalpostId: String,
+        ): Task =
+            Task(
+                type = TASK_STEP_TYPE,
+                payload = journalpostId,
+            )
+    }
+}
