10c88f381661625c5bb6fdb3e003aef9a8b7c136
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2024-11-20 12:12:31 +0100

    flytt hentHjemmeltekst til HjemlerService
    Signed-off-by: FredrikP <57641333+fredrikpf@users.noreply.github.com>

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/brev/HjemlerService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/brev/HjemlerService.kt
@@ -1,8 +1,16 @@
 package no.nav.familie.ba.sak.kjerne.brev
 
+import no.nav.familie.ba.sak.common.FunksjonellFeil
+import no.nav.familie.ba.sak.common.Utils
 import no.nav.familie.ba.sak.integrasjoner.sanity.SanityService
 import no.nav.familie.ba.sak.kjerne.autovedtak.fødselshendelse.Resultat
+import no.nav.familie.ba.sak.kjerne.brev.domene.SanityBegrunnelse
+import no.nav.familie.ba.sak.kjerne.brev.domene.SanityEØSBegrunnelse
+import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.Målform
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersongrunnlagService
+import no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser.EØSStandardbegrunnelse
+import no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser.Standardbegrunnelse
+import no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser.hjemlerTilhørendeFritekst
 import no.nav.familie.ba.sak.kjerne.vedtak.domene.VedtaksperiodeMedBegrunnelser
 import no.nav.familie.ba.sak.kjerne.vedtak.refusjonEøs.RefusjonEøsRepository
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.VilkårsvurderingService
@@ -42,4 +50,166 @@ class HjemlerService(
             erFritekstIBrev = sorterteVedtaksperioderMedBegrunnelser.any { it.fritekster.isNotEmpty() },
         )
     }
+}
+
+fun hentHjemmeltekst(
+    vedtaksperioder: List<VedtaksperiodeMedBegrunnelser>,
+    standardbegrunnelseTilSanityBegrunnelse: Map<Standardbegrunnelse, SanityBegrunnelse>,
+    eøsStandardbegrunnelseTilSanityBegrunnelse: Map<EØSStandardbegrunnelse, SanityEØSBegrunnelse>,
+    opplysningspliktHjemlerSkalMedIBrev: Boolean = false,
+    målform: Målform,
+    vedtakKorrigertHjemmelSkalMedIBrev: Boolean = false,
+    refusjonEøsHjemmelSkalMedIBrev: Boolean = false,
+    erFritekstIBrev: Boolean,
+): String {
+    val sanityStandardbegrunnelser =
+        vedtaksperioder.flatMap { vedtaksperiode -> vedtaksperiode.begrunnelser.mapNotNull { begrunnelse -> standardbegrunnelseTilSanityBegrunnelse[begrunnelse.standardbegrunnelse] } }
+    val sanityEøsBegrunnelser =
+        vedtaksperioder.flatMap { vedtaksperiode -> vedtaksperiode.eøsBegrunnelser.mapNotNull { eøsBegrunnelse -> eøsStandardbegrunnelseTilSanityBegrunnelse[eøsBegrunnelse.begrunnelse] } }
+
+    val ordinæreHjemler =
+        hentOrdinæreHjemler(
+            hjemler =
+                (sanityStandardbegrunnelser.flatMap { it.hjemler } + sanityEøsBegrunnelser.flatMap { it.hjemler })
+                    .toMutableSet(),
+            opplysningspliktHjemlerSkalMedIBrev = opplysningspliktHjemlerSkalMedIBrev,
+            finnesVedtaksperiodeMedFritekst = erFritekstIBrev,
+        )
+
+    val forvaltningsloverHjemler = hentForvaltningsloverHjemler(vedtakKorrigertHjemmelSkalMedIBrev)
+
+    val alleHjemlerForBegrunnelser =
+        hentAlleTyperHjemler(
+            hjemlerSeparasjonsavtaleStorbritannia =
+                sanityEøsBegrunnelser
+                    .flatMap { it.hjemlerSeperasjonsavtalenStorbritannina }
+                    .distinct(),
+            ordinæreHjemler = ordinæreHjemler.distinct(),
+            hjemlerFraFolketrygdloven =
+                (sanityStandardbegrunnelser.flatMap { it.hjemlerFolketrygdloven } + sanityEøsBegrunnelser.flatMap { it.hjemlerFolketrygdloven })
+                    .distinct(),
+            hjemlerEØSForordningen883 = sanityEøsBegrunnelser.flatMap { it.hjemlerEØSForordningen883 }.distinct(),
+            hjemlerEØSForordningen987 = hentHjemlerForEøsForordningen987(sanityEøsBegrunnelser, refusjonEøsHjemmelSkalMedIBrev),
+            målform = målform,
+            hjemlerFraForvaltningsloven = forvaltningsloverHjemler,
+        )
+
+    return slåSammenHjemlerAvUlikeTyper(alleHjemlerForBegrunnelser)
+}
+
+private fun hentHjemlerForEøsForordningen987(
+    sanityEøsBegrunnelser: List<SanityEØSBegrunnelse>,
+    refusjonEøsHjemmelSkalMedIBrev: Boolean,
+): List<String> {
+    val hjemler = mutableListOf<String>()
+
+    hjemler.addAll(sanityEøsBegrunnelser.flatMap { it.hjemlerEØSForordningen987 })
+
+    if (refusjonEøsHjemmelSkalMedIBrev) {
+        hjemler.add("60")
+    }
+
+    return hjemler.distinct()
+}
+
+private fun slåSammenHjemlerAvUlikeTyper(hjemler: List<String>) =
+    when (hjemler.size) {
+        0 -> throw FunksjonellFeil("Ingen hjemler var knyttet til begrunnelsen(e) som er valgt. Du må velge minst én begrunnelse som er knyttet til en hjemmel.")
+        1 -> hjemler.single()
+        else -> hjemler.slåSammen()
+    }
+
+private fun hentAlleTyperHjemler(
+    hjemlerSeparasjonsavtaleStorbritannia: List<String>,
+    ordinæreHjemler: List<String>,
+    hjemlerFraFolketrygdloven: List<String>,
+    hjemlerEØSForordningen883: List<String>,
+    hjemlerEØSForordningen987: List<String>,
+    målform: Målform,
+    hjemlerFraForvaltningsloven: List<String>,
+): List<String> {
+    val alleHjemlerForBegrunnelser = mutableListOf<String>()
+
+    // Rekkefølgen her er viktig
+    if (hjemlerSeparasjonsavtaleStorbritannia.isNotEmpty()) {
+        alleHjemlerForBegrunnelser.add(
+            "${
+                when (målform) {
+                    Målform.NB -> "Separasjonsavtalen mellom Storbritannia og Norge artikkel"
+                    Målform.NN -> "Separasjonsavtalen mellom Storbritannia og Noreg artikkel"
+                }
+            } ${
+                Utils.slåSammen(
+                    hjemlerSeparasjonsavtaleStorbritannia,
+                )
+            }",
+        )
+    }
+    if (ordinæreHjemler.isNotEmpty()) {
+        alleHjemlerForBegrunnelser.add(
+            "${
+                when (målform) {
+                    Målform.NB -> "barnetrygdloven"
+                    Målform.NN -> "barnetrygdlova"
+                }
+            } ${
+                hjemlerTilHjemmeltekst(
+                    hjemler = ordinæreHjemler,
+                    lovForHjemmel = "barnetrygdloven",
+                )
+            }",
+        )
+    }
+    if (hjemlerFraFolketrygdloven.isNotEmpty()) {
+        alleHjemlerForBegrunnelser.add(
+            "${
+                when (målform) {
+                    Målform.NB -> "folketrygdloven"
+                    Målform.NN -> "folketrygdlova"
+                }
+            } ${
+                hjemlerTilHjemmeltekst(
+                    hjemler = hjemlerFraFolketrygdloven,
+                    lovForHjemmel = "folketrygdloven",
+                )
+            }",
+        )
+    }
+    if (hjemlerEØSForordningen883.isNotEmpty()) {
+        alleHjemlerForBegrunnelser.add("EØS-forordning 883/2004 artikkel ${Utils.slåSammen(hjemlerEØSForordningen883)}")
+    }
+    if (hjemlerEØSForordningen987.isNotEmpty()) {
+        alleHjemlerForBegrunnelser.add("EØS-forordning 987/2009 artikkel ${Utils.slåSammen(hjemlerEØSForordningen987)}")
+    }
+    if (hjemlerFraForvaltningsloven.isNotEmpty()) {
+        alleHjemlerForBegrunnelser.add(
+            "${
+                when (målform) {
+                    Målform.NB -> "forvaltningsloven"
+                    Målform.NN -> "forvaltningslova"
+                }
+            } ${
+                hjemlerTilHjemmeltekst(hjemler = hjemlerFraForvaltningsloven, lovForHjemmel = "forvaltningsloven")
+            }",
+        )
+    }
+    return alleHjemlerForBegrunnelser
+}
+
+private fun hentOrdinæreHjemler(
+    hjemler: MutableSet<String>,
+    opplysningspliktHjemlerSkalMedIBrev: Boolean,
+    finnesVedtaksperiodeMedFritekst: Boolean,
+): List<String> {
+    if (opplysningspliktHjemlerSkalMedIBrev) {
+        val hjemlerNårOpplysningspliktIkkeOppfylt = listOf("17", "18")
+        hjemler.addAll(hjemlerNårOpplysningspliktIkkeOppfylt)
+    }
+
+    if (finnesVedtaksperiodeMedFritekst) {
+        hjemler.addAll(hjemlerTilhørendeFritekst.map { it.toString() }.toSet())
+    }
+
+    val sorterteHjemler = hjemler.map { it.toInt() }.sorted().map { it.toString() }
+    return sorterteHjemler
 }
