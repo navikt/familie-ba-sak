64435f0e511116b232a1db09a918c3da4962be18
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2024-11-26 18:12:41 +0100

    Lag tester for finnSisteGyldigeArbeidsfordelingPåBehandlingIFagsak query

--- src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/kjerne/arbeidsfordeling/domene/ArbeidsfordelingPåBehandlingRepositoryTest.kt
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/kjerne/arbeidsfordeling/domene/ArbeidsfordelingPåBehandlingRepositoryTest.kt
@@ -1,15 +1,29 @@
 package no.nav.familie.ba.sak.kjerne.arbeidsfordeling.domene
 
 import no.nav.familie.ba.sak.common.Feil
+import no.nav.familie.ba.sak.common.lagBehandling
+import no.nav.familie.ba.sak.common.lagFagsak
+import no.nav.familie.ba.sak.common.randomAktør
 import no.nav.familie.ba.sak.config.AbstractSpringIntegrationTest
 import no.nav.familie.ba.sak.datagenerator.oppgave.lagArbeidsfordelingPåBehandling
+import no.nav.familie.ba.sak.kjerne.arbeidsfordeling.BarnetrygdEnhet
+import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingRepository
+import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingStatus
+import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandlingsresultat
+import no.nav.familie.ba.sak.kjerne.fagsak.FagsakRepository
+import no.nav.familie.ba.sak.kjerne.personident.AktørIdRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.jpa.domain.AbstractPersistable_.id
+import java.time.LocalDateTime
 
 class ArbeidsfordelingPåBehandlingRepositoryTest(
+    @Autowired private val aktørIdRepository: AktørIdRepository,
+    @Autowired private val fagsakRepository: FagsakRepository,
+    @Autowired private val behandlingRepository: BehandlingRepository,
     @Autowired private val arbeidsfordelingPåBehandlingRepository: ArbeidsfordelingPåBehandlingRepository,
 ) : AbstractSpringIntegrationTest() {
     @Nested
@@ -27,8 +41,7 @@ class ArbeidsfordelingPåBehandlingRepositoryTest(
             arbeidsfordelingPåBehandlingRepository.save(arbeidsfordelingPåBehandling)
 
             // Act
-            val lagretArbeidsfordelingPåBehandling =
-                arbeidsfordelingPåBehandlingRepository.hentArbeidsfordelingPåBehandling(behandlingId)
+            val lagretArbeidsfordelingPåBehandling = arbeidsfordelingPåBehandlingRepository.hentArbeidsfordelingPåBehandling(behandlingId)
 
             // Assert
             assertThat(lagretArbeidsfordelingPåBehandling).isEqualTo(arbeidsfordelingPåBehandling)
@@ -42,6 +55,104 @@ class ArbeidsfordelingPåBehandlingRepositoryTest(
                     arbeidsfordelingPåBehandlingRepository.hentArbeidsfordelingPåBehandling(1L)
                 }
             assertThat(exception.message).isEqualTo("Finner ikke tilknyttet arbeidsfordelingsenhet på behandling 1")
+        }
+    }
+
+    @Nested
+    inner class FinnSisteGyldigeArbeidsfordelingPåBehandlingIFagsak {
+        @Test
+        fun `skal ikke hente arbeidsfordelingPåBehandling med behandlende enhet er midlertidig enhet`() {
+            // Arrange
+            val aktør = aktørIdRepository.save(randomAktør())
+            val fagsak = fagsakRepository.save(lagFagsak(aktør = aktør))
+            val behandling = behandlingRepository.save(lagBehandling(fagsak = fagsak, status = BehandlingStatus.AVSLUTTET))
+
+            arbeidsfordelingPåBehandlingRepository.save(
+                lagArbeidsfordelingPåBehandling(
+                    behandlingId = behandling.id,
+                    behandlendeEnhetId = BarnetrygdEnhet.MIDLERTIDIG_ENHET.enhetsnummer,
+                ),
+            )
+
+            // Act
+            val lagretArbeidsfordelingPåBehandling = arbeidsfordelingPåBehandlingRepository.finnSisteGyldigeArbeidsfordelingPåBehandlingIFagsak(fagsak.id)
+
+            // Assert
+            assertThat(lagretArbeidsfordelingPåBehandling).isNull()
+        }
+
+        @Test
+        fun `skal ikke hente arbeidsfordeling på behandling som ikke har status AVSLUTTET`() {
+            // Arrange
+            val aktør = aktørIdRepository.save(randomAktør())
+            val fagsak = fagsakRepository.save(lagFagsak(aktør = aktør))
+            val behandling = behandlingRepository.save(lagBehandling(fagsak = fagsak, status = BehandlingStatus.FATTER_VEDTAK, aktivertTid = LocalDateTime.now()))
+
+            arbeidsfordelingPåBehandlingRepository.save(lagArbeidsfordelingPåBehandling(behandlingId = behandling.id))
+
+            // Act
+            val lagretArbeidsfordelingPåBehandling = arbeidsfordelingPåBehandlingRepository.finnSisteGyldigeArbeidsfordelingPåBehandlingIFagsak(fagsak.id)
+
+            // Assert
+            assertThat(lagretArbeidsfordelingPåBehandling).isNull()
+        }
+
+        @Test
+        fun `skal ikke hente arbeidsfordeling på behandling som har resultat HENLAGT`() {
+            // Arrange
+            val aktør = aktørIdRepository.save(randomAktør())
+            val fagsak = fagsakRepository.save(lagFagsak(aktør = aktør))
+            val behandling =
+                behandlingRepository.save(
+                    lagBehandling(
+                        fagsak = fagsak,
+                        status = BehandlingStatus.AVSLUTTET,
+                        resultat = Behandlingsresultat.HENLAGT_FEILAKTIG_OPPRETTET,
+                        aktivertTid = LocalDateTime.now(),
+                    ),
+                )
+
+            arbeidsfordelingPåBehandlingRepository.save(lagArbeidsfordelingPåBehandling(behandlingId = behandling.id))
+
+            // Act
+            val lagretArbeidsfordelingPåBehandling = arbeidsfordelingPåBehandlingRepository.finnSisteGyldigeArbeidsfordelingPåBehandlingIFagsak(fagsak.id)
+
+            // Assert
+            assertThat(lagretArbeidsfordelingPåBehandling).isNull()
+        }
+
+        @Test
+        fun `skal hente nyeste arbeidsfordeling på behandling`() {
+            // Arrange
+            val aktør = aktørIdRepository.save(randomAktør())
+            val fagsak = fagsakRepository.save(lagFagsak(aktør = aktør))
+
+            val nyBehandling =
+                behandlingRepository.save(
+                    lagBehandling(
+                        fagsak = fagsak,
+                        status = BehandlingStatus.AVSLUTTET,
+                        aktivertTid = LocalDateTime.now(),
+                    ),
+                )
+            val gammelBehandling =
+                behandlingRepository.save(
+                    lagBehandling(
+                        fagsak = fagsak,
+                        status = BehandlingStatus.AVSLUTTET,
+                        aktivertTid = LocalDateTime.now().minusDays(10),
+                        aktiv = false,
+                    ),
+                )
+
+            val arbeidsfordelingNy = arbeidsfordelingPåBehandlingRepository.save(lagArbeidsfordelingPåBehandling(behandlingId = nyBehandling.id))
+            arbeidsfordelingPåBehandlingRepository.save(lagArbeidsfordelingPåBehandling(behandlingId = gammelBehandling.id))
+
+            // Act
+            val lagretArbeidsfordelingPåBehandling = arbeidsfordelingPåBehandlingRepository.finnSisteGyldigeArbeidsfordelingPåBehandlingIFagsak(fagsak.id)
+
+            // Assert
+            assertThat(lagretArbeidsfordelingPåBehandling).isEqualTo(arbeidsfordelingNy)
         }
     }
 }
