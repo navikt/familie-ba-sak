185b0eaac297208e71292e815255a996ecdb185a
thoalm <thoalm@outlook.com>
2024-11-27 10:26:43 +0100

    NAV-22215: Legger til tester for harRefusjonEøsPåBehandling

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/common/DataGenerator.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/common/DataGenerator.kt
@@ -89,6 +89,7 @@ import no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser.domene.EØSBegrunnelse
 import no.nav.familie.ba.sak.kjerne.vedtak.domene.Vedtaksbegrunnelse
 import no.nav.familie.ba.sak.kjerne.vedtak.domene.VedtaksbegrunnelseFritekst
 import no.nav.familie.ba.sak.kjerne.vedtak.domene.VedtaksperiodeMedBegrunnelser
+import no.nav.familie.ba.sak.kjerne.vedtak.refusjonEøs.RefusjonEøs
 import no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.Utbetalingsperiode
 import no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.UtbetalingsperiodeDetalj
 import no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.VedtaksperiodeService
@@ -801,6 +802,25 @@ fun lagAnnenVurdering(
         resultat = resultat,
         type = type,
         begrunnelse = begrunnelse,
+    )
+
+fun lagRefusjonEøs(
+    behandlingId: Long = 0L,
+    fom: LocalDate = LocalDate.now().minusMonths(1),
+    tom: LocalDate = LocalDate.now().plusMonths(1),
+    refusjonsbeløp: Int = 0,
+    land: String = "NO",
+    refusjonAvklart: Boolean = true,
+    id: Long = 0L,
+): RefusjonEøs =
+    RefusjonEøs(
+        behandlingId = behandlingId,
+        fom = fom,
+        tom = tom,
+        refusjonsbeløp = refusjonsbeløp,
+        land = land,
+        refusjonAvklart = refusjonAvklart,
+        id = id,
     )
 
 fun lagVilkårsvurdering(
