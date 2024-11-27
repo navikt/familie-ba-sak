cebe428ec141afec3113f2e423a4e9ce8d0f0953
Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>
2024-11-27 13:08:37 +0100

    Legg til konsistensavstemmingDato2025 (#4929)
    Favrokort:
    https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-23453
    
    Legger inn dato for konsistensavsstemming  2025

---
+++ src/main/resources/db/migration/V267_konsistensavstemming_datoer_2025.sql
@@ -0,0 +1,13 @@
+INSERT INTO batch (id, kjoredato)
+VALUES (nextval('batch_seq'), TO_TIMESTAMP('03-01-2025', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('30-01-2025', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('27-02-2025', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('27-03-2025', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('29-04-2025', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('23-05-2025', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('27-06-2025', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('30-07-2025', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('01-09-2025', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('29-09-2025', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('30-10-2025', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('21-11-2025', 'DD-MM-YYYY SS:MS'));
