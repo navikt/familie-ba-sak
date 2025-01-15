85cfaf6db34c53e0ea1e6f441706e76e0d656a0a
schmelita <schirin.melita@gmail.com>
2025-01-15 15:49:32 +0100

    Flytt funksjon for generering av økonomi simulering til egen fil

--- src/test/testdata/kotlin/no/nav/familie/ba/sak/datagenerator/ØkonomiSimuleringGenerator.kt
+++ src/test/testdata/kotlin/no/nav/familie/ba/sak/datagenerator/ØkonomiSimuleringGenerator.kt
@@ -1,14 +1,24 @@
-﻿package no.nav.familie.ba.sak.datagenerator.simulering
+package no.nav.familie.ba.sak.datagenerator
 
 import io.mockk.mockk
+import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandling
 import no.nav.familie.ba.sak.kjerne.simulering.domene.ØkonomiSimuleringMottaker
 import no.nav.familie.ba.sak.kjerne.simulering.domene.ØkonomiSimuleringPostering
 import no.nav.familie.kontrakter.felles.simulering.BetalingType
 import no.nav.familie.kontrakter.felles.simulering.FagOmrådeKode
+import no.nav.familie.kontrakter.felles.simulering.MottakerType
 import no.nav.familie.kontrakter.felles.simulering.PosteringType
 import java.time.LocalDate
 
-fun mockØkonomiSimuleringPostering(
+fun lagØkonomiSimuleringMottaker(
+    id: Long = 0,
+    mottakerNummer: String? = randomFnr(),
+    mottakerType: MottakerType = MottakerType.BRUKER,
+    behandling: Behandling = mockk(relaxed = true),
+    økonomiSimuleringPostering: List<ØkonomiSimuleringPostering> = listOf(lagØkonomiSimuleringPostering()),
+) = ØkonomiSimuleringMottaker(id, mottakerNummer, mottakerType, behandling, økonomiSimuleringPostering)
+
+fun lagØkonomiSimuleringPostering(
     økonomiSimuleringMottaker: ØkonomiSimuleringMottaker = mockk(relaxed = true),
     beløp: Int = 0,
     fagOmrådeKode: FagOmrådeKode = FagOmrådeKode.BARNETRYGD,
