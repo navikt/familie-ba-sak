b47eab7aad5c8a3bbbef1faec6c29c026b007fca
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2025-01-15 13:07:36 +0100

    Generer vedtak id direkte i stedet for å bruke funksjon som inkrementerer variabel

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/common/DataGenerator.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/common/DataGenerator.kt
@@ -119,14 +119,8 @@ import java.util.Properties
 import kotlin.math.abs
 import kotlin.random.Random
 
-private var gjeldendeVedtakId: Long = abs(Random.nextLong(10000000))
 private var gjeldendeUtvidetVedtaksperiodeId: Long = abs(Random.nextLong(10000000))
 private const val ID_INKREMENT = 50
-
-fun nesteVedtakId(): Long {
-    gjeldendeVedtakId += ID_INKREMENT
-    return gjeldendeVedtakId
-}
 
 fun nesteUtvidetVedtaksperiodeId(): Long {
     gjeldendeUtvidetVedtaksperiodeId += ID_INKREMENT
--- src/test/testdata/kotlin/VedtakGenerator.kt
+++ src/test/testdata/kotlin/VedtakGenerator.kt
@@ -1,14 +1,14 @@
-import no.nav.familie.ba.sak.common.nesteVedtakId
 import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandling
 import no.nav.familie.ba.sak.kjerne.vedtak.Vedtak
 import java.time.LocalDateTime
+import kotlin.random.Random
 
 fun lagVedtak(
     behandling: Behandling = lagBehandling(),
     stønadBrevPdF: ByteArray? = null,
     vedtaksdato: LocalDateTime? = LocalDateTime.now(),
 ) = Vedtak(
-    id = nesteVedtakId(),
+    id = Random.nextLong(10000000),
     behandling = behandling,
     vedtaksdato = vedtaksdato,
     stønadBrevPdF = stønadBrevPdF,
