6d474bc1b487f05b2ea8d890e5890b9648b3cbac
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2025-02-18 14:15:53 +0100

    Effektiviser konvertering

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/tidslinjefamiliefelles/transformasjon/EndreTid.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/tidslinjefamiliefelles/transformasjon/EndreTid.kt
@@ -5,10 +5,10 @@ import no.nav.familie.ba.sak.common.sisteDagIForrigeMåned
 import no.nav.familie.ba.sak.kjerne.tidslinjefamiliefelles.komposisjon.verdiPåTidspunkt
 import no.nav.familie.tidslinje.Null
 import no.nav.familie.tidslinje.Tidslinje
-import no.nav.familie.tidslinje.Udefinert
 import no.nav.familie.tidslinje.tilPeriodeVerdi
 import no.nav.familie.tidslinje.utvidelser.konverterTilDag
 import no.nav.familie.tidslinje.utvidelser.konverterTilMåned
+import no.nav.familie.tidslinje.utvidelser.tilPerioder
 import no.nav.familie.tidslinje.utvidelser.trim
 
 /**
@@ -33,18 +33,20 @@ fun <V> Tidslinje<V>.tilMåned(mapper: (List<V?>) -> V?): Tidslinje<V> =
  */
 fun <V> Tidslinje<V>.tilMånedFraMånedsskifteIkkeNull(
     mapper: (innholdSisteDagForrigeMåned: V, innholdFørsteDagDenneMåned: V) -> V?,
-): Tidslinje<V> =
-    this
+): Tidslinje<V> {
+    val perioder = this.tilPerioder()
+    return this
         .konverterTilMåned { dato, _ ->
-            val verdiForrigeMåned = verdiPåTidspunkt(dato.sisteDagIForrigeMåned())
-            val verdiDenneMåned = verdiPåTidspunkt(dato.førsteDagIInneværendeMåned())
+            val verdiForrigeMåned = perioder.verdiPåTidspunkt(dato.sisteDagIForrigeMåned())
+            val verdiDenneMåned = perioder.verdiPåTidspunkt(dato.førsteDagIInneværendeMåned())
 
             if (verdiForrigeMåned == null || verdiDenneMåned == null) {
                 Null()
             } else {
                 mapper(verdiForrigeMåned, verdiDenneMåned).tilPeriodeVerdi()
             }
-        }.trim(Null(), Udefinert())
+        }.trim(Null())
+}
 
 /**
  * Extention-metode som konverterer en dag-basert tidslinje til en måned-basert tidslinje.
