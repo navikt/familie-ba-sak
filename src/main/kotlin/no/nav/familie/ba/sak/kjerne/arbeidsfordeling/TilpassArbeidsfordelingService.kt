a17b85497ad074f184471c6d31fae0b1f981cb15
thoalm <thoalm@outlook.com>
2025-02-20 09:54:37 +0100

    NAV-24403: Fjerner toggle for oppretting av behandlinger på Vikafossen

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/arbeidsfordeling/TilpassArbeidsfordelingService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/arbeidsfordeling/TilpassArbeidsfordelingService.kt
@@ -2,8 +2,6 @@ package no.nav.familie.ba.sak.kjerne.arbeidsfordeling
 
 import no.nav.familie.ba.sak.common.Feil
 import no.nav.familie.ba.sak.common.containsExactly
-import no.nav.familie.ba.sak.config.FeatureToggle
-import no.nav.familie.ba.sak.config.featureToggle.UnleashNextMedContextService
 import no.nav.familie.ba.sak.integrasjoner.familieintegrasjoner.IntegrasjonClient
 import no.nav.familie.ba.sak.integrasjoner.familieintegrasjoner.domene.Arbeidsfordelingsenhet
 import no.nav.familie.ba.sak.sikkerhet.SikkerhetContext.SYSTEM_FORKORTELSE
@@ -13,30 +11,16 @@ import org.springframework.stereotype.Service
 @Service
 class TilpassArbeidsfordelingService(
     private val integrasjonClient: IntegrasjonClient,
-    private val unleashService: UnleashNextMedContextService,
 ) {
     fun tilpassArbeidsfordelingsenhetTilSaksbehandler(
         arbeidsfordelingsenhet: Arbeidsfordelingsenhet,
         navIdent: NavIdent?,
-    ): Arbeidsfordelingsenhet {
-        val tillatOpprettAvBehandlingPåVikafossen = unleashService.isEnabled(FeatureToggle.TILLAT_OPPRETT_AV_BEHANDLING_PÅ_VIKAFOSSEN, false)
-        return when (arbeidsfordelingsenhet.enhetId) {
-            BarnetrygdEnhet.MIDLERTIDIG_ENHET.enhetsnummer ->
-                if (tillatOpprettAvBehandlingPåVikafossen) {
-                    håndterMidlertidigEnhet4863(navIdent)
-                } else {
-                    håndterMidlertidigEnhet4863Gammel(navIdent)
-                }
-
+    ): Arbeidsfordelingsenhet =
+        when (arbeidsfordelingsenhet.enhetId) {
+            BarnetrygdEnhet.MIDLERTIDIG_ENHET.enhetsnummer -> håndterMidlertidigEnhet4863(navIdent)
             BarnetrygdEnhet.VIKAFOSSEN.enhetsnummer -> håndterVikafossenEnhet2103(navIdent)
-            else ->
-                if (tillatOpprettAvBehandlingPåVikafossen) {
-                    håndterAndreEnheter(navIdent, arbeidsfordelingsenhet)
-                } else {
-                    håndterAndreEnheterGammel(navIdent, arbeidsfordelingsenhet)
-                }
+            else -> håndterAndreEnheter(navIdent, arbeidsfordelingsenhet)
         }
-    }
 
     fun bestemTilordnetRessursPåOppgave(
         arbeidsfordelingsenhet: Arbeidsfordelingsenhet,
@@ -61,30 +45,6 @@ class TilpassArbeidsfordelingService(
                 .hentBehandlendeEnheterSomNavIdentHarTilgangTil(navIdent = navIdent)
                 .any { it.enhetsnummer == enhetId }
         } ?: false
-
-    private fun håndterMidlertidigEnhet4863Gammel(
-        navIdent: NavIdent?,
-    ): Arbeidsfordelingsenhet {
-        if (navIdent == null) {
-            throw Feil("Kan ikke håndtere ${BarnetrygdEnhet.MIDLERTIDIG_ENHET} om man mangler NAV-ident")
-        }
-        if (navIdent.erSystemIdent()) {
-            throw MidlertidigEnhetIAutomatiskBehandlingFeil("Kan ikke håndtere ${BarnetrygdEnhet.MIDLERTIDIG_ENHET} i automatiske behandlinger")
-        }
-        val enheterNavIdentHarTilgangTil =
-            integrasjonClient
-                .hentBehandlendeEnheterSomNavIdentHarTilgangTil(navIdent = navIdent)
-                .filter { it.enhetsnummer != BarnetrygdEnhet.VIKAFOSSEN.enhetsnummer }
-        if (enheterNavIdentHarTilgangTil.isEmpty()) {
-            throw Feil("Fant ingen passende enhetsnummer for nav-ident $navIdent")
-        }
-        // Velger bare det første enhetsnummeret i tilfeller hvor man har flere, avklart med fag
-        val nyBehandlendeEnhet = enheterNavIdentHarTilgangTil.first()
-        return Arbeidsfordelingsenhet(
-            nyBehandlendeEnhet.enhetsnummer,
-            nyBehandlendeEnhet.enhetsnavn,
-        )
-    }
 
     private fun håndterMidlertidigEnhet4863(
         navIdent: NavIdent?,
@@ -125,42 +85,6 @@ class TilpassArbeidsfordelingService(
         return Arbeidsfordelingsenhet(
             BarnetrygdEnhet.VIKAFOSSEN.enhetsnummer,
             BarnetrygdEnhet.VIKAFOSSEN.enhetsnavn,
-        )
-    }
-
-    private fun håndterAndreEnheterGammel(
-        navIdent: NavIdent?,
-        arbeidsfordelingsenhet: Arbeidsfordelingsenhet,
-    ): Arbeidsfordelingsenhet {
-        if (navIdent == null || navIdent.erSystemIdent()) {
-            // navIdent er null ved automatisk journalføring
-            return Arbeidsfordelingsenhet(
-                arbeidsfordelingsenhet.enhetId,
-                arbeidsfordelingsenhet.enhetNavn,
-            )
-        }
-        val enheterNavIdentHarTilgangTil =
-            integrasjonClient
-                .hentBehandlendeEnheterSomNavIdentHarTilgangTil(navIdent = navIdent)
-                .filter { it.enhetsnummer != BarnetrygdEnhet.VIKAFOSSEN.enhetsnummer }
-        if (enheterNavIdentHarTilgangTil.isEmpty()) {
-            throw Feil("Fant ingen passende enhetsnummer for NAV-ident $navIdent")
-        }
-        val harTilgangTilBehandledeEnhet =
-            enheterNavIdentHarTilgangTil.any {
-                it.enhetsnummer == arbeidsfordelingsenhet.enhetId
-            }
-        if (!harTilgangTilBehandledeEnhet) {
-            // Velger bare det første enhetsnummeret i tilfeller hvor man har flere, avklart med fag
-            val nyBehandlendeEnhet = enheterNavIdentHarTilgangTil.first()
-            return Arbeidsfordelingsenhet(
-                nyBehandlendeEnhet.enhetsnummer,
-                nyBehandlendeEnhet.enhetsnavn,
-            )
-        }
-        return Arbeidsfordelingsenhet(
-            arbeidsfordelingsenhet.enhetId,
-            arbeidsfordelingsenhet.enhetNavn,
         )
     }
 
