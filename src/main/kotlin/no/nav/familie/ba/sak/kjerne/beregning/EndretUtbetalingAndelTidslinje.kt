aadb78ba5bcd550e1a2d6a6ec28c28a30eca3fb7
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2025-02-20 12:53:32 +0100

    Ta i bruk tidslinje fra familie-felles i EndretUtbetalingAndelTidslinje

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/beregning/EndretUtbetalingAndelTidslinje.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/beregning/EndretUtbetalingAndelTidslinje.kt
@@ -1,11 +1,16 @@
 package no.nav.familie.ba.sak.kjerne.beregning
 
 import no.nav.familie.ba.sak.common.Feil
+import no.nav.familie.ba.sak.common.førsteDagIInneværendeMåned
+import no.nav.familie.ba.sak.common.sisteDagIInneværendeMåned
 import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.EndretUtbetalingAndel
 import no.nav.familie.ba.sak.kjerne.tidslinje.Periode
 import no.nav.familie.ba.sak.kjerne.tidslinje.Tidslinje
 import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.Måned
 import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.MånedTidspunkt.Companion.tilTidspunkt
+import no.nav.familie.tidslinje.tilTidslinje
+import no.nav.familie.tidslinje.Periode as FamilieFellesPeriode
+import no.nav.familie.tidslinje.Tidslinje as FamilieFellesTidslinje
 
 class EndretUtbetalingAndelTidslinje(
     private val endretUtbetalingAndeler: List<EndretUtbetalingAndel>,
@@ -19,3 +24,13 @@ class EndretUtbetalingAndelTidslinje(
             )
         }
 }
+
+fun List<EndretUtbetalingAndel>.tilTidslinje(): FamilieFellesTidslinje<EndretUtbetalingAndel> =
+    this
+        .map {
+            FamilieFellesPeriode(
+                verdi = it,
+                fom = it.fom?.førsteDagIInneværendeMåned(),
+                tom = it.tom?.sisteDagIInneværendeMåned(),
+            )
+        }.tilTidslinje()
