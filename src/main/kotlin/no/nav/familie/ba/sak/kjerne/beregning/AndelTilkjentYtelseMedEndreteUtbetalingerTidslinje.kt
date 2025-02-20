e0a6b6010186bcafbf276e69b2845f57387f7f57
Magnus Tønnessen <magnustonnessen@gmail.com>
2025-02-20 10:54:21 +0100

    Bytt til tidslinjebibliotek fra familie-felles (#5089)
    ### 💰 Hva skal gjøres, og hvorfor?
    
    Favro: NAV-23270
    
    Fortsettelse av #5086 
    Bytter ut tidslinje fra ba-sak med tidslinje fra familie-felles enkelte
    steder.
    Holder PR'ene små, så de skal være lette å lese.
    Bør leses commit for commit.
    Det er noe duplikatkode, men dette vil fjernes i senere PR'er

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/beregning/AndelTilkjentYtelseMedEndreteUtbetalingerTidslinje.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/beregning/AndelTilkjentYtelseMedEndreteUtbetalingerTidslinje.kt
@@ -1,10 +1,15 @@
 package no.nav.familie.ba.sak.kjerne.beregning
 
+import no.nav.familie.ba.sak.common.førsteDagIInneværendeMåned
+import no.nav.familie.ba.sak.common.sisteDagIInneværendeMåned
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelseMedEndreteUtbetalinger
 import no.nav.familie.ba.sak.kjerne.tidslinje.Periode
 import no.nav.familie.ba.sak.kjerne.tidslinje.Tidslinje
 import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.Måned
 import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.MånedTidspunkt.Companion.tilTidspunkt
+import no.nav.familie.tidslinje.tilTidslinje
+import no.nav.familie.tidslinje.Periode as FamilieFellesPeriode
+import no.nav.familie.tidslinje.Tidslinje as FamilieFellesTidslinje
 
 class AndelTilkjentYtelseMedEndreteUtbetalingerTidslinje(
     private val andelerTilkjentYtelse: List<AndelTilkjentYtelseMedEndreteUtbetalinger>,
@@ -18,3 +23,13 @@ class AndelTilkjentYtelseMedEndreteUtbetalingerTidslinje(
             )
         }
 }
+
+fun Collection<AndelTilkjentYtelseMedEndreteUtbetalinger>.tilTidslinje(): FamilieFellesTidslinje<AndelTilkjentYtelseMedEndreteUtbetalinger> =
+    this
+        .map {
+            FamilieFellesPeriode(
+                verdi = it,
+                fom = it.stønadFom.førsteDagIInneværendeMåned(),
+                tom = it.stønadTom.sisteDagIInneværendeMåned(),
+            )
+        }.tilTidslinje()
--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/småbarnstilleggkorrigering/SmåbarnstilleggKorrigeringService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/småbarnstilleggkorrigering/SmåbarnstilleggKorrigeringService.kt
@@ -2,7 +2,6 @@ package no.nav.familie.ba.sak.kjerne.småbarnstilleggkorrigering
 
 import jakarta.transaction.Transactional
 import no.nav.familie.ba.sak.common.FunksjonellFeil
-import no.nav.familie.ba.sak.common.opprettBooleanTidslinje
 import no.nav.familie.ba.sak.common.tilMånedÅr
 import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandling
 import no.nav.familie.ba.sak.kjerne.beregning.AndelTilkjentYtelseForTidslinje
@@ -11,14 +10,15 @@ import no.nav.familie.ba.sak.kjerne.beregning.domene.SatsType
 import no.nav.familie.ba.sak.kjerne.beregning.domene.TilkjentYtelseRepository
 import no.nav.familie.ba.sak.kjerne.beregning.domene.YtelseType
 import no.nav.familie.ba.sak.kjerne.beregning.oppdaterTilkjentYtelse
-import no.nav.familie.ba.sak.kjerne.beregning.satstypeTidslinje
+import no.nav.familie.ba.sak.kjerne.beregning.satstypeFamilieFellesTidslinje
 import no.nav.familie.ba.sak.kjerne.beregning.tilAndelerTilkjentYtelse
 import no.nav.familie.ba.sak.kjerne.beregning.tilTryggTidslinjeForSøkersYtelse
 import no.nav.familie.ba.sak.kjerne.logg.LoggService
-import no.nav.familie.ba.sak.kjerne.tidslinje.komposisjon.harIkkeOverlappMed
-import no.nav.familie.ba.sak.kjerne.tidslinje.komposisjon.harOverlappMed
-import no.nav.familie.ba.sak.kjerne.tidslinje.komposisjon.kombinerMed
-import no.nav.familie.ba.sak.kjerne.tidslinje.komposisjon.kombinerUtenNullMed
+import no.nav.familie.ba.sak.kjerne.tidslinjefamiliefelles.komposisjon.harIkkeOverlappMed
+import no.nav.familie.ba.sak.kjerne.tidslinjefamiliefelles.komposisjon.harOverlappMed
+import no.nav.familie.ba.sak.kjerne.tidslinjefamiliefelles.komposisjon.kombinerUtenNullMed
+import no.nav.familie.ba.sak.kjerne.tidslinjefamiliefelles.komposisjon.opprettBooleanTidslinje
+import no.nav.familie.tidslinje.utvidelser.kombinerMed
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
 import java.time.YearMonth
@@ -45,7 +45,7 @@ class SmåbarnstilleggKorrigeringService(
 
         val nyeSmåbarnstillegg =
             skalOpprettesTidslinje
-                .kombinerUtenNullMed(satstypeTidslinje(SatsType.SMA)) { _, sats ->
+                .kombinerUtenNullMed(satstypeFamilieFellesTidslinje(SatsType.SMA)) { _, sats ->
                     AndelTilkjentYtelseForTidslinje(
                         aktør = behandling.fagsak.aktør,
                         ytelseType = YtelseType.SMÅBARNSTILLEGG,
