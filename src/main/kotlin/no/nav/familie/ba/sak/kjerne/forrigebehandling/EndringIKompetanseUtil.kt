63a917f9c1e2e3532eca521718f4c74c36e56777
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2025-02-28 12:21:14 +0100

    Ta i bruk tidslinje fra familie-felles i EndringIKompetanseUtil.kt

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/forrigebehandling/EndringIKompetanseUtil.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/forrigebehandling/EndringIKompetanseUtil.kt
@@ -2,15 +2,14 @@ package no.nav.familie.ba.sak.kjerne.forrigebehandling
 
 import no.nav.familie.ba.sak.kjerne.eøs.felles.beregning.tilTidslinje
 import no.nav.familie.ba.sak.kjerne.eøs.kompetanse.domene.Kompetanse
-import no.nav.familie.ba.sak.kjerne.tidslinje.Tidslinje
-import no.nav.familie.ba.sak.kjerne.tidslinje.komposisjon.kombinerUtenNullMed
-import no.nav.familie.ba.sak.kjerne.tidslinje.tidspunkt.Måned
+import no.nav.familie.ba.sak.kjerne.tidslinjefamiliefelles.komposisjon.kombinerUtenNullMed
+import no.nav.familie.tidslinje.Tidslinje
 
 object EndringIKompetanseUtil {
     fun lagEndringIKompetanseForPersonTidslinje(
         nåværendeKompetanserForPerson: List<Kompetanse>,
         forrigeKompetanserForPerson: List<Kompetanse>,
-    ): Tidslinje<Boolean, Måned> {
+    ): Tidslinje<Boolean> {
         val nåværendeTidslinje = nåværendeKompetanserForPerson.tilTidslinje()
         val forrigeTidslinje = forrigeKompetanserForPerson.tilTidslinje()
 
