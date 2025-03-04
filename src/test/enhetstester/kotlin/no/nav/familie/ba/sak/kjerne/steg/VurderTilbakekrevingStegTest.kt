e41ca3ec8d643630506f655fb4ec0ab19ff09a57
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2025-03-04 22:07:11 +0100

    Legg til validering av 'ikke avregning' (#5134)
    ### 💰 Hva skal gjøres, og hvorfor?
    Legger til validering og feature toggle for tilbakekreving som sjekker
    at behandlingen ikke har både etterbetaling OG feilutbetaling/åpen
    tilbakekreving.
    
    ### 🔎️ Er det noe spesielt du ønsker tilbakemelding om?
    _Er det noe du er usikker på eller ønsker å diskutere? Beskriv det
    gjerne her eller kommenter koden det gjelder._
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [X] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    _Jeg har ikke skrevet tester fordi:_
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [ ] Nei

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/steg/VurderTilbakekrevingStegTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/steg/VurderTilbakekrevingStegTest.kt
@@ -3,6 +3,8 @@ package no.nav.familie.ba.sak.kjerne.steg
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import no.nav.familie.ba.sak.common.FunksjonellFeil
+import no.nav.familie.ba.sak.config.featureToggle.UnleashNextMedContextService
 import no.nav.familie.ba.sak.datagenerator.lagBehandling
 import no.nav.familie.ba.sak.ekstern.restDomene.RestTilbakekreving
 import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandling
@@ -11,17 +13,21 @@ import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingÅrsak
 import no.nav.familie.ba.sak.kjerne.simulering.SimuleringService
 import no.nav.familie.ba.sak.kjerne.tilbakekreving.TilbakekrevingService
 import no.nav.familie.kontrakter.felles.tilbakekreving.Tilbakekrevingsvalg
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import java.math.BigDecimal
 
 class VurderTilbakekrevingStegTest {
     private val tilbakekrevingService: TilbakekrevingService = mockk()
     private val simuleringService: SimuleringService = mockk()
+    private val unleashService: UnleashNextMedContextService = mockk()
 
     private val vurderTilbakekrevingSteg: VurderTilbakekrevingSteg =
-        VurderTilbakekrevingSteg(tilbakekrevingService = tilbakekrevingService, simuleringService = simuleringService)
+        VurderTilbakekrevingSteg(tilbakekrevingService = tilbakekrevingService, simuleringService = simuleringService, unleashService = unleashService)
 
     private val behandling: Behandling =
         lagBehandling(
@@ -41,6 +47,8 @@ class VurderTilbakekrevingStegTest {
         every { tilbakekrevingService.søkerHarÅpenTilbakekreving(any()) } returns false
         every { tilbakekrevingService.validerRestTilbakekreving(any(), any()) } returns Unit
         every { tilbakekrevingService.lagreTilbakekreving(any(), any()) } returns null
+        every { simuleringService.hentEtterbetaling(any(classifier = Long::class)) } returns BigDecimal.ZERO
+        every { unleashService.isEnabled(toggle = any()) } returns true
     }
 
     @Test
@@ -70,5 +78,38 @@ class VurderTilbakekrevingStegTest {
         assertTrue { stegType == StegType.SEND_TIL_BESLUTTER }
         verify(exactly = 0) { tilbakekrevingService.validerRestTilbakekreving(restTilbakekreving, behandling.id) }
         verify(exactly = 0) { tilbakekrevingService.lagreTilbakekreving(restTilbakekreving, behandling.id) }
+    }
+
+    @Test
+    fun `kaster FunksjonellFeil hvis behandling både har etterbetaling og feilutbetaling`() {
+        every { simuleringService.hentEtterbetaling(any(classifier = Long::class)) } returns BigDecimal(100)
+        every { simuleringService.hentFeilutbetaling(any(classifier = Long::class)) } returns BigDecimal(100)
+
+        val funksjonellFeil =
+            assertThrows<FunksjonellFeil> {
+                vurderTilbakekrevingSteg.utførStegOgAngiNeste(
+                    behandling,
+                    restTilbakekreving,
+                )
+            }
+
+        assertThat(funksjonellFeil.melding).isEqualTo("Løsningen i dag legger opp til automatisk avregning der feilutbetalinger trekkes mot etterbetalinger. Dette har vi ikke hjemmel for. Du må derfor splitte saken for å gå videre.")
+    }
+
+    @Test
+    fun `kaster FunksjonellFeil hvis behandling både har etterbetaling og fagsak har åpen tilbakekreving`() {
+        every { simuleringService.hentEtterbetaling(any(classifier = Long::class)) } returns BigDecimal(100)
+        every { simuleringService.hentFeilutbetaling(any(classifier = Long::class)) } returns BigDecimal.ZERO
+        every { tilbakekrevingService.søkerHarÅpenTilbakekreving(any(classifier = Long::class)) } returns true
+
+        val funksjonellFeil =
+            assertThrows<FunksjonellFeil> {
+                vurderTilbakekrevingSteg.utførStegOgAngiNeste(
+                    behandling,
+                    restTilbakekreving,
+                )
+            }
+
+        assertThat(funksjonellFeil.melding).isEqualTo("Løsningen i dag legger opp til automatisk avregning der feilutbetalinger trekkes mot etterbetalinger. Dette har vi ikke hjemmel for. Du må derfor splitte saken for å gå videre.")
     }
 }
