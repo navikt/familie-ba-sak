09499aa4d8592a5849fd5bc74951cbc4b3d78471
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2025-03-26 09:58:30 +0100

    NAV-24658: Utivder logikken for relatert behandling id for å ta hensyn til klage, samt legger til relatert behandling fagsystem (#5190)
    ### 💰 Hva skal gjøres, og hvorfor?
    Favro:
    [NAV-24658](https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-24658)
    
    Ønsker å sende over `relatertBehandlingId` og
    `relatertBehandlingFagsystem` til DVH for statistikk.
    `relatertBehandlingId` må kunne peke på både barnetrygdbehandlinger og
    klagebehandlinger. Hvis man har en behandling av type `REVURDERING` med
    årsak `KLAGE` eller `IVERKSETTE_VA_VEDTAK` skal man bruke den siste
    vedtatte klagebehandlingen. I andre tilfeller skal man bruke, om det
    finnes, den siste vedtatte barnetrygdbehandlingen. Den siste vedtatte
    barnetrygdbehandlingen må være av type `REVURDERING` eller
    `TEKNISK_ENDRING` for å bli tatt med som en relatert behandling. Om man
    har en behandling av type `REVURDERING` med årsak `KLAGE` eller
    `IVERKSETTE_VA_VEDTAK` og det ikke finnes en vedtatt klagebehandling
    skal man kaste feil.
    
    Har deployet til preprod og testet kafka-integrasjonen. Nedenfor ser du
    bilder fra Kafka Manager.
    
    Her fra en barnetrygdbehandling som er en revurdering som ikke har årsak
    klage eller iverksette ka vedtak
    
    ![image](https://github.com/user-attachments/assets/caea5e59-a98a-4707-9171-8b88fd5cb6e7)
    
    Her fra en barnetrygdbehandling som er en revurdering med årsak klage
    
    ![image](https://github.com/user-attachments/assets/b6da72a1-a0c6-4616-8881-7aa2f346adee)
    
    Relatert PR for KS: https://github.com/navikt/familie-ks-sak/pull/1183
    
    ### 🔎️ Er det noe spesielt du ønsker tilbakemelding om?
    Nei
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [x] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [x] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei, men kan ta om ønskelig

---
+++ src/main/kotlin/no/nav/familie/ba/sak/statistikk/saksstatistikk/RelatertBehandlingUtleder.kt
@@ -0,0 +1,42 @@
+package no.nav.familie.ba.sak.statistikk.saksstatistikk
+
+import no.nav.familie.ba.sak.common.Feil
+import no.nav.familie.ba.sak.config.FeatureToggle
+import no.nav.familie.ba.sak.config.featureToggle.UnleashNextMedContextService
+import no.nav.familie.ba.sak.kjerne.behandling.BehandlingHentOgPersisterService
+import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandling
+import no.nav.familie.ba.sak.kjerne.klage.KlageService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Component
+class RelatertBehandlingUtleder(
+    private val behandlingHentOgPersisterService: BehandlingHentOgPersisterService,
+    private val klageService: KlageService,
+    private val unleashService: UnleashNextMedContextService,
+) {
+    private val logger: Logger = LoggerFactory.getLogger(RelatertBehandlingUtleder::class.java)
+
+    fun utledRelatertBehandling(behandling: Behandling): RelatertBehandling? {
+        if (behandling.erRevurderingKlage() && unleashService.isEnabled(FeatureToggle.BEHANDLE_KLAGE, false)) {
+            val sisteVedtatteKlagebehandling = klageService.hentSisteVedtatteKlagebehandling(behandling.fagsak.id)
+            if (sisteVedtatteKlagebehandling == null) {
+                throw Feil("Forventer en vedtatt klagebehandling for fagsak ${behandling.fagsak.id} og behandling ${behandling.id}")
+            }
+            return RelatertBehandling.fraKlagebehandling(sisteVedtatteKlagebehandling)
+        }
+
+        if (behandling.erRevurderingEllerTekniskEndring()) {
+            val sisteVedtatteBarnetrygdbehandling =
+                behandlingHentOgPersisterService.hentForrigeBehandlingSomErVedtatt(behandling)
+            if (sisteVedtatteBarnetrygdbehandling == null) {
+                logger.warn("Forventer en vedtatt barnetrygdbehandling for fagsak ${behandling.fagsak.id} og behandling ${behandling.id}")
+                return null
+            }
+            return RelatertBehandling.fraBarnetrygdbehandling(sisteVedtatteBarnetrygdbehandling)
+        }
+
+        return null
+    }
+}
