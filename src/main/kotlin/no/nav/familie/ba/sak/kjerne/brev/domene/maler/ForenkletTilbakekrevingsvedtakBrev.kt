c1113b6a905808c6440bc0083ddc91320a7b05f4
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2025-04-09 14:45:03 +0200

    Endre hvordan fritekst blir sendt med i ForenkletTilbakekrevingsvedtakBrev

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/brev/domene/maler/ForenkletTilbakekrevingsvedtakBrev.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/brev/domene/maler/ForenkletTilbakekrevingsvedtakBrev.kt
@@ -11,23 +11,21 @@ data class ForenkletTilbakekrevingsvedtakBrev(
 data class ForenkletTilbakekrevingsvedtakBrevData(
     override val delmalData: DelmalData,
     override val flettefelter: Flettefelter,
+    val fritekst: String,
 ) : BrevData {
     data class Flettefelter(
         override val navn: Flettefelt,
         override val fodselsnummer: Flettefelt,
         override val brevOpprettetDato: Flettefelt = flettefelt(LocalDate.now().tilDagMånedÅr()),
-        val forenkletTilbakekrevingsvedtakTekst: Flettefelt,
     ) : FlettefelterForDokument {
         constructor(
             navn: String,
             fodselsnummer: String,
             brevOpprettetDato: LocalDate,
-            forenkletTilbakekrevingsvedtakTekst: String,
         ) : this(
             navn = flettefelt(navn),
             fodselsnummer = flettefelt(fodselsnummer),
             brevOpprettetDato = flettefelt(brevOpprettetDato.tilDagMånedÅr()),
-            forenkletTilbakekrevingsvedtakTekst = flettefelt(forenkletTilbakekrevingsvedtakTekst),
         )
     }
 
