327705c4cb82ef774e7248a4c863071118632cf5
Magnus Tønnessen <magnustonnessen@gmail.com>
2025-05-28 16:10:35 +0200

    Automatisk registrering av søknad (#5346)
    ### 💰 Hva skal gjøres, og hvorfor?
    
    Favro: NAV-25246
    
    Legger til funksjonalitet for å automatisk gjennomføre
    `RegistrereSøknad`-steget, ved å hente søknaden som er knyttet til
    behandlingen fra `familie-integrasjoner`, og lage `RestRegistrerSøknad`
    med data fra søknaden. Legger all funksjonaliteten i en `try/catch`,
    slik at dersom et kall feiler / søknadsreferansen ikke finnes, feiler
    ikke hele tasken, men søknaden må registreres manuelt av saksbehandler.

---
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/steg/AutomatiskRegistrerSøknadService.kt
@@ -0,0 +1,89 @@
+package no.nav.familie.ba.sak.kjerne.steg
+
+import no.nav.familie.ba.sak.common.Feil
+import no.nav.familie.ba.sak.ekstern.restDomene.BarnMedOpplysninger
+import no.nav.familie.ba.sak.ekstern.restDomene.RestRegistrerSøknad
+import no.nav.familie.ba.sak.ekstern.restDomene.SøkerMedOpplysninger
+import no.nav.familie.ba.sak.ekstern.restDomene.SøknadDTO
+import no.nav.familie.ba.sak.ekstern.restDomene.tilDto
+import no.nav.familie.ba.sak.integrasjoner.pdl.PersonopplysningerService
+import no.nav.familie.ba.sak.kjerne.behandling.domene.Behandling
+import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersongrunnlagService
+import no.nav.familie.ba.sak.kjerne.personident.PersonidentService
+import no.nav.familie.ba.sak.kjerne.søknad.SøknadService
+import no.nav.familie.kontrakter.felles.personopplysning.ADRESSEBESKYTTELSEGRADERING
+import no.nav.familie.kontrakter.felles.personopplysning.FORELDERBARNRELASJONROLLE
+import org.springframework.stereotype.Service
+
+@Service
+class AutomatiskRegistrerSøknadService(
+    private val søknadService: SøknadService,
+    private val persongrunnlagService: PersongrunnlagService,
+    private val personopplysningerService: PersonopplysningerService,
+    private val personidentService: PersonidentService,
+) {
+    fun lagRestRegistrerSøknad(behandling: Behandling): RestRegistrerSøknad {
+        val søknad =
+            søknadService.finnSøknad(behandling.id)
+                ?: throw Feil("Fant ikke søknad for behandling med id ${behandling.id}")
+
+        val underkategori = søknad.behandlingUnderkategori.tilDto()
+
+        val søker = persongrunnlagService.hentSøker(behandling.id)
+        val søkerMedOpplysninger =
+            SøkerMedOpplysninger(
+                ident = søker.aktør.aktivFødselsnummer(),
+                målform = søknad.målform,
+            )
+
+        val barnMedRelasjon =
+            personopplysningerService
+                .hentPersoninfoMedRelasjonerOgRegisterinformasjon(søker.aktør)
+                .forelderBarnRelasjon
+                .filter { it.relasjonsrolle == FORELDERBARNRELASJONROLLE.BARN }
+                .mapNotNull { barn ->
+                    // Filtrer bort barn som har adressebeskyttelse og som ikke er med i søknaden
+                    if (barn.adressebeskyttelseGradering == null ||
+                        barn.adressebeskyttelseGradering == ADRESSEBESKYTTELSEGRADERING.UGRADERT ||
+                        søknad.barn.any { it.fnr == barn.aktør.aktivFødselsnummer() }
+                    ) {
+                        BarnMedOpplysninger(
+                            ident = barn.aktør.aktivFødselsnummer(),
+                            navn = barn.navn ?: "Mangler navn",
+                            fødselsdato = barn.fødselsdato,
+                            inkludertISøknaden = søknad.barn.any { it.fnr == barn.aktør.aktivFødselsnummer() },
+                        )
+                    } else {
+                        null
+                    }
+                }
+
+        val barnUtenRelasjonFraSøknad =
+            søknad.barn
+                .filter { barn -> barnMedRelasjon.none { it.ident == barn.fnr } }
+                .map {
+                    val barnAktør = personidentService.hentAktør(it.fnr)
+                    val barn = personopplysningerService.hentPersoninfoEnkel(barnAktør)
+
+                    BarnMedOpplysninger(
+                        ident = barnAktør.aktivFødselsnummer(),
+                        navn = barn.navn ?: "Mangler navn",
+                        fødselsdato = barn.fødselsdato,
+                        inkludertISøknaden = true,
+                    )
+                }
+
+        val barnaMedOpplysninger = (barnMedRelasjon + barnUtenRelasjonFraSøknad).distinctBy { it.ident }
+
+        return RestRegistrerSøknad(
+            søknad =
+                SøknadDTO(
+                    underkategori = underkategori,
+                    søkerMedOpplysninger = søkerMedOpplysninger,
+                    barnaMedOpplysninger = barnaMedOpplysninger,
+                    endringAvOpplysningerBegrunnelse = "",
+                ),
+            bekreftEndringerViaFrontend = false,
+        )
+    }
+}
