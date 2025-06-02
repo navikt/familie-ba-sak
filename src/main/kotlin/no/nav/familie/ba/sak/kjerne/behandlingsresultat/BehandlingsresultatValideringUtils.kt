bfa42fd849e786acf5ba1aedb9e4180794108087
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2025-06-02 12:05:12 +0200

    Ikke konverter Set til List

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatValideringUtils.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/behandlingsresultat/BehandlingsresultatValideringUtils.kt
@@ -76,8 +76,8 @@ object BehandlingsresultatValideringUtils {
     }
 
     fun validerIngenEndringTilbakeITid(
-        andelerDenneBehandlingen: List<AndelTilkjentYtelse>,
-        andelerForrigeBehandling: List<AndelTilkjentYtelse>,
+        andelerDenneBehandlingen: Collection<AndelTilkjentYtelse>,
+        andelerForrigeBehandling: Collection<AndelTilkjentYtelse>,
         nåMåned: YearMonth,
     ) {
         val forrigeMåned = nåMåned.minusMonths(1)
@@ -97,8 +97,8 @@ object BehandlingsresultatValideringUtils {
     }
 
     fun validerSatsErUendret(
-        andelerDenneBehandlingen: List<AndelTilkjentYtelse>,
-        andelerForrigeBehandling: List<AndelTilkjentYtelse>,
+        andelerDenneBehandlingen: Collection<AndelTilkjentYtelse>,
+        andelerForrigeBehandling: Collection<AndelTilkjentYtelse>,
     ) {
         val andelerDenneBehandlingTidslinje = andelerDenneBehandlingen.tilTidslinjerPerAktørOgType()
         val andelerForrigeBehanldingTidslinje = andelerForrigeBehandling.tilTidslinjerPerAktørOgType()
