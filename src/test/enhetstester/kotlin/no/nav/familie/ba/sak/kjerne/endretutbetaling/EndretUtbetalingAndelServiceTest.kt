9ab1df5433f3e50d60fe8bb750a14202e1925aa0
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2025-06-03 13:36:40 +0200

    Test at RestEndretUtbetalingAndel funker med nytt og gammelt felt for personidenter

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/endretutbetaling/EndretUtbetalingAndelServiceTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/endretutbetaling/EndretUtbetalingAndelServiceTest.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ba.sak.kjerne.endretutbetaling
 
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.slot
 import no.nav.familie.ba.sak.common.FunksjonellFeil
 import no.nav.familie.ba.sak.common.førsteDagIInneværendeMåned
 import no.nav.familie.ba.sak.datagenerator.lagAndelTilkjentYtelse
@@ -10,10 +11,12 @@ import no.nav.familie.ba.sak.datagenerator.lagEndretUtbetalingAndelMedAndelerTil
 import no.nav.familie.ba.sak.datagenerator.lagPerson
 import no.nav.familie.ba.sak.datagenerator.lagPersonResultat
 import no.nav.familie.ba.sak.datagenerator.lagTestPersonopplysningGrunnlag
+import no.nav.familie.ba.sak.ekstern.restDomene.RestEndretUtbetalingAndel
 import no.nav.familie.ba.sak.kjerne.autovedtak.fødselshendelse.Resultat
 import no.nav.familie.ba.sak.kjerne.beregning.BeregningService
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelse
 import no.nav.familie.ba.sak.kjerne.beregning.domene.AndelTilkjentYtelseRepository
+import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.EndretUtbetalingAndel
 import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.EndretUtbetalingAndelRepository
 import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.tilRestEndretUtbetalingAndel
 import no.nav.familie.ba.sak.kjerne.endretutbetaling.domene.Årsak
@@ -23,10 +26,13 @@ import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.Personopplysning
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.VilkårsvurderingService
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.Vilkår
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.Vilkårsvurdering
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.YearMonth
 
@@ -37,17 +43,17 @@ class EndretUtbetalingAndelServiceTest {
     private val mockAndelTilkjentYtelseRepository = mockk<AndelTilkjentYtelseRepository>()
     private val mockVilkårsvurderingService = mockk<VilkårsvurderingService>()
     private val mockEndretUtbetalingAndelHentOgPersisterService = mockk<EndretUtbetalingAndelHentOgPersisterService>()
+    private val mockBeregningService = mockk<BeregningService>()
 
     private lateinit var endretUtbetalingAndelService: EndretUtbetalingAndelService
 
     @BeforeEach
     fun setup() {
-        val beregningService = mockk<BeregningService>()
         endretUtbetalingAndelService =
             EndretUtbetalingAndelService(
                 endretUtbetalingAndelRepository = mockEndretUtbetalingAndelRepository,
                 personopplysningGrunnlagRepository = mockPersonopplysningGrunnlagRepository,
-                beregningService = beregningService,
+                beregningService = mockBeregningService,
                 persongrunnlagService = mockPersongrunnlagService,
                 andelTilkjentYtelseRepository = mockAndelTilkjentYtelseRepository,
                 vilkårsvurderingService = mockVilkårsvurderingService,
@@ -124,6 +130,141 @@ class EndretUtbetalingAndelServiceTest {
         Assertions.assertEquals(
             "Du har valgt årsaken 'delt bosted', denne samstemmer ikke med vurderingene gjort på vilkårsvurderingssiden i perioden du har valgt.",
             feil.frontendFeilmelding,
+        )
+    }
+
+    @Test
+    fun `Skal håndtere RestEndretUtbetalingAndel med blanding av nytt og gammelt felt for personIdent(er)`() {
+        // Arrange
+        val behandling = lagBehandling()
+        val barn1 = lagPerson(type = PersonType.BARN)
+        val barn2 = lagPerson(type = PersonType.BARN)
+
+        val endretUtbetalingAndel = EndretUtbetalingAndel(behandlingId = behandling.id)
+
+        val andelerTilkjentYtelse =
+            listOf(
+                lagAndelTilkjentYtelse(
+                    person = barn1,
+                    fom = YearMonth.of(2025, 1),
+                    tom = YearMonth.of(2025, 1),
+                ),
+                lagAndelTilkjentYtelse(
+                    person = barn2,
+                    fom = YearMonth.of(2025, 1),
+                    tom = YearMonth.of(2025, 1),
+                ),
+            )
+
+        val vilkårsvurdering =
+            Vilkårsvurdering(behandling = behandling).apply {
+                personResultater =
+                    setOf(
+                        lagPersonResultat(
+                            vilkårsvurdering = this,
+                            person = barn1,
+                            resultat = Resultat.OPPFYLT,
+                            periodeFom = LocalDate.of(2024, 12, 1),
+                            periodeTom = LocalDate.of(2025, 2, 1),
+                            lagFullstendigVilkårResultat = true,
+                            personType = PersonType.BARN,
+                            vilkårType = Vilkår.BOR_MED_SØKER,
+                        ),
+                        lagPersonResultat(
+                            vilkårsvurdering = this,
+                            person = barn2,
+                            resultat = Resultat.OPPFYLT,
+                            periodeFom = LocalDate.of(2024, 12, 1),
+                            periodeTom = LocalDate.of(2025, 2, 1),
+                            lagFullstendigVilkårResultat = true,
+                            personType = PersonType.BARN,
+                            vilkårType = Vilkår.BOR_MED_SØKER,
+                        ),
+                    )
+            }
+
+        val lagretEndretUtbetalingAndel = slot<EndretUtbetalingAndel>()
+
+        every { mockEndretUtbetalingAndelRepository.getReferenceById(any()) } returns endretUtbetalingAndel
+        every { mockPersongrunnlagService.hentPersonerPåBehandling(any(), behandling) } answers {
+            listOf(barn1, barn2).filter { it.aktør.aktivFødselsnummer() in firstArg<List<String>>() }
+        }
+        every { mockPersonopplysningGrunnlagRepository.findByBehandlingAndAktiv(behandling.id) } returns
+            lagTestPersonopplysningGrunnlag(behandling.id, barn1, barn2)
+        every { mockAndelTilkjentYtelseRepository.finnAndelerTilkjentYtelseForBehandling(behandlingId = behandling.id) } returns andelerTilkjentYtelse
+        every { mockEndretUtbetalingAndelHentOgPersisterService.hentForBehandling(behandlingId = behandling.id) } returns listOf(endretUtbetalingAndel)
+        every { mockVilkårsvurderingService.hentAktivForBehandling(behandlingId = behandling.id) } returns vilkårsvurdering
+        every { mockEndretUtbetalingAndelRepository.saveAndFlush(capture(lagretEndretUtbetalingAndel)) } returns mockk()
+        every { mockBeregningService.oppdaterBehandlingMedBeregning(any(), any(), any()) } returns mockk()
+
+        var restEndretUtbetalingAndel =
+            RestEndretUtbetalingAndel(
+                id = 0,
+                personIdent = null,
+                personIdenter = null,
+                prosent = BigDecimal.ZERO,
+                fom = YearMonth.of(2025, 1),
+                tom = YearMonth.of(2025, 1),
+                årsak = Årsak.ALLEREDE_UTBETALT,
+                avtaletidspunktDeltBosted = LocalDate.now(),
+                søknadstidspunkt = LocalDate.now(),
+                begrunnelse = "Test begrunnelse",
+                erTilknyttetAndeler = true,
+            )
+
+        // Act & Assert
+        assertThrows<FunksjonellFeil> {
+            endretUtbetalingAndelService.oppdaterEndretUtbetalingAndelOgOppdaterTilkjentYtelse(
+                behandling = behandling,
+                endretUtbetalingAndelId = 0,
+                restEndretUtbetalingAndel = restEndretUtbetalingAndel,
+            )
+        }
+
+        // Arrange
+        restEndretUtbetalingAndel =
+            restEndretUtbetalingAndel.copy(
+                personIdenter = null,
+                personIdent = barn1.aktør.aktivFødselsnummer(),
+            )
+
+        // Act & Assert
+        assertDoesNotThrow {
+            endretUtbetalingAndelService.oppdaterEndretUtbetalingAndelOgOppdaterTilkjentYtelse(
+                behandling = behandling,
+                endretUtbetalingAndelId = 0,
+                restEndretUtbetalingAndel = restEndretUtbetalingAndel,
+            )
+        }
+
+        // Assert
+        assertThat(lagretEndretUtbetalingAndel.captured).isEqualTo(
+            endretUtbetalingAndel.copy(
+                personer = mutableSetOf(barn1),
+            ),
+        )
+
+        // Arrange
+        restEndretUtbetalingAndel =
+            restEndretUtbetalingAndel.copy(
+                personIdenter = listOf(barn1.aktør.aktivFødselsnummer(), barn2.aktør.aktivFødselsnummer()),
+                personIdent = barn1.aktør.aktivFødselsnummer(),
+            )
+
+        // Act & Assert
+        assertDoesNotThrow {
+            endretUtbetalingAndelService.oppdaterEndretUtbetalingAndelOgOppdaterTilkjentYtelse(
+                behandling = behandling,
+                endretUtbetalingAndelId = 0,
+                restEndretUtbetalingAndel = restEndretUtbetalingAndel,
+            )
+        }
+
+        // Assert
+        assertThat(lagretEndretUtbetalingAndel.captured).isEqualTo(
+            endretUtbetalingAndel.copy(
+                personer = mutableSetOf(barn1, barn2),
+            ),
         )
     }
 }
