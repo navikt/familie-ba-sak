75084728c6ca84549adf14f40884ab2ed355b720
bragejahren <70642183+bragejahren@users.noreply.github.com>
2025-06-11 08:57:24 +0200

    NAV-25263: Legger til ny vedtaksperiode dersom satsendring av ordinær påvirker utvidet barnetrygd pga differanseberegning (#5382)
    Favro:
    [NAV-25263](https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-25263)
    
    ### 💰 Hva skal gjøres, og hvorfor?
    Satsendring av ordinær barnetrygd kan, i EØS-sekundærland-saker, påvirke
    utbetalingen av utvidet barnetrygd dersom det andre landet utbetaler en
    høyere sum enn den Norge utbetaler. Når det andre landet utbetaler mer
    enn Norge vil en økning av ordinær barnetrygd føre til at vi trekker
    mindre fra den utvidede delen. Dette ble ikke reflektert i
    vedtaksperiodene. Det ble altså ingen splitt dersom den ordinære
    barnetrygden ikke skulle utbetales til tross for at satsen endret seg.
    
    Legger her inn logikk som allikevel oppretter splitt ved nullutbetaling
    dersom sats er endret samtidig som det utbetales en høyere sum fra det
    andre landet, aka negativ differanseberegning.
    
    ### ✅ Checklist
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer.
    - [x] Jeg har skrevet tester. 
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/vedtaksperiodeProdusent/AndelForVedtaksperiodeTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/vedtaksperiodeProdusent/AndelForVedtaksperiodeTest.kt
@@ -83,7 +83,7 @@ class AndelForVedtaksperiodeTest {
     }
 
     @Test
-    fun `equals skal bli true for nullutbetalinger selv om satsen endrer seg`() {
+    fun `equals skal bli true for nullutbetalinger når satsen endrer seg og differanseberegnet periodebeløp er større enn 0`() {
         val andel1 =
             AndelForVedtaksperiode(
                 kalkulertUtbetalingsbeløp = 0,
@@ -106,6 +106,31 @@ class AndelForVedtaksperiodeTest {
 
         assertThat(andel1).isEqualTo(andel2)
         assertThat(andel1.hashCode()).isEqualTo(andel2.hashCode())
+    }
+
+    @Test
+    fun `equals skal bli false for nullutbetalinger når satsen endrer seg og differanseberegnet periodebeløp er mindre enn 0`() {
+        val andel1 =
+            AndelForVedtaksperiode(
+                kalkulertUtbetalingsbeløp = 0,
+                nasjonaltPeriodebeløp = 2000,
+                differanseberegnetPeriodebeløp = -200,
+                type = YtelseType.ORDINÆR_BARNETRYGD,
+                prosent = BigDecimal("50.0"),
+                sats = 100,
+            )
+
+        val andel2 =
+            AndelForVedtaksperiode(
+                kalkulertUtbetalingsbeløp = 0,
+                nasjonaltPeriodebeløp = 2000,
+                differanseberegnetPeriodebeløp = -100,
+                type = YtelseType.ORDINÆR_BARNETRYGD,
+                prosent = BigDecimal("50.0"),
+                sats = 200,
+            )
+
+        assertThat(andel1).isNotEqualTo(andel2)
     }
 
     @Test
