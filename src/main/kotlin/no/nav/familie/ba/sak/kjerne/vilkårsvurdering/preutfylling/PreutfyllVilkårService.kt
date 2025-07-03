7deb5b054ab81cf40b2596d89d4daac7381cc783
schmelita <61985450+schmelita@users.noreply.github.com>
2025-07-03 09:00:11 +0200

    Sperre preutfylling for EØS-saker (#5451)
    [Favro](https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-25673)
    
    ### 💰 Hva skal gjøres, og hvorfor?
    På grunn av utdypende vilkårsvurdering må vi sperre for EØS-saker igjen,
    da dette er komplisert å preutfylle og vi ønsker ikke å fokusere på
    dette nå.

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vilkårsvurdering/preutfylling/PreutfyllVilkårService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vilkårsvurdering/preutfylling/PreutfyllVilkårService.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ba.sak.kjerne.vilkårsvurdering.preutfylling
 
 import no.nav.familie.ba.sak.config.FeatureToggle
 import no.nav.familie.ba.sak.config.featureToggle.UnleashNextMedContextService
+import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingKategori
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.Vilkårsvurdering
 import org.springframework.stereotype.Service
 
@@ -12,6 +13,8 @@ class PreutfyllVilkårService(
     private val unleashService: UnleashNextMedContextService,
 ) {
     fun preutfyllVilkår(vilkårsvurdering: Vilkårsvurdering) {
+        if (vilkårsvurdering.behandling.kategori == BehandlingKategori.EØS) return
+
         if (unleashService.isEnabled(FeatureToggle.PREUTFYLLING_VILKÅR)) {
             preutfyllBosattIRiketService.prefutfyllBosattIRiket(vilkårsvurdering)
         }
