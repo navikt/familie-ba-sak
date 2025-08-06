d1dbeaea220771fb0a5fbdd8b243ea6782d96295
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2025-08-06 15:57:48 +0200

    Legg til autovedtakservice for Finnmarkstillegg

--- src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/pdl/domene/PdlBostedsadresseOgDeltBostedPerson.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/pdl/domene/PdlBostedsadresseOgDeltBostedPerson.kt
@@ -1,9 +1,30 @@
 package no.nav.familie.ba.sak.integrasjoner.pdl.domene
 
+import no.nav.familie.kontrakter.ba.finnmarkstillegg.kommuneErIFinnmarkEllerNordTroms
 import no.nav.familie.kontrakter.felles.personopplysning.Bostedsadresse
 import no.nav.familie.kontrakter.felles.personopplysning.DeltBosted
 
 data class PdlBostedsadresseOgDeltBostedPerson(
     val bostedsadresse: List<Bostedsadresse>,
     val deltBosted: List<DeltBosted>,
-)
+) {
+    fun nåværendeBostedEllerDeltBostedErIFinnmarkEllerNordTroms(): Boolean {
+        val sisteBostedsadresse = bostedsadresse.filter { it.gyldigFraOgMed != null && it.gyldigTilOgMed == null }.maxByOrNull { it.gyldigFraOgMed!! }
+        val sisteDeltBosted = deltBosted.filter { it.startdatoForKontrakt != null && it.sluttdatoForKontrakt == null }.maxByOrNull { it.sluttdatoForKontrakt!! }
+        val bostedskommuneErIFinnmarkEllerNordTroms =
+            (
+                sisteBostedsadresse?.vegadresse?.kommunenummer
+                    ?: sisteBostedsadresse?.matrikkeladresse?.kommunenummer
+                    ?: sisteBostedsadresse?.ukjentBosted?.bostedskommune
+            )?.let { kommuneErIFinnmarkEllerNordTroms(it) } ?: false
+
+        val gjeldendeDeltBostedskommuneErIFinnmarkEllerNordTroms =
+            (
+                sisteDeltBosted?.vegadresse?.kommunenummer
+                    ?: sisteDeltBosted?.matrikkeladresse?.kommunenummer
+                    ?: sisteDeltBosted?.ukjentBosted?.bostedskommune
+            )?.let { kommuneErIFinnmarkEllerNordTroms(it) } ?: false
+
+        return bostedskommuneErIFinnmarkEllerNordTroms || gjeldendeDeltBostedskommuneErIFinnmarkEllerNordTroms
+    }
+}
---
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/autovedtak/finnmarkstillegg/AutovedtakFinnmarkstilleggService.kt
@@ -0,0 +1,99 @@
+package no.nav.familie.ba.sak.kjerne.autovedtak.finnmarkstillegg
+
+import no.nav.familie.ba.sak.common.Feil
+import no.nav.familie.ba.sak.integrasjoner.pdl.SystemOnlyPdlRestClient
+import no.nav.familie.ba.sak.kjerne.autovedtak.AutovedtakBehandlingService
+import no.nav.familie.ba.sak.kjerne.autovedtak.AutovedtakService
+import no.nav.familie.ba.sak.kjerne.autovedtak.AutovedtakStegService
+import no.nav.familie.ba.sak.kjerne.autovedtak.FinnmarkstilleggData
+import no.nav.familie.ba.sak.kjerne.behandling.BehandlingHentOgPersisterService
+import no.nav.familie.ba.sak.kjerne.behandling.BehandlingService
+import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingStatus
+import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingType.REVURDERING
+import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingÅrsak.FINNMARKSTILLEGG
+import no.nav.familie.ba.sak.kjerne.fagsak.FagsakService
+import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersongrunnlagService
+import no.nav.familie.ba.sak.kjerne.steg.StegType
+import no.nav.familie.ba.sak.sikkerhet.SikkerhetContext
+import no.nav.familie.ba.sak.task.FerdigstillBehandlingTask
+import no.nav.familie.ba.sak.task.IverksettMotOppdragTask
+import no.nav.familie.prosessering.internal.TaskService
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class AutovedtakFinnmarkstilleggService(
+    private val autovedtakService: AutovedtakService,
+    private val fagsakService: FagsakService,
+    private val taskService: TaskService,
+    private val behandlingHentOgPersisterService: BehandlingHentOgPersisterService,
+    private val persongrunnlagService: PersongrunnlagService,
+    private val pdlRestClient: SystemOnlyPdlRestClient,
+    private val behandlingService: BehandlingService,
+) : AutovedtakBehandlingService<FinnmarkstilleggData> {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    override fun skalAutovedtakBehandles(behandlingsdata: FinnmarkstilleggData): Boolean {
+        val sisteIverksatteBehandling =
+            behandlingHentOgPersisterService.hentSisteBehandlingSomErIverksatt(behandlingsdata.fagsakId)
+                ?: return false
+
+        val identerISisteIverksatteBehandling =
+            persongrunnlagService
+                .hentAktivThrows(sisteIverksatteBehandling.id)
+                .personer
+                .map { it.aktør.aktivFødselsnummer() }
+
+        return pdlRestClient
+            .hentBostedsadresseOgDeltBostedForPersoner(identerISisteIverksatteBehandling)
+            .any { it.value.nåværendeBostedEllerDeltBostedErIFinnmarkEllerNordTroms() }
+    }
+
+    @Transactional
+    override fun kjørBehandling(behandlingsdata: FinnmarkstilleggData): String {
+        logger.info("Kjører autovedtak for Finnmarkstillegg for fagsakId=${behandlingsdata.fagsakId}")
+
+        val søkerAktør = fagsakService.hentAktør(behandlingsdata.fagsakId)
+        val behandlingEtterBehandlingsresultat =
+            autovedtakService.opprettAutomatiskBehandlingOgKjørTilBehandlingsresultat(
+                aktør = søkerAktør,
+                behandlingType = REVURDERING,
+                behandlingÅrsak = FINNMARKSTILLEGG,
+                fagsakId = behandlingsdata.fagsakId,
+            )
+
+        val opprettetVedtak =
+            autovedtakService.opprettToTrinnskontrollOgVedtaksbrevForAutomatiskBehandling(
+                behandlingEtterBehandlingsresultat,
+            )
+
+        val task =
+            when (behandlingEtterBehandlingsresultat.steg) {
+                StegType.IVERKSETT_MOT_OPPDRAG -> {
+                    IverksettMotOppdragTask.opprettTask(
+                        behandlingEtterBehandlingsresultat,
+                        opprettetVedtak,
+                        SikkerhetContext.hentSaksbehandler(),
+                    )
+                }
+
+                StegType.FERDIGSTILLE_BEHANDLING -> {
+                    behandlingService.oppdaterStatusPåBehandling(
+                        behandlingEtterBehandlingsresultat.id,
+                        BehandlingStatus.IVERKSETTER_VEDTAK,
+                    )
+                    FerdigstillBehandlingTask.opprettTask(
+                        søkerAktør.aktivFødselsnummer(),
+                        behandlingEtterBehandlingsresultat.id,
+                    )
+                }
+
+                else -> throw Feil("Ugyldig neste steg ${behandlingEtterBehandlingsresultat.steg} for behandlingsårsak $FINNMARKSTILLEGG for fagsak=${behandlingsdata.fagsakId}")
+            }
+
+        taskService.save(task)
+
+        return AutovedtakStegService.BEHANDLING_FERDIG
+    }
+}
