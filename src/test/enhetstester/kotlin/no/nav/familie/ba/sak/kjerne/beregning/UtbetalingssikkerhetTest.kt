a4b2692a17ff682c64165e111bdb46e4c8383d93
Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>
2025-08-06 09:37:48 +0200

    Barn må ha mulighet til å ha mer enn 1 andel i periode (#5494)
    Favrokort:
    https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-25830
    
    Barn må kunne ha mer enn 1 type andel gitt en periode (ordinær +
    finnmark)

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/beregning/UtbetalingssikkerhetTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/beregning/UtbetalingssikkerhetTest.kt
@@ -7,6 +7,8 @@ import no.nav.familie.ba.sak.common.inneværendeMåned
 import no.nav.familie.ba.sak.common.nesteMåned
 import no.nav.familie.ba.sak.common.tilKortString
 import no.nav.familie.ba.sak.datagenerator.lagAndelTilkjentYtelse
+import no.nav.familie.ba.sak.datagenerator.lagBehandling
+import no.nav.familie.ba.sak.datagenerator.lagFagsak
 import no.nav.familie.ba.sak.datagenerator.lagInitiellTilkjentYtelse
 import no.nav.familie.ba.sak.datagenerator.tilPersonEnkel
 import no.nav.familie.ba.sak.datagenerator.tilPersonEnkelSøkerOgBarn
@@ -66,6 +68,84 @@ class UtbetalingssikkerhetTest {
             }
 
         assertEquals("Feil med tidslinje. Overlapp på periode", feil.message)
+    }
+
+    @Test
+    fun `Skal ikke kaste feil når et barn har både finnmarkstillegg andel og ordinær for en periode`() {
+        // Arrange
+        val person = tilfeldigPerson(personType = PersonType.BARN)
+
+        val tilkjentYtelse = lagInitiellTilkjentYtelse()
+
+        tilkjentYtelse.andelerTilkjentYtelse.addAll(
+            listOf(
+                lagAndelTilkjentYtelse(
+                    inneværendeMåned().minusYears(1),
+                    inneværendeMåned().minusMonths(6),
+                    YtelseType.FINNMARKSTILLEGG,
+                    1054,
+                    person = person,
+                ),
+                lagAndelTilkjentYtelse(
+                    inneværendeMåned().minusYears(1),
+                    inneværendeMåned().minusMonths(6),
+                    YtelseType.ORDINÆR_BARNETRYGD,
+                    660,
+                    person = person,
+                ),
+            ),
+        )
+
+        // Act && Assert
+        assertDoesNotThrow {
+            TilkjentYtelseValidering.validerAtTilkjentYtelseHarFornuftigePerioderOgBeløp(
+                tilkjentYtelse,
+                listOf(person.tilPersonEnkel()),
+            )
+        }
+    }
+
+    @Test
+    fun `Skal ikke kaste feil når barn har både finnmarkstillegg, utvidet og ordinær andel for en periode i fagsak type barn enslig mindreårig`() {
+        // Arrange
+        val person = tilfeldigPerson(personType = PersonType.BARN)
+        val behandling = lagBehandling(fagsak = lagFagsak(type = FagsakType.BARN_ENSLIG_MINDREÅRIG))
+
+        val tilkjentYtelse = lagInitiellTilkjentYtelse(behandling = behandling)
+
+        tilkjentYtelse.andelerTilkjentYtelse.addAll(
+            listOf(
+                lagAndelTilkjentYtelse(
+                    inneværendeMåned().minusYears(1),
+                    inneværendeMåned().minusMonths(6),
+                    YtelseType.FINNMARKSTILLEGG,
+                    1054,
+                    person = person,
+                ),
+                lagAndelTilkjentYtelse(
+                    inneværendeMåned().minusYears(1),
+                    inneværendeMåned().minusMonths(6),
+                    YtelseType.UTVIDET_BARNETRYGD,
+                    1054,
+                    person = person,
+                ),
+                lagAndelTilkjentYtelse(
+                    inneværendeMåned().minusYears(1),
+                    inneværendeMåned().minusMonths(6),
+                    YtelseType.ORDINÆR_BARNETRYGD,
+                    660,
+                    person = person,
+                ),
+            ),
+        )
+
+        // Act && Assert
+        assertDoesNotThrow {
+            TilkjentYtelseValidering.validerAtTilkjentYtelseHarFornuftigePerioderOgBeløp(
+                tilkjentYtelse,
+                listOf(person.tilPersonEnkel()),
+            )
+        }
     }
 
     @Test
