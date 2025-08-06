1e98f2368b4bdae8d7a68f5f396c3f586076d560
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2025-08-06 13:01:07 +0200

    Endre til bolkspørring, og legg til delt bosted og kommunenummer i spørring

--- src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/pdl/domene/PdlBostedsadresseOgDeltBostedPerson.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/integrasjoner/pdl/domene/PdlBostedsadresseOgDeltBostedPerson.kt
@@ -1,11 +1,9 @@
 package no.nav.familie.ba.sak.integrasjoner.pdl.domene
 
 import no.nav.familie.kontrakter.felles.personopplysning.Bostedsadresse
+import no.nav.familie.kontrakter.felles.personopplysning.DeltBosted
 
-data class PdlBostedsadresseResponse(
-    val person: PdlBostedsadressePerson?,
-)
-
-data class PdlBostedsadressePerson(
+data class PdlBostedsadresseOgDeltBostedPerson(
     val bostedsadresse: List<Bostedsadresse>,
+    val deltBosted: List<DeltBosted>,
 )
