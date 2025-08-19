0262a976bcd8b70e0060de4dc517b3a0c25e9c23
Uy Nguyen <uy.quang.nguyen@nav.no>
2025-08-19 10:40:19 +0200

    Mock featuretoggle i tester

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/beregning/TilkjentYtelseGeneratorTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/beregning/TilkjentYtelseGeneratorTest.kt
@@ -67,6 +67,7 @@ class TilkjentYtelseGeneratorTest {
         every { SatsTidspunkt.senesteSatsTidspunkt } returns LocalDate.of(2022, 12, 31)
         every { unleashServiceMock.isEnabled(FeatureToggle.SKAL_INKLUDERE_ÅRSAK_ENDRE_MOTTAKER_I_INITIELL_GENERERING_AV_ANDELER) } returns true
         every { unleashServiceMock.isEnabled(FeatureToggle.SKAL_GENERERE_FINNMARKSTILLEGG) } returns true
+        every { unleashServiceMock.isEnabled(FeatureToggle.SKAL_GENERERE_SVALBARDTILLEGG) } returns true
     }
 
     @AfterEach
