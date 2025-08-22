bca3aa303cef689a061f26d7731799bc9d58c6ce
bragejahren <brage.ekroll.jahren@nav.no>
2025-08-22 07:17:32 +0200

    Juster test for å validere at spørring ikke henter arkiverte fagsaker

--- src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/kjerne/fagsak/FagsakServiceIntegrationTest.kt
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/kjerne/fagsak/FagsakServiceIntegrationTest.kt
@@ -366,9 +366,15 @@ class FagsakServiceIntegrationTest(
         val behandlingFar = behandlingService.lagreNyOgDeaktiverGammelBehandling(lagBehandlingUtenId(fagsakFar))
         persongrunnlagService.lagreOgDeaktiverGammel(opprettGrunnlag(behandlingFar))
 
+        // Oppretter fagsak med tilhørende behandling og personopplsyninggrunnlag, og arkiverer den.
+        val arkivertFagsak = fagsakService.hentEllerOpprettFagsakForPersonIdent(randomFnr())
+        val behandlingArkivertFagsak = behandlingService.lagreNyOgDeaktiverGammelBehandling(lagBehandlingUtenId(arkivertFagsak))
+        persongrunnlagService.lagreOgDeaktiverGammel(opprettGrunnlag(behandlingArkivertFagsak))
+        fagsakService.lagre(arkivertFagsak.also { it.arkivert = true })
+
         val fagsaker = fagsakService.hentFagsakerPåPerson(barnAktør.first())
         assertEquals(2, fagsaker.size)
-        assertThat(persongrunnlagRepository.findAll()).hasSize(4)
+        assertThat(persongrunnlagRepository.findAll()).hasSize(5)
     }
 
     // Satte XX for at dette testet skal kjøre sist.
