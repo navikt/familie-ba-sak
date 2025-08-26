147cce84c2c8df1ada2457b946049c62efb6aba5
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2025-08-26 12:48:14 +0200

    Flytt Cucumber-tester til enhetstester

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/RunCucumberTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/RunCucumberTest.kt
@@ -1,5 +1,6 @@
-import io.cucumber.core.options.Constants.FILTER_TAGS_PROPERTY_NAME
-import io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME
+package no.nav.familie.ba.sak
+
+import io.cucumber.core.options.Constants
 import org.junit.platform.suite.api.ConfigurationParameter
 import org.junit.platform.suite.api.IncludeEngines
 import org.junit.platform.suite.api.SelectClasspathResource
@@ -8,6 +9,6 @@ import org.junit.platform.suite.api.Suite
 @Suite(failIfNoTests = false)
 @IncludeEngines("cucumber")
 @SelectClasspathResource("no/nav/familie/ba/sak/cucumber")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @Disabled")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "not @Disabled")
 class RunCucumberTest
