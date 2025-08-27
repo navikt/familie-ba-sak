205a106173e9dc541c16e8445a44b91832d1b164
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2025-08-27 10:20:41 +0200

    fjern aktørId-felt i StatusFraOppdragDTO.kt som ikke blir brukt til noe
    Signed-off-by: FredrikP <57641333+fredrikpf@users.noreply.github.com>

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/steg/StatusFraOppdrag.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/steg/StatusFraOppdrag.kt
@@ -78,7 +78,7 @@ class StatusFraOppdrag(
     private fun opprettFerdigstillBehandling(statusFraOppdragDTO: StatusFraOppdragDTO) {
         val ferdigstillBehandling =
             FerdigstillBehandlingTask.opprettTask(
-                søkerIdent = statusFraOppdragDTO.aktørId ?: statusFraOppdragDTO.personIdent,
+                søkerIdent = statusFraOppdragDTO.personIdent,
                 behandlingsId = statusFraOppdragDTO.behandlingsId,
             )
         taskRepository.save(ferdigstillBehandling)
