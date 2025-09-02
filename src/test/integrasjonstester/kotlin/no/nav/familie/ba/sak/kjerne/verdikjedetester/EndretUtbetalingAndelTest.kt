e1ecb0c1eb287d6e49d70e855f240a43446d90df
schmelita <61985450+schmelita@users.noreply.github.com>
2025-09-02 16:50:04 +0200

    Preutfylle bor hos søker vilkår (#5584)
    [Favro](https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-26009)
    
    ### 💰 Hva skal gjøres, og hvorfor?
    Preutfylling av vilkåret _bor fast hos søker_. Vi sjekker barns adresse
    mot søkers adresse i PDL - om barn har hatt samme andresse som søker i
    minst 3 mnd, blir vilkåret **Oppfylt**. Vi ser ikke på delt bosted i
    denne omgang.
    
    ### 🔎️ Er det noe spesielt du ønsker tilbakemelding om?
    _Er det noe du er usikker på eller ønsker å diskutere? Beskriv det
    gjerne her eller kommenter koden det gjelder._
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [x] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [x] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei
    
    ---------
    
    Signed-off-by: FredrikP <57641333+fredrikpf@users.noreply.github.com>
    Co-authored-by: FredrikP <57641333+fredrikpf@users.noreply.github.com>

--- src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/kjerne/verdikjedetester/EndretUtbetalingAndelTest.kt
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/kjerne/verdikjedetester/EndretUtbetalingAndelTest.kt
@@ -143,7 +143,6 @@ class EndretUtbetalingAndelTest(
                             fødselsdato = barnFødselsdato.toString(),
                             fornavn = "Barn",
                             etternavn = "Barnesen",
-                            bostedsadresser = emptyList(),
                         ),
                     ),
             ).also { stubScenario(it) }
@@ -176,7 +175,7 @@ class EndretUtbetalingAndelTest(
             )
 
         restUtvidetBehandling.data!!.personResultater.forEach { restPersonResultat ->
-            restPersonResultat.vilkårResultater.filter { it.resultat == Resultat.IKKE_VURDERT }.forEach {
+            restPersonResultat.vilkårResultater.forEach {
                 familieBaSakKlient().putVilkår(
                     behandlingId = restUtvidetBehandling.data!!.behandlingId,
                     vilkårId = it.id,
