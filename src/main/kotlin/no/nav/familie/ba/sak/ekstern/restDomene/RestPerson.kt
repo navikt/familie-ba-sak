4fba82130a2addeba632194ceaaaabad1089def1
bragejahren <brage.ekroll.jahren@nav.no>
2025-09-02 15:29:26 +0200

    Liten refaktorering og fiksing av feilende tester

--- src/main/kotlin/no/nav/familie/ba/sak/ekstern/restDomene/RestPerson.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/ekstern/restDomene/RestPerson.kt
@@ -4,16 +4,6 @@ import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.Kjønn
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.Målform
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.Person
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersonType
-import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.oppholdsadresse.GrOppholdsadresse
-import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.PersonResultat
-import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.UtdypendeVilkårsvurdering
-import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.Vilkår
-import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.VilkårResultat
-import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.tilTidslinje
-import no.nav.familie.tidslinje.Tidslinje
-import no.nav.familie.tidslinje.beskjærEtter
-import no.nav.familie.tidslinje.utvidelser.kombinerMed
-import no.nav.familie.tidslinje.utvidelser.tilPerioderIkkeNull
 import java.time.LocalDate
 
 /**
@@ -31,32 +21,6 @@ data class RestPerson(
     val dødsfallDato: LocalDate? = null,
     val erManueltLagtTilISøknad: Boolean? = null,
 )
-
-fun List<Person>?.tilManglendeSvalbardmerkingPerioder(personResultater: Set<PersonResultat>?): List<ManglendeSvalbardmerkingDto> {
-    if (this == null || personResultater == null) return emptyList()
-
-    val bosattIRiketVilkårTidslinjePerPerson: Map<String, Tidslinje<VilkårResultat>> = personResultater.associate { it.aktør.aktivFødselsnummer() to it.vilkårResultater.filter { vilkårResultat -> vilkårResultat.vilkårType == Vilkår.BOSATT_I_RIKET && vilkårResultat.periodeFom != null }.tilTidslinje() }
-    val svalbardOppholdTidslinjerPerPerson: Map<String, Tidslinje<GrOppholdsadresse>> = this.associate { it.aktør.aktivFødselsnummer() to it.oppholdsadresser.tilSvalbardOppholdTidslinje() }
-
-    return bosattIRiketVilkårTidslinjePerPerson.mapNotNull { (fnr, bosattIRiketVilkårTidslinje) ->
-        val svalbardOppholdTidslinje = svalbardOppholdTidslinjerPerPerson[fnr] ?: return@mapNotNull null
-
-        // Svalbard opphold utenfor perioder med bosatt i riket vurderes ikke
-        val beskjærtSvalbardOppholdTidslinje = svalbardOppholdTidslinje.beskjærEtter(bosattIRiketVilkårTidslinje)
-
-        val perioderMedManglendeSvalbardMerking =
-            bosattIRiketVilkårTidslinje
-                .kombinerMed(beskjærtSvalbardOppholdTidslinje) { bosattIRiketVilkår, grOppholdsadresse ->
-                    grOppholdsadresse != null && bosattIRiketVilkår != null && !bosattIRiketVilkår.utdypendeVilkårsvurderinger.contains(UtdypendeVilkårsvurdering.BOSATT_PÅ_SVALBARD)
-                }.tilPerioderIkkeNull()
-                .filter { it.verdi }
-                .map { ManglendeSvalbardmerkingPeriodeDto(fom = it.fom, tom = it.tom) }
-
-        if (perioderMedManglendeSvalbardMerking.isEmpty()) return@mapNotNull null
-
-        ManglendeSvalbardmerkingDto(fnr, perioderMedManglendeSvalbardMerking)
-    }
-}
 
 fun Person.tilRestPerson(erManueltLagtTilISøknad: Boolean? = null): RestPerson =
     RestPerson(
