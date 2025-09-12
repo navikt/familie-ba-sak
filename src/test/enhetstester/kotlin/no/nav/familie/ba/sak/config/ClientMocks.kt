0c4bda2109604fc0b46c430abaf2800d96a4b86c
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2025-09-12 13:04:56 +0200

    Slett SøkFagsakNegativeTest, ClientMocks.mockPDL() og bruk av profilen mock-pdl-test-søk
    Veldig mye oppsett for tester som ikke gir noe særlig verdi.

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/config/ClientMocks.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/config/ClientMocks.kt
@@ -8,125 +8,25 @@ import no.nav.familie.ba.sak.common.EnvService
 import no.nav.familie.ba.sak.config.featureToggle.FeatureToggle
 import no.nav.familie.ba.sak.config.featureToggle.FeatureToggleService
 import no.nav.familie.ba.sak.datagenerator.randomFnr
-import no.nav.familie.ba.sak.integrasjoner.familieintegrasjoner.IntegrasjonException
 import no.nav.familie.ba.sak.integrasjoner.pdl.PdlIdentRestClient
 import no.nav.familie.ba.sak.integrasjoner.pdl.PersonopplysningerService
-import no.nav.familie.ba.sak.integrasjoner.pdl.domene.ForelderBarnRelasjon
 import no.nav.familie.ba.sak.integrasjoner.pdl.domene.IdentInformasjon
 import no.nav.familie.ba.sak.integrasjoner.pdl.domene.PersonInfo
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.Kjønn
 import no.nav.familie.ba.sak.kjerne.personident.Aktør
 import no.nav.familie.ba.sak.kjerne.personident.Personident
 import no.nav.familie.kontrakter.felles.personopplysning.Bostedsadresse
-import no.nav.familie.kontrakter.felles.personopplysning.FORELDERBARNRELASJONROLLE
 import no.nav.familie.kontrakter.felles.personopplysning.Matrikkeladresse
-import no.nav.familie.kontrakter.felles.personopplysning.OPPHOLDSTILLATELSE
-import no.nav.familie.kontrakter.felles.personopplysning.Opphold
 import no.nav.familie.kontrakter.felles.personopplysning.SIVILSTANDTYPE
 import no.nav.familie.kontrakter.felles.personopplysning.Sivilstand
-import no.nav.familie.kontrakter.felles.personopplysning.Statsborgerskap
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
-import org.springframework.context.annotation.Profile
-import org.springframework.http.HttpStatus
-import org.springframework.web.client.HttpClientErrorException
 import java.lang.Integer.min
 import java.time.LocalDate
 
 @TestConfiguration
 class ClientMocks {
-    @Bean
-    @Primary
-    @Profile("mock-pdl-test-søk")
-    fun mockPDL(): PersonopplysningerService {
-        val mockPersonopplysningerService = mockk<PersonopplysningerService>()
-
-        val farId = "12345678910"
-        val morId = "21345678910"
-        val barnId = "31245678910"
-
-        val farAktør = tilAktør(farId)
-        val morAktør = tilAktør(morId)
-        val barnAktør = tilAktør(barnId)
-
-        every {
-            mockPersonopplysningerService.hentPersoninfoEnkel(any())
-        } returns personInfo.getValue(INTEGRASJONER_FNR)
-
-        every {
-            mockPersonopplysningerService.hentPersoninfoMedRelasjonerOgRegisterinformasjon(farAktør)
-        } returns PersonInfo(fødselsdato = LocalDate.of(1969, 5, 1), kjønn = Kjønn.MANN, navn = "Far Mocksen")
-
-        every {
-            mockPersonopplysningerService.hentPersoninfoMedRelasjonerOgRegisterinformasjon(morAktør)
-        } returns PersonInfo(fødselsdato = LocalDate.of(1979, 5, 1), kjønn = Kjønn.KVINNE, navn = "Mor Mocksen")
-
-        every {
-            mockPersonopplysningerService.hentPersoninfoMedRelasjonerOgRegisterinformasjon(barnAktør)
-        } returns
-            PersonInfo(
-                fødselsdato = LocalDate.of(2009, 5, 1),
-                kjønn = Kjønn.MANN,
-                navn = "Barn Mocksen",
-                forelderBarnRelasjon =
-                    setOf(
-                        ForelderBarnRelasjon(
-                            farAktør,
-                            FORELDERBARNRELASJONROLLE.FAR,
-                            "Far Mocksen",
-                            LocalDate.of(1969, 5, 1),
-                        ),
-                        ForelderBarnRelasjon(
-                            morAktør,
-                            FORELDERBARNRELASJONROLLE.MOR,
-                            "Mor Mocksen",
-                            LocalDate.of(1979, 5, 1),
-                        ),
-                    ),
-            )
-
-        every {
-            mockPersonopplysningerService.hentGjeldendeStatsborgerskap(any())
-        } answers {
-            Statsborgerskap(
-                "NOR",
-                LocalDate.of(1990, 1, 25),
-                LocalDate.of(1990, 1, 25),
-                null,
-            )
-        }
-
-        every {
-            mockPersonopplysningerService.hentGjeldendeOpphold(any())
-        } answers {
-            Opphold(
-                type = OPPHOLDSTILLATELSE.PERMANENT,
-                oppholdFra = LocalDate.of(1990, 1, 25),
-                oppholdTil = LocalDate.of(2499, 1, 1),
-            )
-        }
-
-        every {
-            mockPersonopplysningerService.hentLandkodeAlpha2UtenlandskBostedsadresse(any())
-        } returns "NO"
-
-        val ukjentAktør = tilAktør(ukjentId)
-
-        every {
-            mockPersonopplysningerService.hentPersoninfoMedRelasjonerOgRegisterinformasjon(ukjentAktør)
-        } throws HttpClientErrorException(HttpStatus.NOT_FOUND, "ikke funnet")
-
-        val feilId = "41235678910"
-        val feilIdAktør = tilAktør(feilId)
-
-        every {
-            mockPersonopplysningerService.hentPersoninfoMedRelasjonerOgRegisterinformasjon(feilIdAktør)
-        } throws IntegrasjonException("feil id")
-
-        return mockPersonopplysningerService
-    }
-
     @Bean
     @Primary
     fun mockEnvService(): EnvService {
