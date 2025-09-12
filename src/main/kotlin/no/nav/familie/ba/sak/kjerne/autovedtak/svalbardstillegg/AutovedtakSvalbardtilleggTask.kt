b543c33433d68859b682153d7d0b11a468f06cd1
schmelita <schirin.melita@gmail.com>
2025-09-12 12:44:27 +0200

    Task for å kjøre autovedtak Svalbardtillegg

---
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/autovedtak/svalbardstillegg/AutovedtakSvalbardtilleggTask.kt
@@ -0,0 +1,47 @@
+package no.nav.familie.ba.sak.kjerne.autovedtak.svalbardstillegg
+
+import io.opentelemetry.instrumentation.annotations.WithSpan
+import no.nav.familie.ba.sak.common.FinnmarkstilleggIngenEndringFeil
+import no.nav.familie.ba.sak.kjerne.autovedtak.AutovedtakStegService
+import no.nav.familie.ba.sak.kjerne.fagsak.FagsakService
+import no.nav.familie.prosessering.AsyncTaskStep
+import no.nav.familie.prosessering.TaskStepBeskrivelse
+import no.nav.familie.prosessering.domene.Task
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+@TaskStepBeskrivelse(
+    taskStepType = AutovedtakSvalbardtilleggTask.TASK_STEP_TYPE,
+    beskrivelse = "Gjennomfør autovedtak for Svalbardtillegg",
+    maxAntallFeil = 3,
+    settTilManuellOppfølgning = true,
+)
+class AutovedtakSvalbardtilleggTask(
+    private val autovedtakStegService: AutovedtakStegService,
+    private val fagsakService: FagsakService,
+) : AsyncTaskStep {
+    @WithSpan
+    override fun doTask(task: Task) {
+        val fagsakId = task.payload.toLong()
+        val aktør = fagsakService.hentAktør(fagsakId)
+        val resultat =
+            try {
+                autovedtakStegService.kjørBehandlingSvalbardtillegg(
+                    mottakersAktør = aktør,
+                    fagsakId = fagsakId,
+                    førstegangKjørt = task.opprettetTid,
+                )
+            } catch (e: FinnmarkstilleggIngenEndringFeil) {
+                "Svalbardtillegg: ${e.message}"
+            }
+
+        logger.info(resultat)
+    }
+
+    companion object {
+        const val TASK_STEP_TYPE = "autovedtakSvalbardtilleggTask"
+        private val logger: Logger = LoggerFactory.getLogger(AutovedtakSvalbardtilleggTask::class.java)
+    }
+}
