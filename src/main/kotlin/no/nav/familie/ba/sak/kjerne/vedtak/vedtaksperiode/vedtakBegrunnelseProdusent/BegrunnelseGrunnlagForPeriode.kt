b3bfee2fb732c141032b10bf4b895b25e090f49d
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2025-09-17 14:39:14 +0200

    ikke regn finnmarkstillegg før startdato for Finnmarkstillegg som periode med krav på finnmarkstillegg (#5665)
    [Favro](https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-26186)
    
    ### 💰 Hva skal gjøres, og hvorfor?
    Når vi prøver å finne begrunnelser for finnmarkstillegg har vi tidligere
    ikke fått det når barn har bodd i Finnmark fra fødsel. Dette er fordi
    finnmarkstillegget ikke regnes som å ha endret seg til å bli innvilget
    siden barn bodde i Finnmark både før og etter at Finnmarkstillegget
    tredde i kraft.
    Legger til at perioder før Finnmarkstillegget trer i kraft ikke skal
    regnes som innvilget.
    
    ### 🔎️ Er det noe spesielt du ønsker tilbakemelding om?
    Bør man finne fom-dato for perioden på en smartere måte? 
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [x] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    ---------
    
    Signed-off-by: FredrikP <57641333+fredrikpf@users.noreply.github.com>

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/vedtakBegrunnelseProdusent/BegrunnelseGrunnlagForPeriode.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/vedtakBegrunnelseProdusent/BegrunnelseGrunnlagForPeriode.kt
@@ -1,9 +1,12 @@
 package no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.vedtakBegrunnelseProdusent
 
+import no.nav.familie.ba.sak.kjerne.beregning.SatsService
+import no.nav.familie.ba.sak.kjerne.beregning.domene.SatsType
 import no.nav.familie.ba.sak.kjerne.beregning.domene.YtelseType
 import no.nav.familie.ba.sak.kjerne.vedtak.vedtaksperiode.Vedtaksperiodetype
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.UtdypendeVilkårsvurdering
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.Vilkår
+import java.time.LocalDate
 
 sealed interface IBegrunnelseGrunnlagForPeriode {
     val dennePerioden: BegrunnelseGrunnlagForPersonIPeriode
@@ -28,14 +31,22 @@ sealed interface IBegrunnelseGrunnlagForPeriode {
                         it.utdypendeVilkårsvurderinger.contains(UtdypendeVilkårsvurdering.BOSATT_I_FINNMARK_NORD_TROMS)
                 } && dennePerioden.erOrdinæreVilkårInnvilget()
 
-    fun sjekkOmHarKravPåFinnmarkstilleggForrigePeriode() =
-        forrigePeriode?.andeler?.any { it.type == YtelseType.FINNMARKSTILLEGG } == true ||
+    fun sjekkOmHarKravPåFinnmarkstilleggForrigePeriode(): Boolean {
+        val startdatoForFinnmarkstillegg =
+            SatsService
+                .hentAllesatser()
+                .filter { it.type == SatsType.FINNMARKSTILLEGG }
+                .minOfOrNull { it.gyldigFom } ?: LocalDate.MAX
+
+        return forrigePeriode?.andeler?.any { it.type == YtelseType.FINNMARKSTILLEGG } == true ||
             forrigePeriode
                 ?.vilkårResultater
                 ?.any {
                     it.vilkårType == Vilkår.BOSATT_I_RIKET &&
-                        it.utdypendeVilkårsvurderinger.contains(UtdypendeVilkårsvurdering.BOSATT_I_FINNMARK_NORD_TROMS)
+                        it.utdypendeVilkårsvurderinger.contains(UtdypendeVilkårsvurdering.BOSATT_I_FINNMARK_NORD_TROMS) &&
+                        (it.tom == null || it.tom >= startdatoForFinnmarkstillegg)
                 } == true && forrigePeriode?.erOrdinæreVilkårInnvilget() == true
+    }
 
     fun sjekkOmharKravPåSvalbardtilleggIForrigeBehandlingPeriode() =
         sammePeriodeForrigeBehandling?.andeler?.any { it.type == YtelseType.SVALBARDTILLEGG } == true ||
