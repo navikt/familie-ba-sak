f5877f88dc3db9dcf5a1085578afb42795bb34f4
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2025-09-30 09:12:04 +0200

    Rekkefølge begrunnelser Finnmark og Svalbardstillegg (#5717)
    Favro: NAV-26344
    
    ### 💰 Hva skal gjøres, og hvorfor?
    Begrunnelser på Finnmarkstillegg bør komme nederst siden det er et
    tillegg.
    * Endrer på compare-metoden for BrevBegrunnelse-interfacet slik at
    Finnmark- og Svalbardbegrunnelser kommer nederst.
    * Liten opprydning
    
    ### 🔎️ Er det noe spesielt du ønsker tilbakemelding om?
    Er det en smartere måte å gjøre dette enn å bruke regex? 
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [x] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [x] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    _Jeg har ikke skrevet tester fordi:_
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei
    
    ---------
    
    Signed-off-by: FredrikP <57641333+fredrikpf@users.noreply.github.com>

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/begrunnelser/IVedtakBegrunnelseTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/begrunnelser/IVedtakBegrunnelseTest.kt
@@ -1,8 +1,12 @@
 package no.nav.familie.ba.sak.kjerne.vedtak.begrunnelser
 
+import no.nav.familie.ba.sak.datagenerator.lagBegrunnelseData
 import no.nav.familie.kontrakter.felles.objectMapper
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 
 internal class IVedtakBegrunnelseTest {
     @Test
@@ -20,5 +24,36 @@ internal class IVedtakBegrunnelseTest {
             objectMapper.readValue(serialisertEØSStandardbegrunnelse, EØSStandardbegrunnelse::class.java),
             EØSStandardbegrunnelse.AVSLAG_EØS_IKKE_EØS_BORGER,
         )
+    }
+
+    @ParameterizedTest
+    @EnumSource(
+        value = Standardbegrunnelse::class,
+        names = [
+            "INNVILGET_FINNMARKSTILLEGG",
+            "INNVILGET_FINNMARKSTILLEGG_INSTITUSJON",
+            "INNVILGET_SVALBARDTILLEGG",
+            "INNVILGET_AUTOVEDTAK_FØDSEL_FINNMARKSTILLEGG",
+            "INNVILGET_AUTOVEDTAK_FØDSEL_SVALBARDTILLEGG",
+            "REDUKSJON_FINNMARKSTILLEGG",
+            "REDUKSJON_SVALBARDTILLEGG",
+            "REDUKSJON_FINNMARKSTILLEGG_BODDE_IKKE_I_TILLEGGSONE",
+            "REDUKSJON_SVALBARDTILLEGG_BODDE_IKKE_PÅ_SVALBARD",
+            "FORTSATT_INNVILGET_FINNMARKSTILLEGG",
+            "FORTSATT_INNVILGET_SVALBARDTILLEGG",
+        ],
+        mode = EnumSource.Mode.INCLUDE,
+    )
+    fun `Finnmark og Svalbard-begrunnelser blir sortert nederst`(standardbegrunnelse: Standardbegrunnelse) {
+        val borHosSøker = lagBegrunnelseData(Standardbegrunnelse.INNVILGET_BOR_HOS_SØKER)
+        val avslagGift = lagBegrunnelseData(Standardbegrunnelse.AVSLAG_GIFT)
+
+        val begrunnelseMedFinnmarkEllerSvalbard =
+            lagBegrunnelseData(standardbegrunnelse)
+
+        val usortertListe = listOf(avslagGift, begrunnelseMedFinnmarkEllerSvalbard, borHosSøker)
+        val sortertListe = usortertListe.sorted()
+
+        assertThat(sortertListe.last()).isEqualTo(begrunnelseMedFinnmarkEllerSvalbard)
     }
 }
