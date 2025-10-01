1409d28728cf6e5294a46f1be71022b05dd2fb74
schmelita <schirin.melita@gmail.com>
2025-10-01 16:25:03 +0200

    endre navn på begrunnelse til 121. Autovedtak Svalbardtillegg

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/begrunnelser/Standardbegrunnelse.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/begrunnelser/Standardbegrunnelse.kt
@@ -509,9 +509,9 @@ enum class Standardbegrunnelse : IVedtakBegrunnelse {
         override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
         override val sanityApiNavn = "innvilgetFinnmarkstilleggUtenDato"
     },
-    INNVILGET_AUTOVEDTAK_FØDSEL_SVALBARDTILLEGG {
+    INNVILGET_SVALBARDTILLEGG_UTEN_DATO {
         override val vedtakBegrunnelseType = VedtakBegrunnelseType.INNVILGET
-        override val sanityApiNavn = "innvilgetAutovedtakFoedselSvalbard"
+        override val sanityApiNavn = "innvilgetSvalbardtilleggUtenDato"
     },
     REDUKSJON_BOSATT_I_RIKTET {
         override val vedtakBegrunnelseType = VedtakBegrunnelseType.REDUKSJON
