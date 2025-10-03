8091729dfcd5bcbee2bb1aea8b913a04f88ccce8
schmelita <schirin.melita@gmail.com>
2025-10-03 09:49:17 +0200

    sett riktig ytelsestype

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/vedtakBegrunnelseProdusent/hentBegrunnelser/HentStandardBegrunnelser.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vedtak/vedtaksperiode/vedtakBegrunnelseProdusent/hentBegrunnelser/HentStandardBegrunnelser.kt
@@ -243,7 +243,7 @@ private fun SanityBegrunnelse.erGjeldendeForSvalbardtillegg(
     val erSvalbardtilleggIForrigeBehandlingPeriode =
         // For innvilgete perioder ønsker vi å ha med alle barna det ble utbetalt for. For f.eks. Reduksjon ønsker vi kun å begrunne for personen som ikke oppfyller kravet
         if (this.periodeResultat == SanityPeriodeResultat.INNVILGET_ELLER_ØKNING) {
-            begrunnelseGrunnlag.sammePeriodeForrigeBehandling?.andeler?.any { it.type == YtelseType.FINNMARKSTILLEGG } == true
+            begrunnelseGrunnlag.sammePeriodeForrigeBehandling?.andeler?.any { it.type == YtelseType.SVALBARDTILLEGG } == true
         } else {
             begrunnelseGrunnlag.sjekkOmharKravPåSvalbardtilleggIForrigeBehandlingPeriode()
         }
@@ -315,7 +315,7 @@ fun ISanityBegrunnelse.erGjeldendeForOpphørFraForrigeBehandling(begrunnelseGrun
     val begrunnelsenGjelderVilkårIkkeLengerOppfylt = this.erLikVilkårOgUtdypendeVilkårIPeriode(ikkeOppfylteVilkårDenneBehandlingen.filter { it.vilkårType in vilkårMistetSidenForrigeBehandling })
 
     val dennePeriodenErFørsteVedtaksperiodePåFagsak =
-        begrunnelseGrunnlag.forrigePeriode == null || begrunnelseGrunnlag.forrigePeriode!!.andeler.firstOrNull() == null
+        begrunnelseGrunnlag.forrigePeriode == null || begrunnelseGrunnlag.forrigePeriode.andeler.firstOrNull() == null
 
     return (begrunnelseGjelderMistedeVilkår || begrunnelsenGjelderVilkårIkkeLengerOppfylt) && dennePeriodenErFørsteVedtaksperiodePåFagsak
 }
