c2a237b2b17725669618c9aaac9dbd2eb59ede16
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2025-10-07 12:49:59 +0200

    50 hvert 5. minutt

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/autovedtak/finnmarkstillegg/AutovedtakFinnmarkstilleggScheduler.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/autovedtak/finnmarkstillegg/AutovedtakFinnmarkstilleggScheduler.kt
@@ -3,7 +3,6 @@ package no.nav.familie.ba.sak.kjerne.autovedtak.finnmarkstillegg
 import no.nav.familie.ba.sak.config.LeaderClientService
 import no.nav.familie.ba.sak.config.featureToggle.FeatureToggle.AUTOMATISK_KJØRING_AV_AUTOVEDTAK_FINNMARKSTILLEGG
 import no.nav.familie.ba.sak.config.featureToggle.FeatureToggleService
-import no.nav.familie.ba.sak.kjerne.autovedtak.satsendring.AutovedtakSatsendringScheduler.Companion.CRON_HVERT_10_MIN_UKEDAG
 import org.slf4j.LoggerFactory
 import org.springframework.context.ApplicationListener
 import org.springframework.context.event.ContextClosedEvent
@@ -21,18 +20,22 @@ class AutovedtakFinnmarkstilleggScheduler(
 
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    @Scheduled(cron = CRON_HVERT_10_MIN_UKEDAG)
+    @Scheduled(cron = CRON_HVERT_5_MIN_UKEDAG)
     fun kjørAutovedtakFinnmarkstillegg() {
         if (isShuttingDown || !leaderClientService.isLeader()) {
             return
         }
         if (featureToggleService.isEnabled(AUTOMATISK_KJØRING_AV_AUTOVEDTAK_FINNMARKSTILLEGG)) {
             logger.info("Starter kjøring av autovedtak finnmarkstillegg")
-            autovedtakFinnmarkstilleggTaskOppretter.opprettTasker(100)
+            autovedtakFinnmarkstilleggTaskOppretter.opprettTasker(50)
         }
     }
 
     override fun onApplicationEvent(event: ContextClosedEvent) {
         isShuttingDown = true
+    }
+
+    companion object {
+        const val CRON_HVERT_5_MIN_UKEDAG = "0 */5 7-20 * * MON-FRI"
     }
 }
