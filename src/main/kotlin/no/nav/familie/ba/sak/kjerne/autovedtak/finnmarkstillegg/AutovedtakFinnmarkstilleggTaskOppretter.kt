4a55e71535b72e6f23b28abbbe0d52d8b31c8ff3
Magnus Tønnessen <magnus.tonnessen@nav.no>
2025-10-07 13:14:21 +0200

    Forbedrer opprettelse av autovedtak finnmarkstillegg (#5749)
    ### 💰 Hva skal gjøres, og hvorfor?
    
    Favro: NAV-26388
    
    - Setter ned antall behandlinger som kjøres per 10. minutt til 100
    - Legger til mer logging
    - Opprett tasker i egen transaksjon
    
    ---------
    
    Co-authored-by: Stig Helge Strøm <stig.helge.strom@nav.no>

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/autovedtak/finnmarkstillegg/AutovedtakFinnmarkstilleggTaskOppretter.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/autovedtak/finnmarkstillegg/AutovedtakFinnmarkstilleggTaskOppretter.kt
@@ -1,6 +1,5 @@
 package no.nav.familie.ba.sak.kjerne.autovedtak.finnmarkstillegg
 
-import no.nav.familie.ba.sak.common.Feil
 import no.nav.familie.ba.sak.integrasjoner.pdl.SystemOnlyPdlRestClient
 import no.nav.familie.ba.sak.kjerne.autovedtak.finnmarkstillegg.domene.FinnmarkstilleggKjøring
 import no.nav.familie.ba.sak.kjerne.autovedtak.finnmarkstillegg.domene.FinnmarkstilleggKjøringRepository
@@ -10,6 +9,7 @@ import no.nav.familie.ba.sak.kjerne.fagsak.FagsakRepository
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersongrunnlagService
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.bostedsadresse.Adresser
 import no.nav.familie.ba.sak.task.OpprettTaskService
+import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -23,6 +23,8 @@ class AutovedtakFinnmarkstilleggTaskOppretter(
     private val behandlingHentOgPersisterService: BehandlingHentOgPersisterService,
     private val pdlRestClient: SystemOnlyPdlRestClient,
 ) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     @Transactional
     fun opprettTasker(antallFagsaker: Int) {
         var antallBehandlingerStartet = 0
@@ -32,23 +34,33 @@ class AutovedtakFinnmarkstilleggTaskOppretter(
             val page = fagsakRepository.finnLøpendeFagsakerForFinnmarkstilleggKjøring(Pageable.ofSize(antallFagsaker))
             val fagsakIder = page.toSet()
 
+            logger.info("Hentet ${fagsakIder.size} fagsaker for vurdering av autovedtak finnmarkstillegg")
+
             val iverksatteBehandlinger =
                 behandlingHentOgPersisterService.hentSisteBehandlingSomErIverksattForFagsaker(fagsakIder).values
 
             val sistIverksatteBehandlingerUtenEøs = iverksatteBehandlinger.filter { it.kategori != BehandlingKategori.EØS }
 
+            logger.info("Av ${iverksatteBehandlinger.size} iverksatte behandlinger er ${sistIverksatteBehandlingerUtenEøs.size} nasjonal")
+
             val grunnlagForIverksatteBehandlinger =
                 persongrunnlagService.hentAktivForBehandlinger(sistIverksatteBehandlingerUtenEøs.map { it.id })
 
+            logger.info("Hentet personopplysningsgrunnlag for ${grunnlagForIverksatteBehandlinger.size} behandlinger")
+
             val fagsakerMedPersonidenter =
-                sistIverksatteBehandlingerUtenEøs.associate { behandling ->
-                    val grunnlag = grunnlagForIverksatteBehandlinger[behandling.id]
-                    if (grunnlag == null) {
-                        throw Feil("Forventet personopplysningsgrunnlag for behandling ${behandling.id} ikke funnet.")
+                sistIverksatteBehandlingerUtenEøs
+                    .filter { behandling ->
+                        val grunnlag = grunnlagForIverksatteBehandlinger[behandling.id]
+                        if (grunnlag == null) {
+                            logger.error("Forventet personopplysningsgrunnlag for behandling ${behandling.id} ikke funnet.")
+                        }
+                        grunnlag != null
+                    }.associate { behandling ->
+                        val grunnlag = grunnlagForIverksatteBehandlinger[behandling.id]!!
+                        val fødselsnummer = grunnlag.personer.map { person -> person.aktør.aktivFødselsnummer() }
+                        behandling.fagsak.id to fødselsnummer
                     }
-                    val fødselsnummer = grunnlag.personer.map { person -> person.aktør.aktivFødselsnummer() }
-                    behandling.fagsak.id to fødselsnummer
-                }
 
             val personerSomBorIFinnmarkEllerNordTroms =
                 fagsakerMedPersonidenter.values
@@ -63,15 +75,23 @@ class AutovedtakFinnmarkstilleggTaskOppretter(
                             .keys
                     }
 
+            logger.info("Fant ${personerSomBorIFinnmarkEllerNordTroms.size} med adresse i Finnmark eller Nord-Troms")
+
             val fagsakerDerMinstEnAktørBorIFinnmarkEllerNordTroms =
                 fagsakerMedPersonidenter
                     .filterValues { personerSomBorIFinnmarkEllerNordTroms.intersect(it).isNotEmpty() }
                     .keys
 
+            logger.info("Fant ${fagsakerDerMinstEnAktørBorIFinnmarkEllerNordTroms.size} fagsaker der minst én person har adresse i Finnmark eller Nord-Troms")
+
             val fagsakIderSomIkkeSkalOpprettesTaskFor = fagsakIder - fagsakerDerMinstEnAktørBorIFinnmarkEllerNordTroms
             finnmarkstilleggKjøringRepository.saveAll(fagsakIderSomIkkeSkalOpprettesTaskFor.map { FinnmarkstilleggKjøring(fagsakId = it) })
 
-            opprettTaskService.opprettAutovedtakFinnmarkstilleggTasker(fagsakerDerMinstEnAktørBorIFinnmarkEllerNordTroms)
+            fagsakerDerMinstEnAktørBorIFinnmarkEllerNordTroms.forEach { fagsakId ->
+                opprettTaskService.opprettAutovedtakFinnmarkstilleggTask(fagsakId)
+            }
+
+            logger.info("Opprettet ${fagsakerDerMinstEnAktørBorIFinnmarkEllerNordTroms.size} tasker for autovedtak finnmarkstillegg")
 
             antallBehandlingerStartet += fagsakerDerMinstEnAktørBorIFinnmarkEllerNordTroms.size
 
