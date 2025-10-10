5d4bfae6db4418e7c0e5d8ce3468a4168c7e20f3
Uy Nguyen <uy.quang.nguyen@nav.no>
2025-10-10 08:25:33 +0200

    Fjerner statsborgerskap med null i fom og tom hvis noe annet finnes

--- src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/vilkårsvurdering/preutfylling/NordiskStatsborgerskapTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ba/sak/kjerne/vilkårsvurdering/preutfylling/NordiskStatsborgerskapTest.kt
@@ -7,9 +7,12 @@ import no.nav.familie.ba.sak.datagenerator.lagAktør
 import no.nav.familie.ba.sak.integrasjoner.pdl.PdlRestClient
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.PersonResultat
 import no.nav.familie.kontrakter.felles.personopplysning.Statsborgerskap
+import no.nav.familie.tidslinje.utvidelser.tilPerioder
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import java.net.URI
+import java.time.LocalDate
 
 class NordiskStatsborgerskapTest {
     @Test
@@ -41,5 +44,35 @@ class NordiskStatsborgerskapTest {
         assertDoesNotThrow {
             pdlRestClientSpyk.lagErNordiskStatsborgerTidslinje(personResultat)
         }
+    }
+
+    @Test
+    fun `Statsborgerskap med null i fom og tom skal fjernes dersom det eksisteres andre innslag med dato`() {
+        val pdlRestClient = PdlRestClient(URI("test"), mockk(), mockk())
+        val pdlRestClientSpyk = spyk(pdlRestClient)
+        val aktør = lagAktør()
+        val personResultat = mockk<PersonResultat>()
+
+        val statsborgerskap1 =
+            Statsborgerskap(
+                land = "NOR",
+                gyldigFraOgMed = null,
+                gyldigTilOgMed = null,
+                bekreftelsesdato = null,
+            )
+
+        val statsborgerskap2 =
+            Statsborgerskap(
+                land = "NOR",
+                gyldigFraOgMed = LocalDate.of(2020, 1, 1),
+                gyldigTilOgMed = null,
+                bekreftelsesdato = null,
+            )
+
+        every { pdlRestClientSpyk.hentStatsborgerskap(any(), any()) } returns listOf(statsborgerskap1, statsborgerskap2)
+        every { personResultat.aktør } returns aktør
+
+        val statsborgerskapTidslinje = pdlRestClientSpyk.lagErNordiskStatsborgerTidslinje(personResultat)
+        assertThat(statsborgerskapTidslinje.tilPerioder().single().fom).isEqualTo(LocalDate.of(2020, 1, 1))
     }
 }
