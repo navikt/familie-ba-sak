5234985d27eb655d6356902d6d3480ded75ede80
bragejahren <70642183+bragejahren@users.noreply.github.com>
2025-10-27 13:18:24 +0100

    NAV-26571: Sørger for at vi klarer å mappe fra VersjonertBarnetrygdSøknadV10 til Søknad (#5832)
    Favro:
    [NAV-26571](https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-26571)
    
    ### 💰 Hva skal gjøres, og hvorfor?
    På grunn av manglende støtte for mapping av V10 søknader til `Søknad`
    fungerer ikke automatisk gjennomføring av "Registrer søknad"-steget for
    alle nye innsendte søknader etter 22.08.25 (da vi gikk over til versjon
    10).
    
    Legger her inn støtte for mapping av V10 søknader og sørger for at vi
    logger error dersom vi mangler mapping for nye versjoner i fremtiden.
    
    ### ✅ Checklist
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer.
    - [x] Jeg har skrevet tester.

--- src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/kjerne/søknad/SøknadMapperLookupTest.kt
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ba/sak/kjerne/søknad/SøknadMapperLookupTest.kt
@@ -1,9 +1,11 @@
 package no.nav.familie.ba.sak.kjerne.søknad
 
 import no.nav.familie.ba.sak.config.AbstractSpringIntegrationTest
+import no.nav.familie.kontrakter.felles.søknad.MissingVersionException
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,7 +16,7 @@ class SøknadMapperLookupTest(
     @Nested
     inner class HentSøknadMapperForVersjon {
         @ParameterizedTest
-        @ValueSource(ints = [9])
+        @ValueSource(ints = [9, 10])
         fun `skal hente alle tilgjengelige mappere`(versjon: Int) {
             // Act
             val søknadMapper = søknadMapperLookup.hentSøknadMapperForVersjon(versjon)
@@ -33,6 +35,28 @@ class SøknadMapperLookupTest(
 
             // Assert
             assertThat(søknadMapper).isInstanceOf(SøknadMapperV9::class.java)
+        }
+
+        @Test
+        fun `skal hente SøknadMapperV10 dersom versjon er 10`() {
+            // Arrange
+            val versjon = 10
+
+            // Act
+            val søknadMapper = søknadMapperLookup.hentSøknadMapperForVersjon(versjon)
+
+            // Assert
+            assertThat(søknadMapper).isInstanceOf(SøknadMapperV10::class.java)
+        }
+
+        @Test
+        fun `skal kaste MissingVersionException dersom dersom mapper for søknadversjon ikke finnes`() {
+            // Arrange
+            val versjon = 11
+
+            // Act & Assert
+            val exception = assertThrows<MissingVersionException> { søknadMapperLookup.hentSøknadMapperForVersjon(versjon) }
+            assertThat(exception.message).isEqualTo("Mangler SøknadMapper for versjon 11")
         }
     }
 }
--- src/test/testdata/kotlin/no/nav/familie/ba/sak/datagenerator/BarnetrygdSøknadGenerator.kt
+++ src/test/testdata/kotlin/no/nav/familie/ba/sak/datagenerator/BarnetrygdSøknadGenerator.kt
@@ -4,10 +4,33 @@ import no.nav.familie.kontrakter.ba.søknad.v1.SIVILSTANDTYPE
 import no.nav.familie.kontrakter.ba.søknad.v1.SøknadAdresse
 import no.nav.familie.kontrakter.ba.søknad.v4.Søknadstype
 import no.nav.familie.kontrakter.ba.søknad.v5.RegistrertBostedType
-import no.nav.familie.kontrakter.ba.søknad.v8.Barn
-import no.nav.familie.kontrakter.ba.søknad.v8.Søker
-import no.nav.familie.kontrakter.ba.søknad.v9.BarnetrygdSøknad
 import no.nav.familie.kontrakter.felles.søknad.Søknadsfelt
+import no.nav.familie.kontrakter.ba.søknad.v10.Barn as BarnV10
+import no.nav.familie.kontrakter.ba.søknad.v10.BarnetrygdSøknad as BarnetrygdSøknadV10
+import no.nav.familie.kontrakter.ba.søknad.v10.Søker as SøkerV10
+import no.nav.familie.kontrakter.ba.søknad.v8.Barn as BarnV8
+import no.nav.familie.kontrakter.ba.søknad.v8.Søker as SøkerV8
+import no.nav.familie.kontrakter.ba.søknad.v9.BarnetrygdSøknad as BarnetrygdSøknadV9
+
+fun lagBarnetrygdSøknadV10(
+    søkerFnr: String = randomFnr(),
+    barnFnr: List<String> = listOf(randomFnr()),
+    søknadstype: Søknadstype = Søknadstype.ORDINÆR,
+    erEøs: Boolean = false,
+    originalspråk: String = "nb",
+): BarnetrygdSøknadV10 =
+    BarnetrygdSøknadV10(
+        kontraktVersjon = 10,
+        søker = lagSøkerV10(søkerFnr),
+        barn = barnFnr.map { lagBarnV10(it) },
+        antallEøsSteg = if (erEøs) 1 else 0,
+        dokumentasjon = emptyList(),
+        originalSpråk = originalspråk,
+        finnesPersonMedAdressebeskyttelse = false,
+        søknadstype = søknadstype,
+        spørsmål = emptyMap(),
+        teksterUtenomSpørsmål = emptyMap(),
+    )
 
 fun lagBarnetrygdSøknadV9(
     søkerFnr: String = randomFnr(),
@@ -15,8 +38,8 @@ fun lagBarnetrygdSøknadV9(
     søknadstype: Søknadstype = Søknadstype.ORDINÆR,
     erEøs: Boolean = false,
     originalspråk: String = "nb",
-): BarnetrygdSøknad =
-    BarnetrygdSøknad(
+): BarnetrygdSøknadV9 =
+    BarnetrygdSøknadV9(
         kontraktVersjon = 9,
         søker = lagSøkerV8(søkerFnr),
         barn = barnFnr.map { lagBarnV8(it) },
@@ -29,8 +52,8 @@ fun lagBarnetrygdSøknadV9(
         teksterUtenomSpørsmål = emptyMap(),
     )
 
-fun lagSøkerV8(fnr: String): Søker =
-    Søker(
+fun lagSøkerV10(fnr: String): SøkerV10 =
+    SøkerV10(
         harEøsSteg = false,
         ident = lagStringSøknadsfelt(fnr),
         navn = lagStringSøknadsfelt("Navn"),
@@ -60,8 +83,54 @@ fun lagSøkerV8(fnr: String): Søker =
         tidligereSamboere = emptyList(),
     )
 
-fun lagBarnV8(fnr: String): Barn =
-    Barn(
+fun lagBarnV10(fnr: String): BarnV10 =
+    BarnV10(
+        harEøsSteg = false,
+        ident = lagStringSøknadsfelt(fnr),
+        navn = lagStringSøknadsfelt(""),
+        registrertBostedType = lagStringSøknadsfelt(RegistrertBostedType.REGISTRERT_SOKERS_ADRESSE),
+        alder = null,
+        andreForelder = null,
+        utenlandsperioder = emptyList(),
+        omsorgsperson = null,
+        idNummer = emptyList(),
+        spørsmål = emptyMap(),
+        eøsBarnetrygdsperioder = emptyList(),
+    )
+
+fun lagSøkerV8(fnr: String): SøkerV8 =
+    SøkerV8(
+        harEøsSteg = false,
+        ident = lagStringSøknadsfelt(fnr),
+        navn = lagStringSøknadsfelt("Navn"),
+        statsborgerskap = lagStringSøknadsfelt(listOf("Norge")),
+        adresse =
+            lagStringSøknadsfelt(
+                SøknadAdresse(
+                    adressenavn = "Gate",
+                    postnummer = null,
+                    husbokstav = null,
+                    bruksenhetsnummer = null,
+                    husnummer = null,
+                    poststed = null,
+                ),
+            ),
+        adressebeskyttelse = false,
+        sivilstand = lagStringSøknadsfelt(SIVILSTANDTYPE.UOPPGITT),
+        utenlandsperioder = emptyList(),
+        arbeidsperioderUtland = emptyList(),
+        pensjonsperioderUtland = emptyList(),
+        arbeidsperioderNorge = emptyList(),
+        pensjonsperioderNorge = emptyList(),
+        andreUtbetalingsperioder = emptyList(),
+        idNummer = emptyList(),
+        spørsmål = emptyMap(),
+        nåværendeSamboer = null,
+        tidligereSamboere = emptyList(),
+    )
+
+fun lagBarnV8(fnr: String): BarnV8 =
+    BarnV8(
         harEøsSteg = false,
         ident = lagStringSøknadsfelt(fnr),
         navn = lagStringSøknadsfelt(""),
