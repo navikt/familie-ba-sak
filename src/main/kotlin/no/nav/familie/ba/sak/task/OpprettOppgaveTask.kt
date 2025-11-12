1c67b70d0c6ce85df18a46f8cefba8e42efe9c86
Stig Helge Strøm <stig.helge.strom@nav.no>
2025-11-12 10:09:03 +0100

    Skipper oppretteting av godkjenn eller underkjenn vedtak oppgaver hvis behandlingen er forbi totrinnskontroll

--- src/main/kotlin/no/nav/familie/ba/sak/task/OpprettOppgaveTask.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/task/OpprettOppgaveTask.kt
@@ -1,13 +1,16 @@
 package no.nav.familie.ba.sak.task
 
 import io.opentelemetry.instrumentation.annotations.WithSpan
+import no.nav.familie.ba.sak.common.secureLogger
 import no.nav.familie.ba.sak.integrasjoner.oppgave.OppgaveService
+import no.nav.familie.ba.sak.kjerne.behandling.BehandlingHentOgPersisterService
 import no.nav.familie.ba.sak.task.dto.OpprettOppgaveTaskDTO
 import no.nav.familie.kontrakter.felles.objectMapper
 import no.nav.familie.kontrakter.felles.oppgave.Oppgavetype
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 
@@ -19,19 +22,35 @@ import java.time.LocalDate
 )
 class OpprettOppgaveTask(
     private val oppgaveService: OppgaveService,
+    private val hentOgPersisterService: BehandlingHentOgPersisterService,
 ) : AsyncTaskStep {
+    val logger = LoggerFactory.getLogger(OpprettOppgaveTask::class.java)
+
     @WithSpan
     override fun doTask(task: Task) {
         val opprettOppgaveTaskDTO = objectMapper.readValue(task.payload, OpprettOppgaveTaskDTO::class.java)
-        task.metadata["oppgaveId"] =
-            oppgaveService.opprettOppgave(
-                behandlingId = opprettOppgaveTaskDTO.behandlingId,
-                oppgavetype = opprettOppgaveTaskDTO.oppgavetype,
-                fristForFerdigstillelse = opprettOppgaveTaskDTO.fristForFerdigstillelse,
-                tilordnetNavIdent = opprettOppgaveTaskDTO.tilordnetRessurs,
-                beskrivelse = opprettOppgaveTaskDTO.beskrivelse,
-                manuellOppgaveType = opprettOppgaveTaskDTO.manuellOppgaveType,
+
+        val behandling = hentOgPersisterService.hent(opprettOppgaveTaskDTO.behandlingId)
+        if (behandling.status.erStatusIverksetterVedtakEllerAvsluttet() &&
+            opprettOppgaveTaskDTO.oppgavetype in
+            listOf(
+                Oppgavetype.GodkjenneVedtak,
+                Oppgavetype.BehandleUnderkjentVedtak,
             )
+        ) {
+            logger.warn("Oppretter ikke oppgave av type ${opprettOppgaveTaskDTO.oppgavetype} for behandling ${behandling.id} som er ferdig behandlet")
+            secureLogger.warn("Oppretter ikke oppgave av type ${opprettOppgaveTaskDTO.oppgavetype} for behandling ${behandling.id} som er ferdig behandlet $opprettOppgaveTaskDTO")
+        } else {
+            task.metadata["oppgaveId"] =
+                oppgaveService.opprettOppgave(
+                    behandlingId = opprettOppgaveTaskDTO.behandlingId,
+                    oppgavetype = opprettOppgaveTaskDTO.oppgavetype,
+                    fristForFerdigstillelse = opprettOppgaveTaskDTO.fristForFerdigstillelse,
+                    tilordnetNavIdent = opprettOppgaveTaskDTO.tilordnetRessurs,
+                    beskrivelse = opprettOppgaveTaskDTO.beskrivelse,
+                    manuellOppgaveType = opprettOppgaveTaskDTO.manuellOppgaveType,
+                )
+        }
     }
 
     companion object {
