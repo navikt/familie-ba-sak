ce2b31963cf0af50eb8e25ac80f4aee29076b1b4
Magnus Tønnessen <magnus.tonnessen@nav.no>
2025-12-09 11:55:34 +0100

    Korriger brevsignatur for enheter (#5967)
    ### 📮 Favro: NAV-26981
    
    ### 💰 Hva skal gjøres, og hvorfor?
    Signatur for enhetene var feil. `Familie` skal skrive med liten f

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/arbeidsfordeling/BarnetrygdEnhet.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/arbeidsfordeling/BarnetrygdEnhet.kt
@@ -8,18 +8,18 @@ enum class BarnetrygdEnhet(
     val gruppenavn: String,
 ) {
     VIKAFOSSEN("2103", "Nav Vikafossen", "0000-GA-ENHET_2103"),
-    DRAMMEN("4806", "Nav Familie- og pensjonsytelser Drammen", "0000-GA-ENHET_4806"),
-    VADSØ("4820", "Nav Familie- og pensjonsytelser Vadsø", "0000-GA-ENHET_4820"),
-    OSLO("4833", "Nav Familie- og pensjonsytelser Oslo 1", "0000-GA-ENHET_4833"),
-    STORD("4842", "Nav Familie- og pensjonsytelser Stord", "0000-GA-ENHET_4842"),
-    STEINKJER("4817", "Nav Familie- og pensjonsytelser Steinkjer", "0000-GA-ENHET_4817"),
+    DRAMMEN("4806", "Nav familie- og pensjonsytelser Drammen", "0000-GA-ENHET_4806"),
+    VADSØ("4820", "Nav familie- og pensjonsytelser Vadsø", "0000-GA-ENHET_4820"),
+    OSLO("4833", "Nav familie- og pensjonsytelser Oslo 1", "0000-GA-ENHET_4833"),
+    STORD("4842", "Nav familie- og pensjonsytelser Stord", "0000-GA-ENHET_4842"),
+    STEINKJER("4817", "Nav familie- og pensjonsytelser Steinkjer", "0000-GA-ENHET_4817"),
     MIDLERTIDIG_ENHET("4863", "Midlertidig enhet", "0000-GA-ENHET_4863"),
     ;
 
     override fun toString(): String = "$enhetsnavn ($enhetsnummer)"
 
     companion object {
-        const val AUTOMATISK_BEHANDLING_BREVSIGNATUR = "Nav Familie- og pensjonsytelser"
+        const val AUTOMATISK_BEHANDLING_BREVSIGNATUR = "Nav familie- og pensjonsytelser"
         private val GYLDIGE_BEHANDLENDE_BARNETRYGD_ENHETER: List<BarnetrygdEnhet> =
             listOf(
                 VIKAFOSSEN,
