b270959af3b546731feeec860c71b43eb4d314bc
juliusbirkevold <62056951+juliusbirkevold@users.noreply.github.com>
2025-12-16 15:53:21 +0100

    legg til replaceFirst
    Signed-off-by: juliusbirkevold <62056951+juliusbirkevold@users.noreply.github.com>

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/eøs/felles/util/Collections.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/eøs/felles/util/Collections.kt
@@ -3,3 +3,5 @@ package no.nav.familie.ba.sak.kjerne.eøs.felles.util
 fun <T> Collection<T>.erEkteDelmengdeAv(mengde: Collection<T>) = this.size < mengde.size && mengde.containsAll(this)
 
 fun <T> Collection<T>.replaceLast(replacer: (T) -> T) = this.take(this.size - 1) + replacer(this.last())
+
+fun <T> Collection<T>.replaceFirst(replacer: (T) -> T) = if (this.isEmpty()) emptyList() else listOf((this.first())).plus(this.drop(1))
