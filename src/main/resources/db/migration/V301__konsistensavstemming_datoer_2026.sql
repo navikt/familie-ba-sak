7a6f64f65443a662679d4a974bdf49a4dbebb3df
Stig Strøm <stig.helge.strom@nav.no>
2025-12-18 13:09:57 +0100

    Datoer for konsistensavstemming 2026 (#5987)
    ### 📮 Favro: NAV-27036 
    
    ### 💰 Hva skal gjøres, og hvorfor?
    Datoene finner du her
    https://nav-it.slack.com/archives/C01ESUV8V52/p1765273136435499
    
    Lagt inn slik at det kjøres konistensavstemming i 2026

---
+++ src/main/resources/db/migration/V301__konsistensavstemming_datoer_2026.sql
@@ -0,0 +1,13 @@
+INSERT INTO batch (id, kjoredato)
+VALUES (nextval('batch_seq'), TO_TIMESTAMP('07-01-2026', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('03-02-2026', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('03-03-2026', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('31-03-2026', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('27-04-2026', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('02-06-2026', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('30-06-2026', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('03-08-2026', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('01-09-2026', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('01-10-2026', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('03-11-2026', 'DD-MM-YYYY SS:MS')),
+       (nextval('batch_seq'), TO_TIMESTAMP('24-11-2026', 'DD-MM-YYYY SS:MS'));
