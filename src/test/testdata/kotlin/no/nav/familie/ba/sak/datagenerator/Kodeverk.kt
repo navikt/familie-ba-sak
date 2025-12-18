7531b6598fca844ec58cd02f85682a0ba5d45af4
juliusbirkevold <62056951+juliusbirkevold@users.noreply.github.com>
2025-12-18 09:21:43 +0100

    endre testdata polen EØS fom
    Signed-off-by: juliusbirkevold <62056951+juliusbirkevold@users.noreply.github.com>

--- src/test/testdata/kotlin/no/nav/familie/ba/sak/datagenerator/Kodeverk.kt
+++ src/test/testdata/kotlin/no/nav/familie/ba/sak/datagenerator/Kodeverk.kt
@@ -7,7 +7,7 @@ import no.nav.familie.kontrakter.felles.kodeverk.KodeverkSpråk
 import java.time.LocalDate
 import java.time.Month
 
-val POL_EØS_FOM = LocalDate.of(2004, Month.JANUARY, 1)
+val POL_EØS_FOM = LocalDate.of(2004, Month.MAY, 1)
 val GBR_EØS_FOM = LocalDate.of(1900, Month.JANUARY, 1)
 val GBR_EØS_TOM = LocalDate.of(2020, Month.DECEMBER, 31)
 val DEU_EØS_FOM = LocalDate.of(1900, Month.JANUARY, 1)
