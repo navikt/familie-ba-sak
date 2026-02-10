3d6e28fbb0ea6db0b0a506a51e07e7da2bed1e13
Magnus Tønnessen <magnus.tonnessen@nav.no>
2026-02-10 15:05:21 +0100

    Bare preutfyll vilkår i førstegangsbehandlinger (#6084)
    ### 💰 Hva skal gjøres, og hvorfor?
    Legger på en sperre slik at vilkår bare preutfylles i
    førstegangsbehandlinger

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/vilkårsvurdering/preutfylling/PreutfyllVilkårService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/vilkårsvurdering/preutfylling/PreutfyllVilkårService.kt
@@ -3,6 +3,7 @@ package no.nav.familie.ba.sak.kjerne.vilkårsvurdering.preutfylling
 import no.nav.familie.ba.sak.config.featureToggle.FeatureToggle
 import no.nav.familie.ba.sak.config.featureToggle.FeatureToggleService
 import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingKategori
+import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingType
 import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.PersongrunnlagService
 import no.nav.familie.ba.sak.kjerne.vilkårsvurdering.domene.Vilkårsvurdering
 import org.springframework.stereotype.Service
@@ -18,6 +19,7 @@ class PreutfyllVilkårService(
 ) {
     fun preutfyllVilkår(vilkårsvurdering: Vilkårsvurdering) {
         if (vilkårsvurdering.behandling.kategori == BehandlingKategori.EØS) return
+        if (vilkårsvurdering.behandling.type != BehandlingType.FØRSTEGANGSBEHANDLING) return
 
         if (featureToggleService.isEnabled(FeatureToggle.PREUTFYLLING_PERSONOPPLYSNIGSGRUNNLAG)) {
             persongrunnlagService.oppdaterRegisteropplysninger(vilkårsvurdering.behandling.id)
