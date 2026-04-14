7d7189b14d3bf91b4047eb0305459e4c18dfa8d8
Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>
2026-04-14 08:33:23 +0200

    Forvalter endepunkt som henter person fra pdl (#6184)
    ### 📮 Favro: Nav-28234
    
    ### 💰 Hva skal gjøres, og hvorfor?
    Det hender ofte at vi trenger spesifikk informasjon om en person fra
    PDL.
    I tilfeller som når det er overlapp i adressene, feil ved sjekk av aktiv
    fnr, eller annet, så gjør det feilsøkingen mye lettere hvis vi har
    tilgang på denne informasjonen.
    
    Dette er informasjon som allerede blir lagret i databasen etterhvert
    (men ofte så kræsjer det før informasjonen lagres), og da er det nyttig
    å ha tilgang til rådataene fra PDL.
    
    Jeg oppretter derfor et forvalter endepunkt som vi utviklere kan bruke
    for å hente personopplysninger.
    Her må man aktivt sette et flag til true hvis det er noe man ønsker å se
    (f.eks adresse eller deltbosted), og alt blir logget ned til secure
    logger. Det gjøres også sjekk om man faktisk har tilgang til personen
    (man får f.eks feil hvis det er kode6/7)
    
    <img width="1468" height="744" alt="swaggger"
    src="https://github.com/user-attachments/assets/54dc1bf0-768c-46bd-bd1a-fbf2caeffa76"
    />

---
+++ src/main/kotlin/no/nav/familie/ba/sak/internal/ForvalterPersonInfoDto.kt
@@ -0,0 +1,67 @@
+package no.nav.familie.ba.sak.internal
+
+import io.swagger.v3.oas.annotations.media.Schema
+import no.nav.familie.ba.sak.integrasjoner.pdl.domene.DødsfallData
+import no.nav.familie.ba.sak.integrasjoner.pdl.domene.PdlFolkeregisteridentifikator
+import no.nav.familie.ba.sak.integrasjoner.pdl.domene.PdlKontaktinformasjonForDødsbo
+import no.nav.familie.ba.sak.integrasjoner.pdl.domene.PersonInfo
+import no.nav.familie.ba.sak.kjerne.grunnlag.personopplysninger.Kjønn
+import no.nav.familie.kontrakter.felles.personopplysning.ADRESSEBESKYTTELSEGRADERING
+import no.nav.familie.kontrakter.felles.personopplysning.Bostedsadresse
+import no.nav.familie.kontrakter.felles.personopplysning.DeltBosted
+import no.nav.familie.kontrakter.felles.personopplysning.Opphold
+import no.nav.familie.kontrakter.felles.personopplysning.Oppholdsadresse
+import no.nav.familie.kontrakter.felles.personopplysning.Sivilstand
+import no.nav.familie.kontrakter.felles.personopplysning.Statsborgerskap
+import java.time.LocalDate
+
+data class HentPersonFraPdlRequest(
+    val ident: String,
+    val begrunnelse: String,
+    @Schema(example = "false") val visFødselsdato: Boolean = false,
+    @Schema(example = "false") val visNavn: Boolean = false,
+    @Schema(example = "false") val visKjønn: Boolean = false,
+    @Schema(example = "false") val visAdressebeskyttelse: Boolean = false,
+    @Schema(example = "false") val visBostedsadresse: Boolean = false,
+    @Schema(example = "false") val visOppholdsadresse: Boolean = false,
+    @Schema(example = "false") val visHistoriskeIdenter: Boolean = false,
+    @Schema(example = "false") val visDødsfall: Boolean = false,
+    @Schema(example = "false") val visStatsborgerskap: Boolean = false,
+    @Schema(example = "false") val visOpphold: Boolean = false,
+    @Schema(example = "false") val visSivilstand: Boolean = false,
+    @Schema(example = "false") val visDeltBosted: Boolean = false,
+    @Schema(example = "false") val visKontaktinformasjonForDødsbo: Boolean = false,
+)
+
+data class ForvalterPersonInfoDto(
+    val fødselsdato: LocalDate? = null,
+    val navn: String? = null,
+    val kjønn: Kjønn? = null,
+    val adressebeskyttelseGradering: ADRESSEBESKYTTELSEGRADERING? = null,
+    val bostedsadresser: List<Bostedsadresse>? = null,
+    val oppholdsadresser: List<Oppholdsadresse>? = null,
+    val deltBosted: List<DeltBosted>? = null,
+    val sivilstander: List<Sivilstand>? = null,
+    val opphold: List<Opphold>? = null,
+    val statsborgerskap: List<Statsborgerskap>? = null,
+    val dødsfall: DødsfallData? = null,
+    val kontaktinformasjonForDoedsbo: PdlKontaktinformasjonForDødsbo? = null,
+    val historiskeIdenter: List<PdlFolkeregisteridentifikator>? = null,
+)
+
+fun PersonInfo.tilForvalterPersonInfoDto(hentPersonFraPdlRequest: HentPersonFraPdlRequest) =
+    ForvalterPersonInfoDto(
+        fødselsdato = fødselsdato.takeIf { hentPersonFraPdlRequest.visFødselsdato },
+        navn = navn.takeIf { hentPersonFraPdlRequest.visNavn },
+        kjønn = kjønn.takeIf { hentPersonFraPdlRequest.visKjønn },
+        adressebeskyttelseGradering = adressebeskyttelseGradering.takeIf { hentPersonFraPdlRequest.visAdressebeskyttelse },
+        bostedsadresser = bostedsadresser.takeIf { hentPersonFraPdlRequest.visBostedsadresse },
+        oppholdsadresser = oppholdsadresser.takeIf { hentPersonFraPdlRequest.visOppholdsadresse },
+        deltBosted = deltBosted.takeIf { hentPersonFraPdlRequest.visDeltBosted },
+        sivilstander = sivilstander.takeIf { hentPersonFraPdlRequest.visSivilstand },
+        opphold = opphold.takeIf { hentPersonFraPdlRequest.visOpphold },
+        statsborgerskap = statsborgerskap.takeIf { hentPersonFraPdlRequest.visStatsborgerskap },
+        dødsfall = dødsfall.takeIf { hentPersonFraPdlRequest.visDødsfall },
+        kontaktinformasjonForDoedsbo = kontaktinformasjonForDoedsbo.takeIf { hentPersonFraPdlRequest.visKontaktinformasjonForDødsbo },
+        historiskeIdenter = historiskeIdenter.takeIf { hentPersonFraPdlRequest.visHistoriskeIdenter },
+    )
