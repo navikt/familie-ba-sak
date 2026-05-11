0159e9aab392d98cc89b56f06340857bfef707ad
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2026-05-11 09:59:40 +0200

    Bruk SecurityContextHolder i SikkerhetContext

--- src/main/kotlin/no/nav/familie/ba/sak/sikkerhet/SikkerhetContext.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/sikkerhet/SikkerhetContext.kt
@@ -3,9 +3,9 @@ package no.nav.familie.ba.sak.sikkerhet
 import no.nav.familie.ba.sak.common.secureLogger
 import no.nav.familie.ba.sak.config.BehandlerRolle
 import no.nav.familie.ba.sak.config.RolleConfig
-import no.nav.security.token.support.core.context.TokenValidationContext
-import no.nav.security.token.support.core.jwt.JwtTokenClaims
-import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 
 object SikkerhetContext {
     const val SYSTEM_FORKORTELSE = "VL"
@@ -14,59 +14,26 @@ object SikkerhetContext {
     fun erSystemKontekst() = hentSaksbehandler() == SYSTEM_FORKORTELSE
 
     fun erMaskinTilMaskinToken(): Boolean {
-        val claims = SpringTokenValidationContextHolder().getTokenValidationContext().getClaims("azuread")
-        return claims.get("oid") != null &&
-            claims.get("oid") == claims.get("sub") &&
-            claims.getAsList("roles").contains("access_as_application")
+        val oid = hentClaimFraToken<String>("oid")
+        val sub = hentClaimFraToken<String>("sub")
+        val roles = hentClaimFraToken<List<String>>("roles").orEmpty()
+        return oid != null && oid == sub && roles.contains("access_as_application")
     }
 
     fun harInnloggetBrukerForvalterRolle(rolleConfig: RolleConfig): Boolean = hentGrupper().contains(rolleConfig.FORVALTER_ROLLE)
 
-    fun hentSaksbehandler(): String =
-        Result
-            .runCatching { SpringTokenValidationContextHolder().getTokenValidationContext() }
-            .fold(
-                onSuccess = {
-                    it.hentClaimsForIssuer("azuread")?.get("NAVident")?.toString() ?: SYSTEM_FORKORTELSE
-                },
-                onFailure = { SYSTEM_FORKORTELSE },
-            )
+    fun hentSaksbehandler(): String = hentClaimFraToken("NAVident") ?: SYSTEM_FORKORTELSE
 
-    fun hentSaksbehandlerEpost(): String =
-        Result
-            .runCatching { SpringTokenValidationContextHolder().getTokenValidationContext() }
-            .fold(
-                onSuccess = { it.hentClaimsForIssuer("azuread")?.get("preferred_username")?.toString() ?: SYSTEM_FORKORTELSE },
-                onFailure = { SYSTEM_FORKORTELSE },
-            )
+    fun hentSaksbehandlerEpost(): String = hentClaimFraToken("preferred_username") ?: SYSTEM_FORKORTELSE
 
-    fun hentSaksbehandlerNavn(): String =
-        Result
-            .runCatching { SpringTokenValidationContextHolder().getTokenValidationContext() }
-            .fold(
-                onSuccess = { it.hentClaimsForIssuer("azuread")?.get("name")?.toString() ?: SYSTEM_NAVN },
-                onFailure = { SYSTEM_NAVN },
-            )
+    fun hentSaksbehandlerNavn(): String = hentClaimFraToken("name") ?: SYSTEM_NAVN
 
-    fun hentRolletilgangFraSikkerhetscontext(
-        rolleConfig: RolleConfig,
-        lavesteSikkerhetsnivå: BehandlerRolle?,
-    ): BehandlerRolle {
-        if (hentSaksbehandler() == SYSTEM_FORKORTELSE) return BehandlerRolle.SYSTEM
-
-        val grupper = hentGrupper()
-        val høyesteSikkerhetsnivåForInnloggetBruker: BehandlerRolle =
-            when {
-                grupper.contains(rolleConfig.BESLUTTER_ROLLE) -> BehandlerRolle.BESLUTTER
-                grupper.contains(rolleConfig.SAKSBEHANDLER_ROLLE) -> BehandlerRolle.SAKSBEHANDLER
-                grupper.contains(rolleConfig.FORVALTER_ROLLE) -> BehandlerRolle.FORVALTER
-                grupper.contains(rolleConfig.VEILEDER_ROLLE) -> BehandlerRolle.VEILEDER
-                else -> BehandlerRolle.UKJENT
-            }
-
+    fun hentRolletilgangFraSikkerhetscontext(lavesteSikkerhetsnivå: BehandlerRolle?): BehandlerRolle {
+        val høyeste = hentHøyesteRolletilgangForInnloggetBruker()
         return when {
+            høyeste == BehandlerRolle.SYSTEM -> BehandlerRolle.SYSTEM
             lavesteSikkerhetsnivå == null -> BehandlerRolle.UKJENT
-            høyesteSikkerhetsnivåForInnloggetBruker.nivå >= lavesteSikkerhetsnivå.nivå -> lavesteSikkerhetsnivå
+            høyeste.nivå >= lavesteSikkerhetsnivå.nivå -> lavesteSikkerhetsnivå
             else -> BehandlerRolle.UKJENT
         }
     }
@@ -84,24 +51,18 @@ object SikkerhetContext {
         }
     }
 
-    fun hentGrupper(): List<String> =
-        Result
-            .runCatching { SpringTokenValidationContextHolder().getTokenValidationContext() }
-            .fold(
-                onSuccess = {
-                    @Suppress("UNCHECKED_CAST")
-                    it.hentClaimsForIssuer("azuread")?.get("groups") as List<String>? ?: emptyList()
-                },
-                onFailure = { emptyList() },
-            )
+    fun hentGrupper(): List<String> = hentClaimFraToken("groups") ?: emptyList()
 
-    fun TokenValidationContext.hentClaimsForIssuer(issuer: String): JwtTokenClaims? = if (this.issuers.contains(issuer)) this.getClaims(issuer) else null
+    fun <T> hentClaimFraToken(claim: String): T? = runCatching { hentJwt()?.getClaim<T>(claim) }.getOrNull()
+
+    fun hentJwt(): Jwt? = hentJwtAuthenticationToken()?.token
+
+    private fun hentJwtAuthenticationToken(): JwtAuthenticationToken? = SecurityContextHolder.getContext().authentication as? JwtAuthenticationToken
 
     fun kallKommerFraKlage(): Boolean = kallKommerFra("teamfamilie:familie-klage")
 
     private fun kallKommerFra(forventetApplikasjonsSuffix: String): Boolean {
-        val claims = SpringTokenValidationContextHolder().getTokenValidationContext().getClaims("azuread")
-        val applikasjonsnavn = claims.get("azp_name")?.toString() ?: "" // e.g. dev-gcp:some-team:application-name
+        val applikasjonsnavn = hentClaimFraToken<String>("azp_name") ?: "" // e.g. dev-gcp:some-team:application-name
         secureLogger.info("Applikasjonsnavn: $applikasjonsnavn")
         return applikasjonsnavn.endsWith(forventetApplikasjonsSuffix)
     }
