a5e22a858e2be4c0d1e0383d6c3827ebc7e127cc
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2026-05-11 10:24:50 +0200

    Legg til SecurityFilterChain og AuthenticationManager for endepunkter beskyttet av TokenX

--- src/main/kotlin/no/nav/familie/ba/sak/config/SecurityConfiguration.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/config/SecurityConfiguration.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.ba.sak.config
 
+import no.nav.familie.ba.sak.sikkerhet.TokenXAuthenticationManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
@@ -12,7 +13,9 @@ import org.springframework.security.web.SecurityFilterChain
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
-class SecurityConfiguration {
+class SecurityConfiguration(
+    private val tokenXAuthenticationManager: TokenXAuthenticationManager,
+) {
     @Bean
     @Order(1)
     fun publicSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
@@ -21,6 +24,24 @@ class SecurityConfiguration {
             securityMatcher("/internal/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
             authorizeHttpRequests {
                 authorize(anyRequest, permitAll)
+            }
+        }
+        return http.build()
+    }
+
+    @Bean
+    @Order(2)
+    fun tokenXSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            csrf { disable() }
+            securityMatcher("/api/minside/**")
+            authorizeHttpRequests {
+                authorize(anyRequest, authenticated)
+            }
+            oauth2ResourceServer {
+                jwt {
+                    authenticationManager = tokenXAuthenticationManager
+                }
             }
         }
         return http.build()
