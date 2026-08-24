cde2e64668cc4591d6ca893f7fd99fffd121a2a1
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2026-08-24 14:37:08 +0200

    Skill ut forvalterendepunkter for behandling til egen controller
    Signed-off-by: FredrikP <57641333+fredrikpf@users.noreply.github.com>

--- src/main/kotlin/no/nav/familie/ba/sak/internal/forvalterendepunkt/ForvalterController.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/internal/forvalterendepunkt/ForvalterController.kt
@@ -1,27 +1,17 @@
 package no.nav.familie.ba.sak.internal.forvalterendepunkt
 
 import io.swagger.v3.oas.annotations.Operation
-import no.nav.familie.ba.sak.common.secureLogger
 import no.nav.familie.ba.sak.config.BehandlerRolle
 import no.nav.familie.ba.sak.config.TaskRepositoryWrapper
 import no.nav.familie.ba.sak.integrasjoner.familieintegrasjoner.IntegrasjonKlient
 import no.nav.familie.ba.sak.integrasjoner.økonomi.UtbetalingsTidslinjeService
 import no.nav.familie.ba.sak.integrasjoner.økonomi.UtbetalingsperiodeDto
 import no.nav.familie.ba.sak.internal.forvalter.ForvalterService
-import no.nav.familie.ba.sak.kjerne.behandling.BehandlingHentOgPersisterService
-import no.nav.familie.ba.sak.kjerne.behandling.BehandlingService
-import no.nav.familie.ba.sak.kjerne.behandling.domene.BehandlingStatus
-import no.nav.familie.ba.sak.kjerne.eøs.felles.BehandlingId
 import no.nav.familie.ba.sak.kjerne.fagsak.FagsakService
 import no.nav.familie.ba.sak.kjerne.fagsak.FagsakStatusScheduler
-import no.nav.familie.ba.sak.kjerne.steg.StegType
 import no.nav.familie.ba.sak.sikkerhet.TilgangService
-import no.nav.familie.ba.sak.task.FerdigstillBehandlingTask
 import no.nav.familie.ba.sak.task.HentAlleIdenterTilPsysTask
-import no.nav.familie.ba.sak.task.MaskineltUnderkjennVedtakTask
 import no.nav.familie.ba.sak.task.OppdaterLøpendeFlagg
-import no.nav.familie.ba.sak.task.dto.FerdigstillBehandlingDTO
-import no.nav.familie.ba.sak.task.dto.HenleggAutovedtakOgSettBehandlingTilbakeTilVentVedSmåbarnstilleggTask
 import no.nav.familie.ba.sak.task.internkonsistensavstemming.OpprettInternKonsistensavstemmingTaskerTask
 import no.nav.familie.kontrakter.ba.finnmarkstillegg.kommuneErIFinnmarkEllerNordTroms
 import no.nav.familie.kontrakter.felles.Ressurs
@@ -30,13 +20,10 @@ import no.nav.familie.prosessering.internal.TaskService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
-import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -52,8 +39,6 @@ class ForvalterController(
     private val fagsakStatusScheduler: FagsakStatusScheduler,
     private val fagsakService: FagsakService,
     private val taskRepository: TaskRepositoryWrapper,
-    private val behandlingHentOgPersisterService: BehandlingHentOgPersisterService,
-    private val behandlingService: BehandlingService,
     private val hentAlleIdenterTilPsysTask: HentAlleIdenterTilPsysTask,
     private val utbetalingsTidslinjeService: UtbetalingsTidslinjeService,
 ) {
@@ -72,20 +57,6 @@ class ForvalterController(
         return ResponseEntity.ok(Ressurs.success("Intern konsistensavstemming startet"))
     }
 
-    @PutMapping("/maskinelt-underkjenn-vedtak/{behandlingId}")
-    @Operation(summary = "Underkjenner et vedtak på vegne av system")
-    fun maskineltUnderkjennVedtak(
-        @PathVariable behandlingId: Long,
-    ): ResponseEntity<Ressurs<String>> {
-        tilgangService.verifiserHarTilgangTilHandling(
-            minimumBehandlerRolle = BehandlerRolle.FORVALTER,
-            handling = "Underkjenner et vedtak på vegne av system",
-        )
-
-        val task = taskService.save(MaskineltUnderkjennVedtakTask.opprettTask(behandlingId))
-        return ResponseEntity.ok(Ressurs.success("Underkjenner vedtak i behandling $behandlingId i task ${task.id}"))
-    }
-
     @PostMapping("/kjør-oppdater-løpende-flagg-task")
     @Operation(summary = "Kjører oppdaterLøpendeFlagg-tasken slik at man oppdaterer tasker som er løpende til avsluttet ved behov.")
     fun kjørOppdaterLøpendeFlaggTask(): ResponseEntity<Ressurs<String>> {
@@ -98,25 +69,6 @@ class ForvalterController(
         taskRepository.save(oppdaterLøpendeFlaggTask)
         logger.info("Opprettet oppdaterLøpendeFlaggTask")
         return ResponseEntity.ok(Ressurs.success("Task for oppdatering av løpende flagg startet"))
-    }
-
-    @PostMapping("/henlegg-autovedtak-og-sett-behandling-tilbake-paa-vent")
-    @Operation(summary = "Henlegger autovedtak og setter behandling tilbake på vent.")
-    @Transactional
-    fun henleggAutovedtakOgSettBehandlingTilbakePåVent(
-        @RequestBody behandlingList: List<Long>,
-    ): ResponseEntity<Ressurs<String>> {
-        tilgangService.verifiserHarTilgangTilHandling(
-            minimumBehandlerRolle = BehandlerRolle.FORVALTER,
-            handling = "Henlegger autovedtak og setter behandling tilbake på vent.",
-        )
-
-        behandlingList.forEach { behandlingId ->
-            logger.info("Opprettet oppdaterLøpendeFlaggTask for behandlingId=$behandlingId")
-            val hennleggAutovedtakTask = HenleggAutovedtakOgSettBehandlingTilbakeTilVentVedSmåbarnstilleggTask.opprettTask(behandlingId)
-            taskRepository.save(hennleggAutovedtakTask)
-        }
-        return ResponseEntity.ok(Ressurs.success("Task for henleggelse av autovedtak startet"))
     }
 
     @GetMapping("/identer-barnetrygd-pensjon/{aar}")
@@ -184,83 +136,6 @@ class ForvalterController(
 
         forvalterService.endreFagsakStatusFraLøpendeTilOpprettet(fagsakId)
         return ResponseEntity.ok("Endret status på fagsak $fagsakId fra løpende til opprettet.")
-    }
-
-    @PostMapping("/opprett-ferdigstill-behandling-task")
-    @Operation(
-        summary = "Oppretter en ferdigstill behandling task for en behandling",
-        description = """
-            Kan brukes hvis distribuer dokument har blitt avvikshåndtert og behandlingen ikke har blitt ferdigstilt.
-
-            Hvis man ikke fikk distribuert brevet gjennom dokdist og saksbehandler manuelt har sendt printet og sendt, så vil 
-            aldri behandlingen gå viderere til ferdigstill behandling-steget. I de tilfellene kan man bruke dette endepunktet
-        """,
-    )
-    fun opprettFerdigstillBehandlingTask(
-        @RequestBody dto: FerdigstillBehandlingDTO,
-    ): ResponseEntity<String> {
-        tilgangService.verifiserHarTilgangTilHandling(
-            minimumBehandlerRolle = BehandlerRolle.FORVALTER,
-            handling = "Opprett task for å ferdigstille behandling for ident",
-        )
-
-        val behandling = behandlingHentOgPersisterService.hent(dto.behandlingsId)
-        if (behandling.status != BehandlingStatus.IVERKSETTER_VEDTAK) {
-            return ResponseEntity.badRequest().body("Kan bare opprette ferdigstill behandling task for behandlinger som er i status IVERKSETTER_VEDTAK")
-        }
-
-        if (behandling.steg != StegType.DISTRIBUER_VEDTAKSBREV) {
-            return ResponseEntity.badRequest().body("Kan bare opprette ferdigstill behandling task for behandlinger som er i steg DISTRIBUER_VEDTAKSBREV")
-        }
-
-        behandlingService.leggTilStegPåBehandlingOgSettTidligereStegSomUtført(
-            behandlingId = dto.behandlingsId,
-            steg = StegType.FERDIGSTILLE_BEHANDLING,
-        )
-
-        val task =
-            FerdigstillBehandlingTask.opprettTask(
-                søkerIdent = dto.personIdent,
-                behandlingsId = dto.behandlingsId,
-            )
-        taskRepository.save(task)
-
-        logger.info("Opprettet ferdigstill behandling task for behandling ${dto.behandlingsId} gjennom forvalter-endepunktet")
-
-        return ResponseEntity.ok("Task opprettet ${task.id}")
-    }
-
-    @PatchMapping("/deaktiverHenlagtBehandlingOgSettSisteVedtatteBehandlingTilAktiv")
-    @Operation(
-        summary = "Deaktiverer en behandling og setter siste vedtatte behandling til aktiv",
-        description = "Dette endepunktet deaktiverer en behandling og setter siste vedtatte behandling til aktiv. Dette for å fikse en sak som ble feil etter en bug i henlegging",
-    )
-    @Transactional
-    fun deaktiverHenlagtBehandlingOgSettSisteVedtatteBehandlingTilAktiv(
-        @RequestBody behandlingId: BehandlingId,
-    ) {
-        tilgangService.verifiserHarTilgangTilHandling(
-            minimumBehandlerRolle = BehandlerRolle.FORVALTER,
-            handling = "Deaktiverer en behandling og setter siste vedtatte behandling til aktiv",
-        )
-
-        val behandling = behandlingHentOgPersisterService.hent(behandlingId.id)
-        val aktivBehandling = behandlingHentOgPersisterService.finnAktivForFagsak(behandling.fagsak.id) ?: error("Finnes ingen aktiv behandling")
-        if (aktivBehandling.id != behandling.id) {
-            error("Aktiv behandling er ${aktivBehandling.id} og ikke ${behandling.id}")
-        }
-
-        if (!aktivBehandling.erHenlagt()) {
-            error("Aktiv behandling er ikke henlagt")
-        }
-
-        aktivBehandling.aktiv = false
-        behandlingHentOgPersisterService.lagreEllerOppdater(aktivBehandling, sendTilDvh = false)
-        behandlingHentOgPersisterService.hentSisteBehandlingSomErVedtatt(behandling.fagsak.id)?.apply {
-            aktiv = true
-            behandlingHentOgPersisterService.lagreEllerOppdater(this, sendTilDvh = false)
-            secureLogger.info("Patchet ${behandling.fagsak.id} ved å deaktivere behandling ${behandling.id} og setter siste vedtatte behandling ($id) til aktiv")
-        }
     }
 
     @PostMapping("/fagsaklåsing/start-batch")
