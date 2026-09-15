92cdc8e86f46dc01356e6e0ed0c327f01ed90202
thoalm <thoalm@outlook.com>
2026-09-15 12:20:45 +0200

    NAV-30720: Legger til toggle SKAL_BEHANDLE_SOKNAD_AUTOMATISK

--- src/main/kotlin/no/nav/familie/ba/sak/kjerne/autovedtak/AutovedtakStegService.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/kjerne/autovedtak/AutovedtakStegService.kt
@@ -135,12 +135,16 @@ class AutovedtakStegService(
         mottakersAktør: Aktør,
         søknad: Søknad,
         førstegangKjørt: LocalDateTime = LocalDateTime.now(),
-    ): String =
-        kjørBehandling(
+    ): String {
+        if (!featureToggleService.isEnabled(FeatureToggle.SKAL_BEHANDLE_SOKNAD_AUTOMATISK, false)) {
+            throw Feil("Toggle SKAL_BEHANDLE_SOKNAD_AUTOMATISK er skrudd av.")
+        }
+        return kjørBehandling(
             mottakersAktør = mottakersAktør,
             automatiskBehandlingData = SøknadData(søknad),
             førstegangKjørt = førstegangKjørt,
         )
+    }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     fun kjørBehandlingFødselshendelse(
