260101440731695ea7469e0994c3c55bcf43d8b3
thoalm <thoalm@outlook.com>
2026-09-23 14:34:42 +0200

    NAV-31034: Reverterer til try-catch

--- src/main/kotlin/no/nav/familie/ba/sak/task/BehandleAutomatiskSøknadTask.kt
+++ src/main/kotlin/no/nav/familie/ba/sak/task/BehandleAutomatiskSøknadTask.kt
@@ -1,13 +1,18 @@
 package no.nav.familie.ba.sak.task
 
 import io.opentelemetry.instrumentation.annotations.WithSpan
+import no.nav.familie.ba.sak.common.AutovedtakMåBehandlesManueltFeil
 import no.nav.familie.ba.sak.common.Feil
 import no.nav.familie.ba.sak.common.secureLogger
+import no.nav.familie.ba.sak.integrasjoner.oppgave.OppgaveService
 import no.nav.familie.ba.sak.kjerne.autovedtak.AutovedtakStegService
 import no.nav.familie.ba.sak.kjerne.behandling.BehandlingHentOgPersisterService
 import no.nav.familie.ba.sak.kjerne.fagsak.FagsakService
+import no.nav.familie.ba.sak.kjerne.steg.StegService
 import no.nav.familie.ba.sak.task.dto.BehandleAutomatiskSøknadTaskDTO
+import no.nav.familie.ba.sak.task.dto.ManuellOppgaveType
 import no.nav.familie.kontrakter.felles.jsonMapper
+import no.nav.familie.kontrakter.felles.oppgave.Oppgavetype
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
 import no.nav.familie.prosessering.domene.Task
@@ -24,6 +29,8 @@ import java.util.Properties
 )
 class BehandleAutomatiskSøknadTask(
     private val autovedtakStegService: AutovedtakStegService,
+    private val oppgaveService: OppgaveService,
+    private val stegService: StegService,
     private val fagsakService: FagsakService,
     private val behandlingHentOgPersisterService: BehandlingHentOgPersisterService,
 ) : AsyncTaskStep {
@@ -44,10 +51,21 @@ class BehandleAutomatiskSøknadTask(
             throw Feil("Det er ikke mulig å behandle en søknad automatisk hvis fagsak=$fagsakId har en åpen behandling.")
         }
 
-        autovedtakStegService.kjørAutomatiskBehandlingSøknad(
-            mottakersAktør = fagsak.aktør,
-            nyBehandling = nyBehandling,
-        )
+        try {
+            autovedtakStegService.kjørAutomatiskBehandlingSøknad(
+                mottakersAktør = fagsak.aktør,
+                nyBehandling = nyBehandling,
+            )
+        } catch (feil: AutovedtakMåBehandlesManueltFeil) {
+            val behandling = stegService.håndterNyBehandlingOgSendInfotrygdFeed(nyBehandling)
+            oppgaveService.opprettOppgaveForManuellBehandling(
+                behandlingId = behandling.id,
+                begrunnelse = "Ikke kandidat for automatisk behandling. Må behandles manuelt.",
+                manuellOppgaveType = ManuellOppgaveType.SØKNAD,
+                oppgavetype = Oppgavetype.BehandleSak,
+            )
+            logger.info("Henlegger automatisk behandling og oppretter oppgave: ${feil.message}")
+        }
     }
 
     companion object {
